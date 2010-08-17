492fa760ff98d0a9834850c4133d8cc15a3edb49
abhishek <abhishek@cloud.com>
2010-08-17 15:19:09 -0700

    Removing unused imports

--- server/src/com/cloud/api/commands/UpdateIsoPermissionsCmd.java
+++ server/src/com/cloud/api/commands/UpdateIsoPermissionsCmd.java
@@ -2,23 +2,17 @@ package com.cloud.api.commands;
 
 import org.apache.log4j.Logger;
 
-import com.cloud.storage.VMTemplateVO;
-import com.cloud.storage.Storage.ImageFormat;
+import com.cloud.api.Implementation;
+import com.cloud.api.BaseCmd.Manager;
 
+@Implementation(method="updateTemplatePermissions", manager=Manager.ManagementServer)
 public class UpdateIsoPermissionsCmd extends UpdateTemplateOrIsoPermissionsCmd {
     protected String getResponseName() {
     	return "updateisopermissionsresponse";
     }
-    
-	protected String getMediaType() {
-    	return "iso";
-    }
-	
+    	
 	protected Logger getLogger() {
 		return Logger.getLogger(UpdateIsoPermissionsCmd.class.getName());    
 	}
 	
-	protected boolean templateIsCorrectType(VMTemplateVO template) {
-		return template.getFormat().equals(ImageFormat.ISO);
-	}
 }
