02837e04f52ef0c12e3f4e0e397d6249a44ee3a4
Chiradeep Vittal <chiradeep@cloud.com>
2010-08-27 18:22:22 -0700

    Issue #: 4203 restart java agent

--- console-proxy/scripts/run.sh
+++ console-proxy/scripts/run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#run.sh runs the agent client.
+#_run.sh runs the agent client.
 
 # set -x
  
 while true
 do
-  ./run.sh "$@"
+  ./_run.sh "$@"
   ex=$?
   if [ $ex -eq 0 ] || [ $ex -eq 1 ] || [ $ex -eq 66 ] || [ $ex -gt 128 ]; then
     exit $ex
