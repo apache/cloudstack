413ec914ed4849705cab6a856e30b7116d784ee3
nit <nitin@cloud.com>
2010-09-08 18:35:07 +0530

    bug 4286: Reverting server-setup.xml checked in accidentally, added more tags to extract template and put in a user and domain check for executing the command.

--- server/src/com/cloud/api/commands/ExtractTemplateCmd.java
+++ server/src/com/cloud/api/commands/ExtractTemplateCmd.java
@@ -9,6 +9,8 @@ import org.apache.log4j.Logger;
 
 import com.cloud.api.BaseCmd;
 import com.cloud.api.ServerApiException;
+import com.cloud.dc.DataCenterVO;
+import com.cloud.server.ManagementServer;
 import com.cloud.storage.VMTemplateVO;
 import com.cloud.user.Account;
 import com.cloud.utils.Pair;
@@ -34,41 +36,44 @@ public class ExtractTemplateCmd extends BaseCmd {
 		Long zoneId		   = (Long) params.get(BaseCmd.Properties.ZONE_ID.getName());
 		Account account = (Account) params.get(BaseCmd.Properties.ACCOUNT_OBJ.getName());				
 		
-        VMTemplateVO template = getManagementServer().findTemplateById(templateId.longValue());
+		ManagementServer managementServer = getManagementServer();
+        VMTemplateVO template = managementServer.findTemplateById(templateId.longValue());
         if (template == null) {
             throw new ServerApiException(BaseCmd.INTERNAL_ERROR, "Unable to find template with id " + templateId);
+        }
+        if (template.getName().startsWith("xs-tools") ){
+        	throw new ServerApiException(BaseCmd.INTERNAL_ERROR, "Unable to extract the template " + template.getName() + " It is not supported yet");
         }
 		
         if(url.toLowerCase().contains("file://")){
         	throw new ServerApiException(BaseCmd.PARAM_ERROR, "file:// type urls are currently unsupported");
         }
-        
-        boolean isAdmin;
-    	if (account == null) {
-    		// Admin API call
-    		isAdmin = true;
-    	} else {
-    		// User API call
-    		isAdmin = isAdmin(account.getType());
-    	}
-    	
-    	if(!isAdmin){
-    		if (template.getAccountId() != account.getId()){
-    			throw new ServerApiException(BaseCmd.PARAM_ERROR, "Unable to find template with ID: " + templateId + " for account: " + account.getAccountName());
+                
+    	if (account != null) {    		    	
+    		if(!isAdmin(account.getType())){
+    			if (template.getAccountId() != account.getId()){
+    				throw new ServerApiException(BaseCmd.PARAM_ERROR, "Unable to find template with ID: " + templateId + " for account: " + account.getAccountName());
+    			}
+    		}else if(!managementServer.isChildDomain(account.getDomainId(), managementServer.findDomainIdByAccountId(template.getAccountId())) ) {
+    			throw new ServerApiException(BaseCmd.PARAM_ERROR, "Unable to extract template " + templateId + " to " + url + ", permission denied.");
     		}
     	}
     	
         try {
-			getManagementServer().extractTemplate(url, templateId, zoneId);
+			managementServer.extractTemplate(url, templateId, zoneId);
 		} catch (Exception e) {			
 			s_logger.error(e.getMessage(), e);
             throw new ServerApiException(BaseCmd.INTERNAL_ERROR, "Internal Error Extracting the template " + e.getMessage());
-		}				
-				
+		}
+		DataCenterVO zone = managementServer.getDataCenterBy(zoneId);		
 		List<Pair<String, Object>> response = new ArrayList<Pair<String, Object>>();
-		response.add(new Pair<String, Object>("template", templateId));
-		response.add(new Pair<String, Object>("url", url));
-		response.add(new Pair<String, Object>("zoneid", zoneId));
+		response.add(new Pair<String, Object>(BaseCmd.Properties.TEMPLATE_ID.getName(), templateId));
+		response.add(new Pair<String, Object>(BaseCmd.Properties.NAME.getName(), template.getName()));
+		response.add(new Pair<String, Object>(BaseCmd.Properties.DISPLAY_TEXT.getName(), template.getDisplayText()));
+		response.add(new Pair<String, Object>(BaseCmd.Properties.URL.getName(), url));
+		response.add(new Pair<String, Object>(BaseCmd.Properties.ZONE_ID.getName(), zoneId));
+		response.add(new Pair<String, Object>(BaseCmd.Properties.ZONE_NAME.getName(), zone.getName()));
+		response.add(new Pair<String, Object>(BaseCmd.Properties.TEMPLATE_STATUS.getName(), "Processing"));		
 		return response;
 	}
 
