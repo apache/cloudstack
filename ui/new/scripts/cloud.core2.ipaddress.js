c91a49dfdc52cc8aa5fbd2973b6868a40afad3db
Jessica Wang <jessica@cloud.com>
2010-09-09 11:55:25 -0700

    new UI - add JS file for IP Address page.

---
+++ ui/new/scripts/cloud.core2.ipaddress.js
@@ -0,0 +1,9 @@
+function loadIpToRigntPanelFn($rightPanelContent) {   
+    var jsonObj = $rightPanelContent.data("jsonObj");
+    
+    var $rightPanelContent = $("#right_panel_content");
+    
+    //$rightPanelContent.find("#type").text(jsonObj.type);
+    //$rightPanelContent.find("#description").text(jsonObj.description);    
+    //setDateField(jsonObj.sent, $rightPanelContent.find("#sent"));	
+}
