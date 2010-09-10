994afb0e293ad22f2cc46e5607be05477194a3b0
Alex Huang <alex@cloud.com>
2010-09-09 17:49:19 -0700

    switched from networkprofile to network configuration

---
+++ server/src/com/cloud/network/NetworkAccountDaoImpl.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.cloud.network;
+
+import com.cloud.utils.db.GenericDao;
+import com.cloud.utils.db.GenericDaoBase;
+
+public class NetworkAccountDaoImpl extends GenericDaoBase<NetworkAccountVO, Long> implements GenericDao<NetworkAccountVO, Long> {
+    public NetworkAccountDaoImpl() {
+        super();
+    }
+}
