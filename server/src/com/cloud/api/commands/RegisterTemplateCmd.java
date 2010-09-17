baeb1af265d84cefa4281bd89a7b60953b034594
edison <edison@cloud.com>
2010-09-17 11:04:06 -0700

    fix register template hypervisor type

--- server/src/com/cloud/api/commands/RegisterTemplateCmd.java
+++ server/src/com/cloud/api/commands/RegisterTemplateCmd.java
@@ -59,7 +59,7 @@ public class RegisterTemplateCmd extends BaseCmd {
         s_properties.add(new Pair<Enum, Boolean>(BaseCmd.Properties.FORMAT, Boolean.TRUE));
         s_properties.add(new Pair<Enum, Boolean>(BaseCmd.Properties.OS_TYPE_ID, Boolean.TRUE));
         s_properties.add(new Pair<Enum, Boolean>(BaseCmd.Properties.ZONE_ID, Boolean.TRUE));
-        s_properties.add(new Pair<Enum, Boolean>(BaseCmd.Properties.HYPERVISOR_TYPE, Boolean.FALSE));
+        s_properties.add(new Pair<Enum, Boolean>(BaseCmd.Properties.HYPERVISOR, Boolean.FALSE));
         
     }
 
@@ -89,7 +89,7 @@ public class RegisterTemplateCmd extends BaseCmd {
         String format = (String)params.get(BaseCmd.Properties.FORMAT.getName());
         Long guestOSId = (Long) params.get(BaseCmd.Properties.OS_TYPE_ID.getName());
         Long zoneId = (Long) params.get(BaseCmd.Properties.ZONE_ID.getName());
-        HypervisorType hyperType = (HypervisorType) params.get(BaseCmd.Properties.HYPERVISOR_TYPE.getName());
+        HypervisorType hyperType = (HypervisorType) params.get(BaseCmd.Properties.HYPERVISOR.getName());
 
         //parameters verification
         if (bits == null) {
