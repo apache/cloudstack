e4b3be97a469c3e37a1d749853991f6f64377ab9
edison <edison@cloud.com>
2010-09-22 14:32:16 -0700

    guest os name can be null

--- setup/db/create-schema.sql
+++ setup/db/create-schema.sql
@@ -980,7 +980,7 @@ CREATE TABLE  `cloud`.`template_spool_ref` (
 CREATE TABLE `cloud`.`guest_os` (
   `id` bigint unsigned NOT NULL auto_increment,
   `category_id` bigint unsigned NOT NULL,
-  `name` varchar(255) NOT NULL,
+  `name` varchar(255),
   `display_name` varchar(255) NOT NULL,
   PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
