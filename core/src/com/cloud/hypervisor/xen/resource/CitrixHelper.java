43bd6d1d2d321935f2ecd7221405408cb4303f0c
anthony <anthony@cloud.com>
2010-09-29 17:23:38 -0700

    remove guestOS list

--- core/src/com/cloud/hypervisor/xen/resource/CitrixHelper.java
+++ core/src/com/cloud/hypervisor/xen/resource/CitrixHelper.java
@@ -90,74 +90,9 @@ public class CitrixHelper {
         _guestOsMap.put("Windows XP SP2 (32-bit)", "Windows XP SP2");
         _guestOsMap.put("Windows XP SP3 (32-bit)", "Windows XP SP3");
         _guestOsMap.put("Other install media", "Other install media");
-
-        //access by index
-        _guestOsList.add("CentOS 4.5");
-        _guestOsList.add("CentOS 4.6");
-        _guestOsList.add("CentOS 4.7");
-        _guestOsList.add("CentOS 4.8");
-        _guestOsList.add("CentOS 5.0");
-        _guestOsList.add("CentOS 5.0 x64");
-        _guestOsList.add("CentOS 5.1");
-        _guestOsList.add("CentOS 5.1 x64");
-        _guestOsList.add("CentOS 5.2");
-        _guestOsList.add("CentOS 5.2 x64");
-        _guestOsList.add("CentOS 5.3");
-        _guestOsList.add("CentOS 5.3 x64");
-        _guestOsList.add("CentOS 5.4");
-        _guestOsList.add("CentOS 5.4 x64");
-        _guestOsList.add("Debian Lenny 5.0 (32-bit)");
-        _guestOsList.add("Oracle Enterprise Linux 5.0");
-        _guestOsList.add("Oracle Enterprise Linux 5.0 x64");
-        _guestOsList.add("Oracle Enterprise Linux 5.1");
-        _guestOsList.add("Oracle Enterprise Linux 5.1 x64");
-        _guestOsList.add("Oracle Enterprise Linux 5.2");
-        _guestOsList.add("Oracle Enterprise Linux 5.2 x64");
-        _guestOsList.add("Oracle Enterprise Linux 5.3");
-        _guestOsList.add("Oracle Enterprise Linux 5.3 x64");
-        _guestOsList.add("Oracle Enterprise Linux 5.4");
-        _guestOsList.add("Oracle Enterprise Linux 5.4 x64");
-        _guestOsList.add("Red Hat Enterprise Linux 4.5");
-        _guestOsList.add("Red Hat Enterprise Linux 4.6");
-        _guestOsList.add("Red Hat Enterprise Linux 4.7");
-        _guestOsList.add("Red Hat Enterprise Linux 4.8");
-        _guestOsList.add("Red Hat Enterprise Linux 5.0");
-        _guestOsList.add("Red Hat Enterprise Linux 5.0 x64");
-        _guestOsList.add("Red Hat Enterprise Linux 5.1");
-        _guestOsList.add("Red Hat Enterprise Linux 5.1 x64");
-        _guestOsList.add("Red Hat Enterprise Linux 5.2");
-        _guestOsList.add("Red Hat Enterprise Linux 5.2 x64");
-        _guestOsList.add("Red Hat Enterprise Linux 5.3");
-        _guestOsList.add("Red Hat Enterprise Linux 5.3 x64");
-        _guestOsList.add("Red Hat Enterprise Linux 5.4");
-        _guestOsList.add("Red Hat Enterprise Linux 5.4 x64");
-        _guestOsList.add("SUSE Linux Enterprise Server 9 SP4");
-        _guestOsList.add("SUSE Linux Enterprise Server 10 SP1");
-        _guestOsList.add("SUSE Linux Enterprise Server 10 SP1 x64");
-        _guestOsList.add("SUSE Linux Enterprise Server 10 SP2");
-        _guestOsList.add("SUSE Linux Enterprise Server 10 SP2 x64");
-        _guestOsList.add("Other install media");
-        _guestOsList.add("SUSE Linux Enterprise Server 11");
-        _guestOsList.add("SUSE Linux Enterprise Server 11 x64");
-        _guestOsList.add("Windows 7");
-        _guestOsList.add("Windows 7 x64");
-        _guestOsList.add("Windows Server 2003");
-        _guestOsList.add("Windows Server 2003 x64");
-        _guestOsList.add("Windows Server 2008");
-        _guestOsList.add("Windows Server 2008 x64");
-        _guestOsList.add("Windows Server 2008 R2 x64");
-        _guestOsList.add("Windows 2000 SP4");
-        _guestOsList.add("Windows Vista");
-        _guestOsList.add("Windows XP SP2");
-        _guestOsList.add("Windows XP SP3");
-        _guestOsList.add("Other install media");
     }
     
     public static String getGuestOsType(String stdType) {
         return _guestOsMap.get(stdType);
-    }
-
-    public static String getGuestOsType(long guestOsId) {
-        return _guestOsList.get((int) (guestOsId-1));
     }
 }
