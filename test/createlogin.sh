e7d3a05134c35d826c3ef23783063a413fd890e8
Chiradeep Vittal <chiradeep@cloud.com>
2010-10-06 15:39:21 -0700

    accidental commit

--- test/createlogin.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sessionkey=$(curl -c admincookie.txt -sS "http://localhost:8080/client/api?command=login&username=admin&password=5f4dcc3b5aa765d61d8327deb882cf99&response=json" | awk -F "," '{print $13}' | awk -F":" '{print $2}' | tr -d '[:space:]' | tr -d '"')
-
-echo $sessionkey
--- test/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sessionkey=$($(dirname $0)/createlogin.sh)
-$(dirname $0)/setupzone.sh $sessionkey
-$(dirname $0)/addhosts.sh $sessionkey
