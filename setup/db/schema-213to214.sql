18205acb3bac6147114616497c0204b963ceeab2
edison <edison@cloud.com>
2010-10-29 19:31:20 -0700

    add db migration for storage_pool: uuid can be null

--- setup/db/schema-213to214.sql
+++ setup/db/schema-213to214.sql
@@ -1,1 +1,2 @@
 ALTER TABLE `cloud`.`vm_instance` MODIFY COLUMN `pod_id` bigint unsigned;	-- remove NOT NULL constraint to allow creating DB record in early time
+ALTER TABLE `cloud`.`storage_pool` MODIFY COLUMN `uuid` varchar(255) UNIQUE;	-- remove NOT NULL constraint to allow creating DB record in early time
