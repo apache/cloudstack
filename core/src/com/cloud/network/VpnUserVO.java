8158f379805fd84c9a0832777abada01e9008d11
Chiradeep Vittal <chiradeep@cloud.com>
2010-11-02 11:23:37 -0700

    users for vpn -- data model

---
+++ core/src/com/cloud/network/VpnUserVO.java
@@ -0,0 +1,104 @@
+/**
+ *  Copyright (C) 2010 Cloud.com, Inc.  All rights reserved.
+ * 
+ * This software is licensed under the GNU General Public License v3 or later.
+ * 
+ * It is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+package com.cloud.network;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.SecondaryTable;
+import javax.persistence.Table;
+
+@Entity
+@Table(name=("vpn_user"))
+@SecondaryTable(name="account",
+        pkJoinColumns={@PrimaryKeyJoinColumn(name="account_id", referencedColumnName="id")})
+public class VpnUserVO {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    @Column(name="account_id")
+    private long accountId;
+    
+    @Column(name="account_name", table="account", insertable=false, updatable=false)
+    private String accountName = null;
+    
+    @Column(name="domain_id", table="account", insertable=false, updatable=false)
+    private long domainId;
+
+    @Column(name="username")
+    private String userName;
+    
+    @Column(name="password")
+    private String password;
+
+    public VpnUserVO() { }
+
+    public VpnUserVO(long accountId, String userName, String password) {
+        this.accountId = accountId;
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public long getAccountId() {
+        return accountId;
+    }
+    
+    public String getAccountName() {
+        return accountName;
+    }
+
+
+
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public long getDomainId() {
+		return domainId;
+	}
+    
+    
+}
---
+++ core/src/com/cloud/network/dao/VpnUserDaoImpl.java
@@ -0,0 +1,66 @@
+/**
+ *  Copyright (C) 2010 Cloud.com, Inc.  All rights reserved.
+ * 
+ * This software is licensed under the GNU General Public License v3 or later.
+ * 
+ * It is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+package com.cloud.network.dao;
+
+import java.util.List;
+
+import javax.ejb.Local;
+
+import org.apache.log4j.Logger;
+
+import com.cloud.network.VpnUserVO;
+import com.cloud.utils.db.GenericDaoBase;
+import com.cloud.utils.db.SearchBuilder;
+import com.cloud.utils.db.SearchCriteria;
+
+@Local(value={RemoteAccessVpnDao.class})
+public class VpnUserDaoImpl extends GenericDaoBase<VpnUserVO, Long> implements VpnUserDao {
+    private static final Logger s_logger = Logger.getLogger(VpnUserDaoImpl.class);
+    private final SearchBuilder<VpnUserVO> AccountSearch;
+    private final SearchBuilder<VpnUserVO> AccountNameSearch;
+
+
+    protected VpnUserDaoImpl() {
+
+        AccountSearch = createSearchBuilder();
+        AccountSearch.and("accountId", AccountSearch.entity().getAccountId(), SearchCriteria.Op.EQ);
+        AccountSearch.done();
+        
+        AccountNameSearch = createSearchBuilder();
+        AccountNameSearch.and("accountId", AccountNameSearch.entity().getAccountId(), SearchCriteria.Op.EQ);
+        AccountNameSearch.and("userName", AccountNameSearch.entity().getUserName(), SearchCriteria.Op.EQ);
+        AccountNameSearch.done();
+    }
+
+    @Override
+    public List<VpnUserVO> listByAccount(Long accountId) {
+        SearchCriteria<VpnUserVO> sc = AccountSearch.create();
+        sc.setParameters("accountId", accountId);
+        return listBy(sc);
+    }
+
+	@Override
+	public VpnUserVO findByAccountAndUsername(Long accountId, String userName) {
+		SearchCriteria<VpnUserVO> sc = AccountSearch.create();
+        sc.setParameters("accountId", accountId);
+        sc.setParameters("userName", userName);
+
+        return findOneBy(sc);
+	}
+}
