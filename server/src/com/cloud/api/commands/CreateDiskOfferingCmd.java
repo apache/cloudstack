3deb7580bb2dfc8abecebe1dc24174f228a3eeb0
Jessica Wang <jessica@cloud.com>
2010-11-03 19:14:35 -0700

    Create Disk Offering - make disksize optional

--- server/src/com/cloud/api/commands/CreateDiskOfferingCmd.java
+++ server/src/com/cloud/api/commands/CreateDiskOfferingCmd.java
@@ -39,7 +39,7 @@ public class CreateDiskOfferingCmd extends BaseCmd {
     //////////////// API parameters /////////////////////
     /////////////////////////////////////////////////////
 
-    @Parameter(name=ApiConstants.DISK_SIZE, type=CommandType.LONG, description="disk size of the disk offering in GB")
+    @Parameter(name=ApiConstants.DISK_SIZE, type=CommandType.LONG, required=false, description="disk size of the disk offering in GB")
     private Long diskSize;
 
     @Parameter(name=ApiConstants.DISPLAY_TEXT, type=CommandType.STRING, required=true, description="alternate display text of the disk offering")
