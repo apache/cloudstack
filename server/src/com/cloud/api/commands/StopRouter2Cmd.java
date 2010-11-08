734cd0b1b1080d377b62daf22ce1096cb1cde178
Alex Huang <alex@cloud.com>
2010-11-08 09:56:00 -0800

    fixed some bugs

---
+++ server/src/com/cloud/api/commands/StopRouter2Cmd.java
@@ -0,0 +1,93 @@
+/**
+ *  Copyright (C) 2010 Cloud.com, Inc.  All rights reserved.
+ * 
+ * This software is licensed under the GNU General Public License v3 or later.
+ * 
+ * It is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+package com.cloud.api.commands;
+
+import org.apache.log4j.Logger;
+
+import com.cloud.api.ApiConstants;
+import com.cloud.api.ApiDBUtils;
+import com.cloud.api.ApiResponseHelper;
+import com.cloud.api.BaseAsyncCmd;
+import com.cloud.api.Implementation;
+import com.cloud.api.Parameter;
+import com.cloud.api.response.DomainRouterResponse;
+import com.cloud.event.EventTypes;
+import com.cloud.network.DomainRouterService;
+import com.cloud.user.Account;
+import com.cloud.uservm.UserVm;
+import com.cloud.vm.DomainRouter;
+
+
+@Implementation(method="stopRouter", manager=DomainRouterService.class, description="Stops a router.")
+public class StopRouter2Cmd extends BaseAsyncCmd {
+	public static final Logger s_logger = Logger.getLogger(StopRouter2Cmd.class.getName());
+    private static final String s_name = "stoprouterresponse";
+
+    /////////////////////////////////////////////////////
+    //////////////// API parameters /////////////////////
+    /////////////////////////////////////////////////////
+
+    @Parameter(name=ApiConstants.ID, type=CommandType.LONG, required=true, description="the ID of the router")
+    private Long id;
+
+    /////////////////////////////////////////////////////
+    /////////////////// Accessors ///////////////////////
+    /////////////////////////////////////////////////////
+
+    public Long getId() {
+        return id;
+    }
+
+    /////////////////////////////////////////////////////
+    /////////////// API Implementation///////////////////
+    /////////////////////////////////////////////////////
+
+    @Override
+    public String getName() {
+        return s_name;
+    }
+
+    @Override
+    public long getAccountId() {
+        UserVm vm = ApiDBUtils.findUserVmById(getId());
+        if (vm != null) {
+            return vm.getAccountId();
+        }
+
+        return Account.ACCOUNT_ID_SYSTEM; // no account info given, parent this command to SYSTEM so ERROR events are tracked
+    }
+
+    @Override
+    public String getEventType() {
+        return EventTypes.EVENT_ROUTER_STOP;
+    }
+
+    @Override
+    public String getEventDescription() {
+        return  "stopping router: " + getId();
+    }
+
+    @Override @SuppressWarnings("unchecked")
+    public DomainRouterResponse getResponse() {
+        DomainRouter router = (DomainRouter)getResponseObject();
+        DomainRouterResponse response =ApiResponseHelper.createDomainRouterResponse(router);
+        response.setResponseName(getName());
+        return response;
+    }
+}
