a241a3fe542061145c8a8ca5fe2b6d8ffb366d20
Jessica Wang <jessica@cloud.com>
2010-11-24 15:13:33 -0800

    new UI - Event page - check if jsonObj is null or not before getting its property.

--- ui/scripts/cloud.core.event.js
+++ ui/scripts/cloud.core.event.js
@@ -43,13 +43,17 @@ function eventToRightPanel($midmenuItem1) {
 }
 
 function eventJsonToDetailsTab() {   
+    var $midmenuItem1 = $("#right_panel_content").data("$midmenuItem1");
+    if($midmenuItem1 == null)
+        return;
+    
+    var jsonObj = $midmenuItem1.data("jsonObj");
+    if(jsonObj == null)
+        return;    
+    
     var $thisTab = $("#right_panel_content #tab_content_details");  
     $thisTab.find("#tab_container").hide(); 
-    $thisTab.find("#tab_spinning_wheel").show();        
-    
-    var $midmenuItem1 = $("#right_panel_content").data("$midmenuItem1");
-    var jsonObj = $midmenuItem1.data("jsonObj");  
-    $thisTab.data("jsonObj", jsonObj);    
+    $thisTab.find("#tab_spinning_wheel").show();    
          
     $thisTab.find("#id").text(noNull(jsonObj.id));
     $thisTab.find("#username").text(fromdb(jsonObj.username));
