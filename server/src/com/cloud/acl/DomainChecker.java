402baae9fabb73ca0a1774f0831ffc4222d5a348
abhishek <abhishek@cloud.com>
2010-11-30 14:09:07 -0800

    adding comments for the domain checker, so that the design decision is better understood down the road

--- server/src/com/cloud/acl/DomainChecker.java
+++ server/src/com/cloud/acl/DomainChecker.java
@@ -116,8 +116,8 @@ public class DomainChecker extends AdapterBase implements SecurityChecker {
 			{
 				return true;
 			}		
-			//if account is normal user
-			//check if account's domain is a child of zone's domain
+			//if account is normal user or domain admin
+			//check if account's domain is a child of zone's domain (Note: This is made consistent with the list command for service offering)
 			else if(account.getType() == Account.ACCOUNT_TYPE_NORMAL || account.getType() == Account.ACCOUNT_TYPE_DOMAIN_ADMIN)
 			{
 				if(account.getDomainId() == so.getDomainId())
