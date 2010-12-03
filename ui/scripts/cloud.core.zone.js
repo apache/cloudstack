e393e15f7a9fe890ca2c8e61e066e29d782ab3a6
Jessica Wang <jessica@cloud.com>
2010-12-03 11:19:28 -0800

    fix a bug that "no items available" was not removed when primary storage was created.

--- ui/scripts/cloud.core.zone.js
+++ ui/scripts/cloud.core.zone.js
@@ -1195,7 +1195,7 @@ function initAddPrimaryStorageButtonOnZonePage($button, zoneId, zoneName) {
 					    }
 					    else {	
 					        var $container = $("#midmenu_container").find("#midmenu_primarystorage_container");
-					        var $noItemsAvailable = $container.siblings("#midmenu_container_no_items_available");
+					        var $noItemsAvailable = $container.find("#midmenu_container_no_items_available_clone");
 					        if($noItemsAvailable.length > 0) {
 					            $noItemsAvailable.slideUp("slow", function() {
 					                $(this).remove();
