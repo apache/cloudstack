cebf829f137b992998db6fee8d0779311d415d22
abhishek <abhishek@cloud.com>
2010-12-10 14:03:36 -0800

    fixing add host failure

--- server/src/com/cloud/dc/DataCenterIpAddressVO.java
+++ server/src/com/cloud/dc/DataCenterIpAddressVO.java
@@ -56,9 +56,6 @@ public class DataCenterIpAddressVO {
     @Column(name="instance_id")
     private Long instanceId;
     
-    @Column(name="mac_address")
-    private long macAddress;
-    
     protected DataCenterIpAddressVO() {
     }
     
@@ -70,15 +67,10 @@ public class DataCenterIpAddressVO {
         this.reservationId = reservationId;
     }
 
-    public DataCenterIpAddressVO(String ipAddress, long dataCenterId, long podId, long macAddress) {
+	public DataCenterIpAddressVO(String ipAddress, long dataCenterId, long podId) {
         this.ipAddress = ipAddress;
         this.dataCenterId = dataCenterId;
         this.podId = podId;
-        this.macAddress = macAddress;
-    }
-    
-    public long getMacAddress() {
-        return macAddress;
     }
     
     public Long getId() {
