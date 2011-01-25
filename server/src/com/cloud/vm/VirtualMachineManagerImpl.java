00ebbbed6dd76357dec574124a0c5fc657d15c96
kishan <kishan@cloud.com>
2011-01-25 15:51:20 +0530

    bug 8098, 8126: Added vm destroy event during expunge status 8098,8126: resolved fixed

--- server/src/com/cloud/vm/VirtualMachineManagerImpl.java
+++ server/src/com/cloud/vm/VirtualMachineManagerImpl.java
@@ -381,6 +381,10 @@ public class VirtualMachineManagerImpl implements VirtualMachineManager {
             s_logger.debug("Expunged " + vm);
         }
 
+        UsageEventVO usageEvent = new UsageEventVO(EventTypes.EVENT_VM_DESTROY, vm.getAccountId(), vm.getDataCenterId(), vm.getId(), vm.getName(), vm.getServiceOfferingId(), vm.getTemplateId(), null);
+        _usageEventDao.persist(usageEvent);
+
+        
         return true;
     }
 
