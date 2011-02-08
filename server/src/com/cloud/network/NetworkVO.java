0abd36c96f25d859fc25ec452eaa01df9f70c8fb
Alex Huang <alex@cloud.com>
2011-02-08 04:36:57 -0800

    Change the wording in log

--- server/src/com/cloud/network/NetworkVO.java
+++ server/src/com/cloud/network/NetworkVO.java
@@ -429,7 +429,7 @@ public class NetworkVO implements Network {
     
     @Override
     public String toString() {
-        StringBuilder buf = new StringBuilder("NtwkCfg[");
+        StringBuilder buf = new StringBuilder("Ntwk[");
         buf.append(id).append("|").append(trafficType.toString()).append("|").append(networkOfferingId).append("]");
         return buf.toString();
     }
