ea8d7a81cb3141d66ca86a2a1b3312ce95ad9a73
Kelven Yang <kelven@cloud.com>
2011-02-08 16:49:20 -0800

    Password reset tool for windows platform

---
+++ pwdmgr-win/win/codesign/sign-vmops.bat
@@ -0,0 +1,6 @@
+signtool sign /f vmopscert.pfx /p vmops /t http://tsa.starfieldtech.com %1\VMOpsInstanceManager.msi
+signtool sign /f vmopscert.pfx /p vmops /t http://tsa.starfieldtech.com %1\VMOpsInstanceManager.msi
+
+
+
+
---
+++ pwdmgr-win/win/service/VMOpsServiceImpl.h
@@ -0,0 +1,100 @@
+//
+// VMOpsServiceImpl.h
+// VMOps instance manager implementation
+//
+// Copyright (C) VMOps Inc.
+// All rights reserved.
+//
+#ifndef __VMOpsServiceImpl_H__
+#define __VMOpsServiceImpl_H__
+
+#include "VMOpsError.h"
+#include "ThreadUtil.h"
+
+#include <stdio.h>
+
+namespace VMOps {
+
+/////////////////////////////////////////////////////////////////////////////
+// class diagram
+//
+class CVMOpsServiceProvider;
+class CThread;
+		class CVMOpsStartupWatcher;
+
+class CLogger;
+
+/////////////////////////////////////////////////////////////////////////////
+// CVMOpsServiceProvider
+//
+class CVMOpsServiceProvider
+{
+public :
+	CVMOpsServiceProvider();
+	~CVMOpsServiceProvider();
+
+public :
+	HERROR SetPassword(LPCTSTR lpszUserName, LPCTSTR lpszPassword);
+	HERROR GetDefaultGateway(LPSTR lpszBuf, LPDWORD pdwLength);
+	HERROR SimpleHttpGet(LPCTSTR lpszUrl, LPCTSTR lpszHeaders, 
+		LPVOID pOutputBuffer, DWORD dwBytesToRead, DWORD* pdwBytesRead);
+
+	HERROR Start();
+	HERROR Stop();
+
+protected :
+	CVMOpsStartupWatcher* m_pWatcher;
+};
+
+/////////////////////////////////////////////////////////////////////////////
+// CVMOpsStartupWatcher
+//
+class CVMOpsStartupWatcher : public CThread
+{
+public :
+	CVMOpsStartupWatcher(CVMOpsServiceProvider* pProvider);
+	virtual ~CVMOpsStartupWatcher();
+
+public :
+	CVMOpsServiceProvider* GetProvider() { return m_pProvider; }
+
+protected :
+	virtual DWORD ThreadRun();
+
+	BOOL DoStartupConfig();
+	BOOL GetDomRUrl(LPTSTR lpszUrl);
+
+protected :
+	CVMOpsServiceProvider* m_pProvider;
+};
+
+/////////////////////////////////////////////////////////////////////////////
+// CLogger
+// A simple logger for internal use
+//
+class CLogger
+{
+public :
+	CLogger();
+	~CLogger();
+
+public :
+	static CLogger* GetInstance();
+	BOOL Initialize();
+	void RotateLog();
+	void Cleanup();
+
+	void Log(LPCSTR lpszCategory, LPCSTR lpszFormat, ...);
+
+private :
+	CCriticalSection m_lock;
+	FILE* m_pFile;
+
+private :
+	static CLogger* s_pInstance;
+};
+
+}
+#endif	// __VMOpsServiceProvider_H__
+
+/////////////////////////////////////////////////////////////////////////////
