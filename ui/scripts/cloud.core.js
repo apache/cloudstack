d764ee92e15a74d49abeb9553753de04c6843c5c
will <will@cloud.com>
2011-03-15 16:33:10 -0700

    - Limit the new domain search to 20 items.

--- ui/scripts/cloud.core.js
+++ ui/scripts/cloud.core.js
@@ -73,7 +73,7 @@ function applyAutoCompleteToDomainField($field) {
     $field.autocomplete({
 		source: function(request, response) {			
 			$.ajax({
-			    data: "command=listDomains&path=" + encodeURIComponent(request.term) +"&response=json&pagesize=20",				
+			    data: "command=listDomains&path=" + encodeURIComponent(request.term) +"&response=json&pagesize=10",				
 				dataType: "json",
 				async: false,
 				success: function(json) {					   
