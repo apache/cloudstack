197a09f29d3181862ee10a31e3334a24c612a5f0
nit <nitin@cloud.com>
2011-03-18 17:20:44 -0700

    217 to 218 data migration for op_host_capacity. We dont want to use this table for secondary/primary storage used

--- setup/db/data-217to218.sql
+++ setup/db/data-217to218.sql
@@ -1,1 +1,2 @@
 INSERT INTO `cloud`.`configuration` VALUES ('Advanced', 'DEFAULT', 'management-server', 'default.page.size', '500', 'Default page size for API list* commands');
+delete FROM `cloud`.`op_host_capacity` where capacity_type in (2,6);
