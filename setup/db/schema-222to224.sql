7103d30f57c603f88a878bbef1395b6b600bafdb
Alex Huang <alex@cloud.com>
2011-03-22 09:46:41 -0700

    router restarted but needs template update

--- setup/db/schema-222to224.sql
+++ setup/db/schema-222to224.sql
@@ -4,6 +4,7 @@
 ALTER TABLE `cloud`.`op_host_capacity` ADD COLUMN `cluster_id` bigint unsigned AFTER `pod_id`;
 ALTER TABLE `cloud`.`op_host_capacity` ADD CONSTRAINT `fk_op_host_capacity__cluster_id` FOREIGN KEY `fk_op_host_capacity__cluster_id` (`cluster_id`) REFERENCES `cloud`.`cluster`(`id`) ON DELETE CASCADE;
 ALTER TABLE `cloud`.`op_host_capacity` ADD INDEX `i_op_host_capacity__cluster_id`(`cluster_id`);
+ALTER TABLE `cloud`.`usage_event` ADD COLUMN `resource_type` varchar(32);
 
 CREATE TABLE `cloud`.`domain_network_ref` (
   `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT 'id',
