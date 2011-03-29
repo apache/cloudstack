2e284e1f75718e5d839ebc664bfe3e0a63eadab2
Kelven Yang <kelven@cloud.com>
2011-03-28 18:41:17 -0700

    Add system VM load scaner

---
+++ server/src/com/cloud/vm/SystemVmLoadScanHandler.java
@@ -0,0 +1,18 @@
+package com.cloud.vm;
+
+import com.cloud.vm.SystemVmLoadScanner.AfterScanAction;
+
+public interface SystemVmLoadScanHandler<T> {
+	boolean canScan();
+
+	void onScanStart();
+	
+	T[] getScannablePools();
+	boolean isPoolReadyForScan(T pool);
+	AfterScanAction scanPool(T pool);
+	void expandPool(T pool);
+	void shrinkPool(T pool);
+	
+	void onScanEnd();
+}
+
