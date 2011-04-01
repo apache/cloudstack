2c8509231649c437c2cba9d08679034e77a03313
anthony <anthony@cloud.com>
2011-03-31 18:23:32 -0700

    bug 9161: better fix

--- server/src/com/cloud/vm/ItWorkDaoImpl.java
+++ server/src/com/cloud/vm/ItWorkDaoImpl.java
@@ -42,7 +42,7 @@ public class ItWorkDaoImpl extends GenericDaoBase<ItWorkVO, String> implements I
         AllFieldsSearch = createSearchBuilder();
         AllFieldsSearch.and("instance", AllFieldsSearch.entity().getInstanceId(), Op.EQ);
         AllFieldsSearch.and("op", AllFieldsSearch.entity().getType(), Op.EQ);
-        AllFieldsSearch.and("step", AllFieldsSearch.entity().getStep(), Op.NEQ);
+        AllFieldsSearch.and("step", AllFieldsSearch.entity().getStep(), Op.EQ);
         AllFieldsSearch.done();
         
         CleanupSearch = createSearchBuilder();
@@ -64,7 +64,7 @@ public class ItWorkDaoImpl extends GenericDaoBase<ItWorkVO, String> implements I
     
     @Override
     public ItWorkVO findByOutstandingWork(long instanceId, State state) {
-        SearchCriteria<ItWorkVO> sc = AllFieldsSearch.create();
+        SearchCriteria<ItWorkVO> sc = OutstandingWorkSearch.create();
         sc.setParameters("instance", instanceId);
         sc.setParameters("op", state);
         sc.setParameters("step", Step.Done);
