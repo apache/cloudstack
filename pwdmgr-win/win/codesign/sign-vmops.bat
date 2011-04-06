a748ddf35da252a133858f3a3b6c14112f15b993
Frank <frank@cloud.com>
2011-04-05 21:55:27 -0700

    Bug 9302
    Resolved Fixed

--- pwdmgr-win/win/codesign/sign-vmops.bat
+++
@@ -1,7 +0,0 @@
-REM signtool sign /f vmopscert.pfx /p vmops /t http://tsa.starfieldtech.com %1\CloudInstanceManager.msi
-REM signtool sign /f vmopscert.pfx /p vmops /t http://tsa.starfieldtech.com %1\CloudInstanceManager.msi
-signtool sign /f code_signing_cert.pfx /p vmops.com /t http://tsa.starfieldtech.com %1\CloudInstanceManager.msi
-signtool sign /f code_signing_cert.pfx /p vmops.com /t http://tsa.starfieldtech.com %1\CloudInstanceManager.msi
-
-
-
