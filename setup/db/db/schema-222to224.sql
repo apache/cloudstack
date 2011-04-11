77e183cc1794b673704c7c040fcf54ce1dd97069
alena <alena@cloud.com>
2011-04-11 10:35:45 -0700

    bug 9376: added missing constraints to vm_instance table status 9376: resolved fixed

--- setup/db/db/schema-222to224.sql
+++ setup/db/db/schema-222to224.sql
@@ -129,3 +129,6 @@ ALTER TABLE `cloud`.`op_dc_link_local_ip_address_alloc` CHANGE COLUMN `instance_
 DELETE FROM `sequence` WHERE name='snapshots_seq';
 
 UPDATE `cloud`.`service_offering` s, `cloud`.`disk_offering` d SET s.ha_enabled=1 where s.id=d.id and d.system_use=1;
+
+ALTER TABLE `cloud`.`vm_instance` ADD CONSTRAINT `fk_vm_instance__account_id` FOREIGN KEY `fk_vm_instance__account_id` (`account_id`) REFERENCES `account` (`id`);
+ALTER TABLE `cloud`.`vm_instance` ADD CONSTRAINT `fk_vm_instance__service_offering_id` FOREIGN KEY `fk_vm_instance__service_offering_id` (`service_offering_id`) REFERENCES `service_offering` (`id`);
