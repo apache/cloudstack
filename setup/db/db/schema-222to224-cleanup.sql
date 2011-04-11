70151389d1507241bb6c414f08061fe7fd5cb13a
alena <alena@cloud.com>
2011-04-11 13:58:26 -0700

    bug 9396: removed status field from volumes table as this field is no longer used status 9396: resolved fixed

--- setup/db/db/schema-222to224-cleanup.sql
+++ setup/db/db/schema-222to224-cleanup.sql
@@ -4,3 +4,9 @@ DELETE FROM `cloud`.`account_vlan_map` WHERE account_id IS NULL;
 ALTER TABLE `cloud`.`data_center` DROP COLUMN `enable`;
 ALTER TABLE `cloud`.`host_pod_ref` DROP COLUMN `enabled`;
 DELETE FROM `cloud`.`configuration` WHERE name in ('direct.attach.security.groups.enabled', 'direct.attach.untagged.vlan.enabled', 'hypervisor.type', 'management-server', 'max.volume.size.gb', 'multicast.throttling.rate', 'network.type', 'xen.preallocated.lun.size.range');
+
+ALTER TABLE `cloud`.`volumes` DROP COLUMN `status`;
+
+ALTER TABLE `cloud`.`user_vm` DROP COLUMN guest_ip_address;
+ALTER TABLE `cloud`.`user_vm` DROP COLUMN guest_mac_address;
+ALTER TABLE `cloud`.`user_vm` DROP COLUMN guest_netmask;
