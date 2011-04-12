5ee6a8757a0a5ccde46c798ac2e404ccd50b9a34
alena <alena@cloud.com>
2011-04-11 17:55:40 -0700

    bug 9378: db upgrade - update guest_os_id in vm_instance table for system vms. status 9378: resolved fixed

--- setup/db/db/schema-21to22.sql
+++ setup/db/db/schema-21to22.sql
@@ -960,6 +960,7 @@ INSERT INTO `cloud`.`vm_template` (id, unique_name, name, public, created, type,
 
 INSERT INTO `cloud`.`vm_template` (id, unique_name, name, public, created, type, hvm, bits, account_id, url, checksum, enable_password, display_text,  format, guest_os_id, featured, cross_zones, hypervisor_type, extractable)
     VALUES (7, 'centos53-x64', 'CentOS 5.3(64-bit) no GUI (vSphere)', 1, now(), 'BUILTIN', 0, 64, 1, 'http://download.cloud.com/releases/2.2.0/CentOS5.3-x86_64.ova', 'f6f881b7f2292948d8494db837fe0f47', 0, 'CentOS 5.3(64-bit) no GUI (vSphere)', 'OVA', 12, 1, 1, 'VMware', 1);
+UPDATE vm_instance SET guest_os_id=15 where vm_template_id=1;
 UPDATE vm_instance SET vm_template_id=100 where vm_template_id=1;
 
 
