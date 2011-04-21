435c20c0ccb8ca29252a0f3c2a50602ea766f046
Murali Reddy <murali@cloud.com>
2011-04-22 01:19:23 +0530

    Bug 9273: resource cont going -ve
    adding addtional check that ensure we decrease the count in advanceStart only on new vm creation.

--- server/src/com/cloud/vm/VirtualMachineManagerImpl.java
+++ server/src/com/cloud/vm/VirtualMachineManagerImpl.java
@@ -738,7 +738,8 @@ public class VirtualMachineManagerImpl implements VirtualMachineManager, Listene
             }
         } finally {
             if (startedVm == null) {
-                if (vm.getType().equals(VirtualMachine.Type.User)) {
+            	// decrement only for user VM's and newly created VM
+                if (vm.getType().equals(VirtualMachine.Type.User) && (vm.getLastHostId() == null)) {
                     _accountMgr.decrementResourceCount(vm.getAccountId(), ResourceType.user_vm);
                 }
                 changeState(vm, Event.OperationFailed, null, work, Step.Done);
