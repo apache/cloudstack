67689860a0a6e05a4b7365cc05e2d55402dcf564
will <will@cloud.com>
2011-04-22 16:23:03 -0700

    bug 9515: Adding the first basic zone will now automatically display the security group left nav
    merge from master

--- ui/scripts/cloud.core.resource.js
+++ ui/scripts/cloud.core.resource.js
@@ -1395,7 +1395,9 @@ function addZoneWizardSubmit($thisWizard) {
 		    
 		    zoneId = item.id;	
 		    
-		    listZonesUpdate();		           
+		    listZonesUpdate();	
+			g_directAttachSecurityGroupsEnabled = true;
+			$("#leftmenu_security_group_container").show();
 	    },
         error: function(XMLHttpResponse) {            
 			handleError(XMLHttpResponse, function() {
