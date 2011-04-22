eccec4ff1141c0c6b7e8d0d2f2c68ac3f1df1a01
anthony <anthony@cloud.com>
2011-04-22 12:48:57 -0700

    bug 9541: fix one snapshot DB migrate, one java check and one script typo
    status 9541: resolved fixed

--- setup/db/db/schema-snapshot-217to223.sql
+++ setup/db/db/schema-snapshot-217to223.sql
@@ -5,8 +5,8 @@ ALTER table snapshots add column `size` bigint unsigned NOT NULL;
 ALTER table snapshots add column `version` varchar(32) DEFAULT '2.1';
 ALTER table snapshots add column `hypervisor_type` varchar(32) DEFAULT 'XenServer';
 
+DELETE FROM snapshot_policy where id=1;
 UPDATE snapshots s, volumes v SET s.data_center_id=v.data_center_id, s.domain_id=v.domain_id, s.disk_offering_id=v.disk_offering_id, s.size=v.size WHERE s.volume_id = v.id;
-UPDATE snapshots s, snapshot_policy sp, snapshot_policy_ref spr SET s.hypervisor_type=sp.interval+3 WHERE s.id=spr.snap_id and spr.policy_id=sp.id;
+UPDATE snapshots s, snapshot_policy sp, snapshot_policy_ref spr SET s.snapshot_type=sp.interval+3 WHERE s.id=spr.snap_id and spr.policy_id=sp.id;
 
 DROP table snapshot_policy_ref;
-DELETE FROM snapshot_policy WHERE id=1;
