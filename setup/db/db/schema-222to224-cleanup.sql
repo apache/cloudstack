25017382b6c1c234df9ecc51d313c1ff04509600
alena <alena@cloud.com>
2011-04-23 22:17:04 -0700

    bug 9563: fixed DB upgrade from 2.2.2/2.2.3 to 2.2.4 status 9563: resolved fixed
    Also added unittest for 2.2.2 to 2.2.4 upgrade

--- setup/db/db/schema-222to224-cleanup.sql
+++ setup/db/db/schema-222to224-cleanup.sql
@@ -1,6 +1,3 @@
-ALTER TABLE `cloud`.`account_vlan_map` DROP FOREIGN KEY `fk_account_vlan_map__domain_id`;
-ALTER TABLE `cloud`.`account_vlan_map` DROP COLUMN `domain_id`;
-DELETE FROM `cloud`.`account_vlan_map` WHERE account_id IS NULL;
 ALTER TABLE `cloud`.`data_center` DROP COLUMN `enable`;
 ALTER TABLE `cloud`.`host_pod_ref` DROP COLUMN `enabled`;
 DELETE FROM `cloud`.`configuration` WHERE name in ('direct.attach.security.groups.enabled', 'direct.attach.untagged.vlan.enabled', 'hypervisor.type', 'management-server', 'max.volume.size.gb', 'multicast.throttling.rate', 'network.type', 'xen.preallocated.lun.size.range');
