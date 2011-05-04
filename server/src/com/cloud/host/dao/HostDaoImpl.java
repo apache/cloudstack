fd08ebfb577ccf762f0bad36b6aa00a4288d377c
Edison Su <edison@cloud.com>
2011-05-04 15:52:20 -0400

    there are more than one secondary storage in one zone

--- server/src/com/cloud/host/dao/HostDaoImpl.java
+++ server/src/com/cloud/host/dao/HostDaoImpl.java
@@ -268,7 +268,7 @@ public class HostDaoImpl extends GenericDaoBase<HostVO, Long> implements HostDao
     	sc.setParameters("dc", dcId);
     	List<HostVO> storageHosts = listBy(sc);
     	
-    	if (storageHosts == null || storageHosts.size() != 1) {
+    	if (storageHosts == null) {
     		return null;
     	} else {
     		return storageHosts.get(0);
