a6b2d67b982e20033821c1f5eea698959b8a550a
Alex Huang <alex@cloud.com>
2011-05-16 16:35:20 -0700

    propagated bb04d0184ea2d713cf211618b53c56c33dbefb87

--- setup/db/db/schema-222to224.sql
+++ setup/db/db/schema-222to224.sql
@@ -1,6 +1,16 @@
 --;
 -- Schema upgrade from 2.2.2 to 2.2.4;
 --;
+
+CREATE TABLE IF NOT EXISTS `cloud`.`version` (
+  `id` bigint unsigned NOT NULL UNIQUE AUTO_INCREMENT COMMENT 'id',
+  `version` char(40) NOT NULL UNIQUE COMMENT 'version',
+  `updated` datetime NOT NULL COMMENT 'Date this version table was updated',
+  `step` char(32) NOT NULL COMMENT 'Step in the upgrade to this version',
+  PRIMARY KEY (`id`),
+  INDEX `i_version__version`(`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 ALTER TABLE `cloud`.`op_host_capacity` ADD COLUMN `cluster_id` bigint unsigned AFTER `pod_id`;
 ALTER TABLE `cloud`.`op_host_capacity` ADD CONSTRAINT `fk_op_host_capacity__cluster_id` FOREIGN KEY `fk_op_host_capacity__cluster_id` (`cluster_id`) REFERENCES `cloud`.`cluster`(`id`) ON DELETE CASCADE;
 ALTER TABLE `cloud`.`op_host_capacity` ADD INDEX `i_op_host_capacity__cluster_id`(`cluster_id`);
