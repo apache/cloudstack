819e67b189962f595997fa8feb2b0aacc42b7725
Sheng Yang <sheng.yang@cloud.com>
2011-06-01 11:55:22 -0700

    Add file lock for keepalived scripts
    They are not blocked callings.

--- patches/systemvm/debian/config/root/redundant_router/backup.sh
+++ patches/systemvm/debian/config/root/redundant_router/backup.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
 
+LOCK=/tmp/rrouter.lock
+locked=0
+
+# Wait the lock
+for i in `seq 1 5`
+do
+    if [ ! -e $LOCK ]
+    then
+        touch $LOCK
+        locked=1
+        break
+    fi
+    sleep 1
+    echo sleep 1
+done
+
+if [ $locked -eq 0 ]
+then
+    echo Status: fail to get the lock! >> /root/keepalived.log
+    exit
+fi
+
 echo To backup called >> /root/keepalived.log
 /root/redundant_router/disable_pubip.sh
 echo Disable public ip $? >> /root/keepalived.log
 /root/redundant_router/primary-backup.sh backup
 echo Switch conntrackd mode backup $? >> /root/keepalived.log
 echo Status: BACKUP >> /root/keepalived.log
+
+rm $LOCK
--- patches/systemvm/debian/config/root/redundant_router/master.sh
+++ patches/systemvm/debian/config/root/redundant_router/master.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
 
+LOCK=/tmp/rrouter.lock
+locked=0
+
+# Wait the lock
+for i in `seq 1 5`
+do
+    if [ ! -e $LOCK ]
+    then
+        touch $LOCK
+        locked=1
+        break
+    fi
+    sleep 1
+    echo sleep 1
+done
+
+if [ $locked -eq 0 ]
+then
+    echo Status: fail to get the lock! >> /root/keepalived.log
+    exit
+fi
+
 echo To master called >> /root/keepalived.log
 /root/redundant_router/enable_pubip.sh
 echo Enable public ip $? >> /root/keepalived.log
 /root/redundant_router/primary-backup.sh primary
 echo Switch conntrackd mode primary $? >> /root/keepalived.log
 echo Status: MASTER >> /root/keepalived.log
+
+rm $LOCK
