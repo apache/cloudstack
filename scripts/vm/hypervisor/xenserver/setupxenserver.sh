5e1bdce0523cfa27f601cefceb8c163f41433408
anthony <anthony@cloud.com>
2011-06-06 18:19:01 -0700

    bug 10081: don't save iptable

--- scripts/vm/hypervisor/xenserver/setupxenserver.sh
+++ scripts/vm/hypervisor/xenserver/setupxenserver.sh
@@ -12,8 +12,6 @@ usage() {
 iptables -D RH-Firewall-1-INPUT -p tcp -m tcp --dport 5900:6099 -j ACCEPT 2>&1
 iptables -I RH-Firewall-1-INPUT -p tcp -m tcp --dport 5900:6099 -j ACCEPT 2>&1
 
-iptables-save > /etc/sysconfig/iptables
-
 # listen vnc on all interface
 sed -i 's/127\.0\.0\.1/0\.0\.0\.0/' /opt/xensource/libexec/vncterm-wrapper 2>&1
 sed -i 's/127\.0\.0\.1/0\.0\.0\.0/' /opt/xensource/libexec/qemu-dm-wrapper 2>&1
