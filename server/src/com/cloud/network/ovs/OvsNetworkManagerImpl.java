CloudStack-extras/CloudStack-archive

bug 9154: Initial check in for enabling redundant virtual router
This patch enable redundant virtual routers.

1. To enable this feature, db need to be updated using follow SQL by now(we
would get a UI way later):

UPDATE network_offerings SET redundant_router=1 WHERE guest_type="Virtual" AND
system_only=0;

2. System would try to start up two routers at different hosts. But if there is
only one host in the zone, system would start up two routers on it.

3. The failover part is using keepalived, and connection tracking part is using
conntrackd. There would be one master router and one backup router. The status
of router(master or backup) can be query from the database table domain_router
now. Management server would update the status every 30s by default.

4. The routers for the same zone would use same external NIC(same ip and mac).
The script used for fail-over would ensure only one external NIC present in the
network at any time.

5. Currently management server don't got the ability to stop one of router is
both of them reported as master. The feature is in the todo list.

After two routers start up, disconnect anyone of them, the guest network
shouldn't be affected, and established connection(http, ssh, etc.) should still
works. The fail-over on gateway part should be 3~4 seconds.

Currently the patch works with KVM. Would deal with vmware and XenServer soon.

server/src/com/cloud/network/ovs/OvsNetworkManagerImpl.java
@@ -395,7 +395,8 @@ public class OvsNetworkManagerImpl implements OvsNetworkManager {
 		long hostId = dest.getHost().getId();
 		long accountId = instance.getAccountId();
 		List<UserVmVO>vms = _userVmDao.listByAccountId(accountId);
-		DomainRouterVO router = _routerDao.findBy(accountId, instance.getDataCenterId());
+		/* FIXME: Redundant virtual router doesn't support OVM */
+		DomainRouterVO router = _routerDao.findBy(accountId, instance.getDataCenterId()).get(0);
 		List<VMInstanceVO>ins = new ArrayList<VMInstanceVO>();
 		ins.addAll(vms);
 		ins.add(router);
@@ -538,7 +539,8 @@ public class OvsNetworkManagerImpl implements OvsNetworkManager {
 		}
 		
 		long accountId = instance.getAccountId();
-		DomainRouterVO router = _routerDao.findBy(accountId, instance.getDataCenterId());
+		/* FIXME: Redundant virtual router doesn't support OVM */
+		DomainRouterVO router = _routerDao.findBy(accountId, instance.getDataCenterId()).get(0);
 		if (router == null) {
 			return;
 		}
@@ -639,7 +641,8 @@ public class OvsNetworkManagerImpl implements OvsNetworkManager {
 		}
 		
 		if (tellRouter && instance.getType() != VirtualMachine.Type.DomainRouter) {
-			DomainRouterVO router = _routerDao.findBy(accountId, instance.getDataCenterId());
+		    /* FIXME: Redundant virtual router doesn't support OVM */
+			DomainRouterVO router = _routerDao.findBy(accountId, instance.getDataCenterId()).get(0);
 			if (router != null) {
 				affectedVms.add(new Long(router.getId()));
 			}

server/src/com/cloud/network/ovs/OvsTunnelManagerImpl.java
@@ -153,7 +153,8 @@ public class OvsTunnelManagerImpl implements OvsTunnelManager {
 		long hostId = dest.getHost().getId();
 		long accountId = instance.getAccountId();
 		List<UserVmVO>vms = _userVmDao.listByAccountId(accountId);
-		DomainRouterVO router = _routerDao.findBy(accountId, instance.getDataCenterId());
+		/* FIXME: Redundant virtual router doesn't support OVM */
+		DomainRouterVO router = _routerDao.findBy(accountId, instance.getDataCenterId()).get(0);
 		List<VMInstanceVO>ins = new ArrayList<VMInstanceVO>();
 		if (vms != null) {
 			ins.addAll(vms);
@@ -284,7 +285,7 @@ public class OvsTunnelManagerImpl implements OvsTunnelManager {
                 return;
             }
             
-            DomainRouterVO router = _routerDao.findBy(vm.getAccountId(), vm.getDataCenterId());
+            DomainRouterVO router = _routerDao.findBy(vm.getAccountId(), vm.getDataCenterId()).get(0);
             if (router.getHostId() == vm.getHostId()) {
                 return;
             }

apache/cloudstack

server/src/com/cloud/network/ovs/OvsNetworkManagerImpl.java
@@ -395,7 +395,8 @@ public class OvsNetworkManagerImpl implements OvsNetworkManager {
 		long hostId = dest.getHost().getId();
 		long accountId = instance.getAccountId();
 		List<UserVmVO>vms = _userVmDao.listByAccountId(accountId);
-		DomainRouterVO router = _routerDao.findBy(accountId, instance.getDataCenterIdToDeployIn());
+		/* FIXME: Redundant virtual router doesn't support OVS */
+		DomainRouterVO router = _routerDao.findBy(accountId, instance.getDataCenterIdToDeployIn()).get(0);
 		List<VMInstanceVO>ins = new ArrayList<VMInstanceVO>();
 		ins.addAll(vms);
 		ins.add(router);
@@ -538,7 +539,8 @@ public class OvsNetworkManagerImpl implements OvsNetworkManager {
 		}
 		
 		long accountId = instance.getAccountId();
-		DomainRouterVO router = _routerDao.findBy(accountId, instance.getDataCenterIdToDeployIn());
+		/* FIXME: Redundant virtual router doesn't support OVS */
+		DomainRouterVO router = _routerDao.findBy(accountId, instance.getDataCenterIdToDeployIn()).get(0);
 		if (router == null) {
 			return;
 		}
@@ -639,7 +641,8 @@ public class OvsNetworkManagerImpl implements OvsNetworkManager {
 		}
 		
 		if (tellRouter && instance.getType() != VirtualMachine.Type.DomainRouter) {
-			DomainRouterVO router = _routerDao.findBy(accountId, instance.getDataCenterIdToDeployIn());
+		    /* FIXME: Redundant virtual router doesn't support OVS */
+			DomainRouterVO router = _routerDao.findBy(accountId, instance.getDataCenterIdToDeployIn()).get(0);
 			if (router != null) {
 				affectedVms.add(new Long(router.getId()));
 			}

server/src/com/cloud/network/ovs/OvsTunnelManagerImpl.java
@@ -153,7 +153,8 @@ public class OvsTunnelManagerImpl implements OvsTunnelManager {
 		long hostId = dest.getHost().getId();
 		long accountId = instance.getAccountId();
 		List<UserVmVO>vms = _userVmDao.listByAccountId(accountId);
-		DomainRouterVO router = _routerDao.findBy(accountId, instance.getDataCenterIdToDeployIn());
+		/* FIXME: Redundant virtual router doesn't support OVS */
+		DomainRouterVO router = _routerDao.findBy(accountId, instance.getDataCenterIdToDeployIn()).get(0);
 		List<VMInstanceVO>ins = new ArrayList<VMInstanceVO>();
 		if (vms != null) {
 			ins.addAll(vms);
@@ -284,7 +285,7 @@ public class OvsTunnelManagerImpl implements OvsTunnelManager {
                 return;
             }
             
-            DomainRouterVO router = _routerDao.findBy(vm.getAccountId(), vm.getDataCenterIdToDeployIn());
+            DomainRouterVO router = _routerDao.findBy(vm.getAccountId(), vm.getDataCenterIdToDeployIn()).get(0);
             if (router.getHostId() == vm.getHostId()) {
                 return;
             }