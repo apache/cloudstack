df5d40dc32cb40d95dca7545a637f64e5fa38a1e
alena <alena@cloud.com>
2011-06-08 17:38:44 -0700

    bug 10199: don't allow network offering Name modification.
    UI has to be fixed as well

--- server/src/com/cloud/configuration/ConfigurationManagerImpl.java
+++ server/src/com/cloud/configuration/ConfigurationManagerImpl.java
@@ -2771,7 +2771,6 @@ public class ConfigurationManagerImpl implements ConfigurationManager, Configura
     public NetworkOffering updateNetworkOffering(UpdateNetworkOfferingCmd cmd) {
         String displayText = cmd.getDisplayText();
         Long id = cmd.getId();
-        String name = cmd.getNetworkOfferingName();
         String availabilityStr = cmd.getAvailability();
         Availability availability = null;
 
@@ -2792,10 +2791,6 @@ public class ConfigurationManagerImpl implements ConfigurationManager, Configura
         }
 
         NetworkOfferingVO offering = _networkOfferingDao.createForUpdate(id);
-
-        if (name != null) {
-            offering.setName(name);
-        }
 
         if (displayText != null) {
             offering.setDisplayText(displayText);
