8b514c79a5ac271778b6777cd4fb57dcff20aa09
Jessica Wang <jessica@cloud.com>
2011-06-16 16:14:32 -0700

    cloudStack UI - Secondary Storage page - fix a bug "secondRowText is not defined".

--- ui/scripts/cloud.core.secondarystorage.js
+++ ui/scripts/cloud.core.secondarystorage.js
@@ -68,7 +68,7 @@ function secondaryStorageToMidmenu(jsonObj, $midmenuItem1) {
     $midmenuItem1.find("#first_row").text(firstRowText.substring(0,midMenuFirstRowLength));     
     $midmenuItem1.find("#first_row_container").attr("title", firstRowText);   
     
-    var secondRowText = fromdb(jsonObj.id);
+    var secondRowText = fromdb(jsonObj.id).toString();
     $midmenuItem1.find("#second_row").text(secondRowText.substring(0,midMenuSecondRowLength));
     $midmenuItem1.find("#second_row_container").attr("title", secondRowText); 
 }
