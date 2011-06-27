7feaca1d60ad5c777679d08900d1f0a617ac0f5f
Edison Su <edison@cloud.com>
2011-06-27 10:31:13 -0400

    fix cloudkit setup, to use new image

--- tools/test/cloudkit.py
+++ tools/test/cloudkit.py
@@ -15,11 +15,14 @@ def fix_default_db():
     database.update(statement % ('http://nfs1.lab.vmops.com/templates/dummy/systemvm.ova', 'routing-8'))
 
     database.update(statement % ('http://nfs1.lab.vmops.com/templates/dummy/builtin.vhd', 'centos53-x86_64'))
-    database.update(statement % ('http://nfs1.lab.vmops.com/templates/rightscale/RightImage_CentOS_5.4_x64_v5.6.32.qcow2.bz2', 'centos55-x86_64'))
     database.update(statement % ('http://nfs1.lab.vmops.com/templates/dummy/builtin.ova', 'centos53-x64'))
     statement="""UPDATE vm_template SET checksum=NULL"""
     database.update(statement)
     
+    statement="""
+    UPDATE vm_template SET url='%s', unique_name='%s', name='%s', display_text='%s'
+    WHERE id=4 """
+    database.update(statement % ('http://nfs1.lab.vmops.com/templates/rightscale/RightImage_CentOS_5.4_x64_v5.6.34.qcow2.bz2', 'Rightscale CentOS 5.4', 'Rightscale CentOS 5.4', 'Rightscale CentOS 5.4'))
     statement="""UPDATE disk_offering set use_local_storage=1"""
     database.update(statement)
     
