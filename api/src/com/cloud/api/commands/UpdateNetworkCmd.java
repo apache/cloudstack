a8a9b37824cfe4e83d463f676f673dc466b0e17a
alena <alena@cloud.com>
2011-07-06 10:51:20 -0700

    bug 10553: allow networkDomain update for Guest system network in Basic zone status 10553: resolved fixed

--- api/src/com/cloud/api/commands/UpdateNetworkCmd.java
+++ api/src/com/cloud/api/commands/UpdateNetworkCmd.java
@@ -27,7 +27,6 @@ import com.cloud.api.BaseCmd;
 import com.cloud.api.Implementation;
 import com.cloud.api.Parameter;
 import com.cloud.api.ServerApiException;
-import com.cloud.api.BaseCmd.CommandType;
 import com.cloud.api.response.NetworkResponse;
 import com.cloud.exception.ConcurrentOperationException;
 import com.cloud.exception.InsufficientCapacityException;
