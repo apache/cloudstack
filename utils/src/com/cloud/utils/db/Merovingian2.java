8ef86467ed5f2f024a777e43741116a666450303
Alex Huang <alex@cloud.com>
2011-07-13 09:53:38 -0700

    more logging and fixed bug in cleanup

--- utils/src/com/cloud/utils/db/Merovingian2.java
+++ utils/src/com/cloud/utils/db/Merovingian2.java
@@ -219,11 +219,13 @@ public class Merovingian2 extends StandardMBean implements MerovingianMBean {
 
     @Override
     public void cleanupForServer(long msId) {
+        s_logger.info("Cleaning up locks for " + msId);
         PreparedStatement pstmt = null;
         try {
             pstmt = _concierge.conn().prepareStatement(CLEANUP_MGMT_LOCKS_SQL);
-            pstmt.setLong(1, _msId);
-            pstmt.executeUpdate();
+            pstmt.setLong(1, msId);
+            int rows = pstmt.executeUpdate();
+            s_logger.info("Released " + rows + " locks for " + msId);
         } catch (SQLException e) {
             throw new CloudRuntimeException("Unable to clear the locks", e);
         } finally {
