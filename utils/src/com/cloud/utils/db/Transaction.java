3d41105e1badc6dc5d30b43fbf7b0696b340567b
alena <alena@cloud.com>
2011-07-15 10:18:51 -0700

    From Alex - assertion fix

--- utils/src/com/cloud/utils/db/Transaction.java
+++ utils/src/com/cloud/utils/db/Transaction.java
@@ -126,13 +126,13 @@ public class Transaction {
     // the existing DAO features
     //
     public void transitToUserManagedConnection(Connection conn) {
-    	assert(_conn == null && _stack.size() <= 1) : "Can't change to a user managed connection unless the stack is empty and the db connection is null: " + toString();
+    	assert(_conn == null /*&& _stack.size() <= 1*/) : "Can't change to a user managed connection unless the stack is empty and the db connection is null: " + toString();
         _conn = conn;
         _dbId = CONNECTED_DB;
     }
     
     public void transitToAutoManagedConnection(short dbId) {
-        assert(_stack.size() <= 1) : "Can't change to auto managed connection unless your stack is empty";
+        // assert(_stack.size() <= 1) : "Can't change to auto managed connection unless your stack is empty";
         _dbId = dbId;
         _conn = null;
     }
