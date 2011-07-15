f67c771410c9cd753a39274ff3ab847d806e4fdd
alena <alena@cloud.com>
2011-07-15 15:07:11 -0700

    bug 10733: fixed/added incorrect/missing keys in 228 to 229 upgrade status 10733: resolved fixed

--- setup/db/db/schema-228to229.sql
+++ setup/db/db/schema-228to229.sql
@@ -5,3 +5,5 @@
 ALTER TABLE `cloud`.`account` ADD COLUMN `network_domain` varchar(255);
 ALTER TABLE `cloud`.`domain` ADD COLUMN `network_domain` varchar(255);
 
+ALTER TABLE `cloud`.`cluster` ADD CONSTRAINT `fk_cluster__data_center_id` FOREIGN KEY (`data_center_id`) REFERENCES `cloud`.`data_center`(`id`) ON DELETE CASCADE;
+
