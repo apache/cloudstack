03fbe13f5860d440352effda28763696c6e5b9c1
frank <frank@cloud.com>
2011-07-29 20:02:44 -0700

    Add upgrade path to 2.2.9 Add files

---
+++ server/src/com/cloud/upgrade/dao/Upgrade228to229.java
@@ -0,0 +1,107 @@
+/**
+ *  Copyright (C) 2010 Cloud.com, Inc.  All rights reserved.
+ * 
+ * This software is licensed under the GNU General Public License v3 or later.
+ * 
+ * It is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+package com.cloud.upgrade.dao;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import com.cloud.utils.exception.CloudRuntimeException;
+import com.cloud.utils.script.Script;
+
+public class Upgrade228to229 implements DbUpgrade {
+    final static Logger s_logger = Logger.getLogger(Upgrade228to229.class);
+
+    @Override
+    public String[] getUpgradableVersionRange() {
+        return new String[] { "2.2.8", "2.2.8"};
+    }
+
+    @Override
+    public String getUpgradedVersion() {
+        return "2.2.9";
+    }
+
+    @Override
+    public boolean supportsRollingUpgrade() {
+        return true;
+    }
+
+    @Override
+    public File[] getPrepareScripts() {
+        String script = Script.findScript("", "db/schema-228to229.sql");
+        if (script == null) {
+            throw new CloudRuntimeException("Unable to find db/schema-228to229.sql");
+        }
+        
+        return new File[] { new File(script) };
+    }
+
+    @Override
+    public void performDataMigration(Connection conn) {
+        dropKeysIfExist(conn);
+        PreparedStatement pstmt;
+        try {
+        	/*fk_cluster__data_center_id has been wrongly added in previous upgrade(not sure which one), 228to229 upgrade drops it and re-add again*/
+			pstmt = conn.prepareStatement("ALTER TABLE `cloud`.`cluster` ADD CONSTRAINT `fk_cluster__data_center_id` FOREIGN KEY (`data_center_id`) REFERENCES `cloud`.`data_center`(`id`) ON DELETE CASCADE;");
+	        pstmt.executeUpdate();
+        } catch (SQLException e) {
+        	throw new CloudRuntimeException("Unable to execute cluster update", e);
+        }
+    }
+
+    @Override
+    public File[] getCleanupScripts() {
+        return null;
+    }
+    
+
+    private void dropKeysIfExist(Connection conn) {
+        HashMap<String, List<String>> indexes = new HashMap<String, List<String>>();
+        HashMap<String, List<String>> foreignKeys = new HashMap<String, List<String>>();
+
+        //indexes to drop
+        List<String> keys = new ArrayList<String>();
+        keys.add("name");
+        indexes.put("network_offerings", keys);
+        
+        //foreign keys to drop - this key would be re-added later
+        keys = new ArrayList<String>();
+        keys.add("fk_cluster__data_center_id");
+        foreignKeys.put("cluster", keys);
+        
+        
+        // drop all foreign keys first
+        s_logger.debug("Dropping keys that don't exist in 2.2.6 version of the DB...");
+        for (String tableName : foreignKeys.keySet()) {
+            DbUpgradeUtils.dropKeysIfExist(conn, tableName, foreignKeys.get(tableName), true);
+        }
+
+        // drop indexes now
+        for (String tableName : indexes.keySet()) {
+            DbUpgradeUtils.dropKeysIfExist(conn, tableName, indexes.get(tableName), false);
+        }
+    }
+    
+}
---
+++ setup/db/db/schema-228to229.sql
@@ -0,0 +1,39 @@
+--;
+-- Schema upgrade from 2.2.8 to 2.2.9;
+--;
+
+ALTER TABLE `cloud`.`account` ADD COLUMN `network_domain` varchar(255);
+ALTER TABLE `cloud`.`domain` ADD COLUMN `network_domain` varchar(255);
+
+INSERT IGNORE INTO configuration VALUES ('Advanced', 'DEFAULT', 'NetworkManager', 'network.dns.basiczone.updates', 'all', 'This parameter can take 2 values: all (default) and pod. It defines if DHCP/DNS requests have to be send to all dhcp servers in cloudstack, or only to the one in the same pod');
+
+ALTER TABLE `cloud`.`op_host_capacity` DROP FOREIGN KEY `fk_op_host_capacity__pod_id`;
+ALTER TABLE `cloud`.`op_host_capacity` DROP FOREIGN KEY `fk_op_host_capacity__data_center_id`;
+ALTER TABLE `cloud`.`op_host_capacity` DROP FOREIGN KEY `fk_op_host_capacity__cluster_id`;
+
+ALTER TABLE `cloud`.`firewall_rules_cidrs` ADD UNIQUE INDEX `unique_rule_cidrs`  (`firewall_rule_id`, `source_cidr`);
+
+ALTER TABLE `cloud`.`cluster` ADD INDEX `i_cluster__removed`(`removed`);
+ALTER TABLE `cloud`.`data_center` ADD INDEX `i_data_center__removed`(`removed`);
+ALTER TABLE `cloud`.`host_pod_ref` ADD INDEX `i_host_pod_ref__removed`(`removed`);
+ALTER TABLE `cloud`.`mshost` ADD INDEX `i_mshost__removed`(`removed`);
+ALTER TABLE `cloud`.`template_zone_ref` ADD INDEX `i_template_zone_ref__removed`(`removed`);
+ALTER TABLE `cloud`.`domain` ADD INDEX `i_domain__removed`(`removed`);
+ALTER TABLE `cloud`.`disk_offering` ADD INDEX `i_disk_offering__removed`(`removed`);
+ALTER TABLE `cloud`.`storage_pool` ADD INDEX `i_storage_pool__removed`(`removed`);
+ALTER TABLE `cloud`.`instance_group` ADD INDEX `i_instance_group__removed`(`removed`);
+
+ALTER TABLE `cloud`.`sync_queue_item` ADD INDEX `i_sync_queue_item__queue_proc_number`(`queue_proc_number`);
+ALTER TABLE `cloud`.`sync_queue_item` ADD INDEX `i_sync_queue_item__queue_proc_msid`(`queue_proc_msid`);
+ALTER TABLE `cloud`.`op_nwgrp_work` ADD INDEX `i_op_nwgrp_work__taken`(`taken`);
+ALTER TABLE `cloud`.`op_nwgrp_work` ADD INDEX `i_op_nwgrp_work__step`(`step`);
+ALTER TABLE `cloud`.`op_nwgrp_work` ADD INDEX `i_op_nwgrp_work__seq_no`(`seq_no`);
+ALTER TABLE `cloud`.`volumes` ADD INDEX `i_volumes__state`(`state`);
+
+
+INSERT IGNORE INTO configuration VALUES ('Advanced', 'DEFAULT', 'management-server', 'agent.load.threshold', '0.70', 'Percentage (as a value between 0 and 1) of connected agents after which agent load balancing will start happening');
+INSERT IGNORE INTO configuration VALUES ('Network', 'DEFAULT', 'management-server', 'network.loadbalancer.haproxy.stats.visibility', 'global', 'Load Balancer(haproxy) stats visibilty, it can be global,guest-network,disabled');
+INSERT IGNORE INTO configuration VALUES ('Network', 'DEFAULT', 'management-server', 'network.loadbalancer.haproxy.stats.uri','/admin?stats','Load Balancer(haproxy) uri.');
+INSERT IGNORE INTO configuration VALUES ('Network', 'DEFAULT', 'management-server', 'network.loadbalancer.haproxy.stats.auth','admin1:AdMiN123','Load Balancer(haproxy) authetication string in the format username:password');
+INSERT IGNORE INTO configuration VALUES ('Network', 'DEFAULT', 'management-server', 'network.loadbalancer.haproxy.stats.port','8081','Load Balancer(haproxy) stats port number.');
+
