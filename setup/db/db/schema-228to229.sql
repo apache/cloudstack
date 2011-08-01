f48efae77c6bedb8f4e2534c866d22b4392a9ad5
alena <alena@cloud.com>
2011-08-01 15:14:12 -0700

    bug 10927: removed network_domain for account/domain as this feature is not in 2.2.9, but in 2.2.10

--- setup/db/db/schema-228to229.sql
+++ setup/db/db/schema-228to229.sql
@@ -2,9 +2,6 @@
 -- Schema upgrade from 2.2.8 to 2.2.9;
 --;
 
-ALTER TABLE `cloud`.`account` ADD COLUMN `network_domain` varchar(255);
-ALTER TABLE `cloud`.`domain` ADD COLUMN `network_domain` varchar(255);
-
 INSERT IGNORE INTO configuration VALUES ('Advanced', 'DEFAULT', 'NetworkManager', 'network.dns.basiczone.updates', 'all', 'This parameter can take 2 values: all (default) and pod. It defines if DHCP/DNS requests have to be send to all dhcp servers in cloudstack, or only to the one in the same pod');
 
 ALTER TABLE `cloud`.`op_host_capacity` DROP FOREIGN KEY `fk_op_host_capacity__pod_id`;
