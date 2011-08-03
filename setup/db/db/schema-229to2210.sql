a7bc9558226be8257c3db8081b592a320e9ba27f
alena <alena@cloud.com>
2011-08-03 10:35:06 -0700

    Added missing config value to 229 to 2210 upgrade

--- setup/db/db/schema-229to2210.sql
+++ setup/db/db/schema-229to2210.sql
@@ -5,6 +5,8 @@
 ALTER TABLE `cloud`.`account` ADD COLUMN `network_domain` varchar(255);
 ALTER TABLE `cloud`.`domain` ADD COLUMN `network_domain` varchar(255);
 
+INSERT IGNORE INTO configuration VALUES ('Advanced', 'DEFAULT', 'NetworkManager', 'use.external.dns', 'false', 'Bypass internal dns, use exetrnal dns1 and dns2');
+
 
 
 
