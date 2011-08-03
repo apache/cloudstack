603de56c936dc0f841ecaa8418c29c772a8a8345
alena <alena@cloud.com>
2011-08-03 10:39:02 -0700

    Merged fixes for 228-229 upgrade from 2.2.8 zucchini branch

--- server/src/com/cloud/upgrade/dao/Upgrade228to229.java
+++ server/src/com/cloud/upgrade/dao/Upgrade228to229.java
@@ -19,6 +19,8 @@ package com.cloud.upgrade.dao;
 
 import java.io.File;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -59,6 +61,23 @@ public class Upgrade228to229 implements DbUpgrade {
     @Override
     public void performDataMigration(Connection conn) {
         dropKeysIfExist(conn);
+        PreparedStatement pstmt;
+        try {
+            /*fk_cluster__data_center_id has been wrongly added in previous upgrade(not sure which one), 228to229 upgrade drops it and re-add again*/
+            pstmt = conn.prepareStatement("ALTER TABLE `cloud`.`cluster` ADD CONSTRAINT `fk_cluster__data_center_id` FOREIGN KEY (`data_center_id`) REFERENCES `cloud`.`data_center`(`id`) ON DELETE CASCADE");
+            pstmt.executeUpdate();
+            
+            pstmt = conn.prepareStatement("ALTER TABLE `cloud`.`snapshots` ADD INDEX `i_snapshots__removed`(`removed`)");
+            pstmt.executeUpdate();
+            
+            pstmt = conn.prepareStatement("ALTER TABLE `cloud`.`network_tags` ADD CONSTRAINT `fk_network_tags__network_id` FOREIGN KEY (`network_id`) REFERENCES `networks`(`id`) ON DELETE CASCADE");
+            pstmt.executeUpdate();
+            
+            pstmt.close();
+            
+        } catch (SQLException e) {
+            throw new CloudRuntimeException("Unable to execute cluster update", e);
+        }
     }
 
     @Override
@@ -66,19 +85,46 @@ public class Upgrade228to229 implements DbUpgrade {
         return null;
     }
     
+
     private void dropKeysIfExist(Connection conn) {
         HashMap<String, List<String>> indexes = new HashMap<String, List<String>>();
         HashMap<String, List<String>> foreignKeys = new HashMap<String, List<String>>();
 
         //indexes to drop
+        //for network_offering
         List<String> keys = new ArrayList<String>();
         keys.add("name");
         indexes.put("network_offerings", keys);
+        
+        //for snapshot
+        keys = new ArrayList<String>();
+        keys.add("i_snapshots__removed");
+        indexes.put("snapshots", keys);
+        
+        //for domain router
+        keys = new ArrayList<String>();
+        keys.add("i_domain_router__public_ip_address");
+        indexes.put("domain_router", keys);
+        
+        //for user_ip_address
+        keys = new ArrayList<String>();
+        keys.add("i_user_ip_address__public_ip_address");
+        indexes.put("user_ip_address", keys);
+        
         
         //foreign keys to drop - this key would be re-added later
         keys = new ArrayList<String>();
         keys.add("fk_cluster__data_center_id");
         foreignKeys.put("cluster", keys);
+        
+        keys = new ArrayList<String>();
+        keys.add("fk_domain_router__public_ip_address");
+        foreignKeys.put("domain_router", keys);
+        
+        //drop foreign key from network tags table - it would be re-added later
+        keys = new ArrayList<String>();
+        keys.add("fk_network_tags__network_id");
+        foreignKeys.put("network_tags", keys);
         
         
         // drop all foreign keys first
--- setup/db/db/schema-229to2210.sql
+++ setup/db/db/schema-229to2210.sql
@@ -1,5 +1,5 @@
 --;
--- Schema upgrade from 2.2.8 to 2.2.9;
+-- Schema upgrade from 2.2.9 to 2.2.10;
 --;
 
 ALTER TABLE `cloud`.`account` ADD COLUMN `network_domain` varchar(255);
