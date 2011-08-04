38366074c931f48a12a81b6ef84c243f137a2ad9
Sheng Yang <sheng.yang@cloud.com>
2011-08-04 12:10:08 -0700

    Add some default values for 2.2.9-2.2.10 scheme

--- setup/db/db/schema-229to2210.sql
+++ setup/db/db/schema-229to2210.sql
@@ -7,9 +7,9 @@ ALTER TABLE `cloud`.`domain` ADD COLUMN `network_domain` varchar(255);
 
 INSERT IGNORE INTO configuration VALUES ('Advanced', 'DEFAULT', 'NetworkManager', 'use.external.dns', 'false', 'Bypass internal dns, use exetrnal dns1 and dns2');
 
-ALTER TABLE `cloud`.`domain_router` ADD COLUMN `is_redundant_router` int(1) unsigned NOT NULL COMMENT 'if in redundant router mode';
+ALTER TABLE `cloud`.`domain_router` ADD COLUMN `is_redundant_router` int(1) unsigned NOT NULL DEFAULT 0 COMMENT 'if in redundant router mode';
 ALTER TABLE `cloud`.`domain_router` ADD COLUMN `priority` int(4) unsigned COMMENT 'priority of router in the redundant router mode';
-ALTER TABLE `cloud`.`domain_router` ADD COLUMN `redundant_state` varchar(64) NOT NULL COMMENT 'the state of redundant virtual router';
+ALTER TABLE `cloud`.`domain_router` ADD COLUMN `redundant_state` varchar(64) NOT NULL DEFAULT 'UNKNOWN' COMMENT 'the state of redundant virtual router';
 
 ALTER TABLE `cloud`.`cluster` ADD COLUMN  `managed_state` varchar(32) NOT NULL DEFAULT 'Managed' COMMENT 'Is this cluster managed by cloudstack';
 
