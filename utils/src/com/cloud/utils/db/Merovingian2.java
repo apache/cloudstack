29a0f8b1f82bff2617e967d2845ad9d385875998
Alex Huang <alex@cloud.com>
2011-08-22 10:04:22 -0700

    fix for the problem of locking connection being gone

--- utils/src/com/cloud/utils/db/Merovingian2.java
+++ utils/src/com/cloud/utils/db/Merovingian2.java
@@ -69,7 +69,7 @@ public class Merovingian2 extends StandardMBean implements MerovingianMBean {
             conn = Transaction.getStandaloneConnectionWithException();
             conn.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
             conn.setAutoCommit(true);
-            _concierge = new ConnectionConcierge("LockMaster", conn, false);
+            _concierge = new ConnectionConcierge("LockMaster", conn, true);
         } catch (SQLException e) {
             s_logger.error("Unable to get a new db connection", e);
             throw new CloudRuntimeException("Unable to initialize a connection to the database for locking purposes: ", e);
