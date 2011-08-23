56d4a10be399458c392bef74b64af2bfbe45ebdc
Naredula Janardhana Reddy <jana@cloud.com>
2011-08-23 20:01:35 +0530

    bug 11191: Added global config to disable/enable rp_filter for domR(public interfaces)

--- server/src/com/cloud/network/router/VirtualNetworkApplianceManagerImpl.java
+++ server/src/com/cloud/network/router/VirtualNetworkApplianceManagerImpl.java
@@ -313,6 +313,8 @@ public class VirtualNetworkApplianceManagerImpl implements VirtualNetworkApplian
     private ServiceOfferingVO _offering;
     private String trafficSentinelHostname;
     private String _dnsBasicZoneUpdates = "all";
+    
+    private boolean _disable_rp_filter = false;
 
     ScheduledExecutorService _executor;
     ScheduledExecutorService _checkExecutor;
@@ -583,6 +585,11 @@ public class VirtualNetworkApplianceManagerImpl implements VirtualNetworkApplian
         _instance = configs.get("instance.name");
         if (_instance == null) {
             _instance = "DEFAULT";
+        }
+        
+        String rpValue = configs.get("network.disable.rpfilter");
+        if (rpValue != null && rpValue.equalsIgnoreCase("true")) {
+            _disable_rp_filter = true;
         }
         
         _dnsBasicZoneUpdates = String.valueOf(_configDao.getValue(Config.DnsBasicZoneUpdates.key()));
@@ -1203,7 +1210,7 @@ public class VirtualNetworkApplianceManagerImpl implements VirtualNetworkApplian
 
         String type = null;
         String dhcpRange = null;
-
+        String rpFilter = " ";
         DataCenter dc = dest.getDataCenter();
         DataCenterVO dcVO = _dcDao.findById(dc.getId());
         _dcDao.loadDetails(dcVO);
@@ -1219,10 +1226,13 @@ public class VirtualNetworkApplianceManagerImpl implements VirtualNetworkApplian
             type = "dhcpsrvr";
         } else {
             type = "router";
+            if (_disable_rp_filter) {
+                rpFilter=" disable_rp_filter=true";
+            }
         }
 
         StringBuilder buf = profile.getBootArgsBuilder();
-        buf.append(" template=domP type=" + type);
+        buf.append(" template=domP type=" + type+rpFilter);
         buf.append(" name=").append(profile.getHostName());
 
         boolean isRedundant = _configDao.getValue("network.redundantrouter").equals("true");
