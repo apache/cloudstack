902bcf969b2e11c42197eb38845e4d18340b6d71
frank <frank@cloud.com>
2011-08-25 14:20:09 -0700

    Remove thirdparty

--- thirdparty/vnc/viewer/AxVncViewer/omnithread/build-bcc32.bat
+++
@@ -1,2 +0,0 @@
-@echo on
-bcc32.exe -v- -O2 -3 -tWM -xd- -q -w-8066 -c -oomnithread.obj nt.cpp
--- thirdparty/vnc/viewer/cab/makecab.bat
+++
@@ -1,3 +0,0 @@
-cabarc -s 6144 N vmopsvnc.cab vmopsvnc.dll cab.inf
-signtool sign /f vmopscert.pfx /p vmops /t http://tsa.starfieldtech.com vmopsvnc.cab
-copy vmopsvnc.cab ..\dist
