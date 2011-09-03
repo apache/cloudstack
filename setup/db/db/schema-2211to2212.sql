8fb76e747ee6dd826c637f9fdbc1e2f9919c9bde
alena <alena@cloud.com>
2011-09-02 17:29:01 -0700

    bug 11321: update vms in obsolete "Creating" state to "Error" state. status 11321: resolved fixed

--- setup/db/db/schema-2211to2212.sql
+++ setup/db/db/schema-2211to2212.sql
@@ -4,3 +4,5 @@
 
 ALTER TABLE `cloud`.`vm_template` ADD COLUMN `template_tag` varchar(255) COMMENT 'template tag';
 
+UPDATE vm_instance SET state='Error' WHERE state='Creating' AND removed IS null;
+
