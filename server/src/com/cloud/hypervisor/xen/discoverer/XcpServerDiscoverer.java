cbe996e1ce608990da01e3aa7d7e941b04d9ac9f
Nitin <nitin@cloud.com>
2011-09-13 03:07:32 +0530

    bug 11403: Making hypervisor_type of xs-tools.iso (id=200) as XenServer. Made the corresponding DB changes as well.

--- server/src/com/cloud/hypervisor/xen/discoverer/XcpServerDiscoverer.java
+++ server/src/com/cloud/hypervisor/xen/discoverer/XcpServerDiscoverer.java
@@ -489,7 +489,7 @@ public class XcpServerDiscoverer extends DiscovererBase implements Discoverer, L
             id = _tmpltDao.getNextInSequence(Long.class, "id");
             VMTemplateVO template = new VMTemplateVO(id, isoName, isoName, ImageFormat.ISO, true, true,
                     TemplateType.PERHOST, null, null, true, 64,
-                    Account.ACCOUNT_ID_SYSTEM, null, "xen-pv-drv-iso", false, 1, false, HypervisorType.Xen);
+                    Account.ACCOUNT_ID_SYSTEM, null, "xen-pv-drv-iso", false, 1, false, HypervisorType.XenServer);
             _tmpltDao.persist(template);
         } else {
             id = tmplt.getId();
