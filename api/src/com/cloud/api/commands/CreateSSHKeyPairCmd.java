4bdfec16f5ee85da3d1bf6a3f3ddf4debcf6510c
alena <alena@cloud.com>
2011-09-13 10:58:14 -0700

    bug 11412: use "sshkeypair" instead of "keypair" in response name for ssh commands status 11412: resolved fixed

--- api/src/com/cloud/api/commands/CreateSSHKeyPairCmd.java
+++ api/src/com/cloud/api/commands/CreateSSHKeyPairCmd.java
@@ -32,7 +32,7 @@ import com.cloud.user.UserContext;
 @Implementation(description="Create a new keypair and returns the private key", responseObject=SSHKeyPairResponse.class) 
 public class CreateSSHKeyPairCmd extends BaseCmd {
     public static final Logger s_logger = Logger.getLogger(CreateSSHKeyPairCmd.class.getName());
-    private static final String s_name = "createkeypairresponse";
+    private static final String s_name = "createsshkeypairresponse";
     
     
     /////////////////////////////////////////////////////
