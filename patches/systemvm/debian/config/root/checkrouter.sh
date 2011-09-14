6b9500a7ed20d54088a66826e031d0c0d1995ddb
Sheng Yang <sheng.yang@cloud.com>
2011-09-14 02:58:43 -0700

    bug 11307: Add PRIORITY bump up script for redundant virtual routers

--- patches/systemvm/debian/config/root/checkrouter.sh
+++ patches/systemvm/debian/config/root/checkrouter.sh
@@ -9,6 +9,16 @@ then
     exit 1
 fi
 
-tail -n 1 /root/keepalived.log | grep "Status"
+bumped="Bumped: NO"
+if [ -e /tmp/rrouter_bumped ]
+then
+    bumped="Bumped: YES"
+fi
+
+stat=`tail -n 1 /root/keepalived.log | grep "Status"`
+if [ $? -eq 0 ]
+then
+    echo "$stat&$bumped"
+fi
 
 unlock_exit $? $lock $locked
