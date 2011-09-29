53b1b9f63b1e1f5af27989e65dcb45b6d7460ba0
alena <alena@cloud.com>
2011-09-29 10:40:57 -0700

    bug 11603: for OSS build - update guru_name from GuestNetworkGuru to ExternalGuestNetworkGuru. status 11603: resolved fixed

---
+++ setup/db/db/schema-2212to2213.sql
@@ -0,0 +1,7 @@
+--;
+-- Schema upgrade from 2.2.12 to 2.2.13;
+--;
+
+UPDATE networks SET guru_name='ExternalGuestNetworkGuru' WHERE guru_name='GuestNetworkGuru';
+UPDATE nics SET reserver_name='ExternalGuestNetworkGuru' WHERE reserver_name='GuestNetworkGuru';
+
