eacb9dde264b442c389535cb5b9ab029e033969d
Edison Su <Edison@citrix.com>
2011-09-30 18:16:25 -0700

    [Simulator] Add API to configure simualtor: you can configure which command can be simulated, or how long it will wait for each command, for which zone/pod/cluster/host etc. e.g: command=configuresimulator&name=SecurityIngressRulesCmd&zoneid=1&value=enabled:true|timeout=30, means enable command SecurityIngressRulesCmd for zone 1, and wait for 30 seconds.

--- agent-simulator/src/com/cloud/simulator/dao/MockConfigurationDaoImpl.java
+++ agent-simulator/src/com/cloud/simulator/dao/MockConfigurationDaoImpl.java
@@ -1,11 +1,19 @@
 package com.cloud.simulator.dao;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Formatter;
+import java.util.List;
+
 import javax.ejb.Local;
 
 import com.cloud.simulator.MockConfigurationVO;
+import com.cloud.storage.VMTemplateHostVO;
 import com.cloud.utils.db.GenericDaoBase;
 import com.cloud.utils.db.SearchBuilder;
 import com.cloud.utils.db.SearchCriteria;
+import com.cloud.utils.db.Transaction;
 
 @Local(value={MockConfigurationDao.class})
 public class MockConfigurationDaoImpl extends GenericDaoBase<MockConfigurationVO, Long> implements MockConfigurationDao {
@@ -14,21 +22,30 @@ public class MockConfigurationDaoImpl extends GenericDaoBase<MockConfigurationVO
     private SearchBuilder<MockConfigurationVO> _searchByDcIDPodIdClusterIdName;
     private SearchBuilder<MockConfigurationVO> _searchByDcIDPodIdClusterIdHostIdName;
     private SearchBuilder<MockConfigurationVO> _searchByGlobalName;
+    																					
     
     public MockConfigurationDaoImpl() {
         _searchByGlobalName = createSearchBuilder();
         _searchByGlobalName.and("dcId", _searchByGlobalName.entity().getDataCenterId(), SearchCriteria.Op.NULL);
+        _searchByGlobalName.and("podId", _searchByGlobalName.entity().getPodId(), SearchCriteria.Op.NULL);
+        _searchByGlobalName.and("clusterId", _searchByGlobalName.entity().getClusterId(), SearchCriteria.Op.NULL);
+        _searchByGlobalName.and("hostId", _searchByGlobalName.entity().getHostId(), SearchCriteria.Op.NULL);
         _searchByGlobalName.and("name", _searchByGlobalName.entity().getName(), SearchCriteria.Op.EQ);
         _searchByGlobalName.done();
         
         _searchByDcIdName = createSearchBuilder();
         _searchByDcIdName.and("dcId", _searchByDcIdName.entity().getDataCenterId(), SearchCriteria.Op.EQ);
+        _searchByDcIdName.and("podId", _searchByDcIdName.entity().getPodId(), SearchCriteria.Op.NULL);
+        _searchByDcIdName.and("clusterId", _searchByDcIdName.entity().getClusterId(), SearchCriteria.Op.NULL);
+        _searchByDcIdName.and("hostId", _searchByDcIdName.entity().getHostId(), SearchCriteria.Op.NULL);
         _searchByDcIdName.and("name", _searchByDcIdName.entity().getName(), SearchCriteria.Op.EQ);
         _searchByDcIdName.done();
         
         _searchByDcIDPodIdName = createSearchBuilder();
         _searchByDcIDPodIdName.and("dcId", _searchByDcIDPodIdName.entity().getDataCenterId(), SearchCriteria.Op.EQ);
         _searchByDcIDPodIdName.and("podId", _searchByDcIDPodIdName.entity().getPodId(), SearchCriteria.Op.EQ);
+        _searchByDcIDPodIdName.and("clusterId", _searchByDcIDPodIdName.entity().getClusterId(), SearchCriteria.Op.NULL);
+        _searchByDcIDPodIdName.and("hostId", _searchByDcIDPodIdName.entity().getHostId(), SearchCriteria.Op.NULL);
         _searchByDcIDPodIdName.and("name", _searchByDcIDPodIdName.entity().getName(), SearchCriteria.Op.EQ);
         _searchByDcIDPodIdName.done();
         
@@ -36,6 +53,7 @@ public class MockConfigurationDaoImpl extends GenericDaoBase<MockConfigurationVO
         _searchByDcIDPodIdClusterIdName.and("dcId", _searchByDcIDPodIdClusterIdName.entity().getDataCenterId(), SearchCriteria.Op.EQ);
         _searchByDcIDPodIdClusterIdName.and("podId", _searchByDcIDPodIdClusterIdName.entity().getPodId(), SearchCriteria.Op.EQ);
         _searchByDcIDPodIdClusterIdName.and("clusterId", _searchByDcIDPodIdClusterIdName.entity().getClusterId(), SearchCriteria.Op.EQ);
+        _searchByDcIDPodIdClusterIdName.and("hostId", _searchByDcIDPodIdClusterIdName.entity().getHostId(), SearchCriteria.Op.NULL);
         _searchByDcIDPodIdClusterIdName.and("name", _searchByDcIDPodIdClusterIdName.entity().getName(), SearchCriteria.Op.EQ);
         _searchByDcIDPodIdClusterIdName.done();
         
@@ -84,8 +102,28 @@ public class MockConfigurationDaoImpl extends GenericDaoBase<MockConfigurationVO
     }
     
     @Override
-    public MockConfigurationVO findByGlobal(String name) {
-        return this.findByCommand(null, null, null, null, name);
+    public MockConfigurationVO findByNameBottomUP(Long dcId, Long podId, Long clusterId, Long hostId, String name) {
+    	 Transaction txn = Transaction.currentTxn();
+    	StringBuilder search = new StringBuilder();
+    	Formatter formatter = new Formatter(search);
+    	formatter.format("select * from mockconfiguration where (name='%s') and ((data_center_id = %d and pod_id = %d and cluster_id = %d and host_id = %d)", name, dcId, podId, clusterId, hostId);
+    	formatter.format(" or (data_center_id = %d and pod_id = %d and cluster_id = %d and host_id is null)", dcId, podId, clusterId);
+    	formatter.format(" or (data_center_id = %d and pod_id = %d and cluster_id is null and host_id is null)", dcId, podId);
+    	formatter.format(" or (data_center_id = %d and pod_id is null and cluster_id is null and host_id is null)", dcId);
+    	formatter.format(" or (data_center_id is null and pod_id is null and cluster_id is null and host_id is null)) LIMIT 1");
+    	
+    	PreparedStatement pstmt = null;
+		try {
+			String sql = search.toString();
+			pstmt = txn.prepareAutoCloseStatement(sql);
+			ResultSet rs = pstmt.executeQuery();
+			if (rs.next()) {
+                return toEntityBean(rs, false);
+            }
+		} catch (Exception e) {
+			
+		}
+		return null;
     } 
 
 }
