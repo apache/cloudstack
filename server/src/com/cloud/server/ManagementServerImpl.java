962bbcd5a0d04278ef339b6893da1cb6affd9322
Edison Su <Edison@citrix.com>
2011-10-10 11:30:07 -0700

    bug 11634: add manual vm migration support for kvm status 11634: resolved fixed

--- server/src/com/cloud/server/ManagementServerImpl.java
+++ server/src/com/cloud/server/ManagementServerImpl.java
@@ -977,11 +977,11 @@ public class ManagementServerImpl implements ManagementServer {
             throw new InvalidParameterValueException("VM is not Running, unable to migrate the vm " + vm);
         }
 
-        if (!vm.getHypervisorType().equals(HypervisorType.XenServer) && !vm.getHypervisorType().equals(HypervisorType.VMware)) {
+        if (!vm.getHypervisorType().equals(HypervisorType.XenServer) && !vm.getHypervisorType().equals(HypervisorType.VMware) && !vm.getHypervisorType().equals(HypervisorType.KVM)) {
             if (s_logger.isDebugEnabled()) {
-                s_logger.debug(vm + " is not XenServer/VMware, cannot migrate this VM.");
+                s_logger.debug(vm + " is not XenServer/VMware/KVM, cannot migrate this VM.");
             }
-            throw new InvalidParameterValueException("Unsupported Hypervisor Type for VM migration, we support XenServer/VMware only");
+            throw new InvalidParameterValueException("Unsupported Hypervisor Type for VM migration, we support XenServer/VMware/KVM only");
         }
         ServiceOfferingVO svcOffering = _offeringsDao.findById(vm.getServiceOfferingId());
         if (svcOffering.getUseLocalStorage()) {
