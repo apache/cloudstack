dc7a3914133515238ff35523ceb46dca8b70e949
frank <frank.zhang@citrix.com>
2011-10-12 15:06:56 -0700

    Bug 11640 - Upgrade from 2.2.9 FOSSOVM to 2.2.12- OVM hypervisor is not provided as one of the options for "Hypervisor" when adding a cluster
    add Ovm in hypervisor.list of configuration table
    
    status 11640: resolved fixed

--- setup/db/db/schema-2212to2213.sql
+++ setup/db/db/schema-2212to2213.sql
@@ -4,4 +4,6 @@
 
 UPDATE networks SET guru_name='ExternalGuestNetworkGuru' WHERE guru_name='GuestNetworkGuru';
 UPDATE nics SET reserver_name='ExternalGuestNetworkGuru' WHERE reserver_name='GuestNetworkGuru';
+UPDATE configuration SET value='KVM,XenServer,VMware,BareMetal,Ovm' WHERE name='hypervisor.list';
+
 
