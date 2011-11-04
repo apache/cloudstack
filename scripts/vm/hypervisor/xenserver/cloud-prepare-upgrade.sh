5c98da6cc48ad5db5ffcd269951cfdfb7096157b
anthony <anthony@cloud.com>
2011-11-04 10:36:52 -0700

    upgrade : fix script

--- scripts/vm/hypervisor/xenserver/cloud-prepare-upgrade.sh
+++ scripts/vm/hypervisor/xenserver/cloud-prepare-upgrade.sh
@@ -35,7 +35,7 @@ for dom0 in $(xe vm-list is-control-domain=true | grep ^uuid | awk '{print $NF}'
 do
   local_vif=$(xe vif-list vm-uuid=$dom0 network-name-label=$local_networkname | grep ^uuid | awk '{print $NF}')
   if [ -z $local_vif ]; then
-      local_vif=$(xe vif-create network-uuid=$local_network vm-uuid=$dom0 device=0)
+      local_vif=$(xe vif-create network-uuid=$local_network vm-uuid=$dom0 device=0 mac=fe:ff:ff:ff:ff:ff)
       xe vif-param-set uuid=$local_vif other-config:nameLabel=link_local_network_vif
       xe vif-plug uuid=$local_vif
   fi
