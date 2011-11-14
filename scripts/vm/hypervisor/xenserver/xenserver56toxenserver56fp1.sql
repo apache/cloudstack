64b5224d438f57bf3d0fef9794c1bb9877cf6b80
anthony <anthony@cloud.com>
2011-11-14 15:13:15 -0800

    bug 11956: change guest_os type for template before upgrading xenserver host
    status 11956: resolved fixed
    
    Reviewed-by: sangeetha

--- scripts/vm/hypervisor/xenserver/xenserver56toxenserver56fp1.sql
+++ scripts/vm/hypervisor/xenserver/xenserver56toxenserver56fp1.sql
@@ -1,2 +1,4 @@
 update vm_instance set guest_os_id=98 where guest_os_id in (111, 113, 134, 121, 136) and hypervisor_type='XenServer';
 update vm_instance set guest_os_id=99 where guest_os_id in (112, 114, 135, 126, 137) and hypervisor_type='XenServer';
+update vm_template set guest_os_id=98 where guest_os_id in (111, 113, 134, 121, 136) and hypervisor_type='XenServer';
+update vm_template set guest_os_id=99 where guest_os_id in (112, 114, 135, 126, 137) and hypervisor_type='XenServer';
