807394cbd0cfe6b1927504e514ae947bb052299c
kishan <kishan@cloud.com>
2011-12-08 12:08:02 +0530

    bug 12436: Add new parameter external.network.stats.interval to upgrade script status 12436 : resolved fixed reviewed-by: Nitin

--- setup/db/db/schema-2213to2214.sql
+++ setup/db/db/schema-2213to2214.sql
@@ -3,3 +3,4 @@
 --;
 
 ALTER TABLE `cloud`.`vm_template` MODIFY `extractable` int(1) unsigned NOT NULL default 0 COMMENT 'Is this template extractable';
+INSERT INTO configuration (category, instance, component, name, value, description) VALUES ('Advanced', 'DEFAULT', 'management-server', 'external.network.stats.interval', '300', 'Interval (in seconds) to report external network statistics.');
