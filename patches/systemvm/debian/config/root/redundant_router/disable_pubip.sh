3b2e2b079be1a1021e21445960662b5197b9811c
Sheng Yang <sheng.yang@cloud.com>
2011-12-21 11:01:00 -0800

    bug 12704: Fix multiply public nics with redundant router
    status 12704: resolved fixed

--- patches/systemvm/debian/config/root/redundant_router/disable_pubip.sh
+++ patches/systemvm/debian/config/root/redundant_router/disable_pubip.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-ifconfig eth2 down
+ip link|grep BROADCAST|grep -v eth0|grep -v eth1|cut -d ":" -f 2 > /tmp/iflist
+while read i
+do
+    ifconfig $i down
+done < /tmp/iflist
 service dnsmasq stop
