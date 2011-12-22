577d0e698216b6ff9aa17435afc8f946c37d6eee
bfederle <bfederle@gmail.com>
2011-12-22 08:41:47 -0800

    Code cleanup

--- ui/scripts/ui/events.js
+++ ui/scripts/ui/events.js
@@ -1,39 +1,39 @@
 (function($, cloudStack) {
-  var event = cloudStack.ui.event = {};
-  
-  // Attach element to a specific event type
-  event.elem = function(widget, elem, $elem, extraData) {
-    // Setup DOM metadata
-    var data = { cloudStack: {} };
-    data.cloudStack[widget] = {
-      elem: elem
-    };
-    if (extraData) $.extend(data.cloudStack[widget], extraData);
+  cloudStack.ui.event = {
+    // Attach element to specific event type
+    elem: function(widget, elem, $elem, extraData) {
+      // Setup DOM metadata
+      var data = { cloudStack: {} };
+      data.cloudStack[widget] = {
+        elem: elem
+      };
+      if (extraData) $.extend(data.cloudStack[widget], extraData);
 
-    return $elem
-      .addClass('cloudStack-elem')
-      .addClass(widget)
-      .data(data);
-  };
+      return $elem
+        .addClass('cloudStack-elem')
+        .addClass(widget)
+        .data(data);
+    },
 
-  // Create widget-based event
-  event.bind = function(widget, events) {
-    return function(event) {
-      var $target = $(event.target);
-      var $widget, $elem;
-      var data, elem;
+    // Create widget-based event
+    bind: function(widget, events) {
+      return function(event) {
+        var $target = $(event.target);
+        var $widget, $elem;
+        var data, elem;
 
-      $elem = $target.closest('.cloudStack-elem.' + widget);
-      if (!$elem.size())
-        return true;
+        $elem = $target.closest('.cloudStack-elem.' + widget);
+        if (!$elem.size())
+          return true;
 
-      $widget = $('.cloudStack-widget.' + widget);
-      data = $elem.data('cloudStack')[widget];
-      elem = data.elem;
+        $widget = $('.cloudStack-widget.' + widget);
+        data = $elem.data('cloudStack')[widget];
+        elem = data.elem;
 
-      events[elem]($elem, $widget, data);
+        events[elem]($elem, $widget, data);
 
-      return false;
-    };
+        return false;
+      };
+    }
   };
 })(jQuery, cloudStack);
