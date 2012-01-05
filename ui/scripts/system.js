3d9bb6591c8c80b68750411d5af15073abd3b7e0
Jessica Wang <jessica@cloud.com>
2012-01-05 15:34:30 -0800

    cloudstack 3.0 new UI - system - virtual router - instances tab - implement search.

--- ui/scripts/system.js
+++ ui/scripts/system.js
@@ -1468,9 +1468,21 @@
                       }
                     }
                   },                 									
-                  dataProvider: function(args) {
+                  dataProvider: function(args) {									 
+									  var array1 = [];  
+										if(args.filterBy != null) {          
+											if(args.filterBy.search != null && args.filterBy.search.by != null && args.filterBy.search.value != null) {
+												switch(args.filterBy.search.by) {
+												case "name":
+													if(args.filterBy.search.value.length > 0)
+														array1.push("&keyword=" + args.filterBy.search.value);
+													break;
+												}
+											}
+										}
+									
                     $.ajax({
-                      url: createURL("listRouters&zoneid=" + selectedZoneObj.id + "&page=" + args.page + "&pagesize=" + pageSize),
+                      url: createURL("listRouters&zoneid=" + selectedZoneObj.id + "&page=" + args.page + "&pagesize=" + pageSize + array1.join("")),
                       dataType: 'json',
                       async: true,
                       success: function(json) {
