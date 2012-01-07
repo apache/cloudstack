639b9b4fb4b7db7292661faa2e8c7c2b9c738a54
Jessica Wang <jessica@cloud.com>
2012-01-06 16:20:40 -0800

    cloudstack 3.0 new UI - system - secondary storages page - from actions from listView since they are available in detailView.

--- ui/scripts/system.js
+++ ui/scripts/system.js
@@ -6281,7 +6281,8 @@
         listView: {
           section: 'seconary-storage',
           fields: {
-            name: { label: 'Name' }
+            name: { label: 'Name' },
+						created: { label: 'Created', converter: cloudStack.converters.toLocalDate }
           },
 
           dataProvider: function(args) {
@@ -6369,39 +6370,7 @@
                   return 'Added new secondary storage';
                 }
               }
-            },
-
-            'delete': {
-              label: 'Delete' ,
-              messages: {
-                confirm: function(args) {
-                  return 'Please confirm that you want to delete this secondary storage.';
-                },
-                success: function(args) {
-                  return 'Secondary storage is being deleted.';
-                },
-                notification: function(args) {
-                  return 'Deleting secondary storage';
-                },
-                complete: function(args) {
-                  return 'Secondary storage has been deleted.';
-                }
-              },
-              action: function(args) {
-                $.ajax({
-                  url: createURL("deleteHost&id=" + args.context.secondarystorages[0].id),
-                  dataType: "json",
-                  async: true,
-                  success: function(json) {
-                    args.response.success({data:{}});
-                  }
-                });
-              },
-              notification: {
-                poll: function(args) { args.complete(); }
-              }
             }
-
           },
 
           detailView: {
@@ -6448,13 +6417,10 @@
                   },
                   {
                     id: { label: 'ID' },
-                    zonename: { label: 'Zone' },
-                    type: { label: 'Type' },
-                    ipaddress: { label: 'IP Address' }
+                    created: { label: 'Created', converter: cloudStack.converters.toLocalDate },
                   }
                 ],
-
-                //dataProvider: testData.dataProvider.detailView('secondaryStorage')
+                
                 dataProvider: function(args) {
                   args.response.success({
                     actionFilter: secondarystorageActionfilter,
