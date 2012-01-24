0f6619a8ce877d5cf82b94ae5ad16045f2da3193
bfederle <bfederle@gmail.com>
2012-01-24 13:48:45 -0800

    bug 11798
    Fix create LB sticky being called twice

--- ui/scripts/network.js
+++ ui/scripts/network.js
@@ -1486,6 +1486,8 @@
                               dataType: 'json',
                               async: true,
                               success: function(data) {
+                                var lbCreationComplete = false;
+
                                 args.response.success({
 																  fullRefresh: true,
                                   _custom: {
@@ -1499,7 +1501,13 @@
                                       
                                       pollAsyncJobResult({
                                         _custom: args._custom,
-                                        complete: function(args) {                                          
+                                        complete: function(args) {
+                                          if (lbCreationComplete) {
+                                            return;
+                                          }
+                                          
+                                          lbCreationComplete = true;
+                                          
                                           // Create stickiness policy
                                           if (stickyData &&
                                               stickyData.methodname &&
