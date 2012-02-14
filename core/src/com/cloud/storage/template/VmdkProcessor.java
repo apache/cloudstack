72cc472222c0cd4b1abc1d4ef06c9cf3679cdc54
Kelven Yang <kelven@cloud.com>
2012-02-14 11:49:16 -0800

    bug 13537: use --no-same-owner when we untar OVA file to work with NFSv4. Reviewed-By: Anthony

--- core/src/com/cloud/storage/template/VmdkProcessor.java
+++ core/src/com/cloud/storage/template/VmdkProcessor.java
@@ -64,6 +64,7 @@ public class VmdkProcessor implements Processor {
         File templateFile = new File(templateFileFullPath);
         
         Script command = new Script("tar", 0, s_logger);
+        command.add("--no-same-owner", templateFileFullPath);
         command.add("-xf", templateFileFullPath);
         command.setWorkDir(templateFile.getParent());
         String result = command.execute();
