8f988b97cbea9191bfee34d7ede0b1eb4747b927
Alena Prokharchyk <Alena.Prokharchyk@citrix.com>
2012-02-24 15:31:17 -0800

    Clone services map when do provider/service verification during network offering addition. Reviwed-by: Prachi Damle

--- server/src/com/cloud/network/NetworkManagerImpl.java
+++ server/src/com/cloud/network/NetworkManagerImpl.java
@@ -5977,20 +5977,22 @@ public class NetworkManagerImpl implements NetworkManager, NetworkService, Manag
                 throw new InvalidParameterValueException("Unable to find the Network Element implementing the Service Provider '" + provider.getName() + "'");
             }
 
-            Set<Service> enabledServices = providersMap.get(provider);
+            Set<Service> enabledServices = new HashSet<Service>();
+            enabledServices.addAll(providersMap.get(provider));
 
             if (enabledServices != null && !enabledServices.isEmpty()) {
                 if (!element.canEnableIndividualServices()) {
-                    Set<Network.Service> requiredServices = element.getCapabilities().keySet();
+                    Set<Service> requiredServices = new HashSet<Service>();                    
+                    requiredServices.addAll(element.getCapabilities().keySet());
+                    
                     if (requiredServices.contains(Network.Service.Gateway)) {
                         requiredServices.remove(Network.Service.Gateway);
                     }
-
-                    // Remove firewall from the list of services-to-compare
+                    
                     if (requiredServices.contains(Network.Service.Firewall)) {
                         requiredServices.remove(Network.Service.Firewall);
                     }
-
+                    
                     if (enabledServices.contains(Network.Service.Firewall)) {
                         enabledServices.remove(Network.Service.Firewall);
                     }
