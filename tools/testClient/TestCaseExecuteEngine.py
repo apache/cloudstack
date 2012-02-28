061ee4977a93ee63d29171e9d61a8d309a0aa26c
Prasanna Santhanam <Prasanna.Santhanam@citrix.com>
2012-02-28 17:01:32 +0530

    adding logging with level and timestamp

--- tools/testClient/TestCaseExecuteEngine.py
+++ tools/testClient/TestCaseExecuteEngine.py
@@ -7,21 +7,30 @@ from functools import partial
 import os
 import sys
 import logging
+
+module_logger = "testclient.testcase"
+
 def testCaseLogger(message, logger=None):
     if logger is not None:
         logger.debug(message)
+
 class TestCaseExecuteEngine(object):
     def __init__(self, testclient, testCaseFolder, testcaseLogFile=None, testResultLogFile=None):
         self.testclient = testclient
         self.testCaseFolder = testCaseFolder
         self.logger = None
         if testcaseLogFile is not None:
-            logger = logging.getLogger("testcase")
+            logger = logging.getLogger("testclient.testcase.TestCaseExecuteEngine")
             fh = logging.FileHandler(testcaseLogFile)
+            fh.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(name)s - %(message)s"))
             logger.addHandler(fh)
             logger.setLevel(logging.DEBUG)
             self.logger = logger
         if testResultLogFile is not None:
+            ch = logging.StreamHandler()
+            ch.setLevel(logging.ERROR)
+            ch.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(name)s - %(message)s"))
+            self.logger.addHandler(ch)
             fp = open(testResultLogFile, "w")
             self.testResultLogFile = fp
         else:
@@ -40,5 +49,3 @@ class TestCaseExecuteEngine(object):
         self.injectTestCase(suite)
         
         unittest.TextTestRunner(stream=self.testResultLogFile, verbosity=2).run(suite)
-        
-        
