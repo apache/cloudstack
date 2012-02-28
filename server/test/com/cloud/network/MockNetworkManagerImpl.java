2eaefc4aa8391b6df61497e68977f2d2f2369f22
Alena Prokharchyk <Alena.Prokharchyk@citrix.com>
2012-02-28 14:44:08 -0800

    Fixed unittest and some issues with 2.2.14-3.0 upgrade

--- server/test/com/cloud/network/MockNetworkManagerImpl.java
+++ server/test/com/cloud/network/MockNetworkManagerImpl.java
@@ -292,12 +292,6 @@ public class MockNetworkManagerImpl implements NetworkManager, Manager, NetworkS
     }
 
     @Override
-    public boolean shutdownNetwork(long networkId, ReservationContext context, boolean cleanupElements) {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
     public boolean destroyNetwork(long networkId, ReservationContext context) {
         // TODO Auto-generated method stub
         return false;
@@ -409,12 +403,6 @@ public class MockNetworkManagerImpl implements NetworkManager, Manager, NetworkS
 
     @Override
     public String getIpInNetworkIncludingRemoved(long vmId, long networkId) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    public Network updateGuestNetwork(long networkId, String name, String displayText, Account callerAccount, User callerUser, String domainSuffix, Long networkOfferingId) {
         // TODO Auto-generated method stub
         return null;
     }
@@ -843,6 +831,18 @@ public class MockNetworkManagerImpl implements NetworkManager, Manager, NetworkS
     public String getDefaultStorageTrafficLabel(long zoneId, HypervisorType hypervisorType) {
         // TODO Auto-generated method stub
         return null;
+    }
+
+    @Override
+    public Network updateGuestNetwork(long networkId, String name, String displayText, Account callerAccount, User callerUser, String domainSuffix, Long networkOfferingId, Boolean changeCidr) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean shutdownNetwork(long networkId, ReservationContext context, boolean cleanupElements) {
+        // TODO Auto-generated method stub
+        return false;
     }
 
 }
