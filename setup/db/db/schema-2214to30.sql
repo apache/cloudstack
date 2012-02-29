8fb378ac53c05a9d752050a08e8d696669dac6dc
Edison Su <Edison.Su@citrix.com>
2012-02-28 17:03:18 -0800

    fix db upgrade

--- setup/db/db/schema-2214to30.sql
+++ setup/db/db/schema-2214to30.sql
@@ -648,3 +648,4 @@ UPDATE `cloud`.`volumes` SET `last_pool_id` = `pool_id`;
 ALTER TABLE `cloud`.`user_ip_address` ADD COLUMN `is_system` int(1) unsigned NOT NULL default '0';
 ALTER TABLE `cloud`.`volumes` ADD COLUMN `update_count` bigint unsigned NOT NULL DEFAULT 0;
 ALTER TABLE `cloud`.`volumes` ADD INDEX `i_volumes__update_count`(`update_count`);
+ALTER TABLE `cloud`.`firewall_rules` ADD COLUMN `type` varchar(10) NOT NULL DEFAULT 'USER';
