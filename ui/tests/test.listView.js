f0b12e0d7d5e1cc70ec8e6dcc2f81b6d3eb99149
Brian Federle <brian.federle@citrix.com>
2012-03-08 13:47:01 -0800

    Add tests for list view widget:
    -Data provider
    
    -Column pre-filter

--- ui/tests/test.listView.js
+++ ui/tests/test.listView.js
@@ -35,6 +35,94 @@
     equal($listView.find('.list-view table tbody tr').size(), 1, 'List view has table body');
     equal($listView.find('.toolbar').size(), 1, 'List view has toolbar');
     equal($listView.find('.toolbar .text-search .search-bar input[type=text]').size(), 1, 'Toolbar has search box');
+    equal($listView.find('.toolbar .text-search .search-bar input[type=text]').size(), 1, 'Toolbar has search box');
     equal($listView.find('.toolbar .text-search .button.search').size(), 1, 'Toolbar has search button');
+    ok(!$listView.find('.toolbar .filters').size(), 'Toolbar doesn\'t have filters');
+  });
+
+  test('Data provider', function() {
+    var $listView = $('<div>');
+    
+    stop();
+    $listView.listView({
+      context: {
+        tests: []
+      },
+      listView: {
+        section: 'test',
+        fields: {
+          fieldA: { label: 'testFieldA' },
+          fieldB: { label: 'testFieldB' }
+        },
+        dataProvider: function(args) {
+          start();
+          equal(args.page, 1, 'Correct page # passed');
+          equal(args.filterBy.search.value, '', 'No search params specified');
+          ok($.isArray(args.context.tests), 'Context passed');
+          args.response.success({
+            data: [
+              {
+                fieldA: '1A',
+                fieldB: '1B'
+              },
+              {
+                fieldA: '2A',
+                fieldB: '2B'
+              },
+              {
+                fieldA: '3A',
+                fieldB: '3B'
+              }
+            ]
+          });
+          stop();
+          setTimeout(function() {
+            start();
+            equal($listView.find('tbody tr').size(), 3, 'Data row count is correct');
+            equal($listView.find('tbody tr:first td.fieldA span').html(), '1A', 'Correct table item value for first row');
+            equal($listView.find('tbody tr:first td.fieldB span').html(), '1B', 'Correct table item value for first row');
+            equal($listView.find('tbody tr:last td.fieldA span').html(), '3A', 'Correct table item value for last row');
+            equal($listView.find('tbody tr:last td.fieldB span').html(), '3B', 'Correct table item value for last row');
+          });
+        }
+      }
+    });
+  });
+
+  test('Pre-filter', function() {
+    var $listView = $('<div>');
+    
+    stop();
+    $listView.listView({
+      listView: {
+        section: 'test',
+        fields: {
+          fieldA: { label: 'testFieldA' },
+          fieldB: { label: 'testFieldB' },
+          fieldC: { label: 'testFieldC' }
+        },
+        preFilter: function(args) {
+          return ['fieldC'];
+        },
+        dataProvider: function(args) {
+          args.response.success({
+            data: [
+              {
+                fieldA: '1A',
+                fieldB: '1B',
+                fieldC: '1C'
+              }
+            ]
+          });
+          setTimeout(function() {
+            start();
+            equal($listView.find('thead th').size(), 2, 'Correct # of filtered columns present');
+            equal($listView.find('tbody tr:first td').size(), 2, 'Correct # of body columns present');
+            equal($listView.find('tbody tr:first td.fieldA span').html(), '1A', 'Correct table item value for data row');
+            equal($listView.find('tbody tr:first td.fieldB span').html(), '1B', 'Correct table item value for data row');
+          });
+        }
+      }
+    });
   });
 }(jQuery)); 
