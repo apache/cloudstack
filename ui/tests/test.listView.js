10c8b05c1a7a1696cd41872042abe63963d1283a
bfederle <bfederle@gmail.com>
2012-03-09 11:11:11 -0800

    WIP: List view async tests

--- ui/tests/test.listView.js
+++ ui/tests/test.listView.js
@@ -252,7 +252,7 @@
     equal($listView.find('.list-view tbody tr:last td:last span').html(), '2B', 'Last table cell has correct data');
   });
 
-  test('Actions', function() {
+  test('Basic sync action', function() {
     var $cloudStack = $('<div>').appendTo('#qunit-fixture');
     var listView = {
       listView: {
@@ -333,5 +333,84 @@
       start();
       ok(!$('.notification-box ul li').hasClass('pending'), 'Notification has completed state');
     });
+  });
+
+  test('Async action', function() {
+    var $cloudStack = $('<div>').appendTo('#qunit-fixture');
+    var listView = {
+      listView: {
+        section: 'test123',
+        fields: {
+          fieldA: { label: 'testFieldA' },
+          fieldB: { label: 'testFieldB' }
+        },
+        actions: {
+          asyncTest: {
+            label: 'asyncAction',
+            messages: {
+              confirm: function() {
+                return 'asyncActionConfirm';
+              },
+              notification: function() {
+                return 'asyncActionNotification';
+              }
+            },
+            action: function(args) {
+              args.response.success();
+            }
+          }
+        },
+        dataProvider: function(args) {
+          args.response.success({
+            data: [
+              {
+                fieldA: '1A',
+                fieldB: '1B',
+                fieldC: '1C'
+              },
+              {
+                fieldA: '2A',
+                fieldB: '2B',
+                fieldC: '2C'
+              }
+            ]
+          });
+        }
+      }
+    };
+
+    // CloudStack object is needed for notification handling for actions
+    var cloudStack = {
+      sections: {
+        testActions: listView
+      },
+
+      home: 'testActions'
+    };
+    
+    ok($cloudStack.cloudStack(cloudStack), 'Initialize cloudStack widget w/ list view');
+
+    var $listView = $cloudStack.find('.list-view');
+    
+    equal($listView.find('table thead th').size(), 3, 'Correct header column count');
+    equal($listView.find('table thead th.actions').size(), 1, 'Action header column present');
+    equal($listView.find('table tbody tr:first td').size(), 3, 'Correct data column count');
+    equal($listView.find('table tbody tr:first td.actions').size(), 1, 'Action data column present');
+    equal($listView.find('table tbody tr:first td.actions .action').size(), 1, 'Correct action count');
+    equal($listView.find('table tbody tr:first td.actions .action:first .icon').size(), 1, 'Action has icon');
+    ok($listView.find('table tbody tr:first td.actions .action:first').hasClass('asyncTest'),
+       'First action has ID as CSS class');
+    ok($listView.find('td.actions .action:first').click(), 'Click first action');
+    equal($('.ui-dialog.confirm .message').html(), 'asyncActionConfirm', 'Confirmation message present');
+
+    // Make sure dialog is cleaned up -- always put below all confirm tests
+    $(':ui-dialog, .overlay').appendTo('#qunit-fixture');
+
+    ok($('.ui-dialog.confirm .ui-button.ok').click(), 'Confirm action');
+    equal($cloudStack.find('.notifications .total span').html(), '1', 'Notification total increased');
+    equal($('.notification-box ul li').size(), 1, 'Notification list has 1 item');
+    equal($('.notification-box ul li span').html(), 'asyncActionNotification', 'Notification list item has correct label');
+    ok($('.notification-box ul li').hasClass('pending'), 'Notification has pending state');
+    stop();
   });
 }(jQuery)); 
