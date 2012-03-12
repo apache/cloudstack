3b7d81b2753addcdb85ced1347ebe3bb5128fda5
abhi <abhinandan.prateek@citrix.com>
2012-03-12 16:56:43 +0530

    adding remove cmd for ldap

---
+++ api/src/com/cloud/api/response/LDAPRemoveResponse.java
@@ -0,0 +1,12 @@
+package com.cloud.api.response;
+
+import com.cloud.api.ApiConstants;
+import com.cloud.serializer.Param;
+import com.google.gson.annotations.SerializedName;
+
+public class LDAPRemoveResponse extends BaseResponse {
+
+	public LDAPRemoveResponse(){
+		super();
+	}
+}
