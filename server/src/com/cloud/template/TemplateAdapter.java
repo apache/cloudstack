e53cfeceb6f24a1cada75fdfbcaad41e693109a8
Alena Prokharchyk <alena.prokharchyk@citrix.com>
2012-03-19 16:18:37 -0700

    bug 14379: don't enable 8096 port by default (set global config variable to null by default, and don't create a listener on this port unless its set to valid integer)

--- server/src/com/cloud/template/TemplateAdapter.java
+++ server/src/com/cloud/template/TemplateAdapter.java
@@ -45,7 +45,7 @@ public interface TemplateAdapter extends Adapter {
             Boolean isExtractable, String format, Long guestOSId, Long zoneId, HypervisorType hypervisorType,
             String accountName, Long domainId, String chksum, Boolean bootable, Map details) throws ResourceAllocationException;
 	
-    public TemplateProfile prepare(boolean isIso, Long userId, String name, String displayText, Integer bits,
+    public TemplateProfile prepare(boolean isIso, long userId, String name, String displayText, Integer bits,
             Boolean passwordEnabled, Boolean requiresHVM, String url, Boolean isPublic, Boolean featured,
             Boolean isExtractable, String format, Long guestOSId, Long zoneId, HypervisorType hypervisorType,
             String chksum, Boolean bootable, String templateTag, Account templateOwner, Map details, Boolean sshKeyEnabled) throws ResourceAllocationException;	
