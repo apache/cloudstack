59e93d9f29c19a59efe21719c1225bf99313aefb
root <root@mgmt-srv.ovs-demo>
2012-03-26 21:58:18 +0800

    Minor changes to string format command

--- server/src/com/cloud/network/ovs/OvsTunnelManagerImpl.java
+++ server/src/com/cloud/network/ovs/OvsTunnelManagerImpl.java
@@ -136,7 +136,7 @@ public class OvsTunnelManagerImpl implements OvsTunnelManager {
 	private String handleFetchInterfaceAnswer(Answer[] answers, Long hostId){
 		OvsFetchInterfaceAnswer ans = (OvsFetchInterfaceAnswer) answers[0];
 		String s = String.format(
-				"(ip:%1$s, netmask:%2$s, mac:%3$s, label:%4s, host:%5l)",
+				"(ip:%1$s, netmask:%2$s, mac:%3$s, label:%4$s, host:%5$d)",
 				ans.getIp(), ans.getNetmask(), ans.getMac(), ans.getLabel(), hostId);
 		s_logger.debug("### About to add DB entry for:" + s);
 		OvsTunnelInterfaceVO ti = createInterfaceRecord(ans.getIp(), ans.getNetmask(), ans.getMac(),
