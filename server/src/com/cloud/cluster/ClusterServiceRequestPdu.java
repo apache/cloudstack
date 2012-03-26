040de04397e40754854ad6e15a05021845fa0e70
Kelven Yang <kelven@cloud.com>
2012-03-26 14:46:33 -0700

    bug 14301: Decouple synchronized crosss management server call with clustering transport. Reviewed-by: Kelven

---
+++ server/src/com/cloud/cluster/ClusterServiceRequestPdu.java
@@ -0,0 +1,37 @@
+package com.cloud.cluster;
+
+public class ClusterServiceRequestPdu extends ClusterServicePdu {
+
+    private String responseResult;
+    private long startTick;
+    private long timeout;
+    
+    public ClusterServiceRequestPdu() {
+        startTick = System.currentTimeMillis();
+        timeout = -1;
+    }
+
+    public String getResponseResult() {
+        return responseResult;
+    }
+
+    public void setResponseResult(String responseResult) {
+        this.responseResult = responseResult;
+    }
+
+    public long getStartTick() {
+        return startTick;
+    }
+
+    public void setStartTick(long startTick) {
+        this.startTick = startTick;
+    }
+
+    public long getTimeout() {
+        return timeout;
+    }
+
+    public void setTimeout(long timeout) {
+        this.timeout = timeout;
+    }
+}
