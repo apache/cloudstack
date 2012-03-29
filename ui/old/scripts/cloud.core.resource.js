01d36768b6c50dcb113876b72489761dac4bd17a
frank <frank.zhang@citrix.com>
2012-03-29 13:45:19 -0700

    remove old copyright from js

--- ui/old/scripts/cloud.core.resource.js
+++ ui/old/scripts/cloud.core.resource.js
@@ -1,21 +1,3 @@
- /**
- *  Copyright (C) 2010 Cloud.com, Inc.  All rights reserved.
- * 
- * This software is licensed under the GNU General Public License v3 or later.
- * 
- * It is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or any later version.
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
- */
-
 function buildZoneTree() {     
     var forceLogout = true;  // We force a logout only if the user has first added a POD for the very first time     
     var $loadingContainer = $("#leftmenu_physical_resource").find("#loading_container").show();
