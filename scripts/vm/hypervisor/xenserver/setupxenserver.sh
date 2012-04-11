537914ffae23526d29f8e3aa9a739db86005e47e
Rajesh Battala <rajesh.battala@citrix.com>
2012-04-11 17:52:24 +0530

    Xenserver Secure Console Proxy. Removing vnc hacks when adding xen host

--- scripts/vm/hypervisor/xenserver/setupxenserver.sh
+++ scripts/vm/hypervisor/xenserver/setupxenserver.sh
@@ -19,13 +19,6 @@ usage() {
 
 }
 
-#set iptables
-iptables -D RH-Firewall-1-INPUT -p tcp -m tcp --dport 5900:6099 -j ACCEPT 2>/dev/null
-iptables -I RH-Firewall-1-INPUT -p tcp -m tcp --dport 5900:6099 -j ACCEPT 2>&1
-
-# listen vnc on all interface
-sed -i 's/127\.0\.0\.1/0\.0\.0\.0/' /opt/xensource/libexec/vncterm-wrapper 2>&1
-sed -i 's/127\.0\.0\.1/0\.0\.0\.0/' /opt/xensource/libexec/qemu-dm-wrapper 2>&1
 
 # disable the default link local on xenserver
 sed -i /NOZEROCONF/d /etc/sysconfig/network
