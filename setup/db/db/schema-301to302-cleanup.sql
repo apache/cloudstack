1584a26ef9ed25fadff9c6ce680ec8e84e5a7b8b
Alena Prokharchyk <alena.prokharchyk@citrix.com>
2012-04-18 13:27:16 -0700

    CS-14558: db upgrade - fixed inconsistencies fresh vs upgraded 3.0.2 build

--- setup/db/db/schema-301to302-cleanup.sql
+++ setup/db/db/schema-301to302-cleanup.sql
@@ -16,8 +16,16 @@
 
 DROP TABLE IF EXISTS `cloud`.`network_tags`;
 
-ALTER TABLE `cloud`.`nics` modify `vm_type` varchar(32) DEFAULT NULL;
-ALTER TABLE `cloud`.`service_offering` modify `default_use` tinyint(1) UNSIGNED NOT NULL DEFAULT '0';
-ALTER TABLE `cloud`.`snapshots` modify `hypervisor_type` varchar(32) NOT NULL;
-ALTER TABLE `cloud`.`snapshots` modify `version` varchar(32) DEFAULT NULL;
-ALTER TABLE `cloud`.`volumes` modify `state` varchar(32) DEFAULT NULL;
+ALTER TABLE `cloud`.`nics` MODIFY `vm_type` varchar(32) DEFAULT NULL;
+ALTER TABLE `cloud`.`service_offering` MODIFY `default_use` tinyint(1) UNSIGNED NOT NULL DEFAULT '0';
+ALTER TABLE `cloud`.`snapshots` MODIFY `hypervisor_type` varchar(32) NOT NULL;
+ALTER TABLE `cloud`.`snapshots` MODIFY `version` varchar(32) DEFAULT NULL;
+ALTER TABLE `cloud`.`volumes` MODIFY `state` varchar(32) DEFAULT NULL;
+
+
+ALTER TABLE `cloud_usage`.`usage_ip_address` MODIFY `id` bigint(20) UNSIGNED NOT NULL;
+ALTER TABLE `cloud_usage`.`usage_ip_address` MODIFY  `is_source_nat` smallint(1) NOT NULL;
+ALTER TABLE `cloud_usage`.`usage_network` MODIFY `host_id` bigint(20) UNSIGNED NOT NULL;
+ALTER TABLE `cloud_usage`.`usage_network` MODIFY `host_type` varchar(32) DEFAULT NULL;
+ALTER TABLE `cloud_usage`.`user_statictics` MODIFY `device_id` bigint(20) UNSIGNED NOT NULL;
+
