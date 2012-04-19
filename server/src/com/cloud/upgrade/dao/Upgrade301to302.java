3337f1488bc36ef503c8148b664a53ab64077af4
Alena Prokharchyk <alena.prokharchyk@citrix.com>
2012-04-18 17:01:35 -0700

    CS-14553: db upgrade - for default network offerings:
    1) Change display text to match fresh 3.0.2
    2) Delete lb service for the offerings using VR as a provider and not having source nat service

--- server/src/com/cloud/upgrade/dao/Upgrade301to302.java
+++ server/src/com/cloud/upgrade/dao/Upgrade301to302.java
@@ -17,6 +17,9 @@ package com.cloud.upgrade.dao;
  */
 import java.io.File;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -70,6 +73,7 @@ public class Upgrade301to302 implements DbUpgrade {
     @Override
     public void performDataMigration(Connection conn) {
         dropKeysIfExists(conn);
+        updateSharedNetworks(conn);
     }
 
     @Override
@@ -80,5 +84,77 @@ public class Upgrade301to302 implements DbUpgrade {
         }
 
         return new File[] { new File(script) };
+    }
+    
+    
+    protected void updateSharedNetworks(Connection conn) {
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        ResultSet rs1 = null;
+
+        try {
+            pstmt = conn.prepareStatement("select n.id, map.id from `cloud`.`network_offerings` n, `cloud`.`ntwk_offering_service_map` map " +
+            		"where n.id=map.network_offering_id and map.service='Lb' and map.provider='VirtualRouter';");
+            rs = pstmt.executeQuery();
+            while (rs.next()) {
+                long ntwkOffId = rs.getLong(1);
+                long mapId = rs.getLong(2);
+                
+                //check if the network offering has source nat service enabled
+                pstmt = conn.prepareStatement("select n.id from `cloud`.`network_offerings` n, `cloud`.`ntwk_offering_service_map`" +
+                		" map where n.id=map.network_offering_id and map.service='SourceNat' AND n.id=?");
+                pstmt.setLong(1, ntwkOffId);
+                rs1 = pstmt.executeQuery();
+                if (rs1.next()) {
+                    continue;
+                }
+                
+                //delete the service only when there are no lb rules for the network(s) using this network offering
+                pstmt = conn.prepareStatement("select * from  `cloud`.`firewall_rules` f, `cloud`.`networks` n, `cloud`.`network_offerings`" +
+                		" off where f.purpose='LB' and f.network_id=n.id and n.network_offering_id=off.id and off.id=?");
+                pstmt.setLong(1, ntwkOffId);
+                rs1 = pstmt.executeQuery();
+                if (rs1.next()) {
+                    continue;
+                }
+                
+                //delete lb service for the network offering
+                pstmt = conn.prepareStatement("DELETE FROM `cloud`.`ntwk_offering_service_map` WHERE id=?");
+                pstmt.setLong(1, mapId);
+                pstmt.executeUpdate();
+                s_logger.debug("Deleted lb service for network offering id=" + ntwkOffId + " as it doesn't have source nat service enabled");
+                
+                //delete lb service for the network
+                pstmt = conn.prepareStatement("SELECT map.id, n.id FROM `cloud`.`ntwk_service_map` map, networks n WHERE n.network_offering_id=? " +
+                		"AND  map.network_id=n.id AND map.service='Lb'");
+                pstmt.setLong(1, ntwkOffId);
+                rs1 = pstmt.executeQuery();
+                while (rs1.next()) {
+                    mapId = rs1.getLong(1);
+                    long ntwkId=rs1.getLong(2);
+                    
+                    pstmt = conn.prepareStatement("DELETE FROM `cloud`.`ntwk_service_map` WHERE id=?");
+                    pstmt.setLong(1, mapId);
+                    pstmt.executeUpdate();
+                    s_logger.debug("Deleted lb service for network id=" + ntwkId + " as it doesn't have source nat service enabled");
+                }
+               
+            }
+        } catch (SQLException e) {
+            throw new CloudRuntimeException("Unable to update shared networks due to exception while executing query " + pstmt, e);
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (rs1 != null) {
+                    rs1.close();
+                }
+                if (pstmt != null) {
+                    pstmt.close();
+                }
+            } catch (SQLException e) {
+            }
+        }
     }
 }
