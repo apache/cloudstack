5ffc9990c3524565d79e32b337e1589842337bec
prachi <prachi@cloud.com>
2012-04-24 16:18:21 -0700

    Corrected the encryption key path

--- awsapi/src/com/cloud/bridge/util/EncryptionSecretKeyCheckerUtil.java
+++ awsapi/src/com/cloud/bridge/util/EncryptionSecretKeyCheckerUtil.java
@@ -20,7 +20,7 @@ import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 public class EncryptionSecretKeyCheckerUtil {
     private static final Logger s_logger = Logger.getLogger(EncryptionSecretKeyCheckerUtil.class);
     
-    private static final String s_keyFile = "C://pscp//key";
+    private static final String s_keyFile = "/etc/cloud/management/key";
     private static final String s_envKey = "CLOUD_SECRET_KEY";
     private static StandardPBEStringEncryptor s_encryptor = new StandardPBEStringEncryptor();
     private static boolean s_useEncryption = false;
