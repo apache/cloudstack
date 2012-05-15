5a5be77d3700d664690a7ea7f8b41553ac4ec116
Edison Su <sudison@gmail.com>
2012-05-15 15:12:18 -0700

    CS-14521
    Remove CLVM, due to license issue

--- agent/src/com/cloud/agent/resource/computing/LibvirtComputingResource.java
+++ agent/src/com/cloud/agent/resource/computing/LibvirtComputingResource.java
@@ -2609,19 +2609,16 @@ public class LibvirtComputingResource extends ServerResourceBase implements
 				}
 			} else {
 				int devId = (int) volume.getDeviceId();
-				if (pool.getType() == StoragePoolType.CLVM) {
-					disk.defBlockBasedDisk(physicalDisk.getPath(), devId,
-							diskBusType);
+
+				if (volume.getType() == Volume.Type.DATADISK) {
+					disk.defFileBasedDisk(physicalDisk.getPath(), devId,
+							DiskDef.diskBus.VIRTIO,
+							DiskDef.diskFmtType.QCOW2);
 				} else {
-					if (volume.getType() == Volume.Type.DATADISK) {
-						disk.defFileBasedDisk(physicalDisk.getPath(), devId,
-								DiskDef.diskBus.VIRTIO,
-								DiskDef.diskFmtType.QCOW2);
-					} else {
-						disk.defFileBasedDisk(physicalDisk.getPath(), devId,
-								diskBusType, DiskDef.diskFmtType.QCOW2);
-					}
+					disk.defFileBasedDisk(physicalDisk.getPath(), devId,
+							diskBusType, DiskDef.diskFmtType.QCOW2);
 				}
+
 			}
 
 			vm.getDevices().addDevice(disk);
@@ -2673,12 +2670,10 @@ public class LibvirtComputingResource extends ServerResourceBase implements
 
 		/* add patch disk */
 		DiskDef patchDisk = new DiskDef();
-		if (pool.getType() == StoragePoolType.CLVM) {
-			patchDisk.defBlockBasedDisk(datadiskPath, 1, rootDisk.getBusType());
-		} else {
-			patchDisk.defFileBasedDisk(datadiskPath, 1, rootDisk.getBusType(),
-					DiskDef.diskFmtType.RAW);
-		}
+
+		patchDisk.defFileBasedDisk(datadiskPath, 1, rootDisk.getBusType(),
+				DiskDef.diskFmtType.RAW);
+		
 		disks.add(patchDisk);
 
 		String bootArgs = vmSpec.getBootArgs();
