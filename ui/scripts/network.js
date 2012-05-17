33ce7933cce8f34d38c95959daea6136b6d7396d
Jessica Wang <jessica@cloud.com>
2012-05-16 17:01:56 -0700

    CS-14901: cloudstack 3.0 UI - IP Address page - non-SourceNAT IP - when network offering's Conserve Mode is off, hide EnableStaticNAT and EnableVPN when any PortForwarding rule or LoadBalancer rule exists.

--- ui/scripts/network.js
+++ ui/scripts/network.js
@@ -62,12 +62,50 @@
 				4. Once a LoadBalancer rule is added, hide StaticNat/VPN/PortForwarding.
 				*/
 				else { //item.issourcenat == false				   
-					 if (item.isstaticnat) { //1. Once StaticNat is enabled, hide VPN/PortForwarding/LoadBalancer.
-					   disallowedActions.push('enableVPN');
-					 }
-					 if (item.vpnenabled) { //2. Once VPN is enabled, hide StaticNat/PortForwarding/LoadBalancer.
-					   disallowedActions.push('enableStaticNAT');
-					 }
+					if (item.isstaticnat) { //1. Once StaticNat is enabled, hide VPN/PortForwarding/LoadBalancer.
+					  disallowedActions.push('enableVPN');
+					}
+					if (item.vpnenabled) { //2. Once VPN is enabled, hide StaticNat/PortForwarding/LoadBalancer.
+					  disallowedActions.push('enableStaticNAT');
+					}
+								 
+					//3. Once a PortForwarding rule is added, hide StaticNat/VPN/LoadBalancer.
+					$.ajax({
+						url: createURL('listLoadBalancerRules'),
+						data: {
+							publicipid: item.id,
+							listAll: true
+						},
+						dataType: 'json',
+						async: false,
+						success: function(json) {
+							var rules = json.listloadbalancerrulesresponse.loadbalancerrule;
+							if(rules != null && rules.length > 0) {
+							  disallowedActions.push('enableVPN');
+								disallowedActions.push('enableStaticNAT'); 
+							}
+						}
+					});						
+					
+					//4. Once a LoadBalancer rule is added, hide StaticNat/VPN/PortForwarding.
+					$.ajax({
+						url: createURL('listPortForwardingRules'),
+						data: {
+							ipaddressid: item.id,
+							listAll: true
+						},
+						dataType: 'json',
+						async: false,
+						success: function(json) {
+							// Get instance
+							var rules = json.listportforwardingrulesresponse.portforwardingrule;
+							if(rules != null && rules.length > 0) {
+							  disallowedActions.push('enableVPN');
+								disallowedActions.push('enableStaticNAT'); 
+							}
+						}
+					});	
+					 
 				}			  				
 			}			
 			
@@ -2103,15 +2141,13 @@
                         }
                       }
                     },
-                    dataProvider: function(args) {
-                      var apiCmd = "listLoadBalancerRules&listAll=true";
-                      //if(args.context.networks[0].type == "Shared")
-                      //	apiCmd += "&domainid=" + g_domainid + "&account=" + g_account;
-                      //else //args.context.networks[0].type == "Isolated"
-                        apiCmd += "&publicipid=" + args.context.ipAddresses[0].id;
-
+                    dataProvider: function(args) {   
                       $.ajax({
-                        url: createURL(apiCmd),
+                        url: createURL('listLoadBalancerRules'),
+												data: {
+												  publicipid: args.context.ipAddresses[0].id,
+													listAll: true
+												},
                         dataType: 'json',
                         async: true,
                         success: function(data) {
