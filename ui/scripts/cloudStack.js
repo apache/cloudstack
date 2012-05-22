67f88398a61e65a927eb5d17056c0405db818cdd
Brian Federle <brian.federle@citrix.com>
2012-05-22 12:32:43 -0700

    Use IE-compatible method for changing browser title

--- ui/scripts/cloudStack.js
+++ ui/scripts/cloudStack.js
@@ -382,11 +382,11 @@
       url: 'eula.' + g_lang + '.html',
       dataType: 'html',
       success: function(html) {
-        $('title').html('CloudPlatform');
+        document.title = 'CloudPlatform';
         cloudStack.uiCustom.login($.extend(loginArgs, { eula: html, hasLogo: true }));
       },
       error: function() {
-        $('title').html('CloudStack');
+        document.title = 'CloudStack';
         cloudStack.uiCustom.login(loginArgs);
       },	
 			beforeSend : function(XMLHttpResponse) {
