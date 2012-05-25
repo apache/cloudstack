9aaaab0ebed00d28bf578aa20a7100ac6ce223d8
prachi <prachi@cloud.com>
2012-05-25 14:30:37 -0700

    Merging awsapi-setup to master

---
+++ awsapi-setup/db/mysql/init_db.bat
@@ -0,0 +1,9 @@
+mysql --user=root --password < cloudbridge_db.sql
+mysql --user=root --password < cloudbridge_schema.sql
+mysql --user=root --password < cloudbridge_multipart.sql
+mysql --user=root --password < cloudbridge_index.sql
+mysql --user=root --password < cloudbridge_multipart_alter.sql
+mysql --user=root --password < cloudbridge_bucketpolicy.sql
+mysql --user=root --password < cloudbridge_policy_alter.sql
+mysql --user=root --password < cloudbridge_offering.sql
+mysql --user=root --password < cloudbridge_offering_alter.sql
