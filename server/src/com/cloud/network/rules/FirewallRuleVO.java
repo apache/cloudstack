822cf3c9ee229e17ae6bfe04c4c6388b882fee8d
anthony <anthony@cloud.com>
2012-06-25 13:09:05 -0700

    VPC : typo in FirewallRuleVO

--- server/src/com/cloud/network/rules/FirewallRuleVO.java
+++ server/src/com/cloud/network/rules/FirewallRuleVO.java
@@ -209,7 +209,7 @@ public class FirewallRuleVO implements Identity, FirewallRule {
         this.accountId = accountId;
         this.domainId = domainId;
         
-        if (sourceIpAddressId == null) {
+        if (ipAddressId == null) {
             assert (purpose == Purpose.NetworkACL) : "ipAddressId can be null for " + Purpose.NetworkACL + " only";
         }
         
