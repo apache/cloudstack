36eafbdb2c53885508a9800166e0059b70958254
anthony <anthony@cloud.com>
2012-07-11 12:01:01 -0700

    VPC : CS-15464, revoked static route will not be sent out

--- api/src/com/cloud/agent/api/routing/SetStaticRouteAnswer.java
+++ api/src/com/cloud/agent/api/routing/SetStaticRouteAnswer.java
@@ -25,8 +25,10 @@ public class SetStaticRouteAnswer extends Answer{
     
     public SetStaticRouteAnswer(SetStaticRouteCommand cmd, boolean success, String[] results) {
         super(cmd, success, null);
-        assert (cmd.getStaticRoutes().length == results.length) : "Static routes and their results should be the same length";
-        this.results = results;
+        if (cmd.getStaticRoutes() != null && results != null ) {
+            assert (cmd.getStaticRoutes().length == results.length) : "Static routes and their results should be the same length";
+            this.results = results;
+        }
     }
     
     public String[] getResults() {
