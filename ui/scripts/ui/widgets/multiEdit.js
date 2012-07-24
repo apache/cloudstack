c67ca89abcfb2f6a75396f874cbfa5712eae1fc2
Brian Federle <brian.federle@citrix.com>
2012-07-24 13:57:24 -0700

    Fix tag widget being appended to all dialogs

--- ui/scripts/ui/widgets/multiEdit.js
+++ ui/scripts/ui/widgets/multiEdit.js
@@ -334,7 +334,7 @@
                 });
 
                 if (options.tags) {
-                  $(':ui-dialog').append(
+                  $(':ui-dialog:last').append(
                     $('<div>').addClass('multi-edit-tags').tagger($.extend(true, {}, options.tags, {
                       context: $.extend(true, {}, options.context, {
                         multiRule: [multiRule]
