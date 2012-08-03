0b2b021f18e45091fe512b1e7c38d964923faed0
Alena Prokharchyk <alena.prokharchyk@citrix.com>
2012-08-03 14:07:18 -0700

    VPC: CS-15812 - fixed listVPCs by projectId

--- api/src/com/cloud/network/vpc/VpcService.java
+++ api/src/com/cloud/network/vpc/VpcService.java
@@ -113,13 +113,14 @@ public interface VpcService {
      * @param listAll TODO
      * @param restartRequired TODO
      * @param tags TODO
+     * @param projectId TODO
      * @param vpc
      * @return
      */
     public List<? extends Vpc> listVpcs(Long id, String vpcName, String displayText, 
             List<String> supportedServicesStr, String cidr, Long vpcOffId, String state, String accountName, Long domainId,
             String keyword, Long startIndex, Long pageSizeVal, Long zoneId, Boolean isRecursive, Boolean listAll, 
-            Boolean restartRequired, Map<String, String> tags);
+            Boolean restartRequired, Map<String, String> tags, Long projectId);
 
     /**
      * @param vpcId
