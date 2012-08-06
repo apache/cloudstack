f62b96125f389b88e5c989bb8082ca28e0c97d75
Pranav Saxena <pranav.saxena@citrix.com>
2012-08-06 15:59:15 +0530

    CS-15844:InterVlan - UI - Remove ACL Icon is not displayed properly for ACL Rules

--- ui/scripts/ui-custom/vpc.js
+++ ui/scripts/ui-custom/vpc.js
@@ -534,7 +534,7 @@
       break;
     case 'acl':
       // Show ACL dialog
-      $('<div>').multiEdit(
+      $('<div>').addClass('acl').multiEdit(
         $.extend(true, {}, actionArgs.multiEdit, {
           context: context
         })
