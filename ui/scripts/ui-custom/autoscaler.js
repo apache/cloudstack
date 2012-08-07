f348fcfb0e12f8448871dbea313e20e8ec001be8
Jessica Wang <jessica.wang@citrix.com>
2012-08-07 16:33:04 -0700

    cloudstack 3.0 UI - autoscale - implement enableAutoScaleVmGroup API, disableAutoScaleVmGroup API.

--- ui/scripts/ui-custom/autoscaler.js
+++ ui/scripts/ui-custom/autoscaler.js
@@ -115,6 +115,9 @@
             };
 
             action.action({
+						  context: {
+							  originalAutoscaleData: args.data,
+							},						  
               response: {
                 success: function(args) {
                   var notification = $.extend(args.notification, {
