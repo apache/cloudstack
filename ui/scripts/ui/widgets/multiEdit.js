a96c9013d4e4a121f02a21fe4d45bfea27629dcf
Brian Federle <brian.federle@citrix.com>
2012-08-10 09:16:01 -0700

    multiEdit: pass context to select handler

--- ui/scripts/ui/widgets/multiEdit.js
+++ ui/scripts/ui/widgets/multiEdit.js
@@ -737,6 +737,7 @@
         });
         $select.appendTo($td);
         field.select({
+          context: context,
           $select: $select,
           $form: $multiForm,
           response: {
