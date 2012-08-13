9bf7d757e84a38e2c99e54974536c9263e8a7c79
Brian Federle <brian.federle@citrix.com>
2012-08-13 12:15:35 -0700

    multiEdit: Support default value and description
    -If 'defaultValue' is specified for a text field, then put that value
     into the text field on load.
    
    -If 'desc' is specified for any field, then use it as a 'title'
     attribute to show a tooltip description.
    
    Example:
    
          'icmptype': {
            ...
            defaultValue: '-1',
            desc: 'Please specify -1 if you want to allow all'
            ...
          }

--- ui/scripts/ui/widgets/multiEdit.js
+++ ui/scripts/ui/widgets/multiEdit.js
@@ -782,6 +782,7 @@
                   );
 
             if (field.isDisabled) $input.hide();
+            if (field.defaultValue) $input.val(field.defaultValue);
           });
         } else {
           var $input = $('<input>')
@@ -794,6 +795,7 @@
                 .appendTo($td);
 
           if (field.isDisabled) $input.hide();
+          if (field.defaultValue) $input.val(field.defaultValue);
         }
       } else if (field.custom) {
         $('<div>').addClass('button add-vm custom-action')
@@ -821,6 +823,8 @@
           _l(args.add.label)
         ).appendTo($td);
       }
+
+      if (field.desc) $input.attr('title', field.desc);
     });
 
     // Setup header fields
