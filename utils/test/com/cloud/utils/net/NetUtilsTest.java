5b85edb96186583eae08d96e01c4ef4ee31e6222
Chiradeep Vittal <chiradeep@apache.org>
2012-08-16 11:28:11 -0700

    bug CS-16034 getRandomIp can return -1 unexpectedly also fixes unit test failures

--- utils/test/com/cloud/utils/net/NetUtilsTest.java
+++ utils/test/com/cloud/utils/net/NetUtilsTest.java
@@ -17,6 +17,7 @@
 package com.cloud.utils.net;
 
 import java.util.Set;
+import java.util.SortedSet;
 import java.util.TreeSet;
 
 import junit.framework.TestCase;
@@ -37,7 +38,7 @@ public class NetUtilsTest extends TestCase {
         ip = NetUtils.getRandomIpFromCidr(cidr, 8, new TreeSet<Long>());
         assertEquals("The ip " + NetUtils.long2Ip(ip) + " retrieved must be within the cidr " + cidr + "/8", cidr.substring(0, 4), NetUtils.long2Ip(ip).substring(0, 4));
 
-        Set<Long> avoid = new TreeSet<Long>();
+        SortedSet<Long> avoid = new TreeSet<Long>();
         ip = NetUtils.getRandomIpFromCidr(cidr, 30, avoid);
         assertTrue("We should be able to retrieve an ip on the first call.", ip != -1);
         avoid.add(ip);
@@ -49,6 +50,7 @@ public class NetUtilsTest extends TestCase {
         assertTrue("We should be able to retrieve an ip on the third call.", ip != -1);
         assertTrue("ip returned is not in the avoid list", !avoid.contains(ip));
         avoid.add(ip);
+       
         ip = NetUtils.getRandomIpFromCidr(cidr, 30, avoid);
         assertEquals("This should be -1 because we ran out of ip addresses: " + ip, ip, -1);
     }
