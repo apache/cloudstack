apache/cloudstack

commit 8254edc25a360051c4debad6040a5bcd516dec6a

CS-16019:InterVlan CIDR list in Add VPN Customer Gateway doesnt let the User know that CIDR list is a comma seperated list

ui/scripts/network.js
@@ -4028,7 +4028,7 @@
                 }
               },
               createForm: {
-                title: 'add VPN Customer Gateway',
+                title: 'Add VPN Customer Gateway',
                 fields: {
 								  name: {
 									  label: 'label.name',
@@ -4040,6 +4040,8 @@
                   },
                   cidrlist: {
                     label: 'CIDR list',
+                    desc:'Please enter a comma separated list of CIDRs if more 
+than one',          
                     validation: { required: true }
                   },
                   ipsecpsk: {

commit 93b0b11a7974fadfebebddfd431ec2ce0d15bafb

cloudstack 3.0 UI - fix a regression that made network disappear, caused by 8254edc25a360051c4debad6040a5bcd516dec6a( CS-16019:InterVlan CIDR list in Add VPN Customer Gateway doesnt let the User know that CIDR list is a comma seperated list).

ui/scripts/network.js
@@ -4040,8 +4040,7 @@
                   },
                   cidrlist: {
                     label: 'CIDR list',
-                    desc:'Please enter a comma separated list of CIDRs if more 
-than one',          
+                    desc:'Please enter a comma separated list of CIDRs if more than one',          
                     validation: { required: true }
                   },
                   ipsecpsk: {