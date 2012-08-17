fbdd65fe6b6233268f146a5a80e8c5de4c664766
Jessica Wang <jessica.wang@citrix.com>
2012-08-17 14:43:29 -0700

    CS-16046: cloudstack 3.0 UI - Edit zone - Local Storage Enabled field is a true/false field. Make it a checkbox in Edit mode.

--- ui/scripts/system.js
+++ ui/scripts/system.js
@@ -3973,7 +3973,7 @@
                       array1.push("&internaldns1=" + todb(args.data.internaldns1));
                       array1.push("&internaldns2=" + todb(args.data.internaldns2));  //internaldns2 can be empty ("") when passed to API
                       array1.push("&domain=" + todb(args.data.domain));
-                      array1.push("&localstorageenabled=" + todb(args.data.localstorageenabled));
+                      array1.push("&localstorageenabled=" + (args.data.localstorageenabled == 'on'));
                       $.ajax({
                         url: createURL("updateZone&id=" + args.context.physicalResources[0].id + array1.join("")),
                         dataType: "json",
@@ -4020,13 +4020,9 @@
                         },
                         localstorageenabled: {
                           label: 'label.local.storage.enabled',
-                          converter: function(args) {
-                            if(args)
-                              return "true";
-                            else
-                              return "false";
-                          },
-                          isEditable: true
+                          isBoolean: true,
+													isEditable: true,
+													converter:cloudStack.converters.toBooleanText
                         }
                       }
                     ],
