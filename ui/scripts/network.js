45eb035e20aeca6936410a54a6d161a8fb05620b
Pranav Saxena <pranav.saxena@citrix.com>
2012-08-20 11:54:52 +0530

    CS-15927:Filter to display only Non AutoscaleVMs (name to displayname property change)

--- ui/scripts/network.js
+++ ui/scripts/network.js
@@ -2206,10 +2206,10 @@
                                 function(instance) {
                                   // Hiding the AutoScale VMs
                                   var nonAutoScale=0;
-                                    if( instance.name.match(/AutoScale-LB-/)==null)
+                                    if( instance.displayname.match(/AutoScale-LB-/)==null)
                                              nonAutoScale =1;
                                      else {
-                                        if(instance.name.match(/AutoScale-LB-/).length)
+                                        if(instance.displayname.match(/AutoScale-LB-/).length)
                                             nonAutoScale =0;
                                           }
                                   var isActiveState = $.inArray(instance.state, ['Destroyed']) == -1;
