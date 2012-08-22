8e330fb69f7200338a7c83d2fda53c365f804c1e
Abhinandan Prateek <abhinandan.prateek@citrix.com>
2012-08-22 13:55:01 +0530

    Revert "CS-15970: Fix Redundant Router status for master and backup routers" reviewed-by: Abhi

--- core/src/com/cloud/hypervisor/vmware/resource/VmwareResource.java
+++ core/src/com/cloud/hypervisor/vmware/resource/VmwareResource.java
@@ -1021,7 +1021,7 @@ public class VmwareResource implements StoragePoolResource, ServerResource, Vmwa
     protected Answer execute(CheckRouterCommand cmd) {
         if (s_logger.isDebugEnabled()) {
             s_logger.debug("Executing resource CheckRouterCommand: " + _gson.toJson(cmd));
-            s_logger.debug("Run command on domR " + cmd.getAccessDetail(NetworkElementCommand.ROUTER_IP) + ", /opt/cloud/bin/checkrouter.sh ");
+            s_logger.debug("Run command on domR " + cmd.getAccessDetail(NetworkElementCommand.ROUTER_IP) + ", /root/checkrouter.sh ");
         }
 
         Pair<Boolean, String> result;
@@ -1029,7 +1029,7 @@ public class VmwareResource implements StoragePoolResource, ServerResource, Vmwa
             VmwareManager mgr = getServiceContext().getStockObject(VmwareManager.CONTEXT_STOCK_NAME);
             String controlIp = getRouterSshControlIp(cmd);
             result = SshHelper.sshExecute(controlIp, DEFAULT_DOMR_SSHPORT, "root", mgr.getSystemVMKeyFile(), null,
-                    "/opt/cloud/bin/checkrouter.sh ");
+                    "/root/checkrouter.sh ");
 
             if (!result.first()) {
                 s_logger.error("check router command on domR " + cmd.getAccessDetail(NetworkElementCommand.ROUTER_IP) + " failed, message: " + result.second());
