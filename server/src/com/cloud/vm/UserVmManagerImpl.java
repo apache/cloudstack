3298a481a8099a717583554196f8e92d4206e344
Abhinandan Prateek <aprateek@apache.org>
2012-08-23 12:22:02 +0530

    For assignVM, marking older snapshots removed in db

--- server/src/com/cloud/vm/UserVmManagerImpl.java
+++ server/src/com/cloud/vm/UserVmManagerImpl.java
@@ -3463,6 +3463,11 @@ public class UserVmManagerImpl implements UserVmManager, UserVmService, Manager 
             _resourceLimitMgr.incrementResourceCount(newAccount.getAccountId(), ResourceType.volume);
             _usageEventDao.persist(new UsageEventVO(EventTypes.EVENT_VOLUME_CREATE, volume.getAccountId(), volume.getDataCenterId(), volume.getId(), volume.getName(),
                     volume.getDiskOfferingId(), volume.getTemplateId(), volume.getSize()));
+            //snapshots: mark these removed in db
+            List<SnapshotVO> snapshots = _snapshotDao.listByVolumeIdIncludingRemoved(volume.getId());
+            for (SnapshotVO snapshot: snapshots){
+                _snapshotDao.remove(snapshot.getId());
+            }
         }
 
         _resourceLimitMgr.incrementResourceCount(newAccount.getAccountId(), ResourceType.user_vm);
