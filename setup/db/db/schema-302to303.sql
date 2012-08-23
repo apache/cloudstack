1d52a47ebf66ab60984529a3370b9b678e093b66
Nitin Mehta <nitin.mehta@citrix.com>
2012-08-23 11:00:29 +0530

    CS-16135: Create volume after upgrade from snapshot taken in  2.2.14 deletes the snapshot physically from sec. storage. Added DB upgrade sql to make swift_id NULL if they are set to 0 Reviewed-by: Kishan

--- setup/db/db/schema-302to303.sql
+++ setup/db/db/schema-302to303.sql
@@ -169,3 +169,5 @@ CREATE TABLE `cloud`.`port_profile` (
 DELETE FROM `cloud`.`storage_pool_host_ref` WHERE pool_id IN (SELECT id FROM storage_pool WHERE removed IS NOT NULL);
 
 ALTER TABLE `cloud`.`service_offering` MODIFY `nw_rate` smallint(5) unsigned DEFAULT '200' COMMENT 'network rate throttle mbits/s';
+UPDATE `cloud`.`snapshots` set swift_id=null where swift_id=0;
+
