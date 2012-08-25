e07f8449f430a1bcdf0449a582083f218c0c7bcd
Edison Su <sudison@gmail.com>
2012-08-24 18:24:26 -0700

    add storage interface

---
+++ platform/api/src/main/java/org/apache/cloudstack/platform/subsystem/api/storage/StorageSubSystem.java
@@ -0,0 +1,13 @@
+package org.apache.cloudstack.platform.subsystem.api.storage;
+
+import java.net.URI;
+
+import com.cloud.org.Grouping;
+
+public interface StorageSubSystem {
+    String getType();
+    Class<? extends Grouping> getScope();
+
+    URI grantAccess(String vol, String reservationId);
+    URI RemoveAccess(String vol, String reservationId);
+}
