3609e44b58cb1c3fe6d29014d807b42c7eafed3a
Kelven Yang <kelven.yang@citrix.com>
2012-08-27 10:39:49 -0700

    commit for https://reviews.apache.org/r/6608/

--- console-proxy/src/com/cloud/consoleproxy/ConsoleProxyAjaxImageHandler.java
+++ console-proxy/src/com/cloud/consoleproxy/ConsoleProxyAjaxImageHandler.java
@@ -16,6 +16,10 @@
 // under the License.
 package com.cloud.consoleproxy;
 
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.Map;
@@ -65,7 +69,14 @@ public class ConsoleProxyAjaxImageHandler implements HttpHandler {
 		String tag = queryMap.get("tag");
 		String ticket = queryMap.get("ticket");
 		String keyStr = queryMap.get("key");
-		int key = 0;
+		String console_url = queryMap.get("consoleurl");
+		String console_host_session = queryMap.get("sessionref");
+                String w = queryMap.get("w");	        
+                String h = queryMap.get("h");
+		
+                int key = 0;
+                int width = 144;
+                int height = 110;
 		
 		if(tag == null)
 			tag = "";
@@ -82,8 +93,15 @@ public class ConsoleProxyAjaxImageHandler implements HttpHandler {
 		}
 		
 		try {
-			key = Integer.parseInt(keyStr);
-		} catch (NumberFormatException e) {
+                    if (keyStr != null)
+            	        key = Integer.parseInt(keyStr);
+                    if(null != w)
+                       width = Integer.parseInt(w);
+
+                    if(null != h)
+                       height = Integer.parseInt(h);
+
+             } catch (NumberFormatException e) {
 			s_logger.warn("Invalid numeric parameter in query string: " + keyStr);
 			throw new IllegalArgumentException(e);
 		}
@@ -94,23 +112,48 @@ public class ConsoleProxyAjaxImageHandler implements HttpHandler {
 		param.setClientHostPassword(sid);
 		param.setClientTag(tag);
 		param.setTicket(ticket);
+		param.setClientTunnelUrl(console_url);
+		param.setClientTunnelSession(console_host_session);
+
 		ConsoleProxyClient viewer = ConsoleProxy.getVncViewer(param);
-		byte[] img = viewer.getAjaxImageCache().getImage(key);
-		if(img != null) {
+
+		if (key == 0) {
+			Image scaledImage = viewer.getClientScaledImage(width, height);
+			BufferedImage bufferedImage = new BufferedImage(width, height,
+					BufferedImage.TYPE_3BYTE_BGR);
+			Graphics2D bufImageGraphics = bufferedImage.createGraphics();
+			bufImageGraphics.drawImage(scaledImage, 0, 0, null);
+			ByteArrayOutputStream bos = new ByteArrayOutputStream(8196);
+			javax.imageio.ImageIO.write(bufferedImage, "jpg", bos);
+			byte[] bs = bos.toByteArray();
 			Headers hds = t.getResponseHeaders();
 			hds.set("Content-Type", "image/jpeg");
-			t.sendResponseHeaders(200, img.length);
-			
+			hds.set("Cache-Control", "no-cache");
+			hds.set("Cache-Control", "no-store");
+			t.sendResponseHeaders(200, bs.length);
 			OutputStream os = t.getResponseBody();
-			try {
-				os.write(img, 0, img.length);
-			} finally {
-				os.close();
-			}
+			os.write(bs);
+			os.close();
 		} else {
-			if(s_logger.isInfoEnabled())
-				s_logger.info("Image has already been swept out, key: " + key);
-			t.sendResponseHeaders(404, -1);
+			AjaxFIFOImageCache imageCache = viewer.getAjaxImageCache();
+	                byte[] img = imageCache.getImage(key);
+	
+			if(img != null) {
+				Headers hds = t.getResponseHeaders();
+				hds.set("Content-Type", "image/jpeg");
+				t.sendResponseHeaders(200, img.length);
+				
+				OutputStream os = t.getResponseBody();
+				try {
+					os.write(img, 0, img.length);
+				} finally {
+					os.close();
+				}
+			} else {
+				if(s_logger.isInfoEnabled())
+					s_logger.info("Image has already been swept out, key: " + key);
+				t.sendResponseHeaders(404, -1);
+			}
 		}
 	}
 }
