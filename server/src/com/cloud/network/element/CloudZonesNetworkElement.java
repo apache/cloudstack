66a2d8a0b498e40fb6abc1a7fa081062008049b0
Fang Wang <fang.wang@citrix.com>
2012-08-29 15:12:28 -0700

    cs-15627 Metadata should not expose "instance-id","vm-id" information to the regular User Description: Add the fix for the instance-id output. instance-id displays the UUID as well now.
    reviewed-by:   vijay

--- server/src/com/cloud/network/element/CloudZonesNetworkElement.java
+++ server/src/com/cloud/network/element/CloudZonesNetworkElement.java
@@ -148,7 +148,7 @@ public class CloudZonesNetworkElement extends AdapterBase implements NetworkElem
     }
 
     private VmDataCommand generateVmDataCommand(String vmPrivateIpAddress,
-            String userData, String serviceOffering, String zoneName, String guestIpAddress, String vmName, String vmInstanceName, String vmUuid, String publicKey) {
+            String userData, String serviceOffering, String zoneName, String guestIpAddress, String vmName, String vmUuid, String publicKey) {
         VmDataCommand cmd = new VmDataCommand(vmPrivateIpAddress, vmName);
 
         cmd.addVmData("userdata", "user-data", userData);
@@ -158,7 +158,7 @@ public class CloudZonesNetworkElement extends AdapterBase implements NetworkElem
         cmd.addVmData("metadata", "local-hostname", vmName);
         cmd.addVmData("metadata", "public-ipv4", guestIpAddress);
         cmd.addVmData("metadata", "public-hostname", guestIpAddress);
-        cmd.addVmData("metadata", "instance-id", vmInstanceName);
+        cmd.addVmData("metadata", "instance-id", vmUuid);
         cmd.addVmData("metadata", "vm-id", vmUuid);
         cmd.addVmData("metadata", "public-keys", publicKey);
 
@@ -208,7 +208,7 @@ public class CloudZonesNetworkElement extends AdapterBase implements NetworkElem
 
             cmds.addCommand(
                     "vmdata",
-                    generateVmDataCommand(nic.getIp4Address(), userData, serviceOffering, zoneName, nic.getIp4Address(), uservm.getVirtualMachine().getHostName(), uservm.getVirtualMachine().getInstanceName(),
+                    generateVmDataCommand(nic.getIp4Address(), userData, serviceOffering, zoneName, nic.getIp4Address(), uservm.getVirtualMachine().getHostName(),
                             uservm.getUuid(), sshPublicKey));
             try {
                 _agentManager.send(dest.getHost().getId(), cmds);
