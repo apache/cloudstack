a8addeea47539c4da39d8a23e17974874db115ac
Koushik Das <Koushik.Das@citrix.com>
2012-09-10 18:25:26 +0530

    CS-16297 : Upgrade from 2.2.14->2.2.15: Host goes to Alert state after upgrade from 2.2.14 to 2.2.15 Configuration value direct.agent.pool.size is missing in case of upgrade. In case of fresh install it is present. Reviewed-by: Kishan

--- setup/db/db/schema-2214to2215.sql
+++ setup/db/db/schema-2214to2215.sql
@@ -1,3 +1,5 @@
 --;
 -- Schema upgrade from 2.2.14 to 2.2.15;
 --;
+
+INSERT IGNORE INTO `cloud`.`configuration` VALUES ('Advanced', 'DEFAULT', 'management-server', 'direct.agent.pool.size', '1000', 'Default size for DirectAgentPool');
