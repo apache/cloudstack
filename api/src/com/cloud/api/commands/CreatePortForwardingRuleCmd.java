bbbccc0a7b912781be01272f9d9a526d49c28ed8
Alena Prokharchyk <alena.prokharchyk@citrix.com>
2012-09-14 11:38:07 -0700

    Port ranges for PF rules: public port start should be equal to ending port start; the same applies to end ports

--- api/src/com/cloud/api/commands/CreatePortForwardingRuleCmd.java
+++ api/src/com/cloud/api/commands/CreatePortForwardingRuleCmd.java
@@ -66,7 +66,6 @@ public class CreatePortForwardingRuleCmd extends BaseAsyncCreateCmd implements P
     @Parameter(name = ApiConstants.PRIVATE_END_PORT, type = CommandType.INTEGER, required = false, description = "the ending port of port forwarding rule's private port range")
     private Integer privateEndPort;
 
-
     @Parameter(name = ApiConstants.PUBLIC_START_PORT, type = CommandType.INTEGER, required = true, 
             description = "the starting port of port forwarding rule's public port range")
     private Integer publicStartPort;
