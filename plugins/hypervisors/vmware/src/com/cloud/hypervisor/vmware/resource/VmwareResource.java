5d301f2af50d24df5f6b3aea35db3894777394b6
Anthony Xu <anthony@cloud.com>
2012-09-21 10:12:20 -0700

    CS-16370 , VCP,  support delete guest network for vmware

--- plugins/hypervisors/vmware/src/com/cloud/hypervisor/vmware/resource/VmwareResource.java
+++ plugins/hypervisors/vmware/src/com/cloud/hypervisor/vmware/resource/VmwareResource.java
@@ -936,7 +936,7 @@ public class VmwareResource implements StoragePoolResource, ServerResource, Vmwa
             int ethDeviceNum = findRouterEthDeviceIndex(domrName, routerIp, nic.getMac());
             s_logger.info("find interface index. routerIp: " + routerIp + ", mac: " + nic.getMac() + ", index: " + ethDeviceNum);
 
-            String args = "-C ";
+            String args =(cmd.isAdd()?"-C":"-D");
             String dev = "eth" + ethDeviceNum;
             args += " -d " + dev;
             args += " -i " + domrGIP;
