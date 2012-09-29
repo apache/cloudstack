295e22881924559905d94c382664aa94e390a9be
Jessica Wang <jessica.wang@citrix.com>
2012-09-28 22:26:47 -0700

    bug 15089: cloudstack UI - domain page - get volume count and VM count from listAccounts API response that has volumetotal property and vmtotal property under each account object. listVolumes API and listVirtualMachines API are no longer called when domains are clicked in domain page.

--- ui/scripts/domains.js
+++ ui/scripts/domains.js
@@ -363,10 +363,21 @@
                   else
                     total = 0;
                   domainObj["accountTotal"] = total;
+                  var itemsAcc;
+                  var totalVMs=0;
+                  var totalVolumes=0;
+                  for(var i=0;i<total;i++) {
+                        itemsAcc = json.listaccountsresponse.account[i];
+                        totalVMs = totalVMs + itemsAcc.vmtotal;
+                        totalVolumes = totalVolumes + itemsAcc.volumetotal;
+                  }
+                  domainObj["vmTotal"] = totalVMs;
+                  domainObj["volumeTotal"] = totalVolumes;
+
                 }
               });
 
-              $.ajax({
+              /* $.ajax({
                 url: createURL("listVirtualMachines&details=min&domainid=" + domainObj.id),
                 async: false,
                 dataType: "json",
@@ -394,7 +405,7 @@
                     total = 0;
                   domainObj["volumeTotal"] = total;
                 }
-              });
+              });*/
 
               $.ajax({
                 url: createURL("listResourceLimits&domainid=" + domainObj.id),
