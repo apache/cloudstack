90fd0793feb124623817e896b883709c610ed40c
Chip Childers <chip.childers@gmail.com>
2012-10-01 16:40:00 -0400

    Modified devcloud build process to include maven3 install and steps.

--- tools/devcloud/devcloudbox/puppet-devcloud/files/installmaven.sh
+++ tools/devcloud/devcloudbox/puppet-devcloud/files/installmaven.sh
@@ -17,12 +17,6 @@
 # specific language governing permissions and limitations
 # under the License.
 
-export CATALINA_HOME=/opt/cloudstack/apache-tomcat-6.0.32
-export M2_HOME=/opt/cloudstack/apache-maven-3.0.4
-export M2=$M2_HOME/bin
-MAVEN_OPTS="-Xms256m -Xmx512m"
-PATH=$M2:$PATH
-cd /opt/cloudstack/incubator-cloudstack/
-/usr/bin/mvn -P deps
-/usr/bin/mvn clean
-/usr/bin/ant clean-all build-all deploy-server deploydb
+cd /opt/cloudstack
+/usr/bin/wget http://apache.mirrors.pair.com/maven/maven-3/3.0.4/binaries/apache-maven-3.0.4-bin.tar.gz
+/bin/tar xvfz apache-maven-3.0.4-bin.tar.gz
