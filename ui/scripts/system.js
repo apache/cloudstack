apache/cloudstack

commit 95b6a5eeda73beeb97fe8dd229c880c97668d44f

CS-15807 :Network label for OVM support on UI : Reviewed-By - Brian Federle

ui/scripts/system.js
@@ -106,7 +106,8 @@
 		  trafficType.kvmnetworklabel = dictionary['label.network.label.display.for.blank.value'];
 		if(trafficType.vmwarenetworklabel == null || trafficType.vmwarenetworklabel == 0)
 		  trafficType.vmwarenetworklabel = dictionary['label.network.label.display.for.blank.value'];
-		
+	        if(trafficType.ovmnetworklabel == null || trafficType.ovmnetworklabel == 0)
+                   trafficType.ovmnetworklabel = dictionary['label.network.label.display.for.blank.value'];	
     return trafficType;
   };
 
@@ -118,7 +119,8 @@
 		  array1.push("&kvmnetworklabel=" + labels.kvmnetworklabel);
 		if(labels.vmwarenetworklabel != dictionary['label.network.label.display.for.blank.value'])
 		  array1.push("&vmwarenetworklabel=" + labels.vmwarenetworklabel);
-				
+	        if(labels.ovmnetworklabel != dictionary['label.network.label.display.for.blank.value'])
+                  array1.push("&ovmnetworklabel=" + labels.ovmnetworklabel); 		
 		$.ajax({
       url: createURL('updateTrafficType' + array1.join("")),
       data: {
@@ -395,7 +397,8 @@
                   {
                     xennetworklabel: { label: 'label.xen.traffic.label', isEditable: true },
                     kvmnetworklabel: { label: 'label.kvm.traffic.label', isEditable: true },
-                    vmwarenetworklabel: { label: 'label.vmware.traffic.label', isEditable: true }
+                    vmwarenetworklabel: { label: 'label.vmware.traffic.label', isEditable: true },
+                    ovmnetworklabel: { label: 'OVM traffic label',isEditable: true }
                   }
                 ],
 
@@ -414,6 +417,7 @@
                       selectedPublicNetworkObj.xennetworklabel = trafficType.xennetworklabel;
                       selectedPublicNetworkObj.kvmnetworklabel = trafficType.kvmnetworklabel;
                       selectedPublicNetworkObj.vmwarenetworklabel = trafficType.vmwarenetworklabel;
+                      selectedPublicNetworkObj.ovmnetworklabel = trafficType.ovmnetworklabel;
 
                       args.response.success({data: selectedPublicNetworkObj});
                     }
@@ -566,7 +570,8 @@
                   {
                     xennetworklabel: { label: 'label.xen.traffic.label', isEditable: true },
                     kvmnetworklabel: { label: 'label.kvm.traffic.label', isEditable: true },
-                    vmwarenetworklabel: { label: 'label.vmware.traffic.label', isEditable: true }
+                    vmwarenetworklabel: { label: 'label.vmware.traffic.label', isEditable: true },
+                    ovmnetworklabel: { label: 'OVM traffic label', isEditable: true }
                   }
                 ],
 
@@ -583,6 +588,7 @@
                       selectedPublicNetworkObj.xennetworklabel = trafficType.xennetworklabel;
                       selectedPublicNetworkObj.kvmnetworklabel = trafficType.kvmnetworklabel;
                       selectedPublicNetworkObj.vmwarenetworklabel = trafficType.vmwarenetworklabel;
+                      selectedPublicNetworkObj.ovmnetworklabel = trafficType.ovmnetworklabel;
 
                       args.response.success({data: selectedPublicNetworkObj});
                     }
@@ -723,7 +729,8 @@
                   {
                     xennetworklabel: { label: 'label.xen.traffic.label', isEditable: true },
                     kvmnetworklabel: { label: 'label.kvm.traffic.label', isEditable: true },
-                    vmwarenetworklabel: { label: 'label.vmware.traffic.label', isEditable: true }
+                    vmwarenetworklabel: { label: 'label.vmware.traffic.label', isEditable: true },
+                    ovmnetworklabel: { label: 'OVM traffic label', isEditable: true } 
                   }
                 ],
                 dataProvider: function(args) {
@@ -738,7 +745,7 @@
                       selectedManagementNetworkObj.xennetworklabel = trafficType.xennetworklabel;
                       selectedManagementNetworkObj.kvmnetworklabel = trafficType.kvmnetworklabel;
                       selectedManagementNetworkObj.vmwarenetworklabel = trafficType.vmwarenetworklabel;
-
+                      selectedPublicNetworkObj.ovmnetworklabel = trafficType.ovmnetworklabel;
                       args.response.success({ data: selectedManagementNetworkObj });
                     }
                   });
@@ -855,7 +862,8 @@
                   { //updateTrafficType API
                     xennetworklabel: { label: 'label.xen.traffic.label', isEditable: true },
                     kvmnetworklabel: { label: 'label.kvm.traffic.label', isEditable: true },
-                    vmwarenetworklabel: { label: 'label.vmware.traffic.label', isEditable: true }
+                    vmwarenetworklabel: { label: 'label.vmware.traffic.label', isEditable: true },
+                    ovmnetworklabel: { label: 'OVM traffic label', isEditable: true }
                   }
                 ],
                 dataProvider: function(args) { //physical network + Guest traffic type       
@@ -890,6 +898,7 @@
 											selectedPhysicalNetworkObj["xennetworklabel"] = trafficType.xennetworklabel;
 											selectedPhysicalNetworkObj["kvmnetworklabel"] = trafficType.kvmnetworklabel;
 											selectedPhysicalNetworkObj["vmwarenetworklabel"] = trafficType.vmwarenetworklabel;
+                                                                                        selectedPhysicalNetworkObj["ovmnetworklabel"] = trafficType.ovmnetworklabel;
 
 											args.response.success({
 												actionFilter: function() {

commit 46fedf877f0eac20629d0cd536d301f6b6e5d2df

CLOUDSTACK-341: Fix incorrect js object in system.js
Use correct object in system.js that fixes the issue of management traffic
details not getting loaded as it fails on type error.

Fixes regression introduced in 95b6a5eeda73beeb97fe8dd229c880c97668d44f

ui/scripts/system.js
@@ -768,7 +768,7 @@
                       selectedManagementNetworkObj.xennetworklabel = trafficType.xennetworklabel;
                       selectedManagementNetworkObj.kvmnetworklabel = trafficType.kvmnetworklabel;
                       selectedManagementNetworkObj.vmwarenetworklabel = trafficType.vmwarenetworklabel;
-                      selectedPublicNetworkObj.ovmnetworklabel = trafficType.ovmnetworklabel;
+                      selectedManagementNetworkObj.ovmnetworklabel = trafficType.ovmnetworklabel;
                       args.response.success({ data: selectedManagementNetworkObj });
                     }
                   });