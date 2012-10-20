6a71110c756a22693f510bc88f6acac71be88a2d
Edison Su <sudison@gmail.com>
2012-10-19 17:31:28 -0700

    add unit test

---
+++ platform/storage/test/org/apache/cloudstack/storage/test/volumeServiceTest.java
@@ -0,0 +1,40 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.cloudstack.storage.test;
+
+import static org.junit.Assert.*;
+
+import org.apache.cloudstack.storage.volume.VolumeService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+
+@ContextConfiguration(locations="resources/storageContext.xml")
+public class volumeServiceTest {
+	
+	@Autowired
+	protected VolumeService volService;
+	@Test
+	public void test() {
+		volService.deleteVolume(1);
+		fail("Not yet implemented");
+	}
+
+}
