c272cf6b69a8d067b6d361be5aea8353ded2bfcf
Kelven Yang <kelveny@gmail.com>
2012-10-25 15:01:12 -0700

    add TransactionContextBuilder based on Spring AOP

---
+++ utils/test/com/cloud/utils/db/TransactionContextBuilderTest.java
@@ -0,0 +1,25 @@
+package com.cloud.utils.db;
+
+import javax.inject.Inject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations="classpath:/transactioncontextBuilderTest.xml")
+public class TransactionContextBuilderTest {
+
+	@Inject
+	DbAnnotatedBaseDerived _derived; 
+	
+	@Inject
+	DbAnnotatedBase _base;
+	
+	@Test
+	public void test() {
+		_derived.DbAnnotatedMethod();
+		_base.MethodWithClassDbAnnotated();
+	}
+}
