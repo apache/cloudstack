e531763d8789a99a503fb95f5eed2dd1be2e5739
Jessica Wang <jessica.wang@citrix.com>
2012-10-25 12:43:25 -0700

    Revert "cloudstack UI - Static NAT, Port Forwarding and Firewall Implementation on JuniperSRX - IP Address page - configuration - Firewall - exclude ICMP from Protocol dropdown."
    This reverts commit b7ccae0dde6f6949e5314036a7aa71460d676316.

--- ui/scripts/network.js
+++ ui/scripts/network.js
@@ -2000,40 +2000,14 @@
                               $icmpFields.parent().find('label.error').hide();
                             }
                           });
-													
-													var data = [
-														{ name: 'tcp', description: 'TCP' },
-														{ name: 'udp', description: 'UDP' }
-													];
-													//ICMP portocol is not supported in Firewall provided by JuniperSRX 													
-													$.ajax({
-														url: createURL('listNetworkOfferings'),
-														data: {
-															id: args.context.networks[0].networkofferingid
-														},
-														async: false,
-														success: function(json) {		
-															var serviceArray = json.listnetworkofferingsresponse.networkoffering[0].service;
-															var FirewallProviderArrayIncludesJuniperSRX = false;
-															for(var i = 0; i < serviceArray.length; i++) {
-																if(serviceArray[i].name == "Firewall") {
-																	var providerArray = serviceArray[i].provider;
-																	for(var k = 0; k < providerArray.length; k++) {
-																		if(providerArray[k].name == "JuniperSRX") {
-																			FirewallProviderArrayIncludesJuniperSRX = true;
-																			break;
-																		}
-																	}																					
-																	break;
-																}															
-															}		                              												
-															if(FirewallProviderArrayIncludesJuniperSRX == false) {																
-																data.push({ name: 'icmp', description: 'ICMP' }); //show ICMP option only when provider is not JuniperSRX
-															}																	
-														}
-													});			
-																										
-                          args.response.success({data: data});
+
+                          args.response.success({
+                            data: [
+                              { name: 'tcp', description: 'TCP' },
+                              { name: 'udp', description: 'UDP' },
+                              { name: 'icmp', description: 'ICMP' }
+                            ]
+                          });
                         }
                       },
                       'startport': { edit: true, label: 'label.start.port' },
