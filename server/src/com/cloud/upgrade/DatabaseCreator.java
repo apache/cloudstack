a388a748d055b837f9479fd797aef512f53a2a51
Alex Huang <alex.huang@citrix.com>
2012-10-25 17:34:37 -0700

    Added a database creator class. not working yet.  Edison will look at it

---
+++ server/src/com/cloud/upgrade/DatabaseCreator.java
@@ -0,0 +1,87 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.cloud.upgrade;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.cloud.utils.PropertiesUtil;
+import com.cloud.utils.db.ScriptRunner;
+import com.cloud.utils.db.Transaction;
+
+/**
+ * Creates the CloudStack Database
+ */
+public class DatabaseCreator {
+    protected static void printHelp(String cmd) {
+        System.out.println(
+                "DatabaseCreator creates the database schema by removing the \n" + 
+                        "previous schema, creating the schema, and running \n" +
+                "through the database updaters.");
+        System.out.println("Usage: " + cmd + " [initial schema file] [database upgrade class]");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+    public static void main2(String[] args) {
+        if (args.length < 2) {
+            printHelp("DatabaseCreator");
+            System.exit(1);
+        }
+
+        File cleanScript = PropertiesUtil.findConfigFile(args[0]);
+        if (cleanScript == null) {
+            System.err.println("Unable to find " + args[0]);
+            printHelp("DatabaseCreator");
+            System.exit(1);
+        }
+
+        Connection conn = Transaction.getStandaloneConnection();
+
+        ScriptRunner runner = new ScriptRunner(conn, true, true);
+        FileReader reader = null;
+        try {
+            reader = new FileReader(cleanScript);
+        } catch (FileNotFoundException e) {
+            System.err.println("Unable to read " + args[0] + ": " + e.getMessage());
+            System.exit(1);
+        } 
+        try {
+            runner.runScript(reader);
+        } catch (IOException e) {
+            System.err.println("Unable to read " + args[0] + ": " + e.getMessage());
+            System.exit(1);
+        } catch (SQLException e) {
+            System.err.println("Unable to execute " + args[0] + ": " + e.getMessage());
+            System.exit(1);
+        }
+
+        try {
+            conn.close();
+        } catch (SQLException e) {
+            System.err.println("Unable to close DB connection: " + e.getMessage());
+        }
+    }
+
+}
