a5ad9c3c50dae2ddc2b872c473cff1bdb488e1b5
Rohit Yadav <bhaisaab@apache.org>
2012-11-07 16:05:01 +0530

    cli: fix keyerror in help doc handler
    - Check and then print help docs
    - Add verb configure
    
    Signed-off-by: Rohit Yadav <bhaisaab@apache.org>

--- tools/cli/cloudmonkey/cloudmonkey.py
+++ tools/cli/cloudmonkey/cloudmonkey.py
@@ -369,7 +369,12 @@ class CloudStackShell(cmd.Cmd):
             subject = fields[2].partition(" ")[0]
             if verb not in self.cache_verbs:
                 self.cache_verb_miss(verb)
-            self.print_shell(self.cache_verbs[verb][subject][2])
+
+            if subject in self.cache_verbs[verb]:
+                self.print_shell(self.cache_verbs[verb][subject][2])
+            else:
+                self.print_shell("Error: no such api (%s) on %s" %
+                                 (subject, verb))
 
     def complete_help(self, text, line, begidx, endidx):
         fields = line.partition(" ")
@@ -400,7 +405,7 @@ def main():
     # Add verbs in grammar
     grammar = ['create', 'list', 'delete', 'update',
                'enable', 'disable', 'add', 'remove', 'attach', 'detach',
-               'assign', 'authorize', 'change', 'register',
+               'assign', 'authorize', 'change', 'register', 'configure',
                'start', 'restart', 'reboot', 'stop', 'reconnect',
                'cancel', 'destroy', 'revoke',
                'copy', 'extract', 'migrate', 'restore',
