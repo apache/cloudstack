13aa4d8530340e69f2a621a9ea813548195843a5
Rohit Yadav <bhaisaab@apache.org>
2012-12-03 23:30:54 -0800

    api_refactor: refactor nat apis
    - Fix refactored apis, fix mappings in commands*.in
    - Fix comments etc.
    
    Signed-off-by: Rohit Yadav <bhaisaab@apache.org>

--- api/src/org/apache/cloudstack/api/user/nat/command/DeleteIpForwardingRuleCmd.java
+++ api/src/org/apache/cloudstack/api/user/nat/command/DeleteIpForwardingRuleCmd.java
@@ -14,7 +14,7 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
-package com.cloud.api.commands;
+package org.apache.cloudstack.api.user.nat.command;
 
 import org.apache.log4j.Logger;
 
--- api/src/org/apache/cloudstack/api/user/nat/command/EnableStaticNatCmd.java
+++ api/src/org/apache/cloudstack/api/user/nat/command/EnableStaticNatCmd.java
@@ -14,8 +14,9 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
-package com.cloud.api.commands;
+package org.apache.cloudstack.api.user.nat.command;
 
+import org.apache.cloudstack.api.user.nat.command.CreateIpForwardingRuleCmd;
 import org.apache.log4j.Logger;
 
 import org.apache.cloudstack.api.ApiConstants;
