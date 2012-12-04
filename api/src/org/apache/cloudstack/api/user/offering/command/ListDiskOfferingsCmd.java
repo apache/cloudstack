6fe1b5f9b08796fdfbe770d8334610978a4e7a44
Rohit Yadav <bhaisaab@apache.org>
2012-12-04 10:37:14 -0800

    api_refactor: move diskoffering to offering
    Signed-off-by: Rohit Yadav <bhaisaab@apache.org>

--- api/src/org/apache/cloudstack/api/user/offering/command/ListDiskOfferingsCmd.java
+++ api/src/org/apache/cloudstack/api/user/offering/command/ListDiskOfferingsCmd.java
@@ -14,7 +14,7 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
-package org.apache.cloudstack.api.user.disk.command;
+package org.apache.cloudstack.api.user.offering.command;
 
 import java.util.ArrayList;
 import java.util.List;
