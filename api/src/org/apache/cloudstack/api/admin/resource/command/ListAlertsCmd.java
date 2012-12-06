089db9c6314a050cf0d09d4ad04f395f81caf609
Rohit Yadav <bhaisaab@apache.org>
2012-12-06 11:25:56 -0800

    api_refactor: capacity, hypvsr, alert and cert apis
    Signed-off-by: Rohit Yadav <bhaisaab@apache.org>

--- api/src/org/apache/cloudstack/api/admin/resource/command/ListAlertsCmd.java
+++ api/src/org/apache/cloudstack/api/admin/resource/command/ListAlertsCmd.java
@@ -14,71 +14,79 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
-package org.apache.cloudstack.api.user.hypervisor.command;
+package org.apache.cloudstack.api.admin.resource.command;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.cloudstack.api.admin.router.command.UpgradeRouterCmd;
 import org.apache.log4j.Logger;
 
+import com.cloud.alert.Alert;
 import org.apache.cloudstack.api.ApiConstants;
-import org.apache.cloudstack.api.BaseCmd;
+import org.apache.cloudstack.api.BaseListCmd;
 import org.apache.cloudstack.api.IdentityMapper;
 import org.apache.cloudstack.api.Implementation;
 import org.apache.cloudstack.api.Parameter;
-import com.cloud.api.response.HypervisorResponse;
+import com.cloud.api.response.AlertResponse;
 import com.cloud.api.response.ListResponse;
-import com.cloud.user.Account;
+import com.cloud.utils.Pair;
 
-@Implementation(description = "List hypervisors", responseObject = HypervisorResponse.class)
-public class ListHypervisorsCmd extends BaseCmd {
-    public static final Logger s_logger = Logger.getLogger(UpgradeRouterCmd.class.getName());
-    private static final String s_name = "listhypervisorsresponse";
+@Implementation(description = "Lists all alerts.", responseObject = AlertResponse.class)
+public class ListAlertsCmd extends BaseListCmd {
+
+    public static final Logger s_logger = Logger.getLogger(ListAlertsCmd.class.getName());
+
+    private static final String s_name = "listalertsresponse";
+
+    // ///////////////////////////////////////////////////
+    // ////////////// API parameters /////////////////////
+    // ///////////////////////////////////////////////////
+
+    @IdentityMapper(entityTableName="alert")
+    @Parameter(name = ApiConstants.ID, type = CommandType.LONG, description = "the ID of the alert")
+    private Long id;
+
+    @Parameter(name = ApiConstants.TYPE, type = CommandType.STRING, description = "list by alert type")
+    private String type;
+
+    // ///////////////////////////////////////////////////
+    // ///////////////// Accessors ///////////////////////
+    // ///////////////////////////////////////////////////
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    // ///////////////////////////////////////////////////
+    // ///////////// API Implementation///////////////////
+    // ///////////////////////////////////////////////////
 
     @Override
     public String getCommandName() {
         return s_name;
     }
 
-    // ///////////////////////////////////////////////////
-    // ////////////// API parameters /////////////////////
-    // ///////////////////////////////////////////////////
-
-    @IdentityMapper(entityTableName="data_center")
-    @Parameter(name = ApiConstants.ZONE_ID, type = CommandType.LONG, description = "the zone id for listing hypervisors.")
-    private Long zoneId;
-
-    // ///////////////////////////////////////////////////
-    // ///////////////// Accessors ///////////////////////
-    // ///////////////////////////////////////////////////
-
-    public Long getZoneId() {
-        return this.zoneId;
-    }
-
-    // ///////////////////////////////////////////////////
-    // ///////////// API Implementation///////////////////
-    // ///////////////////////////////////////////////////
-    @Override
-    public long getEntityOwnerId() {
-        return Account.ACCOUNT_ID_SYSTEM;
-    }
-
     @Override
     public void execute() {
-        List<String> result = _mgr.getHypervisors(getZoneId());
-        ListResponse<HypervisorResponse> response = new ListResponse<HypervisorResponse>();
-        ArrayList<HypervisorResponse> responses = new ArrayList<HypervisorResponse>();
-        if (result != null) {
-            for (String hypervisor : result) {
-                HypervisorResponse hypervisorResponse = new HypervisorResponse();
-                hypervisorResponse.setName(hypervisor);
-                hypervisorResponse.setObjectName("hypervisor");
-                responses.add(hypervisorResponse);
-            }
+        Pair<List<? extends Alert>, Integer> result = _mgr.searchForAlerts(this);
+        ListResponse<AlertResponse> response = new ListResponse<AlertResponse>();
+        List<AlertResponse> alertResponseList = new ArrayList<AlertResponse>();
+        for (Alert alert : result.first()) {
+            AlertResponse alertResponse = new AlertResponse();
+            alertResponse.setId(alert.getId());
+            alertResponse.setAlertType(alert.getType());
+            alertResponse.setDescription(alert.getSubject());
+            alertResponse.setLastSent(alert.getLastSent());
+
+            alertResponse.setObjectName("alert");
+            alertResponseList.add(alertResponse);
         }
-        response.setResponses(responses);
+
+        response.setResponses(alertResponseList, result.second());
         response.setResponseName(getCommandName());
         this.setResponseObject(response);
     }
