apache/cloudstack

commit b14b39a69fa295d1d20484170b95b97fe310af28

Remove IdentityProxy from BaseResponse, ExtractResponse. Now IdentityProxy is only referenced in CreateCmdResponse, which involves some async job logic change. Since it is not impacting list performance, will leave it there for now.
Signed-off-by: Min Chen <min.chen@citrix.com>

api/src/org/apache/cloudstack/api/command/user/volume/ExtractVolumeCmd.java
@@ -133,15 +133,17 @@ public class ExtractVolumeCmd extends BaseAsyncCmd {
                 ExtractResponse response = new ExtractResponse();
                 response.setResponseName(getCommandName());
                 response.setObjectName("volume");
-                response.setIdentityTableName("volumes");
-                response.setId(id);
-                response.setName(_entityMgr.findById(Volume.class, id).getName());
-                response.setZoneId(zoneId);
-                response.setZoneName(_entityMgr.findById(DataCenter.class, zoneId).getName());
+                Volume vol = _entityMgr.findById(Volume.class, id);
+                response.setId(vol.getUuid());
+                response.setName(vol.getName());
+                DataCenter zone = _entityMgr.findById(DataCenter.class, id);
+                response.setZoneId(zone.getUuid());
+                response.setZoneName(zone.getName());
                 response.setMode(mode);
-                response.setUploadId(uploadId);
+                response.setUploadId(uploadInfo.getUuid());
                 response.setState(uploadInfo.getUploadState().toString());
-                response.setAccountId(getEntityOwnerId());
+                Account account = _entityMgr.findById(Account.class, getEntityOwnerId());
+                response.setAccountId(account.getUuid());
                 response.setUrl(uploadInfo.getUploadUrl());
                 this.setResponseObject(response);
             } else {

api/src/com/cloud/storage/Upload.java
@@ -36,6 +36,8 @@ public interface Upload {
 
     long getId();
 
+    String getUuid();
+
     Date getCreated();
 
     Date getLastUpdated();

api/src/org/apache/cloudstack/api/BaseAsyncCmd.java
@@ -57,7 +57,8 @@ public abstract class BaseAsyncCmd extends BaseCmd {
     public ResponseObject getResponse(long jobId) {
         AsyncJobResponse response = new AsyncJobResponse();
 
-        response.setJobId(jobId);
+        AsyncJob job = _entityMgr.findById(AsyncJob.class, jobId);
+        response.setJobId(job.getUuid());
         response.setResponseName(getCommandName());
         return response;
     }

api/src/org/apache/cloudstack/api/BaseAsyncCreateCmd.java
@@ -17,6 +17,8 @@
 package org.apache.cloudstack.api;
 
 import org.apache.cloudstack.api.response.CreateCmdResponse;
+
+import com.cloud.async.AsyncJob;
 import com.cloud.exception.ResourceAllocationException;
 
 public abstract class BaseAsyncCreateCmd extends BaseAsyncCmd {
@@ -37,7 +39,8 @@ public abstract class BaseAsyncCreateCmd extends BaseAsyncCmd {
 
     public String getResponse(long jobId, long objectId, String objectEntityTable) {
         CreateCmdResponse response = new CreateCmdResponse();
-        response.setJobId(jobId);
+        AsyncJob job = _entityMgr.findById(AsyncJob.class, jobId);
+        response.setJobId(job.getUuid());
         response.setId(objectId);
         response.setIdEntityTable(objectEntityTable);
         response.setResponseName(getCommandName());

api/src/org/apache/cloudstack/api/BaseCmd.java
@@ -512,8 +512,9 @@ public abstract class BaseCmd {
                     throw new PermissionDeniedException("Can't add resources to the account id=" + account.getId() + " in state=" + account.getState() + " as it's no longer active");
                 }
             } else {
-                List<IdentityProxy> idList = new ArrayList<IdentityProxy>();
-                idList.add(new IdentityProxy("domain", domainId, "domainId"));
+                // idList is not used anywhere, so removed it now
+                //List<IdentityProxy> idList = new ArrayList<IdentityProxy>();
+                //idList.add(new IdentityProxy("domain", domainId, "domainId"));
                 throw new InvalidParameterValueException("Unable to find account by name " + accountName + " in domain with specified id");
             }
         }

api/src/org/apache/cloudstack/api/BaseResponse.java
@@ -46,18 +46,12 @@ public abstract class BaseResponse implements ResponseObject {
         this.objectName = objectName;
     }
 
-    //TODO: TO be replaced by getObjectUuid() after all response refactoring
-    public Long getObjectId() {
-        return null;
-    }
-
-    public String getObjectUuid(){
+    public String getObjectId() {
         return null;
     }
 
     // For use by list commands with pending async jobs
-    //TODO: To be replaced by jobUuid after all response refactoring
-    protected IdentityProxy jobId = new IdentityProxy("async_job");
+    protected String jobId;
 
     @SerializedName(ApiConstants.JOB_ID) @Param(description="the UUID of the latest async job acting on this object")
     protected String jobUuid;
@@ -65,13 +59,12 @@ public abstract class BaseResponse implements ResponseObject {
     @SerializedName(ApiConstants.JOB_STATUS) @Param(description="the current status of the latest async job acting on this object")
     private Integer jobStatus;
 
-    //TODO: TO be replaced by getter and setters for jobUuid.
-    public Long getJobId() {
-        return jobId.getValue();
+    public String getJobId() {
+        return jobId;
     }
 
-    public void setJobId(Long jobId) {
-        this.jobId.setValue(jobId);
+    public void setJobId(String jobId) {
+        this.jobId = jobId;
     }
 
     public String getJobUuid() {

api/src/org/apache/cloudstack/api/ResponseObject.java
@@ -45,40 +45,24 @@ public interface ResponseObject {
     void setObjectName(String name);
 
     /**
-     * Returns the object Id
+     * Returns the object UUid
      */
-    Long getObjectId();
-
-    /**
-    * Returns the object uuid. To replace getObjectId() later.
-    */
-    String getObjectUuid();
+    String getObjectId();
 
     /**
      * Returns the job id
      *
      * @return
      */
-    Long getJobId();
+    String getJobId();
 
-    /**
-     * Return the async job uuid. To replace getJobId() later.
-     * @return
-     */
-    String getJobUuid();
 
     /**
      * Sets the job id
      *
      * @param jobId
      */
-    void setJobId(Long jobId);
-
-    /**
-     * Set the job uuid. To replace setJobId() later.
-     * @param jobUuid job uuid
-     */
-    void setJobUuid(String jobUuid);
+    void setJobId(String jobId);
 
     /**
      * Returns the job status

api/src/org/apache/cloudstack/api/command/user/volume/CreateVolumeCmd.java
@@ -167,7 +167,7 @@ public class CreateVolumeCmd extends BaseAsyncCreateCmd {
             VolumeResponse response = _responseGenerator.createVolumeResponse(volume);
             //FIXME - have to be moved to ApiResponseHelper
             if (getSnapshotId() != null) {
-                Snapshot snap = _queryService.findSnapshotById(getSnapshotId());
+                Snapshot snap = _entityMgr.findById(Snapshot.class, getSnapshotId());
                 if (snap != null) {
                     response.setSnapshotId(snap.getUuid()); // if the volume was
                                                             // created from a

commit 40779975d33c554ce488a21d84e63e635e95984e

ExtractVolumeCmd: Fix regression, use Zone Id for getting zone
Fixes regression introduced in b14b39a69fa295d1d20484170b95b97fe310af28

Signed-off-by: Rohit Yadav <bhaisaab@apache.org>

api/src/org/apache/cloudstack/api/command/user/volume/ExtractVolumeCmd.java
@@ -137,7 +137,7 @@ public class ExtractVolumeCmd extends BaseAsyncCmd {
                 Volume vol = _entityMgr.findById(Volume.class, id);
                 response.setId(vol.getUuid());
                 response.setName(vol.getName());
-                DataCenter zone = _entityMgr.findById(DataCenter.class, id);
+                DataCenter zone = _entityMgr.findById(DataCenter.class, zoneId);
                 response.setZoneId(zone.getUuid());
                 response.setZoneName(zone.getName());
                 response.setMode(mode);