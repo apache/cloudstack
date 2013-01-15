b9c020fa7c80481fa9444dc6affb1beb069a8768
Rohit Yadav <bhaisaab@apache.org>
2013-01-14 18:54:26 -0800

    APIChecker: Fix interface definition to take in User
    Signed-off-by: Rohit Yadav <bhaisaab@apache.org>

--- api/src/org/apache/cloudstack/acl/APIChecker.java
+++ api/src/org/apache/cloudstack/acl/APIChecker.java
@@ -23,5 +23,8 @@ import com.cloud.utils.component.Adapter;
 // APIChecker checks the ownership and access control to API requests
 public interface APIChecker extends Adapter {
     // Interface for checking access for a role using apiname
+    // If true, apiChecker has checked the operation
+    // If false, apiChecker is unable to handle the operation or not implemented
+    // On exception, checkAccess failed don't allow
     boolean checkAccess(User user, String apiCommandName) throws PermissionDeniedException;
 }
