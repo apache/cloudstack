afb7e289fe3d6936d20b257ec2b753a265a7b9a9
Rohit Yadav <bhaisaab@apache.org>
2013-01-23 18:15:51 -0800

    cli: Fix development status to stable. cloudmonkey v1 at its most stable form
    Signed-off-by: Rohit Yadav <bhaisaab@apache.org>

--- tools/cli/setup.py
+++ tools/cli/setup.py
@@ -53,7 +53,7 @@ setup(
     include_package_data = True,
     zip_safe = False,
     classifiers = [
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Environment :: Console",
         "Intended Audience :: Developers",
         "Intended Audience :: End Users/Desktop",
