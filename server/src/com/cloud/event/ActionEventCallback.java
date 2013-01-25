1a2822d3bfbc3b5833ac8a8490264dfc60554020
Murali Reddy <murali.reddy@citrix.com>
2013-01-25 18:23:22 +0530

    hanlde case where no EventBus implementation is configured in component locator config file

--- server/src/com/cloud/event/ActionEventCallback.java
+++ server/src/com/cloud/event/ActionEventCallback.java
@@ -181,7 +181,9 @@ public class ActionEventCallback implements MethodInterceptor, AnnotationInterce
                 Adapters<EventBus> eventBusImpls = locator.getAdapters(EventBus.class);
                 if (eventBusImpls != null) {
                     Enumeration<EventBus> eventBusenum = eventBusImpls.enumeration();
-                   _eventBus = eventBusenum.nextElement();
+                    if (eventBusenum != null && eventBusenum.hasMoreElements()) {
+                        _eventBus = eventBusenum.nextElement();
+                    }
                 }
                 _eventBusLoaded = true;
             }
