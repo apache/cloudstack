f85ac656452e1091607d2a38f44735567c364658
Min Chen <min.chen@citrix.com>
2013-01-24 16:03:38 -0800

    Fix Snapshot unit testcase.

--- server/test/com/cloud/snapshot/SnapshotDaoTest.java
+++ server/test/com/cloud/snapshot/SnapshotDaoTest.java
@@ -20,17 +20,32 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 import com.cloud.storage.Snapshot;
 import com.cloud.storage.SnapshotVO;
 import com.cloud.storage.dao.SnapshotDaoImpl;
+import com.cloud.utils.component.ComponentContext;
 
 
 import junit.framework.Assert;
 import junit.framework.TestCase;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:/SnapshotDaoTestContext.xml")
 public class SnapshotDaoTest extends TestCase {
     @Inject SnapshotDaoImpl dao;
-    	
+
+    @Before
+    public void setup() throws Exception {
+        ComponentContext.initComponentsLifeCycle();
+    }
+
+    @Test
     public void testListBy() {
         List<SnapshotVO> snapshots = dao.listByInstanceId(3, Snapshot.Status.BackedUp);
         for(SnapshotVO snapshot : snapshots) {
