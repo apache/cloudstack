806105f9a1621a3c626ec4082d5d4fff4d96c629
Brian Federle <brian.federle@citrix.com>
2013-01-29 13:56:36 -0800

    UI plugins: Dynamically load CSS
    Adds a CSS file <pluginName>.css to the plugin structure, which allows
    developer to specify custom CSS to be loaded after their JS code.

--- ui/scripts/plugins.js
+++ ui/scripts/plugins.js
@@ -1,4 +1,16 @@
 (function($, cloudStack, require) {
+  var loadCSS = function(path) {
+    var $link = $('<link>');
+
+    $link.attr({
+      rel: 'stylesheet',
+      type: 'text/css',
+      href: path
+    });
+
+    $('head').append($link);
+  };
+
   var pluginAPI = {
     addSection: function(section) {
       cloudStack.sections[section.id] = section;
@@ -18,14 +30,18 @@
     var basePath = 'plugins/' + pluginID + '/';
     var pluginJS = basePath + pluginID + '.js';
     var configJS = basePath + 'config.js';
+    var pluginCSS = basePath + pluginID + '.css';
 
     require([pluginJS], function() {
       require([configJS]);
+      loadCSS(pluginCSS);
 
       // Execute plugin
       cloudStack.plugins[pluginID]({
         ui: pluginAPI
       });
     });
+
+    // Load CSS
   });
 }(jQuery, cloudStack, require));
