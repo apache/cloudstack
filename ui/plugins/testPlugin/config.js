a065aabda77c167c1533569ee7a7425a8f4e4f47
Brian Federle <brian.federle@citrix.com>
2013-01-29 14:34:42 -0800

    UI plugins: Add more metadata
    Add more metadata to plugin config.js:
    
    - externalLink: Link to plugin/author's web site
    - authorName: Author's full name
    - authorEmail: Author's contact e-mail

--- ui/plugins/testPlugin/config.js
+++ ui/plugins/testPlugin/config.js
@@ -1,6 +1,9 @@
 (function (cloudStack) {
   cloudStack.plugins.testPlugin.config = {
     title: 'Test Plugin',
-    desc: 'Sample plugin'
+    desc: 'Sample plugin',
+    externalLink: 'http://www.cloudstack.org/',
+    authorName: 'Test Plugin Developer',
+    authorEmail: 'plugin.developer@example.com'
   };
 }(cloudStack));
