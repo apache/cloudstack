4e7da0001d983aca351fc97b2385f6a462aa09be
Jessica Wang <jessica.wang@citrix.com>
2013-02-13 11:55:59 -0800

    CLOUDSTACK-618: cloudstack UI - API request throttling - for async job action uploadCustomCertificate, make frequency of calling queryAsyncJobResult API based on listCapabilities response.

--- ui/scripts/sharedFunctions.js
+++ ui/scripts/sharedFunctions.js
@@ -26,7 +26,7 @@ var g_timezone = null;
 var g_supportELB = null;
 var g_userPublicTemplateEnabled = "true";
 var g_cloudstackversion = null;
-var g_queryAsyncJobResultInterval = 6000;
+var g_queryAsyncJobResultInterval = 3000;
 
 //keyboard keycode
 var keycode_Enter = 13;
