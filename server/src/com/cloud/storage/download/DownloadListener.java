10236bee26f79391838c103972d0afa551e5efd4
Sanjay Tripathi <sanjay.tripathi@citrix.com>
2013-02-26 10:43:19 +0530

    CLOUDSTACK-1374 - When user uploads a volume using uploadVolume API command, only volume_host_ref table is getting updated with corresponding size of volume and "volumes" table, the size of volume still remains zero even ager the volume gets completely downloaded. Signed-off-by: Nitin Mehta <nitin.mehta@citrix.com>

--- server/src/com/cloud/storage/download/DownloadListener.java
+++ server/src/com/cloud/storage/download/DownloadListener.java
@@ -343,8 +343,14 @@ public class DownloadListener implements Listener {
 			updateBuilder.setInstallPath(answer.getInstallPath());
 			updateBuilder.setSize(answer.getTemplateSize());
 			updateBuilder.setPhysicalSize(answer.getTemplatePhySicalSize());
-			
+
 			volumeHostDao.update(getVolumeHostId(), updateBuilder);
+
+			// Update volume size in Volume table.
+			VolumeVO updateVolume = _volumeDao.createForUpdate();
+			updateVolume.setSize(answer.getTemplateSize());
+			_volumeDao.update(volume.getId(), updateVolume);
+
 			/*if (answer.getCheckSum() != null) {
 				VMTemplateVO templateDaoBuilder = _vmTemplateDao.createForUpdate();
 				templateDaoBuilder.setChecksum(answer.getCheckSum());
