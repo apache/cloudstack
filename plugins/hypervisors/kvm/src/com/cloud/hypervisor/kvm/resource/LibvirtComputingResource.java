8a50bac8a9958311f30de574d7d26c163f06c707
Marcus Sorensen <marcus@betterservers.com>
2013-03-13 15:29:09 -0600

    CLOUDSTACK-1648 - KVM - make storage pools non-persistent in libvirt. Persisting cloud-defined resources on the host has caused various problems. As a backward compatible fix, if an existing pool with a different name collides with a pool being created (by path), the pool will be redefined with the name cloudstack knows about. This is actually what brought up the bug, a persisted storage pool cloudstack wasn't managing.
    Signed-off-by: Marcus Sorensen <marcus@betterservers.com> 1363210149 -0600

--- plugins/hypervisors/kvm/src/com/cloud/hypervisor/kvm/resource/LibvirtComputingResource.java
+++ plugins/hypervisors/kvm/src/com/cloud/hypervisor/kvm/resource/LibvirtComputingResource.java
@@ -3283,7 +3283,7 @@ public class LibvirtComputingResource extends ServerResourceBase implements
             sscmd.setDataCenter(_dcId);
             sscmd.setResourceType(Storage.StorageResourceType.STORAGE_POOL);
         } catch (CloudRuntimeException e) {
-
+            s_logger.debug("Unable to initialize local storage pool: " + e);
         }
 
         if (sscmd != null) {
