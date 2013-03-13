6df107bcf6439c1de8346bc8b5c6be93694d9ee4
Marcus Sorensen <marcus@betterservers.com>
2013-03-13 11:10:56 -0600

    Summary: KVM - undefine persistent VMs on stop
    Detail: A previous patch fixed an issue where we are defining VMs to persist
    locally on KVM hosts, which can cause issues if the agent isn't running and
    libvirt decides to start the VM unbeknownst to cloudstack. The previous patch
    stopped defining VMs as persistent. This patch adds compatibility for existing
    cloudstack environments, removing the persistent definition on stop if needed.
    
    BUG-ID: CLOUDSTACK-600
    Signed-off-by: Marcus Sorensen <marcus@betterservers.com> 1363194656 -0600

--- plugins/hypervisors/kvm/src/com/cloud/hypervisor/kvm/resource/LibvirtComputingResource.java
+++ plugins/hypervisors/kvm/src/com/cloud/hypervisor/kvm/resource/LibvirtComputingResource.java
@@ -3929,9 +3929,13 @@ ServerResource {
         try {
             dm = conn.domainLookupByUUID(UUID.nameUUIDFromBytes(vmName
                     .getBytes()));
+            int persist = dm.isPersistent();
             if (force) {
                 if (dm.getInfo().state != DomainInfo.DomainState.VIR_DOMAIN_SHUTOFF) {
                     dm.destroy();
+                    if (persist == 1) {
+                        dm.undefine();
+                    }
                 }
             } else {
                 if (dm.getInfo().state == DomainInfo.DomainState.VIR_DOMAIN_SHUTOFF) {
@@ -3949,6 +3953,10 @@ ServerResource {
                     s_logger.warn("Timed out waiting for domain " + vmName
                             + " to shutdown gracefully");
                     return Script.ERR_TIMEOUT;
+                } else {
+                    if (persist == 1) {
+                        dm.undefine();
+                    }
                 }
             }
         } catch (LibvirtException e) {
