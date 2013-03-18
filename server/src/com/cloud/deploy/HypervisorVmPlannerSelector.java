d9b85e397d10f2291058ff972094decd8fc4ad7d
Prachi Damle <prachi@cloud.com>
2013-03-18 12:03:57 -0700

    CLOUDSTACK-1706 Failed to deploy VM with error "cannot find DeployPlannerSelector"
    Changes:
    
    - Regular plugin/adapter components should usually be loaded at run level RUNLEVEL_COMPONENT(5)
    - HypervisorVmPlannerSelector was at level 0, while configurationServer at level 2  - causing config to be not loaded for the HypervisorVmPlannerSelector

--- server/src/com/cloud/deploy/HypervisorVmPlannerSelector.java
+++ server/src/com/cloud/deploy/HypervisorVmPlannerSelector.java
@@ -18,12 +18,16 @@ package com.cloud.deploy;
 
 import javax.ejb.Local;
 
+import org.apache.log4j.Logger;
+
 import com.cloud.deploy.DeploymentPlanner.AllocationAlgorithm;
 import com.cloud.hypervisor.Hypervisor.HypervisorType;
 import com.cloud.vm.UserVmVO;
 
 @Local(value = {DeployPlannerSelector.class})
 public class HypervisorVmPlannerSelector extends AbstractDeployPlannerSelector {
+    private static final Logger s_logger = Logger.getLogger(HypervisorVmPlannerSelector.class);
+
     @Override
     public String selectPlanner(UserVmVO vm) {
         if (vm.getHypervisorType() != HypervisorType.BareMetal) {
@@ -37,6 +41,10 @@ public class HypervisorVmPlannerSelector extends AbstractDeployPlannerSelector {
                 } else if (_allocationAlgorithm.equals(AllocationAlgorithm.userconcentratedpod_random.toString())
                         || _allocationAlgorithm.equals(AllocationAlgorithm.userconcentratedpod_firstfit.toString())) {
                     return "UserConcentratedPodPlanner";
+                }
+            } else {
+                if (s_logger.isDebugEnabled()) {
+                    s_logger.debug("The allocation algorithm is null, cannot select the planner");
                 }
             }
         }
