17f286c62da63ea349978ed365ea1c3b4cac59f3
Chiradeep Vittal <chiradeep@apache.org>
2013-03-27 16:49:56 -0700

    QuickCloud: launch scripts for secondary storage and console proxy outside system vm
    QuickCloud: remove stuff added by the daemon

--- services/secondary-storage/scripts/_run.sh
+++ services/secondary-storage/scripts/_run.sh
@@ -36,6 +36,7 @@ do
 done
 keyvalues=
 
+LOGHOME=/var/log/cloud/
 CMDLINE=$(cat /var/cache/cloud/cmdline)
 
 #CMDLINE="graphical utf8 eth0ip=0.0.0.0 eth0mask=255.255.255.0 eth1ip=192.168.140.40 eth1mask=255.255.255.0 eth2ip=172.24.0.50 eth2mask=255.255.0.0 gateway=172.24.0.1 dns1=72.52.126.11 template=domP dns2=72.52.126.12 host=192.168.1.142 port=8250 mgmtcidr=192.168.1.0/24 localgw=192.168.140.1 zone=5 pod=5"
@@ -60,4 +61,4 @@ then
   maxmem=$eightypcnt
 fi
 
-java -Djavax.net.ssl.trustStore=./certs/realhostip.keystore -mx${maxmem}m -cp $CP com.cloud.agent.AgentShell $keyvalues $@
+java -Djavax.net.ssl.trustStore=./certs/realhostip.keystore -Dlog.home=$LOGHOME -mx${maxmem}m -cp $CP com.cloud.agent.AgentShell $keyvalues $@
