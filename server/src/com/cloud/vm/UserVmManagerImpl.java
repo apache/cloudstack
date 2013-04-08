f92e2657219f39776dd49ec44403040d54f70400
Prachi Damle <prachi@cloud.com>
2013-04-08 13:27:18 -0700

    ACl on affinity group

--- server/src/com/cloud/vm/UserVmManagerImpl.java
+++ server/src/com/cloud/vm/UserVmManagerImpl.java
@@ -2303,6 +2303,9 @@ public class UserVmManagerImpl extends ManagerBase implements UserVmManager, Use
                 AffinityGroupVO ag = _affinityGroupDao.findById(affinityGroupId);
                 if (ag == null) {
                     throw new InvalidParameterValueException("Unable to find affinity group by id " + affinityGroupId);
+                } else {
+                    // verify permissions
+                    _accountMgr.checkAccess(caller, null, true, owner, ag);
                 }
             }
         }
