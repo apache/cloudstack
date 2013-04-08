08467ee30709268b536e649b24ad029db792934c
Koushik Das <koushik.das@citrix.com>
2013-04-08 16:04:54 +0530

    Fixed compilation issues, incorrect merges from last commit

--- api/src/com/cloud/network/Network.java
+++ api/src/com/cloud/network/Network.java
@@ -137,6 +137,7 @@ public interface Network extends ControlledEntity, StateObject<Network.State>, I
         public static final Provider None = new Provider("None", false);
         // NiciraNvp is not an "External" provider, otherwise we get in trouble with NetworkServiceImpl.providersConfiguredForExternalNetworking 
         public static final Provider NiciraNvp = new Provider("NiciraNvp", false);
+        public static final Provider MidokuraMidonet = new Provider("MidokuraMidonet", true);
         public static final Provider CiscoVnmc = new Provider("CiscoVnmc", true);
 
         private String name;
