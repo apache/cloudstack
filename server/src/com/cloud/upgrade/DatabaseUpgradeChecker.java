c6994c37593ba0e8caa8e480569e4145269c01f1
Chip Childers <chip.childers@gmail.com>
2013-04-15 16:37:24 +0100

    CLOUDSTACK-1151: Adding 4.0.2 upgrade step to see if this unblocks testing of the bug in question.

--- server/src/com/cloud/upgrade/DatabaseUpgradeChecker.java
+++ server/src/com/cloud/upgrade/DatabaseUpgradeChecker.java
@@ -164,6 +164,11 @@ public class DatabaseUpgradeChecker implements SystemIntegrityChecker {
         _upgradeMap.put("3.0.2", new DbUpgrade[] { new Upgrade302to40(), new Upgrade40to401() });
 
         _upgradeMap.put("4.0.0", new DbUpgrade[] { new Upgrade40to401()});
+
+        _upgradeMap.put("4.0.1", new DbUpgrade[] { new Upgrade40to401() });
+
+        _upgradeMap.put("4.0.2", new DbUpgrade[] { new Upgrade40to401() });
+
     }
 
     protected void runScript(Connection conn, File file) {
