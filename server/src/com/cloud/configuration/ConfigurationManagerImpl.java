8ee4f6c080062632473c214b6fe896148e3d56e3
Sheng Yang <sheng.yang@citrix.com>
2013-05-07 14:49:50 -0700

    PVLAN: Check BroadcastDomainType before extract pvlan tag

--- server/src/com/cloud/configuration/ConfigurationManagerImpl.java
+++ server/src/com/cloud/configuration/ConfigurationManagerImpl.java
@@ -2689,8 +2689,9 @@ public class ConfigurationManagerImpl extends ManagerBase implements Configurati
             if (uri != null) {
                 String[] vlan = uri.toString().split("vlan:\\/\\/");
                 networkVlanId = vlan[1];
-                //For pvlan
-                networkVlanId = networkVlanId.split("-")[0];
+                if (network.getBroadcastDomainType() == BroadcastDomainType.Pvlan) {
+                	networkVlanId = networkVlanId.split("-")[0];
+                }
             }
 
             if (vlanId != null) {
