c7ebcb4e8c287ae88d2f6e6d71cc536612a5e635
Marcus Sorensen <marcus@betterservers.com>
2013-05-15 10:36:22 -0600

    Summary: Use hypervisor as clock source for system vms
    BUG-ID: CLOUDSTACK-2492
    Bugfix-for: 4.1,master
    Signed-off-by: Marcus Sorensen <marcus@betterservers.com> 1368635782 -0600

--- plugins/hypervisors/kvm/src/com/cloud/hypervisor/kvm/resource/LibvirtComputingResource.java
+++ plugins/hypervisors/kvm/src/com/cloud/hypervisor/kvm/resource/LibvirtComputingResource.java
@@ -3048,6 +3048,8 @@ ServerResource {
         if (vmTO.getOs().startsWith("Windows")) {
             clock.setClockOffset(ClockDef.ClockOffset.LOCALTIME);
             clock.setTimer("rtc", "catchup", null);
+        } else if (vmTO.getType() != VirtualMachine.Type.User) {
+            clock.setTimer("kvmclock", "catchup", null);
         }
 
         vm.addComp(clock);
