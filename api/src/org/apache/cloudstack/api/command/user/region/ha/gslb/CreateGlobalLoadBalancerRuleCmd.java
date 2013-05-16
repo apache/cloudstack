3c51c4e7adb717b5cee3bd01be716d75e288aa0c
Murali Reddy <muralimmreddy@gmail.com>
2013-05-16 12:26:23 +0530

    CLOUDSTACK-2461: createGSLBRule API is failing to pick the default algorithm "round robin" for parameter "gslblbmethod"
    fix defaults to round robin when parameter not specified

--- api/src/org/apache/cloudstack/api/command/user/region/ha/gslb/CreateGlobalLoadBalancerRuleCmd.java
+++ api/src/org/apache/cloudstack/api/command/user/region/ha/gslb/CreateGlobalLoadBalancerRuleCmd.java
@@ -85,7 +85,11 @@ public class CreateGlobalLoadBalancerRuleCmd extends BaseAsyncCreateCmd {
     }
 
     public String getAlgorithm() {
-        return algorithm;
+        if (algorithm != null) {
+            return algorithm;
+        } else {
+            return GlobalLoadBalancerRule.Algorithm.RoundRobin.name();
+        }
     }
 
     public String getGslbMethod() {
