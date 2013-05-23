795fd803da3eadee0fae1a13d0e97ae57b239657
Marcus Sorensen <marcus@betterservers.com>
2013-05-22 23:12:30 -0600

    Summary: KVM - double migration can fail
    Detail: Undefine VM after migration. Lingering domain definitions cause
    migrations back to the original host to fail, since domain already exists.
    
    BUG-ID: CLOUDSTACK-2640
    Bugfix-for: 4.1.0,4.2.0
    Signed-off-by: Marcus Sorensen <marcus@betterservers.com> 1369285950 -0600

--- plugins/hypervisors/kvm/src/com/cloud/hypervisor/kvm/resource/LibvirtComputingResource.java
+++ plugins/hypervisors/kvm/src/com/cloud/hypervisor/kvm/resource/LibvirtComputingResource.java
@@ -2728,6 +2728,9 @@ ServerResource {
         } finally {
             try {
                 if (dm != null) {
+                    if (dm.isPersistent() == 1) {
+                        dm.undefine();
+                    }
                     dm.free();
                 }
                 if (dconn != null) {
