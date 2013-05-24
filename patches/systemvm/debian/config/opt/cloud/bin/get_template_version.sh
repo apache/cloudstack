07715b91afd44c1ebc0a6977923a0de0f5c1b1ee
Sheng Yang <sheng.yang@citrix.com>
2013-05-23 18:45:38 -0700

    CLOUDSTACK-2639: Add flag for booting complete
    Don't execute any command before VR complete booting up process.

--- patches/systemvm/debian/config/opt/cloud/bin/get_template_version.sh
+++ patches/systemvm/debian/config/opt/cloud/bin/get_template_version.sh
@@ -16,5 +16,31 @@
 # specific language governing permissions and limitations
 # under the License.
 
+# As the last command send to router before any rules operation, wait until boot up done
+
+__TIMEOUT=60
+__FLAGFILE=/var/cache/cloud/boot_up_done
+done=0
+for i in `seq 1 $(($__TIMEOUT * 10))`
+do
+    if [ -e $__FLAGFILE ]
+    then
+        done=1
+        break
+    fi
+    sleep 0.1
+    if [ $((i % 10)) -eq 0 ]
+    then
+        logger -t cloud "Waiting for VM boot up done for one second"
+    fi
+done
+
+if [ -z $done ]
+then
+    # declare we failed booting process
+    echo "Waited 60 seconds but boot up haven't been completed"
+    exit
+fi
+
 echo -n `cat /etc/cloudstack-release`'&'
 cat /var/cache/cloud/cloud-scripts-signature
