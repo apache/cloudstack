2ac8f2b4525d664c9e44a9846cd688020df5cc72
Sanjay Tripathi <sanjay.tripathi@citrix.com>
2013-06-17 17:01:39 +0530

    CLOUDSTACK-1047: tracking in logs using job id.

--- server/src/com/cloud/storage/VolumeManagerImpl.java
+++ server/src/com/cloud/storage/VolumeManagerImpl.java
@@ -1872,8 +1872,8 @@ public class VolumeManagerImpl extends ManagerBase implements VolumeManager {
             if (s_logger.isInfoEnabled()) {
                 s_logger.info("Trying to attaching volume " + volumeId
                         + " to vm instance:" + vm.getId()
-                        + ", update async job-" + job.getId()
-                        + " progress status");
+                        + ", update async job-" + job.getId() + " = [ " + job.getUuid()
+                        + " ] progress status");
             }
 
             _asyncMgr.updateAsyncJobAttachment(job.getId(), "volume", volumeId);
@@ -1979,8 +1979,8 @@ public class VolumeManagerImpl extends ManagerBase implements VolumeManager {
             if (s_logger.isInfoEnabled()) {
                 s_logger.info("Trying to attaching volume " + volumeId
                         + "to vm instance:" + vm.getId()
-                        + ", update async job-" + job.getId()
-                        + " progress status");
+                        + ", update async job-" + job.getId() + " = [ " + job.getUuid()
+                        + " ] progress status");
             }
 
             _asyncMgr.updateAsyncJobAttachment(job.getId(), "volume", volumeId);
