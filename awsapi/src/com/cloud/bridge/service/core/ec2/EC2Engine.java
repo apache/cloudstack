8fc515e820e96436a01a09bc86a59cd71d9f2f2e
Likitha Shetty <likitha.shetty@citrix.com>
2013-06-27 16:57:27 +0530

    CLOUDSTACK-3258. In AWSAPI provide Tags support for resource 'security group'
    Signed-off-by: Likitha Shetty <likitha.shetty@citrix.com>

--- awsapi/src/com/cloud/bridge/service/core/ec2/EC2Engine.java
+++ awsapi/src/com/cloud/bridge/service/core/ec2/EC2Engine.java
@@ -2061,7 +2061,14 @@ public class EC2Engine extends ManagerBase {
                     ec2Group.setDomainId(group.getDomainId());
                     ec2Group.setId(group.getId().toString());
                     toPermission(ec2Group, group);
-
+                    List<CloudStackKeyValue> resourceTags = group.getTags();
+                    for(CloudStackKeyValue resourceTag : resourceTags) {
+                        EC2TagKeyValue param = new EC2TagKeyValue();
+                             param.setKey(resourceTag.getKey());
+                        if (resourceTag.getValue() != null)
+                            param.setValue(resourceTag.getValue());
+                        ec2Group.addResourceTag(param);
+                    }
                     groupSet.addGroup(ec2Group);
                 }
             return groupSet;
@@ -2510,6 +2517,8 @@ public class EC2Engine extends ManagerBase {
             return("template");
         else if(resourceType.equalsIgnoreCase("instance"))
             return("userVm");
+        else if (resourceType.equalsIgnoreCase("security-group"))
+            return("securityGroup");
         else
             return resourceType;
     }
@@ -2525,6 +2534,8 @@ public class EC2Engine extends ManagerBase {
             return("image");
         else if(resourceType.equalsIgnoreCase("userVm"))
             return("instance");
+        else if(resourceType.equalsIgnoreCase("securityGroup"))
+            return("security-group");
         else
             return (resourceType.toLowerCase());
     }
