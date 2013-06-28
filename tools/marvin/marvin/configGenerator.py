93712c2715c540150b86efdcc8e02c8f7facd0dc
Prasanna Santhanam <tsp@apache.org>
2013-06-29 00:38:52 +0530

    CLOUDSTACK-3074: include support for vmwaredc mapping to zone
    vmware dc can be mapped to the zone after CLOUDSTACK-1963. include
    support in marvin for adding the vmwaredc.
    
    vmwaredc : {
        name:
        username:
        vcenter:
        password:
        zoneid:
    }
    
    Will be sent during pod creation before cluster creation.
    
    Signed-off-by: Prasanna Santhanam <tsp@apache.org>
    (cherry picked from commit 9f4b7dcff896810e2276e02fa034de283964bf49)
    
    Conflicts:
    	tools/marvin/marvin/configGenerator.py
    
    Signed-off-by: Prasanna Santhanam <tsp@apache.org>

--- tools/marvin/marvin/configGenerator.py
+++ tools/marvin/marvin/configGenerator.py
@@ -94,10 +94,21 @@ class pod():
         self.endip = None
         self.zoneid = None
         self.clusters = []
+        self.vmwaredc = []
         '''Used in basic network mode'''
         self.guestIpRanges = []
 
-class cluster():
+
+class VmwareDc(object):
+    def __init__(self):
+        self.zoneid = None
+        self.name = None
+        self.vcenter = None
+        self.username = None
+        self.password = None
+
+
+class cluster(object):
     def __init__(self):
         self.clustername = None
         self.clustertype = None
