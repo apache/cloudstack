1f34d345b3ef21073436750d03e62fca94157ff6
Jessica Wang <jessicawang@apache.org>
2013-07-01 15:23:02 -0700

    CLOUDSTACK-3286: UI - Add Network Offering dialog - reset provider dropdown to first enabled option only if selected option is diabled.

--- ui/scripts/configuration.js
+++ ui/scripts/configuration.js
@@ -1292,7 +1292,10 @@
                         }                             
                       }     
                       
-                      $lbProvider.val($lbProvider.find('option:first'));     
+                      //if selected option is disabled, select the first enabled option instead
+                      if($lbProvider.find('option:selected:disabled').length > 0) { 
+                        $lbProvider.val($lbProvider.find('option:first'));                         
+                      }     
                     }
                     else {
                       $lbType.hide();                      
