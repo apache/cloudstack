5970b3927dbd4ff6761815788a129df08bdfa84b
Prasanna Santhanam <tsp@apache.org>
2013-07-06 19:19:02 +0530

    do not wait for cleanup
    simply cleanup and exit and network.gc will happen later
    
    Signed-off-by: Prasanna Santhanam <tsp@apache.org>
    (cherry picked from commit 620ed6e0f177bf32aa8d966cade07b93c7045dc9)

--- test/integration/component/test_vpc_network_staticnatrule.py
+++ test/integration/component/test_vpc_network_staticnatrule.py
@@ -37,7 +37,6 @@ from marvin.integration.lib.common import (get_domain,
                                                         get_zone,
                                                         get_template,
                                                         cleanup_resources,
-                                                        wait_for_cleanup,
                                                         list_routers)
 
 
@@ -248,9 +247,6 @@ class TestVPCNetworkPFRules(cloudstackTestCase):
         try:
             #Clean up, terminate the created network offerings
             cleanup_resources(self.apiclient, self._cleanup)
-            wait_for_cleanup(self.apiclient, [
-                                                            "network.gc.interval",
-                                                            "network.gc.wait"])
         except Exception as e:
             self.debug("Warning: Exception during cleanup : %s" % e)
             #raise Exception("Warning: Exception during cleanup : %s" % e)
