3a9c9bd7178c4af957da7f6bad897da4d680b956
tuna <ng.tuna@gmail.com>
2013-07-12 17:56:00 +0700

    fix white-space

--- plugins/hypervisors/xen/src/com/cloud/hypervisor/xen/resource/CitrixResourceBase.java
+++ plugins/hypervisors/xen/src/com/cloud/hypervisor/xen/resource/CitrixResourceBase.java
@@ -8741,7 +8741,7 @@ public abstract class CitrixResourceBase implements ServerResource, HypervisorRe
     @Override
     public void setRunLevel(int level) {
     }
-    
+
     private boolean is_xcp() {
     	Connection conn = getConnection();
     	String result = callHostPlugin(conn, "ovstunnel", "is_xcp");
@@ -8749,7 +8749,7 @@ public abstract class CitrixResourceBase implements ServerResource, HypervisorRe
     		return true;
     	return false;
     }
-    
+
     private String getLabel() {
     	Connection conn = getConnection();
     	String result = callHostPlugin(conn, "ovstunnel", "getLabel");
