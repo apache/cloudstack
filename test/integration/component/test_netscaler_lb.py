cfd892fc3b72def40ab4cde78a7b844510bb860a
Sowmya Krishnan <sowmya.krishnan@citrix.com>
2013-07-17 12:16:53 +0530

    Fixes CLOUDSTACK-3582
    Signed-off-by: Prasanna Santhanam <tsp@apache.org>

--- test/integration/component/test_netscaler_lb.py
+++ test/integration/component/test_netscaler_lb.py
@@ -2778,11 +2778,27 @@ class TestVmWithLb(cloudstackTestCase):
             hostnames.append(result)
             self.debug("Hostnames: %s" % str(hostnames))
 
-            self.assertNotEqual(
+            if hostnames[0] == hostnames[1]:
+                ssh_3 = self.vm_2.get_ssh_client(
+                                ipaddress=self.public_ip_1.ipaddress.ipaddress,
+                                reconnect=True
+                                )
+                self.debug("Command: hostname")
+                result = ssh_3.execute("hostname")
+                self.debug("Output: %s" % result)
+                hostnames.append(result)
+
+                self.assertNotEqual(
+                    hostnames[0],
+                    hostnames[2],
+                    "Both request should be served by different instances"
+                )
+            else:
+                 self.assertNotEqual(
                     hostnames[0],
                     hostnames[1],
                     "Both request should be served by different instances"
-                )
+                 )
         except Exception as e:
             self.fail("Exception occured during SSH: %s - %s" % (
                                         self.public_ip_1.ipaddress.ipaddress,
