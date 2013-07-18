9379190da9be5cb18b3985b0997aace57dfa8b75
frank <frank.zhang@citrix.com>
2013-07-18 16:30:56 -0700

    CLOUDSTACK-3426 UCS: Session cookie refresh must be supported.
    fixed resolved

--- plugins/hypervisors/ucs/src/com/cloud/ucs/manager/UcsCommands.java
+++ plugins/hypervisors/ucs/src/com/cloud/ucs/manager/UcsCommands.java
@@ -5,9 +5,9 @@
 // to you under the Apache License, Version 2.0 (the
 // "License"); you may not use this file except in compliance
 // with the License.  You may obtain a copy of the License at
-// 
+//
 //   http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing,
 // software distributed under the License is distributed on an
 // "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -24,6 +24,14 @@ public class UcsCommands {
         XmlObject cmd = new XmlObject("aaaLogin");
         cmd.putElement("inName", username);
         cmd.putElement("inPassword", password);
+        return cmd.dump();
+    }
+
+    public static String refreshCmd(String username, String password, String cookie) {
+        XmlObject cmd = new XmlObject("aaaRefresh");
+        cmd.putElement("inName", username);
+        cmd.putElement("inPassword", password);
+        cmd.putElement("inCookie", cookie);
         return cmd.dump();
     }
 
