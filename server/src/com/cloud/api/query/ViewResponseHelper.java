9a1cd1ac5398563b65b689d5f5d6a10bae3815b2
Min Chen <min.chen@citrix.com>
2013-07-18 10:53:33 -0700

    ListIsos only shows cross-zone ISO in one zone.

--- server/src/com/cloud/api/query/ViewResponseHelper.java
+++ server/src/com/cloud/api/query/ViewResponseHelper.java
@@ -400,9 +400,9 @@ public class ViewResponseHelper {
     }
 
     public static List<TemplateResponse> createIsoResponse(TemplateJoinVO... templates) {
-        Hashtable<Long, TemplateResponse> vrDataList = new Hashtable<Long, TemplateResponse>();
+        Hashtable<String, TemplateResponse> vrDataList = new Hashtable<String, TemplateResponse>();
         for (TemplateJoinVO vr : templates) {
-            TemplateResponse vrData = vrDataList.get(vr.getId());
+            TemplateResponse vrData = vrDataList.get(vr.getTempZonePair());
             if ( vrData == null ){
                 // first time encountering this volume
                 vrData = ApiDBUtils.newIsoResponse(vr);
@@ -411,7 +411,7 @@ public class ViewResponseHelper {
                 // update tags
                 vrData = ApiDBUtils.fillTemplateDetails(vrData, vr);
             }
-            vrDataList.put(vr.getId(), vrData);
+            vrDataList.put(vr.getTempZonePair(), vrData);
         }
         return new ArrayList<TemplateResponse>(vrDataList.values());
     }
