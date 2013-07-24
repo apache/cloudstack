8e30ee3f1a6de50c7ae6599d62e53c291d443103
Prasanna Santhanam <tsp@apache.org>
2013-07-24 16:52:59 +0530

    Revert "CLOUDSTACK-3696: Test case test_project_limits.TestResourceLimitsProject.test_06_volumes_per_project failed due to volume usage exceeded for project."
    Despite the patch the test fails. Removing the patch applied for testing
    locally.
    
    This reverts commit 605a31eb70e8998b4476ffadf2514c969b83f7d7.

--- test/integration/component/test_project_limits.py
+++ test/integration/component/test_project_limits.py
@@ -149,15 +149,10 @@ class TestProjectLimits(cloudstackTestCase):
                             cls.services["user"],
                             domainid=cls.domain.id
                             )
-        cls.disk_offering = DiskOffering.create(
-                                    cls.api_client,
-                                    cls.services["disk_offering"]
-                                    )
         cls._cleanup = [
             cls.admin,
             cls.user,
-            cls.domain,
-            cls.disk_offering
+            cls.domain
             ]
         return
 
@@ -199,8 +194,8 @@ class TestProjectLimits(cloudstackTestCase):
         #    account resource limits
         # 3. Increase Projects Resources limits above domains limit. Verify
         #    project can't have more resources than domain level limit allows.
-        # 4. Create Resource more than its set limit for the parent domain.
-        #    Verify resource allocation should fail giving proper message
+        # 4. Create Resource more than its set limit for a project. Verify
+        #    resource allocation should fail giving proper message
 
         # Create project as a domain admin
         project = Project.create(
@@ -314,41 +309,22 @@ class TestProjectLimits(cloudstackTestCase):
                                         self.apiclient,
                                         resource.resourcetype,
                                         domainid=self.domain.id,
-                                        max=1
+                                        max=2
                                       )
-            max_value = 2
-            self.debug(
-                "Attempting to update project: %s resource limit to: %s" % (
-                                                            project.id,
-                                                            max_value
-                                                            ))
-            # Update project resource limits to 3
-            update_resource_limit(
-                                    self.apiclient,
-                                    resource.resourcetype,
-                                    max=max_value,
-                                    projectid=project.id
-                                  )
-
-        # Verify project can't have more resources then limit set for domain by adding volumes.
-        volume = Volume.create(
-                          self.apiclient,
-                          self.services["volume"],
-                          zoneid=self.zone.id,
-                          diskofferingid=self.disk_offering.id,
-                          projectid=project.id
-                        )
-        # Exception should be raised for second volume
-        with self.assertRaises(Exception):
-            Volume.create(
-                          self.apiclient,
-                          self.services["volume"],
-                          zoneid=self.zone.id,
-                          diskofferingid=self.disk_offering.id,
-                          projectid=project.id
-                        )
-        volume.delete(self.apiclient);
-
+            with self.assertRaises(Exception):
+                max_value = 3
+                self.debug(
+                    "Attempting to update project: %s resource limit to: %s" % (
+                                                                project.id,
+                                                                max_value
+                                                                ))
+                # Update project resource limits to 3
+                update_resource_limit(
+                                        self.apiclient,
+                                        resource.resourcetype,
+                                        max=max_value,
+                                        projectid=project.id
+                                      )
         return
 
     @attr(tags=["advanced", "basic", "sg", "eip", "advancedns", "simulator"])
