apache/cloudstack

commit 4c81ea69c4e8326cc81a0e2607f6a6d99645ce5f

Fix for CLOUDSTACK-2536
Removing memoryovercommitratio and cpuovercommitratio parameters from addCluster and updateCluster APIs,
since these can be configurable using updateConfiguration API at cluster level.
By default while creating cluster these values are taken from global configuration parameters.

Conflicts:
	server/src/com/cloud/resource/ResourceManagerImpl.java

server/src/com/cloud/resource/ResourceManagerImpl.java
@@ -31,6 +31,10 @@ import javax.inject.Inject;
 import javax.naming.ConfigurationException;
 
 import com.cloud.dc.*;
+import com.cloud.server.ConfigurationServer;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Component;
+
 import org.apache.cloudstack.api.ApiConstants;
 import org.apache.cloudstack.api.command.admin.cluster.AddClusterCmd;
 import org.apache.cloudstack.api.command.admin.cluster.DeleteClusterCmd;
@@ -212,6 +216,8 @@ public class ResourceManagerImpl extends ManagerBase implements ResourceManager,
     PlannerHostReservationDao _plannerHostReserveDao;
     @Inject
     protected DedicatedResourceDao           _dedicatedDao;
+    @Inject
+    protected ConfigurationServer _configServer;
 
     protected List<? extends Discoverer> _discoverers;
 
@@ -477,8 +483,8 @@ public class ResourceManagerImpl extends ManagerBase implements ResourceManager,
         clusterId = cluster.getId();
         result.add(cluster);
 
-        ClusterDetailsVO cluster_detail_cpu = new ClusterDetailsVO(clusterId, "cpuOvercommitRatio", Float.toString(cmd.getCpuOvercommitRatio()));
-        ClusterDetailsVO cluster_detail_ram = new ClusterDetailsVO(clusterId, "memoryOvercommitRatio", Float.toString(cmd.getMemoryOvercommitRatio()));
+        ClusterDetailsVO cluster_detail_cpu = new ClusterDetailsVO(clusterId, "cpuOvercommitRatio", _configServer.getConfigValue(Config.CPUOverprovisioningFactor.key(), null, null));
+        ClusterDetailsVO cluster_detail_ram = new ClusterDetailsVO(clusterId, "memoryOvercommitRatio", _configServer.getConfigValue(Config.MemOverprovisioningFactor.key(), null, null));
         _clusterDetailsDao.persist(cluster_detail_cpu);
         _clusterDetailsDao.persist(cluster_detail_ram);
 
@@ -492,20 +498,6 @@ public class ResourceManagerImpl extends ManagerBase implements ResourceManager,
         details.put("username", username);
         details.put("password", password);
         _clusterDetailsDao.persist(cluster.getId(), details);
-
-        _clusterDetailsDao.persist(cluster_detail_cpu);
-        _clusterDetailsDao.persist(cluster_detail_ram);
-        // create a new entry only if the overcommit ratios are greater than 1.
-        if (cmd.getCpuOvercommitRatio().compareTo(1f) > 0) {
-            cluster_detail_cpu = new ClusterDetailsVO(clusterId, "cpuOvercommitRatio", Float.toString(cmd.getCpuOvercommitRatio()));
-            _clusterDetailsDao.persist(cluster_detail_cpu);
-        }
-
-
-        if(cmd.getMemoryOvercommitRatio().compareTo(1f) > 0) {
-             cluster_detail_ram = new ClusterDetailsVO(clusterId, "memoryOvercommitRatio", Float.toString(cmd.getMemoryOvercommitRatio()));
-            _clusterDetailsDao.persist(cluster_detail_ram);
-        }
 
         boolean success = false;
         try {
@@ -996,8 +988,7 @@ public class ResourceManagerImpl extends ManagerBase implements ResourceManager,
 
     @Override
     @DB
-    public Cluster updateCluster(Cluster clusterToUpdate, String clusterType, String hypervisor, String allocationState, String managedstate,
-            Float memoryovercommitratio, Float cpuovercommitratio) {
+    public Cluster updateCluster(Cluster clusterToUpdate, String clusterType, String hypervisor, String allocationState, String managedstate) {
 
         ClusterVO cluster = (ClusterVO) clusterToUpdate;
         // Verify cluster information and update the cluster if needed
@@ -1062,19 +1053,6 @@ public class ResourceManagerImpl extends ManagerBase implements ResourceManager,
                 doUpdate = true;
             }
         }
-
-
-       if (memoryovercommitratio != null) {
-           ClusterDetailsVO memory_detail = _clusterDetailsDao.findDetail(cluster.getId(),"memoryOvercommitRatio");
-           memory_detail.setValue(Float.toString(memoryovercommitratio));
-           _clusterDetailsDao.update(memory_detail.getId(),memory_detail);
-       }
-
-       if (cpuovercommitratio != null) {
-            ClusterDetailsVO cpu_detail = _clusterDetailsDao.findDetail(cluster.getId(),"cpuOvercommitRatio");
-            cpu_detail.setValue(Float.toString(cpuovercommitratio));
-            _clusterDetailsDao.update(cpu_detail.getId(),cpu_detail);
-       }
 
         if (doUpdate) {
             Transaction txn = Transaction.currentTxn();

api/src/com/cloud/resource/ResourceService.java
@@ -64,7 +64,7 @@ public interface ResourceService {
 
     boolean deleteCluster(DeleteClusterCmd cmd);
 
-    Cluster updateCluster(Cluster cluster, String clusterType, String hypervisor, String allocationState, String managedstate,Float memoryOvercommitRatio, Float cpuOvercommitRatio);
+    Cluster updateCluster(Cluster cluster, String clusterType, String hypervisor, String allocationState, String managedstate);
 
     List<? extends Host> discoverHosts(AddHostCmd cmd) throws IllegalArgumentException, DiscoveryException, InvalidParameterValueException;
 

api/src/org/apache/cloudstack/api/ApiConstants.java
@@ -48,7 +48,6 @@ public class ApiConstants {
     public static final String COMPONENT = "component";
     public static final String CPU_NUMBER = "cpunumber";
     public static final String CPU_SPEED = "cpuspeed";
-    public static final String CPU_OVERCOMMIT_RATIO="cpuovercommitratio";
     public static final String CREATED = "created";
     public static final String CUSTOMIZED = "customized";
     public static final String CUSTOMIZED_IOPS = "customizediops";
@@ -142,7 +141,6 @@ public class ApiConstants {
     public static final String MAX = "max";
     public static final String MAX_SNAPS = "maxsnaps";
     public static final String MEMORY = "memory";
-    public static final String MEMORY_OVERCOMMIT_RATIO="memoryovercommitratio";
     public static final String MODE = "mode";
     public static final String NAME = "name";
     public static final String METHOD_NAME = "methodname";

api/src/org/apache/cloudstack/api/command/admin/cluster/AddClusterCmd.java
@@ -81,12 +81,6 @@ public class AddClusterCmd extends BaseCmd {
     @Parameter(name = ApiConstants.VSM_IPADDRESS, type = CommandType.STRING, required = false, description = "the ipaddress of the VSM associated with this cluster")
     private String vsmipaddress;
 
-    @Parameter (name=ApiConstants.CPU_OVERCOMMIT_RATIO, type = CommandType.STRING, required = false , description = "value of the cpu overcommit ratio, defaults to 1")
-    private String cpuOvercommitRatio;
-
-    @Parameter(name = ApiConstants.MEMORY_OVERCOMMIT_RATIO, type = CommandType.STRING, required = false, description = "value of the default memory overcommit ratio, defaults to 1")
-    private String memoryOvercommitRatio;
-
     @Parameter(name = ApiConstants.VSWITCH_TYPE_GUEST_TRAFFIC, type = CommandType.STRING, required = false, description = "Type of virtual switch used for guest traffic in the cluster. Allowed values are, vmwaresvs (for VMware standard vSwitch) and vmwaredvs (for VMware distributed vSwitch)")
     private String vSwitchTypeGuestTraffic;
 
@@ -181,26 +175,9 @@ public class AddClusterCmd extends BaseCmd {
         this.allocationState = allocationState;
     }
 
-    public Float getCpuOvercommitRatio (){
-        if(cpuOvercommitRatio != null){
-           return Float.parseFloat(cpuOvercommitRatio);
-        }
-        return 1.0f;
-    }
-
-    public Float getMemoryOvercommitRatio(){
-        if (memoryOvercommitRatio != null){
-            return Float.parseFloat(memoryOvercommitRatio);
-        }
-        return 1.0f;
-    }
-
     @Override
     public void execute(){
         try {
-            if (getMemoryOvercommitRatio().compareTo(1f) < 0 || getCpuOvercommitRatio().compareTo(1f) < 0) {
-                throw new InvalidParameterValueException("cpu and memory overcommit ratios should be greater than or equal to one");
-            }
             List<? extends Cluster> result = _resourceService.discoverCluster(this);
             ListResponse<ClusterResponse> response = new ListResponse<ClusterResponse>();
             List<ClusterResponse> clusterResponses = new ArrayList<ClusterResponse>();

api/src/org/apache/cloudstack/api/command/admin/cluster/UpdateClusterCmd.java
@@ -54,12 +54,6 @@ public class UpdateClusterCmd extends BaseCmd {
     @Parameter(name=ApiConstants.MANAGED_STATE, type=CommandType.STRING, description="whether this cluster is managed by cloudstack")
     private String managedState;
 
-    @Parameter(name=ApiConstants.CPU_OVERCOMMIT_RATIO, type = CommandType.STRING, description = "Value of cpu overcommit ratio")
-    private String cpuovercommitratio;
-
-    @Parameter(name=ApiConstants.MEMORY_OVERCOMMIT_RATIO, type = CommandType.STRING, description = "Value of memory overcommit ratio")
-    private String memoryovercommitratio;
-
 
     public String getClusterName() {
         return clusterName;
@@ -107,40 +101,14 @@ public class UpdateClusterCmd extends BaseCmd {
         this.managedState = managedstate;
     }
 
-    public Float getCpuOvercommitRatio (){
-        if(cpuovercommitratio != null){
-            return Float.parseFloat(cpuovercommitratio);
-        }
-        return null;
-    }
-
-    public Float getMemoryOvercommitRaito (){
-        if (memoryovercommitratio != null){
-            return Float.parseFloat(memoryovercommitratio);
-        }
-        return null;
-    }
-
     @Override
     public void execute(){
         Cluster cluster = _resourceService.getCluster(getId());
         if (cluster == null) {
             throw new InvalidParameterValueException("Unable to find the cluster by id=" + getId());
         }
-        if (getMemoryOvercommitRaito() !=null){
-            if ((getMemoryOvercommitRaito().compareTo(1f) < 0)) {
-                throw new InvalidParameterValueException("Memory overcommit ratio should be greater than or equal to one");
-            }
-        }
-
-        if (getCpuOvercommitRatio() !=null){
-            if (getCpuOvercommitRatio().compareTo(1f) < 0) {
-                throw new InvalidParameterValueException("Cpu overcommit ratio should be greater than or equal to one");
-            }
-        }
-
-        Cluster result = _resourceService.updateCluster(cluster, getClusterType(), getHypervisor(), getAllocationState(), getManagedstate(), getMemoryOvercommitRaito(), getCpuOvercommitRatio());
-        if (result != null) {
+       Cluster result = _resourceService.updateCluster(cluster, getClusterType(), getHypervisor(), getAllocationState(), getManagedstate());
+       if (result != null) {
                 ClusterResponse clusterResponse = _responseGenerator.createClusterResponse(cluster, false);
                 clusterResponse.setResponseName(getCommandName());
                 this.setResponseObject(clusterResponse);

server/src/com/cloud/configuration/ConfigurationManagerImpl.java
@@ -310,6 +310,7 @@ public class ConfigurationManagerImpl extends ManagerBase implements Configurati
     private long _defaultPageSize = Long.parseLong(Config.DefaultPageSize.getDefaultValue());
     protected Set<String> configValuesForValidation;
     private Set<String> weightBasedParametersForValidation;
+    private Set<String> overprovisioningFactorsForValidation;
 
     @Override
     public boolean configure(final String name, final Map<String, Object> params) throws ConfigurationException {
@@ -323,6 +324,7 @@ public class ConfigurationManagerImpl extends ManagerBase implements Configurati
 
         populateConfigValuesForValidationSet();
         weightBasedParametersForValidation();
+        overProvisioningFactorsForValidation();
         return true;
     }
 
@@ -368,6 +370,13 @@ public class ConfigurationManagerImpl extends ManagerBase implements Configurati
         weightBasedParametersForValidation.add(Config.VmUserDispersionWeight.key());
 
 
+    }
+
+    private void overProvisioningFactorsForValidation() {
+        overprovisioningFactorsForValidation = new HashSet<String>();
+        overprovisioningFactorsForValidation.add(Config.MemOverprovisioningFactor.key());
+        overprovisioningFactorsForValidation.add(Config.CPUOverprovisioningFactor.key());
+        overprovisioningFactorsForValidation.add(Config.StorageOverprovisioningFactor.key());
     }
 
     @Override
@@ -685,11 +694,6 @@ public class ConfigurationManagerImpl extends ManagerBase implements Configurati
                 s_logger.error("Invalid scope id provided for the parameter " + name);
                 return "Invalid scope id provided for the parameter " + name;
             }
-            if ((name.equalsIgnoreCase("cpu.overprovisioning.factor") || name
-                    .equalsIgnoreCase("mem.overprovisioning.factor")) && value == null) {
-                s_logger.error("value cannot be null for cpu.overprovisioning.factor/mem.overprovisioning.factor");
-                return "value cannot be null for cpu.overprovisioning.factor/mem.overprovisioning.factor";
-            }
         }
 
         Class<?> type = c.getType();
@@ -698,9 +702,26 @@ public class ConfigurationManagerImpl extends ManagerBase implements Configurati
             if (type.equals(Boolean.class)) {
                 return "Please enter either 'true' or 'false'.";
             }
+            if (overprovisioningFactorsForValidation.contains(name)) {
+                String msg = "value cannot be null for the parameter " + name;
+                s_logger.error(msg);
+                return msg;
+            }
             return null;
         }
+
         value = value.trim();
+        try {
+            if (overprovisioningFactorsForValidation.contains(name) && (Float.parseFloat(value) < 1f)) {
+                String msg = name + " should be greater than or equal to 1";
+                s_logger.error(msg);
+                throw new InvalidParameterValueException(msg);
+            }
+        } catch (NumberFormatException e) {
+            String msg = "There was an error trying to parse the float value for: " + name;
+            s_logger.error(msg);
+            throw new InvalidParameterValueException(msg);
+        }
 
         if (type.equals(Boolean.class)) {
             if (!(value.equals("true") || value.equals("false"))) {

server/test/com/cloud/resource/MockResourceManagerImpl.java
@@ -110,7 +110,7 @@ public class MockResourceManagerImpl extends ManagerBase implements ResourceMana
      */
     @Override
     public Cluster updateCluster(Cluster cluster, String clusterType, String hypervisor, String allocationState,
-                                 String managedstate, Float memoryOvercommitRaito, Float cpuOvercommitRatio) {
+                                 String managedstate) {
         // TODO Auto-generated method stub
         return null;
     }

commit 784bef766c5ece7165cd37dcc2994111846ef586

CLOUDSTACK-2536: fix the regression introduced by 4c81ea69c4e8326cc81a0e2607f6a6d99645ce5f

server/src/com/cloud/resource/ResourceManagerImpl.java
@@ -216,8 +216,6 @@ public class ResourceManagerImpl extends ManagerBase implements ResourceManager,
     PlannerHostReservationDao _plannerHostReserveDao;
     @Inject
     protected DedicatedResourceDao           _dedicatedDao;
-    @Inject
-    protected ConfigurationServer _configServer;
 
     protected List<? extends Discoverer> _discoverers;
 
@@ -463,7 +461,6 @@ public class ResourceManagerImpl extends ManagerBase implements ResourceManager,
 
         List<ClusterVO> result = new ArrayList<ClusterVO>();
 
-        long clusterId = 0;
         ClusterVO cluster = new ClusterVO(dcId, podId, clusterName);
         cluster.setHypervisorType(cmd.getHypervisor());
 
@@ -480,13 +477,7 @@ public class ResourceManagerImpl extends ManagerBase implements ResourceManager,
             ex.addProxyObject(zone.getUuid(), "dcId");
             throw ex;
         }
-        clusterId = cluster.getId();
         result.add(cluster);
-
-        ClusterDetailsVO cluster_detail_cpu = new ClusterDetailsVO(clusterId, "cpuOvercommitRatio", _configServer.getConfigValue(Config.CPUOverprovisioningFactor.key(), null, null));
-        ClusterDetailsVO cluster_detail_ram = new ClusterDetailsVO(clusterId, "memoryOvercommitRatio", _configServer.getConfigValue(Config.MemOverprovisioningFactor.key(), null, null));
-        _clusterDetailsDao.persist(cluster_detail_cpu);
-        _clusterDetailsDao.persist(cluster_detail_ram);
 
         if (clusterType == Cluster.ClusterType.CloudManaged) {
             return result;
@@ -497,6 +488,8 @@ public class ResourceManagerImpl extends ManagerBase implements ResourceManager,
         details.put("url", url);
         details.put("username", username);
         details.put("password", password);
+        details.put("cpuOvercommitRatio", _configDao.getValue(Config.CPUOverprovisioningFactor.key()));
+        details.put("memoryOvercommitRatio", _configDao.getValue(Config.MemOverprovisioningFactor.key()));
         _clusterDetailsDao.persist(cluster.getId(), details);
 
         boolean success = false;
@@ -516,7 +509,7 @@ public class ResourceManagerImpl extends ManagerBase implements ResourceManager,
 
             List<HostVO> hosts = new ArrayList<HostVO>();
             Map<? extends ServerResource, Map<String, String>> resources = null;
-            resources = discoverer.find(dcId, podId, clusterId, uri, username, password, null);
+            resources = discoverer.find(dcId, podId, cluster.getId(), uri, username, password, null);
 
             if (resources != null) {
                 for (Map.Entry<? extends ServerResource, Map<String, String>> entry : resources.entrySet()) {
@@ -537,8 +530,8 @@ public class ResourceManagerImpl extends ManagerBase implements ResourceManager,
             throw new DiscoveryException("Unable to add the external cluster");
         } finally {
             if (!success) {
-                _clusterDetailsDao.deleteDetails(clusterId);
-                _clusterDao.remove(clusterId);
+                _clusterDetailsDao.deleteDetails(cluster.getId());
+                _clusterDao.remove(cluster.getId());
             }
         }
     }