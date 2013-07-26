0f7870332ad2080d9f61da463308fe52438d1cc7
Prasanna Santhanam <tsp@apache.org>
2013-07-27 00:01:02 +0530

    CLOUDSTACK-3797: Fix wget installation of xs-tools in to the builtin
    Signed-off-by: Prasanna Santhanam <tsp@apache.org>

--- tools/appliance/definitions/builtin/install-xs-tools.sh
+++ tools/appliance/definitions/builtin/install-xs-tools.sh
@@ -1,7 +1,7 @@
 # get the latest xs tools available from xen.org
-wget --no-check-certificate http://downloads.xen.org/XCP/debian/xs-tools-5.9.960.iso -o xs-tools.iso
+wget --no-check-certificate http://downloads.xen.org/XCP/debian/xs-tools-5.9.960.iso -O xs-tools.iso
 
 sudo mount -o loop xs-tools.iso /mnt
 
 #install the xs tools
-sudo sh /mnt/Linux/install.sh
+sudo yes | sudo sh /mnt/Linux/install.sh
