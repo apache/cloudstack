1b340e054401eaf865ce4b77a2f87608023dcf7b
Devdeep Singh <devdeep@gmail.com>
2013-07-26 10:57:37 +0530

    CLOUDSTACK-3590. Adding new xenserver host to cluster fails in first attempt. While adding a host to the pool it may take a little longer to join the pool. Increased the time the resource waits and checks to make sure the host has joined as slave to the pool.

--- plugins/hypervisors/xen/src/com/cloud/hypervisor/xen/resource/XenServerConnectionPool.java
+++ plugins/hypervisors/xen/src/com/cloud/hypervisor/xen/resource/XenServerConnectionPool.java
@@ -197,7 +197,7 @@ public class XenServerConnectionPool {
                     }
                 }
                 try {
-                    Thread.sleep(2000);
+                    Thread.sleep(3000);
                 } catch (InterruptedException e) {
                 } 
             }
