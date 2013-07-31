86aafa2db613ff8985770363315c47d70c6f98cb
Chip Childers <chipchilders@apache.org>
2013-07-31 11:08:43 -0400

    Fixing setnextversion.sh

--- tools/build/setnextversion.sh
+++ tools/build/setnextversion.sh
@@ -64,8 +64,8 @@ echo "found $currentversion"
 echo 'setting version numbers'
 mvn versions:set -DnewVersion=$version -P vmware -P developer -P systemvm -P simulator -P baremetal -P ucs -Dnonoss
 mv deps/XenServerJava/pom.xml.versionsBackup deps/XenServerJava/pom.xml
-perl -pi -e 's/$ENV{'currentversion'}/$ENV{'version'}/' deps/XenServerJava/pom.xml
-perl -pi -e 's/$ENV{'currentversion'}/$ENV{'version'}/' tools/apidoc/pom.xml
+perl -pi -e "s/$currentversion/$version/" deps/XenServerJava/pom.xml
+perl -pi -e "s/$currentversion/$version/" tools/apidoc/pom.xml
 git clean -f
 
 echo 'commit changes'
