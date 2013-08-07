d47b4a994923246225131624ab806485987ee01f
Vijayendra Bhamidipati <vijayendra.bhamidipati@citrix.com>
2013-08-07 11:53:42 -0700

    CLOUDSTACK-4167: Put in upgrade paths in master for 4.1.2 and 4.2
    Description:
    
        Define upgrade paths from 4.1.0 to 4.1.1, and 4.1.1 to 4.1.2,
        and 4.1.2 to 4.2.0. This new path will replace the existing
        4.1.0 to 4.2.0 path. This is required to allow upgrades from
        4.1.2 installations to 4.2.0. The 4.1.2 installation will have
        that db version upon installation from the 4.1 branch. Please
        note that these new upgrade paths are empty and don't make
        any SQL schema modifications.

--- engine/schema/src/com/cloud/upgrade/dao/Upgrade412to420.java
+++ engine/schema/src/com/cloud/upgrade/dao/Upgrade412to420.java
@@ -41,13 +41,13 @@ import com.cloud.network.vpc.NetworkACL;
 import com.cloud.utils.exception.CloudRuntimeException;
 import com.cloud.utils.script.Script;
 
-public class Upgrade410to420 implements DbUpgrade {
-    final static Logger s_logger = Logger.getLogger(Upgrade410to420.class);
+public class Upgrade412to420 implements DbUpgrade {
+    final static Logger s_logger = Logger.getLogger(Upgrade412to420.class);
 
 
     @Override
     public String[] getUpgradableVersionRange() {
-        return new String[] { "4.1.0", "4.2.0" };
+        return new String[] { "4.1.2", "4.2.0" };
     }
 
     @Override
@@ -62,9 +62,9 @@ public class Upgrade410to420 implements DbUpgrade {
 
     @Override
     public File[] getPrepareScripts() {
-        String script = Script.findScript("", "db/schema-410to420.sql");
+        String script = Script.findScript("", "db/schema-412to420.sql");
         if (script == null) {
-            throw new CloudRuntimeException("Unable to find db/schema-410to420.sql");
+            throw new CloudRuntimeException("Unable to find db/schema-412to420.sql");
         }
 
         return new File[] { new File(script) };
@@ -487,9 +487,9 @@ public class Upgrade410to420 implements DbUpgrade {
 
     @Override
     public File[] getCleanupScripts() {
-        String script = Script.findScript("", "db/schema-410to420-cleanup.sql");
+        String script = Script.findScript("", "db/schema-412to420-cleanup.sql");
         if (script == null) {
-            throw new CloudRuntimeException("Unable to find db/schema-410to420-cleanup.sql");
+            throw new CloudRuntimeException("Unable to find db/schema-412to420-cleanup.sql");
         }
 
         return new File[] { new File(script) };
