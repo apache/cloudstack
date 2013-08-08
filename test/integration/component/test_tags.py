c2840b93f5fd0d2d4e6dc4daa428b16a213fa8e7
Prasanna Santhanam <tsp@apache.org>
2013-08-08 12:07:09 +0530

    Fix the kwargs passed in as dict to listVirtualMachines
    listVirtualMachines queried using tags needs the tags (key,value) to be
    passed as a dictionary.
    
    Signed-off-by: Prasanna Santhanam <tsp@apache.org>

--- test/integration/component/test_tags.py
+++ test/integration/component/test_tags.py
@@ -1948,8 +1948,7 @@ class TestResourceTags(cloudstackTestCase):
         
         return
 
-    @unittest.skip("skipped - Need to enable this test after fixing TC issues")
-    @attr(tags=["advanced", "basic"])
+    @attr(tags=["advanced", "basic", "simulator"])
     def test_18_invalid_list_parameters(self):
         """ Test listAPI with invalid tags parameter
         """
@@ -1976,9 +1975,10 @@ class TestResourceTags(cloudstackTestCase):
         self.debug("Passing invalid key parameter to the listAPI for vms")
 
         vms = VirtualMachine.list(self.apiclient,
-                  listall=True,
-                  tags={'region111': 'India'}
-                 )
+            **{'tags[0].key': 'region111',
+             'tags[0].value': 'India',
+             'listall' : 'True'}
+        )
         self.assertEqual(
                          vms,
                          None,
