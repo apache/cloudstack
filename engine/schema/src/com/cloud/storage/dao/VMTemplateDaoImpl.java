48c8e64ae9c872d339b0fb8a2fc7fa453862fada
Harikrishna Patnala <harikrishna.patnala@citrix.com>
2013-08-10 15:36:06 +0530

    CLOUDSTACK-4123: 3.0.6 to ASF 4.2 Upgrade: On Adding VMWare Cluster to the Setup, 3.0.6 System VM Template for VMWare is downloaded instead of 4.2
    Updating the new system template URLs for the existing templates during upgrade to 4.2.
    If new 4.2 system template is registered before upgrade then marking the old templates as removed during upgrade.

--- engine/schema/src/com/cloud/storage/dao/VMTemplateDaoImpl.java
+++ engine/schema/src/com/cloud/storage/dao/VMTemplateDaoImpl.java
@@ -334,6 +334,7 @@ public class VMTemplateDaoImpl extends GenericDaoBase<VMTemplateVO, Long> implem
         tmpltTypeHyperSearch.done();
 
         readySystemTemplateSearch = createSearchBuilder();
+        readySystemTemplateSearch.and("removed", readySystemTemplateSearch.entity().getRemoved(), SearchCriteria.Op.NULL);
         readySystemTemplateSearch.and("templateType", readySystemTemplateSearch.entity().getTemplateType(), SearchCriteria.Op.EQ);
         SearchBuilder<TemplateDataStoreVO>  templateDownloadSearch = _templateDataStoreDao.createSearchBuilder();
         templateDownloadSearch.and("downloadState", templateDownloadSearch.entity().getDownloadState(), SearchCriteria.Op.EQ);
@@ -800,7 +801,6 @@ public class VMTemplateDaoImpl extends GenericDaoBase<VMTemplateVO, Long> implem
 
         if (tmplts.size() > 0) {
             if (hypervisorType == HypervisorType.Any) {
-                Collections.shuffle(tmplts);
                 return tmplts.get(0);
             }
             for (VMTemplateVO tmplt : tmplts) {
