37972a2020f034dfa901e6b6ded5bf073fcce93f
Alena Prokharchyk <alena.prokharchyk@citrix.com>
2013-08-12 09:36:10 -0700

    CLOUDSTACK-4035: set uuid for autoscale related tables as a part of the upgrade (was null on the previous versions of the code)
    Conflicts:
    	setup/db/db/schema-410to420.sql

--- setup/db/db/schema-410to420.sql
+++ setup/db/db/schema-410to420.sql
@@ -2286,3 +2286,14 @@ INSERT IGNORE INTO `cloud`.`configuration` VALUES ('Advanced', 'DEFAULT', 'manag
 INSERT IGNORE INTO `cloud`.`configuration` VALUES ('Snapshots', 'DEFAULT', 'SnapshotManager', 'kvm.snapshot.enabled', 'false', 'whether snapshot is enabled for KVM hosts');
 INSERT IGNORE INTO `cloud`.`configuration` VALUES ('Advanced', 'DEFAULT', 'management-server', 'eip.use.multiple.netscalers', 'false', 'Should be set to true, if there will be multiple NetScaler devices providing EIP service in a zone');
 INSERT IGNORE INTO `cloud`.`configuration` VALUES ('Snapshots', 'DEFAULT', 'SnapshotManager', 'snapshot.backup.rightafter', 'true', 'backup snapshot right after snapshot is taken');
+
+DELETE FROM `cloud`.`configuration` where name='vmware.guest.vswitch';
+DELETE FROM `cloud`.`configuration` where name='vmware.private.vswitch';
+DELETE FROM `cloud`.`configuration` where name='vmware.public.vswitch';
+
+
+UPDATE `cloud`.`autoscale_vmgroups` set uuid=id WHERE uuid is NULL;
+UPDATE `cloud`.`autoscale_vmprofiles` set uuid=id WHERE uuid is NULL;
+UPDATE `cloud`.`autoscale_policies` set uuid=id WHERE uuid is NULL;
+UPDATE `cloud`.`counter` set uuid=id WHERE uuid is NULL;
+UPDATE `cloud`.`conditions` set uuid=id WHERE uuid is NULL;
