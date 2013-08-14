374de76dae63db7fb79201f37db2254c80bd28c6
Edison Su <sudison@gmail.com>
2013-08-14 14:18:02 -0700

    CLOUDSTACK-4324: need to clean up snapshot_store_ref, if creating snapshot failed.

--- engine/storage/snapshot/src/org/apache/cloudstack/storage/snapshot/SnapshotServiceImpl.java
+++ engine/storage/snapshot/src/org/apache/cloudstack/storage/snapshot/SnapshotServiceImpl.java
@@ -214,7 +214,8 @@ public class SnapshotServiceImpl implements SnapshotService {
         try {
             result = future.get();
             if (result.isFailed()) {
-                s_logger.debug("Failed to create snapshot:" + result.getResult());
+                snapshot.processEvent(Snapshot.Event.OperationFailed);
+                snapshot.processEvent(Event.OperationFailed);
                 throw new CloudRuntimeException(result.getResult());
             }
             return result;
@@ -224,8 +225,10 @@ public class SnapshotServiceImpl implements SnapshotService {
         } catch (ExecutionException e) {
             s_logger.debug("Failed to create snapshot", e);
             throw new CloudRuntimeException("Failed to create snapshot", e);
+        } catch (NoTransitionException e) {
+            s_logger.debug("Failed to create snapshot", e);
+            throw new CloudRuntimeException("Failed to create snapshot", e);
         }
-
     }
 
     // if a snapshot has parent snapshot, the new snapshot should be stored in
