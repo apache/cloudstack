c6e2e465ed8d4524f79d00b8d283337b2d26e8a7
Bharat Kumar <bharat.kumar@citrix.com>
2013-08-16 15:09:56 +0530

    CLOUDSTACK-4068 scaling up user vm and system vm is failing with NPE if current service offering is deleted Nitin Mehta <nitin.mehta@citrix.com>

--- server/src/com/cloud/vm/UserVmManagerImpl.java
+++ server/src/com/cloud/vm/UserVmManagerImpl.java
@@ -1242,7 +1242,7 @@ public class UserVmManagerImpl extends ManagerBase implements UserVmManager, Use
 
         //Check if its a scale "up"
         ServiceOffering newServiceOffering = _configMgr.getServiceOffering(newServiceOfferingId);
-        ServiceOffering currentServiceOffering = _configMgr.getServiceOffering(vmInstance.getServiceOfferingId());
+        ServiceOffering currentServiceOffering = _offeringDao.findByIdIncludingRemoved(vmInstance.getServiceOfferingId());
         int newCpu = newServiceOffering.getCpu();
         int newMemory = newServiceOffering.getRamSize();
         int newSpeed = newServiceOffering.getSpeed();
