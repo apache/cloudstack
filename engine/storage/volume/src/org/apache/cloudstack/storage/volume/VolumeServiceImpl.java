apache/cloudstack

commit 5d9fa5d42e13a8c8ee10df14c466f66de678e954

throw exception if stopping VM fails, then UI can show the error message in stead of fails silently

server/src/com/cloud/vm/VirtualMachineManagerImpl.java
@@ -1262,8 +1262,10 @@ public class VirtualMachineManagerImpl extends ManagerBase implements VirtualMac
 
         } catch (AgentUnavailableException e) {
             s_logger.warn("Unable to stop vm, agent unavailable: " + e.toString());
+            throw e;
         } catch (OperationTimedoutException e) {
             s_logger.warn("Unable to stop vm, operation timed out: " + e.toString());
+            throw e;
         } finally {
             if (!stopped) {
                 if (!forced) {

commit 1b90a4554f19ef51940943ab8b2bde28fc933777

CLOUDSTACK-3535: fix regression introduced in 5d9fa5d42e13a8c8ee10df14c466f66de678e954 (cherry picked from commit 6b4aed2acacb1663cfd3955cc64bc84adef402bc)
Signed-off-by: animesh <animesh@apache.org>

engine/storage/volume/src/org/apache/cloudstack/storage/volume/VolumeServiceImpl.java
@@ -522,6 +522,7 @@ public class VolumeServiceImpl implements VolumeService {
         if (result.isSuccess()) {
             vo.processEvent(Event.OperationSuccessed, result.getAnswer());
         } else {
+
             vo.processEvent(Event.OperationFailed);
             volResult.setResult(result.getResult());
         }

plugins/hypervisors/kvm/src/com/cloud/ha/KVMInvestigator.java
@@ -59,15 +59,19 @@ public class KVMInvestigator extends AdapterBase implements Investigator {
             return null;
         }
         CheckOnHostCommand cmd = new CheckOnHostCommand(agent);
-        List<HostVO> neighbors = _resourceMgr.listAllHostsInCluster(agent.getClusterId());
+        List<HostVO> neighbors = _resourceMgr.listHostsInClusterByStatus(agent.getClusterId(), Status.Up);
         for (HostVO neighbor : neighbors) {
             if (neighbor.getId() == agent.getId() || neighbor.getHypervisorType() != Hypervisor.HypervisorType.KVM) {
                 continue;
             }
-            Answer answer = _agentMgr.easySend(neighbor.getId(), cmd);
-
-            return answer.getResult() ? Status.Down : Status.Up;
-
+            try {
+                Answer answer = _agentMgr.easySend(neighbor.getId(), cmd);
+                if (answer != null) {
+                    return answer.getResult() ? Status.Down : Status.Up;
+                }
+            } catch (Exception e) {
+                s_logger.debug("Failed to send command to host: " + neighbor.getId());
+            }
         }
 
         return null;

server/src/com/cloud/vm/VirtualMachineManagerImpl.java
@@ -1254,10 +1254,14 @@ public class VirtualMachineManagerImpl extends ManagerBase implements VirtualMac
 
         } catch (AgentUnavailableException e) {
             s_logger.warn("Unable to stop vm, agent unavailable: " + e.toString());
-            throw e;
+            if (!forced) {
+                throw e;
+            }
         } catch (OperationTimedoutException e) {
             s_logger.warn("Unable to stop vm, operation timed out: " + e.toString());
-            throw e;
+            if (!forced) {
+                throw e;
+            }
         } finally {
             if (!stopped) {
                 if (!forced) {