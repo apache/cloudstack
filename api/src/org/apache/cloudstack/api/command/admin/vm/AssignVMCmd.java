f77b957e8a0664060530a163cfce5d7e7600b274
Jessica <jessica.tomechak@citrix.com>
2013-08-28 10:07:10 -0700

    CLOUDSTACK-2405. DOC. Changing ownership of a VM from one account to another in any domain. (cherry picked from commit 0d8ce418c54047bcbbd300c1445b4ddf338f81c2)
    Signed-off-by: animesh <animesh@apache.org>

--- api/src/org/apache/cloudstack/api/command/admin/vm/AssignVMCmd.java
+++ api/src/org/apache/cloudstack/api/command/admin/vm/AssignVMCmd.java
@@ -35,7 +35,7 @@ import org.apache.log4j.Logger;
 import com.cloud.user.Account;
 import com.cloud.uservm.UserVm;
 
-@APICommand(name = "assignVirtualMachine", description="Assign a VM from one account to another under the same domain. This API is available for Basic zones with security groups and Advance zones with guest networks. The VM is restricted to move between accounts under same domain.", responseObject=UserVmResponse.class, since="3.0.0")
+@APICommand(name = "assignVirtualMachine", description="Change ownership of a VM from one account to another. This API is available for Basic zones with security groups and Advanced zones with guest networks. A root administrator can reassign a VM from any account to any other account in any domain. A domain administrator can reassign a VM to any account in the same domain.", responseObject=UserVmResponse.class, since="3.0.0")
 public class AssignVMCmd extends BaseCmd  {
     public static final Logger s_logger = Logger.getLogger(AssignVMCmd.class.getName());
 
