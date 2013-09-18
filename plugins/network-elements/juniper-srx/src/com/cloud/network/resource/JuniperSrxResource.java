apache/cloudstack

commit 2614b00c513734ce6b1c29e572fbd7a37d4059fc

sdn hosted vpc gateways (using lswitch)

plugins/network-elements/juniper-srx/src/com/cloud/network/resource/JuniperSrxResource.java
@@ -68,6 +68,7 @@ import com.cloud.host.Host;
 import com.cloud.network.Networks.BroadcastDomainType;
 import com.cloud.network.rules.FirewallRule;
 import com.cloud.network.rules.FirewallRule.Purpose;
+import com.cloud.network.Networks.BroadcastDomainType;
 import com.cloud.resource.ServerResource;
 import com.cloud.utils.NumbersUtil;
 import com.cloud.utils.exception.ExecutionException;
@@ -697,11 +698,11 @@ public class JuniperSrxResource implements ServerResource {
             String guestVlanSubnet = NetUtils.getCidrSubNet(guestVlanGateway, cidrSize);    
             
             Long publicVlanTag = null;
-            if (ip.getVlanId() != null && !ip.getVlanId().equals("untagged")) {
+            if (ip.getBroadcastUri() != null && !ip.getBroadcastUri().equals("untagged")) {
             	try {
                     publicVlanTag = Long.parseLong(BroadcastDomainType.getValue(ip.getVlanId()));
             	} catch (Exception e) {
-            		throw new ExecutionException("Could not parse public VLAN tag: " + ip.getVlanId());
+			throw new ExecutionException("Could not parse public VLAN tag: " + ip.getBroadcastUri());
             	}
             } 
 

api/src/com/cloud/agent/api/to/IpAddressTO.java
@@ -26,7 +26,7 @@ public class IpAddressTO {
     private boolean add;
     private boolean oneToOneNat;
     private boolean firstIP;
-    private String vlanId;
+    private String broadcastUri;
     private String vlanGateway;
     private String vlanNetmask;
     private String vifMacAddress;
@@ -34,14 +34,14 @@ public class IpAddressTO {
     private TrafficType trafficType;
     private String networkName;
 
-    public IpAddressTO(long accountId, String ipAddress, boolean add, boolean firstIP, boolean sourceNat, String vlanId,
+    public IpAddressTO(long accountId, String ipAddress, boolean add, boolean firstIP, boolean sourceNat, String broadcastUri,
             String vlanGateway, String vlanNetmask, String vifMacAddress, Integer networkRate, boolean isOneToOneNat) {
         this.accountId = accountId;
         this.publicIp = ipAddress;
         this.add = add;
         this.firstIP = firstIP;
         this.sourceNat = sourceNat;
-        this.vlanId = vlanId;
+        this.broadcastUri = broadcastUri;
         this.vlanGateway = vlanGateway;
         this.vlanNetmask = vlanNetmask;
         this.vifMacAddress = vifMacAddress;
@@ -97,8 +97,8 @@ public class IpAddressTO {
         return sourceNat;
     }
 
-    public String getVlanId() {
-        return vlanId;
+    public String getBroadcastUri() {
+        return broadcastUri;
     }
 
     public String getVlanGateway() {

api/src/com/cloud/agent/api/to/NetworkTO.java
@@ -163,6 +163,10 @@ public class NetworkTO {
     }
 
     public void setBroadcastUri(URI broadcastUri) {
+        // only do this if the scheme needs aligning with the broadcastUri
+        if(broadcastUri != null && getBroadcastType() == null) {
+            setBroadcastType(BroadcastDomainType.getSchemeValue(broadcastUri));
+        }
         this.broadcastUri = broadcastUri;
     }
 

api/src/com/cloud/network/NetworkService.java
@@ -19,11 +19,12 @@ package com.cloud.network;
 import java.util.List;
 import java.util.Map;
 
-import com.cloud.offering.NetworkOffering;
 import org.apache.cloudstack.api.command.admin.network.DedicateGuestVlanRangeCmd;
 import org.apache.cloudstack.api.command.admin.network.ListDedicatedGuestVlanRangesCmd;
 import org.apache.cloudstack.api.command.admin.usage.ListTrafficTypeImplementorsCmd;
-import org.apache.cloudstack.api.command.user.network.*;
+import org.apache.cloudstack.api.command.user.network.CreateNetworkCmd;
+import org.apache.cloudstack.api.command.user.network.ListNetworksCmd;
+import org.apache.cloudstack.api.command.user.network.RestartNetworkCmd;
 import org.apache.cloudstack.api.command.user.vm.ListNicsCmd;
 
 import com.cloud.exception.ConcurrentOperationException;
@@ -34,6 +35,7 @@ import com.cloud.exception.ResourceUnavailableException;
 import com.cloud.network.GuestVlan;
 import com.cloud.network.Network.Service;
 import com.cloud.network.Networks.TrafficType;
+import com.cloud.offering.NetworkOffering;
 import com.cloud.user.Account;
 import com.cloud.user.User;
 import com.cloud.utils.Pair;
@@ -156,7 +158,7 @@ public interface NetworkService {
      * @param networkName
      * @param displayText
      * @param physicalNetworkId
-     * @param vlan
+     * @param broadcastUri TODO set the guru name based on the broadcastUri?
      * @param startIp
      * @param endIP TODO
      * @param gateway
@@ -169,8 +171,8 @@ public interface NetworkService {
      * @throws ConcurrentOperationException
      * @throws ResourceAllocationException
      */
-    Network createPrivateNetwork(String networkName, String displayText, long physicalNetworkId, String vlan,
-                                     String startIp, String endIP, String gateway, String netmask, long networkOwnerId, Long vpcId, Boolean sourceNat)
+    Network createPrivateNetwork(String networkName, String displayText, long physicalNetworkId, String broadcastUri,
+            String startIp, String endIP, String gateway, String netmask, long networkOwnerId, Long vpcId, Boolean sourceNat, Long networkOfferingId)
                     throws ResourceAllocationException, ConcurrentOperationException, InsufficientCapacityException;
 
     /* Requests an IP address for the guest nic */

api/src/com/cloud/network/Networks.java
@@ -91,7 +91,7 @@ public class Networks {
             @Override
             public <T> URI toUri(T value) {
                 try {
-                    return new URI("lswitch",value.toString(),null,null);
+                    return new URI("lswitch", value.toString(), null, null);
                 } catch (URISyntaxException e) {
                     throw new CloudRuntimeException(
                             "Unable to convert to broadcast URI: " + value);
@@ -181,6 +181,7 @@ public class Networks {
          * @return the value of this
          */
         public static BroadcastDomainType toEnumValue(String scheme) {
+            // scheme might be null and some of the enumvalue.scheme are as well, so
             if (scheme == null) {
                 return UnDecided;
             }
@@ -242,7 +243,7 @@ public class Networks {
                 try {
                     URI uri = new URI(candidate);
                     BroadcastDomainType tiep = getSchemeValue(uri);
-                    if (tiep.scheme.equals(uri.getScheme())) {
+                    if (tiep.scheme != null && tiep.scheme.equals(uri.getScheme())) {
                         return uri;
                     } else {
                         throw new CloudRuntimeException("string '" + candidate + "' has an unknown BroadcastDomainType.");

api/src/com/cloud/network/vpc/PrivateIp.java
@@ -26,7 +26,7 @@ public interface PrivateIp {
     /**
      * @return
      */
-    String getVlanTag();
+    String getBroadcastUri();
 
     /**
      * @return

api/src/com/cloud/network/vpc/StaticRouteProfile.java
@@ -41,7 +41,7 @@ public class StaticRouteProfile implements StaticRoute {
         this.gatewayId = staticRoute.getVpcGatewayId();
         this.state = staticRoute.getState();
         this.vpcId = staticRoute.getVpcId();
-        this.vlanTag = gateway.getVlanTag();
+        this.vlanTag = gateway.getBroadcastUri();
         this.gateway = gateway.getGateway();
         this.netmask = gateway.getNetmask();
         this.ipAddress = gateway.getIp4Address();

api/src/com/cloud/network/vpc/VpcGateway.java
@@ -71,7 +71,7 @@ public interface VpcGateway extends Identity, ControlledEntity, InternalIdentity
     /**
      * @return
      */
-    String getVlanTag();
+    String getBroadcastUri();
 
     /**
      * @return

commit 3ab8d8d8f20c453fdc684f177a612922eae5f415

Fixed non-oss build broken in Juniper SRX with commit 2614b00c513734ce6b1c29e572fbd7a37d4059fc

plugins/network-elements/juniper-srx/src/com/cloud/network/resource/JuniperSrxResource.java
@@ -700,7 +700,7 @@ public class JuniperSrxResource implements ServerResource {
             Long publicVlanTag = null;
             if (ip.getBroadcastUri() != null && !ip.getBroadcastUri().equals("untagged")) {
             	try {
-                    publicVlanTag = Long.parseLong(BroadcastDomainType.getValue(ip.getVlanId()));
+                    publicVlanTag = Long.parseLong(ip.getBroadcastUri());
             	} catch (Exception e) {
 			throw new ExecutionException("Could not parse public VLAN tag: " + ip.getBroadcastUri());
             	}