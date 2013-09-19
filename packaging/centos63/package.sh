d0b896ed36a54f1191994b1a400513684177b07d
Hugo Trippaers <htrippaers@schubergphilis.com>
2013-09-19 15:11:40 +0800

    Simplify package.sh

--- packaging/centos63/package.sh
+++ packaging/centos63/package.sh
@@ -18,7 +18,7 @@
 
 function usage() {
  echo ""
- echo "usage: ./package.sh [-p|e-pack] [-h|--help] [ARGS]"
+ echo "usage: ./package.sh [-p|-pack] [-h|--help] [ARGS]"
  echo ""
  echo "The commonly used Arguments are:"
  echo "oss|OSS             To package OSS specific"
@@ -30,45 +30,14 @@ function usage() {
  exit 1
 }
 
-function defaultPackaging() {
-CWD=`pwd`
-RPMDIR=$CWD/../../dist/rpmbuild
-PACK_PROJECT=cloudstack
-
-VERSION=`(cd ../../; mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version) | grep '^[0-9]\.'`
-if echo $VERSION | grep SNAPSHOT ; then
-  REALVER=`echo $VERSION | cut -d '-' -f 1`
-  DEFVER="-D_ver $REALVER"
-  DEFPRE="-D_prerelease 1"
-  DEFREL="-D_rel SNAPSHOT"
-else
-  REALVER=$VERSION
-  DEFVER="-D_ver $REALVER"
-  DEFREL="-D_rel 1"
-fi
-
-mkdir -p $RPMDIR/SPECS
-mkdir -p $RPMDIR/BUILD
-mkdir -p $RPMDIR/SRPMS
-mkdir -p $RPMDIR/RPMS
-mkdir -p $RPMDIR/SOURCES/$PACK_PROJECT-$VERSION
-
-(cd ../../; tar -c --exclude .git --exclude dist  .  | tar -C $RPMDIR/SOURCES/$PACK_PROJECT-$VERSION -x )
-(cd $RPMDIR/SOURCES/; tar -czf $PACK_PROJECT-$VERSION.tgz $PACK_PROJECT-$VERSION)
-
-cp cloud.spec $RPMDIR/SPECS
-
-(cd $RPMDIR; rpmbuild --define "_topdir $RPMDIR" "${DEFVER}" "${DEFREL}" ${DEFPRE+"${DEFPRE}"} -ba SPECS/cloud.spec)
-
-exit
-}
-
 function packaging() {
 	 
 CWD=`pwd`
 RPMDIR=$CWD/../../dist/rpmbuild
 PACK_PROJECT=cloudstack
-DEFOSSNOSS="-D_ossnoss $packageval"
+if [ -n "$1" ] ; then
+  DEFOSSNOSS="-D_ossnoss $packageval"
+fi
 
 
 VERSION=`(cd ../../; mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version) | grep '^[0-9]\.'`
@@ -95,7 +64,7 @@ mkdir -p $RPMDIR/SOURCES/$PACK_PROJECT-$VERSION
 
 cp cloud.spec $RPMDIR/SPECS
 
-(cd $RPMDIR; rpmbuild --define "_topdir $RPMDIR" "${DEFVER}" "${DEFREL}" ${DEFPRE+\"${DEFPRE}\"} "${DEFOSSNOSS}" -bb SPECS/cloud.spec)
+(cd $RPMDIR; rpmbuild --define "_topdir $RPMDIR" "${DEFVER}" "${DEFREL}" ${DEFPRE+"${DEFPRE}"} ${DEFOSSNOSS+"$DEFOSSNOSS"} -bb SPECS/cloud.spec)
 
 exit
 }
@@ -103,7 +72,7 @@ exit
 
 if [ $# -lt 1 ] ; then
 
-	defaultPackaging
+	packaging
 
 elif [ $# -gt 0 ] ; then
 
@@ -114,7 +83,6 @@ elif [ $# -gt 0 ] ; then
 	eval set -- "$ARGS"
 
 	while [ $# -gt 0 ] ; do
-        echo $1
 	case "$1" in
 	-h | --help)
 		usage
@@ -124,9 +92,9 @@ elif [ $# -gt 0 ] ; then
 		echo "Doing CloudStack Packaging ....."
 		packageval=$2
 		if [ "$packageval" == "oss" -o "$packageval" == "OSS" ] ; then
-			defaultPackaging
-		elif [ "$packageval" == "noredist" -o "$packageval" == "NOREDIST" ] ; then
 			packaging
+		elif [ "$packageval" == "noredist" -o "$packageval" == "NOREDIST" ] ; then
+			packaging noredist
 		else
 			echo "Error: Incorrect value provided in package.sh script, Please see help ./package.sh --help|-h for more details."
 			exit 1
