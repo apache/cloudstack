e61f1bb1586caa7434cefc79b11fcbe8ecb300be
Wei Zhou <w.zhou@leaseweb.com>
2013-09-24 11:59:05 +0200

    fix issue: listHosts with hahost throws exception (cherry picked from commit a206f423a3851a4d090f58b05e2936dd68c528d0)

--- server/src/com/cloud/api/query/QueryManagerImpl.java
+++ server/src/com/cloud/api/query/QueryManagerImpl.java
@@ -1573,7 +1573,7 @@ public class QueryManagerImpl extends ManagerBase implements QueryService {
         }
 
         if (haHosts != null && haTag != null && !haTag.isEmpty()) {
-            sc.setJoinParameters("hostTagSearch", "tag", haTag);
+            sc.setParameters("tag", haTag);
         }
 
         // search host details by ids
