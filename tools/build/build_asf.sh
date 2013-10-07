23ddf29532d269ce340e0e68015ea3afd2121eaa
Darren Shepherd <darren.s.shepherd@gmail.com>
2013-10-07 09:19:45 -0700

    Fix vmware code inclusion into systemvm.iso and other random nonoss->noredist

--- tools/build/build_asf.sh
+++ tools/build/build_asf.sh
@@ -90,7 +90,7 @@ export currentversion=`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:eval
 echo "found $currentversion"
 
 echo 'setting version numbers'
-mvn versions:set -DnewVersion=$version -P vmware -P developer -P systemvm -P simulator -P baremetal -P ucs -Dnonoss
+mvn versions:set -DnewVersion=$version -P vmware -P developer -P systemvm -P simulator -P baremetal -P ucs -Dnoredist
 mv deps/XenServerJava/pom.xml.versionsBackup deps/XenServerJava/pom.xml
 perl -pi -e "s/-SNAPSHOT//" deps/XenServerJava/pom.xml
 perl -pi -e "s/-SNAPSHOT//" tools/apidoc/pom.xml
