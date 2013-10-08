66fe3abe3b73766df03771cb68640424ee2099c8
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2013-10-08 20:06:44 +0200

    ReflectUtil: findMethod removed
    findMethod was not used, it is removed
    
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- utils/src/com/cloud/utils/ReflectUtil.java
+++ utils/src/com/cloud/utils/ReflectUtil.java
@@ -26,7 +26,6 @@ import java.beans.PropertyDescriptor;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
@@ -57,19 +56,6 @@ public class ReflectUtil {
             }
             return null;
         }
-    }
-    
-    public static Method findMethod(Class<?> clazz, String methodName) {
-        do {
-            Method[] methods = clazz.getDeclaredMethods();
-            for (Method method : methods) {
-                if (methodName.equals(method.getName())) {
-                    return method;
-                }
-            }
-            clazz = clazz.getSuperclass();
-        } while (clazz != null);
-        return null;
     }
 
     // Gets all classes with some annotation from a package
