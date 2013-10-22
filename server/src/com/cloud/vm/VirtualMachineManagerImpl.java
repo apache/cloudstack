c31f87c0781bfe2a4662992fe6d2cac3c0b7207b
Anthony Xu <anthony.xu@citrix.com>
2013-10-22 11:20:27 -0700

    CLOUDSTACK-4649:         vm sync tracks the pv driver version for xenserver
     Anthony

--- server/src/com/cloud/vm/VirtualMachineManagerImpl.java
+++ server/src/com/cloud/vm/VirtualMachineManagerImpl.java
@@ -2466,6 +2466,14 @@ public class VirtualMachineManagerImpl extends ManagerBase implements VirtualMac
             _alertMgr.sendAlert(alertType, vm.getDataCenterId(), vm.getPodIdToDeployIn(), "VM (name: " + vm.getInstanceName() + ", id: " + vm.getId() + ") stopped on host " + hostDesc
                     + " due to storage failure", "Virtual Machine " + vm.getInstanceName() + " (id: " + vm.getId() + ") running on host [" + vm.getHostId() + "] stopped due to storage failure.");
         }
+        // track hypervsion tools version
+        if( info.hvtoolsversion != null && !info.hvtoolsversion.isEmpty() ) {
+
+            UserVmVO userVm = _userVmDao.findById(vm.getId());
+            _userVmDao.loadDetails(userVm);
+            userVm.setDetail("hypervisortoolsversion",  info.hvtoolsversion);
+            _userVmDao.saveDetails(userVm);
+        }
 
         if (trackExternalChange) {
             if (serverState == State.Starting) {
