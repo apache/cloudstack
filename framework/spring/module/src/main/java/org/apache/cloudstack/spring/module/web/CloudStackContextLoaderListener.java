24c0513eea4f3b9a4de5f004bfea8527bd583c0e
Darren Shepherd <darren.s.shepherd@gmail.com>
2013-11-05 08:20:27 -0700

    Add servlet filter class that is compatible with the Spring Modularization

--- framework/spring/module/src/main/java/org/apache/cloudstack/spring/module/web/CloudStackContextLoaderListener.java
+++ framework/spring/module/src/main/java/org/apache/cloudstack/spring/module/web/CloudStackContextLoaderListener.java
@@ -49,6 +49,7 @@ public class CloudStackContextLoaderListener extends ContextLoaderListener {
     public void contextInitialized(ServletContextEvent event) {
         try {
             cloudStackContext = new CloudStackSpringContext();
+            event.getServletContext().setAttribute(CloudStackSpringContext.CLOUDSTACK_CONTEXT_SERVLET_KEY, cloudStackContext);
         } catch (IOException e) {
             log.error("Failed to start CloudStack", e);
             throw new RuntimeException("Failed to initialize CloudStack Spring modules", e);
