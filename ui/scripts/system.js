fc7489a2813315322766a6dda17cec2e4a7d8130
Jessica Wang <jessicawang@apache.org>
2013-11-06 15:51:53 -0800

    CLOUDSTACK-5037: UI > Infrastructure > clusters > Settings tab > when "cpu.overprovisioning.factor" or "mem.overprovisioning.factor" is changed, pop up a warning message > change text.

--- ui/scripts/system.js
+++ ui/scripts/system.js
@@ -12033,7 +12033,7 @@
                                                    
                                                     if (args.data.jsonObj.name == 'cpu.overprovisioning.factor' || args.data.jsonObj.name == 'mem.overprovisioning.factor') {
                                                     	cloudStack.dialog.notice({
-                                                    		message: 'Please note - you are changing the over provisioning factor for a cluster with vms running. Please refer to the admin guide to understand the capacity calculation.'
+                                                    		message: 'Please note - if you are changing the over provisioning factor for a cluster with vms running, please refer to the admin guide to understand the capacity calculation.'
                                                     	});
                                                     }
                                                     
