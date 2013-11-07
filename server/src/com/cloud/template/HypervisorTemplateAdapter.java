53b0bf6733d28cb104a231f28f58c7f1dff11742
Harikrishna Patnala <harikrishna.patnala@citrix.com>
2013-11-07 17:07:56 +0530

    Template/ISO download fails cause the object to disappear from UI in 4.2
    Signed-off-by: Koushik Das <koushik@apache.org>

--- server/src/com/cloud/template/HypervisorTemplateAdapter.java
+++ server/src/com/cloud/template/HypervisorTemplateAdapter.java
@@ -233,11 +233,7 @@ public class HypervisorTemplateAdapter extends TemplateAdapterBase {
             CreateTemplateContext<TemplateApiResult> context) {
         TemplateApiResult result = callback.getResult();
         TemplateInfo template = context.template;
-        if (result.isFailed()) {
-            // failed in creating template, we need to remove those already
-            // populated template entry
-            _tmpltDao.remove(template.getId());
-        } else {
+        if (result.isSuccess()) {
             VMTemplateVO tmplt = _tmpltDao.findById(template.getId());
             long accountId = tmplt.getAccountId();
             if (template.getSize() != null) {
