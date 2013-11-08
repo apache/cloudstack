79ef40c0a9f7ad60bd4830977a6e556e0125b80e
John Kinsella <jlk@stratosec.co>
2013-11-07 19:54:30 -0800

    Summary: Implement stored-password encryption for Stratosphere plugin
    BUG-ID: CLOUDSTACK-4929
    Bugfix-for:
    Reviewed-by:
    Reported-by:
    Signed-off-by: John Kinsella <jlk@stratosec.co> 1383882870 -0800
    Signed-off-by: Abhinandan Prateek <aprateek@apache.org>

--- plugins/network-elements/stratosphere-ssp/src/org/apache/cloudstack/network/dao/SspCredentialVO.java
+++ plugins/network-elements/stratosphere-ssp/src/org/apache/cloudstack/network/dao/SspCredentialVO.java
@@ -23,6 +23,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import com.cloud.utils.db.Encrypt;
+
 @Entity
 @Table(name="external_stratosphere_ssp_credentials")
 public class SspCredentialVO {
@@ -38,6 +40,7 @@ public class SspCredentialVO {
     @Column(name="username")
     private String username;
 
+    @Encrypt
     @Column(name="password")
     private String password;
 
