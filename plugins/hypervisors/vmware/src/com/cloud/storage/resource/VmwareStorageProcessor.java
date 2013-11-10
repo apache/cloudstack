42941b3376dae903bf512f24dc3d77c06de3feb4
Likitha Shetty <likitha.shetty@citrix.com>
2013-11-10 22:41:06 +0530

    CLOUDSTACK-5122. System VMs are getting recreated with old template after upgrading to 4.2. In VMware during VM start the existing disk information is used to configure the VMs. So even if a new disk is created using the new template VM continues to use the old disk. Once the old root disk is marked for destroy force expunge it and sync the new disk into the VM folder before VM start

--- plugins/hypervisors/vmware/src/com/cloud/storage/resource/VmwareStorageProcessor.java
+++ plugins/hypervisors/vmware/src/com/cloud/storage/resource/VmwareStorageProcessor.java
@@ -397,8 +397,6 @@ public class VmwareStorageProcessor implements StorageProcessor {
             // restoreVM - move the new ROOT disk into corresponding VM folder
             String vmInternalCSName = volume.getVmName();
             if (dsMo.folderExists(String.format("[%s]", dsMo.getName()), vmInternalCSName)) {
-                String oldRootDisk = VmwareStorageLayoutHelper.getVmwareDatastorePathFromVmdkFileName(dsMo, vmInternalCSName, vmdkName);
-                if (oldRootDisk != null)
                     VmwareStorageLayoutHelper.syncVolumeToVmDefaultFolder(dcMo, vmInternalCSName, dsMo, vmdkName);
             }
 
