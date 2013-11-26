3deb7dd6f5dd6c8df42a8ada425300018b656e97
Wei Zhou <w.zhou@leaseweb.com>
2013-11-26 14:05:18 +0100

    CLOUDSTACK-5272: return unlimit if max.account.* or max.project.* is set to negative (cherry picked from commit 888ddd724aabbd22d675fe2bba873971888e1a8a)

--- server/src/com/cloud/resourcelimit/ResourceLimitManagerImpl.java
+++ server/src/com/cloud/resourcelimit/ResourceLimitManagerImpl.java
@@ -284,6 +284,9 @@ public class ResourceLimitManagerImpl extends ManagerBase implements ResourceLim
                 value = accountResourceLimitMap.get(type);
             }
             if (value != null) {
+                if (value < 0) { // return unlimit if value is set to negative
+                    return max;
+                }
                 // convert the value from GiB to bytes in case of primary or secondary storage.
                 if (type == ResourceType.primary_storage || type == ResourceType.secondary_storage) {
                     value = value * ResourceType.bytesToGiB;
@@ -318,6 +321,9 @@ public class ResourceLimitManagerImpl extends ManagerBase implements ResourceLim
                 value = accountResourceLimitMap.get(type);
             }
             if (value != null) {
+                if (value < 0) { // return unlimit if value is set to negative
+                    return max;
+                }
                 if (type == ResourceType.primary_storage || type == ResourceType.secondary_storage) {
                     value = value * ResourceType.bytesToGiB;
                 }
