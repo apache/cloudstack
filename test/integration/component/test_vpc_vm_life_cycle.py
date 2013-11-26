58fd6f0603673efb51b6027f06b9ee39e727be09
Parth Jagirdar <Parth.Jagirdar@citrix.com>
2013-11-26 13:23:40 +0530

    CLOUDSTACK-5223: Fix hosttags.
    Signed-off-by: Girish Shilamkar <girish@clogeny.com>

--- test/integration/component/test_vpc_vm_life_cycle.py
+++ test/integration/component/test_vpc_vm_life_cycle.py
@@ -57,7 +57,7 @@ class Services:
                 "cpunumber": 1,
                 "cpuspeed": 100,
                 "memory": 128,
-                "hosttags": "host1"
+                "tags": "host1"
             },
             "service_offering_2": {
                 "name": "Tiny Instance- tagged host 2",
@@ -65,7 +65,7 @@ class Services:
                 "cpunumber": 1,
                 "cpuspeed": 100,
                 "memory": 128,
-                "hosttags": "host2"
+                "tags": "host2"
             },
             "network_offering": {
                 "name": 'VPC Network offering',
