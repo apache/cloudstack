abd4a826e9e2d481e8b18be134429c7b37778708
Wei Zhou <w.zhou@leaseweb.com>
2013-11-29 10:52:51 +0100

    Revert "CLOUDSTACK-5033: set origin ip address in logs if management servers is behind a reverse proxy or load balancer"
    This reverts commit 5cc47f77c5d7c1c8c40c5e7cb2f7083fd2ce56cd.

--- server/src/com/cloud/api/ApiServlet.java
+++ server/src/com/cloud/api/ApiServlet.java
@@ -121,13 +121,7 @@ public class ApiServlet extends HttpServlet {
 
     private void processRequestInContext(HttpServletRequest req, HttpServletResponse resp) {
         StringBuffer auditTrailSb = new StringBuffer();
-        String ipAddress = req.getHeader("X-FORWARDED-FOR");
-        if (ipAddress == null) {
-            ipAddress = req.getRemoteAddr();
-        } else {
-            ipAddress = ipAddress.split(",")[0];
-        }
-        auditTrailSb.append(" " + ipAddress);
+        auditTrailSb.append(" " + req.getRemoteAddr());
         auditTrailSb.append(" -- " + req.getMethod() + " ");
         // get the response format since we'll need it in a couple of places
         String responseType = BaseCmd.RESPONSE_TYPE_XML;
