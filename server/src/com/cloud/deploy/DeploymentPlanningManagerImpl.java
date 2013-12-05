c06e69db1973fcabe404b3d94501bcdcea2f21b5
Nitin Mehta <nitin.mehta@citrix.com>
2013-12-05 15:18:30 -0800

    CLOUDSTACK-5391: check for host cpu capability while stop starting a vm on the same host. Also changed the FirstFitAllocator to use the same method.

--- server/src/com/cloud/deploy/DeploymentPlanningManagerImpl.java
+++ server/src/com/cloud/deploy/DeploymentPlanningManagerImpl.java
@@ -350,7 +350,8 @@ public class DeploymentPlanningManagerImpl extends ManagerBase implements Deploy
                     Float cpuOvercommitRatio = Float.parseFloat(cluster_detail_cpu.getValue());
                     Float memoryOvercommitRatio = Float.parseFloat(cluster_detail_ram.getValue());
                     if (_capacityMgr.checkIfHostHasCapacity(host.getId(), cpu_requested, ram_requested, true,
-                            cpuOvercommitRatio, memoryOvercommitRatio, true)) {
+                            cpuOvercommitRatio, memoryOvercommitRatio, true)
+                            && _capacityMgr.checkIfHostHasCpuCapability(host.getId(), offering.getCpu(), offering.getSpeed())) {
                         s_logger.debug("The last host of this VM is UP and has enough capacity");
                         s_logger.debug("Now checking for suitable pools under zone: " + host.getDataCenterId()
                                 + ", pod: " + host.getPodId() + ", cluster: " + host.getClusterId());
