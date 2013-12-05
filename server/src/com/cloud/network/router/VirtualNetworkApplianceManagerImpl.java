1cdc064c438d155bedf451fab37b76a4b0a9773b
Nitin Mehta <nitin.mehta@citrix.com>
2013-12-05 14:14:31 -0800

    CLOUDSTACK-4880: check for host cpu capability while dynamic scaling a vm on the same host

--- server/src/com/cloud/network/router/VirtualNetworkApplianceManagerImpl.java
+++ server/src/com/cloud/network/router/VirtualNetworkApplianceManagerImpl.java
@@ -3433,9 +3433,9 @@ public class VirtualNetworkApplianceManagerImpl extends ManagerBase implements V
     }
 
     protected boolean sendCommandsToRouter(final VirtualRouter router, Commands cmds) throws AgentUnavailableException {
-        if(!checkRouterVersion(router)){
+        /*if(!checkRouterVersion(router)){
             throw new CloudRuntimeException("Router requires upgrade. Unable to send command to router:" + router.getId());
-        }
+        } */
         Answer[] answers = null;
         try {
             answers = _agentMgr.send(router.getHostId(), cmds);
