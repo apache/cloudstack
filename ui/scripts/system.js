d5fcc6ef5a75544e36cddc8f730fd61197fb324b
Jessica Wang <jessicawang@apache.org>
2013-12-12 16:05:12 -0800

    CLOUDSTACK-5345: UI > Infrastructure > virtual routers > no grouping > hide Upgrade Router to Use Newer Template action when a router has latest version (i.e. does not require upgrade).

--- ui/scripts/system.js
+++ ui/scripts/system.js
@@ -18632,7 +18632,9 @@
         var jsonObj = args.context.item;
         var allowedActions = [];
 
-        allowedActions.push('upgradeRouterToUseNewerTemplate');
+        if(jsonObj.requiresupgrade == true) {
+            allowedActions.push('upgradeRouterToUseNewerTemplate');
+        }
         
         if (jsonObj.state == 'Running') {
             allowedActions.push("stop");
