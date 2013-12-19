03811c0c46a3c74f4271b08faa8ce40a4e2598f7
Gaurav Aradhye <gaurav.aradhye@clogeny.com>
2013-12-19 10:21:33 +0530

    CLOUDSTACK-4780: Changes related to checking snapshot on NFS server

--- test/integration/component/test_add_remove_network.py
+++ test/integration/component/test_add_remove_network.py
@@ -47,12 +47,12 @@ from marvin.integration.lib.common import (get_domain,
                                         list_events,
                                         list_zones,
                                         get_free_vlan,
-                                        get_hypervisor_type,
                                         update_resource_limit
                                         )
 
 from marvin.integration.lib.utils import (validateList,
-					                      random_gen)
+					                      random_gen,
+                                          get_hypervisor_type)
 
 from marvin.cloudstackAPI import (addNicToVirtualMachine,
                                   removeNicFromVirtualMachine,
