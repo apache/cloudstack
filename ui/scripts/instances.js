766dab64e87d469f8451a8c3ed2ae56dfa02b334
Brian Federle <brian.federle@citrix.com>
2014-01-08 10:37:17 -0800

    Instance list view: Truncate long names to prevent misalignment

--- ui/scripts/instances.js
+++ ui/scripts/instances.js
@@ -139,16 +139,19 @@
             },
             fields: {
                 name: {
-                    label: 'label.name'
+                    label: 'label.name',
+                    truncate: true
                 },
                 instancename: {
                     label: 'label.internal.name'
                 },
                 displayname: {
-                    label: 'label.display.name'
+                    label: 'label.display.name',
+                    truncate: true
                 },
                 zonename: {
-                    label: 'label.zone.name'
+                    label: 'label.zone.name',
+                    truncate: true
                 },
                 state: {
                     label: 'label.state',
