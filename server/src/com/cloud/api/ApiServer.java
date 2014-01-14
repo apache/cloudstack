e5512960afa384245cfb9d012ac6482b2b7f8940
Likitha Shetty <likitha.shetty@citrix.com>
2014-01-14 13:45:50 +0530

    CLOUDSTACK-5865. Unable to use login API if domainId parameter is id and not uuid

--- server/src/com/cloud/api/ApiServer.java
+++ server/src/com/cloud/api/ApiServer.java
@@ -760,7 +760,11 @@ public class ApiServer extends ManagerBase implements HttpRequestHandler, ApiSer
 
     @Override
     public Long fetchDomainId(String domainUUID) {
-        return _domainMgr.getDomain(domainUUID).getId();
+        Domain domain = _domainMgr.getDomain(domainUUID);
+        if (domain != null)
+            return domain.getId();
+        else 
+            return null; 
     }
 
     @Override
