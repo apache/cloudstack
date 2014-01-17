5121f8cd4e4adcdf23b6ec25cb1c28bec8f417a7
Abhinandan Prateek <aprateek@apache.org>
2014-01-17 21:15:51 +0530

    update packages list before getting jre 7

--- tools/appliance/definitions/systemvm64template/postinstall.sh
+++ tools/appliance/definitions/systemvm64template/postinstall.sh
@@ -69,6 +69,7 @@ install_packages() {
   apt-get --no-install-recommends -q -y --force-yes install keepalived conntrackd ipvsadm libnetfilter-conntrack3 libnl1
   # ipcalc
   apt-get --no-install-recommends -q -y --force-yes install ipcalc
+  apt-get update
   # java
   apt-get --no-install-recommends -q -y --force-yes install  openjdk-7-jre-headless
 
