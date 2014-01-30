db3dc2ee17c699f1abf42ea2fbe09ec2748caa4a
Hiroaki KAWAI <kawai@stratosphere.co.jp>
2014-01-30 20:24:57 +0900

    Some rework stratosphere ssp plugin
    * add missing command entry in commands.properties
    * migrate httpclient 3.x to 4.x
    * fix the broken SspClient
    * add webapp session checking in mock ssp server

--- plugins/network-elements/stratosphere-ssp/src/org/apache/cloudstack/network/element/SspElement.java
+++ plugins/network-elements/stratosphere-ssp/src/org/apache/cloudstack/network/element/SspElement.java
@@ -189,14 +189,10 @@ public class SspElement extends AdapterBase implements ConnectivityProvider, Ssp
     public boolean isReady(PhysicalNetworkServiceProvider provider) {
         PhysicalNetwork physicalNetwork = _physicalNetworkDao.findById(provider.getPhysicalNetworkId());
         assert(physicalNetwork!=null);
-        if(physicalNetwork != null){
-            if(fetchSspClients(physicalNetwork.getId(), physicalNetwork.getDataCenterId(), false).size() > 0){
-                return true;
-            }
-            s_logger.warn("Ssp api endpoint not found. "+physicalNetwork.toString());
-        }else{
-            s_logger.warn("PhysicalNetwork is NULL.");
+        if(fetchSspClients(physicalNetwork.getId(), physicalNetwork.getDataCenterId(), false).size() > 0){
+            return true;
         }
+        s_logger.warn("Ssp api endpoint not found. "+physicalNetwork.toString());
         return false;
     }
 
