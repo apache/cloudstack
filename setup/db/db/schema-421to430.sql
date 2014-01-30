ccc8aa78cd8f02b175adad99417d83796e3d4a82
Jayapal <jayapal@apache.org>
2014-01-30 14:16:58 +0530

    CLOUDSTACK-5966 Updated service monitoring config to start by default. Global settings EnableServiceMonitoring param set to true (cherry picked from commit 7255e50f2051aff1cb5e1991e2dde4ef48f57454)
    Signed-off-by: Animesh Chaturvedi <animesh@apache.org>

--- setup/db/db/schema-421to430.sql
+++ setup/db/db/schema-421to430.sql
@@ -1089,6 +1089,5 @@ CREATE VIEW `cloud`.`user_vm_view` AS
            left join
         `cloud`.`user_vm_details` `custom_ram_size`  ON (((`custom_ram_size`.`vm_id` = `cloud`.`vm_instance`.`id`) and (`custom_ram_size`.`name` = 'memory')));
 
-INSERT INTO `cloud`.`configuration`(category, instance, component, name, value, description, default_value) VALUES ('NetworkManager', 'DEFAULT', 'management-server', 'network.router.EnableServiceMonitoring', 'false', 'service monitoring in router enable/disable option, default false', 'false') ON DUPLICATE KEY UPDATE category='NetworkManager';
 
 INSERT IGNORE INTO `cloud`.`configuration`(category, instance, component, name, value, description, default_value) VALUES ('NetworkManager', 'DEFAULT', 'management-server', 'network.router.EnableServiceMonitoring', 'true', 'service monitoring in router enable/disable option, default true', 'true') ON DUPLICATE KEY UPDATE category='NetworkManager';
