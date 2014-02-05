e51d472b0cfda56db91fc82728c6bb29016e4d50
Alena Prokharchyk <alena.prokharchyk@citrix.com>
2014-02-05 15:28:34 -0800

    External UUID control: as only Root admin can change external UUID, so modified @Parameter to enforce the check

--- api/src/org/apache/cloudstack/api/BaseCustomIdCmd.java
+++ api/src/org/apache/cloudstack/api/BaseCustomIdCmd.java
@@ -16,11 +16,13 @@
 // under the License.
 package org.apache.cloudstack.api;
 
+import org.apache.cloudstack.acl.RoleType;
+
 public abstract class BaseCustomIdCmd extends BaseCmd {
 
     @Parameter(name = ApiConstants.CUSTOM_ID,
                type = CommandType.STRING,
- description = "an optional field, in case you want to set a custom id to the resource. Allowed to Root Admins only", since = "4.4")
+ description = "an optional field, in case you want to set a custom id to the resource. Allowed to Root Admins only", since = "4.4", authorized = {RoleType.Admin})
     private String customId;
 
     public String getCustomId() {
