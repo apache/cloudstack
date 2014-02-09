8ed9162de7bb6d32d719c5afb11dc62296d2c45e
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2014-02-09 21:57:07 +0100

    Cleanup in UriUtils.encodeURIComponent
    - StringBuffer replaced with StringBuilder
    - nullcheck of tokens array removed since String.split does not return null
    - unit test added
    
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- utils/src/com/cloud/utils/UriUtils.java
+++ utils/src/com/cloud/utils/UriUtils.java
@@ -94,15 +94,12 @@ public class UriUtils {
 
         if (pathStart > 0) {
             String[] tokens = url.substring(pathStart + 1).split("/");
-            if (tokens != null) {
-                StringBuffer sb = new StringBuffer();
-                sb.append(url.substring(0, pathStart));
-                for (String token : tokens) {
-                    sb.append("/").append(URLEncoder.encode(token));
-                }
-
-                return sb.toString();
+            StringBuilder sb = new StringBuilder(url.substring(0, pathStart));
+            for (String token : tokens) {
+                sb.append("/").append(URLEncoder.encode(token));
             }
+
+            return sb.toString();
         }
 
         // no need to do URL component encoding
