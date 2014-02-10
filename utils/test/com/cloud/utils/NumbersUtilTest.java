6c770958f1f9ecc7271cf7d308e7f149fad0274f
Hugo Trippaers <htrippaers@schubergphilis.com>
2014-02-10 12:27:03 +0100

    Fix unittest issue when you are in a locale that uses comma for decimal separation.

--- utils/test/com/cloud/utils/NumbersUtilTest.java
+++ utils/test/com/cloud/utils/NumbersUtilTest.java
@@ -18,12 +18,15 @@ package com.cloud.utils;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Locale;
+
 import org.junit.Test;
 
 public class NumbersUtilTest {
 
     @Test
     public void toReadableSize() {
+        Locale.setDefault(Locale.US); // Fixed locale for the test
         assertEquals("1.0000 TB", NumbersUtil.toReadableSize((1024l * 1024l * 1024l * 1024l)));
         assertEquals("1.00 GB", NumbersUtil.toReadableSize(1024 * 1024 * 1024));
         assertEquals("1.00 MB", NumbersUtil.toReadableSize(1024 * 1024));
