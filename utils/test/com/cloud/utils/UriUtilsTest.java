59364ee9a418606550a0f9d6b42390ca5adf8e26
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2014-02-11 18:45:38 +0100

    Added test for UriUtils.getUpdateUri
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- utils/test/com/cloud/utils/UriUtilsTest.java
+++ utils/test/com/cloud/utils/UriUtilsTest.java
@@ -30,4 +30,24 @@ public class UriUtilsTest {
         Assert.assertEquals("http://localhost/foo/bar",
                 UriUtils.encodeURIComponent("http://localhost/foo/bar"));
     }
+
+    @Test
+    public void getUpdateUri() {
+        // no password param, no request for encryption
+        Assert.assertEquals("http://localhost/foo/bar?param=true", UriUtils
+                .getUpdateUri("http://localhost/foo/bar?param=true", false));
+        // there is password param but still no request for encryption, should
+        // be unchanged
+        Assert.assertEquals("http://localhost/foo/bar?password=1234", UriUtils
+                .getUpdateUri("http://localhost/foo/bar?password=1234", false));
+        // if there is password param and encryption is requested then it may or
+        // may not be changed depending on how the EncrytionUtils is setup, but
+        // at least it needs to start with the same url
+        Assert.assertTrue(UriUtils.getUpdateUri(
+                "http://localhost/foo/bar?password=1234", true).startsWith(
+                "http://localhost/foo/bar"));
+
+        //XXX: Interesting cases not covered:
+        // * port is ignored and left out from the return value
+    }
 }
