859a78e1d321a936efd18dc6f0a55ab336cd3b70
Marcus Sorensen <marcus@betterservers.com>
2014-03-04 22:19:08 -0700

    CLOUDSTACK-6194: Failed to increase Shared network IP Range
    Submitted-by:Saksham Srivastava <saksham.srivastava@citrix.com>
    (cherry picked from commit 135247afd114c1dfc6dde22184430237e79aafc5)
    
    Signed-off-by: Animesh Chaturvedi <animesh@apache.org>

--- server/src/com/cloud/configuration/ConfigurationManagerImpl.java
+++ server/src/com/cloud/configuration/ConfigurationManagerImpl.java
@@ -2666,7 +2666,7 @@ ConfigurationManagerImpl extends ManagerBase implements ConfigurationManager, Co
                 List<VlanVO> vlans = _vlanDao.listVlansByNetworkId(network.getId());
                 if (vlans != null && vlans.size() > 0) {
                     VlanVO vlan = vlans.get(0);
-                    if (vlanId == null) {
+                    if (vlanId == null || vlanId.contains(Vlan.UNTAGGED)) {
                         vlanId = vlan.getVlanTag();
                     } else if (!NetUtils.isSameIsolationId(vlan.getVlanTag(), vlanId)) {
                         throw new InvalidParameterValueException("there is already one vlan " + vlan.getVlanTag()
