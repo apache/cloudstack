apache/cloudstack

commit 39e5b768e8fcae9622e01b2ca72a49675eb6d3fb

More generic parameters that are not unknown
Adding some more parameter names that should not be considered unknown since they belong to general use paramters

Signed-off-by: Alena Prokharchyk <alena.prokharchyk@citrix.com>

api/src/org/apache/cloudstack/api/ApiConstants.java
@@ -59,7 +59,7 @@ public class ApiConstants {
     public static final String CTX_ACCOUNT_ID = "ctxaccountid";
     public static final String CTX_USER_ID = "ctxuserid";
     public static final String CTXSTARTEVENTID = "ctxstarteventid";
-    public static final String CTX_START_EVENT_ID = "ctxStartEventId";
+    public static final String CTX_START_EVENT_ID = "ctxstarteventid";
     public static final String CUSTOMIZED = "customized";
     public static final String CUSTOMIZED_IOPS = "customizediops";
     public static final String CUSTOM_ID = "customid";

server/src/com/cloud/api/dispatch/ParamGenericValidationWorker.java
@@ -42,6 +42,7 @@ public class ParamGenericValidationWorker implements DispatchWorker {
     protected static final List<String> defaultParamNames = new ArrayList<String>();
 
     static {
+        defaultParamNames.add(ApiConstants.ACCOUNT_ID);
         defaultParamNames.add(ApiConstants.CTX_START_EVENT_ID);
         defaultParamNames.add(ApiConstants.COMMAND);
         defaultParamNames.add(ApiConstants.CMD_EVENT_TYPE);
@@ -62,6 +63,8 @@ public class ParamGenericValidationWorker implements DispatchWorker {
         defaultParamNames.add(ApiConstants.CTX_ACCOUNT_ID);
         defaultParamNames.add(ApiConstants.CTX_START_EVENT_ID);
         defaultParamNames.add(ApiConstants.CTX_USER_ID);
+        defaultParamNames.add(ApiConstants.UUID);
+        defaultParamNames.add(ApiConstants.ID);
         defaultParamNames.add("_");
     }
 

server/test/com/cloud/api/dispatch/ParamGenericValidationWorkerTest.java
@@ -75,6 +75,30 @@ public class ParamGenericValidationWorkerTest {
         final BaseCmd cmd = new FakeCmd();
         final Map<String, String> params = new HashMap<String, String>();
         params.put(ApiConstants.COMMAND, "");
+        params.put(ApiConstants.ACCOUNT_ID, "");
+        params.put(ApiConstants.CTX_START_EVENT_ID, "");
+        params.put(ApiConstants.COMMAND, "");
+        params.put(ApiConstants.CMD_EVENT_TYPE, "");
+        params.put(ApiConstants.USERNAME, "");
+        params.put(ApiConstants.USER_ID, "");
+        params.put(ApiConstants.PASSWORD, "");
+        params.put(ApiConstants.DOMAIN, "");
+        params.put(ApiConstants.DOMAIN_ID, "");
+        params.put(ApiConstants.DOMAIN__ID, "");
+        params.put(ApiConstants.SESSIONKEY, "");
+        params.put(ApiConstants.RESPONSE, "");
+        params.put(ApiConstants.PAGE, "");
+        params.put(ApiConstants.USER_API_KEY, "");
+        params.put(ApiConstants.API_KEY, "");
+        params.put(ApiConstants.PAGE_SIZE, "");
+        params.put(ApiConstants.HTTPMETHOD, "");
+        params.put(ApiConstants.SIGNATURE, "");
+        params.put(ApiConstants.CTX_ACCOUNT_ID, "");
+        params.put(ApiConstants.CTX_START_EVENT_ID, "");
+        params.put(ApiConstants.CTX_USER_ID, "");
+        params.put(ApiConstants.UUID, "");
+        params.put(ApiConstants.ID, "");
+        params.put("_", "");
         params.put("addedParam", "");
 
         Account account = new AccountVO("testaccount", 1L, "networkdomain", (short) 0, "uuid");

commit d3fd66e9f555616530e483d1eacf2525556bf14e

Fix VM deploy failure bug introduced by commit 39e5b768e8fcae9622e01b2ca72a49675eb6d3fb.

api/src/org/apache/cloudstack/api/ApiConstants.java
@@ -59,7 +59,7 @@ public class ApiConstants {
     public static final String CTX_ACCOUNT_ID = "ctxaccountid";
     public static final String CTX_USER_ID = "ctxuserid";
     public static final String CTXSTARTEVENTID = "ctxstarteventid";
-    public static final String CTX_START_EVENT_ID = "ctxstarteventid";
+    public static final String CTX_START_EVENT_ID = "ctxStartEventId";
     public static final String CUSTOMIZED = "customized";
     public static final String CUSTOMIZED_IOPS = "customizediops";
     public static final String CUSTOM_ID = "customid";
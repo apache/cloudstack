d3fd66e9f555616530e483d1eacf2525556bf14e
Min Chen <min.chen@citrix.com>
2014-03-13 16:37:26 -0700

    Fix VM deploy failure bug introduced by commit 39e5b768e8fcae9622e01b2ca72a49675eb6d3fb.

--- api/src/org/apache/cloudstack/api/ApiConstants.java
+++ api/src/org/apache/cloudstack/api/ApiConstants.java
@@ -59,7 +59,7 @@ public class ApiConstants {
     public static final String CTX_ACCOUNT_ID = "ctxaccountid";
     public static final String CTX_USER_ID = "ctxuserid";
     public static final String CTXSTARTEVENTID = "ctxstarteventid";
-    public static final String CTX_START_EVENT_ID = "ctxstarteventid";
+    public static final String CTX_START_EVENT_ID = "ctxStartEventId";
     public static final String CUSTOMIZED = "customized";
     public static final String CUSTOMIZED_IOPS = "customizediops";
     public static final String CUSTOM_ID = "customid";
