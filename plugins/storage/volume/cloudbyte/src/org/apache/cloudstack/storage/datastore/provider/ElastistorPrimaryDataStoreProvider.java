f109e387916fd6ffda4afe2f1ef79a8ccd6b0eb4
Edison Su <sudison@gmail.com>
2014-03-18 15:01:16 -0700

    add apache licence header for cloudbyte

--- plugins/storage/volume/cloudbyte/src/org/apache/cloudstack/storage/datastore/provider/ElastistorPrimaryDataStoreProvider.java
+++ plugins/storage/volume/cloudbyte/src/org/apache/cloudstack/storage/datastore/provider/ElastistorPrimaryDataStoreProvider.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.cloudstack.storage.datastore.provider;
 
 import java.util.HashSet;
@@ -23,14 +41,6 @@ import com.cloud.alert.AlertManager;
 import com.cloud.storage.dao.StoragePoolHostDao;
 import com.cloud.utils.component.ComponentContext;
 
-/**
- * This is the starting point of the elastistor storage plugin. This bean will
- * be detected by Spring container & initialized. This will be one of the
- * providers available via {@link DataStoreProviderManagerImpl} object.
- *
- * @author amit.das@cloudbyte.com
- * @author punith.s@cloudbyte.com
- */
 @Component
 public class ElastistorPrimaryDataStoreProvider implements PrimaryDataStoreProvider {
 
--- plugins/storage/volume/cloudbyte/src/org/apache/cloudstack/storage/datastore/util/ElastistorUtil.java
+++ plugins/storage/volume/cloudbyte/src/org/apache/cloudstack/storage/datastore/util/ElastistorUtil.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.apache.cloudstack.storage.datastore.util;
 
 import java.net.ConnectException;
@@ -31,13 +50,6 @@ import com.sun.jersey.api.client.config.ClientConfig;
 import com.sun.jersey.api.client.config.DefaultClientConfig;
 import com.sun.jersey.core.util.MultivaluedMapImpl;
 
-/**
- * The util class for elastistor's storage plugin codebase.
- *
- * @author amit.das@cloudbyte.com
- * @author punith.s@cloudbyte.com
- *
- */
 public class ElastistorUtil {
 
     private static final Logger s_logger = Logger.getLogger(ElastistorUtil.class);
