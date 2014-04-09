c94eb0b946c9fab5160a61d6a9f1af44fab99570
Sanjay Tripathi <sanjay.tripathi@citrix.com>
2014-04-09 21:50:16 +0530

    Fix for commented package error.

--- api/src/com/cloud/agent/api/VgpuTypesInfo.java
+++ api/src/com/cloud/agent/api/VgpuTypesInfo.java
@@ -13,7 +13,8 @@
 // "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
-// under the License.package com.cloud.agent.api;
+// under the License.
+package com.cloud.agent.api;
 public class VgpuTypesInfo {
 
     private String modelName;
