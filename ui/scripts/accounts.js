20c581e63201d8be518e793cdaed77f88bfa3b58
Jessica Wang <jessicawang@apache.org>
2014-04-23 16:27:44 -0700

    BUG-ID: CS-19795: UI > Add LDAP Account - fix a bug that a LDAP account that does not have email and all LDAP accounts below it are missing from the listing. Reviewed-by: Brian
    (cherry picked from commit 953167808c3e9a9c40a6f7be6e16ebf32c608a4c)

--- ui/scripts/accounts.js
+++ ui/scripts/accounts.js
@@ -96,6 +96,7 @@
                             label: 'Add LDAP Account',
                             isHeader: true,
                             preFilter: function(args) {
+                                //if ((isAdmin() || isDomainAdmin()) && true) { //for testing only
                                 if ((isAdmin() || isDomainAdmin()) && isLdapEnabled()) {
                                     return true;
                                 } else {
