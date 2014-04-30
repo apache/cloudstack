537536835bfe5161946fbc83675b4a4696536949
Sachchidanand Vaidya <vaidyasd@juniper.net>
2014-04-30 16:11:06 -0700

    - Created new devcloud advanced networking config file (devcloud-advanced_juniper-contrail.cfg). - Modification to deployDataCenter script, to enable "JuniperContrailRouter" & "JuniperContrailVpcRouter" providers.
    Signed-off-by: Daan Hoogland <daan@onecht.net>

--- tools/marvin/marvin/deployDataCenter.py
+++ tools/marvin/marvin/deployDataCenter.py
@@ -516,6 +516,13 @@ class DeployDataCenters(object):
                         self.enableProvider(pnetprovres[0].id)
                     elif provider.name == 'SecurityGroupProvider':
                         self.enableProvider(pnetprovres[0].id)
+                elif provider.name in ['JuniperContrailRouter', 'JuniperContrailVpcRouter']:
+                    netprov = addNetworkServiceProvider.\
+                        addNetworkServiceProviderCmd()
+                    netprov.name = provider.name
+                    netprov.physicalnetworkid = phynetwrk.id
+                    result = self.__apiClient.addNetworkServiceProvider(netprov)
+                    self.enableProvider(result.id)
                 elif provider.name in ['Netscaler', 'JuniperSRX', 'F5BigIp']:
                     netprov = addNetworkServiceProvider.\
                         addNetworkServiceProviderCmd()
