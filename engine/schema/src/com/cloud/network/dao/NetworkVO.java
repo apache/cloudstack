a2d8f6c9cb94b46782f93a3e430196512d2babb6
Daan Hoogland <daan@onecht.net>
2014-05-09 16:51:34 +0200

    be able to remove vpc as owning entity of a network

--- engine/schema/src/com/cloud/network/dao/NetworkVO.java
+++ engine/schema/src/com/cloud/network/dao/NetworkVO.java
@@ -600,4 +600,8 @@ public class NetworkVO implements Network {
     public void setStrechedL2Network(boolean strechedL2Network) {
         this.strechedL2Network = strechedL2Network;
     }
+
+    public void setVpcId(long vpcId) {
+        this.vpcId = vpcId;
+    }
 }
