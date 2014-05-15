ee29915ef019a85e30c03518b5fc83aaa54d5296
Gaurav Aradhye <gaurav.aradhye@clogeny.com>
2014-05-14 20:17:01 -0400

    Fixed config issue related to Persistent Networks

--- tools/marvin/marvin/config/test_data.py
+++ tools/marvin/marvin/config/test_data.py
@@ -176,7 +176,7 @@ test_data = {
         "guestiptype": 'Isolated',
         "supportedservices": 'Dhcp,Dns,SourceNat,PortForwarding',
         "traffictype": 'GUEST',
-        "isPersistent": 'True',
+        "ispersistent": 'True',
         "availability": 'Optional',
         "serviceProviderList": {
                 "Dhcp": 'VirtualRouter',
@@ -455,7 +455,7 @@ test_data = {
         "supportedservices":
         'Vpn,Dhcp,Dns,SourceNat,PortForwarding,Firewall,Lb,UserData,StaticNat',
         "traffictype": 'GUEST',
-        "isPersistent": 'True',
+        "ispersistent": 'True',
         "availability": 'Optional',
         "serviceProviderList": {
                 "Vpn": 'VirtualRouter',
@@ -552,7 +552,7 @@ test_data = {
         "supportedservices":
         'Dhcp,Dns,SourceNat,PortForwarding,Vpn,Firewall,Lb,UserData,StaticNat',
         "traffictype": 'GUEST',
-        "isPersistent": 'True',
+        "ispersistent": 'True',
         "availability": 'Optional',
         "serviceProviderList": {
             "Dhcp": 'VirtualRouter',
@@ -575,7 +575,7 @@ test_data = {
         'Vpn,Dhcp,Dns,SourceNat,PortForwarding,UserData,StaticNat,NetworkACL',
         "traffictype": 'GUEST',
         "availability": 'Optional',
-        "isPersistent": 'True',
+        "ispersistent": 'True',
         "useVpc": 'on',
         "serviceProviderList": {
                 "Vpn": 'VpcVirtualRouter',
