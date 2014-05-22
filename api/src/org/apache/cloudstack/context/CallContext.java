cd62aecb2f2407e950960336d606a1a32b410f83
Daan Hoogland <daan@onecht.net>
2014-05-22 10:00:05 +0200

    conflict headers removed from comment

--- api/src/org/apache/cloudstack/context/CallContext.java
+++ api/src/org/apache/cloudstack/context/CallContext.java
@@ -145,10 +145,7 @@ public class CallContext {
 
     protected static CallContext register(User callingUser, Account callingAccount, Long userId, Long accountId, String contextId) {
         /*
-<<<<<<< HEAD
                 Unit tests will have multiple times of setup/tear-down call to this, remove assertions to all unit test to run
-=======
->>>>>>> a7a8a19... BUG-ID: CS-19295: add job path to help associate an API job to related internal job. Reviewed-By: Self
                 assert s_currentContext.get() == null : "There's a context already so what does this new register context mean? " + s_currentContext.get().toString();
                 if (s_currentContext.get() != null) { // FIXME: This should be removed soon.  I added this check only to surface all the places that have this problem.
                     throw new CloudRuntimeException("There's a context already so what does this new register context mean? " + s_currentContext.get().toString());
