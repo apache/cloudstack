3d5a7abba96092db7957f68fae92263213550fc0
Bharat Kumar <bharat.kumar@citrix.com>
2014-06-09 18:42:43 +0530

    CLOUDSTACK-6877 Tagged the testcase with the bugId
    Signed-off-by: Abhinandan Prateek <aprateek@apache.org>
    (cherry picked from commit 06bcc8d9eee802a3f5ada6bb7335ffe0cb7b3008)

--- test/integration/smoke/test_secondary_storage.py
+++ test/integration/smoke/test_secondary_storage.py
@@ -81,7 +81,7 @@ class TestSecStorageServices(cloudstackTestCase):
             raise Exception("Warning: Exception during cleanup : %s" % e)
         return
 
-    @attr(tags = ["advanced", "advancedns", "smoke", "basic", "eip", "sg", "selfservice"])
+    @attr(tags = ["advanced", "advancedns", "smoke", "basic", "eip", "sg", "selfservice"],BugId="CLOUDSTACK-6877")
     def test_01_sys_vm_start(self):
         """Test system VM start
         """
