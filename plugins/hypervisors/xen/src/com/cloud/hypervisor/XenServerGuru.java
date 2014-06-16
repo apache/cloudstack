de04881da177b28d7cb3efc09adb2d8069f0fdd3
amoghvk <amogh.vasekar@citrix.com>
2014-06-16 14:29:14 -0700

    CLOUDSTACK-6915: Include searching deleted OS entries

--- plugins/hypervisors/xen/src/com/cloud/hypervisor/XenServerGuru.java
+++ plugins/hypervisors/xen/src/com/cloud/hypervisor/XenServerGuru.java
@@ -89,7 +89,7 @@ public class XenServerGuru extends HypervisorGuruBase implements HypervisorGuru 
         to.setBootloader(bt);
 
         // Determine the VM's OS description
-        GuestOSVO guestOS = _guestOsDao.findById(vm.getVirtualMachine().getGuestOSId());
+        GuestOSVO guestOS = _guestOsDao.findByIdIncludingRemoved(vm.getVirtualMachine().getGuestOSId());
         to.setOs(guestOS.getDisplayName());
         HostVO host = hostDao.findById(vm.getVirtualMachine().getHostId());
         GuestOSHypervisorVO guestOsMapping = _guestOsHypervisorDao.findByOsIdAndHypervisor(guestOS.getId(), getHypervisorType().toString(), host.getHypervisorVersion());
