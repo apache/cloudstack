05b79a127a0eae6d44def3626c944b080b7bcf5b
Hugo Trippaers <htrippaers@schubergphilis.com>
2014-06-25 16:07:00 +0200

    Fix the license headers Nitin forgot.

--- engine/schema/src/org/apache/cloudstack/resourcedetail/SnapshotPolicyDetailVO.java
+++ engine/schema/src/org/apache/cloudstack/resourcedetail/SnapshotPolicyDetailVO.java
@@ -1,6 +1,20 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
 package org.apache.cloudstack.resourcedetail;
-
-import org.apache.cloudstack.api.ResourceDetail;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -8,6 +22,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import org.apache.cloudstack.api.ResourceDetail;
 
 @Entity
 @Table(name = "snapshot_policy_details")
@@ -33,7 +49,7 @@ public class SnapshotPolicyDetailVO implements ResourceDetail {
     }
 
     public SnapshotPolicyDetailVO(long id, String name, String value) {
-        this.resourceId = id;
+        resourceId = id;
         this.name = name;
         this.value = value;
     }
