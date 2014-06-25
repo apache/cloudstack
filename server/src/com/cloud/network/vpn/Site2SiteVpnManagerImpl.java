86aff86b7872485d7068c4b8ee2c759d54b2112f
Daan Hoogland <dhoogland@schubergphilis.com>
2014-06-25 15:45:28 +0200

    CLOUDSTACK-6747: call a more forgiving test on the supplied peer cidr-list (cherry picked from commit 17850c7aff432a504d65a34d2f22ca7e1952a770)

--- server/src/com/cloud/network/vpn/Site2SiteVpnManagerImpl.java
+++ server/src/com/cloud/network/vpn/Site2SiteVpnManagerImpl.java
@@ -185,9 +185,9 @@ public class Site2SiteVpnManagerImpl extends ManagerBase implements Site2SiteVpn
         if (name == null) {
             name = "VPN-" + gatewayIp;
         }
-        String guestCidrList = cmd.getGuestCidrList();
-        if (!NetUtils.validateGuestCidrList(guestCidrList)) {
-            throw new InvalidParameterValueException("The customer gateway guest cidr list " + guestCidrList + " is invalid guest cidr!");
+        String peerCidrList = cmd.getGuestCidrList();
+        if (!NetUtils.isValidCidrList(peerCidrList)) {
+            throw new InvalidParameterValueException("The customer gateway peer cidr list " + peerCidrList + " contains an invalid cidr!");
         }
         String ipsecPsk = cmd.getIpsecPsk();
         String ikePolicy = cmd.getIkePolicy();
@@ -228,10 +228,10 @@ public class Site2SiteVpnManagerImpl extends ManagerBase implements Site2SiteVpn
             throw new InvalidParameterValueException("The customer gateway with name " + name + " already existed!");
         }
 
-        checkCustomerGatewayCidrList(guestCidrList);
+        checkCustomerGatewayCidrList(peerCidrList);
 
         Site2SiteCustomerGatewayVO gw =
-            new Site2SiteCustomerGatewayVO(name, accountId, owner.getDomainId(), gatewayIp, guestCidrList, ipsecPsk, ikePolicy, espPolicy, ikeLifetime, espLifetime, dpd);
+            new Site2SiteCustomerGatewayVO(name, accountId, owner.getDomainId(), gatewayIp, peerCidrList, ipsecPsk, ikePolicy, espPolicy, ikeLifetime, espLifetime, dpd);
         _customerGatewayDao.persist(gw);
         return gw;
     }
