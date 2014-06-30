0c9c5a71b4bcddf89381d6d3f66c5b83905c94d9
Nitin Mehta <nitin.mehta@citrix.com>
2014-06-30 14:36:57 -0700

    CLOUDSTACK-4364: Restore VM - log a usage event for newly created volume

--- server/test/com/cloud/vm/UserVmManagerTest.java
+++ server/test/com/cloud/vm/UserVmManagerTest.java
@@ -36,6 +36,7 @@ import java.util.List;
 import java.util.UUID;
 
 import com.cloud.storage.Storage;
+import com.cloud.event.dao.UsageEventDao;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
@@ -146,6 +147,8 @@ public class UserVmManagerTest {
     ResourceLimitService _resourceLimitMgr;
     @Mock
     PrimaryDataStoreDao _storagePoolDao;
+    @Mock
+    UsageEventDao _usageEventDao;
 
     @Before
     public void setup() {
@@ -155,6 +158,7 @@ public class UserVmManagerTest {
         _userVmMgr._vmInstanceDao = _vmInstanceDao;
         _userVmMgr._templateDao = _templateDao;
         _userVmMgr._volsDao = _volsDao;
+        _userVmMgr._usageEventDao = _usageEventDao;
         _userVmMgr._itMgr = _itMgr;
         _userVmMgr.volumeMgr = _storageMgr;
         _userVmMgr._accountDao = _accountDao;
