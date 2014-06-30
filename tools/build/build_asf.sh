3142bb7f3a59edbeac2f1afd012feb97c20ba150
Daan Hoogland <dhoogland@schubergphilis.com>
2014-06-30 17:53:17 +0200

    edit checkstyle pom on rc creation

--- tools/build/build_asf.sh
+++ tools/build/build_asf.sh
@@ -92,6 +92,7 @@ echo "found $currentversion"
 echo 'setting version numbers'
 mvn versions:set -DnewVersion=$version -P vmware -P developer -P systemvm -P simulator -P baremetal -P ucs -Dnoredist
 mv deps/XenServerJava/pom.xml.versionsBackup deps/XenServerJava/pom.xml
+perl -pi -e "s/-SNAPSHOT//" tools/checkstyle/pom.xml
 perl -pi -e "s/-SNAPSHOT//" deps/XenServerJava/pom.xml
 perl -pi -e "s/-SNAPSHOT//" tools/apidoc/pom.xml
 case "$currentversion" in 
