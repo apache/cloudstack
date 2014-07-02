9f68870a779d0b969e4fb8f766846537a456e2ad
Prachi Damle <prachi.damle@citrix.com>
2014-07-02 16:32:13 -0700

    CLOUDSTACK-7047: DeploymentPlanner should include disabled resources only when the VM owner is Admin account
    Changes:
    -DeploymentPlanner should include disabled resources only when the VM owner is Admin account.
    The disabled resources should be ignored when VM is owned by any other user.

--- server/src/com/cloud/deploy/DeploymentPlanningManagerImpl.java
+++ server/src/com/cloud/deploy/DeploymentPlanningManagerImpl.java
@@ -1233,7 +1233,7 @@ public class DeploymentPlanningManagerImpl extends ManagerBase implements Deploy
             if (s_logger.isDebugEnabled()) {
                 s_logger.debug("We need to allocate new storagepool for this volume");
             }
-            if (!isRootAdmin(plan.getReservationContext())) {
+            if (!isRootAdmin(vmProfile)) {
                 if (!isEnabledForAllocation(plan.getDataCenterId(), plan.getPodId(), plan.getClusterId())) {
                     if (s_logger.isDebugEnabled()) {
                         s_logger.debug("Cannot allocate new storagepool for this volume in this cluster, allocation state is disabled");
@@ -1361,10 +1361,10 @@ public class DeploymentPlanningManagerImpl extends ManagerBase implements Deploy
         return true;
     }
 
-    private boolean isRootAdmin(ReservationContext reservationContext) {
-        if (reservationContext != null) {
-            if (reservationContext.getAccount() != null) {
-                return _accountMgr.isRootAdmin(reservationContext.getAccount().getId());
+    private boolean isRootAdmin(VirtualMachineProfile vmProfile) {
+        if (vmProfile != null) {
+            if (vmProfile.getOwner() != null) {
+                return _accountMgr.isRootAdmin(vmProfile.getOwner().getId());
             } else {
                 return false;
             }
