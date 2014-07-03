1a85213bbf602ccd10a9256f36d4c221be450099
Antonio Fornie <afornie@schubergphilis.com>
2014-07-03 12:17:26 -0500

    Rules and visitors for Load Balance Rules
    Conflicts:
    	server/src/com/cloud/network/element/VirtualRouterElement.java

---
+++ server/src/com/cloud/network/rules/PasswordToRouterRules.java
@@ -0,0 +1,53 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package com.cloud.network.rules;
+
+import com.cloud.exception.ResourceUnavailableException;
+import com.cloud.network.Network;
+import com.cloud.network.router.VirtualRouter;
+import com.cloud.network.topology.NetworkTopologyVisitor;
+import com.cloud.vm.NicProfile;
+import com.cloud.vm.VirtualMachineProfile;
+
+public class PasswordToRouterRules extends RuleApplier {
+
+    private final NicProfile nic;
+    private final VirtualMachineProfile profile;
+
+    public PasswordToRouterRules(final Network network, final NicProfile nic, final VirtualMachineProfile profile) {
+        super(network);
+
+        this.nic = nic;
+        this.profile = profile;
+    }
+
+    @Override
+    public boolean accept(final NetworkTopologyVisitor visitor, final VirtualRouter router) throws ResourceUnavailableException {
+        this.router = router;
+
+        return visitor.visit(this);
+    }
+
+    public NicProfile getNic() {
+        return nic;
+    }
+
+    public VirtualMachineProfile getProfile() {
+        return profile;
+    }
+}
---
+++ server/src/com/cloud/network/rules/UserdataPwdRules.java
@@ -0,0 +1,60 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package com.cloud.network.rules;
+
+import com.cloud.deploy.DeployDestination;
+import com.cloud.exception.ResourceUnavailableException;
+import com.cloud.network.Network;
+import com.cloud.network.router.VirtualRouter;
+import com.cloud.network.topology.NetworkTopologyVisitor;
+import com.cloud.vm.NicProfile;
+import com.cloud.vm.VirtualMachineProfile;
+
+public class UserdataPwdRules extends RuleApplier {
+
+    private final NicProfile nic;
+    private final VirtualMachineProfile profile;
+    private final DeployDestination destination;
+
+    public UserdataPwdRules(final Network network, final NicProfile nic, final VirtualMachineProfile profile, final DeployDestination destination) {
+        super(network);
+
+        this.nic = nic;
+        this.profile = profile;
+        this.destination = destination;
+    }
+
+    @Override
+    public boolean accept(final NetworkTopologyVisitor visitor, final VirtualRouter router) throws ResourceUnavailableException {
+        this.router = router;
+
+        return visitor.visit(this);
+    }
+
+    public NicProfile getNic() {
+        return nic;
+    }
+
+    public VirtualMachineProfile getProfile() {
+        return profile;
+    }
+
+    public DeployDestination getDestination() {
+        return destination;
+    }
+}
