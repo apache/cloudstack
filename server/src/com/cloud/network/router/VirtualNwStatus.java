6b6da56522b2e8938e9668654d38b277e655f12e
Antonio Fornie <afornie@schubergphilis.com>
2014-07-10 09:54:01 -0500

    Extract general behavior to Router and Vpc delegates

---
+++ server/src/com/cloud/network/router/VirtualNwStatus.java
@@ -0,0 +1,10 @@
+package com.cloud.network.router;
+
+import com.cloud.user.Account;
+
+public class VirtualNwStatus {
+
+    public static String instance = "";
+
+    public static Account account = null;
+}
