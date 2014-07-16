66aec9a8080ab229ac433f1095631efa492f3b79
wrodrigues <wrodrigues@schubergphilis.com>
2014-07-16 15:01:59 +0200

    fixing injection of beans with a relationship
    Conflicts:
    	server/src/org/apache/cloudstack/network/topology/AdvancedNetworkTopology.java
    	server/src/org/apache/cloudstack/network/topology/BasicNetworkTopology.java

--- server/src/org/apache/cloudstack/network/topology/AdvancedNetworkTopology.java
+++ server/src/org/apache/cloudstack/network/topology/AdvancedNetworkTopology.java
@@ -22,13 +22,11 @@ import java.util.List;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
-import com.cloud.dc.DataCenter;
 import com.cloud.deploy.DeployDestination;
 import com.cloud.exception.ResourceUnavailableException;
 import com.cloud.network.Network;
-import com.cloud.network.lb.LoadBalancingRule;
-import com.cloud.network.router.VirtualRouter;
 import com.cloud.network.rules.RuleApplier;
 import com.cloud.network.rules.RuleApplierWrapper;
 import com.cloud.network.rules.UserdataPwdRules;
@@ -36,50 +34,8 @@ import com.cloud.vm.DomainRouterVO;
 import com.cloud.vm.NicProfile;
 import com.cloud.vm.VirtualMachineProfile;
 
-public class AdvancedNetworkTopology implements NetworkTopology {
-
-    @Override
-    public StringBuilder createGuestBootLoadArgs(final NicProfile guestNic, final String defaultDns1, final String defaultDns2, final DomainRouterVO router) {
-        return null;
-    }
-
-    @Override
-    public String retrieveGuestDhcpRange(final NicProfile guestNic, final Network guestNetwork, final DataCenter dc) {
-        return null;
-    }
-
-    @Override
-    public NicProfile retrieveControlNic(final VirtualMachineProfile profile) {
-        return null;
-    }
-
-    @Override
-    public boolean configDhcpForSubnet(final Network network, final NicProfile nic, final VirtualMachineProfile profile, final DeployDestination dest,
-            final List<DomainRouterVO> routers) throws ResourceUnavailableException {
-        return false;
-    }
-
-    @Override
-    public boolean applyDhcpEntry(final Network network, final NicProfile nic, final VirtualMachineProfile profile, final DeployDestination dest,
-            final List<DomainRouterVO> routers) throws ResourceUnavailableException {
-        return false;
-    }
-
-    @Override
-    public boolean applyRules(final Network network, final List<? extends VirtualRouter> routers, final String typeString, final boolean isPodLevelException, final Long podId,
-            final boolean failWhenDisconnect, final RuleApplier applier) throws ResourceUnavailableException {
-
-        AdvancedNetworkVisitor visitor = new AdvancedNetworkVisitor(this);
-        applier.accept(visitor, null);
-
-        return false;
-    }
-
-    @Override
-    public boolean sendCommandsToRouter(final VirtualRouter router, final List<LoadBalancingRule> rules, final long id) {
-        // TODO Auto-generated method stub
-        return false;
-    }
+@Component
+public class AdvancedNetworkTopology extends BasicNetworkTopology {
 
     private static final Logger s_logger = Logger.getLogger(AdvancedNetworkTopology.class);
 
