d86f589a1cccf02d182171b667c87835cff94a93
Wilder Rodrigues <wrodrigues@schubergphilis.com>
2014-07-16 09:30:10 +0200

    adding user data pwd rules
    Conflicts:
    	server/src/org/apache/cloudstack/network/topology/AdvancedNetworkTopology.java
    	server/src/org/apache/cloudstack/network/topology/AdvancedNetworkVisitor.java
    	server/src/org/apache/cloudstack/network/topology/BasicNetworkTopology.java

--- server/src/com/cloud/network/rules/UserdataPwdRules.java
+++ server/src/com/cloud/network/rules/UserdataPwdRules.java
@@ -19,43 +19,82 @@ package com.cloud.network.rules;
 
 import org.apache.cloudstack.network.topology.NetworkTopologyVisitor;
 
+import com.cloud.agent.api.routing.NetworkElementCommand;
+import com.cloud.agent.api.routing.SavePasswordCommand;
+import com.cloud.agent.manager.Commands;
+import com.cloud.dc.DataCenterVO;
 import com.cloud.deploy.DeployDestination;
 import com.cloud.exception.ResourceUnavailableException;
 import com.cloud.network.Network;
 import com.cloud.network.router.VirtualRouter;
+import com.cloud.utils.PasswordGenerator;
 import com.cloud.vm.NicProfile;
+import com.cloud.vm.NicVO;
+import com.cloud.vm.UserVmVO;
 import com.cloud.vm.VirtualMachineProfile;
 
 public class UserdataPwdRules extends RuleApplier {
 
-    private final NicProfile nic;
-    private final VirtualMachineProfile profile;
-    private final DeployDestination destination;
+    private final NicProfile _nic;
+    private final VirtualMachineProfile _profile;
+    private final DeployDestination _destination;
+
+    private NicVO _nicVo;
+    private UserVmVO _userVM;
 
     public UserdataPwdRules(final Network network, final NicProfile nic, final VirtualMachineProfile profile, final DeployDestination destination) {
         super(network);
 
-        this.nic = nic;
-        this.profile = profile;
-        this.destination = destination;
+        _nic = nic;
+        _profile = profile;
+        _destination = destination;
     }
 
     @Override
     public boolean accept(final NetworkTopologyVisitor visitor, final VirtualRouter router) throws ResourceUnavailableException {
-        this._router = router;
+        _router = router;
+
+        _userVM = _userVmDao.findById(_profile.getId());
+        _userVmDao.loadDetails(_userVM);
+
+        //for basic zone, send vm data/password information only to the router in the same pod
+        _nicVo = _nicDao.findById(_nic.getId());
 
         return visitor.visit(this);
     }
 
-    public NicProfile getNic() {
-        return nic;
-    }
-
     public VirtualMachineProfile getProfile() {
-        return profile;
+        return _profile;
     }
 
     public DeployDestination getDestination() {
-        return destination;
+        return _destination;
+    }
+
+    public NicVO getNicVo() {
+        return _nicVo;
+    }
+
+    public UserVmVO getUserVM() {
+        return _userVM;
+    }
+
+    public void createPasswordCommand(final VirtualRouter router, final VirtualMachineProfile profile, final NicVO nic, final Commands cmds) {
+        final String password = (String)profile.getParameter(VirtualMachineProfile.Param.VmPassword);
+        final DataCenterVO dcVo = _dcDao.findById(router.getDataCenterId());
+
+        // password should be set only on default network element
+        if (password != null && nic.isDefaultNic()) {
+            final String encodedPassword = PasswordGenerator.rot13(password);
+            final SavePasswordCommand cmd =
+                    new SavePasswordCommand(encodedPassword, nic.getIp4Address(), profile.getVirtualMachine().getHostName(), _networkModel.getExecuteInSeqNtwkElmtCmd());
+            cmd.setAccessDetail(NetworkElementCommand.ROUTER_IP, _routerControlHelper.getRouterControlIp(router.getId()));
+            cmd.setAccessDetail(NetworkElementCommand.ROUTER_GUEST_IP, _routerControlHelper.getRouterIpInNetwork(nic.getNetworkId(), router.getId()));
+            cmd.setAccessDetail(NetworkElementCommand.ROUTER_NAME, router.getInstanceName());
+            cmd.setAccessDetail(NetworkElementCommand.ZONE_NETWORK_TYPE, dcVo.getNetworkType().toString());
+
+            cmds.addCommand("password", cmd);
+        }
+
     }
 }
