06c1c16dc38a615dfead07dd78179b0f0dfb56f2
Leo Simons <lsimons@schubergphilis.com>
2014-07-21 11:46:02 +0200

    CLOUDSTACK-7143: move install_packages from postinstall.sh to its own file

--- tools/appliance/definitions/systemvmtemplate/postinstall.sh
+++ tools/appliance/definitions/systemvmtemplate/postinstall.sh
@@ -21,93 +21,6 @@ ROOTPW=password
 HOSTNAME=systemvm
 CLOUDSTACK_RELEASE=4.5.0
 
-install_packages() {
-  DEBIAN_FRONTEND=noninteractive
-  DEBIAN_PRIORITY=critical
-  local arch=`dpkg --print-architecture`
-
-  # Basic packages
-  apt-get --no-install-recommends -q -y --force-yes install rsyslog logrotate cron chkconfig insserv net-tools ifupdown vim-tiny netbase iptables
-  apt-get --no-install-recommends -q -y --force-yes install openssh-server openssl e2fsprogs dhcp3-client tcpdump socat wget
-  # apt-get --no-install-recommends -q -y --force-yes install grub-legacy
-  apt-get --no-install-recommends -q -y --force-yes install python bzip2 sed gawk diffutils grep gzip less tar telnet ftp rsync traceroute psmisc lsof procps  inetutils-ping iputils-arping httping
-  apt-get --no-install-recommends -q -y --force-yes install dnsutils zip unzip ethtool uuid file iproute acpid virt-what sudo
-
-  # sysstat
-  echo 'sysstat sysstat/enable boolean true' | debconf-set-selections
-  apt-get --no-install-recommends -q -y --force-yes install sysstat
-  # apache
-  apt-get --no-install-recommends -q -y --force-yes install apache2 ssl-cert
-
-  # dnsmasq
-  apt-get --no-install-recommends -q -y --force-yes install dnsmasq dnsmasq-utils
-  # nfs client
-  apt-get --no-install-recommends -q -y --force-yes install nfs-common
-  # nfs irqbalance
-  apt-get --no-install-recommends -q -y --force-yes install irqbalance
-
-  # cifs client
-  apt-get --no-install-recommends -q -y --force-yes install samba-common
-  apt-get --no-install-recommends -q -y --force-yes install cifs-utils
-
-  # vpn stuff
-  apt-get --no-install-recommends -q -y --force-yes install xl2tpd bcrelay ppp ipsec-tools tdb-tools
-  echo "openswan openswan/install_x509_certificate boolean false" | debconf-set-selections
-  echo "openswan openswan/install_x509_certificate seen true" | debconf-set-selections
-  apt-get --no-install-recommends -q -y --force-yes install openswan=1:2.6.37-3
-
-  # xenstore utils
-  apt-get --no-install-recommends -q -y --force-yes install xenstore-utils libxenstore3.0
-  # keepalived and conntrackd for redundant router
-  apt-get --no-install-recommends -q -y --force-yes install keepalived conntrackd ipvsadm libnetfilter-conntrack3 libnl1
-  # ipcalc
-  apt-get --no-install-recommends -q -y --force-yes install ipcalc
-  apt-get update
-  # java
-  apt-get --no-install-recommends -q -y --force-yes install  openjdk-7-jre-headless
-
-  echo "iptables-persistent iptables-persistent/autosave_v4 boolean true" | debconf-set-selections
-  echo "iptables-persistent iptables-persistent/autosave_v6 boolean true" | debconf-set-selections
-  apt-get --no-install-recommends -q -y --force-yes install iptables-persistent
-
-  # Hyperv  kvp daemon - 64bit only
-  if [ "${arch}" == "amd64" ]; then
-    # Download the hv kvp daemon
-    wget http://people.apache.org/~rajeshbattala/hv-kvp-daemon_3.1_amd64.deb
-    dpkg -i hv-kvp-daemon_3.1_amd64.deb
-  fi
-
-  #libraries required for rdp client (Hyper-V)
-  apt-get --no-install-recommends -q -y --force-yes install libtcnative-1 libssl-dev libapr1-dev
-
-  # vmware tools
-  apt-get --no-install-recommends -q -y --force-yes install open-vm-tools
-  # commented installaion of vmware-tools  as we are using the opensource open-vm-tools:
-  # apt-get --no-install-recommends -q -y --force-yes install build-essential linux-headers-`uname -r`
-  # df -h
-  # PREVDIR=$PWD
-  # cd /opt
-  # wget http://people.apache.org/~bhaisaab/cloudstack/VMwareTools-9.2.1-818201.tar.gz
-  # tar xzf VMwareTools-9.2.1-818201.tar.gz
-  # rm VMwareTools-*.tar.gz
-  # cd vmware-tools-distrib
-  # ./vmware-install.pl -d
-  # cd $PREV
-  # rm -fr /opt/vmware-tools-distrib
-  # apt-get -q -y --force-yes purge build-essential
-
-  apt-get --no-install-recommends -q -y --force-yes install haproxy
-
-  #32 bit architecture support:: not required for 32 bit template
-  if [ "${arch}" != "i386" ]; then
-    dpkg --add-architecture i386
-    apt-get update
-    apt-get --no-install-recommends -q -y --force-yes install links:i386 libuuid1:i386
-  fi
-
-  apt-get --no-install-recommends -q -y --force-yes install radvd
-}
-
 fix_vhdutil() {
   wget --no-check-certificate http://download.cloud.com.s3.amazonaws.com/tools/vhd-util -O /bin/vhd-util
   chmod a+x /bin/vhd-util
@@ -176,9 +89,6 @@ do_signature() {
 
 begin=$(date +%s)
 
-echo "*************INSTALLING PACKAGES********************"
-install_packages
-echo "*************DONE INSTALLING PACKAGES********************"
 configure_services
 configure_apache2
 echo "*************DONE SETTING UP SERVICES********************"
