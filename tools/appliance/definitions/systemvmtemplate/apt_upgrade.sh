58ca92ce527e7cee50f67508d49d3aaa6c32a348
Leo Simons <lsimons@schubergphilis.com>
2014-07-21 10:58:57 +0200

    CLOUDSTACK-7143: cleanup apt_upgrade.sh code

--- tools/appliance/definitions/systemvmtemplate/apt_upgrade.sh
+++ tools/appliance/definitions/systemvmtemplate/apt_upgrade.sh
@@ -1,15 +1,21 @@
-# Update the box
+#!/bin/bash
 
-export DEBIAN_FRONTEND=noninteractive
-export DEBIAN_PRIORITY=critical
+set -e
+set -x
 
-add_backports () {
-    sed -i '/backports/d' /etc/apt/sources.list
-    echo 'deb http://http.us.debian.org/debian wheezy-backports main' >> /etc/apt/sources.list
+function add_backports() {
+  sed -i '/backports/d' /etc/apt/sources.list
+  echo 'deb http://http.us.debian.org/debian wheezy-backports main' >> /etc/apt/sources.list
 }
 
-add_backports
+function apt_upgrade() {
+  DEBIAN_FRONTEND=noninteractive
+  DEBIAN_PRIORITY=critical
 
-apt-get -q -y --force-yes update
-apt-get -q -y --force-yes install curl unzip
-apt-get clean
+  add_backports
+
+  apt-get -q -y --force-yes update
+  apt-get -q -y --force-yes upgrade
+}
+
+return 2>/dev/null || apt_upgrade
