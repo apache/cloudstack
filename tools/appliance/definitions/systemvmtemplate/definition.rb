30fe938af9e958972bceaea60275c6b2dfada848
Leo Simons <lsimons@schubergphilis.com>
2014-07-21 13:06:34 +0200

    CLOUDSTACK-7143: tune timeouts to expect about <=~60 min build

--- tools/appliance/definitions/systemvmtemplate/definition.rb
+++ tools/appliance/definitions/systemvmtemplate/definition.rb
@@ -26,7 +26,7 @@ config = {
     :cpu_count => '1',
     :memory_size => '256',
     :disk_size => '2500', :disk_format => 'VDI', :hostiocache => 'off',
-    :iso_download_timeout => '1000',
+    :iso_download_timeout => '1200',
     :boot_wait => '10',
     :boot_cmd_sequence => [
         '<Esc>',
@@ -46,9 +46,9 @@ config = {
         '<Enter>'
     ],
     :kickstart_port => '7122',
-    :kickstart_timeout => '10000',
+    :kickstart_timeout => '1200',
     :kickstart_file => 'preseed.cfg',
-    :ssh_login_timeout => '10000',
+    :ssh_login_timeout => '1200',
     :ssh_user => 'root',
     :ssh_password => 'password',
     :ssh_key => '',
@@ -57,6 +57,7 @@ config = {
     :sudo_cmd => "echo '%p'|sudo -S sh '%f'",
     :shutdown_cmd => 'halt -p',
     :postinstall_files => [
+        # basic minimal vm creation
         'build_time.sh',
         'apt_upgrade.sh',
         'configure_grub.sh',
@@ -64,13 +65,15 @@ config = {
         'configure_login.sh',
         'configure_networking.sh',
         'configure_acpid.sh',
+        # turning it into a systemvm
         'install_systemvm_packages.sh',
-        'configure_systemvm_services.sh',
         'configure_conntrack.sh',
+        'configure_systemvm_services.sh',
+        # cleanup & space-saving
         'cleanup.sh',
         'zerodisk.sh'
     ],
-    :postinstall_timeout => '10000'
+    :postinstall_timeout => '1200'
 }
 
 config.merge! architectures[arch.to_sym]
