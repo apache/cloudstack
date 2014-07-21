4523490d44160b054de9e943f72db1d0ce06054a
Santhosh Edukulla <santhosh.edukulla@gmail.com>
2014-07-21 20:49:03 +0530

    Fixed Coverity Issues reported
    Signed-off-by: Santhosh Edukulla <santhosh.edukulla@gmail.com>

--- engine/storage/src/org/apache/cloudstack/storage/endpoint/DefaultEndPointSelector.java
+++ engine/storage/src/org/apache/cloudstack/storage/endpoint/DefaultEndPointSelector.java
@@ -115,33 +115,21 @@ public class DefaultEndPointSelector implements EndPointSelector {
         // TODO: order by rand() is slow if there are lot of hosts
         sbuilder.append(" ORDER by rand() limit 1");
         String sql = sbuilder.toString();
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
         HostVO host = null;
         TransactionLegacy txn = TransactionLegacy.currentTxn();
-
-        try {
-            pstmt = txn.prepareStatement(sql);
+        try(PreparedStatement pstmt = txn.prepareStatement(sql);) {
             pstmt.setLong(1, poolId);
-            rs = pstmt.executeQuery();
-            while (rs.next()) {
-                long id = rs.getLong(1);
-                host = hostDao.findById(id);
+            try(ResultSet rs = pstmt.executeQuery();) {
+                while (rs.next()) {
+                    long id = rs.getLong(1);
+                    host = hostDao.findById(id);
+                }
+            }catch (SQLException e) {
+                s_logger.warn("can't find endpoint", e);
             }
         } catch (SQLException e) {
             s_logger.warn("can't find endpoint", e);
-        } finally {
-            try {
-                if (rs != null) {
-                    rs.close();
-                }
-                if (pstmt != null) {
-                    pstmt.close();
-                }
-            } catch (SQLException e) {
-            }
         }
-
         if (host == null) {
             return null;
         }
