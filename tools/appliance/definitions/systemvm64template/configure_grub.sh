35b1875226201d5923dea57db64bbd789d9ad908
Leo Simons <lsimons@schubergphilis.com>
2014-07-21 09:55:37 +0200

    CLOUDSTACK-7143: split base.sh into its two functions.

---
+++ tools/appliance/definitions/systemvm64template/configure_grub.sh
@@ -0,0 +1,13 @@
+# Remove 5s grub timeout to speed up booting
+cat <<EOF > /etc/default/grub
+# If you change this file, run 'update-grub' afterwards to update
+# /boot/grub/grub.cfg.
+
+GRUB_DEFAULT=0
+GRUB_TIMEOUT=0
+GRUB_DISTRIBUTOR=`lsb_release -i -s 2> /dev/null || echo Debian`
+GRUB_CMDLINE_LINUX_DEFAULT="quiet"
+GRUB_CMDLINE_LINUX="debian-installer=en_US"
+EOF
+
+update-grub
