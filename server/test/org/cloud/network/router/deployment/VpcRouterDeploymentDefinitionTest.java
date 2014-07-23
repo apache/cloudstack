867353596d79b63c07deaeea89d24853e0396709
Antonio Fornie <afornie@schubergphilis.com>
2014-07-23 07:48:07 -0500

    RouterDeployment and VPC. Improving code and Unit Testing

---
+++ server/test/org/cloud/network/router/deployment/VpcRouterDeploymentDefinitionTest.java
@@ -0,0 +1,52 @@
+package org.cloud.network.router.deployment;
+
+import org.junit.Test;
+
+public class VpcRouterDeploymentDefinitionTest {
+
+    @Test
+    public void testGenerateDeploymentPlan() {
+        // TODO Implement this test
+    }
+
+    @Test
+    public void testLock() {
+        // TODO Implement this test
+    }
+
+    @Test
+    public void testUnlock() {
+        // TODO Implement this test
+    }
+
+    @Test
+    public void testCheckPreconditions() {
+        // TODO Implement this test
+    }
+
+    @Test
+    public void testFindDestinations() {
+        // TODO Implement this test
+    }
+
+    @Test
+    public void testExecuteDeployment() {
+        // TODO Implement this test
+    }
+
+    @Test
+    public void testPlanDeploymentRouters() {
+        // TODO Implement this test
+    }
+
+    @Test
+    public void testDeployVpcRouter() {
+        // TODO Implement this test
+    }
+
+    @Test
+    public void testCreateVpcRouterNetworks() {
+        // TODO Implement this test
+    }
+
+}
