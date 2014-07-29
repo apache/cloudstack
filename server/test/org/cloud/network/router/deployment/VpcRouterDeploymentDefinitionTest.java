6ada3763984e02f49fc0f817b9277fcca1a2dfcd
Antonio Fornie <afornie@schubergphilis.com>
2014-07-29 09:20:33 -0500

    Increasing test coverage for Vpc Deployment

--- server/test/org/cloud/network/router/deployment/VpcRouterDeploymentDefinitionTest.java
+++ server/test/org/cloud/network/router/deployment/VpcRouterDeploymentDefinitionTest.java
@@ -20,24 +20,38 @@ import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNotNull;
 import static junit.framework.Assert.assertNull;
 import static junit.framework.Assert.assertTrue;
+import static org.mockito.Matchers.anyLong;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 
+import com.cloud.deploy.DeployDestination;
 import com.cloud.exception.ConcurrentOperationException;
+import com.cloud.network.dao.PhysicalNetworkDao;
+import com.cloud.network.dao.PhysicalNetworkServiceProviderDao;
 import com.cloud.network.vpc.VpcVO;
 import com.cloud.network.vpc.dao.VpcDao;
+import com.cloud.vm.DomainRouterVO;
 
 public class VpcRouterDeploymentDefinitionTest extends RouterDeploymentDefinitionTestBase {
 
+    private static final String FOR_VPC_ONLY_THE_GIVEN_DESTINATION_SHOULD_BE_USED = "For Vpc only the given destination should be used";
+
     private static final long VPC_ID = 201L;
+    private static final long ZONE_ID = 211L;
 
     @Mock
     protected VpcDao mockVpcDao;
+    @Mock
+    protected PhysicalNetworkDao mockPhNwDao;
+    protected PhysicalNetworkServiceProviderDao mockPhProviderDao;
 
     @Mock
     protected VpcVO mockVpc;
@@ -48,6 +62,7 @@ public class VpcRouterDeploymentDefinitionTest extends RouterDeploymentDefinitio
     protected void initMocks() {
         super.initMocks();
         when(this.mockVpc.getId()).thenReturn(VPC_ID);
+        when(this.mockVpc.getZoneId()).thenReturn(VPC_ID);
     }
 
     @Before
@@ -105,7 +120,52 @@ public class VpcRouterDeploymentDefinitionTest extends RouterDeploymentDefinitio
 
     @Test
     public void testUnlock() {
-        // TODO Implement this test
+        // Prepare
+        this.deployment.tableLockId = VPC_ID;
+
+        // Execute
+        this.deployment.unlock();
+
+        // Assert
+        verify(this.mockVpcDao, times(1)).releaseFromLockTable(VPC_ID);
+    }
+
+    @Test
+    public void testUnlockWithoutLock() {
+        // Prepare
+        this.deployment.tableLockId = null;
+
+        // Execute
+        this.deployment.unlock();
+
+        // Assert
+        verify(this.mockVpcDao, times(0)).releaseFromLockTable(anyLong());
+    }
+
+    @Test
+    public void testFindDestinations() {
+        // Execute
+        List<DeployDestination> foundDestinations = this.deployment.findDestinations();
+        // Assert
+        assertEquals(FOR_VPC_ONLY_THE_GIVEN_DESTINATION_SHOULD_BE_USED,
+                this.deployment.dest, foundDestinations.get(0));
+        assertEquals(FOR_VPC_ONLY_THE_GIVEN_DESTINATION_SHOULD_BE_USED,
+                1, foundDestinations.size());
+    }
+
+    @Test
+    public void testGetNumberOfRoutersToDeploy() {
+        assertEquals("If there are no routers, it should deploy one",
+                1, this.deployment.getNumberOfRoutersToDeploy());
+        this.deployment.routers.add(mock(DomainRouterVO.class));
+        assertEquals("If there is already a router found, there is no need to deploy more",
+                0, this.deployment.getNumberOfRoutersToDeploy());
+    }
+
+    @Test
+    public void testPrepareDeployment() {
+        assertTrue("There are no preconditions for Vpc Deployment, thus it should always pass",
+                this.deployment.prepareDeployment());
     }
 
     @Test
@@ -115,11 +175,6 @@ public class VpcRouterDeploymentDefinitionTest extends RouterDeploymentDefinitio
 
     @Test
     public void testCheckPreconditions() {
-        // TODO Implement this test
-    }
-
-    @Test
-    public void testFindDestinations() {
         // TODO Implement this test
     }
 
