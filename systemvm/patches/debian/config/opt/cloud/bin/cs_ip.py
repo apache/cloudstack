efaaea095ca40cdcc4d8b332672154510fbf5ced
Ian Southam <isoutham@schubergphilis.com>
2014-07-30 14:10:03 +0200

    Corrected a hole in my logic

--- systemvm/patches/debian/config/opt/cloud/bin/cs_ip.py
+++ systemvm/patches/debian/config/opt/cloud/bin/cs_ip.py
@@ -1,18 +1,4 @@
 from pprint import pprint
-#[{u'accountId': 2,
-  #u'add': True,
-  #u'broadcastUri': u'vlan://untagged',
-  #u'firstIP': False,
-  #u'networkRate': 200,
-  #u'newNic': False,
-  #u'nicDevId': 1,
-  #u'oneToOneNat': False,
-  #u'publicIp': u'10.0.2.102',
-  #u'sourceNat': True,
-  #u'trafficType': u'Public',
-  #u'vifMacAddress': u'06:f6:5e:00:00:03',
-  #u'vlanGateway': u'10.0.2.1',
-  #u'vlanNetmask': u'255.255.255.0'}]
 
 def merge(dbag, ip):
     added = False
