cf4cdde29133368dbe328679237c2dabff4bf434
Leo Simons <lsimons@schubergphilis.com>
2014-08-07 14:46:45 +0200

    Fix update_config.py use of subprocess.

--- systemvm/patches/debian/config/opt/cloud/bin/update_config.py
+++ systemvm/patches/debian/config/opt/cloud/bin/update_config.py
@@ -4,7 +4,7 @@ import sys
 from merge import loadQueueFile
 import logging
 import subprocess
-from subprocess import PIPE
+from subprocess import PIPE, STDOUT
 
 logging.basicConfig(filename='/var/log/cloud.log',level=logging.DEBUG, format='%(asctime)s %(message)s')
 
@@ -19,12 +19,12 @@ qf.load(None)
 
 # Converge
 run = subprocess.Popen(["/opt/cloud/bin/configure.py"],
-                           stdout=PIPE, stderr=PIPE)
-result = run.wait()
+                           stdout=PIPE, stderr=STDOUT)
+stdout, stderr = run.communicate()
 
-if (result != 0):
-    print run.stderr
+if run.returncode:
+    print stdout
 else:
     print "Convergence is achieved - you have been assimilated!"
 
-sys.exit(result)
+sys.exit(run.returncode)
