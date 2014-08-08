74847dce47be2a40c8068adc71ad45b6b8460c6b
Jessica Wang <jessicawang@apache.org>
2014-08-07 18:54:09 -0700

    UI > dialog widget > text field > set default value > pass context to function(){}.

--- ui/scripts/ui/dialog.js
+++ ui/scripts/ui/dialog.js
@@ -566,7 +566,7 @@
                     }).appendTo($value);
 
                     if (field.defaultValue) {
-                        $input.val(strOrFunc(field.defaultValue));
+                        $input.val(strOrFunc(field.defaultValue, args.context));
                     }
                     if (field.id) {
                         $input.attr('id', field.id);
