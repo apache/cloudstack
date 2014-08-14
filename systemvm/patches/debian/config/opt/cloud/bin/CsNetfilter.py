c2f6f806ad93c331260c022c8aaef5fccfb6d59f
Ian Southam <isoutham@schubergphilis.com>
2014-08-14 16:32:17 +0200

    First part of some serious refactoring
    The problem is that teh convergence model does not works as it currently is
    Firewall rules will get added but not deleted
    
    This builds a table of firewall rules that need to be present
    The new CsNetfilter class will then do:
    1.  Add rules that are not present
    2.  Delete any that were not explicitly in the add list
    
    WIP

--- systemvm/patches/debian/config/opt/cloud/bin/CsNetfilter.py
+++ systemvm/patches/debian/config/opt/cloud/bin/CsNetfilter.py
@@ -1,4 +1,77 @@
+import CsHelper
+from pprint import pprint
 
-class Netfilter(object):
+class CsChain(object):
 
     def __init__(self):
+        self.chain = {}
+
+    def add(self, table, chain):
+        if not table in self.chain.keys():
+            self.chain.setdefault(table, []).append( chain )
+        else:
+            self.chain[table].append(chain)
+
+    def get(self, table):
+        return self.chain[table]
+
+class CsTable(object):
+
+    def __init__(self):
+        self.table = []
+        self.last_added = ''
+
+    def add(self, name):
+        if not name in self.table:
+            self.table.append(name)
+            self.last_added = name
+
+    def get(self):
+        return self.table
+
+    def last(self):
+        return self.last_added
+
+class CsNetfilters(object):
+
+    def __init__(self):
+        self.rules = []
+        self.table = CsTable()
+        self.chain = CsChain()
+        self.get_all_rules()
+
+    def get_all_rules(self):
+        for i in CsHelper.execute("iptables-save"):
+            if i.startswith('*'): # Table
+                self.table.add(i[1:])
+            if i.startswith(':'): # Chain
+                self.chain.add(self.table.last(), i[1:].split(' ')[0])
+            if i.startswith('-A'): # Rule
+                rule = CsNetfilter()
+                rule.parse(i)
+                self.save(rule)
+
+    def save(self,rule):
+        self.rules.append(rule)
+
+    def get(self):
+        return self.rules
+
+    def hasTable(self, table):
+        return table in self.table.get()
+
+    def hasChain(self, table, chain):
+        return chain in self.chain.get(table)
+
+class CsNetfilter(object):
+
+    def parse(self, rule):
+        rule.replace('! -', '!_-')
+        bits = rule.split(' ')
+        self.rule = dict(zip(bits[0::2], bits[1::2])).iteritems()
+
+if __name__ == "__main__":
+
+    t = CsNetfilters()
+    print t.hasTable('mangle');
+    print t.hasChain('mangle', 'PREROUTING');
