45cb3c6d3e406614d5cf21994be07a54c7b1671c
Wilder Rodrigues <wrodrigues@schubergphilis.com>
2014-08-18 17:01:54 +0200

    Replacing the old [Vpc}VirtualNetworkApplianceManager by the new ones; Implement the missing commands related to DHCP PV Lan and DHCP SubNet

--- server/src/org/apache/cloudstack/network/topology/NetworkTopologyVisitor.java
+++ server/src/org/apache/cloudstack/network/topology/NetworkTopologyVisitor.java
@@ -51,6 +51,6 @@ public abstract class NetworkTopologyVisitor {
     public abstract boolean visit(VpnRules userdata) throws ResourceUnavailableException;
     public abstract boolean visit(PrivateGatewayRules userdata) throws ResourceUnavailableException;
     public abstract boolean visit(DhcpPvlanRules vpn) throws ResourceUnavailableException;
-    public abstract boolean visit(DhcpSubNetRules vpn) throws ResourceUnavailableException;
+    public abstract boolean visit(DhcpSubNetRules subnet) throws ResourceUnavailableException;
     public abstract boolean visit(NicPlugInOutRules nicPlugInOutRules) throws ResourceUnavailableException;
 }
