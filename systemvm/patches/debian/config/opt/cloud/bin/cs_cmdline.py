4de632ec9291bc5c5e26eb8a4ac30af0d9192f3b
Ian Southam <isoutham@schubergphilis.com>
2014-08-19 17:32:21 +0200

    Acl code linked in Dhcp code Test file for dhcp entry

--- systemvm/patches/debian/config/opt/cloud/bin/cs_cmdline.py
+++ systemvm/patches/debian/config/opt/cloud/bin/cs_cmdline.py
@@ -1,5 +1,9 @@
 from pprint import pprint
 
 def merge(dbag, cmdline):
-    dbag['config'] = cmdline
+    if 'redundant' in cmdline['cmd_line']:
+        cmdline['cmd_line']['redundant'] = "true"
+    else:
+        cmdline['cmd_line']['redundant'] = "false"
+    dbag['config'] = cmdline['cmd_line']
     return dbag
