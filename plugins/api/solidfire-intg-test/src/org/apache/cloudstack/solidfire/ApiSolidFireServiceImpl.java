0e79cd1172e4340b957b0328354761e8a9305609
Mike Tutkowski <mike.tutkowski@solidfire.com>
2014-08-28 23:14:42 -0600

    Minor changes to SolidFire automation-related code

--- plugins/api/solidfire-intg-test/src/org/apache/cloudstack/solidfire/ApiSolidFireServiceImpl.java
+++ plugins/api/solidfire-intg-test/src/org/apache/cloudstack/solidfire/ApiSolidFireServiceImpl.java
@@ -24,7 +24,7 @@ import javax.ejb.Local;
 import javax.inject.Inject;
 import javax.naming.ConfigurationException;
 
-import org.apache.log4j.Logger;
+// import org.apache.log4j.Logger;
 import org.apache.cloudstack.acl.APIChecker;
 import org.apache.cloudstack.storage.datastore.util.SolidFireUtil;
 import org.apache.cloudstack.api.command.user.solidfire.GetSolidFireAccountIdCmd;
@@ -55,14 +55,11 @@ import com.cloud.utils.component.AdapterBase;
 @Component
 @Local(value = APIChecker.class)
 public class ApiSolidFireServiceImpl extends AdapterBase implements APIChecker, ApiSolidFireService {
-    private static final Logger s_logger = Logger.getLogger(ApiSolidFireServiceImpl.class);
+    // private static final Logger s_logger = Logger.getLogger(ApiSolidFireServiceImpl.class);
 
-    @Inject
-    private AccountDetailsDao _accountDetailsDao;
-    @Inject
-    private DataStoreProviderManager _dataStoreProviderMgr;
-    @Inject
-    private ClusterDetailsDao _clusterDetailsDao;
+    @Inject private AccountDetailsDao _accountDetailsDao;
+    @Inject private DataStoreProviderManager _dataStoreProviderMgr;
+    @Inject private ClusterDetailsDao _clusterDetailsDao;
 
     @Override
     public boolean configure(String name, Map<String, Object> params) throws ConfigurationException {
@@ -91,7 +88,7 @@ public class ApiSolidFireServiceImpl extends AdapterBase implements APIChecker, 
                 primaryStoreDriver = (PrimaryDataStoreDriver)storeDriver;
             }
         }
-        catch(InvalidParameterValueException e) {
+        catch (InvalidParameterValueException e) {
             throw new InvalidParameterValueException("Invalid Storage Driver Type");
         }
 
