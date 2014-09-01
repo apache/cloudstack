a58f098ec01e6b6aa963171250914fe5a6394746
Ian Duffy <duffy@apache.org>
2014-09-01 10:56:44 +0100

    [CLOUDSTACK-6115] Investigate the use of TravisCI for CloudStack integration testing
    (cherry picked from commit 26069aa3776ea4b634fb9d614062caa595bc0fa0)
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>

---
+++ tools/travis/before_script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# This script should be used to bring up the environment.
+#
+
+export CATALINA_BASE=/opt/tomcat
+export CATALINA_HOME=/opt/tomcat
+export M2_HOME="/usr/local/maven-3.2.1/"
+export MAVEN_OPTS="-Xmx1024m -XX:MaxPermSize=500m"
+
+mvn -Dsimulator -pl :cloud-client-ui jetty:run 2>&1 > /dev/null &
+
+while ! nc -vz localhost 8096 2>&1 > /dev/null; do sleep 10; done
+python -m marvin.deployDataCenter -i setup/dev/advanced.cfg 2>&1 || true
---
+++ tools/travis/script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#
+# This script should be responsible for bring up the
+# test environment and executing the tests.
+#
