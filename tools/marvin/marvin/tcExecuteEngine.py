87b5e10bf8f6d2d4dde64b748057307373c132a9
Santhosh Edukulla <santhosh.edukulla@gmail.com>
2014-09-01 17:55:51 +0530

    Adding first cut(draft) for marvincli
    Signed-off-by: Santhosh Edukulla <santhosh.edukulla@gmail.com>

--- tools/marvin/marvin/tcExecuteEngine.py
+++ tools/marvin/marvin/tcExecuteEngine.py
@@ -24,50 +24,53 @@ from functools import partial
 
 class TestCaseExecuteEngine(object):
 
-    def __init__(self, testclient, config, tc_logger=None, debug_stream=None):
+    def __init__(self, testclient, config, tc_logger=None,
+                 debug_stream=sys.stdout):
         """
-        Initialize the testcase execution engine, just the basics here
-        @var testcaseLogFile: client log file
-        @var testResultLogFile: summary report file
+        Initialize the testcase execution engine
         """
-        self.testclient = testclient
-        self.config = config
-        self.tcRunLogger = tc_logger
-        self.debugStream = debug_stream
-        self.loader = unittest.loader.TestLoader()
-        self.suite = None
+        self.__testClient = testclient
+        self.__parsedConfig = config
+        self.__tcRunLogger = tc_logger
+        self.__debugStream = debug_stream
+        self.__loader = unittest.loader.TestLoader()
+        self.__suite = None
 
-    def loadTestsFromDir(self, testDirectory):
+    def loadTestsFromDir(self, test_directory):
         """ Load the test suites from a package with multiple test files """
-        self.suite = self.loader.discover(testDirectory)
-        self.injectTestCase(self.suite)
+        self.__suite = self.__loader.discover(test_directory)
+        self.injectTestCase(self.__suite)
 
     def loadTestsFromFile(self, file_name):
         """ Load the tests from a single script/module """
         if os.path.isfile(file_name):
-            self.suite = self.loader.discover(os.path.dirname(file_name),
-                                              os.path.basename(file_name))
-            self.injectTestCase(self.suite)
+            self.__suite = self.__loader.discover(os.path.dirname(file_name),
+                                                  os.path.basename(file_name))
+            self.injectTestCase(self.__suite)
 
-    def injectTestCase(self, testSuites):
-        for test in testSuites:
+    def injectTestCase(self, test_suites):
+        for test in test_suites:
             if isinstance(test, unittest.BaseTestSuite):
                 self.injectTestCase(test)
             else:
                 # inject testclient and logger into each unittest
-                self.tcRunLogger.name = test.__str__()
-                setattr(test, "testClient", self.testclient)
-                setattr(test, "config", self.config)
-                setattr(test, "debug", self.tcRunLogger.debug)
-                setattr(test.__class__, "clstestclient", self.testclient)
+                setattr(test, "debug", self.__tcRunLogger.debug)
+                setattr(test, "info", self.__tcRunLogger.info)
+                setattr(test, "warn", self.__tcRunLogger.warning)
+                setattr(test, "error", self.__tcRunLogger.error)
+                setattr(test, "clstestclient", self.__testClient)
+                setattr(test, "testClient", self.__testClient)
+                setattr(test, "config", self.__parsedConfig)
                 if hasattr(test, "user"):
-                    # attribute when test is entirely executed as user
-                    self.testclient.\
-                        getUserApiClient(test.UserName,
-                                         test.DomainName,
-                                         test.AcctType)
+                    # when the class-level attr applied. all test runs as
+                    # 'user'
+                    self.__testClient.getUserApiClient(test.UserName,
+                                                       test.DomainName,
+                                                       test.AcctType)
 
     def run(self):
-        if self.suite:
-            unittest.TextTestRunner(stream=self.debugStream,
-                                    verbosity=2).run(self.suite)
+        if self.__suite:
+            print "\n==== Test Suite :%s Started ====" % (str(self.__suite))
+            unittest.TextTestRunner(stream=self.__debugStream,
+                                    verbosity=2).run(self.__suite)
+            print "\n==== Test Suite :%s Finished ====" % (str(self.__suite))
