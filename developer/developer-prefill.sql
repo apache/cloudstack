c45f73cc21a49e22a2947841b2017da380b392ef
Rohit Yadav <rohit.yadav@shapeblue.com>
2014-09-15 14:47:32 +0200

    CLOUDSTACK-6738: Add configs in developer prefill to avoid restart mgmt server
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>

--- developer/developer-prefill.sql
+++ developer/developer-prefill.sql
@@ -48,7 +48,23 @@ INSERT INTO `cloud`.`configuration` (category, instance, component, name, value)
 
 INSERT INTO `cloud`.`configuration` (category, instance, component, name, value)
             VALUES ('Advanced', 'DEFAULT', 'management-server',
+            'secstorage.allowed.internal.sites', '0.0.0.0/0');
+
+INSERT INTO `cloud`.`configuration` (category, instance, component, name, value)
+            VALUES ('Advanced', 'DEFAULT', 'management-server',
             'account.cleanup.interval', '60');
+
+INSERT INTO `cloud`.`configuration` (category, instance, component, name, value)
+            VALUES ('Advanced', 'DEFAULT', 'management-server',
+            'expunge.delay', '60');
+
+INSERT INTO `cloud`.`configuration` (category, instance, component, name, value)
+            VALUES ('Advanced', 'DEFAULT', 'management-server',
+            'expunge.interval', '60');
+
+INSERT INTO `cloud`.`configuration` (category, instance, component, name, value)
+            VALUES ('Advanced', 'DEFAULT', 'management-server',
+            'system.vm.use.local.storage', 'true');
 
 -- Add developer configuration entry; allows management server to be run as a user other than "cloud"
 INSERT INTO `cloud`.`configuration` (category, instance, component, name, value)
