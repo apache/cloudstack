4f297efef5627f04348a6b9ac550bef2364cb661
Gaurav Aradhye <gaurav.aradhye@clogeny.com>
2014-10-09 15:14:30 +0530

    CLOUDSTACK-7692: Fixed template register issue in test_project_usage.py
    Signed-off-by: SrikanteswaraRao Talluri <talluri@apache.org>

--- test/integration/component/test_project_usage.py
+++ test/integration/component/test_project_usage.py
@@ -712,6 +712,9 @@ class TestTemplateUsage(cloudstackTestCase):
                             domainid=cls.domain.id
                             )
             cls._cleanup.append(cls.account)
+            cls.userapiclient = cls.testClient.getUserApiClient(
+                                    UserName=cls.account.name,
+                                    DomainName=cls.account.domain)
             cls.services["account"] = cls.account.name
 
             cls.project = Project.create(
@@ -789,7 +792,7 @@ class TestTemplateUsage(cloudstackTestCase):
 
         #Create template from Virtual machine and Volume ID
         self.template = Template.create(
-                                self.apiclient,
+                                self.userapiclient,
                                 self.services["templates"],
                                 self.volume.id,
                                 projectid=self.project.id
