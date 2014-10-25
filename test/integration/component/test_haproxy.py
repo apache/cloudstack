540687c3924e9e563c09f9fc100e90c7cbb01794
Chandan Purushothama <Chandan.Purushothama@citrix.com>
2014-10-25 06:57:05 -0700

    CLOUDSTACK-7786: Fixed the script 'test_haproxy.py' to be run only on hardware

--- test/integration/component/test_haproxy.py
+++ test/integration/component/test_haproxy.py
@@ -397,7 +397,7 @@ class TestHAProxyStickyness(cloudstackTestCase):
                                     (e, ip_addr))
         return hostnames
 
-    @attr(tags=["advanced", "advancedns"])
+    @attr(tags=["advanced", "advancedns"],required_hardware="true")
     @attr(speed="slow")
     def test_01_create_sticky_policy_default_values(self):
         """Test Configure stickiness policies with default values"""
@@ -431,7 +431,7 @@ class TestHAProxyStickyness(cloudstackTestCase):
             self.delete_Stickiness_policy(policy, lb_rule)
         return
 
-    @attr(tags=["advanced", "advancedns"])
+    @attr(tags=["advanced", "advancedns"],required_hardware="true")
     @attr(speed="slow")
     def test_02_create_sticky_policy_custom_values(self):
         """Test Configure stickiness policies with custom values"""
@@ -519,7 +519,7 @@ class TestHAProxyStickyness(cloudstackTestCase):
 
         return
 
-    @attr(tags=["advanced", "advancedns"])
+    @attr(tags=["advanced", "advancedns"],required_hardware="true")
     @attr(speed="slow")
     def test_04_delete_lb_rule(self):
         """Test LB rule before/after stickiness policy creation"""
@@ -582,7 +582,7 @@ class TestHAProxyStickyness(cloudstackTestCase):
                     LoadBalancerRule.list(self.apiclient, id=lb_rule.id)
         return
 
-    @attr(tags=["advanced", "advancedns"])
+    @attr(tags=["advanced", "advancedns"],required_hardware="true")
     @attr(speed="slow")
     def test_05_error_alerts_after_create(self):
         """Test error/alerts after creating stickiness policy"""
@@ -639,7 +639,7 @@ class TestHAProxyStickyness(cloudstackTestCase):
                     "Create/update/delete should not produce any alert/error")
         return
 
-    @attr(tags=["advanced", "advancedns"])
+    @attr(tags=["advanced", "advancedns"],required_hardware="true")
     @attr(speed="slow")
     def test_06_release_ip(self):
         """Test release public IP with stickiness policy"""
