d8b46b81b946672ab6836f13ea0389cee63321d8
Anshul Gangwar <anshul.gangwar@citrix.com>
2014-10-28 11:49:30 +0530

    CLOUDSTACK-7767: fixed events are not generated for snapshot creation

--- server/src/com/cloud/storage/VolumeApiServiceImpl.java
+++ server/src/com/cloud/storage/VolumeApiServiceImpl.java
@@ -1828,6 +1828,7 @@ public class VolumeApiServiceImpl extends ManagerBase implements VolumeApiServic
     }
 
     @Override
+    @ActionEvent(eventType = EventTypes.EVENT_SNAPSHOT_CREATE, eventDescription = "taking snapshot", async = true)
     public Snapshot takeSnapshot(Long volumeId, Long policyId, Long snapshotId, Account account, boolean quiescevm) throws ResourceAllocationException {
 
         VolumeInfo volume = volFactory.getVolume(volumeId);
@@ -1914,6 +1915,7 @@ public class VolumeApiServiceImpl extends ManagerBase implements VolumeApiServic
     }
 
     @Override
+    @ActionEvent(eventType = EventTypes.EVENT_SNAPSHOT_CREATE, eventDescription = "allocating snapshot", create = true)
     public Snapshot allocSnapshot(Long volumeId, Long policyId) throws ResourceAllocationException {
         Account caller = CallContext.current().getCallingAccount();
 
