67ee33ff8a6d2873ab9b0a01159ad1f328a69b89
Hugo Trippaers <htrippaers@schubergphilis.com>
2014-11-17 15:33:40 +0100

    More pep8 checks and fixes

--- systemvm/test/python/runtests.sh
+++ systemvm/test/python/runtests.sh
@@ -5,4 +5,7 @@
 export PYTHONPATH="../../patches/debian/config/opt/cloud/bin/"
 export PYTHONDONTWRITEBYTECODE=False
 
+pep8 --max-line-length=179 --exclude=monitorServices.py,baremetal-vr.py `find ../../patches -name \*.py`
+pep8 --max-line-length=179 *py
+
 nosetests .
