638da54fcee53cea305c0e730fef28c914298731
Daan Hoogland <daan@onecht.net>
2014-11-18 12:12:00 +0100

    CID-1254833 refactor condition to include null-case

--- utils/src/com/cloud/utils/net/NetUtils.java
+++ utils/src/com/cloud/utils/net/NetUtils.java
@@ -1317,11 +1317,8 @@ public class NetUtils {
         BigInteger startInt = convertIPv6AddressToBigInteger(start);
         BigInteger endInt = convertIPv6AddressToBigInteger(end);
         if (endInt != null) {
-            if (startInt != null)
-            {
-                if(startInt.compareTo(endInt) > 0) {
-                    return null;
-                }
+            if (startInt == null || startInt.compareTo(endInt) > 0) {
+                return null;
             }
             return endInt.subtract(startInt).add(BigInteger.ONE);
         }
