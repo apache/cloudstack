7de9d2f002d190808f3a168d297554a0863b38e6
Min Chen <min.chen@citrix.com>
2014-11-20 09:44:55 -0800

    Revert "BUG-ID: CS-27662: ListTags API is ignoring the resourceID and displaying"
    This reverts commit 3014fd00895a8467e627b09845fb5de4fdc373b5.

--- server/src/com/cloud/api/query/QueryManagerImpl.java
+++ server/src/com/cloud/api/query/QueryManagerImpl.java
@@ -653,8 +653,9 @@ public class QueryManagerImpl extends ManagerBase implements QueryService {
         sb.and("value", sb.entity().getValue(), SearchCriteria.Op.EQ);
 
         if (resourceId != null) {
-            sb.and("resourceId", sb.entity().getResourceId(), SearchCriteria.Op.EQ);
-            sb.and("resourceUuid", sb.entity().getResourceUuid(), SearchCriteria.Op.EQ);
+            sb.and().op("resourceId", sb.entity().getResourceId(), SearchCriteria.Op.EQ);
+            sb.or("resourceUuid", sb.entity().getResourceUuid(), SearchCriteria.Op.EQ);
+            sb.cp();
         }
 
         sb.and("resourceType", sb.entity().getResourceType(), SearchCriteria.Op.EQ);
@@ -674,13 +675,8 @@ public class QueryManagerImpl extends ManagerBase implements QueryService {
         }
 
         if (resourceId != null) {
-            try {
-                long rid = Long.parseLong(resourceId);
-                sc.setParameters("resourceId", rid);
-            } catch (NumberFormatException ex) {
-                // internal id instead of resource id is passed
-                sc.setParameters("resourceUuid", resourceId);
-            }
+            sc.setParameters("resourceId", resourceId);
+            sc.setParameters("resourceUuid", resourceId);
         }
 
         if (resourceType != null) {
