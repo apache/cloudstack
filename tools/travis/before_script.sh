df9fe7817c17b045f2f293bd238bf4982eebd7b5
Rohit Yadav <rohit.yadav@shapeblue.com>
2014-11-21 20:02:21 +0530

    travis: run jetty using IPv4 stack only
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>

--- tools/travis/before_script.sh
+++ tools/travis/before_script.sh
@@ -24,7 +24,7 @@ export CATALINA_HOME=/opt/tomcat
 export M2_HOME="/usr/local/maven-3.2.1/"
 export MAVEN_OPTS="-Xmx1024m -XX:MaxPermSize=500m"
 
-mvn -Dsimulator -pl :cloud-client-ui jetty:run 2>&1 > /dev/null &
+mvn -Dsimulator -Djava.net.preferIPv4Stack=true -pl :cloud-client-ui jetty:run 2>&1 > /dev/null &
 
 while ! nc -vz localhost 8096 2>&1 > /dev/null; do sleep 10; done
 python -m marvin.deployDataCenter -i setup/dev/advanced.cfg 2>&1 || true
