cdfdda205126bce86a4411264aca4347cfb6e254
Harikrishna Patnala <harikrishna.patnala@citrix.com>
2014-11-27 12:41:22 +0530

    CLOUDSTACK-6075: Increase the ram size for router service offering
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>
    (cherry picked from commit 488c17858f17f548d907cd72df54e0abdfd439b2)
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>

--- server/src/com/cloud/configuration/Config.java
+++ server/src/com/cloud/configuration/Config.java
@@ -1443,7 +1443,7 @@ public enum Config {
             "SAML2 IDP Metadata Downloading and parsing etc. activity timeout in milliseconds",
             null),
     //NetworkType("Hidden", ManagementServer.class, String.class, "network.type", "vlan", "The type of network that this deployment will use.", "vlan,direct"),
-    RouterRamSize("Hidden", NetworkOrchestrationService.class, Integer.class, "router.ram.size", "128", "Default RAM for router VM (in MB).", null),
+    RouterRamSize("Hidden", NetworkOrchestrationService.class, Integer.class, "router.ram.size", "256", "Default RAM for router VM (in MB).", null),
 
     DefaultPageSize("Advanced", ManagementServer.class, Long.class, "default.page.size", "500", "Default page size for API list* commands", null),
 
