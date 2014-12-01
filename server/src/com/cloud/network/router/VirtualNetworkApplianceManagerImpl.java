5241d0faf8fb23e00c043527698b0ee13397fd2d
Rohit Yadav <rohit.yadav@shapeblue.com>
2014-12-01 16:30:15 +0530

    CLOUDSTACK-7903: Fix build regression from previous fix
    The previous fix tried to access StatsCollector from UsageManagerImpl
    which is not possible due to dependency cycle.
    
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>
    (cherry picked from commit 1e0880cbabfb2c8edbd6a5a35b9417b2f3e6f681)
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>
    
    Conflicts:
    	server/src/com/cloud/network/router/VirtualNetworkApplianceManagerImpl.java

--- server/src/com/cloud/network/router/VirtualNetworkApplianceManagerImpl.java
+++ server/src/com/cloud/network/router/VirtualNetworkApplianceManagerImpl.java
@@ -178,7 +178,6 @@ import com.cloud.offerings.NetworkOfferingVO;
 import com.cloud.offerings.dao.NetworkOfferingDao;
 import com.cloud.resource.ResourceManager;
 import com.cloud.server.ConfigurationServer;
-import com.cloud.server.StatsCollector;
 import com.cloud.service.ServiceOfferingVO;
 import com.cloud.service.dao.ServiceOfferingDao;
 import com.cloud.storage.GuestOSVO;
@@ -261,6 +260,7 @@ import org.apache.cloudstack.framework.jobs.AsyncJobManager;
 import org.apache.cloudstack.framework.jobs.impl.AsyncJobVO;
 import org.apache.cloudstack.managed.context.ManagedContextRunnable;
 import org.apache.cloudstack.utils.identity.ManagementServerNode;
+import org.apache.cloudstack.utils.usage.UsageUtils;
 import org.apache.log4j.Logger;
 
 import javax.ejb.Local;
@@ -808,9 +808,9 @@ VirtualMachineGuru, Listener, Configurable, StateListener<State, VirtualMachine.
             _dailyOrHourly = false;
         }
 
-        if (_usageAggregationRange < StatsCollector.USAGE_AGGREGATION_RANGE_MIN) {
-            s_logger.warn("Usage stats job aggregation range is to small, using the minimum value of " + StatsCollector.USAGE_AGGREGATION_RANGE_MIN);
-            _usageAggregationRange = StatsCollector.USAGE_AGGREGATION_RANGE_MIN;
+        if (_usageAggregationRange < UsageUtils.USAGE_AGGREGATION_RANGE_MIN) {
+            s_logger.warn("Usage stats job aggregation range is to small, using the minimum value of " + UsageUtils.USAGE_AGGREGATION_RANGE_MIN);
+            _usageAggregationRange = UsageUtils.USAGE_AGGREGATION_RANGE_MIN;
         }
 
         _networkStatsUpdateExecutor.scheduleAtFixedRate(new NetworkStatsUpdateTask(), (endDate - System.currentTimeMillis()), (_usageAggregationRange * 60 * 1000),
