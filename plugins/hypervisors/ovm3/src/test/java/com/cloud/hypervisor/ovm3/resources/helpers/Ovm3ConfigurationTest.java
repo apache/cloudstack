8482730b44f78c0b75a91e42c4a080113b27bec1
Funs <funs@barred.org>
2014-12-15 11:31:42 +0100

    checkstyle

--- plugins/hypervisors/ovm3/src/test/java/com/cloud/hypervisor/ovm3/resources/helpers/Ovm3ConfigurationTest.java
+++ plugins/hypervisors/ovm3/src/test/java/com/cloud/hypervisor/ovm3/resources/helpers/Ovm3ConfigurationTest.java
@@ -15,48 +15,48 @@ public class Ovm3ConfigurationTest {
     private static Map<String, Object> params;
     static {
         params = new HashMap<String, Object>();
-        params.put("agentusername","oracle");
-        params.put("xenserver.heartbeat.interval","60");
-        params.put("public.network.device","xenbr0");
-        params.put("private.network.device","xenbr0");
-        params.put("agentpassword","unknown");
-        params.put("secondary.storage.vm","false");
-        params.put("Hypervisor.Version","4.1.3OVM");
-        params.put("Host.OS","Oracle VM Server");
-        params.put("ipaddress","192.168.1.65");
-        params.put("ovm3pool","true");
-        params.put("password","unknown");
-        params.put("username","root");
-        params.put("pool","a9c1219d-817d-4242-b23e-2607801c79d5");
-        params.put("ismaster","false");
-        params.put("storage.network.device","xenbr0");
-        params.put("Host.OS.Version","5.7");
-        params.put("xenserver.nics.max","7");
-        params.put("agentVersion","3.2.1-183");
-        params.put("router.aggregation.command.each.timeout","3");
-        params.put("pod","1");
-        params.put("max.template.iso.size","50");
-        params.put("host","ovm-2");
-        params.put("com.cloud.network.Networks.RouterPrivateIpStrategy","DcGlobal");
-        params.put("agentport","8899");
-        params.put("Host.OS.Kernel.Version","2.6.39-300.22.2.el5uek");
-        params.put("migratewait","3600");
-        params.put("storage.network.device1","xenbr0");
-        params.put("ovm3cluster","false");
-        params.put("ip","192.168.1.65");
-        params.put("guid","19e5f1e7-22f4-3b6d-8d41-c82f89c65295");
-        params.put("ovm3vip","192.168.1.230");
-        params.put("hasmaster","true");
-        params.put("guest.network.device","xenbr0");
-        params.put("cluster","1");
-        params.put("xenserver.heartbeat.timeout","120");
-        params.put("zone","1");
+        params.put("agentusername", "oracle");
+        params.put("xenserver.heartbeat.interval", "60");
+        params.put("public.network.device", "xenbr0");
+        params.put("private.network.device", "xenbr0");
+        params.put("agentpassword", "unknown");
+        params.put("secondary.storage.vm", "false");
+        params.put("Hypervisor.Version", "4.1.3OVM");
+        params.put("Host.OS", "Oracle VM Server");
+        params.put("ipaddress", "192.168.1.65");
+        params.put("ovm3pool", "true");
+        params.put("password", "unknown");
+        params.put("username", "root");
+        params.put("pool", "a9c1219d-817d-4242-b23e-2607801c79d5");
+        params.put("ismaster", "false");
+        params.put("storage.network.device", "xenbr0");
+        params.put("Host.OS.Version", "5.7");
+        params.put("xenserver.nics.max", "7");
+        params.put("agentVersion", "3.2.1-183");
+        params.put("router.aggregation.command.each.timeout", "3");
+        params.put("pod", "1");
+        params.put("max.template.iso.size", "50");
+        params.put("host", "ovm-2");
+        params.put("com.cloud.network.Networks.RouterPrivateIpStrategy",
+                "DcGlobal");
+        params.put("agentport", "8899");
+        params.put("Host.OS.Kernel.Version", "2.6.39-300.22.2.el5uek");
+        params.put("migratewait", "3600");
+        params.put("storage.network.device1", "xenbr0");
+        params.put("ovm3cluster", "false");
+        params.put("ip", "192.168.1.65");
+        params.put("guid", "19e5f1e7-22f4-3b6d-8d41-c82f89c65295");
+        params.put("ovm3vip", "192.168.1.230");
+        params.put("hasmaster", "true");
+        params.put("guest.network.device", "xenbr0");
+        params.put("cluster", "1");
+        params.put("xenserver.heartbeat.timeout", "120");
+        params.put("zone", "1");
     }
 
     @Test
     public void testConfigLoad() throws ConfigurationException {
         ovm3config = new Ovm3Configuration(params);
-        results.basicStringTest(ovm3config.getAgentHostname(), "ovm-2");        
+        results.basicStringTest(ovm3config.getAgentHostname(), "ovm-2");
     }
-
 }
