81571b2eb11041cfe20105e59453c5294ca35d2c
Sateesh Chodapuneedi <sateesh@apache.org>
2015-01-14 08:13:10 +0530

    CLOUDSTACK-4787 Allow selection of scsi controller type in vSphere
        commit #7
        So far only 1 controller (scsi or ide) is supported in Cloudstack for ide or
        scsi, this is existing limitation. Added support for 2nd IDE controller. Support adding IDE
        virtual disk to VM. Also added check if VM is running as IDE virtual disk cannot be attached
        to VM if VM is runnning.If user detaches a virtual disk on lower unit number of controller,
        then subsequent attach operation should find free unit number on the controller and attach
        the virtual disk there.
    
        commit #6
        Let the controllers of existing VMs continue without flip, current busInfo retrieved from
        chain_info field of volume record from database would be preferred over
        controller settings from all configuration settings.
    
        commit #5
        Editing global configuration param vmware.root.disk.controller osdefault value results
        in loss of previous root disk controller type. Hence root disk's controller type for legacy
        VMs is unknow post that modificaiton by user. If VM is stop/start then we could get this
        infromation from bus info of existing volume. But if user resets VM and then try to start VM.
        The existing bus info would be lost. Hence existing disk info is not available to depend on.
        Using lsilogic or generic scsi controller for ROOT disk of legacy VMs if reset.
    
        commit #4
        Avoid adding additional (>1) scsi controllers to system vms. While attaching volume to legacy VM
        don't use osdefault optoin which applicable only for VM created with the option enabled, use
        legacy data disk controller type (lsilogic)
    
        commit #3
        If root disk's controller type is scsi and data disk controller type condenses
        to any of scsi sub-types then data disk controller type would fall back to root disk controller itself. This
        ensures data volumes would be accessible in all cases as controller of root volume would be reliable
        and it means VM has the supported controller. It also avoids mix of scsi controller sub-types in a user instance.
        Also translating disk controller type scsi to lsilogic.
    
        commit #2
        Support auto detection of recommended virtual disk controller type for specific guest OS.
    
        commit #1
        Support granual controller types. Add support for controller types in template registration as well.
    
        Fix white spaces.
        Removed stale HEAD merge lines
        Removed tail of merge lines
        Fixed VmwareResource, removing storage commands that moved to VmwareStorageProcessor.
        removed stale code of controller that is present in processor
        Fixed check style errors.
        Fixed injection.
        Tested with Linux and windows templates. Unable to run iso based tests due to few bugs in register iso area.
    
        Signed-off-by: Sateesh Chodapuneedi <sateesh@apache.org>
    
    (cherry picked from commit a4cc987a6f66f20c434942956fffe5951df09e43)
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>

--- vmware-base/src/com/cloud/hypervisor/vmware/mo/DiskControllerType.java
+++ vmware-base/src/com/cloud/hypervisor/vmware/mo/DiskControllerType.java
@@ -17,5 +17,34 @@
 package com.cloud.hypervisor.vmware.mo;
 
 public enum DiskControllerType {
-    ide, scsi
+    ide,
+    scsi,
+    osdefault,
+    lsilogic,
+    lsisas1068,
+    buslogic,
+    pvscsi,
+    none;
+    public static DiskControllerType getType(String diskController) {
+        if (diskController == null || diskController.equalsIgnoreCase("osdefault")) {
+            return DiskControllerType.osdefault;
+        } else if (diskController.equalsIgnoreCase("vim.vm.device.VirtualLsiLogicSASController") || diskController.equalsIgnoreCase("VirtualLsiLogicSASController")
+                || diskController.equalsIgnoreCase(ScsiDiskControllerType.LSILOGIC_SAS)) {
+            return DiskControllerType.lsisas1068;
+        } else if (diskController.equalsIgnoreCase("vim.vm.device.VirtualLsiLogicController") || diskController.equalsIgnoreCase("VirtualLsiLogicController")
+                || diskController.equalsIgnoreCase(ScsiDiskControllerType.LSILOGIC_PARALLEL) || diskController.equalsIgnoreCase("scsi")) {
+            return DiskControllerType.lsilogic;
+        } else if (diskController.equalsIgnoreCase("vim.vm.device.VirtualIDEController") || diskController.equalsIgnoreCase("VirtualIDEController")
+                || diskController.equalsIgnoreCase("ide")) {
+            return DiskControllerType.ide;
+        } else if (diskController.equalsIgnoreCase("vim.vm.device.ParaVirtualSCSIController") || diskController.equalsIgnoreCase("ParaVirtualSCSIController")
+                || diskController.equalsIgnoreCase(ScsiDiskControllerType.VMWARE_PARAVIRTUAL)) {
+            return DiskControllerType.pvscsi;
+        } else if (diskController.equalsIgnoreCase("vim.vm.device.VirtualBusLogicController") || diskController.equalsIgnoreCase("VirtualBusLogicController")
+                || diskController.equalsIgnoreCase(ScsiDiskControllerType.BUSLOGIC)) {
+            return DiskControllerType.buslogic;
+        } else {
+            return DiskControllerType.none;
+        }
+    }
 }
