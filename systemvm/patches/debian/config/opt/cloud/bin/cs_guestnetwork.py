0be56a5ff6b4642297a8c3b9c682822a0393bf69
wilderrodrigues <wrodrigues@schubergphilis.com>
2015-01-21 17:09:42 +0100

    Fixing the timout )ssh hanging) on the CsApp.py file Adding kill_all() method on the CsProcess.py file (not being used yet) Changing a bit the logic in the merge.py
    The changes in the logic might be reverted, but they are causing no harm now.

--- systemvm/patches/debian/config/opt/cloud/bin/cs_guestnetwork.py
+++ systemvm/patches/debian/config/opt/cloud/bin/cs_guestnetwork.py
@@ -17,16 +17,24 @@
 
 from pprint import pprint
 
+keys = ['eth1', 'eth2', 'eth3', 'eth4', 'eth5', 'eth6', 'eth7', 'eth8', 'eth9']
 
 def merge(dbag, gn):
-    added = False
-    for dev in dbag:
-        if dev == "id":
-            continue
-        if len(dbag[dev]) == 0:
-            continue
-        if dbag[dev][0]['device'] == gn['device']:
-            dbag[dev].remove(dbag[dev][0])
-    if gn['add']:
-        dbag.setdefault(gn['device'], []).append(gn)
+    device = gn['device']
+    
+    if not gn['add'] and device in dbag:
+        
+        if dbag[device]:
+            device_to_die = dbag[device][0]
+            try:
+                dbag[device].remove(device_to_die)
+            except ValueError, e:
+                print "[WARN] cs_guestnetwork.py :: Error occurred removing item from databag. => %s" % device_to_die
+                del(dbag[device])
+        else:
+            del(dbag[device])
+    
+    else:
+        dbag.setdefault(device, []).append(gn)
+    
     return dbag
