35ede0418ac1730e2024bde6fcacd26168efdbed
Marcus Sorensen <marcus_sorensen@apple.com>
2015-01-23 02:34:08 -0700

    CLOUDSTACK-8177 KVM: disable tickpolicy for kvmclock. Newer libvirt enforces that kvmclock cannot accept parameters.

--- plugins/hypervisors/kvm/src/com/cloud/hypervisor/kvm/resource/LibvirtComputingResource.java
+++ plugins/hypervisors/kvm/src/com/cloud/hypervisor/kvm/resource/LibvirtComputingResource.java
@@ -3726,7 +3726,7 @@ public class LibvirtComputingResource extends ServerResourceBase implements Serv
             clock.setClockOffset(ClockDef.ClockOffset.LOCALTIME);
             clock.setTimer("rtc", "catchup", null);
         } else if (vmTO.getType() != VirtualMachine.Type.User || isGuestPVEnabled(vmTO.getOs())) {
-            clock.setTimer("kvmclock", "catchup", null, _noKvmClock);
+            clock.setTimer("kvmclock", null, null, _noKvmClock);
         }
 
         vm.addComp(clock);
