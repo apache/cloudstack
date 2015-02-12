7e75f339bcfdd6352f81a04219d3b8029f35634e
Daan Hoogland <daan@onecht.net>
2015-02-12 14:47:12 +0000

    license and format

--- engine/orchestration/test/com/cloud/agent/manager/DirectAgentAttacheTest.java
+++ engine/orchestration/test/com/cloud/agent/manager/DirectAgentAttacheTest.java
@@ -1,3 +1,19 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
 package com.cloud.agent.manager;
 
 import org.junit.Before;
@@ -12,27 +28,27 @@ import com.cloud.resource.ServerResource;
 
 @RunWith(MockitoJUnitRunner.class)
 public class DirectAgentAttacheTest {
-	@Mock
-	private AgentManagerImpl _agentMgr;
+    @Mock
+    private AgentManagerImpl _agentMgr;
 
-	@Mock
-	private ServerResource _resource;
+    @Mock
+    private ServerResource _resource;
 
-	long _id = 0L;
+    long _id = 0L;
 
-	@Before
-	public void setup() {
-		directAgentAttache = new DirectAgentAttache(_agentMgr, _id, "myDirectAgentAttache", _resource, false);
-		
-		MockitoAnnotations.initMocks(directAgentAttache);
-	}
-	private DirectAgentAttache directAgentAttache;
+    @Before
+    public void setup() {
+        directAgentAttache = new DirectAgentAttache(_agentMgr, _id, "myDirectAgentAttache", _resource, false);
 
-	@Test
-	public void testPingTask() throws Exception {
-		DirectAgentAttache.PingTask pt = directAgentAttache.new PingTask();
-		Mockito.doReturn(2).when(_agentMgr).getDirectAgentThreadCap();
-		pt.runInContext();
-		Mockito.verify(_resource, Mockito.times(1)).getCurrentStatus(_id);
-	}
+        MockitoAnnotations.initMocks(directAgentAttache);
+    }
+    private DirectAgentAttache directAgentAttache;
+
+    @Test
+    public void testPingTask() throws Exception {
+        DirectAgentAttache.PingTask pt = directAgentAttache.new PingTask();
+        Mockito.doReturn(2).when(_agentMgr).getDirectAgentThreadCap();
+        pt.runInContext();
+        Mockito.verify(_resource, Mockito.times(1)).getCurrentStatus(_id);
+    }
 }
