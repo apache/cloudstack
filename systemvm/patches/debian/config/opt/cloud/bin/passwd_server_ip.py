83736ab534f54aa20899021fd86cfbac3c203ce9
Rohit Yadav <rohit.yadav@shapeblue.com>
2015-03-17 14:21:48 +0530

    CLOUDSTACK-8331: savepassword should try all IPs on eth0
    The logic is same as passwd_server_ip script which runs password server on all
    IPs on eth0 interface.
    
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>
    (cherry picked from commit 294503a0251a1305c61a83bf4a094c1af8030a4c)
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>

--- systemvm/patches/debian/config/opt/cloud/bin/passwd_server_ip.py
+++ systemvm/patches/debian/config/opt/cloud/bin/passwd_server_ip.py
@@ -39,6 +39,7 @@ from SocketServer     import ThreadingMixIn #, ForkingMixIn
 
 passMap = {}
 secureToken = None
+listeningAddress = '127.0.0.1'
 lock = threading.RLock()
 
 def getTokenFile():
@@ -133,7 +134,7 @@ class PasswordRequestHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.end_headers()
         clientAddress = self.client_address[0]
-        if clientAddress not in ['localhost', '127.0.0.1']:
+        if clientAddress not in ['localhost', '127.0.0.1', listeningAddress]:
             syslog.syslog('serve_password: non-localhost IP trying to save password: %s' % clientAddress)
             self.send_response(403)
             return
@@ -162,7 +163,7 @@ class PasswordRequestHandler(BaseHTTPRequestHandler):
 def serve(HandlerClass = PasswordRequestHandler,
           ServerClass = ThreadedHTTPServer):
 
-    listeningAddress = '127.0.0.1'
+    global listeningAddress
     if len(sys.argv) > 1:
         listeningAddress = sys.argv[1]
 
