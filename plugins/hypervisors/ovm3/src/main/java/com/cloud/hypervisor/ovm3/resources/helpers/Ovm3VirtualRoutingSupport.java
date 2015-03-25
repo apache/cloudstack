724699424ecc60c96d2994ea35cf865a9d713df9
Daan Hoogland <dhoogland@schubergphilis.com>
2015-03-25 21:20:39 +0100

    findbugs: boxing/unboxing

--- plugins/hypervisors/ovm3/src/main/java/com/cloud/hypervisor/ovm3/resources/helpers/Ovm3VirtualRoutingSupport.java
+++ plugins/hypervisors/ovm3/src/main/java/com/cloud/hypervisor/ovm3/resources/helpers/Ovm3VirtualRoutingSupport.java
@@ -152,8 +152,8 @@ public class Ovm3VirtualRoutingSupport {
                 String[] splitResult = result.split(":");
                 int i = 0;
                 while (i < splitResult.length - 1) {
-                    stats[0] += (Long.valueOf(splitResult[i++])).longValue();
-                    stats[1] += (Long.valueOf(splitResult[i++])).longValue();
+                    stats[0] += (Long.parseLong(splitResult[i++]));
+                    stats[1] += (Long.parseLong(splitResult[i++]));
                 }
                 return new NetworkUsageAnswer(cmd, SUCCESS, stats[0],
                         stats[1]);
