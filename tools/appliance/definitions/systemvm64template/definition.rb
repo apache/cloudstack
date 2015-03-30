03ff244ff19b71327d8323db0ca018ebbb9a64dc
Rohit Yadav <rohit.yadav@shapeblue.com>
2015-03-30 13:56:41 +0530

    systemvmtemplate: increase disk size to 3000
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>

--- tools/appliance/definitions/systemvm64template/definition.rb
+++ tools/appliance/definitions/systemvm64template/definition.rb
@@ -8,7 +8,7 @@
 Veewee::Definition.declare({
   :cpu_count => '1',
   :memory_size=> '256',
-  :disk_size => '2560', :disk_format => 'VDI', :hostiocache => 'off',
+  :disk_size => '3000', :disk_format => 'VDI', :hostiocache => 'off',
   :os_type_id => 'Debian_64',
   :iso_file => "debian-7.8.0-amd64-netinst.iso",
   :iso_src => "http://cdimage.debian.org/debian-cd/7.8.0/amd64/iso-cd/debian-7.8.0-amd64-netinst.iso",
