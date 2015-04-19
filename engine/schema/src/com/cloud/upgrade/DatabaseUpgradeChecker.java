582687fb3f5e17fd8ccbedee68eefff0eef3e44a
Daan Hoogland <daan@onecht.net>
2015-04-19 18:10:03 +0200

    4.4.4 to 4.5.2 upgrade
     Conflicts:
    	engine/schema/src/com/cloud/upgrade/DatabaseUpgradeChecker.java
    
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>
    
    This closes #529

--- engine/schema/src/com/cloud/upgrade/DatabaseUpgradeChecker.java
+++ engine/schema/src/com/cloud/upgrade/DatabaseUpgradeChecker.java
@@ -72,6 +72,7 @@ import com.cloud.upgrade.dao.Upgrade440to441;
 import com.cloud.upgrade.dao.Upgrade441to442;
 import com.cloud.upgrade.dao.Upgrade442to450;
 import com.cloud.upgrade.dao.Upgrade443to450;
+import com.cloud.upgrade.dao.Upgrade444to450;
 import com.cloud.upgrade.dao.Upgrade450to451;
 import com.cloud.upgrade.dao.Upgrade451to460;
 import com.cloud.upgrade.dao.UpgradeSnapshot217to224;
@@ -230,6 +231,8 @@ public class DatabaseUpgradeChecker implements SystemIntegrityChecker {
         _upgradeMap.put("4.4.2", new DbUpgrade[] {new Upgrade442to450(), new Upgrade450to451(), new Upgrade451to460()});
 
         _upgradeMap.put("4.4.3", new DbUpgrade[] {new Upgrade443to450(), new Upgrade450to451(), new Upgrade451to460()});
+
+        _upgradeMap.put("4.4.4", new DbUpgrade[] {new Upgrade444to450(), new Upgrade450to451(), new Upgrade451to460()});
 
         _upgradeMap.put("4.5.0", new DbUpgrade[] {new Upgrade450to451(), new Upgrade451to460()});
 
