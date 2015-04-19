a8959bca553013d7b8182dfd355a2ecb799c3995
Daan Hoogland <daan@onecht.net>
2015-04-19 18:10:03 +0200

    4.4.4 to 4.5.2 upgrade
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>
    
    This closes #528

---
+++ engine/schema/src/com/cloud/upgrade/dao/Upgrade443to444.java
@@ -0,0 +1,64 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package com.cloud.upgrade.dao;
+
+import java.io.File;
+import java.sql.Connection;
+
+import org.apache.log4j.Logger;
+
+import com.cloud.utils.exception.CloudRuntimeException;
+import com.cloud.utils.script.Script;
+
+public class Upgrade443to444 implements DbUpgrade {
+    final static Logger s_logger = Logger.getLogger(Upgrade443to444.class);
+
+    @Override
+    public String[] getUpgradableVersionRange() {
+        return new String[] {"4.4.3", "4.4.4"};
+    }
+
+    @Override
+    public String getUpgradedVersion() {
+        return "4.4.4";
+    }
+
+    @Override
+    public boolean supportsRollingUpgrade() {
+        return false;
+    }
+
+    @Override
+    public File[] getPrepareScripts() {
+        String script = Script.findScript("", "db/schema-443to444.sql");
+        if (script == null) {
+            throw new CloudRuntimeException("Unable to find db/schema-empty.sql");
+        }
+
+        return new File[] {new File(script)};
+    }
+
+    @Override
+    public void performDataMigration(Connection conn) {
+    }
+
+    @Override
+    public File[] getCleanupScripts() {
+        return null;
+    }
+}
