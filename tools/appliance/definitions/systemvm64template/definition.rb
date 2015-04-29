a4120b24e484f896acb541af13f6f41b6ae819d9
Rohit Yadav <rohit.yadav@shapeblue.com>
2015-04-29 17:14:11 +0200

    systemvmtemplate: 7.8.0 iso is archived now, use link from archives
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>

--- tools/appliance/definitions/systemvm64template/definition.rb
+++ tools/appliance/definitions/systemvm64template/definition.rb
@@ -11,7 +11,7 @@ Veewee::Definition.declare({
   :disk_size => '3000', :disk_format => 'VDI', :hostiocache => 'off',
   :os_type_id => 'Debian_64',
   :iso_file => "debian-7.8.0-amd64-netinst.iso",
-  :iso_src => "http://cdimage.debian.org/debian-cd/7.8.0/amd64/iso-cd/debian-7.8.0-amd64-netinst.iso",
+  :iso_src => "http://cdimage.debian.org/cdimage/archive/7.8.0/amd64/iso-cd/debian-7.8.0-amd64-netinst.iso",
   :iso_md5 => "a91fba5001cf0fbccb44a7ae38c63b6e",
   :iso_download_timeout => "1000",
   :boot_wait => "10", :boot_cmd_sequence => [
