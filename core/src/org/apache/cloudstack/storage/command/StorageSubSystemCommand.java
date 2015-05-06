b284b841929c74a8e5273f8a31a26ed9bba5d139
wilderrodrigues <wrodrigues@schubergphilis.com>
2015-05-06 16:55:03 +0200

    Fixing method call on KVMGuru to reach StorageSubSystemCommand

--- core/src/org/apache/cloudstack/storage/command/StorageSubSystemCommand.java
+++ core/src/org/apache/cloudstack/storage/command/StorageSubSystemCommand.java
@@ -22,5 +22,5 @@ package org.apache.cloudstack.storage.command;
 import com.cloud.agent.api.Command;
 
 public abstract class StorageSubSystemCommand extends Command {
-    abstract void setExecuteInSequence(boolean inSeq);
+    public abstract void setExecuteInSequence(boolean inSeq);
 }
