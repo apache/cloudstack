42b89278e9386b8bc90e36fb600d7730e676ae72
Anshul Gangwar <anshul.gangwar@citrix.com>
2015-06-10 15:33:42 +0530

    CLOUDSTACK-8833: Fixed  Generating url and migrate volume to another storage , resulting two entry in UI and listvolume is not working for that volume Update the volume id in volume_store_ref table to newly created volume for migration

--- engine/storage/volume/src/org/apache/cloudstack/storage/volume/VolumeServiceImpl.java
+++ engine/storage/volume/src/org/apache/cloudstack/storage/volume/VolumeServiceImpl.java
@@ -1428,6 +1428,7 @@ public class VolumeServiceImpl implements VolumeService {
             srcVolume.processEvent(Event.OperationSuccessed);
             destVolume.processEvent(Event.MigrationCopySucceeded, result.getAnswer());
             volDao.updateUuid(srcVolume.getId(), destVolume.getId());
+            _volumeStoreDao.updateVolumeId(srcVolume.getId(), destVolume.getId());
             try {
                 destroyVolume(srcVolume.getId());
                 srcVolume = volFactory.getVolume(srcVolume.getId());
