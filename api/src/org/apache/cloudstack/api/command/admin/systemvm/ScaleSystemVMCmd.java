8634fe5cf59c007c5dc86c1741be31ad3b9c66d2
Rafael da Fonseca <rsafonseca@gmail.com>
2015-06-14 15:57:54 +0200

    Remove unused imports to fix checkstyle warnings
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>
    
    This closes #412

--- api/src/org/apache/cloudstack/api/command/admin/systemvm/ScaleSystemVMCmd.java
+++ api/src/org/apache/cloudstack/api/command/admin/systemvm/ScaleSystemVMCmd.java
@@ -16,9 +16,6 @@
 // under the License.
 package org.apache.cloudstack.api.command.admin.systemvm;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 
 import org.apache.log4j.Logger;
