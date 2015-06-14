b869a1633289a283a441be15da6f4c63f3f6b4de
Rafael da Fonseca <rsafonseca@gmail.com>
2015-06-14 16:04:35 +0200

    Remove unused import to fix checkstyle warning
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>
    
    This closes #400

--- plugins/hypervisors/xenserver/src/com/cloud/hypervisor/xenserver/resource/wrapper/xenbase/CitrixConsoleProxyLoadCommandWrapper.java
+++ plugins/hypervisors/xenserver/src/com/cloud/hypervisor/xenserver/resource/wrapper/xenbase/CitrixConsoleProxyLoadCommandWrapper.java
@@ -25,7 +25,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
-import java.nio.charset.Charset;
 
 import org.apache.log4j.Logger;
 
