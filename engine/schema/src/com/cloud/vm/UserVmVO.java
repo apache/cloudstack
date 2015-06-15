ce506000cd5ab1959a52032a063224c7c9616d06
Rafael da Fonseca <rsafonseca@gmail.com>
2015-06-15 17:33:54 +0200

    Re-run travis
    Signed-off-by: Daan Hoogland <daan.hoogland@gmail.com>
    
    This closes #453

--- engine/schema/src/com/cloud/vm/UserVmVO.java
+++ engine/schema/src/com/cloud/vm/UserVmVO.java
@@ -106,7 +106,7 @@ public class UserVmVO extends VMInstanceVO implements UserVm {
 
     @Override
     public String getDetail(String name) {
-        return details != null ? details.get(name) : null;
+        return details != null ? details.get(name) : null ;
     }
 
     @Override
