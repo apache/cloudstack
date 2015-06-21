301a7b9a7e26a803d9d7b02d7bb537c8393dca26
Rafael da Fonseca <rsafonseca@gmail.com>
2015-06-21 16:52:37 +0200

    Specify all external dependencies in pom Upgrade to lang3 and change usage of deprecated xmlparse method
    Add another missing dependency
    
    Signed-off-by: Daan Hoogland <daan.hoogland@gmail.com>

--- plugins/hypervisors/ovm3/src/main/java/com/cloud/hypervisor/ovm3/objects/Xen.java
+++ plugins/hypervisors/ovm3/src/main/java/com/cloud/hypervisor/ovm3/objects/Xen.java
@@ -23,7 +23,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 
 public class Xen extends OvmObject {
