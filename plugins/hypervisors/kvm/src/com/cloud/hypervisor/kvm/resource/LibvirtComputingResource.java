3a19a8916eb7840302882cddd8465ac56150d32d
Kishan Kavala <kishan@apache.org>
2015-07-02 16:52:46 +0530

    BUG-ID:CLOUDSTACK-7472: kvmclock fix for LXC is not required after fixing CLOUDSTACK-8177
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>
    
    This closes #557

--- plugins/hypervisors/kvm/src/com/cloud/hypervisor/kvm/resource/LibvirtComputingResource.java
+++ plugins/hypervisors/kvm/src/com/cloud/hypervisor/kvm/resource/LibvirtComputingResource.java
@@ -783,9 +783,6 @@ public class LibvirtComputingResource extends ServerResourceBase implements Serv
         value = (String) params.get("kvmclock.disable");
         if (Boolean.parseBoolean(value)) {
             _noKvmClock = true;
-        } else if(HypervisorType.LXC.equals(_hypervisorType) && value == null){
-            //Disable kvmclock by default for LXC
-            _noKvmClock = true;
         }
 
         LibvirtConnection.initialize(_hypervisorURI);
