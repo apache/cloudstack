168199360d45657c9752237e3aa208df79bbdc0b
Daan Hoogland <daan.hoogland@gmail.com>
2015-07-28 17:23:44 +0200

    coverity 1116562: resource count resource leak

--- engine/schema/src/com/cloud/upgrade/dao/Upgrade420to421.java
+++ engine/schema/src/com/cloud/upgrade/dao/Upgrade420to421.java
@@ -280,14 +280,13 @@ public class Upgrade420to421 implements DbUpgrade {
 
     private static void upgradeResourceCountforDomain(Connection conn, Long domainId, String type, Long resourceCount) throws SQLException {
         //update or insert into resource_count table.
-        PreparedStatement pstmt = null;
-        pstmt =
-                conn.prepareStatement("INSERT INTO `cloud`.`resource_count` (domain_id, type, count) VALUES (?,?,?) ON DUPLICATE KEY UPDATE id=LAST_INSERT_ID(id), count=?");
-        pstmt.setLong(1, domainId);
-        pstmt.setString(2, type);
-        pstmt.setLong(3, resourceCount);
-        pstmt.setLong(4, resourceCount);
-        pstmt.executeUpdate();
-        pstmt.close();
+        String sqlInsertResourceCount = "INSERT INTO `cloud`.`resource_count` (domain_id, type, count) VALUES (?,?,?) ON DUPLICATE KEY UPDATE id=LAST_INSERT_ID(id), count=?";
+        try (PreparedStatement pstmt = conn.prepareStatement(sqlInsertResourceCount);) {
+            pstmt.setLong(1, domainId);
+            pstmt.setString(2, type);
+            pstmt.setLong(3, resourceCount);
+            pstmt.setLong(4, resourceCount);
+            pstmt.executeUpdate();
+        }
     }
 }
