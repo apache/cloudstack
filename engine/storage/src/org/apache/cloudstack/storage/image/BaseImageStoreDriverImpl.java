24500bd0e4be822111125c581520feddb0827e55
Daan Hoogland <daan@onecht.net>
2015-09-14 15:22:52 +0200

    CID 1324349: conditionally return -1 or the dc id for the volume

--- engine/storage/src/org/apache/cloudstack/storage/image/BaseImageStoreDriverImpl.java
+++ engine/storage/src/org/apache/cloudstack/storage/image/BaseImageStoreDriverImpl.java
@@ -239,7 +239,8 @@ public abstract class BaseImageStoreDriverImpl implements ImageStoreDriver {
             result.setResult(answer.getErrorString());
             caller.complete(result);
             String msg = "Failed to upload volume: " + obj.getUuid() + " with error: " + answer.getErrorString();
-            _alertMgr.sendAlert(AlertManager.AlertType.ALERT_TYPE_UPLOAD_FAILED, volStoreVO.getZoneId(), null, msg, msg);
+            _alertMgr.sendAlert(AlertManager.AlertType.ALERT_TYPE_UPLOAD_FAILED,
+                    (volStoreVO == null ? -1L : volStoreVO.getZoneId()), null, msg, msg);
             s_logger.error(msg);
         } else if (answer.getDownloadStatus() == VMTemplateStorageResourceAssoc.Status.DOWNLOADED) {
             CreateCmdResult result = new CreateCmdResult(null, null);
