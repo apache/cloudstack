3d34e4a52c95c7585fd49ec07001620621c3c846
Wilder Rodrigues <wrodrigues@schubergphilis.com>
2015-09-29 19:38:02 +0200

    CLOUDSTACK-8915 - Reverting changes from commit id 18dbc0c4cbe506ad698bc513c901dc2d0e48159f
       - If the file is always copied, it will result in restarting keepalived everytime which makes the routers transit between master/backup

--- systemvm/patches/debian/config/opt/cloud/bin/cs/CsRedundant.py
+++ systemvm/patches/debian/config/opt/cloud/bin/cs/CsRedundant.py
@@ -96,7 +96,8 @@ class CsRedundant(object):
                 d = s.replace(".templ", "")
             CsHelper.copy_if_needed(
                 "%s/%s" % (self.CS_TEMPLATES_DIR, s), "%s/%s" % (self.CS_ROUTER_DIR, d))
-        CsHelper.copy(
+
+        CsHelper.copy_if_needed(
             "%s/%s" % (self.CS_TEMPLATES_DIR, "keepalived.conf.templ"), self.KEEPALIVED_CONF)
         CsHelper.copy_if_needed(
             "%s/%s" % (self.CS_TEMPLATES_DIR, "conntrackd.conf.templ"), self.CONNTRACKD_CONF)
