b128e567c4eacf4d22e6a8c0d6888c297c7fb726
Daan Hoogland <daan@onecht.net>
2015-10-02 11:42:42 +0200

    CLOUDSTACK-8848: added null pointer guard to new public method

--- engine/schema/src/com/cloud/vm/dao/VMInstanceDaoImpl.java
+++ engine/schema/src/com/cloud/vm/dao/VMInstanceDaoImpl.java
@@ -807,6 +807,9 @@ public class VMInstanceDaoImpl extends GenericDaoBase<VMInstanceVO, Long> implem
     @Override
     public boolean isPowerStateUpToDate(final long instanceId) {
         VMInstanceVO instance = findById(instanceId);
+        if(instance == null) {
+            throw new CloudRuntimeException("checking power state update count on non existing instance " + instanceId);
+        }
         return instance.getPowerStateUpdateCount() < MAX_CONSECUTIVE_SAME_STATE_UPDATE_COUNT;
     }
 
