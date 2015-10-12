f731d58dadfa0e6cddf57beb01b358fcfa42a395
Daan Hoogland <daan@onecht.net>
2015-10-12 16:13:00 +0200

    CLOUDSTACK-8677: extra test for stand alone logging of GetHostStatsCmd..

--- core/test/com/cloud/agent/transport/RequestTest.java
+++ core/test/com/cloud/agent/transport/RequestTest.java
@@ -47,6 +47,7 @@ import com.cloud.storage.Storage.ImageFormat;
 import com.cloud.storage.Storage.TemplateType;
 import com.cloud.storage.VMTemplateStorageResourceAssoc.Status;
 import com.cloud.template.VirtualMachineTemplate;
+import com.google.gson.Gson;
 
 /**
  *
@@ -61,7 +62,7 @@ public class RequestTest extends TestCase {
     public void testSerDeser() {
         s_logger.info("Testing serializing and deserializing works as expected");
 
-        s_logger.info("UpdateHostPasswordCommand should have two parameters that doesn't show in logging");
+        s_logger.info("UpdateHostPasswordCommand should have two parameters that don't show in logging");
         UpdateHostPasswordCommand cmd1 = new UpdateHostPasswordCommand("abc", "def");
         s_logger.info("SecStorageFirewallCfgCommand has a context map that shouldn't show up in debug level");
         SecStorageFirewallCfgCommand cmd2 = new SecStorageFirewallCfgCommand();
@@ -233,6 +234,33 @@ public class RequestTest extends TestCase {
         logger.setLevel(level);
     }
 
+    public void testGson() {
+        final Gson gson = GsonHelper.getGson();
+        s_logger.info("Testing gson");
+        Command cmd_in;
+        Command cmd_out;
+        String json;
+        String hostguid = "hostguid";
+        String hostname = "hostname";
+        Long hostId = 101l;
+
+        cmd_in = new GetHostStatsCommand(hostguid, hostname, hostId);
+        json = gson.toJson(cmd_in, GetHostStatsCommand.class);
+        cmd_out = gson.fromJson(json, GetHostStatsCommand.class);
+
+        assertEquals(((GetHostStatsCommand)cmd_out).getHostGuid(),hostguid);
+        assertEquals(((GetHostStatsCommand)cmd_out).getHostName(),hostname);
+        assertEquals(((GetHostStatsCommand)cmd_out).getHostId(),hostId);
+
+        String username = "abc";
+        String password = "def";
+        cmd_in = new UpdateHostPasswordCommand(username, password);
+        json = gson.toJson(cmd_in, UpdateHostPasswordCommand.class);
+        cmd_out = gson.fromJson(json, UpdateHostPasswordCommand.class);
+
+        assertEquals(((UpdateHostPasswordCommand)cmd_out).getUsername(), username);
+        assertEquals(((UpdateHostPasswordCommand)cmd_out).getNewPassword(), password);
+    }
     protected void compareRequest(Request req1, Request req2) {
         assert req1.getSequence() == req2.getSequence();
         assert req1.getAgentId() == req2.getAgentId();
