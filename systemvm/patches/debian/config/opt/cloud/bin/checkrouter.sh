fb33cb28aba7bfc829651e8881a9a6afa6a70a76
Wilder Rodrigues <wrodrigues@schubergphilis.com>
2015-10-17 14:48:08 +0200

    CLOUDSTACK-8952 - Make the checkrouter.sh compatible with RVR as well

--- systemvm/patches/debian/config/opt/cloud/bin/checkrouter.sh
+++ systemvm/patches/debian/config/opt/cloud/bin/checkrouter.sh
@@ -16,10 +16,20 @@
 # specific language governing permissions and limitations
 # under the License.
 
-STATUS=BACKUP
-ETH1_STATE=$(ip addr | grep eth1 | grep state | awk '{print $9;}')
+STATUS=UNKNOWN
+INTERFACE=eth1
+ROUTER_TYPE=$(cat /etc/cloudstack/cmdline.json | grep type | awk '{print $2;}' | sed -e 's/[,\"]//g')
+if [ $ROUTER_TYPE = "router" ]
+then
+    INTERFACE=eth2
+fi
+
+ETH1_STATE=$(ip addr | grep $INTERFACE | grep state | awk '{print $9;}')
 if [ $ETH1_STATE = "UP" ]
 then
     STATUS=MASTER
+elif [ $ETH1_STATE = "DOWN" ]
+then
+    STATUS=BACKUP
 fi
 echo "Status: ${STATUS}"
