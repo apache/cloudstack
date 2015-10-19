0fddcc663e201edaff7ed80eb09cf2e1197ae08c
Nitin Kumar Maharana <nitinkumar.maharana@citrix.com>
2015-10-20 00:37:56 +0530

    CLOUDSTACK-8968: UI icon over VM snapshot to deploy user instance
    Added a new Icon in Instance page to launch the VM from the snapshot.

--- ui/scripts/vm_snapshots.js
+++ ui/scripts/vm_snapshots.js
@@ -189,6 +189,50 @@
                         notification: {
                             poll: pollAsyncJobResult
                         }
+                    },
+                    start: {
+                        label: 'label.deploy.instance',
+
+                        action: {
+                            custom: function(args)   {
+                                var zoneObj, snapshotObj, hypervisorObj;
+
+                                $.ajax({
+                                    url: createURL("listZones&available=true"),
+                                    dataType: "json",
+                                    data: {id: args.context.instances[0].zoneid},
+                                    async: false,
+                                    success: function(json) {
+                                        zoneObj = json.listzonesresponse.zone;
+                                    }
+                                });
+
+                                snapshotObj = args.context.vmsnapshots;
+
+                                $.ajax({
+                                    url: createURL("listHypervisors&zoneid=" + args.context.instances[0].zoneid),
+                                    dataType: "json",
+                                    async: false,
+                                    success: function(json) {
+                                        hypervisorObj = json.listhypervisorsresponse.hypervisor;
+                                    }
+                                });
+
+                                var customFunc = cloudStack.uiCustom.instanceWizard(cloudStack.instanceWizard, {step: 3, title: 'label.deploy.instance', zone: zoneObj, snapshot: snapshotObj, hypervisor: hypervisorObj});
+
+                                customFunc(args);
+                            }
+                        },
+
+                        messages: {
+                            notification: function(args) {
+                                return 'label.deploy.instance';
+                            }
+                        },
+
+                        notification: {
+                            poll: pollAsyncJobResult
+                        }
                     }
                 }
             }
