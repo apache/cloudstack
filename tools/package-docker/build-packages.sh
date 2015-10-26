895ac2a6ad1b8d91b2d9e306b8d4276e740eb45e
Sverrir Berg <sverrir@greenqloud.com>
2015-10-26 16:45:58 +0000

    Debian packaging scripts file permissions fix

--- tools/package-docker/build-packages.sh
+++ tools/package-docker/build-packages.sh
@@ -59,7 +59,7 @@ if [ -n "${VERSION}" ]; then
 fi
 
 # Make sure the user running the process owns the files (problem with volume mounts in docker build).
-find . -user 1000 -group 1000 | xargs chown --no-dereference root:root
+find . -user 1000 -group 1000 -exec chown --no-dereference root:root {} \;
 
 dpkg-buildpackage
 
