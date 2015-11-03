938f1d2a7deb6800e005d004289635a17abc3fc0
Rohit Yadav <rohit.yadav@shapeblue.com>
2015-11-03 15:27:46 +0530

    CLOUDSTACK-9020: Metrics views for CloudStack UI
    Implements following:
    - A metrics table widget that is:
      - vertically and horizontally scrollable with pagination/infinite scrolling
      - sortable columns (client side)
      - groupable/collapsible columns
      - alternate row coloring
      - refresh button to refresh views
      - threshold table cell coloring
      - panel/breadcrumb navigation
      - quick view action column
      - translatable labels
    - Sortable column for all CloudStack tables (client side)
    - Configurable UI pagesize for list API calls, 'default.ui.page.size'
    - Metrics views: Zones, Clusters, Hosts, Instances, Storage pools, Volumes
    - Resource filtering/navigation: Zones->Clusters->Hosts->Instances->Volumes, Storage Pool->Volumes
    
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>

--- ui/scripts/storage.js
+++ ui/scripts/storage.js
@@ -253,6 +253,23 @@
                             }
                         },
 
+                        viewMetrics: {
+                            label: 'label.metrics',
+                            isHeader: true,
+                            addRow: false,
+                            preFilter: function(args) {
+                                return isAdmin();
+                            },
+                            action: {
+                                custom: cloudStack.uiCustom.metricsView({resource: 'volumes'})
+                            },
+                            messages: {
+                                notification: function (args) {
+                                    return 'label.metrics';
+                                }
+                            }
+                        },
+
                         uploadVolume: {
                             isHeader: true,
                             label: 'label.upload.volume',
