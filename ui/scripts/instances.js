1391f476f1db211392b7adf23a58358f10d0aa4a
Rohit Yadav <rohit.yadav@shapeblue.com>
2015-11-07 16:50:08 +0530

    CLOUDSTACK-9020: add ipaddress in instances view
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>
    (cherry picked from commit edc74aebbffc3f8fb4853de3cf6740eee83c78fd)
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>

--- ui/scripts/instances.js
+++ ui/scripts/instances.js
@@ -164,6 +164,9 @@
                     label: 'label.display.name',
                     truncate: true
                 },
+                ipaddress: {
+                    label: 'label.ip.address'
+                },
                 zonename: {
                     label: 'label.zone.name'
                 },
@@ -371,6 +374,11 @@
                     data: data,
                     success: function(json) {
                         var items = json.listvirtualmachinesresponse.virtualmachine;
+                        $.each(items, function(idx, vm) {
+                            if (vm.nic && vm.nic.length > 0 && vm.nic[0].ipaddress) {
+                                items[idx].ipaddress = vm.nic[0].ipaddress;
+                            }
+                        });
                         args.response.success({
                             data: items
                         });
