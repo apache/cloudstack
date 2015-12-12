c456eeb959326d9e442972820fed27796c22f380
Remi Bergsma <github@remi.nl>
2015-12-12 21:51:48 +0100

    Fixed typo in iam/pom.xml

--- tools/build/build_asf.sh
+++ tools/build/build_asf.sh
@@ -103,7 +103,7 @@ perl -pi -e "s/-SNAPSHOT//" tools/marvin/setup.py
 perl -pi -e "s/-SNAPSHOT//" tools/marvin/marvin/deployAndRun.py
 perl -pi -e "s/-SNAPSHOT//" debian/changelog
 perl -pi -e "s/-SNAPSHOT//" services/iam/plugin/pom.xml
-perl -pi -e "s/-SNAPSHOT//" services/iam/pom.xm
+perl -pi -e "s/-SNAPSHOT//" services/iam/pom.xml
 perl -pi -e "s/-SNAPSHOT//" services/iam/server/pom.xml
 
 case "$currentversion" in 
