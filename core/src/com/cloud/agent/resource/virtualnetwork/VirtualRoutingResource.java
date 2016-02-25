f632215b0493b210201fa7590db00ce64cf25e4a
Miguel Ferreira <miguelferreira@me.com>
2016-02-25 14:32:09 +0100

    Make the generated json files unique to prevent concurrency issues
    Backported from Cosmic.

--- core/src/com/cloud/agent/resource/virtualnetwork/VirtualRoutingResource.java
+++ core/src/com/cloud/agent/resource/virtualnetwork/VirtualRoutingResource.java
@@ -159,6 +159,7 @@ public class VirtualRoutingResource {
     private ExecutionResult applyConfigToVR(String routerAccessIp, ConfigItem c, int timeout) {
         if (c instanceof FileConfigItem) {
             FileConfigItem configItem = (FileConfigItem)c;
+
             return _vrDeployer.createFileInVR(routerAccessIp, configItem.getFilePath(), configItem.getFileName(), configItem.getFileContents());
         } else if (c instanceof ScriptConfigItem) {
             ScriptConfigItem configItem = (ScriptConfigItem)c;
