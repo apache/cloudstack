52f52847a493a2f0aeea9ec1eb45a0bc816c1c66
Pall Helgason <palli@greenqloud.com>
2016-03-03 13:32:24 +0000

    CLOUDSTACK-9644: Adding missing bits field to TemplateResponse

--- server/src/com/cloud/api/query/dao/TemplateJoinDaoImpl.java
+++ server/src/com/cloud/api/query/dao/TemplateJoinDaoImpl.java
@@ -284,6 +284,7 @@ public class TemplateJoinDaoImpl extends GenericDaoBaseWithTagInformation<Templa
 
         isoResponse.setOsTypeId(iso.getGuestOSUuid());
         isoResponse.setOsTypeName(iso.getGuestOSName());
+        isoResponse.setBits(iso.getBits());
 
         // populate owner.
         ApiResponseHelper.populateOwner(isoResponse, iso);
