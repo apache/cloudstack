2cddaf3d36cf76f5db601d583ba3d06aaf2778dd
Rohit Yadav <rohit.yadav@shapeblue.com>
2016-05-04 16:21:24 +0530

    CLOUDSTACK-9447: fix build and upgrade to debian 7.11 iso
    - Update base debian iso to version 7.11
    - Upgrade ruby version to 2.3.0 (latest/stable)
    - Fix Gemfile
    - Update README
    - Fix openswan pkg name with the same version
    - Remove cloud-cleanup it's not available
    
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>

--- tools/appliance/definitions/systemvmtemplate/definition.rb
+++ tools/appliance/definitions/systemvmtemplate/definition.rb
@@ -27,15 +27,15 @@ arch = ENV['VM_ARCH'] || 'i386'
 architectures = {
     :i386 => {
         :os_type_id => 'Debian',
-        :iso_file => 'debian-7.9.0-i386-netinst.iso',
-        :iso_src => 'http://cdimage.debian.org/cdimage/archive/7.9.0/i386/iso-cd/debian-7.9.0-i386-netinst.iso',
-        :iso_md5 => 'e101a11ddb31f85acef542df1a49bf57',
+        :iso_file => 'debian-7.11.0-i386-netinst.iso',
+        :iso_src => 'http://cdimage.debian.org/cdimage/archive/7.11.0/i386/iso-cd/debian-7.11.0-i386-netinst.iso',
+        :iso_md5 => '75055a694508f5b891038ec12d703c9e',
     },
     :amd64 => {
         :os_type_id => 'Debian_64',
-        :iso_file => 'debian-7.9.0-amd64-netinst.iso',
-        :iso_src => 'http://cdimage.debian.org/cdimage/archive/7.9.0/amd64/iso-cd/debian-7.9.0-amd64-netinst.iso',
-        :iso_md5 => '774d1fc8c5364e63b22242c33a89c1a3'
+        :iso_file => 'debian-7.11.0-amd64-netinst.iso',
+        :iso_src => 'http://cdimage.debian.org/cdimage/archive/7.11.0/amd64/iso-cd/debian-7.11.0-amd64-netinst.iso',
+        :iso_md5 => '096c1c18b44c269808bd815d58c53c8f'
     }
 }
 
