ea22869593f68a3a34b12aeb23c2bb6c34efd365
Rohit Yadav <rohit.yadav@shapeblue.com>
2016-05-05 23:19:33 +0530

    CLOUDSTACK-9348: Optimize NioTest and NioConnection main loop
    - Reduces SSL handshake timeout to 15s, previously this was only 10s in
      commit debfcdef788ce0d51be06db0ef10f6815f9b563b
    - Adds an aggresive explicit wakeup to save the Nio main IO loop/handler from
      getting blocked
    - Fix NioTest to fail/succeed in about 60s, previously this was 300s
    - Due to aggresive wakeup usage, NioTest should complete in less than 5s on most
      systems. On virtualized environment this may slightly increase due to thread,
      CPU burst/scheduling delays.
    
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>

--- utils/src/main/java/com/cloud/utils/nio/Link.java
+++ utils/src/main/java/com/cloud/utils/nio/Link.java
@@ -596,8 +596,8 @@ public class Link {
         while (handshakeStatus != SSLEngineResult.HandshakeStatus.FINISHED
                 && handshakeStatus != SSLEngineResult.HandshakeStatus.NOT_HANDSHAKING) {
             final long timeTaken = System.currentTimeMillis() - startTimeMills;
-            if (timeTaken > 60000L) {
-                s_logger.warn("SSL Handshake has taken more than 60s to connect to: " + socketChannel.getRemoteAddress() +
+            if (timeTaken > 15000L) {
+                s_logger.warn("SSL Handshake has taken more than 15s to connect to: " + socketChannel.getRemoteAddress() +
                         ". Please investigate this connection.");
                 return false;
             }
