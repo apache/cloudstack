0381b7ea185ef753873594216a67b8d376e3d658
Rohit Yadav <rohit.yadav@shapeblue.com>
2016-07-01 14:32:58 +0530

    CLOUDSTACK-9348: Reduce Nio selector wait time
    This reduced the Nio loop selector wait time, this way the selector will
    check frequently (as much as 100ms per iteration) and handle any pending
    connection/tasks. This would make reconnections very quick at the expense of
    some CPU usage.
    
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>

--- utils/src/main/java/com/cloud/utils/nio/NioConnection.java
+++ utils/src/main/java/com/cloud/utils/nio/NioConnection.java
@@ -125,7 +125,7 @@ public abstract class NioConnection implements Callable<Boolean> {
     public Boolean call() throws NioConnectionException {
         while (_isRunning) {
             try {
-                _selector.select(1000);
+                _selector.select(100);
 
                 // Someone is ready for I/O, get the ready keys
                 final Set<SelectionKey> readyKeys = _selector.selectedKeys();
