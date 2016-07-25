d302269fe53ca0d3ef4c7d4c51b28ce94d8a4847
Will Stevens <williamstevens@gmail.com>
2016-07-25 16:44:38 -0400

    CLOUDSTACK-9437: Create egress chain on upgrade and cleanup for allow all traffic
    - Ensure that FW_EGRESS_RULE chain exists after upgrading the router
    - Flush allow all egress rule on 0.0.0.0/0, if such a rule exists in the config
      it will be added later (CLOUDSTACK-9437)

--- systemvm/patches/debian/config/opt/cloud/bin/configure.py
+++ systemvm/patches/debian/config/opt/cloud/bin/configure.py
@@ -263,6 +263,12 @@ class CsAcl(CsDataBag):
                 rstr = rstr.replace("  ", " ").lstrip()
                 self.fw.append([self.table, self.count, rstr])
 
+    def flushAllowAllEgressRules(self):
+        logging.debug("Flush allow 'all' egress firewall rule")
+        # Ensure that FW_EGRESS_RULES chain exists
+        CsHelper.execute("iptables-save | grep '^:FW_EGRESS_RULES' || iptables -t filter -N FW_EGRESS_RULES")
+        CsHelper.execute("iptables-save | grep '^-A FW_EGRESS_RULES -j ACCEPT$' | sed 's/^-A/iptables -t filter -D/g' | bash")
+
     def process(self):
         for item in self.dbag:
             if item == "id":
@@ -978,6 +984,7 @@ def main(argv):
             acls.process()
 
             acls = CsAcl('firewallrules', config)
+            acls.flushAllowAllEgressRules()
             acls.process()
 
             fwd = CsForwardingRules("forwardingrules", config)
