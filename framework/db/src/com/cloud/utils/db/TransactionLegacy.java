635aa2005899b28f5d7d5428c4b55cde44a64928
Abhinandan Prateek <abhinandan.prateek@shapeblue.com>
2016-08-17 16:15:15 +0530

    CLOUDSTACK-9460: For long running transactions, if the connection is timed out by the mysql server then refresh it

--- framework/db/src/com/cloud/utils/db/TransactionLegacy.java
+++ framework/db/src/com/cloud/utils/db/TransactionLegacy.java
@@ -173,12 +173,22 @@ public class TransactionLegacy implements Closeable {
                 isNew = true;
             }
         }
-
+        txn.checkConnection();
         txn.takeOver(name, false);
         if (isNew) {
             s_mbean.addTransaction(txn);
         }
         return txn;
+    }
+
+    public void checkConnection() {
+        try {
+            if (_conn != null && !_conn.isValid(3)) {
+                _conn = null;
+            }
+        } catch (SQLException e) {
+            _conn = null;
+        }
     }
 
     protected StackElement peekInStack(Object obj) {
