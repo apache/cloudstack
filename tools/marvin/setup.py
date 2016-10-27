839cdbaea88c726522655cf8d1f7d754713dc62a
Rohit Yadav <rohit.yadav@shapeblue.com>
2016-10-27 16:08:49 +0530

    Updating pom.xml version numbers for release 4.5.2.2
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>

--- tools/marvin/setup.py
+++ tools/marvin/setup.py
@@ -27,7 +27,7 @@ except ImportError:
         raise RuntimeError("python setuptools is required to build Marvin")
 
 
-VERSION = "4.5.2.1"
+VERSION = "4.5.2.2"
 
 setup(name="Marvin",
       version=VERSION,
