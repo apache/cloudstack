82a9aa0c18559026e20644ba32fa1aeb97f2b6a6
Nathan Johnson <njohnson@ena.com>
2016-11-09 17:18:52 -0600

    EC-1171 Pull in cs_ip.py portion of PR 1741
    https://github.com/apache/cloudstack/pull/1741
    Updated StrongSwan VPN Implementation -- (partial merge)

--- systemvm/patches/debian/config/opt/cloud/bin/cs_ip.py
+++ systemvm/patches/debian/config/opt/cloud/bin/cs_ip.py
@@ -15,18 +15,17 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-from pprint import pprint
+
 from netaddr import *
 
-
 def merge(dbag, ip):
-    added = False
+    index = -1 # a non-valid array index
     for dev in dbag:
         if dev == "id":
             continue
-        for address in dbag[dev]:
+        for i, address in enumerate(dbag[dev]):
             if address['public_ip'] == ip['public_ip']:
-                dbag[dev].remove(address)
+                index = i
 
     ipo = IPNetwork(ip['public_ip'] + '/' + ip['netmask'])
     ip['device'] = 'eth' + str(ip['nic_dev_id'])
@@ -37,9 +36,11 @@ def merge(dbag, ip):
     if 'nw_type' not in ip.keys():
         ip['nw_type'] = 'public'
     if ip['nw_type'] == 'control':
-        dbag['eth' + str(ip['nic_dev_id'])] = [ip]
-    # if ip['add'] is false, the IP will be removed from the databag
-    elif ip['add']:
-        dbag.setdefault('eth' + str(ip['nic_dev_id']), []).append(ip)
+        dbag[ip['device']] = [ip]
+    else:
+        if index != -1:
+            dbag[ip['device']][index] = ip
+        else:
+            dbag.setdefault(ip['device'], []).append(ip)
 
     return dbag
