31ff13d0167899d76ec734a8ec9660f3397b3758
Murali Reddy <muralimmreddy@gmail.com>
2016-11-18 10:58:14 +0530

    CLOUDSTACK-9503: Increased the VR script timeout. Most of the changes are about converting int/long time values to joda Duration.

--- core/src/com/cloud/agent/resource/virtualnetwork/VirtualRouterDeployer.java
+++ core/src/com/cloud/agent/resource/virtualnetwork/VirtualRouterDeployer.java
@@ -19,13 +19,14 @@
 
 package com.cloud.agent.resource.virtualnetwork;
 
+import org.joda.time.Duration;
+
 import com.cloud.agent.api.routing.NetworkElementCommand;
 import com.cloud.utils.ExecutionResult;
 
 public interface VirtualRouterDeployer {
     ExecutionResult executeInVR(String routerIp, String script, String args);
-    /* timeout in seconds */
-    ExecutionResult executeInVR(String routerIp, String script, String args, int timeout);
+    ExecutionResult executeInVR(String routerIp, String script, String args, Duration timeout);
     ExecutionResult createFileInVR(String routerIp, String path, String filename, String content);
     ExecutionResult prepareCommand(NetworkElementCommand cmd);
     ExecutionResult cleanupCommand(NetworkElementCommand cmd);
