8049e6f2418c0fdf43b23f12ec975bb5f7aae939
Mrunalini Kankariya <mrunalini_kankariya@persistent.co.in>
2017-05-12 18:37:25 +0530

    CLOUDSTACK-9742:Simultaneous snapshots for detached volume

--- server/src/com/cloud/storage/VolumeApiServiceImpl.java
+++ server/src/com/cloud/storage/VolumeApiServiceImpl.java
@@ -1,4 +1,4 @@
-// Licensed to the Apache Software Foundation (ASF) under one
+    // Licensed to the Apache Software Foundation (ASF) under one
 // or more contributor license agreements.  See the NOTICE file
 // distributed with this work for additional information
 // regarding copyright ownership.  The ASF licenses this file
@@ -2082,7 +2082,6 @@ public class VolumeApiServiceImpl extends ManagerBase implements VolumeApiServic
                 }
 
             } else {
-                s_logger.debug("Scheduling snapshot thru job queues");
                 Outcome<Snapshot> outcome = takeVolumeSnapshotThroughJobQueue(vm.getId(), volumeId, policyId, snapshotId, account.getId(), quiescevm, locationType);
 
                 try {
