5d989f322cb59ec15e8a5d23b9ef6f90d9852f59
Jayapal <jayapal.uradi@accelerite.com>
2017-05-29 15:32:04 +0530

    CLOUDSTACK-9162: Handled vpn user add when vpn is not enabled on the account

--- server/src/com/cloud/network/vpn/RemoteAccessVpnManagerImpl.java
+++ server/src/com/cloud/network/vpn/RemoteAccessVpnManagerImpl.java
@@ -90,6 +90,7 @@ import com.cloud.utils.db.TransactionCallbackNoReturn;
 import com.cloud.utils.db.TransactionCallbackWithException;
 import com.cloud.utils.db.TransactionStatus;
 import com.cloud.utils.net.NetUtils;
+import org.apache.commons.collections.CollectionUtils;
 
 public class RemoteAccessVpnManagerImpl extends ManagerBase implements RemoteAccessVpnService, Configurable {
     private final static Logger s_logger = Logger.getLogger(RemoteAccessVpnManagerImpl.class);
@@ -508,6 +509,12 @@ public class RemoteAccessVpnManagerImpl extends ManagerBase implements RemoteAcc
 
         s_logger.debug("Applying vpn users for " + owner);
         List<RemoteAccessVpnVO> vpns = _remoteAccessVpnDao.findByAccount(vpnOwnerId);
+
+        if (CollectionUtils.isEmpty(vpns)) {
+            s_logger.debug("There are no remote access vpns configured on this account  " + owner +" to apply vpn user, failing add vpn user ");
+            return false;
+        }
+
         RemoteAccessVpnVO vpnTemp  = null;
 
         List<VpnUserVO> users = _vpnUsersDao.listByAccount(vpnOwnerId);
