1ea1bc32f94e9862c67e1afd97d59e5f78fdf786
gabrascher <gabrascher@hotmail.com>
2017-07-18 17:10:55 -0300

    Update Quota plugin to support currency values up to 5 decimal places
    Summary: this commit alters column currency_value from table
    cloud_usage.quota_tariff to support values up to 5 decimal places. The
    current implementation allows up to 2 decimal places.
    
    Issue: need to use more than 2 decimal places to define resources values
    in Quota tariff.
    
    Solution: modify column currency_value from table
    cloud_usage.quota_tariff to support values up to 5 decimal places.
    Values with more than 5 decimal places will be displayed with scientific
    notation in the user interface.
    
    SQL command: "ALTER TABLE cloud_usage.quota_tariff MODIFY currency_value
    DECIMAL(15,5) not null"

--- setup/db/db/schema-41000to41100-cleanup.sql
+++ setup/db/db/schema-41000to41100-cleanup.sql
@@ -18,3 +18,6 @@
 --;
 -- Schema upgrade cleanup from 4.10.0.0 to 4.11.0.0
 --;
+
+-- CLOUDSTACK-9914: Alter quota_tariff to support currency values up to 5 decimal places
+ALTER TABLE `cloud_usage`.`quota_tariff` MODIFY `currency_value` DECIMAL(15,5) not null
