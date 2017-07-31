83e545a7ff0a66971a164c2693ed7adaf23d30ca
Daan Hoogland <daan.hoogland@shapeblue.com>
2017-07-31 09:14:53 +0200

    comment

--- api/test/com/cloud/network/IsolationMethodTest.java
+++ api/test/com/cloud/network/IsolationMethodTest.java
@@ -22,9 +22,6 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
-/**
- * Created by dahn on 18/07/17.
- */
 public class IsolationMethodTest {
     @After
     public void cleanTheRegistry() {
