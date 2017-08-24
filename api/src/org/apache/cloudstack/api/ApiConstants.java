4b38d362842aab1ff459d76f75e09c19fe0fa6f0
vedulasantosh <vedula.santosh@accelerite.com>
2017-08-24 16:51:13 +0530

    CLOUDSTACK-9915  ListSnapshots API does not provide virtual size information of the snapshots (#2101)

--- api/src/org/apache/cloudstack/api/ApiConstants.java
+++ api/src/org/apache/cloudstack/api/ApiConstants.java
@@ -653,6 +653,7 @@ public class ApiConstants {
     public static final String OVM3_CLUSTER = "ovm3cluster";
     public static final String OVM3_VIP = "ovm3vip";
     public static final String CLEAN_UP_DETAILS = "cleanupdetails";
+    public static final String VIRTUAL_SIZE = "virtualsize";
     public static final String NETSCALER_CONTROLCENTER_ID = "netscalercontrolcenterid";
     public static final String NETSCALER_SERVICEPACKAGE_ID = "netscalerservicepackageid";
 
