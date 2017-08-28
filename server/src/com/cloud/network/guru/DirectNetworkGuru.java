99d3ca22f6a38b0583e0a9abc8f6278095d09959
Daan Hoogland <daan.hoogland@shapeblue.com>
2017-08-28 09:29:52 +0200

    CLOUDSTACK-10007 remove hardcoded exemptions on directnetworkguru

--- server/src/com/cloud/network/guru/DirectNetworkGuru.java
+++ server/src/com/cloud/network/guru/DirectNetworkGuru.java
@@ -135,9 +135,8 @@ public class DirectNetworkGuru extends AdapterBase implements NetworkGuru {
 
     protected boolean canHandle(NetworkOffering offering, DataCenter dc, PhysicalNetwork physnet) {
         // this guru handles only Guest networks in Advance zone with source nat service disabled
-        if (dc.getNetworkType() == NetworkType.Advanced && isMyTrafficType(offering.getTrafficType()) && isMyIsolationMethod(physnet) && offering.getGuestType() == GuestType.Shared
-                && !_ntwkOfferingSrvcDao.isProviderForNetworkOffering(offering.getId(), Network.Provider.NuageVsp)
-                && !_ntwkOfferingSrvcDao.isProviderForNetworkOffering(offering.getId(), Network.Provider.NiciraNvp)) {
+        if (dc.getNetworkType() == NetworkType.Advanced && isMyTrafficType(offering.getTrafficType()) && offering.getGuestType() == GuestType.Shared &&
+            isMyIsolationMethod(physnet)) {
             return true;
         } else {
             s_logger.trace("We only take care of Guest networks of type " + GuestType.Shared);
