3e19ab76a5644c053caf60eca4277ef01c637394
Daan Hoogland <daan.hoogland@shapeblue.com>
2017-10-13 10:03:26 +0200

    CLOUDSTACK-10007 no isolationmethod check for directpodbase basic zone nets

--- server/src/com/cloud/network/guru/DirectPodBasedNetworkGuru.java
+++ server/src/com/cloud/network/guru/DirectPodBasedNetworkGuru.java
@@ -85,8 +85,8 @@ public class DirectPodBasedNetworkGuru extends DirectNetworkGuru {
 
     @Override
     protected boolean canHandle(NetworkOffering offering, DataCenter dc, PhysicalNetwork physnet) {
-        // this guru handles system Direct pod based network
-        if (dc.getNetworkType() == NetworkType.Basic && isMyTrafficType(offering.getTrafficType()) && isMyIsolationMethod(physnet)) {
+        // this guru handles system Direct pod based network in Basic zones only (no isolation type specified)
+        if (dc.getNetworkType() == NetworkType.Basic && isMyTrafficType(offering.getTrafficType())) {
             return true;
         } else {
             s_logger.trace("We only take care of Guest Direct Pod based networks");
