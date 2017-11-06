0e2297e70dfd651f6e09ea634e54b5cd4b8232b1
Daan Hoogland <daan.hoogland@shapeblue.com>
2017-11-06 13:41:24 +0100

    CLOUDSTACK-10121 cleanup api response name generation

--- api/src/org/apache/cloudstack/api/command/admin/user/MoveUserCmd.java
+++ api/src/org/apache/cloudstack/api/command/admin/user/MoveUserCmd.java
@@ -1,8 +1,25 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
 package org.apache.cloudstack.api.command.admin.user;
 
 import com.cloud.user.Account;
 import com.cloud.user.User;
 import com.google.common.base.Preconditions;
+import org.apache.cloudstack.acl.RoleType;
 import org.apache.cloudstack.api.APICommand;
 import org.apache.cloudstack.api.ApiConstants;
 import org.apache.cloudstack.api.ApiErrorCode;
@@ -19,11 +36,12 @@ import org.apache.log4j.Logger;
 import javax.inject.Inject;
 
 @APICommand(name = "moveUser", description = "Moves a user to another account", responseObject = SuccessResponse.class,
-        requestHasSensitiveInfo = false, responseHasSensitiveInfo = false)
+        requestHasSensitiveInfo = false, responseHasSensitiveInfo = false,
+        authorized = {RoleType.Admin})
 public class MoveUserCmd extends BaseCmd {
     public static final Logger s_logger = Logger.getLogger(UpdateUserCmd.class.getName());
 
-    private static final String s_name = "moveuseruserresponse";
+    public static final String APINAME = "moveUser";
 
     /////////////////////////////////////////////////////
     //////////////// API parameters /////////////////////
@@ -70,7 +88,7 @@ public class MoveUserCmd extends BaseCmd {
 
     @Override
     public String getCommandName() {
-        return s_name;
+        return APINAME.toLowerCase() + BaseCmd.RESPONSE_SUFFIX;
     }
 
     @Override
