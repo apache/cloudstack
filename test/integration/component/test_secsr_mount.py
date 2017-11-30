068e647c3270f4b91b4370fc9779dead230014e2
Rohit Yadav <bhaisaab@apache.org>
2017-11-30 14:06:25 +0530

    Fix test description

--- test/integration/component/test_secsr_mount.py
+++ test/integration/component/test_secsr_mount.py
@@ -15,7 +15,7 @@
 # specific language governing permissions and limitations
 # under the License.
 from __builtin__ import False
-""" BVT tests for Hosts Maintenance
+""" Tests for Secondary Storage with Local Storage
 """
 
 # Import Local Modules
