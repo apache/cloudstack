apache/cloudstack

commit 90ef67bab993c95b253cc810fb0b67fe8d6fc6e8

CLOUDSTACK-10109: Enable dedication of public IPs to SSVM and CPVM (#2295)
This feature allow admins to dedicate a range of public IP addresses to the SSVM and CPVM, such that they can be subject to specific external firewall rules. The option to dedicate a public IP range to the System VMs (SSVM & CPVM) is added to the createVlanIpRange API method and the UI.

Solution:
Global setting 'system.vm.public.ip.reservation.mode.strictness' is added to determine if the use of the system VM reservation is strict (when true) or preferred (false), false by default.
When a range has been dedicated to System VMs, CloudStack should apply IPs from that range to
the public interfaces of the CPVM and the SSVM depending on global setting's value:

If the global setting is set to false: then CloudStack will use any unused and unreserved public IP
addresses for system VMs only when the pool of reserved IPs has been exhausted
If the global setting is set to true: then CloudStack will fail to deploy the system VM when the pool
of reserved IPs has been exhausted, citing the lack of available IPs.
UI Changes
Under Infrastructure -> Zone -> Physical Network -> Public -> IP Ranges, button 'Account' label is refactored to 'Set reservation'.

When that button is clicked, dialog displayed is also refactored, including a new checkbox 'System VMs' which indicates if range should be dedicated for CPVM and SSVM, and a note indicating its usage.

When clicking on button for any created range, UI dialog displayed indicates whether IP range is dedicated for system vms or not.

server/src/com/cloud/network/IpAddressManagerImpl.java
@@ -295,6 +295,10 @@ public class IpAddressManagerImpl extends ManagerBase implements IpAddressManage
 
     static Boolean rulesContinueOnErrFlag = true;
 
+    private static final ConfigKey<Boolean> SystemVmPublicIpReservationModeStrictness = new ConfigKey<Boolean>("Advanced",
+            Boolean.class, "system.vm.public.ip.reservation.mode.strictness", "false",
+            "If enabled, the use of System VMs public IP reservation is strict, preferred if not.", false, ConfigKey.Scope.Global);
+
     @Override
     public boolean configure(String name, Map<String, Object> params) {
         // populate providers
@@ -395,6 +399,9 @@ public class IpAddressManagerImpl extends ManagerBase implements IpAddressManage
         AssignIpAddressSearch.and("dc", AssignIpAddressSearch.entity().getDataCenterId(), Op.EQ);
         AssignIpAddressSearch.and("allocated", AssignIpAddressSearch.entity().getAllocatedTime(), Op.NULL);
         AssignIpAddressSearch.and("vlanId", AssignIpAddressSearch.entity().getVlanId(), Op.IN);
+        if (SystemVmPublicIpReservationModeStrictness.value()) {
+            AssignIpAddressSearch.and("forSystemVms", AssignIpAddressSearch.entity().isForSystemVms(), Op.EQ);
+        }
         SearchBuilder<VlanVO> vlanSearch = _vlanDao.createSearchBuilder();
         vlanSearch.and("type", vlanSearch.entity().getVlanType(), Op.EQ);
         vlanSearch.and("networkId", vlanSearch.entity().getNetworkId(), Op.EQ);
@@ -675,20 +682,20 @@ public class IpAddressManagerImpl extends ManagerBase implements IpAddressManage
     }
 
     @Override
-    public PublicIp assignPublicIpAddress(long dcId, Long podId, Account owner, VlanType type, Long networkId, String requestedIp, boolean isSystem)
+    public PublicIp assignPublicIpAddress(long dcId, Long podId, Account owner, VlanType type, Long networkId, String requestedIp, boolean isSystem, boolean forSystemVms)
             throws InsufficientAddressCapacityException {
-        return fetchNewPublicIp(dcId, podId, null, owner, type, networkId, false, true, requestedIp, isSystem, null, null);
+        return fetchNewPublicIp(dcId, podId, null, owner, type, networkId, false, true, requestedIp, isSystem, null, null, forSystemVms);
     }
 
     @Override
     public PublicIp assignPublicIpAddressFromVlans(long dcId, Long podId, Account owner, VlanType type, List<Long> vlanDbIds, Long networkId, String requestedIp, boolean isSystem)
             throws InsufficientAddressCapacityException {
-        return fetchNewPublicIp(dcId, podId, vlanDbIds, owner, type, networkId, false, true, requestedIp, isSystem, null, null);
+        return fetchNewPublicIp(dcId, podId, vlanDbIds, owner, type, networkId, false, true, requestedIp, isSystem, null, null, false);
     }
 
     @DB
     public PublicIp fetchNewPublicIp(final long dcId, final Long podId, final List<Long> vlanDbIds, final Account owner, final VlanType vlanUse, final Long guestNetworkId,
-            final boolean sourceNat, final boolean assign, final String requestedIp, final boolean isSystem, final Long vpcId, final Boolean displayIp)
+            final boolean sourceNat, final boolean assign, final String requestedIp, final boolean isSystem, final Long vpcId, final Boolean displayIp, final boolean forSystemVms)
                     throws InsufficientAddressCapacityException {
         IPAddressVO addr = Transaction.execute(new TransactionCallbackWithException<IPAddressVO, InsufficientAddressCapacityException>() {
             @Override
@@ -758,7 +765,13 @@ public class IpAddressManagerImpl extends ManagerBase implements IpAddressManage
                     errorMessage.append(": requested ip " + requestedIp + " is not available");
                 }
 
-                Filter filter = new Filter(IPAddressVO.class, "vlanId", true, 0l, 1l);
+                boolean ascOrder = ! forSystemVms;
+                Filter filter = new Filter(IPAddressVO.class, "forSystemVms", ascOrder, 0l, 1l);
+                if (SystemVmPublicIpReservationModeStrictness.value()) {
+                    sc.setParameters("forSystemVms", forSystemVms);
+                }
+
+                filter.addOrderBy(IPAddressVO.class,"vlanId", true);
 
                 List<IPAddressVO> addrs = _ipAddressDao.search(sc, filter, false);
 
@@ -951,7 +964,13 @@ public class IpAddressManagerImpl extends ManagerBase implements IpAddressManage
                         VpcVO vpc = _vpcDao.findById(vpcId);
                         displayIp = vpc.isDisplay();
                     }
-                    return fetchNewPublicIp(dcId, null, null, owner, VlanType.VirtualNetwork, guestNtwkId, isSourceNat, true, null, false, vpcId, displayIp);
+                    PublicIp ip = fetchNewPublicIp(dcId, null, null, owner, VlanType.VirtualNetwork, guestNtwkId, isSourceNat, false, null, false, vpcId, displayIp, false);
+                    IPAddressVO publicIp = ip.ip();
+
+                    markPublicIpAsAllocated(publicIp);
+                    _ipAddressDao.update(publicIp.getId(), publicIp);
+
+                    return ip;
                 }
             });
             if (ip.getState() != State.Allocated) {
@@ -1147,7 +1166,7 @@ public class IpAddressManagerImpl extends ManagerBase implements IpAddressManage
             ip = Transaction.execute(new TransactionCallbackWithException<PublicIp, InsufficientAddressCapacityException>() {
                 @Override
                 public PublicIp doInTransaction(TransactionStatus status) throws InsufficientAddressCapacityException {
-                    PublicIp ip = fetchNewPublicIp(zone.getId(), null, null, ipOwner, vlanType, null, false, assign, null, isSystem, null, displayIp);
+                    PublicIp ip = fetchNewPublicIp(zone.getId(), null, null, ipOwner, vlanType, null, false, assign, null, isSystem, null, displayIp, false);
 
                     if (ip == null) {
                         InsufficientAddressCapacityException ex = new InsufficientAddressCapacityException("Unable to find available public IP addresses", DataCenter.class, zone
@@ -2009,7 +2028,7 @@ public class IpAddressManagerImpl extends ManagerBase implements IpAddressManage
                         }
 
                         if (ip == null) {
-                            ip = assignPublicIpAddress(dc.getId(), null, vm.getOwner(), VlanType.DirectAttached, network.getId(), requestedIpv4, false);
+                            ip = assignPublicIpAddress(dc.getId(), null, vm.getOwner(), VlanType.DirectAttached, network.getId(), requestedIpv4, false, false);
                         }
 
                         nic.setIPv4Address(ip.getAddress().toString());
@@ -2142,7 +2161,7 @@ public class IpAddressManagerImpl extends ManagerBase implements IpAddressManage
 
     @Override
     public String allocatePublicIpForGuestNic(Network network, Long podId, Account owner, String requestedIp) throws InsufficientAddressCapacityException {
-        PublicIp ip = assignPublicIpAddress(network.getDataCenterId(), podId, owner, VlanType.DirectAttached, network.getId(), requestedIp, false);
+        PublicIp ip = assignPublicIpAddress(network.getDataCenterId(), podId, owner, VlanType.DirectAttached, network.getId(), requestedIp, false, false);
         if (ip == null) {
             s_logger.debug("There is no free public ip address");
             return null;
@@ -2163,6 +2182,6 @@ public class IpAddressManagerImpl extends ManagerBase implements IpAddressManage
 
     @Override
     public ConfigKey<?>[] getConfigKeys() {
-        return new ConfigKey<?>[] {UseSystemPublicIps, RulesContinueOnError};
+        return new ConfigKey<?>[] {UseSystemPublicIps, RulesContinueOnError, SystemVmPublicIpReservationModeStrictness};
     }
 }

test/integration/smoke/test_public_ip_range.py
@@ -25,6 +25,8 @@ from marvin.lib.utils import *
 from marvin.lib.base import *
 from marvin.lib.common import *
 import datetime
+from socket import inet_aton
+from struct import unpack
 
 class TestDedicatePublicIPRange(cloudstackTestCase):
 
@@ -147,3 +149,241 @@ class TestDedicatePublicIPRange(cloudstackTestCase):
 
         return
 
+    @attr(tags = ["advanced", "publiciprange", "dedicate", "release"], required_hardware="false")
+    def test_dedicate_public_ip_range_for_system_vms(self):
+        """Test public IP range dedication for SSVM and CPVM
+        """
+
+        # Validate the following:
+        # 1. Create a Public IP range for system vms
+        # 2. Created IP range should be present and marked as forsystemvms=true, verify with listVlanIpRanges
+        # 7. Delete the Public IP range
+        
+        services = {
+            "gateway":"192.168.99.1",
+            "netmask":"255.255.255.0",
+            "startip":"192.168.99.2",
+            "endip":"192.168.99.200",
+            "forvirtualnetwork":self.services["forvirtualnetwork"],
+            "zoneid":self.services["zoneid"],
+            "vlan":self.services["vlan"]
+        }
+        public_ip_range = PublicIpRange.create(
+            self.apiclient,
+            services,
+            forsystemvms = True
+        )
+        created_ip_range_response = PublicIpRange.list(
+            self.apiclient,
+            id = public_ip_range.vlan.id
+        )
+        self.assertEqual(
+            len(created_ip_range_response),
+            1,
+            "Check listVlanIpRanges response"
+        )
+        self.assertTrue(
+            created_ip_range_response[0].forsystemvms,
+            "Check forsystemvms parameter in created vlan ip range"
+        )
+        
+        # Delete range
+        public_ip_range.delete(self.apiclient)
+        
+    def get_ip_as_number(self, ip_string):
+        """ Return numeric value for ip (passed as a string)
+        """
+        packed_ip = inet_aton(ip_string)
+        return unpack(">L", packed_ip)[0]
+    
+    def is_ip_in_range(self, start_ip, end_ip, ip_to_test):
+        """ Check whether ip_to_test belongs to IP range between start_ip and end_ip
+        """
+        start = self.get_ip_as_number(start_ip)
+        end = self.get_ip_as_number(end_ip)
+        ip = self.get_ip_as_number(ip_to_test)
+        return start <= ip and ip <= end
+    
+    def wait_for_system_vm_start(self, domain_id, srv_timeout, srv_sleep, systemvmtype):
+        """ Wait until system vm is Running
+        """
+        timeout = srv_timeout
+        while True:
+            list_systemvm_response = list_ssvms(
+                self.apiclient,
+                systemvmtype=systemvmtype,
+                domainid=domain_id
+            )
+            if isinstance(list_systemvm_response, list):
+                if list_systemvm_response[0].state == 'Running':
+                    return list_systemvm_response[0].id
+            if timeout == 0:
+                raise Exception("List System VM call failed!")
+
+            time.sleep(srv_sleep)
+            timeout = timeout - 1
+        return None
+
+    def base_system_vm(self, services, systemvmtype):
+        """
+        Base for CPVM or SSVM depending on systemvmtype parameter
+        """
+
+        # Create range for system vms
+        self.debug("Creating Public IP range for system vms")
+        public_ip_range = PublicIpRange.create(
+            self.apiclient,
+            services,
+            forsystemvms = True
+        )
+
+        # List Running System VM
+        list_systemvm_response = list_ssvms(
+            self.apiclient,
+            systemvmtype=systemvmtype,
+            state='Running',
+            domainid=public_ip_range.vlan.domainid
+        )
+        self.assertTrue(
+            isinstance(list_systemvm_response, list),
+            "Check list response returns a valid list"
+        )
+        self.assertEqual(
+            len(list_systemvm_response),
+            1,
+            "Check list response size"
+        )
+
+        # Delete System VM
+        systemvm = list_systemvm_response[0]
+        self.debug("Destroying System VM: %s" % systemvm.id)
+        cmd = destroySystemVm.destroySystemVmCmd()
+        cmd.id = systemvm.id
+        self.apiclient.destroySystemVm(cmd)
+
+        # Wait for CPVM to start
+        systemvm_id = self.wait_for_system_vm_start(
+            public_ip_range.vlan.domainid,
+            self.services["timeout"],
+            self.services["sleep"],
+            systemvmtype
+        )
+        self.assertNotEqual(
+            systemvm_id,
+            None,
+            "Check CPVM id is not none"
+        )
+        list_systemvm_response = list_ssvms(
+            self.apiclient,
+            id=systemvm_id
+        )
+        self.assertEqual(
+            isinstance(list_systemvm_response, list),
+            True,
+            "Check list response returns a valid list"
+        )
+        systemvm_response = list_systemvm_response[0]
+        self.debug("System VM state after debug: %s" % systemvm_response.state)
+        self.assertEqual(
+            systemvm_response.state,
+            'Running',
+            "Check whether System VM is running or not"
+        )
+
+        # Verify System VM got IP in the created range
+        startip = services["startip"]
+        endip = services["endip"]
+        cpvm_ip = systemvm_response.publicip
+
+        self.assertTrue(
+            self.is_ip_in_range(startip, endip, cpvm_ip),
+            "Check whether System VM Public IP is in range dedicated to system vms"
+        )
+
+        # Delete System VM and IP range, so System VM can get IP from original ranges
+        self.debug("Destroying System VM: %s" % systemvm_id)
+        cmd = destroySystemVm.destroySystemVmCmd()
+        cmd.id = systemvm_id
+        self.apiclient.destroySystemVm(cmd)
+
+        domain_id = public_ip_range.vlan.domainid
+        public_ip_range.delete(self.apiclient)
+
+        # Wait for System VM to start and check System VM public IP
+        systemvm_id = self.wait_for_system_vm_start(
+            domain_id,
+            self.services["timeout"],
+            self.services["sleep"],
+            systemvmtype
+        )
+        list_systemvm_response = list_ssvms(
+            self.apiclient,
+            id=systemvm_id
+        )
+        self.assertFalse(
+            self.is_ip_in_range(startip, endip, list_systemvm_response[0].publicip),
+            "Check System VM Public IP is not in range dedicated to system vms"
+        )
+
+        return True
+
+    def exists_public_ip_range_for_system_vms(self, zoneid):
+        """
+        Return True if there exists a public IP range dedicated for system vms in zoneid
+        """
+        existing_ip_ranges_response = PublicIpRange.list(
+            self.apiclient,
+            zoneid=zoneid
+        )
+        for r in existing_ip_ranges_response:
+            if r.forsystemvms:
+                return True
+        return False
+
+    @attr(tags = ["advanced", "publiciprange", "dedicate", "release"], required_hardware="false")
+    def test_dedicate_public_ip_range_for_system_vms_cpvm(self):
+        """Test CPVM Public IP
+        """
+        self.debug("Precondition: No public IP range dedicated for system vms in the environment")
+        if self.exists_public_ip_range_for_system_vms(self.services["zoneid"]):
+            self.skipTest("An existing IP range defined for system vms, aborting test")
+        
+        services = {
+            "gateway":"192.168.100.1",
+            "netmask":"255.255.255.0",
+            "startip":"192.168.100.2",
+            "endip":"192.168.100.200",
+            "forvirtualnetwork":self.services["forvirtualnetwork"],
+            "zoneid":self.services["zoneid"],
+            "vlan":self.services["vlan"]
+        }
+
+        self.base_system_vm(
+            services,
+            'consoleproxy'
+        )
+        return
+
+    @attr(tags = ["advanced", "publiciprange", "dedicate", "release"], required_hardware="false")
+    def test_dedicate_public_ip_range_for_system_vms_ssvm(self):
+        """Test SSVM Public IP
+        """
+        self.debug("Precondition: No public IP range dedicated for system vms in the environment")
+        if self.exists_public_ip_range_for_system_vms(self.services["zoneid"]):
+            self.skipTest("An existing IP range defined for system vms, aborting test")
+
+        services = {
+            "gateway":"192.168.200.1",
+            "netmask":"255.255.255.0",
+            "startip":"192.168.200.2",
+            "endip":"192.168.200.200",
+            "forvirtualnetwork":self.services["forvirtualnetwork"],
+            "zoneid":self.services["zoneid"],
+            "vlan":self.services["vlan"]
+        }
+
+        self.base_system_vm(
+            services,
+            'secondarystoragevm'
+        )
+        return

api/src/org/apache/cloudstack/api/command/admin/vlan/CreateVlanIpRangeCmd.java
@@ -112,6 +112,9 @@ public class CreateVlanIpRangeCmd extends BaseCmd {
     @Parameter(name = ApiConstants.IP6_CIDR, type = CommandType.STRING, description = "the CIDR of IPv6 network, must be at least /64")
     private String ip6Cidr;
 
+    @Parameter(name = ApiConstants.FOR_SYSTEM_VMS, type = CommandType.BOOLEAN, description = "true if IP range is set to system vms, false if not")
+    private Boolean forSystemVms;
+
     /////////////////////////////////////////////////////
     /////////////////// Accessors ///////////////////////
     /////////////////////////////////////////////////////
@@ -134,6 +137,10 @@ public class CreateVlanIpRangeCmd extends BaseCmd {
 
     public String getGateway() {
         return gateway;
+    }
+
+    public Boolean isForSystemVms() {
+        return forSystemVms == null ? Boolean.FALSE : forSystemVms;
     }
 
     public String getNetmask() {

api/src/org/apache/cloudstack/api/response/VlanIpRangeResponse.java
@@ -116,8 +116,20 @@ public class VlanIpRangeResponse extends BaseResponse implements ControlledEntit
     @Param(description = "the cidr of IPv6 network")
     private String ip6Cidr;
 
+    @SerializedName(ApiConstants.FOR_SYSTEM_VMS)
+    @Param(description = "indicates whether VLAN IP range is dedicated to system vms or not")
+    private Boolean forSystemVms;
+
     public void setId(String id) {
         this.id = id;
+    }
+
+    public Boolean getForSystemVms() {
+        return forSystemVms;
+    }
+
+    public void setForSystemVms(Boolean forSystemVms) {
+        this.forSystemVms = forSystemVms;
     }
 
     public void setForVirtualNetwork(Boolean forVirtualNetwork) {

engine/components-api/src/com/cloud/configuration/ConfigurationManager.java
@@ -213,7 +213,7 @@ public interface ConfigurationManager {
             boolean conserveMode, Map<Service, Map<Capability, String>> serviceCapabilityMap, boolean specifyIpRanges, boolean isPersistent,
             Map<NetworkOffering.Detail, String> details, boolean egressDefaultPolicy, Integer maxconn, boolean enableKeepAlive, Boolean forVpc);
 
-    Vlan createVlanAndPublicIpRange(long zoneId, long networkId, long physicalNetworkId, boolean forVirtualNetwork, Long podId, String startIP, String endIP,
+    Vlan createVlanAndPublicIpRange(long zoneId, long networkId, long physicalNetworkId, boolean forVirtualNetwork, boolean forSystemVms, Long podId, String startIP, String endIP,
         String vlanGateway, String vlanNetmask, String vlanId, boolean bypassVlanOverlapCheck, Domain domain, Account vlanOwner, String startIPv6, String endIPv6, String vlanIp6Gateway, String vlanIp6Cidr)
         throws InsufficientCapacityException, ConcurrentOperationException, InvalidParameterValueException;
 

engine/components-api/src/com/cloud/network/IpAddressManager.java
@@ -61,7 +61,7 @@ public interface IpAddressManager {
      * @return
      * @throws InsufficientAddressCapacityException
      */
-    PublicIp assignPublicIpAddress(long dcId, Long podId, Account owner, VlanType type, Long networkId, String requestedIp, boolean isSystem)
+    PublicIp assignPublicIpAddress(long dcId, Long podId, Account owner, VlanType type, Long networkId, String requestedIp, boolean isSystem, boolean forSystemVms)
             throws InsufficientAddressCapacityException;
 
     /**

engine/schema/resources/META-INF/db/schema-41000to41100.sql
@@ -524,6 +524,10 @@ ADD COLUMN `forsystemvms` TINYINT(1) NOT NULL DEFAULT '0' COMMENT 'Indicates if 
 ALTER TABLE `cloud`.`op_dc_ip_address_alloc`
 ADD COLUMN `vlan` INT(10) UNSIGNED NULL COMMENT 'Vlan the management network range is on';
 
+-- CLOUDSTACK-10109: Enable dedication of public IPs to SSVM and CPVM
+ALTER TABLE `cloud`.`user_ip_address`
+ADD COLUMN `forsystemvms` TINYINT(1) NOT NULL DEFAULT '0' COMMENT 'true if IP is set to system vms, false if not';
+
 -- ldap binding on domain level
 CREATE TABLE IF NOT EXISTS `cloud`.`domain_details` (
     `id` bigint unsigned NOT NULL auto_increment,
@@ -539,4 +543,3 @@ ALTER TABLE cloud.ldap_trust_map ADD COLUMN account_id BIGINT(20) DEFAULT 0;
 ALTER TABLE cloud.ldap_trust_map DROP FOREIGN KEY fk_ldap_trust_map__domain_id;
 DROP INDEX uk_ldap_trust_map__domain_id ON cloud.ldap_trust_map;
 CREATE UNIQUE INDEX uk_ldap_trust_map__bind_location ON ldap_trust_map (domain_id, account_id);
-

engine/schema/src/com/cloud/network/dao/IPAddressVO.java
@@ -122,6 +122,9 @@ public class IPAddressVO implements IpAddress {
     @Column(name = "rule_state")
     State ruleState;
 
+    @Column(name = "forsystemvms")
+    private boolean forSystemVms = false;
+
     @Column(name= GenericDao.REMOVED_COLUMN)
     private Date removed;
 
@@ -381,5 +384,9 @@ public class IPAddressVO implements IpAddress {
     @Override
     public void setRuleState(State ruleState) {
         this.ruleState = ruleState;
+    }
+
+    public boolean isForSystemVms() {
+        return forSystemVms;
     }
 }

commit 477eb43609481a012d52cf10ba7f7b24c23f0398

CLOUDSTACK-10109: Fix regression from PR #2295
This fixes regression introduced in PR #2295:
- Pass assign=true to fetch new public IP
- Use wait_until instead of sleep+wait in tests
- Loop through list of public IP ranges to match the systemvm gateway
- Fix potential NPE seen when adding simulator host(s)
- Removes aria2 installation from setup_agent.sh using yum, it's already
  dependency for cloudstack-agent package

Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>

plugins/storage/volume/solidfire/src/org/apache/cloudstack/storage/datastore/provider/SolidFireHostListener.java
@@ -74,6 +74,11 @@ public class SolidFireHostListener implements HypervisorHostListener {
     public boolean hostAdded(long hostId) {
         HostVO host = _hostDao.findById(hostId);
 
+        if (host == null) {
+            s_logger.error("Failed to add host by SolidFireHostListener as host was not found with id=" + hostId);
+            return false;
+        }
+
         SolidFireUtil.hostAddedToOrRemovedFromCluster(hostId, host.getClusterId(), true, SolidFireUtil.PROVIDER_NAME,
                 _clusterDao, _clusterDetailsDao, _storagePoolDao, _storagePoolDetailsDao, _hostDao);
 

plugins/storage/volume/solidfire/src/org/apache/cloudstack/storage/datastore/provider/SolidFireSharedHostListener.java
@@ -68,6 +68,11 @@ public class SolidFireSharedHostListener implements HypervisorHostListener {
     public boolean hostAdded(long hostId) {
         HostVO host = hostDao.findById(hostId);
 
+        if (host == null) {
+            LOGGER.error("Failed to add host by SolidFireSharedHostListener as host was not found with id=" + hostId);
+            return false;
+        }
+
         SolidFireUtil.hostAddedToOrRemovedFromCluster(hostId, host.getClusterId(), true, SolidFireUtil.SHARED_PROVIDER_NAME,
                 clusterDao, clusterDetailsDao, storagePoolDao, storagePoolDetailsDao, hostDao);
 

scripts/vm/hypervisor/kvm/setup_agent.sh
@@ -224,17 +224,5 @@ then
     setenforce 0
 fi
 
-which aria2c
-if [ $? -gt 0 ]
-then
-    yum install epel-release -y
-    yum install aria2 -y
-    if [ $? -gt 0 ]
-    then
-        printf "failed to install aria2"
-        exit 1
-    fi
-fi
-
 cloudstack-setup-agent --host=$host --zone=$zone --pod=$pod --cluster=$cluster --guid=$guid $paramters -a > /dev/null
 #cloud_consoleP_setup $host $zone $pod

server/src/com/cloud/network/IpAddressManagerImpl.java
@@ -964,13 +964,7 @@ public class IpAddressManagerImpl extends ManagerBase implements IpAddressManage
                         VpcVO vpc = _vpcDao.findById(vpcId);
                         displayIp = vpc.isDisplay();
                     }
-                    PublicIp ip = fetchNewPublicIp(dcId, null, null, owner, VlanType.VirtualNetwork, guestNtwkId, isSourceNat, false, null, false, vpcId, displayIp, false);
-                    IPAddressVO publicIp = ip.ip();
-
-                    markPublicIpAsAllocated(publicIp);
-                    _ipAddressDao.update(publicIp.getId(), publicIp);
-
-                    return ip;
+                    return fetchNewPublicIp(dcId, null, null, owner, VlanType.VirtualNetwork, guestNtwkId, isSourceNat, true, null, false, vpcId, displayIp, false);
                 }
             });
             if (ip.getState() != State.Allocated) {

test/integration/smoke/test_public_ip_range.py
@@ -204,25 +204,24 @@ class TestDedicatePublicIPRange(cloudstackTestCase):
         ip = self.get_ip_as_number(ip_to_test)
         return start <= ip and ip <= end
     
-    def wait_for_system_vm_start(self, domain_id, srv_timeout, srv_sleep, systemvmtype):
+    def wait_for_system_vm_start(self, domain_id, systemvmtype):
         """ Wait until system vm is Running
         """
-        timeout = srv_timeout
-        while True:
-            list_systemvm_response = list_ssvms(
+        def checkSystemVMUp():
+            response = list_ssvms(
                 self.apiclient,
                 systemvmtype=systemvmtype,
                 domainid=domain_id
             )
-            if isinstance(list_systemvm_response, list):
-                if list_systemvm_response[0].state == 'Running':
-                    return list_systemvm_response[0].id
-            if timeout == 0:
-                raise Exception("List System VM call failed!")
+            if isinstance(response, list):
+                if response[0].state == 'Running':
+                    return True, response[0].id
+            return False, None
 
-            time.sleep(srv_sleep)
-            timeout = timeout - 1
-        return None
+        res, systemvmId = wait_until(3, 100, checkSystemVMUp)
+        if not res:
+            raise Exception("Failed to wait for systemvm to be running")
+        return systemvmId
 
     def base_system_vm(self, services, systemvmtype):
         """
@@ -264,8 +263,6 @@ class TestDedicatePublicIPRange(cloudstackTestCase):
         # Wait for CPVM to start
         systemvm_id = self.wait_for_system_vm_start(
             public_ip_range.vlan.domainid,
-            self.services["timeout"],
-            self.services["sleep"],
             systemvmtype
         )
         self.assertNotEqual(
@@ -312,8 +309,6 @@ class TestDedicatePublicIPRange(cloudstackTestCase):
         # Wait for System VM to start and check System VM public IP
         systemvm_id = self.wait_for_system_vm_start(
             domain_id,
-            self.services["timeout"],
-            self.services["sleep"],
             systemvmtype
         )
         list_systemvm_response = list_ssvms(

test/integration/smoke/test_ssvm.py
@@ -348,9 +348,13 @@ class TestSSVMs(cloudstackTestCase):
                         self.apiclient,
                         physicalnetworkid=listphyntwk[0].id),
                     list) is True):
-                self.assertEqual(
-                    cpvm.gateway,
-                    iprange.gateway,
+                cpvmValidGateway = False
+                for iprange in ipranges_response:
+                    if iprange.gateway == cpvm.gateway:
+                        cpvmValidGateway = True
+                        break
+                self.assertTrue(
+                    cpvmValidGateway,
                     "Check gateway with that of corresponding ip range"
                 )
 