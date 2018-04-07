22f5a74cf927eaf19b150eca8c4ac8bfa144d6bb
zhenhaoli <gino@zhenhaolideair.wireless.concordia.ca>
2018-04-07 14:40:33 -0400

    update stack trace to make them consistent

--- api/src/main/java/org/apache/cloudstack/api/command/user/firewall/CreatePortForwardingRuleCmd.java
+++ api/src/main/java/org/apache/cloudstack/api/command/user/firewall/CreatePortForwardingRuleCmd.java
@@ -358,7 +358,7 @@ public class CreatePortForwardingRuleCmd extends BaseAsyncCreateCmd implements P
             setEntityId(result.getId());
             setEntityUuid(result.getUuid());
         } catch (NetworkRuleConflictException ex) {
-            s_logger.info("Network rule conflict: " + ex.getMessage());
+            s_logger.info("Network rule conflict: ", ex);
             s_logger.trace("Network Rule Conflict: ", ex);
             throw new ServerApiException(ApiErrorCode.NETWORK_RULE_CONFLICT_ERROR, ex.getMessage());
         }
