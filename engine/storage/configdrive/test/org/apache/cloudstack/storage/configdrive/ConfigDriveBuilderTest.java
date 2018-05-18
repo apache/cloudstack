57f2dfdfaddcd6ff3de44ae075b5978b1a06b293
Rohit Yadav <rohit.yadav@shapeblue.com>
2018-05-18 17:14:17 +0530

    cleanup and fix tests
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>

--- engine/storage/configdrive/test/org/apache/cloudstack/storage/configdrive/ConfigDriveBuilderTest.java
+++ engine/storage/configdrive/test/org/apache/cloudstack/storage/configdrive/ConfigDriveBuilderTest.java
@@ -1,24 +1,42 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 package org.apache.cloudstack.storage.configdrive;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.List;
 
-import org.junit.After;
-import org.junit.Before;
+import org.apache.commons.io.FileUtils;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class ConfigDriveBuilderTest {
-    @Before
-    public void setUp() throws Exception {
-    }
 
-    @After
-    public void tearDown() throws Exception {
+    @Test
+    public void testConfigDriveIsoPath() throws IOException {
+        Assert.assertEquals(ConfigDrive.createConfigDrivePath("i-x-y"), "configdrive/i-x-y/configdrive.iso");
     }
 
     @Test
-    public void testConfigDriveBuild() {
-
+    public void testConfigDriveBuild() throws IOException {
         List<String[]> actualVmData = Arrays.asList(
                 new String[]{"userdata", "user_data", "c29tZSB1c2VyIGRhdGE="},
                 new String[]{"metadata", "service-offering", "offering"},
@@ -33,6 +51,15 @@ public class ConfigDriveBuilderTest {
                 new String[]{"metadata", "cloud-identifier", String.format("CloudStack-{%s}", "uuid")},
                 new String[]{"password", "vm_password", "password123"}
         );
-        String encodedString = ConfigDriveBuilder.buildConfigDrive(actualVmData, "i-x-y.iso", "config-2");
+
+        final Path tempDir = Files.createTempDirectory(ConfigDrive.CONFIGDRIVEDIR);
+        final String isoData = ConfigDriveBuilder.buildConfigDrive(actualVmData, "i-x-y.iso", "config-2");
+        final File isoFile = ConfigDriveBuilder.base64StringToFile(isoData, tempDir.toAbsolutePath().toString(), ConfigDrive.CONFIGDRIVEFILENAME);
+
+        Assert.assertTrue(isoFile.exists());
+        Assert.assertTrue(isoFile.isFile());
+        Assert.assertTrue(isoFile.length() > 0L);
+
+        FileUtils.deleteDirectory(tempDir.toFile());
     }
 }
