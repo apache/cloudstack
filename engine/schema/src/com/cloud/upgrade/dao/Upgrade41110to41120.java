6f6e66ab1687292f8a31b6e5788f85be7587c719
Rohit Yadav <rohit.yadav@shapeblue.com>
2018-08-14 12:35:13 +0530

    address review comments
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>

--- engine/schema/src/com/cloud/upgrade/dao/Upgrade41110to41120.java
+++ engine/schema/src/com/cloud/upgrade/dao/Upgrade41110to41120.java
@@ -1,21 +1,19 @@
-/*
- * // Licensed to the Apache Software Foundation (ASF) under one
- * // or more contributor license agreements.  See the NOTICE file
- * // distributed with this work for additional information
- * // regarding copyright ownership.  The ASF licenses this file
- * // to you under the Apache License, Version 2.0 (the
- * // "License"); you may not use this file except in compliance
- * // with the License.  You may obtain a copy of the License at
- * //
- * //   http://www.apache.org/licenses/LICENSE-2.0
- * //
- * // Unless required by applicable law or agreed to in writing,
- * // software distributed under the License is distributed on an
- * // "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * // KIND, either express or implied.  See the License for the
- * // specific language governing permissions and limitations
- * // under the License.
- */
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
 
 package com.cloud.upgrade.dao;
 
@@ -172,7 +170,7 @@ public class Upgrade41110to41120 implements DbUpgrade {
                     }
                     // update template ID of system Vms
                     try (PreparedStatement update_templ_id_pstmt = conn
-                            .prepareStatement("update `cloud`.`vm_instance` set vm_template_id = ? where type <> 'User' and hypervisor_type = ?");) {
+                            .prepareStatement("update `cloud`.`vm_instance` set vm_template_id = ? where type <> 'User' and hypervisor_type = ? and removed is NULL");) {
                         update_templ_id_pstmt.setLong(1, templateId);
                         update_templ_id_pstmt.setString(2, hypervisorAndTemplateName.getKey().toString());
                         update_templ_id_pstmt.executeUpdate();
