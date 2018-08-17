8ed19288bed004f3b99e2216e2bae13b1591528c
Rafael Weingärtner <rafaelweingartner@gmail.com>
2018-08-17 09:10:50 -0300

    Remove Empty test class "SnapshotDataFactoryTest"

--- engine/storage/snapshot/src/test/java/org/apache/cloudstack/storage/vmsnapshot/VMSnapshotStrategyTest.java
+++ engine/storage/snapshot/src/test/java/org/apache/cloudstack/storage/vmsnapshot/VMSnapshotStrategyTest.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package src;
+package org.apache.cloudstack.storage.vmsnapshot;
 
 import java.io.IOException;
 import java.util.ArrayList;
