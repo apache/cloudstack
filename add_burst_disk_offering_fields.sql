03b4d0a4e63b5ff9f64634541543d86aab4cf5d2
Nathan Johnson <njohnson@ena.com>
2018-09-06 16:44:04 -0500

    updating sql for disk_offering_view
    Update the DDL for the view.

--- add_burst_disk_offering_fields.sql
+++ add_burst_disk_offering_fields.sql
@@ -6,3 +6,47 @@ alter table disk_offering add iops_read_rate_max bigint(20) default null after i
 alter table disk_offering add iops_read_rate_max_length bigint(20) default null after iops_read_rate_max;
 alter table disk_offering add iops_write_rate_max bigint(20) default null after iops_write_rate;
 alter table disk_offering add iops_write_rate_max_length bigint(20) default null after iops_write_rate_max;
+
+
+ALTER VIEW `disk_offering_view` AS
+    SELECT 
+        `disk_offering`.`id` AS `id`,
+        `disk_offering`.`uuid` AS `uuid`,
+        `disk_offering`.`name` AS `name`,
+        `disk_offering`.`display_text` AS `display_text`,
+        `disk_offering`.`provisioning_type` AS `provisioning_type`,
+        `disk_offering`.`disk_size` AS `disk_size`,
+        `disk_offering`.`min_iops` AS `min_iops`,
+        `disk_offering`.`max_iops` AS `max_iops`,
+        `disk_offering`.`created` AS `created`,
+        `disk_offering`.`tags` AS `tags`,
+        `disk_offering`.`customized` AS `customized`,
+        `disk_offering`.`customized_iops` AS `customized_iops`,
+        `disk_offering`.`removed` AS `removed`,
+        `disk_offering`.`use_local_storage` AS `use_local_storage`,
+        `disk_offering`.`system_use` AS `system_use`,
+        `disk_offering`.`hv_ss_reserve` AS `hv_ss_reserve`,
+        `disk_offering`.`bytes_read_rate` AS `bytes_read_rate`,
+        `disk_offering`.`bytes_read_rate_max` AS `bytes_read_rate_max`,
+        `disk_offering`.`bytes_read_rate_max_length` AS `bytes_read_rate_max_length`,
+        `disk_offering`.`bytes_write_rate_max` AS `bytes_write_rate_max`,
+        `disk_offering`.`bytes_write_rate_max_length` AS `bytes_write_rate_max_length`,
+        `disk_offering`.`iops_read_rate` AS `iops_read_rate`,
+        `disk_offering`.`iops_read_rate_max` AS `iops_read_rate_max`,
+        `disk_offering`.`iops_read_rate_max_length` AS `iops_read_rate_max_length`,
+        `disk_offering`.`iops_write_rate` AS `iops_write_rate`,
+        `disk_offering`.`iops_write_rate_max` AS `iops_write_rate_max`,
+        `disk_offering`.`iops_write_rate_max_length` AS `iops_write_rate_max_length`,
+        `disk_offering`.`cache_mode` AS `cache_mode`,
+        `disk_offering`.`sort_key` AS `sort_key`,
+        `disk_offering`.`type` AS `type`,
+        `disk_offering`.`display_offering` AS `display_offering`,
+        `domain`.`id` AS `domain_id`,
+        `domain`.`uuid` AS `domain_uuid`,
+        `domain`.`name` AS `domain_name`,
+        `domain`.`path` AS `domain_path`
+    FROM
+        (`disk_offering`
+        LEFT JOIN `domain` ON ((`disk_offering`.`domain_id` = `domain`.`id`)))
+    WHERE
+        (`disk_offering`.`state` = 'ACTIVE');
