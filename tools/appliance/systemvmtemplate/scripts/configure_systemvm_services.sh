a27c3899e69471081c850e571530e9738c38042d
Paul Angus <paul.angus@shapeblue.com>
2018-12-23 11:10:55 +0000

    clear screen before boot screen

--- tools/appliance/systemvmtemplate/scripts/configure_systemvm_services.sh
+++ tools/appliance/systemvmtemplate/scripts/configure_systemvm_services.sh
@@ -63,6 +63,7 @@ function do_signature() {
 
 function configure_issue() {
   cat > /etc/issue <<EOF
+ESC [ 2J
    __?.o/  Apache CloudStack SystemVM $CLOUDSTACK_RELEASE
   (  )#    https://cloudstack.apache.org
  (___(_)   Debian GNU/Linux 9 \n \l
