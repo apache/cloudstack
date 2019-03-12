1a1566372014ce78b1f727b04ae14f2ef8bc8302
Rohit Yadav <rohit.yadav@shapeblue.com>
2019-03-12 11:04:26 +0530

    move StorageSubSystemCommand instanceof check above as CopyCommand is a type of StorageSubSystemCommand
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>

--- server/src/main/java/com/cloud/hypervisor/KVMGuru.java
+++ server/src/main/java/com/cloud/hypervisor/KVMGuru.java
@@ -126,6 +126,10 @@ public class KVMGuru extends HypervisorGuruBase implements HypervisorGuru {
 
     @Override
     public Pair<Boolean, Long> getCommandHostDelegation(long hostId, Command cmd) {
+        if (cmd instanceof StorageSubSystemCommand) {
+            StorageSubSystemCommand c = (StorageSubSystemCommand)cmd;
+            c.setExecuteInSequence(false);
+        }
         if (cmd instanceof CopyCommand) {
             CopyCommand c = (CopyCommand) cmd;
             boolean inSeq = true;
@@ -140,10 +144,6 @@ public class KVMGuru extends HypervisorGuruBase implements HypervisorGuru {
             if (c.getSrcTO().getHypervisorType() == HypervisorType.KVM) {
                 return new Pair<>(true, hostId);
             }
-        }
-        if (cmd instanceof StorageSubSystemCommand) {
-            StorageSubSystemCommand c = (StorageSubSystemCommand)cmd;
-            c.setExecuteInSequence(false);
         }
         return new Pair<>(false, hostId);
     }
