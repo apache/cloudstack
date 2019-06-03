cd6e84659e77bfcc5928738f0571f565476636fb
Rohit Yadav <rohit.yadav@shapeblue.com>
2019-06-03 17:29:43 +0530

    Revert "Fix template size for managed storage / refactor cloud-install-sys-tmplt and createtmplt.sh (#3346)"
    This reverts commit 6946f41784423788e71441255e67c2b9b1c3633d because it
    adds regression to deployment and testing. Testing against VMware, KVM
    and XenServer confirm failure to deploy an environment where systemvms
    don't come online.

--- scripts/storage/secondary/createtmplt.sh
+++ scripts/storage/secondary/createtmplt.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-
+#!/usr/bin/env bash
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
@@ -7,9 +6,9 @@
 # to you under the Apache License, Version 2.0 (the
 # "License"); you may not use this file except in compliance
 # with the License.  You may obtain a copy of the License at
-#
+# 
 #   http://www.apache.org/licenses/LICENSE-2.0
-#
+# 
 # Unless required by applicable law or agreed to in writing,
 # software distributed under the License is distributed on an
 # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -17,31 +16,41 @@
 # specific language governing permissions and limitations
 # under the License.
 
+ 
+
+# $Id: createtmplt.sh 9132 2010-06-04 20:17:43Z manuel $ $HeadURL: svn://svn.lab.vmops.com/repos/vmdev/java/scripts/storage/secondary/createtmplt.sh $
+# createtmplt.sh -- install a template
 
 usage() {
-  printf "\nUsage: %s:\n\t-t templatefilesystem\n\t-n templatename\n\t-f templatefile\n\n" $(basename $0) >&2
+  printf "Usage: %s: -t <template-fs> -n <templatename> -f <root disk file> -d <descr> -h  [-u] [-v]\n" $(basename $0) >&2
 }
+
 
 #set -x
 ulimit -c 0
 
-# Usage: e.g. failed $? "this is an error"
-failed() {
-  local returnval=$1
-  local returnmsg=$2
-  
-  # check for an message, if there is no one dont print anything
-  if [[ -z $returnmsg ]]; then
-    :
-  else
-    echo -e $returnmsg
-  fi
-  if [[ $returnval -eq 0 ]]; then
-    return 0
-  else
-    echo "Installation failed"
-    exit $returnval
-  fi
+rollback_if_needed() {
+  if [ $2 -gt 0 ]
+  then
+    printf "$3\n"
+    #back out all changes
+    rm -rf $1
+    exit 2
+fi
+}
+
+untar() {
+  local ft=$(file $1| awk -F" " '{print $2}')
+  case $ft in
+  USTAR) 
+     printf "tar archives not supported\n"  >&2
+     return 1
+          ;;
+  *) printf "$1"
+     return 0
+	  ;;
+  esac
+
 }
 
 is_compressed() {
@@ -79,12 +88,12 @@ uncompress() {
 	;;
   esac
 
-  if [[ $? -gt 0 ]]; then
-    printf "Failed to uncompress file (filetype=$ft), exiting"
-    rm -f $tmpfile
-    return 1
+  if [ $? -gt 0 ] 
+  then
+    printf "Failed to uncompress file (filetype=$ft), exiting "
+    return 1 
   fi
-
+ 
   rm -f $1
   printf $tmpfile
 
@@ -96,22 +105,21 @@ create_from_file() {
   local tmpltimg=$2
   local tmpltname=$3
 
-  echo "Moving to $tmpltfs/$tmpltname...could take a while" >&2
+  [ -n "$verbose" ] && echo "Moving to $tmpltfs/$tmpltname...could take a while" >&2
   mv $tmpltimg /$tmpltfs/$tmpltname
+
 }
 
 tflag=
 nflag=
 fflag=
+sflag=
+hflag=
+hvm=false
+cleanup=false
+dflag=
 
-# check if first parameter is not a dash (-) then print the usage block
-if [[ ! $@ =~ ^\-.+ ]]; then
-	usage
-	exit 0
-fi
-
-OPTERR=0
-while getopts 't:n:f:' OPTION
+while getopts 'vuht:n:f:s:d:S:' OPTION
 do
   case $OPTION in
   t)	tflag=1
@@ -123,6 +131,23 @@ do
   f)	fflag=1
 		tmpltimg="$OPTARG"
 		;;
+  s)	sflag=1
+		;;
+  d)	dflag=1
+		descr="$OPTARG"
+		;;
+  S)	Sflag=1
+		size=$OPTARG
+                let "size>>=10"
+		ulimit -f $size
+		;;
+  h)	hflag=1
+		hvm="true"
+		;;
+  u)	cleanup="true"
+		;;
+  v)	verbose="true"
+		;;
   ?)	usage
 		exit 2
 		;;
@@ -130,59 +155,68 @@ do
 done
 
 isCifs() {
-   #TO:DO incase of multiple zone where cifs and nfs exists,
+   #TO:DO incase of multiple zone where cifs and nfs exists, 
    #then check if the template file is from cifs using df -P filename
    #Currently only cifs is supported in hyperv zone.
    mount | grep "type cifs" > /dev/null
    echo $?
 }
 
-findtmpltype() {
-  local ft=$(file $1| awk -F" " '{print $2}')
-
-  case $ft in
-  Microsoft)
-    if [[ $(isCifs) -ne 0 ]]; then
-      if [[ $(which vhd-util &>/dev/null) ]]; then
-        vhd-util read -p -n ${tmpltimg2} > /dev/null
-        failed $? "vhd check of $tmpltimg2 failed\n"
-        vhd-util set -n ${tmpltimg2} -f "hidden" -v "0" > /dev/null
-        failed $? "vhd remove $tmpltimg2 hidden failed\n"
-      else
-        failed 2 "Please install vhd-util command"
-      fi
-    fi
-    ;;
-  POSIX)
-    tar xvf $tmpltimg2 -C $tmpltfs &> /dev/null
-    failed $? "untar $tmpltimg2 to $tmpltfs"
-    ;;
-  x86)
-    :
-    ;;
-  QEMU)
-    :
-    ;;
-  esac
-}
-
-if [[ "$tflag$nflag$fflag" != "111" ]]; then
-  usage
-  exit 2
+if [ "$tflag$nflag$fflag$sflag" != "1111" ]
+then
+ usage
+ exit 2
 fi
 
 mkdir -p $tmpltfs
 
-if [[ ! -f $tmpltimg ]]; then
-  printf "template file $tmpltimg doesn't exist\n"
-  exit 2
+if [ ! -f $tmpltimg ] 
+then
+  printf "root disk file $tmpltimg doesn't exist\n"
+  exit 3
 fi
 
-is_compressed $tmpltimg
+[ -n "$verbose" ] && is_compressed $tmpltimg
 tmpltimg2=$(uncompress $tmpltimg)
+rollback_if_needed $tmpltfs $? "failed to uncompress $tmpltimg\n"
 
-findtmpltype $tmpltimg2
+tmpltimg2=$(untar $tmpltimg2)
+rollback_if_needed $tmpltfs $? "tar archives not supported\n"
+
+if [ ${tmpltname%.vhd} != ${tmpltname} ]
+then
+  if [ $(isCifs) -ne 0 ] ;
+  then
+      if  which  vhd-util &>/dev/null
+      then
+        vhd-util read -p -n ${tmpltimg2} > /dev/null
+        rollback_if_needed $tmpltfs $? "vhd check of $tmpltimg2 failed\n"
+        vhd-util set -n ${tmpltimg2} -f "hidden" -v "0" > /dev/null
+        rollback_if_needed $tmpltfs $? "vhd remove $tmpltimg2 hidden failed\n"
+     fi
+  fi
+fi
+
+imgsize=$(ls -l $tmpltimg2| awk -F" " '{print $5}')
 
 create_from_file $tmpltfs $tmpltimg2 $tmpltname
+
+touch /$tmpltfs/template.properties
+rollback_if_needed $tmpltfs $? "Failed to create template.properties file"
+echo -n "" > /$tmpltfs/template.properties
+
+today=$(date '+%m_%d_%Y')
+echo "filename=$tmpltname" > /$tmpltfs/template.properties
+echo "description=$descr" >> /$tmpltfs/template.properties
+# we need to rethink this property as it might get changed after download due to decompression
+# option is to recalcutate it here
+echo "checksum=$cksum" >> /$tmpltfs/template.properties
+echo "hvm=$hvm" >> /$tmpltfs/template.properties
+echo "size=$imgsize" >> /$tmpltfs/template.properties
+
+if [ "$cleanup" == "true" ]
+then
+  rm -f $tmpltimg
+fi
 
 exit 0
