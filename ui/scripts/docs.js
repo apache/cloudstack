a12ac14c6c707d8901a3a978e6f50c8df84a4594
Nico Wohlfarth <n.wohlfarth@ewerk.com>
2019-06-25 18:18:03 +0200

    ui: add new way to create a volume snapshot from instance quick view tooltip (#3406)
    Added a shortcut to take a volume snapshot of a VM directly from the instance view. Therefor an option was added to the quick view tooltip. On activation a new dialog will pop up, which is based on the original dialog for creating a volume snapshot (Storage > Volumes > Take snapshot) extended with a select menu to choose the desired snapshot volume of the VM.

--- ui/scripts/docs.js
+++ ui/scripts/docs.js
@@ -310,6 +310,13 @@ cloudStack.docs = {
     helpCreateInstanceSnapshotMemory: {
         desc: 'Check this to include CPU/memory state. Does not quiesce the VM. If not checked, the snapshot contain only volumes.'
     },
+    // Create instance storage snapshot
+    helpCreateInstanceStorageSnapshotVolume: {
+        desc: 'Choose a volume that you want to take a snapshot of'
+    },
+    helpCreateInstanceStorageSnapshotName: {
+        desc: 'Give the snapshot a name. A unique name will be automatically generated if you leave this blank'
+    },
     // Add disk offering
     helpDiskOfferingName: {
         desc: 'Any desired name for the offering',
