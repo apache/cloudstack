1ef05984c66068c92a3ef369e25a1901fc50055b
Rohit Yadav <rohit.yadav@shapeblue.com>
2019-06-25 17:10:48 +0530

    vmware: add support for VMware 6.7 (#3413)
    This adds support for VMware 6.7
    Fixes #2700
    
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>

--- engine/schema/src/main/resources/META-INF/db/schema-41200to41300.sql
+++ engine/schema/src/main/resources/META-INF/db/schema-41200to41300.sql
@@ -19,6 +19,10 @@
 -- Schema upgrade from 4.12.0.0 to 4.13.0.0
 --;
 
+-- Add support for VMware 6.7
+INSERT IGNORE INTO `cloud`.`hypervisor_capabilities` (uuid, hypervisor_type, hypervisor_version, max_guests_limit, security_group_enabled, max_data_volumes_limit, max_hosts_per_cluster, storage_motion_supported, vm_snapshot_enabled) values (UUID(), 'VMware', '6.7', 128, 0, 13, 32, 1, 1);
+INSERT IGNORE INTO `cloud`.`guest_os_hypervisor` (uuid,hypervisor_type, hypervisor_version, guest_os_name, guest_os_id, created, is_user_defined) SELECT UUID(),'VMware', '6.7', guest_os_name, guest_os_id, utc_timestamp(), 0  FROM `cloud`.`guest_os_hypervisor` WHERE hypervisor_type='VMware' AND hypervisor_version='6.5';
+
 -- DPDK client and server mode support
 ALTER TABLE `cloud`.`service_offering_details` CHANGE COLUMN `value` `value` TEXT NOT NULL;
 
