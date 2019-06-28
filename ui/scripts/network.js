c160e7532aed9a85f7a1c12892c2e139a817cf54
Rohit Yadav <rohit.yadav@shapeblue.com>
2019-06-29 00:23:36 +0530

    ui: fix LB protocol bug (#3438)
    Fixes #3252
    
    This fixes labels for missing LB protocol(s).
    
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>

--- ui/scripts/network.js
+++ ui/scripts/network.js
@@ -3393,6 +3393,10 @@
                                                             name: 'tcp',
                                                             description: _l('label.lb.protocol.tcp')
                                                         }, {
+                                                            id: 'tcp-proxy',
+                                                            name: 'tcp-proxy',
+                                                            description: _l('label.lb.protocol.tcp.proxy')
+                                                        }, {
                                                             id: 'udp',
                                                             name: 'udp',
                                                             description: _l('label.lb.protocol.udp')
