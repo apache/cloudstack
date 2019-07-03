b72aadb65dbe88a52c1209f5b205889efc83b171
Paul Angus <paul.angus@shapeblue.com>
2019-07-03 09:28:09 +0100

    Updating pom.xml version numbers for release 4.11.4.0-SNAPSHOT
    Signed-off-by: Paul Angus <paul.angus@shapeblue.com>

--- tools/marvin/setup.py
+++ tools/marvin/setup.py
@@ -27,7 +27,7 @@ except ImportError:
         raise RuntimeError("python setuptools is required to build Marvin")
 
 
-VERSION = "4.11.3.0"
+VERSION = "4.11.4.0-SNAPSHOT"
 
 setup(name="Marvin",
       version=VERSION,
