9f18b5b620cc2f619138d7f21f0cb1dcba698e40
EK <20902920+kioie@users.noreply.github.com>
2019-07-04 12:19:32 +0300

    ui: allow for the VM Hostname to be edited  when VM is switched off  (#3412)
    Allow for VM name (hostname) to be edited when VM is switched off.

--- ui/scripts/instances.js
+++ ui/scripts/instances.js
@@ -1477,6 +1477,11 @@
                                     displayName: args.data.displayname
                                 });
                             }
+                            if (args.data.name != args.context.instances[0].name) {
+                                $.extend(data, {
+                                    name: args.data.name
+                                });
+                            }
                             $.ajax({
                                 url: createURL('updateVirtualMachine'),
                                 data: data,
@@ -2798,7 +2803,8 @@
                                 converter: cloudStack.converters.toLocalDate
                             },
                             name: {
-                                label: 'label.name'
+                                label: 'label.name',
+                                isEditable: true
                             },
                             id: {
                                 label: 'label.id'
