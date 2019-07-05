7f2325ca67be7088f8c175b28d797152340973b2
Rohit Yadav <rohit.yadav@shapeblue.com>
2019-07-06 02:27:37 +0530

    add stub views
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>

---
+++ ui/src/views/compute/Instance.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: '',
+  components: {
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+</style>
