23b2e013fa675f57f4a58757d9ec4440d31123fa
Rohit Yadav <rohit.yadav@shapeblue.com>
2019-07-31 13:47:54 +0530

    show template progress when template belong to a project
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>

--- server/src/main/java/com/cloud/api/query/dao/TemplateJoinDaoImpl.java
+++ server/src/main/java/com/cloud/api/query/dao/TemplateJoinDaoImpl.java
@@ -166,8 +166,10 @@ public class TemplateJoinDaoImpl extends GenericDaoBaseWithTagInformation<Templa
         templateResponse.setDomainId(template.getDomainUuid());
         templateResponse.setDomainName(template.getDomainName());
 
-        // If the user is an 'Admin' or 'the owner of template', add the template download status
-        if (view == ResponseView.Full ||  template.getAccountId() == CallContext.current().getCallingAccount().getId() ) {
+        // If the user is an 'Admin' or 'the owner of template' or template belongs to a project, add the template download status
+        if (view == ResponseView.Full ||
+                template.getAccountId() == CallContext.current().getCallingAccount().getId() ||
+                template.getAccountType() == Account.ACCOUNT_TYPE_PROJECT) {
             String templateStatus = getTemplateStatus(template);
             if (templateStatus != null) {
                 templateResponse.setStatus(templateStatus);
