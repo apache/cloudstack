5e8df47e6e37bf09267f1bfb970922946ae45b57
Philipp Bankonier <philipp.bankonier@gmail.com>
2019-08-05 17:08:58 +0200

    login: Remove console output of login credentials (#5)
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>

--- ui/src/views/user/Login.vue
+++ ui/src/views/user/Login.vue
@@ -165,7 +165,6 @@ export default {
 
       validateFields(validateFieldsKey, { force: true }, (err, values) => {
         if (!err) {
-          console.log('login form', values)
           const loginParams = { ...values }
           delete loginParams.username
           loginParams[!state.loginType ? 'email' : 'username'] = values.username
