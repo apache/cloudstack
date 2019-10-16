ddab0b9fe51a13d99e1af06873d495f30eab7601
Rohit Yadav <rohit.yadav@shapeblue.com>
2019-10-16 17:35:45 +0530

    locales: fix extra spaces issue
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>

--- ui/src/locales/index.js
+++ ui/src/locales/index.js
@@ -2,23 +2,23 @@ import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 
 function loadLocaleMessages () {
-  const locales = require.context('./', true, /[A-Za-z0-9-_,\s]+\.json$/i)
-  const messages = {}
-  locales.keys().forEach(key => {
-    const matched = key.match(/([A-Za-z0-9-_]+)\./i)
-    if (matched && matched.length > 1) {
-      const locale = matched[1]
-      messages[locale] = locales(key)
-    }
-  })
-  return messages
+const locales = require.context('./', true, /[A-Za-z0-9-_,\s]+\.json$/i)
+const messages = {}
+locales.keys().forEach(key => {
+  const matched = key.match(/([A-Za-z0-9-_]+)\./i)
+  if (matched && matched.length > 1) {
+    const locale = matched[1]
+    messages[locale] = locales(key)
+  }
+})
+return messages
 }
 
 Vue.use(VueI18n)
 
 export default new VueI18n({
-  locale: Vue.ls ? Vue.ls.get('current_locale') || 'en' : 'en',
-  fallbackLocale: 'en',
-  silentTranslationWarn: true,
-  messages: loadLocaleMessages()
+locale: Vue.ls ? Vue.ls.get('current_locale') || 'en' : 'en',
+fallbackLocale: 'en',
+silentTranslationWarn: true,
+messages: loadLocaleMessages()
 })
