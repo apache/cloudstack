716ea3d1212a2f919e7464e7c9cf11b8e27828bf
Abhishek Kumar <abhishek.mrt22@gmail.com>
2020-01-06 02:55:11 +0530

    added smoke test for kubernetes version
    Signed-off-by: Abhishek Kumar <abhishek.mrt22@gmail.com>

---
+++ plugins/integrations/kubernetes-service/test/integration/smoke/test_kubernetes_supported_versions.py
@@ -0,0 +1,230 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+""" Tests for Kubernetes supported version """
+
+#Import Local Modules
+from marvin.cloudstackTestCase import cloudstackTestCase, unittest
+from marvin.cloudstackAPI import (listKubernetesSupportedVersions,
+                                  addKubernetesSupportedVersion,
+                                  deleteKubernetesSupportedVersion)
+from marvin.cloudstackException import CloudstackAPIException
+from marvin.codes import FAILED
+from marvin.lib.utils import (cleanup_resources,
+                              random_gen)
+from marvin.lib.common import get_zone
+from nose.plugins.attrib import attr
+
+import time
+
+_multiprocess_shared_ = True
+
+class TestKubernetesSupportedVersion(cloudstackTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        testClient = super(TestKubernetesSupportedVersion, cls).getClsTestClient()
+        cls.apiclient = testClient.getApiClient()
+        cls.services = testClient.getParsedTestDataConfig()
+        cls.zone = get_zone(cls.apiclient, cls.testClient.getZoneForTests())
+        cls.kubernetes_version_iso_url = 'http://172.20.0.1/files/setup-1.16.3.iso'
+        cls._cleanup = []
+        return
+
+    @classmethod
+    def tearDownClass(cls):
+        try:
+            cleanup_resources(cls.apiclient, cls._cleanup)
+        except Exception as e:
+            raise Exception("Warning: Exception during cleanup : %s" % e)
+        return
+
+    def setUp(self):
+        self.services = self.testClient.getParsedTestDataConfig()
+        self.apiclient = self.testClient.getApiClient()
+        self.dbclient = self.testClient.getDbConnection()
+        self.cleanup = []
+        return
+
+    def tearDown(self):
+        try:
+            #Clean up, terminate the created templates
+            cleanup_resources(self.apiclient, self.cleanup)
+
+        except Exception as e:
+            raise Exception("Warning: Exception during cleanup : %s" % e)
+        return
+
+    @attr(tags=["advanced", "smoke"], required_hardware="true")
+    def test_01_add_delete_kubernetes_supported_version(self):
+        """Test to add a new Kubernetes supported version
+
+        # Validate the following:
+        # 1. addKubernetesSupportedVersion should return valid info for new version
+        # 2. The Cloud Database contains the valid information when listKubernetesSupportedVersions is called
+        """
+
+        version = '1.16.3'
+        name = 'v' + version + '-' + random_gen()
+
+        self.debug("Adding Kubernetes supported version with name: %s" % name)
+
+        version_response = self.addKubernetesSupportedVersionUsingDetails(version, name, self.zone.id, self.kubernetes_version_iso_url)
+
+        list_versions_response = self.listKubernetesSupportedVersionId(version_response.id)
+
+        self.assertEqual(
+            list_versions_response.name,
+            name,
+            "Check KubernetesSupportedVersion name {}, {}".format(list_versions_response.name, name)
+        )
+
+        self.assertEqual(
+            list_versions_response.semanticversion,
+            version,
+            "Check KubernetesSupportedVersion version {}, {}".format(list_versions_response.semanticversion, version)
+        )
+        self.assertEqual(
+            list_versions_response.zoneid,
+            self.zone.id,
+            "Check KubernetesSupportedVersion zone {}, {}".format(list_versions_response.zoneid, self.zone.id)
+        )
+
+        db_version_name = self.dbclient.execute("select name from kubernetes_supported_version where uuid = '%s';" % version_response.id)[0][0]
+
+        self.assertEqual(
+            str(db_version_name),
+            name,
+            "Check KubernetesSupportedVersion name in DB {}, {}".format(db_version_name, name)
+        )
+
+        self.debug("Added Kubernetes supported version with ID: %s. Waiting for its ISO to be Ready" % version_response.id)
+
+        self.checkKubernetesSupportedVersionIsoState(version_response.id)
+
+        self.debug("Deleting Kubernetes supported version with ID: %s" % version_response.id)
+
+        delete_response = self.deleteKubernetesSupportedVersionId(version_response.id, True)
+
+        self.assertEqual(
+            delete_response.success,
+            True,
+            "Check KubernetesSupportedVersion deletion in DB {}, {}".format(delete_response.success, True)
+        )
+
+        db_version_removed = self.dbclient.execute("select removed from kubernetes_supported_version where uuid = '%s';" % version_response.id)[0][0]
+
+        self.assertNotEqual(
+            db_version_removed,
+            None,
+            "KubernetesSupportedVersion not removed in DB"
+        )
+
+        return
+
+    @attr(tags=["advanced", "smoke"], required_hardware="true")
+    def test_02_add_unsupported_kubernetes_supported_version(self):
+        """Test to trying to add a new unsupported Kubernetes supported version
+
+        # Validate the following:
+        # 1. API should return an error
+        """
+
+        version = '1.1.1'
+        name = 'v' + version + '-' + random_gen()
+        try:
+            version_response = self.addKubernetesSupportedVersionUsingDetails(version, name, self.zone.id, self.kubernetes_version_iso_url)
+            self.fail("Kubernetes supported version below version 1.11.0 been added. Must be an error.")
+        except CloudstackAPIException as e:
+            self.debug("Unsupported version error check successful, API failure: %s" % e)
+        return
+
+    @attr(tags=["advanced", "smoke"], required_hardware="true")
+    def test_03_add_invalid_kubernetes_supported_version(self):
+        """Test to trying to add a new unsupported Kubernetes supported version
+
+        # Validate the following:
+        # 1. API should return an error
+        """
+
+        version = 'invalid'
+        name = 'v' + version + '-' + random_gen()
+        try:
+            version_response = self.addKubernetesSupportedVersionUsingDetails(version, name, self.zone.id, self.kubernetes_version_iso_url)
+            self.fail("Kubernetes supported version below version 1.11.0 been added. Must be an error.")
+        except CloudstackAPIException as e:
+            self.debug("Unsupported version error check successful, API failure: %s" % e)
+        return
+
+    def addKubernetesSupportedVersion(self, cmd):
+        version = self.apiclient.addKubernetesSupportedVersion(cmd)
+        if not version:
+            self.cleanup.append(version)
+        return version
+
+    def addKubernetesSupportedVersionUsingDetails(self, version, name, zoneId, isoUrl):
+        addKubernetesSupportedVersionCmd = addKubernetesSupportedVersion.addKubernetesSupportedVersionCmd()
+        addKubernetesSupportedVersionCmd.semanticversion = version
+        addKubernetesSupportedVersionCmd.name = name
+        addKubernetesSupportedVersionCmd.zoneid = zoneId
+        addKubernetesSupportedVersionCmd.url = isoUrl        
+        versionResponse = self.addKubernetesSupportedVersion(addKubernetesSupportedVersionCmd)
+        return versionResponse
+
+    def listKubernetesSupportedVersions(self, cmd):
+        versions = self.apiclient.listKubernetesSupportedVersions(cmd)
+        return versions
+
+    def listKubernetesSupportedVersionId(self, versionId):
+        listKubernetesSupportedVersionsCmd = listKubernetesSupportedVersions.listKubernetesSupportedVersionsCmd()
+        listKubernetesSupportedVersionsCmd.id = versionId
+        versionResponse = self.listKubernetesSupportedVersions(listKubernetesSupportedVersionsCmd)
+        return versionResponse[0]
+
+    def listAllKubernetesSupportedVersions(self):
+        listKubernetesSupportedVersionsCmd = listKubernetesSupportedVersions.listKubernetesSupportedVersionsCmd()
+        versionResponse = self.listKubernetesSupportedVersionsCmd(listKubernetesSupportedVersionsCmd)
+        return versionResponse
+
+    def deleteKubernetesSupportedVersion(self, cmd):
+        response = self.apiclient.deleteKubernetesSupportedVersion(cmd)
+        return response
+
+    def deleteKubernetesSupportedVersionId(self, versionId, deleteIso):
+        deleteKubernetesSupportedVersionCmd = deleteKubernetesSupportedVersion.deleteKubernetesSupportedVersionCmd()
+        deleteKubernetesSupportedVersionCmd.id = versionId
+        deleteKubernetesSupportedVersionCmd.deleteiso = deleteIso
+        response = self.deleteKubernetesSupportedVersion(deleteKubernetesSupportedVersionCmd)
+        return response
+
+    def checkKubernetesSupportedVersionIsoState(self, version_id, retries=15, interval=15):
+        """Check if Kubernetes supported version ISO is in Ready state"""
+
+        while retries > -1:
+            time.sleep(interval)
+            list_versions_response = self.listKubernetesSupportedVersionId(version_id)
+            if not hasattr(list_versions_response, 'isostate') or not list_versions_response or not list_versions_response.isostate:
+                retries = retries - 1
+                continue
+            if 'Creating' == list_versions_response.isostate:
+                retries = retries - 1
+            elif 'Ready' == list_versions_response.isostate:
+                return
+            else:
+                raise Exception(
+                    "Failed to download Kubernetes supported version ISO: status - %s" %
+                    list_versions_response.isostate)
+        raise Exception("Kubernetes supported version Ready state timed out")
