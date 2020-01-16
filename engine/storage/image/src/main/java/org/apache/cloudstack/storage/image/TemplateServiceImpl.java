77d5ea63379ef608f863ae7e6b82782656f2011f
Daan Hoogland <daan.hoogland@shapeblue.com>
2020-01-16 13:59:14 +0100

    passing whatever checksum was in the command   the checksum from the command will be ignored by the async job   it will read it from the record anew

--- engine/storage/image/src/main/java/org/apache/cloudstack/storage/image/TemplateServiceImpl.java
+++ engine/storage/image/src/main/java/org/apache/cloudstack/storage/image/TemplateServiceImpl.java
@@ -1045,8 +1045,6 @@ public class TemplateServiceImpl implements TemplateService {
         }
         tmplForCopy.setUrl(url);
 
-        tmplForCopy.getImage().setChecksum("");
-
         if (s_logger.isDebugEnabled()) {
             s_logger.debug("Mark template_store_ref entry as Creating");
         }
