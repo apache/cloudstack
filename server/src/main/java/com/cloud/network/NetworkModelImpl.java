2f395aeea9c9c43b1bd28579b96b27d5ae24c853
Pearl Dsilva <pearl1594@gmail.com>
2020-01-21 20:50:17 +0530

    Update NetworkModelImpl.java

--- server/src/main/java/com/cloud/network/NetworkModelImpl.java
+++ server/src/main/java/com/cloud/network/NetworkModelImpl.java
@@ -2298,10 +2298,11 @@ public class NetworkModelImpl extends ManagerBase implements NetworkModel, Confi
                     } else {
                         ipv6 = _ipv6Dao.findByNetworkIdAndIp(network.getId(), nic.getIPv6Address());
                     }
-                    //return nic only when its ip address belong to the pod range (for the Basic zone case)
+                    
                     if (vlans.isEmpty()) {
                         return nic;
                     }
+                    //return nic only when its ip address belong to the pod range (for the Basic zone case)
                     for (Vlan vlan : vlans) {
                         if (ip != null && ip.getVlanId() == vlan.getId()) {
                             return nic;
