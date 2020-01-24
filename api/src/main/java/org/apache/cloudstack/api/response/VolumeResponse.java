616aaee231d12d7e14a3353df9b47af39a229b60
Pearl Dsilva <pearl.dsilva@shapeblue.com>
2020-01-24 20:07:48 +0530

    Reverted naming of a property getter function

--- api/src/main/java/org/apache/cloudstack/api/response/VolumeResponse.java
+++ api/src/main/java/org/apache/cloudstack/api/response/VolumeResponse.java
@@ -293,7 +293,7 @@ public class VolumeResponse extends BaseResponseWithTagInformation implements Co
         return this.getId();
     }
 
-    public Boolean getDestroyed() {
+    public Boolean isDestroyed() {
         return destroyed;
     }
 
