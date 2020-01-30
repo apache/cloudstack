8c92c75b3fae451359c8b9ab2efef150ad0df31c
Rohit Yadav <rohit.yadav@shapeblue.com>
2020-01-30 13:26:01 +0530

    agent: SSVM/CPVM agent cert setup workaround
    This fixes excessive logging issue. Stops agent before changing/setting
    up private key and start afterwards for SSVM/CPVM.
    
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>

--- agent/src/main/java/com/cloud/agent/Agent.java
+++ agent/src/main/java/com/cloud/agent/Agent.java
@@ -298,8 +298,7 @@ public class Agent implements HandlerFactory, IAgentControl {
                 } catch (final IOException ex) {
                     s_logger.warn("Fail to clean up old connection. " + ex);
                 }
-                s_logger.warn("NIO Connection Exception  " + e);
-                s_logger.info("Attempted to connect to the server, but received an unexpected exception, trying again...");
+                s_logger.info("Attempted to connect to the server, but received an unexpected exception, trying again...", e);
             }
         }
         _shell.updateConnectedHost();
@@ -522,8 +521,7 @@ public class Agent implements HandlerFactory, IAgentControl {
             try {
                 _connection.start();
             } catch (final NioConnectionException e) {
-                s_logger.warn("NIO Connection Exception  " + e);
-                s_logger.info("Attempted to connect to the server, but received an unexpected exception, trying again...");
+                s_logger.info("Attempted to re-connect to the server, but received an unexpected exception, trying again...", e);
                 _connection.stop();
                 try {
                     _connection.cleanUp();
