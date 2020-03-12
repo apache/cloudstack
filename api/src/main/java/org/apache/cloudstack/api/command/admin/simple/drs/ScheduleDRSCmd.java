c615cf1aaf8a11b2084f7aef086ad158d73ca9d4
nvazquez <nicovazquez90@gmail.com>
2020-03-12 03:00:22 -0300

    Define DRS manager skeleton and new API

---
+++ api/src/main/java/org/apache/cloudstack/api/command/admin/simple/drs/ScheduleDRSCmd.java
@@ -0,0 +1,52 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+package org.apache.cloudstack.api.command.admin.simple.drs;
+
+import com.cloud.exception.ConcurrentOperationException;
+import com.cloud.exception.InsufficientCapacityException;
+import com.cloud.exception.NetworkRuleConflictException;
+import com.cloud.exception.ResourceAllocationException;
+import com.cloud.exception.ResourceUnavailableException;
+import org.apache.cloudstack.api.BaseAsyncCmd;
+import org.apache.cloudstack.api.ServerApiException;
+
+public class ScheduleDRSCmd extends BaseAsyncCmd {
+    @Override
+    public String getEventType() {
+        return null;
+    }
+
+    @Override
+    public String getEventDescription() {
+        return null;
+    }
+
+    @Override
+    public void execute() throws ResourceUnavailableException, InsufficientCapacityException, ServerApiException, ConcurrentOperationException, ResourceAllocationException, NetworkRuleConflictException {
+
+    }
+
+    @Override
+    public String getCommandName() {
+        return null;
+    }
+
+    @Override
+    public long getEntityOwnerId() {
+        return 0;
+    }
+}
