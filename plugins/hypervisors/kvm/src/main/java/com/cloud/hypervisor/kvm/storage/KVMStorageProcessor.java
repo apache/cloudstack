814af6a1829b5835e240cc2b50aa9bde20ce2637
GabrielBrascher <gabriel@apache.org>
2020-03-16 08:31:45 -0300

    checkstyle trailing spaces

--- plugins/hypervisors/kvm/src/main/java/com/cloud/hypervisor/kvm/storage/KVMStorageProcessor.java
+++ plugins/hypervisors/kvm/src/main/java/com/cloud/hypervisor/kvm/storage/KVMStorageProcessor.java
@@ -127,7 +127,7 @@ public class KVMStorageProcessor implements StorageProcessor {
     private String _createTmplPath;
     private String _manageSnapshotPath;
     private int _cmdsTimeout;
-    
+
     private static final String MANAGE_SNAPSTHOT_CREATE = "-c";
     private static final String MANAGE_SNAPSTHOT_DESTROY = "-d";
     private static final String NAME = "-n";
@@ -1546,7 +1546,7 @@ public class KVMStorageProcessor implements StorageProcessor {
         s_logger.debug("Succesfully connected to Ceph cluster at " + r.confGet(CEPH_MON_HOST));
         return r;
     }
-    
+
     @Override
     public Answer deleteVolume(final DeleteCommand cmd) {
         final VolumeObjectTO vol = (VolumeObjectTO)cmd.getData();
