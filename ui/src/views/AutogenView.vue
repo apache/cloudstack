6c4fdec2acede1c5dfbbf9e5bd0cfcbc13dff409
Abhishek Kumar <abhishek.mrt22@gmail.com>
2020-03-31 16:45:15 +0530

    view: reset listview count on list API call (#271)
    Fixes #258
    
    Reset items count for listview while calling a list API to prevent wrong pagination on empty list in response.
    
    Signed-off-by: Abhishek Kumar <abhishek.mrt22@gmail.com>
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>

--- ui/src/views/AutogenView.vue
+++ ui/src/views/AutogenView.vue
@@ -457,6 +457,7 @@ export default {
             break
           }
         }
+        this.itemCount = 0
         for (const key in json[responseName]) {
           if (key === 'count') {
             this.itemCount = json[responseName].count
