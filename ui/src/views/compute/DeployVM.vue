3bc553695efd822fed0f1d8a1315409c3355e452
Rohit Yadav <rohit.yadav@shapeblue.com>
2020-04-22 15:08:16 +0530

    compute: fix null exception while listing networks
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>

--- ui/src/views/compute/DeployVM.vue
+++ ui/src/views/compute/DeployVM.vue
@@ -515,9 +515,9 @@ export default {
           options: {
             zoneid: _.get(this.zone, 'id'),
             canusefordeploy: true,
-            projectid: store.getters.project.id,
-            domainid: store.getters.project.id ? null : store.getters.userInfo.domainid,
-            account: store.getters.project.id ? null : store.getters.userInfo.account,
+            projectid: store.getters.project ? store.getters.project.id : null,
+            domainid: store.getters.project && store.getters.project.id ? null : store.getters.userInfo.domainid,
+            account: store.getters.project && store.getters.project.id ? null : store.getters.userInfo.account,
             page: 1,
             pageSize: 10,
             keyword: undefined
