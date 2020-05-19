141635d6386da022a1f081475de3aaedbb1304be
Pearl Dsilva <pearl.dsilva@shapeblue.com>
2020-05-19 17:54:55 +0530

    updated comment

--- engine/orchestration/src/main/java/org/apache/cloudstack/engine/orchestration/DataMigrationUtility.java
+++ engine/orchestration/src/main/java/org/apache/cloudstack/engine/orchestration/DataMigrationUtility.java
@@ -81,8 +81,9 @@ public class DataMigrationUtility {
     @Inject
     SnapshotDao snapshotDao;
 
-    /** This function verifies if the given image store comprises of data objects that are not in either the "Ready" or
-     * "Allocated" state - in such a case, if the migration policy is complete, the migration is terminated
+    /** This function verifies if the given image store comprises of data objects that are not in the following states"
+     *  "Ready" "Allocated", "Destroying", "Destroyed", "Failed" - in such a case, if the migration policy is complete,
+     *  the migration is terminated
      */
     private boolean filesReadyToMigrate(Long srcDataStoreId) {
         String[] validStates = new String[]{"Ready", "Allocated", "Destroying", "Destroyed", "Failed"};
