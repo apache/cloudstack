b9c7c9d040716e5345d7a57ce72aa906171cbf34
Harikrishna Patnala <harikrishna.patnala@gmail.com>
2020-06-01 11:17:33 +0530

    Added importVsphereStoragePolicies API and scheme changes

--- api/src/main/java/com/cloud/event/EventTypes.java
+++ api/src/main/java/com/cloud/event/EventTypes.java
@@ -615,6 +615,9 @@ public class EventTypes {
     public static final String EVENT_POD_ROLLING_MAINTENANCE = "POD.ROLLING.MAINTENANCE";
     public static final String EVENT_ZONE_ROLLING_MAINTENANCE = "ZONE.ROLLING.MAINTENANCE";
 
+    // Storage Policies
+    public static final String EVENT_IMPORT_VCENTER_STORAGE_POLICIES = "IMPORT.VCENTER.STORAGE.POLICIES";
+
     static {
 
         // TODO: need a way to force author adding event types to declare the entity details as well, with out braking
@@ -1021,6 +1024,8 @@ public class EventTypes {
         entityEventDetails.put(EVENT_POD_ROLLING_MAINTENANCE, PodResponse.class);
         entityEventDetails.put(EVENT_CLUSTER_ROLLING_MAINTENANCE, ClusterResponse.class);
         entityEventDetails.put(EVENT_HOST_ROLLING_MAINTENANCE, HostResponse.class);
+
+        entityEventDetails.put(EVENT_IMPORT_VCENTER_STORAGE_POLICIES, "StoragePolicies");
     }
 
     public static String getEntityForEvent(String eventName) {
