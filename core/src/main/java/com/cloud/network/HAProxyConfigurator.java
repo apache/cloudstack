8f9f7524b4396cc520c1a8a20d7fcfabe2f0a5fc
Wei Zhou <w.zhou@global.leaseweb.com>
2020-06-04 08:06:21 +0000

    Add 'no option forceclose' to http keep-alive rules

--- core/src/main/java/com/cloud/network/HAProxyConfigurator.java
+++ core/src/main/java/com/cloud/network/HAProxyConfigurator.java
@@ -629,7 +629,7 @@ public class HAProxyConfigurator implements LoadBalancerConfigurator {
             result.add(sb.toString());
             if (keepAliveEnabled) {
                 sb = new StringBuilder();
-                sb.append("\t").append("option http-keep-alive");
+                sb.append("\t").append("no option forceclose");
                 result.add(sb.toString());
             } else {
                 sb = new StringBuilder();
@@ -654,7 +654,6 @@ public class HAProxyConfigurator implements LoadBalancerConfigurator {
         rule.append("\n\tmode http");
         if (lbCmd.keepAliveEnabled) {
             s_logger.info("Haproxy option http-keep-alive enabled");
-            rule.append("\n\toption http-keep-alive");
         } else {
             s_logger.info("Haproxy option httpclose enabled");
             rule.append("\n\toption httpclose");
