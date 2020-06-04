2ff4989feb6d48ffaa4ed74b00425bff8ecd9cae
Harikrishna Patnala <harikrishna.patnala@gmail.com>
2020-06-04 15:31:34 +0530

    Added gen_toc.py changes for API importvpsherestoragepolicies

--- tools/apidoc/gen_toc.py
+++ tools/apidoc/gen_toc.py
@@ -196,7 +196,8 @@ known_categories = {
     'KubernetesSupportedVersion': 'Kubernetes Service',
     'KubernetesCluster': 'Kubernetes Service',
     'UnmanagedInstance': 'Virtual Machine',
-    'Rolling': 'Rolling Maintenance'
+    'Rolling': 'Rolling Maintenance',
+    'importVsphereStoragePolicies' : 'vSphere storage policies'
     }
 
 
