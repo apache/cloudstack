94963dffc16583a4dc8805c70fa11cf41c7f56b5
Rohit Yadav <rohit.yadav@shapeblue.com>
2020-06-16 09:52:14 +0530

    Update ListTemplatesCmd.java

--- api/src/main/java/org/apache/cloudstack/api/command/user/template/ListTemplatesCmd.java
+++ api/src/main/java/org/apache/cloudstack/api/command/user/template/ListTemplatesCmd.java
@@ -74,12 +74,12 @@ public class ListTemplatesCmd extends BaseListTaggedResourcesCmd {
 
     @Parameter(name = ApiConstants.SHOW_REMOVED, type = CommandType.BOOLEAN, description = "show removed templates as well")
     private Boolean showRemoved;
+        
+    @Parameter(name = ApiConstants.SHOW_UNIQUE, type = CommandType.BOOLEAN, description = "If set to true, list only unique templates across zones", since = "4.13.2")
+    private Boolean showUnique;
 
     @Parameter(name = ApiConstants.PARENT_TEMPLATE_ID, type = CommandType.UUID, entityType = TemplateResponse.class, description = "list datadisk templates by parent template id", since = "4.4")
     private Long parentTemplateId;
-
-    @Parameter(name = ApiConstants.SHOW_UNIQUE, type = CommandType.BOOLEAN, description = "If set to true, list only unique templates across zones", since = "4.15")
-    private Boolean showUnique;
 
     /////////////////////////////////////////////////////
     /////////////////// Accessors ///////////////////////
