039f4429a76b7318ef24e0b47c9b23183fb416c6
Rohit Yadav <rohit.yadav@shapeblue.com>
2020-06-24 14:20:36 +0530

    views: show doclink icon for custom action forms
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>

--- ui/src/views/AutogenView.vue
+++ ui/src/views/AutogenView.vue
@@ -82,7 +82,6 @@
     <div v-show="showAction">
       <keep-alive v-if="currentAction.component">
         <a-modal
-          :title="$t(currentAction.label)"
           :visible="showAction"
           :closable="true"
           style="top: 20px;"
@@ -92,6 +91,16 @@
           centered
           width="auto"
         >
+          <span slot="title">
+            {{ $t(currentAction.label) }}
+            <a
+              v-if="currentAction.docHelp || $route.meta.docHelp"
+              style="margin-left: 5px"
+              :href="$config.docBase + '/' + (currentAction.docHelp || $route.meta.docHelp)"
+              target="_blank">
+              <a-icon type="question-circle-o"></a-icon>
+            </a>
+          </span>
           <component
             :is="currentAction.component"
             :resource="resource"
