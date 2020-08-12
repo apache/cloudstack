86939e7f9dae71c09c98e7bc7f53c5b37bbad4ce
Spaceman1984 <49917670+Spaceman1984@users.noreply.github.com>
2020-08-12 09:59:12 +0200

    server: Fixed private gateway can't be deleted (#4016)
    When the static route service is not available on the VPC and a static route is created, the static route is created in a revoked state.
    
    Currently, the UI doesn't distinguish between active or revoked static routes.
    
    This PR adds the missing state filter to the list routes command and only lists active routes in the UI.
    It also ignores revoked routes when the private gateway is being removed but clears out the inactive routes before the gateway is removed.
    
    Fixes #2908

--- server/src/main/java/com/cloud/network/vpc/VpcManagerImpl.java
+++ server/src/main/java/com/cloud/network/vpc/VpcManagerImpl.java
@@ -2027,13 +2027,23 @@ public class VpcManagerImpl extends ManagerBase implements VpcManager, VpcProvis
                 }
             }
 
-            // 2) Delete private gateway from the DB
+            // 2) Clean up any remaining routes
+            cleanUpRoutesByGatewayId(gatewayId);
+
+            // 3) Delete private gateway from the DB
             return deletePrivateGatewayFromTheDB(gateway);
 
         } finally {
             if (gatewayVO != null) {
                 _vpcGatewayDao.releaseFromLockTable(gatewayId);
             }
+        }
+    }
+
+    private void cleanUpRoutesByGatewayId(long gatewayId){
+        List<StaticRouteVO> routes = _staticRouteDao.listByGatewayId(gatewayId);
+        for (StaticRouteVO route: routes){
+            _staticRouteDao.remove(route.getId());
         }
     }
 
@@ -2329,6 +2339,7 @@ public class VpcManagerImpl extends ManagerBase implements VpcManager, VpcProvis
         final List<Long> permittedAccounts = new ArrayList<Long>();
         final Map<String, String> tags = cmd.getTags();
         final Long projectId = cmd.getProjectId();
+        final String state = cmd.getState();
 
         final Ternary<Long, Boolean, ListProjectResourcesCriteria> domainIdRecursiveListProject = new Ternary<Long, Boolean, ListProjectResourcesCriteria>(domainId, isRecursive,
                 null);
@@ -2344,6 +2355,7 @@ public class VpcManagerImpl extends ManagerBase implements VpcManager, VpcProvis
         sb.and("id", sb.entity().getId(), SearchCriteria.Op.EQ);
         sb.and("vpcId", sb.entity().getVpcId(), SearchCriteria.Op.EQ);
         sb.and("vpcGatewayId", sb.entity().getVpcGatewayId(), SearchCriteria.Op.EQ);
+        sb.and("state", sb.entity().getState(), SearchCriteria.Op.EQ);
 
         if (tags != null && !tags.isEmpty()) {
             final SearchBuilder<ResourceTagVO> tagSearch = _resourceTagDao.createSearchBuilder();
@@ -2369,6 +2381,10 @@ public class VpcManagerImpl extends ManagerBase implements VpcManager, VpcProvis
 
         if (gatewayId != null) {
             sc.addAnd("vpcGatewayId", Op.EQ, gatewayId);
+        }
+
+        if (state != null) {
+            sc.addAnd("state", Op.EQ, state);
         }
 
         if (tags != null && !tags.isEmpty()) {
