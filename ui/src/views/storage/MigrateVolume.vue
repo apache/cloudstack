8801446de1c5318bc7fff3f62ba9bc79c64171e6
davidjumani <dj.davidjumani1994@gmail.com>
2020-08-17 14:03:43 +0530

    MigrateVolume : ReplaceDiskOffering defaulting to false (#583)
    Fixes #582
    
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>

--- ui/src/views/storage/MigrateVolume.vue
+++ ui/src/views/storage/MigrateVolume.vue
@@ -74,7 +74,7 @@ export default {
       storagePools: [],
       selectedStoragePool: null,
       diskOfferings: [],
-      replaceDiskOffering: !!this.resource.virtualmachineid,
+      replaceDiskOffering: false,
       selectedDiskOffering: null
     }
   },
