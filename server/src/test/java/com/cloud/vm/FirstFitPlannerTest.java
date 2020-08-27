d4d80807681993c9090e8b024deb973bdcc7f1ee
Sina Kashipazha <s.kashipazha@global.leaseweb.com>
2020-08-27 14:54:02 +0200

    #4534 Fix Vms are migrated to same clusters in CloudStack caused by dedicated resources.

--- server/src/test/java/com/cloud/vm/FirstFitPlannerTest.java
+++ server/src/test/java/com/cloud/vm/FirstFitPlannerTest.java
@@ -291,7 +291,7 @@ public class FirstFitPlannerTest {
         clustersWithEnoughCapacity.add(6L);
 
         when(
-            capacityDao.listClustersInZoneOrPodByHostCapacities(dataCenterId, noOfCpusInOffering * cpuSpeedInOffering, ramInOffering * 1024L * 1024L,
+            capacityDao.listClustersInZoneOrPodByHostCapacities(dataCenterId, 12L, noOfCpusInOffering * cpuSpeedInOffering, ramInOffering * 1024L * 1024L,
                 Capacity.CAPACITY_TYPE_CPU, true)).thenReturn(clustersWithEnoughCapacity);
 
         Map<Long, Double> clusterCapacityMap = new HashMap<Long, Double>();
@@ -303,7 +303,7 @@ public class FirstFitPlannerTest {
         clusterCapacityMap.put(6L, 2048D);
 
         Pair<List<Long>, Map<Long, Double>> clustersOrderedByCapacity = new Pair<List<Long>, Map<Long, Double>>(clustersWithEnoughCapacity, clusterCapacityMap);
-        when(capacityDao.orderClustersByAggregateCapacity(dataCenterId, Capacity.CAPACITY_TYPE_CPU, true)).thenReturn(clustersOrderedByCapacity);
+        when(capacityDao.orderClustersByAggregateCapacity(dataCenterId, 12L, Capacity.CAPACITY_TYPE_CPU, true)).thenReturn(clustersOrderedByCapacity);
 
         List<Long> disabledClusters = new ArrayList<Long>();
         List<Long> clustersWithDisabledPods = new ArrayList<Long>();
