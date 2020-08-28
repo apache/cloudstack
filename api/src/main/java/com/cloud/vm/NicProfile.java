8dfc11a57ce8d210661e37897c0fc14dd26d3d73
Wei Zhou <w.zhou@global.leaseweb.com>
2020-08-28 11:14:00 +0200

    router: Save PlaceHolder nic for VR if network does not have source nat (#3902)
    This PR aims to fix the issue below
    
    Create a network offering for isolated network, services: Dns/Dhcp/Userdata, and enable it
    create a isolated network with the new offering
    create a vm
    check the guest IP of virtual router,
    restart network with cleanup
    check the guest IP of new virtual router
    The IP in step4 and step6 should be the same, but they are different actually.

--- api/src/main/java/com/cloud/vm/NicProfile.java
+++ api/src/main/java/com/cloud/vm/NicProfile.java
@@ -423,6 +423,7 @@ public class NicProfile implements InternalIdentity, Serializable {
                 .append(iPv4Address)
                 .append("-")
                 .append(broadcastUri)
+                .append("]")
                 .toString();
     }
 }
