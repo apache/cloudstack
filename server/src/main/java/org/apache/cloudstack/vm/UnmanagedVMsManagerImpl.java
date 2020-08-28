0f64bb3c417408e8657b8421fad48fa0bdf35cd3
Harikrishna Patnala <harikrishna.patnala@gmail.com>
2020-08-28 10:25:32 +0530

    Handle import unmanaged VM into CloudStack to support any type of datastore

--- server/src/main/java/org/apache/cloudstack/vm/UnmanagedVMsManagerImpl.java
+++ server/src/main/java/org/apache/cloudstack/vm/UnmanagedVMsManagerImpl.java
@@ -103,7 +103,6 @@ import com.cloud.service.ServiceOfferingVO;
 import com.cloud.service.dao.ServiceOfferingDao;
 import com.cloud.storage.GuestOS;
 import com.cloud.storage.GuestOSHypervisor;
-import com.cloud.storage.Storage;
 import com.cloud.storage.StoragePool;
 import com.cloud.storage.VMTemplateStoragePoolVO;
 import com.cloud.storage.VMTemplateVO;
@@ -491,23 +490,20 @@ public class UnmanagedVMsManagerImpl implements UnmanagedVMsManager {
         final String dsPath = disk.getDatastorePath();
         final String dsType = disk.getDatastoreType();
         final String dsName = disk.getDatastoreName();
-        if (dsType.equals(Storage.StoragePoolType.VMFS.toString())) {
-            List<StoragePoolVO> pools = primaryDataStoreDao.listPoolsByCluster(cluster.getId());
-            pools.addAll(primaryDataStoreDao.listByDataCenterId(zone.getId()));
+        if (dsType != null) {
+            List<StoragePoolVO> pools = primaryDataStoreDao.listPoolByHostPath(dsHost, dsPath);
             for (StoragePool pool : pools) {
-                if (pool.getPoolType() != Storage.StoragePoolType.VMFS) {
-                    continue;
-                }
-                if (pool.getPath().endsWith(dsName)) {
+                if (pool.getDataCenterId() == zone.getId() &&
+                        (pool.getClusterId() == null || pool.getClusterId().equals(cluster.getId()))) {
                     storagePool = pool;
                     break;
                 }
             }
         } else {
-            List<StoragePoolVO> pools = primaryDataStoreDao.listPoolByHostPath(dsHost, dsPath);
+            List<StoragePoolVO> pools = primaryDataStoreDao.listPoolsByCluster(cluster.getId());
+            pools.addAll(primaryDataStoreDao.listByDataCenterId(zone.getId()));
             for (StoragePool pool : pools) {
-                if (pool.getDataCenterId() == zone.getId() &&
-                        (pool.getClusterId() == null || pool.getClusterId().equals(cluster.getId()))) {
+                if (pool.getPath().endsWith(dsName)) {
                     storagePool = pool;
                     break;
                 }
