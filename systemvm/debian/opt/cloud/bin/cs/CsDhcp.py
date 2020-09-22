ab02cf70789aafde4186c6f07f9407b8f1553923
Lucas Granet <Lucas@granetlucas.fr>
2020-09-22 09:37:56 +0200

    router: adding "data-server" dns entry in /etc/hosts (#4319)
    The DNS entry "data-server" was not added in /etc/hosts.
    
    Since the VR is now considered as a "dhcpsrvr" (?), we need to apply this commit to add this DNS entry.
    /etc/hosts is fully rewritten by this script.
    
    Fixes: #4308
    (cherry picked from commit dc65f31f9f3cb47240946c8c1cced44a7ecf9640)
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>

--- systemvm/debian/opt/cloud/bin/cs/CsDhcp.py
+++ systemvm/debian/opt/cloud/bin/cs/CsDhcp.py
@@ -158,7 +158,7 @@ class CsDhcp(CsDataBag):
         self.add_host("::1", "localhost ip6-localhost ip6-loopback")
         self.add_host("ff02::1", "ip6-allnodes")
         self.add_host("ff02::2", "ip6-allrouters")
-        if self.config.is_router():
+        if self.config.is_router() or self.config.is_dhcp():
             self.add_host(self.config.address().get_guest_ip(), "%s data-server" % CsHelper.get_hostname())
 
     def write_hosts(self):
