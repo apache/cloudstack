75bacaf8c942fd482f466cafa796a6dcb5256056
Hoang Nguyen <hoangnm@unitech.vn>
2020-09-28 17:49:17 +0700

    compute: Back to list VM when deleting VM with the Expunge option. (#755)
    * compute: back to list VM when deleting VM with the Expunge option.
    
    * fixes: go back if is a detail view
    
    * fix go back only vm detail view
    
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>

--- ui/src/views/compute/DestroyVM.vue
+++ ui/src/views/compute/DestroyVM.vue
@@ -134,9 +134,15 @@ export default {
             jobId,
             loadingMessage: `${this.$t('message.deleting.vm')} ${this.resource.name}`,
             catchMessage: this.$t('error.fetching.async.job.result'),
-            successMessage: `${this.$t('message.success.delete.vm')} ${this.resource.name}`
+            successMessage: `${this.$t('message.success.delete.vm')} ${this.resource.name}`,
+            successMethod: () => {
+              if (this.$route.path.includes('/vm/') && values.expunge) {
+                this.$router.go(-1)
+              } else {
+                this.parentFetchData()
+              }
+            }
           })
-          this.parentFetchData()
           this.closeAction()
         }).catch(error => {
           this.$notifyError(error)
