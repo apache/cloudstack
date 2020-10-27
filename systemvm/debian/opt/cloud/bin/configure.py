244ecd1111ad7406cd23075366304036add3f1e3
Rakesh Venkatesh <r.venkatesh@global.leaseweb.com>
2020-10-27 13:19:30 +0100

    FIX issue in VR if remote access vpn is enabled
    Use both tcp protocol and module so that
    vpn is enabled properly

--- systemvm/debian/opt/cloud/bin/configure.py
+++ systemvm/debian/opt/cloud/bin/configure.py
@@ -755,7 +755,7 @@ class CsRemoteAccessVpn(CsDataBag):
 
         self.fw.append(["", "", "-A INPUT -i ppp+ -m udp -p udp --dport 53 -j ACCEPT"])
         self.fw.append(["", "", "-A INPUT -i ppp+ -m tcp -p tcp --dport 53 -j ACCEPT"])
-        self.fw.append(["nat", "", "-I PREROUTING -i ppp+ -m tcp --dport 53 -j DNAT --to-destination %s" % local_ip])
+        self.fw.append(["nat", "", "-I PREROUTING -i ppp+ -p tcp -m tcp --dport 53 -j DNAT --to-destination %s" % local_ip])
 
         if self.config.is_vpc():
             return
