apache/cloudstack

commit 93f3d352071be326ab3b810e23da7c27a948aedd

kvm: FIX cpucorespersocket is not working on KVM (#4497)

plugins/hypervisors/kvm/src/main/java/com/cloud/hypervisor/kvm/resource/LibvirtComputingResource.java
@@ -2302,14 +2302,7 @@ public class LibvirtComputingResource extends ServerResourceBase implements Serv
             if (vmTO.getType() == VirtualMachine.Type.User) {
                 cmd.setFeatures(_cpuFeatures);
             }
-            // multi cores per socket, for larger core configs
-            if (vcpus % 6 == 0) {
-                final int sockets = vcpus / 6;
-                cmd.setTopology(6, sockets);
-            } else if (vcpus % 4 == 0) {
-                final int sockets = vcpus / 4;
-                cmd.setTopology(4, sockets);
-            }
+            setCpuTopology(cmd, vcpus, vmTO.getDetails());
             vm.addComp(cmd);
         }
 
@@ -4229,5 +4222,27 @@ public class LibvirtComputingResource extends ServerResourceBase implements Serv
         }
 
         return false;
+    }
+
+    private void setCpuTopology(CpuModeDef cmd, int vcpus, Map<String, String> details) {
+        // multi cores per socket, for larger core configs
+        int numCoresPerSocket = -1;
+        if (details != null) {
+            final String coresPerSocket = details.get(VmDetailConstants.CPU_CORE_PER_SOCKET);
+            final int intCoresPerSocket = NumbersUtil.parseInt(coresPerSocket, numCoresPerSocket);
+            if (intCoresPerSocket > 0 && vcpus % intCoresPerSocket == 0) {
+                numCoresPerSocket = intCoresPerSocket;
+            }
+        }
+        if (numCoresPerSocket <= 0) {
+            if (vcpus % 6 == 0) {
+                numCoresPerSocket = 6;
+            } else if (vcpus % 4 == 0) {
+                numCoresPerSocket = 4;
+            } else {
+                numCoresPerSocket = 1;
+            }
+        }
+        cmd.setTopology(numCoresPerSocket, vcpus / numCoresPerSocket);
     }
 }

server/src/main/java/com/cloud/vm/UserVmManagerImpl.java
@@ -2478,6 +2478,15 @@ public class UserVmManagerImpl extends ManagerBase implements UserVmManager, Vir
                         if (ovfPropertyVO != null && ovfPropertyVO.isPassword()) {
                             details.put(detailName, DBEncryptionUtil.encrypt(details.get(detailName)));
                         }
+                    } else if (VmDetailConstants.CPU_CORE_PER_SOCKET.equals(detailName)) {
+                        try {
+                            final int val = Integer.parseInt(details.get(detailName));
+                            if (val <= 0) {
+                                throw new InvalidParameterValueException("Please enter a positive integer value for the vm setting: " + detailName);
+                            }
+                        } catch (final NumberFormatException e) {
+                            throw new InvalidParameterValueException("Please enter an integer value for vm setting: " + detailName);
+                        }
                     }
                 }
                 vmInstance.setDetails(details);

commit 1a4771958860a72f50d179b58c4e319c99d12308

kvm: set cpu topology only if cpucore per socket is positive value (#4527)
This PR fixes a regression issue in #4497

In cloudstack 4.14 or before, the cpu topology is set only when cpucore per socket is set (to 4 or 6).
in other conditions, there is no cpu topology in vm xml definition.

with #4497, vm will have cpu topology in its xml definition, if cpucore per socket is not set.

    <topology sockets='<vm cpu cores>' cores='1' threads='1'/>
Not sure if it causes any issue. I think it would be better not to add this part in vm xml definition if cpucore per socket is not set.

plugins/hypervisors/kvm/src/main/java/com/cloud/hypervisor/kvm/resource/LibvirtComputingResource.java
@@ -4239,10 +4239,10 @@ public class LibvirtComputingResource extends ServerResourceBase implements Serv
                 numCoresPerSocket = 6;
             } else if (vcpus % 4 == 0) {
                 numCoresPerSocket = 4;
-            } else {
-                numCoresPerSocket = 1;
             }
         }
-        cmd.setTopology(numCoresPerSocket, vcpus / numCoresPerSocket);
+        if (numCoresPerSocket > 0) {
+            cmd.setTopology(numCoresPerSocket, vcpus / numCoresPerSocket);
+        }
     }
 }