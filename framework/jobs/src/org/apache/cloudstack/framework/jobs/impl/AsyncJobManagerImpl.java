6be203decd35a35758c533e8c104de8d324d1fcd
Sean Lair <slair@ippathways.com>
2021-01-05 15:38:43 -0600

    Fixed duplicate id error when creating VM work jobs AsyncJobManager contains code to expunge async job records after a configurable time (24h default). Some async jobs have corresponding vm_work_job entries, which are created using the generated key of async_job. The expunge code only removes the async_job record, leaving dangling vm_work_job rows.
    Because the supported version of mysql recalculates autonumber sequences based on the last current row, this can cause async jobs to be created with IDs referencing existing rows in vm_work_job, which causes a primary key violation if this clashes, showing the following log message:
    
    Unable to schedule async job for command com.cloud.vm.VmWorkStart, unexpected exception.
    javax.persistence.EntityExistsException: Entity already exists
    
    https://github.com/apache/cloudstack/issues/3829
    https://github.com/apache/cloudstack/pull/3871

--- framework/jobs/src/org/apache/cloudstack/framework/jobs/impl/AsyncJobManagerImpl.java
+++ framework/jobs/src/org/apache/cloudstack/framework/jobs/impl/AsyncJobManagerImpl.java
@@ -50,6 +50,7 @@ import org.apache.cloudstack.framework.jobs.AsyncJobExecutionContext;
 import org.apache.cloudstack.framework.jobs.AsyncJobManager;
 import org.apache.cloudstack.framework.jobs.dao.AsyncJobDao;
 import org.apache.cloudstack.framework.jobs.dao.AsyncJobJoinMapDao;
+import org.apache.cloudstack.framework.jobs.dao.VmWorkJobDao;
 import org.apache.cloudstack.framework.jobs.dao.AsyncJobJournalDao;
 import org.apache.cloudstack.framework.jobs.dao.SyncQueueItemDao;
 import org.apache.cloudstack.framework.messagebus.MessageBus;
@@ -130,6 +131,8 @@ public class AsyncJobManagerImpl extends ManagerBase implements AsyncJobManager,
     private AsyncJobMonitor _jobMonitor;
     @Inject
     private VMInstanceDao _vmInstanceDao;
+    @Inject
+    private VmWorkJobDao _vmWorkJobDao;
     @Inject
     private VolumeDetailsDao _volumeDetailsDao;
     @Inject
@@ -896,6 +899,9 @@ public class AsyncJobManagerImpl extends ManagerBase implements AsyncJobManager,
         Transaction.execute(new TransactionCallbackNoReturn() {
             @Override
             public void doInTransactionWithoutResult(TransactionStatus status) {
+                if ("VmWork".equals(job.getType())) {
+                    _vmWorkJobDao.expunge(job.getId());
+                }
                 _jobDao.expunge(job.getId());
                 // purge corresponding sync queue item
                 _queueMgr.purgeAsyncJobQueueItemId(job.getId());
