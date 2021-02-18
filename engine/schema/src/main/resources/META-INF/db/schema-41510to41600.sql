7c865ca7d9be781e7ec96d53448101c5f4b660ad
Daniel Augusto Veronezi Salvador <daniel@scclouds.com.br>
2021-02-18 14:54:55 -0300

    Create a procedure to insert guest_os and guest_os_hypervisor data

--- engine/schema/src/main/resources/META-INF/db/schema-41510to41600.sql
+++ engine/schema/src/main/resources/META-INF/db/schema-41510to41600.sql
@@ -304,43 +304,42 @@ from
 -- Update name for global configuration user.vm.readonly.ui.details
 Update configuration set name='user.vm.readonly.details' where name='user.vm.readonly.ui.details';
 
--- PR#4699 Create a temporary table to use as a list of new guest_os to insert.
-CREATE TEMPORARY TABLE temp_new_guest_os_to_insert (name varchar(50));
+-- PR#4699 Drop the procedure `ADD_GUEST_OS_AND_HYPERVISOR_MAPPING` if it already exist.
+DROP PROCEDURE IF EXISTS `cloud`.`ADD_GUEST_OS_AND_HYPERVISOR_MAPPING`;
 
--- PR#4699 Populate the temporary table with the new guest_os to insert.
-INSERT INTO temp_new_guest_os_to_insert (name)
-VALUES
-('Ubuntu 20.04 LTS'),
-('Ubuntu 21.04'),
-('pfSense 2.4'),
-('OpenBSD 6.7'),
-('OpenBSD 6.8'),
-('AlmaLinux 8.3');
+-- PR#4699 Create the procedure `ADD_GUEST_OS_AND_HYPERVISOR_MAPPING` to add guest_os and guest_os_hypervisor mapping.
+CREATE PROCEDURE `cloud`.`ADD_GUEST_OS_AND_HYPERVISOR_MAPPING` (
+    IN guest_os_category_id bigint(20) unsigned,
+    IN guest_os_display_name VARCHAR(255),
+    IN guest_os_hypervisor_hypervisor_type VARCHAR(32),
+    IN guest_os_hypervisor_hypervisor_version VARCHAR(32),
+    IN guest_os_hypervisor_guest_os_name VARCHAR(255)
+)
+BEGIN	
+	INSERT  INTO cloud.guest_os (uuid, category_id, display_name, created) 
+	SELECT 	UUID(), guest_os_category_id, guest_os_display_name, now()	
+	WHERE 	not exists( SELECT  1 
+	                    FROM    cloud.guest_os
+	                    WHERE   cloud.guest_os.category_id = guest_os_category_id
+	                    AND     cloud.guest_os.display_name = guest_os_display_name);	
+						
+	INSERT  INTO cloud.guest_os_hypervisor (uuid, hypervisor_type, hypervisor_version, guest_os_name, guest_os_id, created) 
+	SELECT 	UUID(), guest_os_hypervisor_hypervisor_type, guest_os_hypervisor_hypervisor_version, guest_os_hypervisor_guest_os_name, guest_os.id, now()
+	FROM 	cloud.guest_os
+	WHERE 	guest_os.category_id = guest_os_category_id
+	AND 	guest_os.display_name = guest_os_display_name
+	AND	NOT EXISTS (SELECT  1 
+	                    FROM    cloud.guest_os_hypervisor as hypervisor
+	                    WHERE   hypervisor_type = guest_os_hypervisor_hypervisor_type			
+	                    AND     hypervisor_version = guest_os_hypervisor_hypervisor_version
+	                    AND     hypervisor.guest_os_id = guest_os.id
+	                    AND     hypervisor.guest_os_name = guest_os_hypervisor_guest_os_name);    
+END;
 
--- PR#4699 Insert the new guest_os if they do not already exists in the table.
-INSERT  INTO cloud.guest_os (uuid, category_id, display_name, created, is_user_defined) 
-SELECT 	UUID(), '1', new_guest_os.name, now(), '0'
-FROM    temp_new_guest_os_to_insert AS new_guest_os
-WHERE 	NOT EXISTS (SELECT  1 
-                    FROM    cloud.guest_os
-                    WHERE   cloud.guest_os.category_id = 1
-                    AND     cloud.guest_os.is_user_defined = 0
-                    AND     cloud.guest_os.display_name = new_guest_os.name);	
-
--- PR#4699 Insert the new guest_os_hypervisor if they do not already exists in the table.					
-INSERT  INTO cloud.guest_os_hypervisor (uuid, hypervisor_type, hypervisor_version, guest_os_name, guest_os_id, created, is_user_defined) 
-SELECT 	UUID(), 'KVM', 'default', new_guest_os.name, guest_os.id, now(), 0
-FROM 	temp_new_guest_os_to_insert AS new_guest_os
-INNER 	JOIN cloud.guest_os AS guest_os ON (guest_os.category_id = 1
-                                        AND guest_os.is_user_defined = 0
-                                        AND guest_os.display_name = new_guest_os.name)
-WHERE 	NOT EXISTS (SELECT  1 
-                    FROM    cloud.guest_os_hypervisor AS hypervisor
-                    WHERE   hypervisor.hypervisor_type = 'KVM'					
-                    AND     hypervisor.hypervisor_version = 'default'
-                    AND     hypervisor.guest_os_id = guest_os.id
-                    AND     hypervisor.guest_os_name = new_guest_os.name);
-
--- PR#4699 Drop temporary table after use it.
-DROP TEMPORARY TABLE temp_new_guest_os_to_insert;
-
+-- PR#4699 Call procedure `ADD_GUEST_OS_AND_HYPERVISOR_MAPPING` to add new data to guest_os and guest_os_hypervisor.
+CALL ADD_GUEST_OS_AND_HYPERVISOR_MAPPING (1, 'Ubuntu 20.04 LTS', 'KVM', 'default', 'Ubuntu 20.04 LTS');
+CALL ADD_GUEST_OS_AND_HYPERVISOR_MAPPING (1, 'Ubuntu 21.04', 'KVM', 'default', 'Ubuntu 21.04');
+CALL ADD_GUEST_OS_AND_HYPERVISOR_MAPPING (1, 'pfSense 2.4', 'KVM', 'default', 'pfSense 2.4');
+CALL ADD_GUEST_OS_AND_HYPERVISOR_MAPPING (1, 'OpenBSD 6.7', 'KVM', 'default', 'OpenBSD 6.7');
+CALL ADD_GUEST_OS_AND_HYPERVISOR_MAPPING (1, 'OpenBSD 6.8', 'KVM', 'default', 'OpenBSD 6.8');
+CALL ADD_GUEST_OS_AND_HYPERVISOR_MAPPING (1, 'AlmaLinux 8.3', 'KVM', 'default', 'AlmaLinux 8.3');
