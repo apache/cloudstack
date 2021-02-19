dff9c56a754e77388e96d4d38c54b33fb45de5cd
Rohit Yadav <rohit@apache.org>
2021-02-19 14:30:38 +0530

    Update VolumeApiServiceImpl.java

--- server/src/main/java/com/cloud/storage/VolumeApiServiceImpl.java
+++ server/src/main/java/com/cloud/storage/VolumeApiServiceImpl.java
@@ -2174,7 +2174,7 @@ public class VolumeApiServiceImpl extends ManagerBase implements VolumeApiServic
                     liveMigrateVolume = capabilities.isStorageMotionSupported();
                 }
 
-                if(liveMigrateVolume && HypervisorType.KVM.equals(host.getHypervisorType())) {
+                if (liveMigrateVolume && HypervisorType.KVM.equals(host.getHypervisorType())) {
                     throw new InvalidParameterValueException("KVM does not support volume live migration due to the limited possibility to refresh VM XML domain. " +
                             "Therefore, to live migrate a volume between storage pools, one must migrate the VM to a different host as well to force the VM XML domain update. " +
                             "Use 'migrateVirtualMachineWithVolumes' instead.");
