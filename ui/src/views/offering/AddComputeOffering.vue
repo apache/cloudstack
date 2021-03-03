46cbdc739e901351f3b7d6c99b2a3955f0c784a2
utchoang <hoangnm@unitech.vn>
2021-03-03 09:45:02 +0700

    fix error lint

--- ui/src/views/offering/AddComputeOffering.vue
+++ ui/src/views/offering/AddComputeOffering.vue
@@ -737,7 +737,7 @@
       </a-form>
       <div :span="24" class="action-button">
         <a-button @click="closeAction">{{ this.$t('label.cancel') }}</a-button>
-        <a-button :loading="loading" type="primary" html-type="submit"  @click="handleSubmit">{{ this.$t('label.ok') }}</a-button>
+        <a-button :loading="loading" type="primary" html-type="submit" @click="handleSubmit">{{ this.$t('label.ok') }}</a-button>
       </div>
     </a-spin>
   </div>
