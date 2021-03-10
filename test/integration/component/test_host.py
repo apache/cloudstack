00055502b07101ca0ebae460884c9a485ff7e356
Daan Hoogland <dahn@onecht.net>
2021-03-10 10:33:23 +0100

    no importing of False in python3 (or in sensible code)

--- test/integration/component/test_host.py
+++ test/integration/component/test_host.py
@@ -14,7 +14,6 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-from builtins import False
 """ BVT tests for Hosts Test
 """
 
