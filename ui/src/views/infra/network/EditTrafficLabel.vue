d475f60c8444e3d8835c9d4cfc013161166b0ffa
utchoang <hoangnm@unitech.vn>
2021-03-24 16:11:17 +0700

    revert the deleted function

--- ui/src/views/infra/network/EditTrafficLabel.vue
+++ ui/src/views/infra/network/EditTrafficLabel.vue
@@ -143,6 +143,7 @@ export default {
       })
     }
   },
+  inject: ['parentFetchData'],
   created () {
     this.fetchData()
   },
--- ui/src/views/offering/ImportBackupOffering.vue
+++ ui/src/views/offering/ImportBackupOffering.vue
@@ -131,6 +131,7 @@ export default {
   created () {
     this.fetchData()
   },
+  inject: ['parentFetchData'],
   methods: {
     fetchData () {
       this.fetchZone()
