0344b56bc839b62fa08f20793f15ef56b4daf561
Abhishek Kumar <abhishek.mrt22@gmail.com>
2021-04-07 14:13:16 +0530

    refactor
    Signed-off-by: Abhishek Kumar <abhishek.mrt22@gmail.com>

--- plugins/host-allocators/random/src/main/java/com/cloud/agent/manager/allocator/impl/RandomAllocator.java
+++ plugins/host-allocators/random/src/main/java/com/cloud/agent/manager/allocator/impl/RandomAllocator.java
@@ -22,6 +22,7 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Component;
 
@@ -56,44 +57,46 @@ public class RandomAllocator extends AdapterBase implements HostAllocator {
     @Inject
     private CapacityManager capacityManager;
 
-    @Override
-    public List<Host> allocateTo(VirtualMachineProfile vmProfile, DeploymentPlan plan, Type type, ExcludeList avoid, int returnUpTo) {
-        return allocateTo(vmProfile, plan, type, avoid, returnUpTo, true);
-    }
-
-    @Override
-    public List<Host> allocateTo(VirtualMachineProfile vmProfile, DeploymentPlan plan, Type type, ExcludeList avoid, List<? extends Host> hosts, int returnUpTo,
-        boolean considerReservedCapacity) {
+    private List<Host> findSuitableHosts(VirtualMachineProfile vmProfile, DeploymentPlan plan, Type type,
+                                         ExcludeList avoid, List<? extends Host> hosts, int returnUpTo,
+                                         boolean considerReservedCapacity) {
         long dcId = plan.getDataCenterId();
         Long podId = plan.getPodId();
         Long clusterId = plan.getClusterId();
         ServiceOffering offering = vmProfile.getServiceOffering();
+        List<? extends Host> hostsCopy = null;
         List<Host> suitableHosts = new ArrayList<Host>();
-        List<Host> hostsCopy = new ArrayList<Host>(hosts);
 
         if (type == Host.Type.Storage) {
             return suitableHosts;
         }
-
         String hostTag = offering.getHostTag();
         if (hostTag != null) {
             s_logger.debug("Looking for hosts in dc: " + dcId + "  pod:" + podId + "  cluster:" + clusterId + " having host tag:" + hostTag);
         } else {
             s_logger.debug("Looking for hosts in dc: " + dcId + "  pod:" + podId + "  cluster:" + clusterId);
         }
-
-        // list all computing hosts, regardless of whether they support routing...it's random after all
-        if (hostTag != null) {
-            hostsCopy.retainAll(_hostDao.listByHostTag(type, clusterId, podId, dcId, hostTag));
+        if (hosts != null) {
+            // retain all computing hosts, regardless of whether they support routing...it's random after all
+            hostsCopy = new ArrayList<Host>(hosts);
+            if (hostTag != null) {
+                hostsCopy.retainAll(_hostDao.listByHostTag(type, clusterId, podId, dcId, hostTag));
+            } else {
+                hostsCopy.retainAll(_resourceMgr.listAllUpAndEnabledHosts(type, clusterId, podId, dcId));
+            }
         } else {
-            hostsCopy.retainAll(_resourceMgr.listAllUpAndEnabledHosts(type, clusterId, podId, dcId));
+            // list all computing hosts, regardless of whether they support routing...it's random after all
+            hostsCopy = new ArrayList<HostVO>();
+            if (hostTag != null) {
+                hostsCopy = _hostDao.listByHostTag(type, clusterId, podId, dcId, hostTag);
+            } else {
+                hostsCopy = _resourceMgr.listAllUpAndEnabledHosts(type, clusterId, podId, dcId);
+            }
         }
-
         s_logger.debug("Random Allocator found " + hostsCopy.size() + "  hosts");
         if (hostsCopy.size() == 0) {
             return suitableHosts;
         }
-
         Collections.shuffle(hostsCopy);
         for (Host host : hostsCopy) {
             if (suitableHosts.size() == returnUpTo) {
@@ -117,76 +120,34 @@ public class RandomAllocator extends AdapterBase implements HostAllocator {
             }
             suitableHosts.add(host);
         }
-
         if (s_logger.isDebugEnabled()) {
             s_logger.debug("Random Host Allocator returning " + suitableHosts.size() + " suitable hosts");
         }
-
         return suitableHosts;
     }
 
     @Override
-    public List<Host> allocateTo(VirtualMachineProfile vmProfile, DeploymentPlan plan, Type type, ExcludeList avoid, int returnUpTo, boolean considerReservedCapacity) {
+    public List<Host> allocateTo(VirtualMachineProfile vmProfile, DeploymentPlan plan, Type type, ExcludeList avoid, int returnUpTo) {
+        return allocateTo(vmProfile, plan, type, avoid, returnUpTo, true);
+    }
 
-        long dcId = plan.getDataCenterId();
-        Long podId = plan.getPodId();
-        Long clusterId = plan.getClusterId();
-        ServiceOffering offering = vmProfile.getServiceOffering();
-
-        List<Host> suitableHosts = new ArrayList<Host>();
-
-        if (type == Host.Type.Storage) {
-            return suitableHosts;
-        }
-
-        String hostTag = offering.getHostTag();
-        if (hostTag != null) {
-            s_logger.debug("Looking for hosts in dc: " + dcId + "  pod:" + podId + "  cluster:" + clusterId + " having host tag:" + hostTag);
-        } else {
-            s_logger.debug("Looking for hosts in dc: " + dcId + "  pod:" + podId + "  cluster:" + clusterId);
-        }
-
-        // list all computing hosts, regardless of whether they support routing...it's random after all
-        List<? extends Host> hosts = new ArrayList<HostVO>();
-        if (hostTag != null) {
-            hosts = _hostDao.listByHostTag(type, clusterId, podId, dcId, hostTag);
-        } else {
-            hosts = _resourceMgr.listAllUpAndEnabledHosts(type, clusterId, podId, dcId);
-        }
-
-        s_logger.debug("Random Allocator found " + hosts.size() + "  hosts");
-
-        if (hosts.size() == 0) {
-            return suitableHosts;
-        }
-
-        Collections.shuffle(hosts);
-        for (Host host : hosts) {
-            if (suitableHosts.size() == returnUpTo) {
-                break;
-            }
-            if (avoid.shouldAvoid(host)) {
-                if (s_logger.isDebugEnabled()) {
-                    s_logger.debug("Host name: " + host.getName() + ", hostId: " + host.getId() + " is in avoid set, skipping this and trying other available hosts");
-                }
-                continue;
-            }
-            Pair<Boolean, Boolean> cpuCapabilityAndCapacity = capacityManager.checkIfHostHasCpuCapabilityAndCapacity(host, offering, considerReservedCapacity);
-            if (!cpuCapabilityAndCapacity.first() || !cpuCapabilityAndCapacity.second()) {
-                if (s_logger.isDebugEnabled()) {
-                    s_logger.debug("Not using host " + host.getId() + "; host has cpu capability? " + cpuCapabilityAndCapacity.first() + ", host has capacity?" + cpuCapabilityAndCapacity.second());
-                }
-                continue;
-            }
+    @Override
+    public List<Host> allocateTo(VirtualMachineProfile vmProfile, DeploymentPlan plan, Type type,
+                                 ExcludeList avoid, List<? extends Host> hosts, int returnUpTo,
+                                 boolean considerReservedCapacity) {
+        if (CollectionUtils.isEmpty(hosts)) {
             if (s_logger.isDebugEnabled()) {
-                s_logger.debug("Found a suitable host, adding to list: " + host.getId());
+                s_logger.debug("Random Allocator found 0 hosts as given host list is empty");
             }
-            suitableHosts.add(host);
+            return new ArrayList<Host>();
         }
-        if (s_logger.isDebugEnabled()) {
-            s_logger.debug("Random Host Allocator returning " + suitableHosts.size() + " suitable hosts");
-        }
-        return suitableHosts;
+        return findSuitableHosts(vmProfile, plan, type, avoid, hosts, returnUpTo, considerReservedCapacity);
+    }
+
+    @Override
+    public List<Host> allocateTo(VirtualMachineProfile vmProfile, DeploymentPlan plan,
+                                 Type type, ExcludeList avoid, int returnUpTo, boolean considerReservedCapacity) {
+        return findSuitableHosts(vmProfile, plan, type, avoid, null, returnUpTo, considerReservedCapacity);
     }
 
     @Override
