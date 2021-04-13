60bb41367c720f51853d3400b55cd969d4d38a55
utchoang <hoangnm@unitech.vn>
2021-04-13 13:41:59 +0700

    fixes the color style of the filter icon

--- ui/src/components/view/SearchView.vue
+++ ui/src/components/view/SearchView.vue
@@ -104,7 +104,7 @@
             class="filter-button"
             size="small"
             @click="() => { searchQuery = null }">
-            <a-icon type="filter" :theme="Object.keys(searchParams).length > 0 ? 'twoTone' : 'outlined'" />
+            <a-icon type="filter" :theme="isFiltered ? 'twoTone' : 'outlined'" />
           </a-button>
         </a-popover>
       </a-input-search>
@@ -140,7 +140,8 @@ export default {
       fields: [],
       inputKey: null,
       inputValue: null,
-      fieldValues: {}
+      fieldValues: {},
+      isFiltered: false
     }
   },
   beforeCreate () {
@@ -164,6 +165,13 @@ export default {
     if (this.$route && this.$route.query && 'q' in this.$route.query) {
       this.searchQuery = this.$route.query.q
     }
+    this.isFiltered = false
+    this.searchFilters.some(item => {
+      if (this.searchParams[item]) {
+        this.isFiltered = true
+        return true
+      }
+    })
   },
   computed: {
     styleSearch () {
@@ -417,6 +425,7 @@ export default {
         field[item] = undefined
         this.form.setFieldsValue(field)
       })
+      this.isFiltered = false
       this.inputKey = null
       this.inputValue = null
       this.searchQuery = null
@@ -430,6 +439,7 @@ export default {
         if (err) {
           return
         }
+        this.isFiltered = true
         for (const key in values) {
           const input = values[key]
           if (input === '' || input === null || input === undefined) {
