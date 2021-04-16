0cd40f437806d71748488b0142f1589322e1ff52
Gabriel Brascher <gabriel@apache.org>
2021-04-16 10:13:24 -0300

    Allow upgrading offering with shared storage or local storage if the volume is already allocated on the storage that matches with new offering type.
    This is done by changing the validation in a way that it checks if new offering matches the current volume's storage pool as a shared or local.
    
    Return Service Offerings with the same storage type (local vs shared) than the storage pool where the VM's root volume is allocated

--- engine/api/src/main/java/com/cloud/vm/VirtualMachineManager.java
+++ engine/api/src/main/java/com/cloud/vm/VirtualMachineManager.java
@@ -255,4 +255,9 @@ public interface VirtualMachineManager extends Manager {
     boolean unmanage(String vmUuid);
 
     UserVm restoreVirtualMachine(long vmId, Long newTemplateId) throws ResourceUnavailableException, InsufficientCapacityException;
+
+    /**
+     * Returns true if the VM's Root volume is allocated at a local storage pool
+     */
+    boolean isRootVolumeOnLocalStorage(long vmId);
 }
