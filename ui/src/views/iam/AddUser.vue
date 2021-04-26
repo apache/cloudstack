1476bd36ab7bf676b55e80539b0cbea2d914f7cf
davidjumani <dj.davidjumani1994@gmail.com>
2021-04-26 11:32:31 +0530

    ui: Fix error in adduser

--- ui/src/views/iam/AddUser.vue
+++ ui/src/views/iam/AddUser.vue
@@ -218,7 +218,7 @@ export default {
       domainid: null
     }
   },
-  beforeCreate () {
+  created () {
     this.form = this.$form.createForm(this)
     this.apiConfig = this.$store.getters.apis.createUser || {}
     this.apiParams = {}
