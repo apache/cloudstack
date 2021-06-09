cb3c4a58f5ec845a7a7c2d098096621d9e4dc475
Apurv Gupta <apurv3351@gmail.com>
2021-06-09 12:22:17 +0530

    vue-shortkey

--- ui/src/main.js
+++ ui/src/main.js
@@ -28,13 +28,14 @@ import './permission' // permission control
 import './utils/filter' // global filter
 import { pollJobPlugin, notifierPlugin, toLocaleDatePlugin, configUtilPlugin } from './utils/plugins'
 import { VueAxios } from './utils/request'
+import VueShortkey from 'vue-shortkey' // keyboard shortcuts
 
 Vue.config.productionTip = false
 Vue.use(VueAxios, router)
 Vue.use(pollJobPlugin)
 Vue.use(notifierPlugin)
 Vue.use(toLocaleDatePlugin)
-Vue.use(require('vue-shortkey'))
+Vue.use(VueShortkey)
 
 fetch('config.json').then(response => response.json()).then(config => {
   Vue.prototype.$config = config
