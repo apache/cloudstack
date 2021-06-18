f3d3405b4ffa824dd65f46794fb56c3fd04c1ca0
nicolas <nicovazquez90@gmail.com>
2021-06-18 16:19:30 -0300

    New annotations tab

---
+++ ui/src/components/view/AnnotationsTable.vue
@@ -0,0 +1,206 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+<template>
+
+  <div class="account-center-team" v-if="annotationType && 'listAnnotations' in $store.getters.apis">
+    <a-divider :dashed="true"/>
+    <a-spin :spinning="loadingAnnotations">
+      <div class="title">
+        {{ $t('label.comments') }} ({{ notes.length }})
+      </div>
+      <a-list
+        v-if="notes.length"
+        :dataSource="notes"
+        itemLayout="horizontal"
+        size="small" >
+        <a-list-item slot="renderItem" slot-scope="item">
+          <a-comment
+            :content="item.annotation"
+            :datetime="$toLocaleDate(item.created)"
+            :author="item.username" >
+            <a-button
+              v-if="'removeAnnotation' in $store.getters.apis && isAdminOrAnnotationOwner(item)"
+              slot="avatar"
+              type="danger"
+              shape="circle"
+              size="small"
+              @click="deleteNote(item)">
+              <a-icon type="delete"/>
+            </a-button>
+          </a-comment>
+        </a-list-item>
+      </a-list>
+
+      <a-comment v-if="'addAnnotation' in $store.getters.apis">
+        <a-avatar
+          slot="avatar"
+          icon="edit"
+          @click="showNotesInput = true" />
+        <div slot="content">
+          <a-textarea
+            rows="4"
+            @change="handleNoteChange"
+            :value="annotation"
+            :placeholder="$t('label.add.note')" />
+          <a-checkbox @change="toggleNoteVisibility" v-if="['Admin'].includes(this.$store.getters.userInfo.roletype)">
+            {{ $t('label.annotation.admins.only') }}
+          </a-checkbox>
+          <a-button
+            style="margin-top: 10px"
+            @click="saveNote"
+            type="primary"
+          >
+            {{ $t('label.save') }}
+          </a-button>
+        </div>
+      </a-comment>
+    </a-spin>
+  </div>
+</template>
+
+<script>
+
+import { api } from '@/api'
+
+export default {
+  name: 'AnnotationsTab',
+  props: {
+    resource: {
+      type: Object,
+      required: true
+    },
+    loading: {
+      type: Boolean,
+      default: false
+    }
+  },
+  inject: ['parentFetchData'],
+  data () {
+    return {
+      loadingAnnotations: false,
+      notes: [],
+      annotation: '',
+      annotationType: '',
+      annotationAdminsOnly: false,
+      showNotesInput: false,
+
+      annotationColumns: [
+        {
+          title: this.$t('label.created'),
+          dataIndex: 'created'
+        },
+        {
+          title: this.$t('label.username'),
+          dataIndex: 'username'
+        },
+        {
+          title: this.$t('label.annotation'),
+          dataIndex: 'annotation'
+        },
+        {
+          title: this.$t('label.annotation.entity.id'),
+          dataIndex: 'entityid'
+        },
+        {
+          title: this.$t('label.annotation.entity.type'),
+          dataIndex: 'entitytype'
+        },
+        {
+          title: this.$t('label.actions'),
+          detaIndex: ''
+        }
+      ]
+    }
+  },
+  watch: {
+    resource: function (newItem, oldItem) {
+      this.resource = newItem
+      this.resourceType = this.$route.meta.resourceType
+      this.annotationType = ''
+
+      switch (this.resourceType) {
+        case 'UserVm':
+          this.annotationType = 'VM'
+          break
+        case 'Domain':
+          this.annotationType = 'DOMAIN'
+          break
+        case 'Host':
+          this.annotationType = 'HOST'
+          break
+      }
+
+      if (this.annotationType) {
+        this.getAnnotations()
+      }
+    }
+  },
+  methods: {
+    getAnnotations () {
+      if (!('listAnnotations' in this.$store.getters.apis)) {
+        return
+      }
+      this.loadingAnnotations = true
+      this.notes = []
+      api('listAnnotations', { entityid: this.resource.id, entitytype: this.annotationType }).then(json => {
+        if (json.listannotationsresponse && json.listannotationsresponse.annotation) {
+          this.notes = json.listannotationsresponse.annotation
+        }
+      }).finally(() => {
+        this.loadingAnnotations = false
+      })
+    },
+    handleNoteChange (e) {
+      this.annotation = e.target.value
+    },
+    toggleNoteVisibility () {
+      this.annotationAdminsOnly = !this.annotationAdminsOnly
+    },
+    isAdminOrAnnotationOwner (annotation) {
+      return ['Admin'].includes(this.$store.getters.userInfo.roletype) || this.$store.getters.userInfo.id === annotation.userid
+    },
+    saveNote () {
+      if (this.annotation.length < 1) {
+        return
+      }
+      this.loadingAnnotations = true
+      this.showNotesInput = false
+      const args = {}
+      args.entityid = this.resource.id
+      args.entitytype = this.annotationType
+      args.annotation = this.annotation
+      args.adminsonly = this.annotationAdminsOnly
+      api('addAnnotation', args).then(json => {
+      }).finally(e => {
+        this.getNotes()
+      })
+      this.annotation = ''
+      this.annotationAdminsOnly = false
+    },
+    deleteNote (annotation) {
+      this.loadingAnnotations = true
+      const args = {}
+      args.id = annotation.id
+      api('removeAnnotation', args).then(json => {
+      }).finally(e => {
+        this.getNotes()
+      })
+    }
+  }
+}
+</script>
--- ui/src/views/compute/InstanceTab.vue
+++ ui/src/views/compute/InstanceTab.vue
@@ -138,6 +138,10 @@
       <a-tab-pane :tab="$t('label.settings')" key="settings">
         <DetailSettings :resource="resource" :loading="loading" />
       </a-tab-pane>
+      <a-tab-pane :tab="$t('label.annotations')" key="annotations" v-if="'listAnnotations' in $store.getters.apis">
+        <AnnotationsTable :resource="vm" :loading="loading">
+        </AnnotationsTable>
+      </a-tab-pane>
     </a-tabs>
 
     <a-modal
@@ -262,6 +266,7 @@ import DetailSettings from '@/components/view/DetailSettings'
 import NicsTable from '@/views/network/NicsTable'
 import ListResourceTable from '@/components/view/ListResourceTable'
 import TooltipButton from '@/components/widgets/TooltipButton'
+import AnnotationsTable from '@/components/view/AnnotationsTable'
 
 export default {
   name: 'InstanceTab',
@@ -272,7 +277,8 @@ export default {
     NicsTable,
     Status,
     ListResourceTable,
-    TooltipButton
+    TooltipButton,
+    AnnotationsTable
   },
   mixins: [mixinDevice],
   props: {
