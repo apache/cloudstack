24fd8e43e56df4e12bfb6d714860379361ce8840
utchoang <hoangnm@unitech.vn>
2021-06-24 12:00:39 +0700

    add new vue 3 library & and 2.x (beta)

--- ui/vue.config.js
+++ ui/vue.config.js
@@ -95,35 +95,6 @@ const vueConfig = {
     config.module.rule('eslint').use('eslint-loader').tap(
       opts => ({ ...opts, emitWarning: false })
     )
-
-    const svgRule = config.module.rule('svg')
-    svgRule.uses.clear()
-    svgRule
-      .oneOf('inline')
-      .resourceQuery(/inline/)
-      .use('vue-svg-icon-loader')
-      .loader('vue-svg-icon-loader')
-      .end()
-      .end()
-      .oneOf('external')
-      .use('file-loader')
-      .loader('file-loader')
-      .options({
-        name: 'assets/[name].[hash:8].[ext]'
-      })
-    /* svgRule.oneOf('inline')
-      .resourceQuery(/inline/)
-      .use('vue-svg-loader')
-      .loader('vue-svg-loader')
-      .end()
-      .end()
-      .oneOf('external')
-      .use('file-loader')
-      .loader('file-loader')
-      .options({
-        name: 'assets/[name].[hash:8].[ext]'
-      })
-    */
   },
 
   css: {
