f56f35884b7e91cb4ec496ec4575059ee1be2a20
Niklas Koch <niklaskoch2001@web.de>
2021-06-28 10:59:12 +0200

    Changed Vars and some minior Fixes.

--- ui/docker.sh
+++ ui/docker.sh
@@ -29,18 +29,18 @@ DATE="$(date --iso-8601=seconds)"
 LABEL_DATE="--label \"org.opencontainers.image.created=${DATE}\""
 GIT_REV="$(git rev-parse HEAD)"
 LABEL_GIT_REV="--label \"org.opencontainers.image.revision=${GIT_REV}\""
-use_podman=0
-use_docker=0
+USE_PODMAN=0
+USE_DOCKER=0
 OPTIONS='pd'
 while getopts $OPTIONS OPTION
 do
         case "$OPTION" in
-                p)use_podman=1;;
-                d)use_docker=1;;
+                p)USE_PODMAN=1;;
+                d)USE_DOCKER=1;;
                 *)echo "Please choose between -p for podman or -d for docker.";;
         esac
 done
-if [ "$use_podman" -eq 1 ]; then
+if [[ $USE_PODMAN -eq 1 ]]; then
         podman build -t cloudstack-ui ${LABEL_DATE} ${LABEL_GIT_REV} ${LABEL_GIT_TAG}
         exit
 fi
