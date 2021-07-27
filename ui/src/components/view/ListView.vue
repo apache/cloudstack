2643854173ee10acaf1165d14928b1c0d30361a3
davidjumani <dj.davidjumani1994@gmail.com>
2021-07-27 14:57:55 +0530

    ui: Fix current for vmsnapshots (#5247)

--- ui/src/components/view/ListView.vue
+++ ui/src/components/view/ListView.vue
@@ -233,6 +233,9 @@
     <a slot="readonly" slot-scope="text, record">
       <status :text="record.readonly ? 'ReadOnly' : 'ReadWrite'" />
     </a>
+    <span slot="current" slot-scope="text, record">
+      <status :text="record.current ? record.current.toString() : 'false'" />
+    </span>
     <span slot="created" slot-scope="text">
       {{ $toLocaleDate(text) }}
     </span>
