45effff82235235f1f96bc4edd73b91ead3a139c
utchoang <hoangnm@unitech.vn>
2021-08-24 13:13:23 +0700

    show display name in alert message

--- ui/src/views/AutogenView.vue
+++ ui/src/views/AutogenView.vue
@@ -143,9 +143,14 @@
           </a>
         </span>
         <a-spin :spinning="actionLoading">
-          <span v-if="currentAction.message">
+          <span v-if="currentAction.message || 'displayName' in currentAction">
             <a-alert type="warning">
-              <span slot="message" v-html="$t(currentAction.message)" />
+              <span slot="message">
+                <div v-if="currentAction.message" v-html="$t(currentAction.message)"></div>
+                <div v-if="'displayName' in currentAction">
+                  <strong>{{ $t('label.name') + ': ' + currentAction.displayName(resource) }}</strong>
+                </div>
+              </span>
             </a-alert>
             <br v-if="currentAction.paramFields.length > 0"/>
           </span>
@@ -153,17 +158,6 @@
             :form="form"
             @submit="handleSubmit"
             layout="vertical" >
-            <a-form-item
-              v-if="'displayName' in currentAction"
-              :class="[currentAction.paramFields.length > 0 ? '' : 'custom-form-input']">
-              <span slot="label">
-                {{ $t('label.name') }}
-                <a-tooltip :title="$t('label.name')">
-                  <a-icon type="info-circle" />
-                </a-tooltip>
-              </span>
-              <a-alert type="info" :message="currentAction.displayName(resource)"></a-alert>
-            </a-form-item>
             <a-form-item
               v-for="(field, fieldIndex) in currentAction.paramFields"
               :key="fieldIndex"
@@ -1244,9 +1238,5 @@ export default {
 
 .ant-breadcrumb {
   vertical-align: text-bottom;
-}
-
-.custom-form-input {
-  margin-top: 10px;
 }
 </style>
