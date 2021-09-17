f2315ec0e2419826b9aba5dcc3f12fa15f03ed48
Wei Zhou <weizhou@apache.org>
2021-09-17 09:26:55 +0200

    ui: list static routes with listall=true (#5463)

--- ui/src/views/network/StaticRoutesTab.vue
+++ ui/src/views/network/StaticRoutesTab.vue
@@ -118,7 +118,10 @@ export default {
   methods: {
     fetchData () {
       this.componentLoading = true
-      api('listStaticRoutes', { gatewayid: this.resource.id }).then(json => {
+      api('listStaticRoutes', {
+        gatewayid: this.resource.id,
+        listall: true
+      }).then(json => {
         this.routes = json.liststaticroutesresponse.staticroute
       }).catch(error => {
         this.$notifyError(error)
