d9154e8a69d4e09f2ec9ed4fe8ca30c9e62f1e6b
davidjumani <dj.davidjumani1994@gmail.com>
2021-09-21 17:04:57 +0530

    ui: Fix editVM in projectview (#5485)

--- ui/src/views/compute/EditVM.vue
+++ ui/src/views/compute/EditVM.vue
@@ -180,11 +180,16 @@ export default {
     fetchInstaceGroups () {
       this.groups.loading = true
       this.groups.opts = []
-      api('listInstanceGroups', {
-        account: this.$store.getters.userInfo.account,
+      const params = {
         domainid: this.$store.getters.userInfo.domainid,
         listall: true
-      }).then(json => {
+      }
+      if (this.$store.getters.project && this.$store.getters.project.id) {
+        params.projectid = this.$store.getters.project.id
+      } else {
+        params.account = this.$store.getters.userInfo.account
+      }
+      api('listInstanceGroups', params).then(json => {
         const groups = json.listinstancegroupsresponse.instancegroup || []
         groups.forEach(x => {
           this.groups.opts.push(x.name)
