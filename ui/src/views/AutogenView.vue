4a42e7ef9e2dd0766d9a3f425a7e8fbd66d0bdce
Abhishek Kumar <abhishek.mrt22@gmail.com>
2021-10-03 14:41:42 +0530

    vmware, ui: update portgroup on network update (#5470)
    Enhanced update network form in the UI.
    On network offering change for an isolated network,
    
    - VMware portgroup should be updated accordingly.
    - VMs on the network should be placed on the correct VMware portgroup based on the network rate, https://docs.cloudstack.apache.org/en/latest/adminguide/service_offerings.html#network-throttling.
    
    Signed-off-by: Abhishek Kumar <abhishek.mrt22@gmail.com>

--- ui/src/views/AutogenView.vue
+++ ui/src/views/AutogenView.vue
@@ -1073,6 +1073,13 @@ export default {
         params.isofilter = 'executable'
       } else if (possibleApi === 'listHosts') {
         params.type = 'routing'
+      } else if (possibleApi === 'listNetworkOfferings' && this.resource) {
+        if (this.resource.type) {
+          params.guestiptype = this.resource.type
+        }
+        if (!this.resource.vpcid) {
+          params.forvpc = false
+        }
       }
       if (showIcon) {
         params.showicon = true
