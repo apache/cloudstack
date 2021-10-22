2e58cdf5bcf76a41914b29155dd47174463648dc
nvazquez <nicovazquez90@gmail.com>
2021-10-22 10:56:37 -0300

    Fix duplicate provider field

--- ui/src/views/infra/AddPrimaryStorage.vue
+++ ui/src/views/infra/AddPrimaryStorage.vue
@@ -195,18 +195,6 @@
             </a-select-option>
           </a-select>
         </a-form-item>
-        <div v-if="protocolSelected !== 'Linstor'">
-          <a-form-item>
-            <tooltip-label slot="label" :title="$t('label.providername')" :tooltip="apiParams.provider.description"/>
-            <a-select
-              v-decorator="['provider', { initialValue: providerSelected, rules: [{ required: true, message: `${$t('label.required')}`}] }]"
-              @change="updateProviderAndProtocol">
-              <a-select-option :value="provider" v-for="(provider,idx) in providers" :key="idx">
-                {{ provider }}
-              </a-select-option>
-            </a-select>
-          </a-form-item>
-        </div>
         <div v-if="this.providerSelected !== 'DefaultPrimary' && this.providerSelected !== 'PowerFlex' && this.providerSelected !== 'Linstor'">
           <a-form-item>
             <tooltip-label slot="label" :title="$t('label.ismanaged')" :tooltip="apiParams.managed.description"/>
