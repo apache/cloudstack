c66f308d3885a462cf09070c9c87463d6978f155
Hoang Nguyen <hoangnm@unitech.vn>
2021-10-22 12:47:46 +0700

    fix the error of not being able to search for ostype selectbox (#5607)

--- ui/src/views/AutogenView.vue
+++ ui/src/views/AutogenView.vue
@@ -269,7 +269,7 @@
                   }"
                   :autoFocus="fieldIndex === firstIndex"
                 >
-                  <a-select-option key="">{{ }}</a-select-option>
+                  <a-select-option key="" label="">{{ }}</a-select-option>
                   <a-select-option v-for="opt in field.opts" :key="opt.id" :label="opt.name || opt.description || opt.traffictype || opt.publicip">
                     <div>
                       <span v-if="(field.name.startsWith('template') || field.name.startsWith('iso'))">
