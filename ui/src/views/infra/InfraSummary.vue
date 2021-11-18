a8e7315e53b3539d336b35fc127c2dd34624d413
DH Park <pdh1974@gmail.com>
2021-11-18 17:14:25 +0900

    UI : Fix SSL certificate submit button not working (#5682)
    * Updating pom.xml version numbers for release 4.17.0.0-SNAPSHOT
    
    Signed-off-by: nicolas <nicovazquez90@gmail.com>
    
    * Fix build and version numbers
    
    * SSL Certificates 버튼이 작동하지않는 버그
    
    SSL Certificates 버튼이 작동하지않는 버그
    
    * Revert "Updating pom.xml version numbers for release 4.17.0.0-SNAPSHOT"
    
    This reverts commit e4dba9fb95b5be9f3f3c0ea84ffbc45c9ecfe2eb.
    
    * Revert "Fix build and version numbers"
    
    This reverts commit fd6161a9dfc2c4c9ab7d6d17677e142dfe473ea3.
    
    Co-authored-by: nicolas <nicovazquez90@gmail.com>
    Co-authored-by: Dajeong-Park <ekwjd7182@gmail.com>

--- ui/src/views/infra/InfraSummary.vue
+++ ui/src/views/infra/InfraSummary.vue
@@ -136,7 +136,7 @@
                 <a-button @click="this.sslModalClose" class="close-button">
                   {{ $t('label.cancel' ) }}
                 </a-button>
-                <a-button type="primary" ref="submit" :loading="sslFormSubmitting">
+                <a-button @click="this.handleSslFormSubmit" type="primary" ref="submit" :loading="sslFormSubmitting">
                   {{ $t('label.submit' ) }}
                 </a-button>
               </a-form-item>
