f03558ed3873156419f7b6129e836c65a8444c77
Daan Hoogland <dahn@onecht.net>
2021-12-02 13:10:12 +0100

    conditional broadcastUri

--- ui/src/config/section/network.js
+++ ui/src/config/section/network.js
@@ -29,7 +29,13 @@ export default {
       icon: 'apartment',
       permission: ['listNetworks'],
       resourceType: 'Network',
-      columns: ['name', 'state', 'type', 'vpcname', 'cidr', 'ip6cidr', 'broadcasturi', 'domain', 'account', 'zonename'],
+      columns: () => {
+        var fields = ['name', 'state', 'type', 'vpcname', 'cidr', 'ip6cidr', 'domain', 'account', 'zonename']
+        if (['Admin'].includes(store.getters.userInfo.roletype)) {
+          fields.push('broadcasturi')
+        }
+        return fields
+      },
       details: ['name', 'id', 'description', 'type', 'traffictype', 'vpcid', 'vlan', 'broadcasturi', 'cidr', 'ip6cidr', 'netmask', 'gateway', 'aclname', 'ispersistent', 'restartrequired', 'reservediprange', 'redundantrouter', 'networkdomain', 'zonename', 'account', 'domain'],
       filters: ['all', 'isolated', 'shared', 'l2'],
       searchFilters: ['keyword', 'zoneid', 'domainid', 'account', 'tags'],
