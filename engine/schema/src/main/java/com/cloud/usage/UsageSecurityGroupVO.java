daa7509c75dfcef37aa67f566352148061506e1c
nvazquez <nicovazquez90@gmail.com>
2021-12-16 15:00:18 -0300

    Fix imports

--- engine/schema/src/main/java/com/cloud/usage/UsageSecurityGroupVO.java
+++ engine/schema/src/main/java/com/cloud/usage/UsageSecurityGroupVO.java
@@ -20,7 +20,14 @@ import org.apache.cloudstack.api.InternalIdentity;
 
 import java.util.Date;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
 @Table(name = "usage_security_group")
--- engine/schema/src/main/java/com/cloud/usage/UsageVolumeVO.java
+++ engine/schema/src/main/java/com/cloud/usage/UsageVolumeVO.java
@@ -18,7 +18,14 @@ package com.cloud.usage;
 
 import java.util.Date;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 import org.apache.cloudstack.api.InternalIdentity;
 
