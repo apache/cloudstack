a44e6311369acd4c9b78aa51bbbb5b3311127579
Abhishek Kumar <abhishek.mrt22@gmail.com>
2021-12-17 12:35:04 +0530

    changes for diagnostics
    Signed-off-by: Abhishek Kumar <abhishek.mrt22@gmail.com>

--- ui/src/config/section/infra/systemVms.js
+++ ui/src/config/section/infra/systemVms.js
@@ -116,7 +116,7 @@ export default {
           value: (record) => { return record.id }
         },
         type: {
-          options: ['ping', 'traceroute', 'arping']
+          options: ['ping', 'ping6', 'traceroute', 'traceroute6', 'arping']
         }
       },
       response: (result) => { return result && result.diagnostics ? `<strong>Output</strong>:<br/>${result.diagnostics.stdout}<br/><strong>Error</strong>: ${result.diagnostics.stderr}<br/><strong>Exit Code</strong>: ${result.diagnostics.exitcode}` : 'Invalid response' }
