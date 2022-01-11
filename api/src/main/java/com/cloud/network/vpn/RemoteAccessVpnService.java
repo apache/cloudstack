02627ee100e004c168dd1a9575972f2d6d7c8a22
Abhishek Kumar <abhishek.mrt22@gmail.com>
2022-01-11 15:39:06 +0530

    api, server: fix add-remove vpn user without vpn owner
    Fixes #5711
    
    ACS should not add a new user in Add state when the owner account does not have VPN access.
    While removing VPN user ACS should not fail completely when owner account ahs no VPN.
    
    Signed-off-by: Abhishek Kumar <abhishek.mrt22@gmail.com>

--- api/src/main/java/com/cloud/network/vpn/RemoteAccessVpnService.java
+++ api/src/main/java/com/cloud/network/vpn/RemoteAccessVpnService.java
@@ -43,6 +43,10 @@ public interface RemoteAccessVpnService {
 
     List<? extends VpnUser> listVpnUsers(long vpnOwnerId, String userName);
 
+    void removeVpnUserWithoutValidVpnOwner(long id);
+
+    boolean applyVpnUsers(long vpnOwnerId, String userName, boolean forRemove) throws ResourceUnavailableException;
+
     boolean applyVpnUsers(long vpnOwnerId, String userName) throws ResourceUnavailableException;
 
     Pair<List<? extends RemoteAccessVpn>, Integer> searchForRemoteAccessVpns(ListRemoteAccessVpnsCmd cmd);
