940a558e6c5dc659110b84ddd725eaf64575c969
Abhishek Kumar <abhishek.mrt22@gmail.com>
2022-01-27 11:15:33 +0530

    refactor
    Signed-off-by: Abhishek Kumar <abhishek.mrt22@gmail.com>

--- ui/src/components/view/DetailSettings.vue
+++ ui/src/components/view/DetailSettings.vue
@@ -173,13 +173,6 @@ export default {
         this.deployasistemplate = json.listtemplatesresponse.template[0].deployasis
       })
     },
-    filterReadOnlyDetails () {
-      for (var i = this.details.length - 1; i >= 0; i--) {
-        if (!this.allowEditOfDetail(this.details[i].name)) {
-          this.details.splice(i, 1)
-        }
-      }
-    },
     allowEditOfDetail (name) {
       if (this.resource.readonlydetails) {
         if (this.resource.readonlydetails.split(',').map(item => item.trim()).includes(name)) {
@@ -211,6 +204,27 @@ export default {
         (this.resource.domainid === this.$store.getters.userInfo.domainid && this.resource.account === this.$store.getters.userInfo.account) ||
         this.resource.project && this.resource.projectid === this.$store.getters.project.id
     },
+    getDetailsParam (details) {
+      var params = {}
+      var filteredDetails = details
+      if (this.resource.readonlydetails && filteredDetails) {
+        filteredDetails = []
+        var readOnlyDetailNames = this.resource.readonlydetails.split(',').map(item => item.trim())
+        for (var detail of this.details) {
+          if (!readOnlyDetailNames.includes(detail.name)) {
+            filteredDetails.push(detail)
+          }
+        }
+      }
+      if (filteredDetails.length === 0) {
+        params.cleanupdetails = true
+      } else {
+        filteredDetails.forEach(function (item, index) {
+          params['details[0].' + item.name] = item.value
+        })
+      }
+      return params
+    },
     runApi () {
       var apiName = ''
       if (this.resourceType === 'UserVm') {
@@ -226,14 +240,8 @@ export default {
         return
       }
 
-      const params = { id: this.resource.id }
-      if (this.details.length === 0) {
-        params.cleanupdetails = true
-      } else {
-        this.details.forEach(function (item, index) {
-          params['details[0].' + item.name] = item.value
-        })
-      }
+      var params = { id: this.resource.id }
+      params = Object.assign(params, this.getDetailsParam(this.details))
       this.loading = true
       api(apiName, params).then(json => {
         var details = {}
@@ -261,16 +269,13 @@ export default {
       }
       this.error = false
       this.details.push({ name: this.newKey, value: this.newValue })
-      this.filterReadOnlyDetails()
       this.runApi()
     },
     updateDetail (index) {
-      this.filterReadOnlyDetails()
       this.runApi()
     },
     deleteDetail (index) {
       this.details.splice(index, 1)
-      this.filterReadOnlyDetails()
       this.runApi()
     },
     onShowAddDetail () {
