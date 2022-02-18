ecf03402f5bb9b150467255e84ca8e6b172a9a22
nvazquez <nicovazquez90@gmail.com>
2022-02-18 15:23:48 -0300

    Improve

--- ui/src/utils/request.js
+++ ui/src/utils/request.js
@@ -137,9 +137,7 @@ service.interceptors.request.use(config => {
     if (!config.params.projectid && project && project.id) {
       if (config.params.command === 'listTags') {
         config.params.projectid = '-1'
-      } else if (config.params.command === 'assignVirtualMachine') {
-        // Do not set the project ID
-      } else {
+      } else if (config.params.command !== 'assignVirtualMachine') {
         config.params.projectid = project.id
       }
     }
