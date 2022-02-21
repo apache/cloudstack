7ab053049e184bc6a02a56f89fcd98a54c09d87b
DK101010 <dirk.klahre@itelligence.de>
2022-02-21 11:02:48 +0000

    add VolumeGroup

---
+++ engine/schema/src/main/java/com/cloud/storage/VolumeGroupVO.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2022 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.cloud.storage;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import org.apache.cloudstack.api.InternalIdentity;
+
+@Entity
+@Table(name = "volume_group")
+public class VolumeGroupVO implements InternalIdentity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "vm_id")
+    private long vmId;
+
+    @Column(name = "volume_id")
+    private long volumeId;
+
+    @Column(name = "group_number")
+    private int groupNumber;
+
+
+    public VolumeGroupVO() {}
+
+    public VolumeGroupVO(long vmId, long volumeId, int groupNumber) {
+        this.vmId = vmId;
+        this.volumeId = volumeId;
+        this.groupNumber = groupNumber;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getVmId() {
+        return vmId;
+    }
+
+    public void setVmId(long vmId) {
+        this.vmId = vmId;
+    }
+
+    public int getGroupNumber() {
+        return groupNumber;
+    }
+
+    public void setGroupNumber(int groupNumber) {
+        this.groupNumber = groupNumber;
+    }
+
+    public long getVolumeId() {
+        return volumeId;
+    }
+
+    public void setVolumeId(long volumeId) {
+        this.volumeId = volumeId;
+    }
+}
---
+++ engine/schema/src/main/java/com/cloud/storage/dao/VolumeGroupDaoImpl.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2022 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.cloud.storage.dao;
+
+import com.cloud.utils.db.GenericDaoBase;
+import com.cloud.storage.VolumeGroupVO;
+import com.cloud.utils.db.SearchBuilder;
+import com.cloud.utils.db.SearchCriteria;
+
+
+public class VolumeGroupDaoImpl extends GenericDaoBase<VolumeGroupVO, Long> implements VolumeGroupDao {
+    protected final SearchBuilder<VolumeGroupVO> allFieldsSearch;
+
+    public VolumeGroupDaoImpl(){
+        allFieldsSearch = createSearchBuilder();
+        allFieldsSearch.and("vmId", allFieldsSearch.entity().getVmId(), SearchCriteria.Op.EQ);
+        allFieldsSearch.and("volumeId", allFieldsSearch.entity().getVolumeId(), SearchCriteria.Op.EQ);
+        allFieldsSearch.and("groupNumber", allFieldsSearch.entity().getGroupNumber(), SearchCriteria.Op.EQ);
+        allFieldsSearch.done();
+    }
+
+    @Override
+    public void addVolumeToGroup(long vmId, long volumeId, long deviceId, int groupNumber) {
+        if (groupNumber != -1) {
+            if (deviceId == 0L) {
+                groupNumber = 1;
+            }
+            VolumeGroupVO group = new VolumeGroupVO();
+            group.setVmId(vmId);
+            group.setVolumeId(volumeId);
+            group.setGroupNumber(groupNumber);
+            persist(group);
+        }
+    }
+
+    @Override
+    public void deleteVolumeFromGroup(long volumeId) {
+        SearchCriteria<VolumeGroupVO> sc = allFieldsSearch.create();
+        sc.setParameters("volumeId", volumeId);
+        expunge(sc);
+    }
+
+    @Override
+    public VolumeGroupVO findByVmAndVolume(long vmId, long volumeId){
+        SearchCriteria<VolumeGroupVO> sc = allFieldsSearch.create();
+        sc.setParameters("vmId", vmId);
+        sc.setParameters("volumeId", volumeId);
+        return findOneBy(sc);
+    }
+}
