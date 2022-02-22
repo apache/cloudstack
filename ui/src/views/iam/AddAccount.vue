f3aa6a2e91691f813d7699332467db330ceffdeb
Daan Hoogland <dahn@onecht.net>
2022-02-22 15:10:45 +0100

    some fixes by hook on containing div

--- ui/src/views/iam/AddAccount.vue
+++ ui/src/views/iam/AddAccount.vue
@@ -16,7 +16,9 @@
 // under the License.
 
 <template>
-  <div class="form-layout" @keyup.ctrl.enter="handleSubmit">
+  <div
+    class="form-layout"
+    @keyup.ctrl.enter="handleSubmit">
     <a-spin :spinning="loading">
       <a-form :form="form" :loading="loading" @submit="handleSubmit" layout="vertical">
         <a-form-item>
