38535459d24194ab219d55ca0a7c8ed74078060b
gabriel <gabriel@apache.org>
2022-03-09 18:02:20 +0100

    Change column `value` size from 255 to 4096 characters, matching the API size

--- engine/schema/src/main/resources/META-INF/db/schema-41610to41700.sql
+++ engine/schema/src/main/resources/META-INF/db/schema-41610to41700.sql
@@ -652,3 +652,5 @@ INSERT INTO `cloud`.`user_vm_details`(`vm_id`, `name`, `value`)
         INNER JOIN `cloud`.`vm_instance` ON vm_instance.id = user_vm_details.vm_id
         WHERE ssh_keypairs.account_id = vm_instance.account_id;
 
+-- PR #6080 Change column `value` size from 255 to 4096 characters, matching the API "updateConfiguration" "value" size
+ALTER TABLE `cloud`.`account_details` MODIFY `value` VARCHAR(4096);
