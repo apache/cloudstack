apache/cloudstack

commit b286cb76774e301d15cf2b3b6ea316eaa306018f

Update #5769: display a warning message if network offering list is empty

ui/src/views/network/CreateSharedNetworkForm.vue
@@ -217,6 +217,9 @@
                 {{ opt.displaytext || opt.name || opt.description }}
               </a-select-option>
             </a-select>
+            <a-alert type="warning" v-if="this.networkOfferings === null || this.networkOfferings.length === 0">
+              <span slot="message" v-html="$t('message.shared.network.offering.warning')" />
+            </a-alert>
           </a-form-item>
           <a-form-item v-if="!this.isObjectEmpty(this.selectedNetworkOffering) && this.selectedNetworkOffering.specifyvlan && isAdmin()">
             <tooltip-label slot="label" :title="$t('label.vlan')" :tooltip="apiParams.vlan.description"/>

commit 679c3253bf1a46198e9d89c027c76f29de6b2748

Update #5769: fix an UI bug caused by previous commit b286cb76774e301d15cf2b3b6ea316eaa306018f

ui/src/views/network/CreateSharedNetworkForm.vue
@@ -217,7 +217,7 @@
                 {{ opt.displaytext || opt.name || opt.description }}
               </a-select-option>
             </a-select>
-            <a-alert type="warning" v-if="this.networkOfferings === null || this.networkOfferings.length === 0">
+            <a-alert type="warning" v-if="!this.arrayHasItems(this.networkOfferings)">
               <span slot="message" v-html="$t('message.shared.network.offering.warning')" />
             </a-alert>
           </a-form-item>