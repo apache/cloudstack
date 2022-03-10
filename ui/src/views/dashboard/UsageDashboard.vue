c8e8682020fcd59febb2e2c911a9b05ad6db6062
davidjumani <dj.davidjumani1994@gmail.com>
2022-03-10 11:00:02 +0530

    ui: Fix dashboard links

--- ui/src/views/dashboard/UsageDashboard.vue
+++ ui/src/views/dashboard/UsageDashboard.vue
@@ -52,7 +52,7 @@
                 :bordered="false"
                 :loading="loading"
                 :style="stat.bgcolor ? { 'background': stat.bgcolor } : {}">
-                <router-link :to="{ path: stat.path }">
+                <router-link :to="{ path: stat.path, query: stat.query }">
                   <div
                     class="usage-dashboard-chart-card-inner">
                     <h3>{{ stat.name }}</h3>
@@ -173,7 +173,7 @@ export default {
           count = json.listvirtualmachinesresponse.count
         }
         var tileColor = this.$config.theme['@dashboard-tile-runningvms-bg'] || '#dfe9cc'
-        this.stats.splice(0, 1, { name: this.$t('label.running'), count: count, icon: 'desktop-outlined', bgcolor: tileColor, path: '/vm?state=running&filter=running' })
+        this.stats.splice(0, 1, { name: this.$t('label.running'), count: count, icon: 'desktop-outlined', bgcolor: tileColor, path: '/vm', query: { state: 'running', filter: 'running' } })
       })
       api('listVirtualMachines', { state: 'Stopped', listall: true }).then(json => {
         var count = 0
@@ -181,7 +181,7 @@ export default {
           count = json.listvirtualmachinesresponse.count
         }
         var tileColor = this.$config.theme['@dashboard-tile-stoppedvms-bg'] || '#edcbce'
-        this.stats.splice(1, 1, { name: this.$t('label.stopped'), count: count, icon: 'poweroff-outlined', bgcolor: tileColor, path: '/vm?state=stopped&filter=stopped' })
+        this.stats.splice(1, 1, { name: this.$t('label.stopped'), count: count, icon: 'poweroff-outlined', bgcolor: tileColor, path: '/vm', query: { state: 'stopped', filter: 'stopped' } })
       })
       api('listVirtualMachines', { listall: true }).then(json => {
         var count = 0
