13efa590979408893076274cd79f574c917d09c0
dahn <daan.hoogland@shapeblue.com>
2022-03-21 03:44:06 +0100

    Router health check notification mail to show router name next to UUID (#6130)
    * alerrt message altered
    
    * review feedback and format
    
    Co-authored-by: Daan Hoogland <dahn@onecht.net>

--- server/src/main/java/com/cloud/network/router/VirtualNetworkApplianceManagerImpl.java
+++ server/src/main/java/com/cloud/network/router/VirtualNetworkApplianceManagerImpl.java
@@ -1215,7 +1215,7 @@ Configurable, StateListener<VirtualMachine.State, VirtualMachine.Event, VirtualM
             return;
         }
 
-        String alertMessage = "Health checks failed: " + failingChecks.size() + " failing checks on router " + router.getUuid();
+        String alertMessage = String.format("Health checks failed: %d failing checks on router %s / %s", failingChecks.size(), router.getName(), router.getUuid());
         _alertMgr.sendAlert(AlertType.ALERT_TYPE_DOMAIN_ROUTER, router.getDataCenterId(), router.getPodIdToDeployIn(),
                 alertMessage, alertMessage);
         s_logger.warn(alertMessage + ". Checking failed health checks to see if router needs recreate");
@@ -1227,6 +1227,8 @@ Configurable, StateListener<VirtualMachine.State, VirtualMachine.Event, VirtualM
             String failedCheck = failingChecks.get(i);
             if (i == 0) {
                 failingChecksEvent.append("Router ")
+                        .append(router.getName())
+                        .append(" / ")
                         .append(router.getUuid())
                         .append(" has failing checks: ");
             }
