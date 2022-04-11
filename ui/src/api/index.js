5eb2b693e907886e826f059aeca3aab975167d0f
Hoang Nguyen <hoangnm@unitech.vn>
2022-04-11 20:45:51 +0700

    fixes login button not work and Single Sign-On tab disabled after logout (#6197)

--- ui/src/api/index.js
+++ ui/src/api/index.js
@@ -41,7 +41,9 @@ export function api (command, args = {}, method = 'GET', data = {}) {
 }
 
 export function login (arg) {
-  sourceToken.init()
+  if (!sourceToken.checkExistSource()) {
+    sourceToken.init()
+  }
 
   // Logout before login is called to purge any duplicate sessionkey cookies
   api('logout')
