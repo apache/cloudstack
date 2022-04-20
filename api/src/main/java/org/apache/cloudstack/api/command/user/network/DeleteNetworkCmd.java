4f174b0f705db0106921e8bce93a805ade7f675f
Abhishek Kumar <abhishek.mrt22@gmail.com>
2022-04-20 12:01:18 +0530

    wrong merge fix
    Signed-off-by: Abhishek Kumar <abhishek.mrt22@gmail.com>

--- api/src/main/java/org/apache/cloudstack/api/command/user/network/DeleteNetworkCmd.java
+++ api/src/main/java/org/apache/cloudstack/api/command/user/network/DeleteNetworkCmd.java
@@ -16,8 +16,6 @@
 // under the License.
 package org.apache.cloudstack.api.command.user.network;
 
-import java.util.logging.Logger;
-
 import org.apache.cloudstack.acl.SecurityChecker.AccessType;
 import org.apache.cloudstack.api.ACL;
 import org.apache.cloudstack.api.APICommand;
@@ -30,6 +28,7 @@ import org.apache.cloudstack.api.ServerApiException;
 import org.apache.cloudstack.api.response.NetworkResponse;
 import org.apache.cloudstack.api.response.SuccessResponse;
 import org.apache.cloudstack.context.CallContext;
+import org.apache.log4j.Logger;
 
 import com.cloud.event.EventTypes;
 import com.cloud.exception.InvalidParameterValueException;
