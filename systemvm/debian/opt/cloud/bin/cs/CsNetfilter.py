76e04e6e143a84b02f8e4eaf392123354e78540b
Abhishek Kumar <abhishek.mrt22@gmail.com>
2022-04-22 12:04:50 +0530

    fix ping
    Signed-off-by: Abhishek Kumar <abhishek.mrt22@gmail.com>

--- systemvm/debian/opt/cloud/bin/cs/CsNetfilter.py
+++ systemvm/debian/opt/cloud/bin/cs/CsNetfilter.py
@@ -230,7 +230,7 @@ class CsNetfilters(object):
                 chain_policy = "%s policy %s;" % (chain_policy, action)
             CsHelper.execute("nft add chain %s %s %s '{ %s }'" % (address_family, table, chain, chain_policy))
             if chain_policy:
-                CsHelper.execute("nft add rule %s %s %s icmpv6 type { nd-neighbor-solicit, nd-router-advert, nd-neighbor-advert } accept" % (address_family, table, chain))
+                CsHelper.execute("nft add rule %s %s %s icmpv6 type { echo-request, echo-reply, nd-neighbor-solicit, nd-router-advert, nd-neighbor-advert } accept" % (address_family, table, chain))
 
     def apply_ip6_rules(self, rules, type):
         logging.debug("Add IPv6 rules: %s", rules)
