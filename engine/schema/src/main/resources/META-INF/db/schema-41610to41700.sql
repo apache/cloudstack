362488ee3d5e26b17dd9ce34e6542b413980112e
Nicolas Vazquez <nicovazquez90@gmail.com>
2022-05-06 13:04:52 -0300

    Bugfix: no support for XCPng 8.2.1 (#6362)
    * Initial support for XCPng 8.2.1
    
    * Refactor

--- engine/schema/src/main/resources/META-INF/db/schema-41610to41700.sql
+++ engine/schema/src/main/resources/META-INF/db/schema-41610to41700.sql
@@ -933,3 +933,42 @@ CREATE VIEW `cloud`.`event_view` AS
         `cloud`.`projects` ON projects.project_account_id = event.account_id
             LEFT JOIN
         `cloud`.`event` eve ON event.start_id = eve.id;
+
+-- Add XenServer 8.2.1 hypervisor capabilities
+INSERT IGNORE INTO `cloud`.`hypervisor_capabilities`(uuid, hypervisor_type, hypervisor_version, max_guests_limit, max_data_volumes_limit, max_hosts_per_cluster, storage_motion_supported) VALUES (UUID(), 'XenServer', '8.2.1', 1000, 253, 64, 1);
+
+-- Copy XenServer 8.2.0 hypervisor guest OS mappings to XenServer 8.2.1
+INSERT IGNORE INTO `cloud`.`guest_os_hypervisor` (uuid,hypervisor_type, hypervisor_version, guest_os_name, guest_os_id, created, is_user_defined) SELECT UUID(),'Xenserver', '8.2.1', guest_os_name, guest_os_id, utc_timestamp(), 0 FROM `cloud`.`guest_os_hypervisor` WHERE hypervisor_type='Xenserver' AND hypervisor_version='8.2.0';
+
+DROP PROCEDURE IF EXISTS `cloud`.`ADD_GUEST_OS_AND_HYPERVISOR_MAPPING`;
+CREATE PROCEDURE `cloud`.`ADD_GUEST_OS_AND_HYPERVISOR_MAPPING` (
+    IN guest_os_category_id bigint(20) unsigned,
+    IN guest_os_display_name VARCHAR(255),
+    IN guest_os_hypervisor_hypervisor_type VARCHAR(32),
+    IN guest_os_hypervisor_hypervisor_version VARCHAR(32),
+    IN guest_os_hypervisor_guest_os_name VARCHAR(255)
+        )
+BEGIN
+INSERT  INTO cloud.guest_os (uuid, category_id, display_name, created)
+SELECT 	UUID(), guest_os_category_id, guest_os_display_name, now()
+FROM    DUAL
+WHERE 	not exists( SELECT  1
+                     FROM    cloud.guest_os
+                     WHERE   cloud.guest_os.category_id = guest_os_category_id
+                       AND     cloud.guest_os.display_name = guest_os_display_name)
+
+;	INSERT  INTO cloud.guest_os_hypervisor (uuid, hypervisor_type, hypervisor_version, guest_os_name, guest_os_id, created)
+     SELECT 	UUID(), guest_os_hypervisor_hypervisor_type, guest_os_hypervisor_hypervisor_version, guest_os_hypervisor_guest_os_name, guest_os.id, now()
+     FROM 	cloud.guest_os
+     WHERE 	guest_os.category_id = guest_os_category_id
+       AND 	guest_os.display_name = guest_os_display_name
+       AND	NOT EXISTS (SELECT  1
+                          FROM    cloud.guest_os_hypervisor as hypervisor
+                          WHERE   hypervisor_type = guest_os_hypervisor_hypervisor_type
+                            AND     hypervisor_version = guest_os_hypervisor_hypervisor_version
+                            AND     hypervisor.guest_os_id = guest_os.id
+                            AND     hypervisor.guest_os_name = guest_os_hypervisor_guest_os_name)
+;END;
+
+CALL ADD_GUEST_OS_AND_HYPERVISOR_MAPPING (2, 'Debian GNU/Linux 11 (64-bit)', 'XenServer', '8.2.1', 'Debian Bullseye 11');
+CALL ADD_GUEST_OS_AND_HYPERVISOR_MAPPING (2, 'Debian GNU/Linux 11 (32-bit)', 'XenServer', '8.2.1', 'Debian Bullseye 11');
