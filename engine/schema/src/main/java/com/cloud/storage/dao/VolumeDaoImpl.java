8c3f17a1e7e8f374897bd88685439a6b4fa12327
Bryan Lima <bryan.lima@hotmail.com>
2022-05-11 16:15:27 -0300

    Change disk search var name

--- engine/schema/src/main/java/com/cloud/storage/dao/VolumeDaoImpl.java
+++ engine/schema/src/main/java/com/cloud/storage/dao/VolumeDaoImpl.java
@@ -61,7 +61,7 @@ public class VolumeDaoImpl extends GenericDaoBase<VolumeVO, Long> implements Vol
     protected final GenericSearchBuilder<VolumeVO, Long> ActiveTemplateSearch;
     protected final SearchBuilder<VolumeVO> InstanceStatesSearch;
     protected final SearchBuilder<VolumeVO> AllFieldsSearch;
-    protected final SearchBuilder<VolumeVO> DiskOfferingSearch;
+    protected final SearchBuilder<VolumeVO> diskOfferingSearch;
     protected final SearchBuilder<VolumeVO> RootDiskStateSearch;
     protected GenericSearchBuilder<VolumeVO, Long> CountByAccount;
     protected GenericSearchBuilder<VolumeVO, SumCount> primaryStorageSearch;
@@ -265,7 +265,7 @@ public class VolumeDaoImpl extends GenericDaoBase<VolumeVO, Long> implements Vol
 
     @Override
     public List<VolumeVO> findByDiskOfferingId(long diskOfferingId) {
-        SearchCriteria<VolumeVO> sc = DiskOfferingSearch.create();
+        SearchCriteria<VolumeVO> sc = diskOfferingSearch.create();
         sc.setParameters("diskOfferingId", diskOfferingId);
 
         return listBy(sc);
@@ -401,9 +401,9 @@ public class VolumeDaoImpl extends GenericDaoBase<VolumeVO, Long> implements Vol
         TemplateZoneSearch.and("zone", TemplateZoneSearch.entity().getDataCenterId(), Op.EQ);
         TemplateZoneSearch.done();
 
-        DiskOfferingSearch = createSearchBuilder();
-        DiskOfferingSearch.and("diskOfferingId", DiskOfferingSearch.entity().getDiskOfferingId(), Op.EQ);
-        DiskOfferingSearch.done();
+        diskOfferingSearch = createSearchBuilder();
+        diskOfferingSearch.and("diskOfferingId", diskOfferingSearch.entity().getDiskOfferingId(), Op.EQ);
+        diskOfferingSearch.done();
 
         TotalSizeByPoolSearch = createSearchBuilder(SumCount.class);
         TotalSizeByPoolSearch.select("sum", Func.SUM, TotalSizeByPoolSearch.entity().getSize());
