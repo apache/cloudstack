8b0553d31ab6a2e4f9672df69ee65998e6daec35
Wei Zhou <weizhou@apache.org>
2022-06-30 19:26:22 +0200

    AS: consider server state in get_haproxy_stats.sh

--- systemvm/debian/opt/cloud/bin/get_haproxy_stats.sh
+++ systemvm/debian/opt/cloud/bin/get_haproxy_stats.sh
@@ -24,6 +24,22 @@ poolName=$(echo $publicIp-$port | tr "." "_")
 # read "scur" from haproxy stats socket. The values are
 # pxname,svname,qcur,qmax,scur,smax,slim,stot,bin,bout,dreq,dresp,ereq,econ,eresp,wretr,wredis,status,weight,act,bck,chkfail,chkdown,lastchg,downtime,qlimit,pid,iid,sid,throttle,lbtot,tracked,type,rate,rate_lim,rate_max,check_status,check_code,check_duration,hrsp_1xx,hrsp_2xx,hrsp_3xx,hrsp_4xx,hrsp_5xx,hrsp_other,hanafail,req_rate,req_rate_max,req_tot,cli_abrt,srv_abrt,comp_in,comp_out,comp_byp,comp_rsp,lastsess,last_chk,last_agt,qtime,ctime,rtime,ttime,agent_status,agent_code,agent_duration,check_desc,agent_desc,check_rise,check_fall,check_health,agent_rise,agent_fall,agent_health,addr,cookie,mode,algo,conn_rate,conn_rate_max,conn_tot,intercepted,dcon,dses,wrew,connect,reuse,cache_lookups,cache_hits,srv_icur,src_ilim,qtime_max,ctime_max,rtime_max,ttime_max,eint,idle_conn_cur,safe_conn_cur,used_conn_cur,need_conn_est,
 
-current_session=$(echo "show stat" | socat stdio unix-connect:/run/haproxy/admin.sock | grep "^$poolName,FRONTEND" | cut -d "," -f5)
+current_sessions=$(echo "show stat" | socat stdio unix-connect:/run/haproxy/admin.sock | grep "^$poolName,$poolName" | cut -d "," -f5,18)
 
-echo $current_session
+sum=0
+count=0
+for session in $current_sessions;do
+  stat=$(echo $session |cut -d "," -f1)
+  state=$(echo $session |cut -d "," -f2)
+  if [ "$state" = "DOWN" ];then
+    sum=$((sum+stat))
+    count=$((count+1))
+  fi
+done
+
+average=0
+if [[ $count -gt 0 ]];then
+  average=$((sum/count))
+fi
+
+echo $average
