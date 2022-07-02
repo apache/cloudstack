eb446e52cafdda1e0ff58473112cbd6961677db6
Pritam Neog <neog.geek123@gmail.com>
2022-07-02 19:16:39 +0530

    fixed typo

--- plugins/user-authenticators/oauth2/src/main/java/org/apache/cloudstack/oauth2/LoginCallbackServlet.java
+++ plugins/user-authenticators/oauth2/src/main/java/org/apache/cloudstack/oauth2/LoginCallbackServlet.java
@@ -39,7 +39,7 @@ public class LoginCallbackServlet extends AbstractAuthorizationCodeCallbackServl
 
     @Override
     protected void onError(HttpServletRequest request, HttpServletResponse httpServletResponse,
-                           AuthorizationCodseResponseUrl errorResponse)
+                           AuthorizationCodeResponseUrl errorResponse)
             throws IOException {
         httpServletResponse.getWriter().print("Error in authorization");
     }
