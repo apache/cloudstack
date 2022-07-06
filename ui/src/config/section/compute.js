f1bce69b5dd3a59d22ee54483092b4866a1e5d46
Abhishek Kumar <abhishek.mrt22@gmail.com>
2022-07-06 09:26:31 +0530

    ui: allow instances to be filtered by group (#6495)
    Fixes #6428
    
    Signed-off-by: Abhishek Kumar <abhishek.mrt22@gmail.com>

--- ui/src/config/section/compute.js
+++ ui/src/config/section/compute.js
@@ -75,7 +75,7 @@ export default {
         }
         return fields
       },
-      searchFilters: ['name', 'zoneid', 'domainid', 'account', 'tags'],
+      searchFilters: ['name', 'zoneid', 'domainid', 'account', 'groupid', 'tags'],
       details: () => {
         var fields = ['displayname', 'name', 'id', 'state', 'ipaddress', 'ip6address', 'templatename', 'ostypename', 'serviceofferingname', 'isdynamicallyscalable', 'haenable', 'hypervisor', 'boottype', 'bootmode', 'account', 'domain', 'zonename']
         const listZoneHaveSGEnabled = store.getters.zones.filter(zone => zone.securitygroupsenabled === true)
