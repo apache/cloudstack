731a83babfb371b9ee9bb18af9bd33ade7f2c774
dahn <daan@onecht.net>
2022-07-15 10:01:35 +0200

    add global setting to allow parallel execution on vmware (#6413)
    * add global setting to allow parallel execution on vmware
    
    * cleanup setting distribution for vmware.create.full.clone
    
    * query setting in vmware guru
    
    * don´t touch other hypervisor's commands
    
    * guru hierarchy cleanup

--- server/src/main/java/com/cloud/capacity/CapacityManagerImpl.java
+++ server/src/main/java/com/cloud/capacity/CapacityManagerImpl.java
@@ -1256,6 +1256,6 @@ public class CapacityManagerImpl extends ManagerBase implements CapacityManager,
     @Override
     public ConfigKey<?>[] getConfigKeys() {
         return new ConfigKey<?>[] {CpuOverprovisioningFactor, MemOverprovisioningFactor, StorageCapacityDisableThreshold, StorageOverprovisioningFactor,
-            StorageAllocatedCapacityDisableThreshold, StorageOperationsExcludeCluster, VmwareCreateCloneFull, ImageStoreNFSVersion, SecondaryStorageCapacityThreshold};
+            StorageAllocatedCapacityDisableThreshold, StorageOperationsExcludeCluster, ImageStoreNFSVersion, SecondaryStorageCapacityThreshold};
     }
 }
