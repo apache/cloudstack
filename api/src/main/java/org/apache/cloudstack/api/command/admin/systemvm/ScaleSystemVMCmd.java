3bedddba4bf112cf2ccd6db73753387a21f59826
Ruben Bosch <8641284+Rubueno@users.noreply.github.com>
2022-07-20 12:41:07 +0200

    Update API documentation for resizing (#6556)
    The correct parameter is `cpuspeed` and not `cpu`. This led me to some
    confusion when resizing a VM attached to an offering allowing custom
    values.
    
    Co-authored-by: Ruben Bosch <ruben.bosch@cldin.eu>

--- api/src/main/java/org/apache/cloudstack/api/command/admin/systemvm/ScaleSystemVMCmd.java
+++ api/src/main/java/org/apache/cloudstack/api/command/admin/systemvm/ScaleSystemVMCmd.java
@@ -60,7 +60,7 @@ public class ScaleSystemVMCmd extends BaseAsyncCmd {
     @Parameter(name = ApiConstants.SERVICE_OFFERING_ID, type = CommandType.UUID, entityType = ServiceOfferingResponse.class, required = true, description = "the service offering ID to apply to the system vm")
     private Long serviceOfferingId;
 
-    @Parameter(name = ApiConstants.DETAILS, type = CommandType.MAP, description = "name value pairs of custom parameters for cpu, memory and cpunumber. example details[i].name=value")
+    @Parameter(name = ApiConstants.DETAILS, type = CommandType.MAP, description = "name value pairs of custom parameters for cpuspeed, memory and cpunumber. example details[i].name=value")
     private Map<String, String> details;
 
     /////////////////////////////////////////////////////
