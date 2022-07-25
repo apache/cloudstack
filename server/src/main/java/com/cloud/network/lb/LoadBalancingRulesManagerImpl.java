4ca3b87b58148358056c71b265a672de30b31d89
Wei Zhou <weizhou@apache.org>
2022-07-25 10:34:36 +0200

    AS: fix unable to create VM group in VPC networks

--- server/src/main/java/com/cloud/network/lb/LoadBalancingRulesManagerImpl.java
+++ server/src/main/java/com/cloud/network/lb/LoadBalancingRulesManagerImpl.java
@@ -1004,7 +1004,7 @@ public class LoadBalancingRulesManagerImpl<Type> extends ManagerBase implements 
             return false;
         }
         if (provider.get(0) == Provider.Netscaler || provider.get(0) == Provider.F5BigIp ||
-            provider.get(0) == Provider.VirtualRouter) {
+            provider.get(0) == Provider.VirtualRouter || provider.get(0) == Provider.VPCVirtualRouter) {
             return true;
         }
         return false;
