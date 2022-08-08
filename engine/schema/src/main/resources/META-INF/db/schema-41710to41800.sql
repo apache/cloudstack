caf553a54afa28df0a7d1e1d00ba0930ed26f921
João Jandre <48719461+JoaoJandre@users.noreply.github.com>
2022-08-08 05:48:21 -0300

    Add new parameter to createLoadBalancerRule API (#6460)
    * Add new parameter to createLoadBalancerRule API
    
    * address review
    
    Co-authored-by: João Paraquetti <joao@scclouds.com.br>

--- engine/schema/src/main/resources/META-INF/db/schema-41710to41800.sql
+++ engine/schema/src/main/resources/META-INF/db/schema-41710to41800.sql
@@ -23,3 +23,7 @@
 UPDATE `cloud`.`service_offering` so
 SET so.limit_cpu_use = 1
 WHERE so.default_use = 1 AND so.vm_type IN ('domainrouter', 'secondarystoragevm', 'consoleproxy', 'internalloadbalancervm', 'elasticloadbalancervm');
+
+-- Add cidr_list column to load_balancing_rules
+ALTER TABLE `cloud`.`load_balancing_rules`
+ADD cidr_list VARCHAR(4096);
