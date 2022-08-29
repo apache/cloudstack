fa0ad2d88f5d137ef73df1e3f81c4eb3a4dc6fc2
José Flauzino <jose.wilson.vf@gmail.com>
2022-08-29 08:42:03 -0300

    VM stats history visualization (#6401)
    * Add VM stats view feature
    
    * Add dynamic label to the submit button of the VM stats filtering modal
    
    * Improve CPU info message
    
    * Reduce identation
    
    * Remove missed line
    
    * Fix timestamp when 'Use local timezone' is selected
    
    * Fix bug when filtering for all data
    
    * Support for toggle 'Use local timezone' button
    
    Co-authored-by: joseflauzino <jose@scclouds.com.br>
    Co-authored-by: Daniel Augusto Veronezi Salvador <38945620+GutoVeronezi@users.noreply.github.com>

---
+++ ui/src/components/view/stats/ResourceStatsInfo.vue
@@ -0,0 +1,90 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements. See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership. The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License. You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+<template>
+  <div>
+    <div v-if="messages.length > 1">
+      <ul>
+        <li v-for="(msg, index) in messages" :key="index">
+          <div v-if="index === messages.length - 1">
+            {{ msg }}.
+          </div>
+          <div v-else>
+            {{ msg }};
+          </div>
+        </li>
+      </ul>
+    </div>
+    <p v-else-if="messages.length === 1">
+      {{ messages[0] }}.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ResourceStatsInfo',
+  props: {
+    resourceType: {
+      type: String,
+      default: null
+    }
+  },
+  data () {
+    return {
+      info: [
+        {
+          resourceType: 'CPU',
+          messageList: [
+            this.$t('message.cpu.usage.info.multi.cpu'),
+            this.$t('message.cpu.usage.info.cpu.cap')
+          ]
+        },
+        {
+          resourceType: 'MEM',
+          messageList: [
+            this.$t('message.memory.usage.info.negative.value'),
+            this.$t('message.memory.usage.info.hypervisor.additionals')
+          ]
+        },
+        {
+          resourceType: 'NET',
+          messageList: [
+            this.$t('message.network.usage.info.sum.of.vnics'),
+            this.$t('message.network.usage.info.data.points')
+          ]
+        },
+        {
+          resourceType: 'DISK',
+          messageList: [
+            this.$t('message.disk.usage.info.sum.of.disks'),
+            this.$t('message.disk.usage.info.data.points')
+          ]
+        }
+      ],
+      messages: []
+    }
+  },
+  mounted () {
+    for (const element of this.info) {
+      if (element.resourceType === this.resourceType) {
+        this.messages = element.messageList
+      }
+    }
+  }
+}
+</script>
