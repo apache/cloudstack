336ba6c23fd866d739960d1b48228c1f87f4ba05
Daan Hoogland <daan@onecht.net>
2022-09-08 17:31:05 +0200

    assertions/import

--- server/src/test/java/com/cloud/resourcelimit/CheckedReservationTest.java
+++ server/src/test/java/com/cloud/resourcelimit/CheckedReservationTest.java
@@ -33,7 +33,6 @@ import org.powermock.core.classloader.annotations.PrepareForTest;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
