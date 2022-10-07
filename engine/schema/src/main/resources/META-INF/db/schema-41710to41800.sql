278efc063e5cc713816fdbaa33279965efbffe24
Gabriel Ortiga Fernandes <gabriel.fernandes@scclouds.com.br>
2022-10-07 13:25:36 -0300

    fix description of `secstorage.encrypt.copy`

--- engine/schema/src/main/resources/META-INF/db/schema-41710to41800.sql
+++ engine/schema/src/main/resources/META-INF/db/schema-41710to41800.sql
@@ -575,3 +575,9 @@ FROM
             AND (`custom_speed`.`name` = 'CpuSpeed'))))
         LEFT JOIN `user_vm_details` `custom_ram_size` ON (((`custom_ram_size`.`vm_id` = `vm_instance`.`id`)
         AND (`custom_ram_size`.`name` = 'memory'))));
+
+-- Improve description of configuration `secstorage.encrypt.copy` #6810.
+UPDATE  cloud.configuration
+SET     description = "Use SSL method used to encrypt copy traffic between zones. Also ensures that the certificate assigned to the zone is used when
+generating links for external access."
+WHERE   name = 'secstorage.encrypt.copy';
