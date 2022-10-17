b1fb6e8c893085f8b7932b708b0e8a464f47a85d
Harikrishna Patnala <harikrishna.patnala@gmail.com>
2022-10-17 08:40:59 +0530

    Added Global/zone setting allow.diskOffering.change.during.scale.vm to allow or skip disk offering change upon scale VM operation.
    Added marvin tests to verify scale VM operation with user account and also to verify the new global/zone setting

--- test/integration/smoke/test_scale_vm.py
+++ test/integration/smoke/test_scale_vm.py
@@ -25,8 +25,10 @@ from marvin.lib.base import (Account,
                              Host,
                              VirtualMachine,
                              ServiceOffering,
+                             DiskOffering,
                              Template,
-                             Configurations)
+                             Configurations,
+                             Volume)
 from marvin.lib.common import (get_zone,
                                get_template,
                                get_domain)
@@ -54,7 +56,7 @@ class TestScaleVm(cloudstackTestCase):
             return
 
         # Get Zone, Domain and templates
-        domain = get_domain(cls.apiclient)
+        cls.domain = get_domain(cls.apiclient)
         cls.zone = get_zone(cls.apiclient, testClient.getZoneForTests())
         cls.services['mode'] = cls.zone.networktype
 
@@ -90,7 +92,7 @@ class TestScaleVm(cloudstackTestCase):
         cls.account = Account.create(
             cls.apiclient,
             cls.services["account"],
-            domainid=domain.id
+            domainid=cls.domain.id
         )
         cls._cleanup.append(cls.account)
 
@@ -170,6 +172,11 @@ class TestScaleVm(cloudstackTestCase):
             name="enable.dynamic.scale.vm",
             value="false"
         )
+        Configurations.update(
+            cls.apiclient,
+            name="allow.diskOffering.change.during.scale.vm",
+            value="false"
+        )
         super(TestScaleVm,cls).tearDownClass()
         return
 
@@ -177,6 +184,7 @@ class TestScaleVm(cloudstackTestCase):
         self.apiclient = self.testClient.getApiClient()
         self.dbclient = self.testClient.getDbConnection()
         self.cleanup = []
+        self.services["disk_offering"]["disksize"] = 8
 
         if self.unsupportedHypervisor:
             self.skipTest("Skipping test because unsupported hypervisor\
@@ -479,5 +487,356 @@ class TestScaleVm(cloudstackTestCase):
                 pass
         else:
             self.fail("Expected an exception to be thrown, failing")
+
+        return
+
+    @attr(hypervisor="xenserver")
+    @attr(tags=["advanced", "basic"], required_hardware="false")
+    def test_04_scale_vm_with_user_account(self):
+        """Test scale virtual machine under useraccount
+        """
+        # Validate the following
+        # Create a user Account and create a VM in it.
+        # Scale up the vm and see if it scales to the new svc offering
+
+        # Create an user account
+        self.userAccount = Account.create(
+            self.apiclient,
+            self.services["account"],
+            admin=False,
+            domainid=self.domain.id
+        )
+
+        self.cleanup.append(self.userAccount)
+        # Create user api client of the user account
+        self.userapiclient = self.testClient.getUserApiClient(
+            UserName=self.userAccount.name,
+            DomainName=self.userAccount.domain
+        )
+
+        # create a virtual machine
+        self.virtual_machine_in_user_account = VirtualMachine.create(
+            self.userapiclient,
+            self.services["small"],
+            accountid=self.userAccount.name,
+            domainid=self.userAccount.domainid,
+            serviceofferingid=self.small_offering.id,
+            mode=self.services["mode"]
+        )
+
+        if self.hypervisor.lower() == "vmware":
+            sshClient = self.virtual_machine_in_user_account.get_ssh_client()
+            result = str(
+                sshClient.execute("service vmware-tools status")).lower()
+            self.debug("and result is: %s" % result)
+            if not "running" in result:
+                self.skipTest("Skipping scale VM operation because\
+                    VMware tools are not installed on the VM")
+        if self.hypervisor.lower() != 'simulator':
+            list_vm_response = VirtualMachine.list(
+                self.apiclient,
+                id=self.virtual_machine_in_user_account.id
+            )[0]
+            hostid = list_vm_response.hostid
+            host = Host.list(
+                       self.apiclient,
+                       zoneid=self.zone.id,
+                       hostid=hostid,
+                       type='Routing'
+                   )[0]
+
+            try:
+                username = self.hostConfig["username"]
+                password = self.hostConfig["password"]
+                ssh_client = self.get_ssh_client(host.ipaddress, username, password)
+                res = ssh_client.execute("hostnamectl | grep 'Operating System' | cut -d':' -f2")
+            except Exception as e:
+                pass
+
+            if 'XenServer' in res[0]:
+                self.skipTest("Skipping test for XenServer as it's License does not allow scaling")
+
+        self.virtual_machine_in_user_account.update(
+            self.userapiclient,
+            isdynamicallyscalable='true')
+
+        self.debug("Scaling VM-ID: %s to service offering: %s and state %s" % (
+            self.virtual_machine_in_user_account.id,
+            self.big_offering.id,
+            self.virtual_machine_in_user_account.state
+        ))
+
+        cmd = scaleVirtualMachine.scaleVirtualMachineCmd()
+        cmd.serviceofferingid = self.big_offering.id
+        cmd.id = self.virtual_machine_in_user_account.id
+
+        try:
+            self.userapiclient.scaleVirtualMachine(cmd)
+        except Exception as e:
+            if "LicenceRestriction" in str(e):
+                self.skipTest("Your XenServer License does not allow scaling")
+            else:
+                self.fail("Scaling failed with the following exception: " + str(e))
+
+        list_vm_response = VirtualMachine.list(
+            self.userapiclient,
+            id=self.virtual_machine_in_user_account.id
+        )
+
+        vm_response = list_vm_response[0]
+
+        self.debug(
+            "Scaling VM-ID: %s from service offering: %s to new service\
+                    offering %s and the response says %s" %
+            (self.virtual_machine_in_user_account.id,
+             self.virtual_machine_in_user_account.serviceofferingid,
+             self.big_offering.id,
+             vm_response.serviceofferingid))
+        self.assertEqual(
+            vm_response.serviceofferingid,
+            self.big_offering.id,
+            "Check service offering of the VM"
+        )
+
+        self.assertEqual(
+            vm_response.state,
+            'Running',
+            "Check the state of VM"
+        )
+        return
+
+    @attr(hypervisor="xenserver")
+    @attr(tags=["advanced", "basic"], required_hardware="false")
+    def test_05_scale_vm_dont_allow_disk_offering_change(self):
+        """Test scale virtual machine with disk offering changes
+        """
+        # Validate the following two cases
+
+        # 1
+        # create serviceoffering1 with diskoffering1
+        # create serviceoffering2 with diskoffering2
+        # create a VM with serviceoffering1
+        # Scale up the vm to serviceoffering2
+        # Check disk offering of root volume to be diskoffering1 since setting allow.diskOffering.change.during.scale.vm is false
+
+        # 2
+        # create serviceoffering3 with diskoffering3
+        # update setting allow.diskOffering.change.during.scale.vm to true
+        # scale up the VM to serviceoffering3
+        # Check disk offering of root volume to be diskoffering3 since setting allow.diskOffering.change.during.scale.vm is true
+
+        self.disk_offering1 = DiskOffering.create(
+                                    self.apiclient,
+                                    self.services["disk_offering"],
+                                    )
+        self._cleanup.append(self.disk_offering1)
+        offering_data = {
+            'displaytext': 'ServiceOffering1WithDiskOffering1',
+            'cpuspeed': 500,
+            'cpunumber': 1,
+            'name': 'ServiceOffering1WithDiskOffering1',
+            'memory': 512,
+            'diskofferingid': self.disk_offering1.id
+        }
+
+        self.ServiceOffering1WithDiskOffering1 = ServiceOffering.create(
+            self.apiclient,
+            offering_data,
+        )
+        self._cleanup.append(self.ServiceOffering1WithDiskOffering1)
+
+        # create a virtual machine
+        self.virtual_machine_test = VirtualMachine.create(
+            self.apiclient,
+            self.services["small"],
+            accountid=self.account.name,
+            domainid=self.account.domainid,
+            serviceofferingid=self.ServiceOffering1WithDiskOffering1.id,
+            mode=self.services["mode"]
+        )
+
+        if self.hypervisor.lower() == "vmware":
+            sshClient = self.virtual_machine_test.get_ssh_client()
+            result = str(
+                sshClient.execute("service vmware-tools status")).lower()
+            self.debug("and result is: %s" % result)
+            if not "running" in result:
+                self.skipTest("Skipping scale VM operation because\
+                    VMware tools are not installed on the VM")
+        if self.hypervisor.lower() != 'simulator':
+            hostid = self.virtual_machine_test.hostid
+            host = Host.list(
+                       self.apiclient,
+                       zoneid=self.zone.id,
+                       hostid=hostid,
+                       type='Routing'
+                   )[0]
+
+            try:
+                username = self.hostConfig["username"]
+                password = self.hostConfig["password"]
+                ssh_client = self.get_ssh_client(host.ipaddress, username, password)
+                res = ssh_client.execute("hostnamectl | grep 'Operating System' | cut -d':' -f2")
+            except Exception as e:
+                pass
+
+            if 'XenServer' in res[0]:
+                self.skipTest("Skipping test for XenServer as it's License does not allow scaling")
+
+        self.virtual_machine_test.update(
+            self.apiclient,
+            isdynamicallyscalable='true')
+
+        self.disk_offering2 = DiskOffering.create(
+                                    self.apiclient,
+                                    self.services["disk_offering"],
+                                    )
+        self._cleanup.append(self.disk_offering2)
+        offering_data = {
+            'displaytext': 'ServiceOffering2WithDiskOffering2',
+            'cpuspeed': 1000,
+            'cpunumber': 2,
+            'name': 'ServiceOffering2WithDiskOffering2',
+            'memory': 1024,
+            'diskofferingid': self.disk_offering2.id
+        }
+
+        self.ServiceOffering2WithDiskOffering2 = ServiceOffering.create(
+            self.apiclient,
+            offering_data,
+        )
+        self._cleanup.append(self.ServiceOffering2WithDiskOffering2)
+
+        self.debug("Scaling VM-ID: %s to service offering: %s and state %s" % (
+            self.virtual_machine_test.id,
+            self.ServiceOffering2WithDiskOffering2.id,
+            self.virtual_machine_test.state
+        ))
+
+        cmd = scaleVirtualMachine.scaleVirtualMachineCmd()
+        cmd.serviceofferingid = self.ServiceOffering2WithDiskOffering2.id
+        cmd.id = self.virtual_machine_test.id
+
+        try:
+            self.apiclient.scaleVirtualMachine(cmd)
+        except Exception as e:
+            if "LicenceRestriction" in str(e):
+                self.skipTest("Your XenServer License does not allow scaling")
+            else:
+                self.fail("Scaling failed with the following exception: " + str(e))
+
+        list_vm_response = VirtualMachine.list(
+            self.apiclient,
+            id=self.virtual_machine_test.id
+        )
+
+        vm_response = list_vm_response[0]
+
+        self.debug(
+            "Scaling VM-ID: %s from service offering: %s to new service\
+                    offering %s and the response says %s" %
+            (self.virtual_machine_test.id,
+             self.virtual_machine_test.serviceofferingid,
+             self.ServiceOffering2WithDiskOffering2.id,
+             vm_response.serviceofferingid))
+
+        vm_response = list_vm_response[0]
+
+        self.assertEqual(
+            vm_response.serviceofferingid,
+            self.ServiceOffering2WithDiskOffering2.id,
+            "Check service offering of the VM"
+        )
+
+        volume_response = Volume.list(
+            self.apiclient,
+            virtualmachineid=self.virtual_machine_test.id,
+            listall=True
+        )[0]
+
+        self.assertEqual(
+            volume_response.diskofferingid,
+            self.disk_offering1.id,
+            "Check disk offering of the VM, this should not change since allow.diskOffering.change.during.scale.vm is false"
+        )
+
+        # Do same scale vm operation with allow.diskOffering.change.during.scale.vm value to true
+
+        self.disk_offering3 = DiskOffering.create(
+                                    self.apiclient,
+                                    self.services["disk_offering"],
+                                    )
+        self._cleanup.append(self.disk_offering3)
+        offering_data = {
+            'displaytext': 'ServiceOffering3WithDiskOffering3',
+            'cpuspeed': 1500,
+            'cpunumber': 2,
+            'name': 'ServiceOffering3WithDiskOffering3',
+            'memory': 1024,
+            'diskofferingid': self.disk_offering3.id
+        }
+
+        self.ServiceOffering3WithDiskOffering3 = ServiceOffering.create(
+            self.apiclient,
+            offering_data,
+        )
+        self._cleanup.append(self.ServiceOffering3WithDiskOffering3)
+
+        Configurations.update(
+            self.apiclient,
+            name="allow.diskOffering.change.during.scale.vm",
+            value="true"
+        )
+
+        self.debug("Scaling VM-ID: %s to service offering: %s and state %s" % (
+            self.virtual_machine_test.id,
+            self.ServiceOffering3WithDiskOffering3.id,
+            self.virtual_machine_test.state
+        ))
+
+        cmd = scaleVirtualMachine.scaleVirtualMachineCmd()
+        cmd.serviceofferingid = self.ServiceOffering3WithDiskOffering3.id
+        cmd.id = self.virtual_machine_test.id
+
+        try:
+            self.apiclient.scaleVirtualMachine(cmd)
+        except Exception as e:
+            if "LicenceRestriction" in str(e):
+                self.skipTest("Your XenServer License does not allow scaling")
+            else:
+                self.fail("Scaling failed with the following exception: " + str(e))
+
+        list_vm_response = VirtualMachine.list(
+            self.apiclient,
+            id=self.virtual_machine_test.id
+        )
+
+        vm_response = list_vm_response[0]
+
+        self.debug(
+            "Scaling VM-ID: %s from service offering: %s to new service\
+                    offering %s and the response says %s" %
+            (self.virtual_machine_test.id,
+             self.virtual_machine_test.serviceofferingid,
+             self.ServiceOffering3WithDiskOffering3.id,
+             vm_response.serviceofferingid))
+
+        self.assertEqual(
+            vm_response.serviceofferingid,
+            self.ServiceOffering3WithDiskOffering3.id,
+            "Check service offering of the VM"
+        )
+
+        volume_response = Volume.list(
+            self.apiclient,
+            virtualmachineid=self.virtual_machine_test.id,
+            listall=True
+        )[0]
+
+        self.assertEqual(
+            volume_response.diskofferingid,
+            self.disk_offering3.id,
+            "Check disk offering of the VM, this should change since allow.diskOffering.change.during.scale.vm is true"
+        )
 
         return
