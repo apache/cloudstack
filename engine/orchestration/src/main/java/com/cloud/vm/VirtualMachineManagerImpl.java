0c192f36095fc60b16b479c09812a05ce32d1535
Marcus Sorensen <marcus_sorensen@apple.com>
2022-11-04 01:15:57 -0600

    When VM start fails at host for admin, report error (#208)
    * When VM start fails at host for admin, report error
    
    Signed-off-by: Marcus Sorensen <mls@apple.com>
    
    * Report ResourceUnavailableExceptions that result in InsufficientCapacityException to admin
    
    * Update error message to be more straightforward
    
    Signed-off-by: Marcus Sorensen <mls@apple.com>
    Co-authored-by: Marcus Sorensen <mls@apple.com>
    (cherry picked from commit 8885d252f07a2dc0d46b3d1d4e09d919e2599787)
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>

--- engine/orchestration/src/main/java/com/cloud/vm/VirtualMachineManagerImpl.java
+++ engine/orchestration/src/main/java/com/cloud/vm/VirtualMachineManagerImpl.java
@@ -837,8 +837,15 @@ public class VirtualMachineManagerImpl extends ManagerBase implements VirtualMac
     public void start(final String vmUuid, final Map<VirtualMachineProfile.Param, Object> params, final DeploymentPlan planToDeploy, final DeploymentPlanner planner) {
         try {
             advanceStart(vmUuid, params, planToDeploy, planner);
-        } catch (ConcurrentOperationException | InsufficientCapacityException e) {
-            throw new CloudRuntimeException(String.format("Unable to start a VM [%s] due to [%s].", vmUuid, e.getMessage()), e).add(VirtualMachine.class, vmUuid);
+        } catch (final ConcurrentOperationException e) {
+            throw new CloudRuntimeException("Unable to start a VM due to concurrent operation", e).add(VirtualMachine.class, vmUuid);
+        } catch (final InsufficientCapacityException e) {
+            final CallContext cctxt = CallContext.current();
+            final Account account = cctxt.getCallingAccount();
+            if (account.getType() == Account.ACCOUNT_TYPE_ADMIN) {
+                throw new CloudRuntimeException("Unable to start a VM due to insufficient capacity: " + e.getMessage(), e).add(VirtualMachine.class, vmUuid);
+            }
+            throw new CloudRuntimeException("Unable to start a VM due to insufficient capacity", e).add(VirtualMachine.class, vmUuid);
         } catch (final ResourceUnavailableException e) {
             if (e.getScope() != null && e.getScope().equals(VirtualRouter.class)){
                 throw new CloudRuntimeException("Network is unavailable. Please contact administrator", e).add(VirtualMachine.class, vmUuid);
@@ -1133,6 +1140,7 @@ public class VirtualMachineManagerImpl extends ManagerBase implements VirtualMac
             resourceCountIncrement(owner.getAccountId(),new Long(offering.getCpu()), new Long(offering.getRamSize()));
         }
 
+        String adminError = null;
         boolean canRetry = true;
         ExcludeList avoids = null;
         try {
@@ -1221,6 +1229,10 @@ public class VirtualMachineManagerImpl extends ManagerBase implements VirtualMac
                         planChangedByVolume = false;
                         reuseVolume = false;
                         continue;
+                    }
+                    if (account.getType() == Account.ACCOUNT_TYPE_ADMIN && adminError != null) {
+                        String message = String.format("Unable to create a deployment for %s. Previous error: %s", vmProfile, adminError);
+                        throw new InsufficientServerCapacityException(message, DataCenter.class, plan.getDataCenterId(), areAffinityGroupsAssociated(vmProfile));
                     }
                     throw new InsufficientServerCapacityException("Unable to create a deployment for " + vmProfile, DataCenter.class, plan.getDataCenterId(),
                             areAffinityGroupsAssociated(vmProfile));
@@ -1386,7 +1398,9 @@ public class VirtualMachineManagerImpl extends ManagerBase implements VirtualMac
                             throw new ExecutionException("Unable to start  VM:"+vm.getUuid()+" due to error in finalizeStart, not retrying");
                         }
                     }
-                    s_logger.info("Unable to start VM on " + dest.getHost() + " due to " + (startAnswer == null ? " no start answer" : startAnswer.getDetails()));
+                    adminError = startAnswer == null ? " no start answer" : startAnswer.getDetails();
+                    s_logger.info("Unable to start VM on " + dest.getHost() + " due to " + adminError);
+
                     if (startAnswer != null && startAnswer.getContextParam("stopRetry") != null) {
                         break;
                     }
@@ -1399,7 +1413,8 @@ public class VirtualMachineManagerImpl extends ManagerBase implements VirtualMac
                     canRetry = false;
                     throw new AgentUnavailableException("Unable to start " + vm.getHostName(), destHostId, e);
                 } catch (final ResourceUnavailableException e) {
-                    s_logger.warn("Unable to contact resource.", e);
+                    s_logger.info("Unable to contact resource.", e);
+                    adminError = e.getMessage();
                     if (!avoids.add(e)) {
                         if (e.getScope() == Volume.class || e.getScope() == Nic.class) {
                             throw e;
@@ -1455,6 +1470,9 @@ public class VirtualMachineManagerImpl extends ManagerBase implements VirtualMac
         }
 
         if (startedVm == null) {
+            if (account.getType() == Account.ACCOUNT_TYPE_ADMIN && adminError != null) {
+                throw new CloudRuntimeException("Unable to start instance '" + vm.getHostName() + "' (" + vm.getUuid() + "): " + adminError);
+            }
             throw new CloudRuntimeException("Unable to start instance '" + vm.getHostName() + "' (" + vm.getUuid() + "), see management server log for details");
         }
     }
