b448f83deaa8f8bcf3d71532654b3d1581770f90
GutoVeronezi <daniel@scclouds.com.br>
2022-11-09 18:32:20 -0300

    Fix merge main

--- agent/src/main/java/com/cloud/agent/properties/AgentProperties.java
+++ agent/src/main/java/com/cloud/agent/properties/AgentProperties.java
@@ -715,14 +715,6 @@ public class AgentProperties{
     }
 
     /**
-     * Disables memory ballooning on VM guests for overcommit.<br>
-     * By default overcommit feature enables balloon and sets currentMemory to a minimum value.<br>
-     * Data type: Boolean.<br>
-     * Default value: <code>false</code>
-     */
-    public static final Property<Boolean> VM_MEMBALLOON_DISABLE = new Property<>("vm.memballoon.disable", false);
-
-    /**
      * The time interval (in seconds) at which the balloon driver will get memory stats updates. This is equivalent to Libvirt's <code>--period</code> parameter when using the dommemstat command.
      * Data type: Integer.<br>
      * Default value: <code>0</code>
