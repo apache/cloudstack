a4d37801437af045a6e6043d859ca8bd9973e717
Abhishek Kumar <abhishek.mrt22@gmail.com>
2022-12-07 20:10:50 +0800

    ui: fix incorrect hypervisor in deploy VM wizard (#6952)
    Fixes #6756
    
    Pass hypervisor parameter for deployVirtualMachine API only when ISO is selected and not for a template.
    
    Signed-off-by: Abhishek Kumar <abhishek.mrt22@gmail.com>

--- ui/src/views/compute/DeployVM.vue
+++ ui/src/views/compute/DeployVM.vue
@@ -1713,6 +1713,7 @@ export default {
         // step 2: select template/iso
         if (this.tabKey === 'templateid') {
           deployVmData.templateid = values.templateid
+          values.hypervisor = null
         } else {
           deployVmData.templateid = values.isoid
         }
