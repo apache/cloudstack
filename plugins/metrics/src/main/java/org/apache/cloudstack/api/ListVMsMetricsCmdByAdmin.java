b88d76ee88e2f6eeaffd3024065509dd53494172
Rohit Yadav <rohityadav89@gmail.com>
2022-12-08 12:05:59 +0530

    Update plugins/metrics/src/main/java/org/apache/cloudstack/api/ListVMsMetricsCmdByAdmin.java

--- plugins/metrics/src/main/java/org/apache/cloudstack/api/ListVMsMetricsCmdByAdmin.java
+++ plugins/metrics/src/main/java/org/apache/cloudstack/api/ListVMsMetricsCmdByAdmin.java
@@ -28,7 +28,6 @@ import org.apache.cloudstack.response.VmMetricsResponse;
 @APICommand(name = ListVMsMetricsCmd.APINAME, description = "Lists VM metrics", responseObject = VmMetricsResponse.class,
         requestHasSensitiveInfo = false, responseHasSensitiveInfo = false,  responseView = ResponseObject.ResponseView.Full,
         since = "4.9.3", authorized = {RoleType.Admin})
-@Deprecated(since = "4.17.0")
 public class ListVMsMetricsCmdByAdmin extends ListVMsMetricsCmd implements AdminCmd {
 
     /////////////////////////////////////////////////////
