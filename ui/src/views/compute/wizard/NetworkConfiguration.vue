29c9216ab5c2ad88a0a97b41ac62ce42f529be96
Abhishek Kumar <abhishek.mrt22@gmail.com>
2022-12-15 16:24:16 +0530

    ui: fix persist networks config in deploy vm form
    Fixes #6974
    
    Signed-off-by: Abhishek Kumar <abhishek.mrt22@gmail.com>

--- ui/src/views/compute/wizard/NetworkConfiguration.vue
+++ ui/src/views/compute/wizard/NetworkConfiguration.vue
@@ -168,12 +168,20 @@ export default {
       const rules = {}
 
       this.dataItems.forEach(record => {
-        rules['ipAddress' + record.id] = [{
+        const ipAddressKey = 'ipAddress' + record.id
+        const macAddressKey = 'macAddress' + record.id
+        rules[ipAddressKey] = [{
           validator: this.validatorIpAddress,
           cidr: record.cidr,
           networkType: record.type
         }]
-        rules['macAddress' + record.id] = [{ validator: this.validatorMacAddress }]
+        if (record.ipAddress) {
+          form[ipAddressKey] = record.ipAddress
+        }
+        rules[macAddressKey] = [{ validator: this.validatorMacAddress }]
+        if (record.macAddress) {
+          form[macAddressKey] = record.macAddress
+        }
       })
       this.form = reactive(form)
       this.rules = reactive(rules)
@@ -197,7 +205,7 @@ export default {
 
         this.networks.filter((item, index) => {
           if (item.key === key) {
-            this.networks[index].name = value
+            this.networks[index][name] = value
           }
         })
         this.$emit('update-network-config', this.networks)
