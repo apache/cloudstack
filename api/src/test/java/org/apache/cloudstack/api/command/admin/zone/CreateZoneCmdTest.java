b35da60699d6e5172c95df8077f120771a3130dd
Abhishek Kumar <abhishek.mrt22@gmail.com>
2022-12-16 14:07:00 +0530

    add more tests
    Signed-off-by: Abhishek Kumar <abhishek.mrt22@gmail.com>

--- api/src/test/java/org/apache/cloudstack/api/command/admin/zone/CreateZoneCmdTest.java
+++ api/src/test/java/org/apache/cloudstack/api/command/admin/zone/CreateZoneCmdTest.java
@@ -1,26 +1,19 @@
 package org.apache.cloudstack.api.command.admin.zone;
 
-import java.lang.reflect.Field;
-
 import org.junit.Assert;
 import org.junit.Test;
+import org.springframework.test.util.ReflectionTestUtils;
 
 public class CreateZoneCmdTest {
 
     @Test
     public void isEdge() {
         CreateZoneCmd createZoneCmd = new CreateZoneCmd();
-        try {
-            Field f = createZoneCmd.getClass().getDeclaredField("isEdge");
-            f.setAccessible(true);
-            f.set(createZoneCmd, null);
-            Assert.assertFalse("Null or no isedge param value for API should return false", createZoneCmd.isEdge());
-            f.set(createZoneCmd, false);
-            Assert.assertFalse("false value for isedge param value for API should return false", createZoneCmd.isEdge());
-            f.set(createZoneCmd, true);
-            Assert.assertTrue("true value for isedge param value for API should return true", createZoneCmd.isEdge());
-        } catch (NoSuchFieldException | SecurityException | IllegalAccessException e) {
-            Assert.fail();
-        }
+        ReflectionTestUtils.setField(createZoneCmd, "isEdge", null);
+        Assert.assertFalse("Null or no isedge param value for API should return false", createZoneCmd.isEdge());
+        ReflectionTestUtils.setField(createZoneCmd, "isEdge", false);
+        Assert.assertFalse("false value for isedge param value for API should return false", createZoneCmd.isEdge());
+        ReflectionTestUtils.setField(createZoneCmd, "isEdge", true);
+        Assert.assertTrue("true value for isedge param value for API should return true", createZoneCmd.isEdge());
     }
 }
