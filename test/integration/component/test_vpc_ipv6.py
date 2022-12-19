588287af17f083504c9ea9c58ae53de89deb7f19
Abhishek Kumar <abhishek.mrt22@gmail.com>
2022-12-19 15:49:56 +0530

    test: use normal user for createing networks, vpcs (#6988)
    When using admin=True in account creation with domain it creates a domain admin. It would be better to run tests as normal user.
    
    Signed-off-by: Abhishek Kumar <abhishek.mrt22@gmail.com>

--- test/integration/component/test_vpc_ipv6.py
+++ test/integration/component/test_vpc_ipv6.py
@@ -152,7 +152,7 @@ class TestIpv6Vpc(cloudstackTestCase):
             cls.account = Account.create(
                 cls.apiclient,
                 cls.services["account"],
-                admin=True,
+                admin=False,
                 domainid=cls.domain.id
             )
             cls._cleanup.append(cls.account)
