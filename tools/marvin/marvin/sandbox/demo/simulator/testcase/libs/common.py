d0902893b82e471b4e54f1203aa4e938e1c2a81f
John Bampton <jbampton@users.noreply.github.com>
2022-12-22 20:47:35 +1000

    Fix spelling (#7002)
    Co-authored-by: Stephan Krug <stekrug@icloud.com>

--- tools/marvin/marvin/sandbox/demo/simulator/testcase/libs/common.py
+++ tools/marvin/marvin/sandbox/demo/simulator/testcase/libs/common.py
@@ -109,8 +109,8 @@ def download_systemplates_sec_storage(server, services):
                                           server["password"]
                                           )
     except Exception as e:
-        raise Exception("SSH access failted for server with IP address: %s" %
-                                                            server["ipaddess"])
+        raise Exception("SSH access failed for server with IP address: %s" %
+                                                            server["ipaddress"])
     # Mount Secondary Storage on Management Server
     cmds = [
             "mkdir -p %s" % services["mnt_dir"],
