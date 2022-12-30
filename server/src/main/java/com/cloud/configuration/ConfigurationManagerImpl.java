acb3cf9935843c521caf77346367fd331d8e3a85
Sina Kashipazha <soreana@users.noreply.github.com>
2022-12-30 15:17:21 +0100

    Apply suggestions @stephankruggg code review.
    Co-authored-by: Stephan Krug <stekrug@icloud.com>

--- server/src/main/java/com/cloud/configuration/ConfigurationManagerImpl.java
+++ server/src/main/java/com/cloud/configuration/ConfigurationManagerImpl.java
@@ -777,7 +777,7 @@ public class ConfigurationManagerImpl extends ManagerBase implements Configurati
             case Network:
                 final NetworkVO network = _networkDao.findById(resourceId);
                 if (network == null) {
-                    throw new InvalidParameterValueException("unable to find network by id " + resourceId);
+                    throw new InvalidParameterValueException(String.format("Unable to find network by ID [%s].", resourceId));
                 }
                 NetworkDetailVO networkDetailVO = _networkDetailsDao.findDetail(resourceId, name);
                 if (networkDetailVO == null) {
@@ -1179,7 +1179,7 @@ public class ConfigurationManagerImpl extends ManagerBase implements Configurati
             case Network:
                 final NetworkVO network = _networkDao.findById(id);
                 if (network == null) {
-                    throw new InvalidParameterValueException("unable to find network by id " + id);
+                    throw new InvalidParameterValueException(String.format("Unable to find network by ID [%s].", id));
                 }
 
                 NetworkDetailVO networkDetailVO = _networkDetailsDao.findDetail(id, name);
