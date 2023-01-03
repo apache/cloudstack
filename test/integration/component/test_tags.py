34a6afb4765ca060e41ef9a097add6e84a9944cc
Daan Hoogland <daan@onecht.net>
2023-01-03 14:59:55 +0100

    import

--- test/integration/component/test_tags.py
+++ test/integration/component/test_tags.py
@@ -19,6 +19,7 @@
 # Import Local Modules
 from nose.plugins.attrib import attr
 from marvin.cloudstackTestCase import cloudstackTestCase
+import unittest
 from marvin.lib.utils import cleanup_resources, validateList
 from marvin.lib.base import (Tag,
                              Account,
