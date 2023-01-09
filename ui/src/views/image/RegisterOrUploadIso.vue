0f9c9ce93958aceb73a8f1b9a2778e6fadf6ab13
Abhishek Kumar <abhishek.mrt22@gmail.com>
2023-01-09 13:44:45 +0530

    restrict iso and k8s cluster
    Signed-off-by: Abhishek Kumar <abhishek.mrt22@gmail.com>

--- ui/src/views/image/RegisterOrUploadIso.vue
+++ ui/src/views/image/RegisterOrUploadIso.vue
@@ -283,9 +283,7 @@ export default {
         const listZones = json.listzonesresponse.zone
         if (listZones) {
           this.zones = this.zones.concat(listZones)
-          if (this.currentForm === 'Upload') {
-            this.zones = this.zones.filter(zone => zone.type !== 'Edge')
-          }
+          this.zones = this.zones.filter(zone => zone.type !== 'Edge')
         }
       }).finally(() => {
         this.zoneLoading = false
