c352017bf9254cbbdb75bcef6023c890cd46a672
Wei Zhou <weizhou@apache.org>
2023-03-24 09:07:26 +0100

    UI: fix default network is not passed to deployvm API
    This line is changed in #5080, but reverted in #5151

--- ui/src/views/compute/DeployVM.vue
+++ ui/src/views/compute/DeployVM.vue
@@ -1634,7 +1634,7 @@ export default {
       this.form.networkids = ids
     },
     updateDefaultNetworks (id) {
-      this.defaultNetwork = id
+      this.defaultnetworkid = id
       this.form.defaultnetworkid = id
     },
     updateNetworkConfig (networks) {
