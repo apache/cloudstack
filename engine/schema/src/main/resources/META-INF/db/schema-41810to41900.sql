7d803199e8ad3a5aa0ad8a91c5e4e29a80a8b311
Rahul Agarwal <rahulbcn27@gmail.com>
2023-03-29 09:48:04 +0530

    Schedule Table added to 4.19 DB upgrade

--- engine/schema/src/main/resources/META-INF/db/schema-41810to41900.sql
+++ engine/schema/src/main/resources/META-INF/db/schema-41810to41900.sql
@@ -19,3 +19,22 @@
 -- Schema upgrade from 4.18.1.0 to 4.19.0.0
 --;
 
+--- Create table for Virtual Machine Schedule Entity
+
+CREATE TABLE IF NOT EXISTS `cloud`.`vm_schedule` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `uuid` varchar(40) UNIQUE COMMENT 'UUID for the VM Schedule',
+  `description` varchar(255) COMMENT 'description of VM Schedule',
+  `action` varchar(40) NOT NULL COMMENT 'action Scheduled on VM',
+  `schedule_type` varchar(40) NOT NULL COMMENT 'interval type of VM',
+  `schedule` varchar(255) NOT NULL COMMENT 'schedule of VM',
+  `scheduled_timestamp` DATE  COMMENT 'timestamp of VM Schedule',
+  `timezone` varchar(255)  COMMENT 'timezone of VM',
+  `state` varchar(40)  COMMENT 'state of VM',
+  `tag` varchar(255)  COMMENT 'Tag Value of VM Schedule',
+  `async_job_id` bigint(20) unsigned COMMENT 'async job id',
+  `vm_id` bigint(20) unsigned NOT NULL COMMENT 'virtual machine id',
+  INDEX(`vm_id`),
+  PRIMARY KEY (`id`),
+  KEY (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
