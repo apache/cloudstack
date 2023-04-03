63baf1ec9efb57c1d1765cc6b2e71c0adbdc521a
Abhishek Kumar <abhishek.mrt22@gmail.com>
2023-04-03 16:18:55 +0530

    ui: allow creating account networkdomain
    Fixes #7227
    
    Signed-off-by: Abhishek Kumar <abhishek.mrt22@gmail.com>

--- ui/src/views/AutogenView.vue
+++ ui/src/views/AutogenView.vue
@@ -1416,13 +1416,13 @@ export default {
               continue
             }
             if (input === undefined || input === null ||
-              (input === '' && !['updateStoragePool', 'updateHost', 'updatePhysicalNetwork', 'updateDiskOffering', 'updateNetworkOffering', 'updateServiceOffering'].includes(action.api))) {
+              (input === '' && !['updateStoragePool', 'updateHost', 'updatePhysicalNetwork', 'updateDiskOffering', 'updateNetworkOffering', 'updateServiceOffering', 'updateAccount'].includes(action.api))) {
               if (param.type === 'boolean') {
                 params[key] = false
               }
               break
             }
-            if (input === '' && !['tags', 'hosttags', 'storagetags'].includes(key)) {
+            if (input === '' && !['tags', 'hosttags', 'storagetags', 'networkdomain'].includes(key)) {
               break
             }
             if (action.mapping && key in action.mapping && action.mapping[key].options) {
