016c5508f5065d7f7e2f20b3e0444d0d415e6ea4
dahn <daan.hoogland@gmail.com>
2023-07-13 13:22:36 +0200

    Update server/src/main/java/com/cloud/network/NetworkModelImpl.java

--- server/src/main/java/com/cloud/network/NetworkModelImpl.java
+++ server/src/main/java/com/cloud/network/NetworkModelImpl.java
@@ -600,7 +600,7 @@ public class NetworkModelImpl extends ManagerBase implements NetworkModel, Confi
             if (mac == null) {
                 throw new InsufficientAddressCapacityException("Unable to create another mac address", Network.class, networkId);
             }
-        } while(isMACUnique(mac));
+        } while(! isMACUnique(mac));
         return mac;
     }
 
