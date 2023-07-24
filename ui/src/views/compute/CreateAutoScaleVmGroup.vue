8c5b19b8621563ba8a2b645325c16c07dd11fb84
Wei Zhou <weizhou@apache.org>
2023-07-24 11:12:12 +0200

    CreateAutoScaleVmGroup.vue: Remove unsupported params listall (6686)

--- ui/src/views/compute/CreateAutoScaleVmGroup.vue
+++ ui/src/views/compute/CreateAutoScaleVmGroup.vue
@@ -2546,7 +2546,7 @@ export default {
       return new Promise((resolve) => {
         this.loading.zones = true
         const param = this.params.zones
-        const args = { listall: true, showicon: true }
+        const args = { showicon: true }
         if (zoneId) args.id = zoneId
         api(param.list, args).then(json => {
           const zoneResponse = (json.listzonesresponse.zone || []).filter(item => item.securitygroupsenabled === false)
@@ -2578,7 +2578,7 @@ export default {
       param.loading = true
       param.opts = []
       const options = param.options || {}
-      if (!('listall' in options)) {
+      if (!('listall' in options) && !['zones', 'pods', 'clusters', 'hosts', 'dynamicScalingVmConfig', 'hypervisors'].includes(name)) {
         options.listall = true
       }
       api(param.list, options).then((response) => {
