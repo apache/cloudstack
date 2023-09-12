41882e92fe14b58037f206beba19a9f75adf9ac1
Rohit Yadav <rohit.yadav@shapeblue.com>
2023-09-12 23:06:22 +0530

    first iteration for admin dashboard
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>

--- ui/src/views/dashboard/CapacityDashboard.vue
+++ ui/src/views/dashboard/CapacityDashboard.vue
@@ -16,8 +16,8 @@
 // under the License.
 
 <template>
-  <a-row class="capacity-dashboard" :gutter="12">
-    <a-col :xl="18">
+  <a-row class="capacity-dashboard" :gutter="[12,12]">
+    <a-col :span="24">
       <div class="capacity-dashboard-wrapper">
         <div class="capacity-dashboard-select">
           <a-select
@@ -41,90 +41,231 @@
         <div class="capacity-dashboard-button">
           <a-button
             shape="round"
-            @click="() => { listCapacity(zoneSelected, true); listEvents() }">
+            @click="() => { listCapacity(zoneSelected, true); listHosts(zoneSelected); listInstances(zoneSelected); listAlerts(); listEvents(); }">
             {{ $t('label.fetch.latest') }}
           </a-button>
         </div>
       </div>
-      <a-row :gutter="12">
-        <a-col
-          :xs="12"
-          :sm="8"
-          :md="6"
-          :style="{ marginBottom: '12px' }"
-          v-for="stat in stats"
-          :key="stat.type">
-          <chart-card :loading="loading">
-            <router-link :to="{ path: '/zone/' + zoneSelected.id }">
-              <div class="capacity-dashboard-chart-card-inner">
-                <h3>{{ $t(ts[stat.name]) }}</h3>
-                <a-progress
-                  type="dashboard"
-                  :status="getStatus(parseFloat(stat.percentused))"
-                  :percent="parseFloat(stat.percentused)"
-                  :format="percent => `${parseFloat(stat.percentused).toFixed(2)}%`"
-                  :strokeColor="getStrokeColour(parseFloat(stat.percentused))"
-                  :width="100" />
-              </div>
-            </router-link>
-            <template #footer>
-              <div class="center">{{ displayData(stat.name, stat.capacityused) }} / {{ displayData(stat.name, stat.capacitytotal) }}</div>
-            </template>
-          </chart-card>
-        </a-col>
-      </a-row>
     </a-col>
-
-    <a-col :xl="6" class="dashboard-event">
+    <a-col :span="8" class="dashboard-card">
       <chart-card :loading="loading">
-        <div style="text-align: center">
-          <a-tooltip placement="bottom" class="capacity-dashboard-button-wrapper">
-            <template #title>
-              {{ $t('label.view') + ' ' + $t('label.host.alerts') }}
-            </template>
-            <a-button type="primary" danger shape="circle">
-              <router-link :to="{ name: 'host', query: {'state': 'Alert'} }">
-                <desktop-outlined class="capacity-dashboard-button-icon" />
-              </router-link>
-            </a-button>
-          </a-tooltip>
-          <a-tooltip placement="bottom" class="capacity-dashboard-button-wrapper">
-            <template #title>
-              {{ $t('label.view') + ' ' + $t('label.alerts') }}
-            </template>
-            <a-button shape="circle">
-              <router-link :to="{ name: 'alert' }">
-                <flag-outlined class="capacity-dashboard-button-icon" />
-              </router-link>
-            </a-button>
-          </a-tooltip>
-          <a-tooltip placement="bottom" class="capacity-dashboard-button-wrapper">
-            <template #title>
-              {{ $t('label.view') + ' ' + $t('label.events') }}
-            </template>
-            <a-button shape="circle">
-              <router-link :to="{ name: 'event' }">
-                <schedule-outlined class="capacity-dashboard-button-icon" />
-              </router-link>
-            </a-button>
-          </a-tooltip>
+        <div>
+          <div v-for="ctype in ['MEMORY', 'CPU', 'CPU_CORE', 'GPU']" :key="ctype" >
+            <div v-if="statsMap[ctype]?.capacitytotal > -1">
+              <div>
+                <strong>{{ $t(ts[ctype]) }}</strong>
+              </div>
+              <a-progress
+              status="active"
+              :percent="parseFloat(statsMap[ctype]?.percentused)"
+              stroke-color="#52c41a"
+              size="small"
+              style="width:95%; float: left"
+              />
+              <br/>
+              <div style="text-align: center">
+                {{ displayData(ctype, statsMap[ctype]?.capacityused) }} {{ $t('label.allocated') }} | {{ displayData(ctype, statsMap[ctype]?.capacitytotal) }} {{ $t('label.total') }}
+              </div>
+            </div>
+          </div>
         </div>
         <template #footer>
-          <div class="capacity-dashboard-footer">
+          <div class="center">
+            <h3>{{ $t('label.compute') }}</h3>
+          </div>
+        </template>
+      </chart-card>
+    </a-col>
+    <a-col :span="8" class="dashboard-card">
+      <chart-card :loading="loading">
+        <div>
+          <div v-for="ctype in ['STORAGE', 'STORAGE_ALLOCATED', 'LOCAL_STORAGE', 'SECONDARY_STORAGE']" :key="ctype" >
+            <div v-if="statsMap[ctype]?.capacitytotal > 0">
+              <div>
+                <strong>{{ $t(ts[ctype]) }}</strong>
+              </div>
+              <a-progress
+              status="active"
+              :percent="parseFloat(statsMap[ctype]?.percentused)"
+              stroke-color="#52c41a"
+              size="small"
+              style="width:95%; float: left"
+              />
+              <br/>
+              <div style="text-align: center">
+                {{ displayData(ctype, statsMap[ctype]?.capacityused) }} {{ $t('label.allocated') }} | {{ displayData(ctype, statsMap[ctype]?.capacitytotal) }} {{ $t('label.total') }}
+              </div>
+            </div>
+          </div>
+        </div>
+        <template #footer>
+          <div class="center">
+            <h3>{{ $t('label.storage') }}</h3>
+          </div>
+        </template>
+      </chart-card>
+    </a-col>
+    <a-col :span="8" class="dashboard-card">
+      <chart-card :loading="loading">
+        <div>
+          <div v-for="ctype in ['VLAN', 'VIRTUAL_NETWORK_PUBLIC_IP', 'VIRTUAL_NETWORK_IPV6_SUBNET', 'DIRECT_ATTACHED_PUBLIC_IP', 'PRIVATE_IP']" :key="ctype" >
+            <div v-if="statsMap[ctype]?.capacitytotal > 0">
+              <div>
+                <strong>{{ $t(ts[ctype]) }}</strong>
+              </div>
+              <a-progress
+              status="active"
+              :percent="parseFloat(statsMap[ctype]?.percentused)"
+              stroke-color="#52c41a"
+              size="small"
+              style="width:95%; float: left"
+              />
+              <br/>
+              <div style="text-align: center">
+                {{ displayData(ctype, statsMap[ctype]?.capacityused) }} {{ $t('label.allocated') }} | {{ displayData(ctype, statsMap[ctype]?.capacitytotal) }} {{ $t('label.total') }}
+              </div>
+            </div>
+          </div>
+        </div>
+        <template #footer>
+          <div class="center">
+            <h3>{{ $t('label.network') }}</h3>
+          </div>
+        </template>
+      </chart-card>
+    </a-col>
+    <a-col :span="8" class="dashboard-card">
+      <chart-card :loading="loading">
+        <a-row>
+          <a-col :span="12">
+            <router-link :to="{ path: '/host', query: { zoneid: zoneSelected.id, state: 'up' } }">
+              <a-statistic :title="$t('label.up')" :value="hosts.up">
+                <template #prefix>
+                  <status class="status" text="Up"/>
+                </template>
+              </a-statistic>
+            </router-link>
+          </a-col>
+          <a-col :span="12">
+            <router-link :to="{ path: '/host', query: { zoneid: zoneSelected.id, state: 'down' } }">
+              <a-statistic :title="$t('label.down')" :value="hosts.alert">
+                <template #prefix>
+                  <status class="status" text="Down"/>
+                </template>
+              </a-statistic>
+            </router-link>
+          </a-col>
+        </a-row>
+        <a-row>
+          <a-col :span="12">
+            <router-link :to="{ path: '/host', query: { zoneid: zoneSelected.id, state: 'alert' } }">
+              <a-statistic :title="$t('label.alert')" :value="hosts.alert">
+                <template #prefix>
+                  <status class="status" text="Alert"/>
+                </template>
+              </a-statistic>
+            </router-link>
+          </a-col>
+          <a-col :span="12">
+            <router-link :to="{ path: '/host', query: { zoneid: zoneSelected.id, resourcestate: 'maintenance' } }">
+              <a-statistic :title="$t('label.maintenance')" :value="hosts.maintenance">
+                <template #prefix>
+                  <status class="status" text="Maintenance"/>
+                </template>
+              </a-statistic>
+            </router-link>
+          </a-col>
+        </a-row>
+        <div>
+        </div>
+        <template #footer>
+          <div class="center">
+            <h3>
+              {{ $t('label.hosts') }} |
+              <router-link :to="{ path: '/host', query: { zoneid: zoneSelected.id } }">
+                {{ hosts.total }}
+              </router-link>
+              {{ $t('label.total') }}
+            </h3>
+          </div>
+        </template>
+      </chart-card>
+      <chart-card :loading="loading" style="margin-top: 12px">
+        <a-row>
+          <a-col :span="12">
+            <router-link :to="{ path: '/vm', query: { zoneid: zoneSelected.id, projectid: '-1', state: 'running' } }">
+              <a-statistic :title="$t('label.running')" :value="instances.running">
+                <template #prefix>
+                  <status class="status" text="Running"/>
+                </template>
+              </a-statistic>
+            </router-link>
+          </a-col>
+          <a-col :span="12">
+            <router-link :to="{ path: '/vm', query: { zoneid: zoneSelected.id, projectid: '-1', state: 'stopped' } }">
+              <a-statistic :title="$t('label.stopped')" :value="instances.stopped">
+                <template #prefix>
+                  <status class="status" text="Stopped"/>
+                </template>
+              </a-statistic>
+            </router-link>
+          </a-col>
+        </a-row>
+        <template #footer>
+          <div class="center">
+            <h3>
+              {{ $t('label.instances') }} |
+              <router-link :to="{ path: '/vm', query: { zoneid: zoneSelected.id, projectid: '-1' } }">
+                {{ instances.total }}
+              </router-link>
+              {{ $t('label.total') }}
+            </h3>
+          </div>
+        </template>
+      </chart-card>
+    </a-col>
+    <a-col :xl="16" class="dashboard-event">
+      <a-card :loading="loading" :border="false">
+        <a-tabs v-model:activeKey="tabKey">
+          <a-tab-pane key="alerts" :tab="$t('label.alerts')">
+            <a-timeline>
+              <a-timeline-item
+                v-for="alert in alerts"
+                :key="alert.id"
+                color="red">
+                <span :style="{ color: '#999' }"><small>{{ $toLocaleDate(alert.sent) }}</small></span>&nbsp;
+                <span :style="{ color: '#666' }"><small><router-link :to="{ path: '/alert/' + alert.id }">{{ alert.name }}</router-link></small></span><br/>
+                <span :style="{ color: '#aaa' }">{{ alert.description }}</span>
+              </a-timeline-item>
+            </a-timeline>
+            <router-link :to="{ path: '/alert' }">
+              <a-button>
+                {{ $t('label.view') }} {{ $t('label.alerts') }}
+              </a-button>
+            </router-link>
+          </a-tab-pane>
+          <a-tab-pane key="event" :tab="$t('label.events')" force-render>
             <a-timeline>
               <a-timeline-item
                 v-for="event in events"
                 :key="event.id"
                 :color="getEventColour(event)">
-                <span :style="{ color: '#999' }"><small>{{ $toLocaleDate(event.created) }}</small></span><br/>
+                <span :style="{ color: '#999' }"><small>{{ $toLocaleDate(event.created) }}</small></span>&nbsp;
                 <span :style="{ color: '#666' }"><small><router-link :to="{ path: '/event/' + event.id }">{{ event.type }}</router-link></small></span><br/>
-                <resource-label :resourceType="event.resourcetype" :resourceId="event.resourceid" :resourceName="event.resourcename" />
+                <span>
+                  <resource-label :resourceType="event.resourcetype" :resourceId="event.resourceid" :resourceName="event.resourcename" />
+                </span>
                 <span :style="{ color: '#aaa' }">({{ event.username }}) {{ event.description }}</span>
               </a-timeline-item>
             </a-timeline>
-          </div>
-        </template>
-      </chart-card>
+            <router-link :to="{ path: '/event' }">
+              <a-button>
+                {{ $t('label.view') }} {{ $t('label.events') }}
+              </a-button>
+            </router-link>
+          </a-tab-pane>
+        </a-tabs>
+      </a-card>
     </a-col>
   </a-row>
 </template>
@@ -135,21 +276,38 @@ import { api } from '@/api'
 import ChartCard from '@/components/widgets/ChartCard'
 import ResourceIcon from '@/components/view/ResourceIcon'
 import ResourceLabel from '@/components/widgets/ResourceLabel'
+import Status from '@/components/widgets/Status'
 
 export default {
   name: 'CapacityDashboard',
   components: {
     ChartCard,
     ResourceIcon,
-    ResourceLabel
+    ResourceLabel,
+    Status
   },
   data () {
     return {
       loading: true,
+      tabKey: 'alerts',
+      alerts: [],
       events: [],
       zones: [],
       zoneSelected: {},
       stats: [],
+      statsMap: {},
+      hosts: {
+        total: 0,
+        up: 0,
+        alert: 0,
+        down: 0,
+        maintenance: 0
+      },
+      instances: {
+        total: 0,
+        running: 0,
+        stopped: 0
+      },
       ts: {
         CPU: 'label.cpu',
         CPU_CORE: 'label.cpunumber',
@@ -196,12 +354,6 @@ export default {
       }
       return 'normal'
     },
-    getStrokeColour (value) {
-      if (value >= 80) {
-        return this.$config.theme['@graph-exception-color'] || 'red'
-      }
-      return this.$config.theme['@graph-normal-color'] || 'primary'
-    },
     displayData (dataType, value) {
       switch (dataType) {
         case 'CPU':
@@ -224,6 +376,7 @@ export default {
     },
     fetchData () {
       this.listZones()
+      this.listAlerts()
       this.listEvents()
     },
     listCapacity (zone, latest = false) {
@@ -238,12 +391,68 @@ export default {
         if (json && json.listcapacityresponse && json.listcapacityresponse.capacity) {
           this.stats = json.listcapacityresponse.capacity
         }
+        for (const stat of this.stats) {
+          this.statsMap[stat.name] = stat
+        }
+      })
+    },
+    listHosts (zone) {
+      this.loading = true
+      api('listHosts', { zoneid: zone.id, listall: true, details: 'min', type: 'routing' }).then(json => {
+        this.loading = false
+        this.hosts.total = json?.listhostsresponse?.count
+      })
+      api('listHosts', { zoneid: zone.id, listall: true, details: 'min', type: 'routing', state: 'up' }).then(json => {
+        this.loading = false
+        this.hosts.up = json?.listhostsresponse?.count
+      })
+      api('listHosts', { zoneid: zone.id, listall: true, details: 'min', type: 'routing', state: 'alert' }).then(json => {
+        this.loading = false
+        this.hosts.alert = json?.listhostsresponse?.count
+      })
+      api('listHosts', { zoneid: zone.id, listall: true, details: 'min', type: 'routing', state: 'down' }).then(json => {
+        this.loading = false
+        this.hosts.down = json?.listhostsresponse?.count
+      })
+      api('listHosts', { zoneid: zone.id, listall: true, details: 'min', type: 'routing', resourcestate: 'maintenance' }).then(json => {
+        this.loading = false
+        this.hosts.maintenance = json?.listhostsresponse?.count
+      })
+    },
+    listInstances (zone) {
+      this.loading = true
+      api('listVirtualMachines', { zoneid: zone.id, listall: true, projectid: '-1', details: 'min' }).then(json => {
+        this.loading = false
+        this.instances.total = json?.listvirtualmachinesresponse?.count
+      })
+      api('listVirtualMachines', { zoneid: zone.id, listall: true, projectid: '-1', details: 'min', state: 'running' }).then(json => {
+        this.loading = false
+        this.instances.running = json?.listvirtualmachinesresponse?.count
+      })
+      api('listVirtualMachines', { zoneid: zone.id, listall: true, projectid: '-1', details: 'min', state: 'stopped' }).then(json => {
+        this.loading = false
+        this.instances.stopped = json?.listvirtualmachinesresponse?.count
+      })
+    },
+    listAlerts () {
+      const params = {
+        page: 1,
+        pagesize: 3,
+        listall: true
+      }
+      this.loading = true
+      api('listAlerts', params).then(json => {
+        this.alerts = []
+        this.loading = false
+        if (json && json.listalertsresponse && json.listalertsresponse.alert) {
+          this.alerts = json.listalertsresponse.alert
+        }
       })
     },
     listEvents () {
       const params = {
         page: 1,
-        pagesize: 6,
+        pagesize: 3,
         listall: true
       }
       this.loading = true
@@ -271,6 +480,8 @@ export default {
           if (this.zones.length > 0) {
             this.zoneSelected = this.zones[0]
             this.listCapacity(this.zones[0])
+            this.listHosts(this.zones[0])
+            this.listInstances(this.zones[0])
           }
         }
       })
@@ -278,6 +489,8 @@ export default {
     changeZone (index) {
       this.zoneSelected = this.zones[index]
       this.listCapacity(this.zoneSelected)
+      this.listHosts(this.zoneSelected)
+      this.listInstances(this.zoneSelected)
     },
     filterZone (input, option) {
       return option.label.toLowerCase().indexOf(input.toLowerCase()) >= 0
@@ -290,7 +503,6 @@ export default {
 .capacity-dashboard {
   &-wrapper {
     display: flex;
-    margin-bottom: 12px;
   }
 
   &-chart-card-inner {
@@ -328,12 +540,16 @@ export default {
   }
 }
 
+.dashboard-card {
+  width: 100%;
+}
+
 .center {
   display: block;
   text-align: center;
 }
 
-@media (max-width: 1200px) {
+@media (max-width: 1200) {
   .dashboard-event {
     width: 100%;
   }
