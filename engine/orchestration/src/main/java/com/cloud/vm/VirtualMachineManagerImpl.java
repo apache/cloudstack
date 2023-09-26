fe50018fba726b293e0910997dd060f5ed4e209d
Marcus Sorensen <marcus_sorensen@apple.com>
2023-09-26 03:21:11 -0600

    Allow configkey to set 'cloud-name' cloud-init metadata (#7964)
    * Allow configkey to set 'cloud-name' cloud-init metadata
    
    * Update engine/api/src/main/java/com/cloud/vm/VirtualMachineManager.java
    
    Co-authored-by: Daniel Augusto Veronezi Salvador <38945620+GutoVeronezi@users.noreply.github.com>
    
    * Update server/src/main/java/com/cloud/network/NetworkModelImpl.java
    
    Co-authored-by: Daniel Augusto Veronezi Salvador <38945620+GutoVeronezi@users.noreply.github.com>
    
    * Update server/src/main/java/com/cloud/network/router/CommandSetupHelper.java
    
    Co-authored-by: Daniel Augusto Veronezi Salvador <38945620+GutoVeronezi@users.noreply.github.com>
    
    * Revert "Update server/src/main/java/com/cloud/network/router/CommandSetupHelper.java"
    
    This reverts commit 8abc3e38c4f973227e77253a8bcf3c94827d187f.
    
    * Revert "Update server/src/main/java/com/cloud/network/NetworkModelImpl.java"
    
    This reverts commit 7f239be919112913d764b141e943acca4473f755.
    
    * Rework/Fix review code suggestions
    
    ---------
    
    Co-authored-by: Marcus Sorensen <mls@apple.com>
    Co-authored-by: Daniel Augusto Veronezi Salvador <38945620+GutoVeronezi@users.noreply.github.com>
    (cherry picked from commit 155a30748cfdcdffe31ce3b13f54008323f4774a)
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>

--- engine/orchestration/src/main/java/com/cloud/vm/VirtualMachineManagerImpl.java
+++ engine/orchestration/src/main/java/com/cloud/vm/VirtualMachineManagerImpl.java
@@ -4727,7 +4727,7 @@ public class VirtualMachineManagerImpl extends ManagerBase implements VirtualMac
                 VmOpLockStateRetry, VmOpWaitInterval, ExecuteInSequence, VmJobCheckInterval, VmJobTimeout, VmJobStateReportInterval,
                 VmConfigDriveLabel, VmConfigDriveOnPrimaryPool, VmConfigDriveForceHostCacheUse, VmConfigDriveUseHostCacheOnUnsupportedPool,
                 HaVmRestartHostUp, ResourceCountRunningVMsonly, AllowExposeHypervisorHostname, AllowExposeHypervisorHostnameAccountLevel, SystemVmRootDiskSize,
-                AllowExposeDomainInMetadata
+                AllowExposeDomainInMetadata, MetadataCustomCloudName
         };
     }
 
