7d64bdc4ccc90c1f6aca298464f5a03057483271
Wei Zhou <weizhou@apache.org>
2023-10-20 11:24:11 +0200

    VNF: fix 'Advanced mode' in vm deployment wizard

--- ui/src/views/compute/DeployVM.vue
+++ ui/src/views/compute/DeployVM.vue
@@ -522,7 +522,7 @@
                     {{ $t('label.isadvanced') }}
                     <a-switch v-model:checked="showDetails" style="margin-left: 10px"/>
                   </span>
-                  <div style="margin-top: 15px" v-show="showDetails">
+                  <div style="margin-top: 15px" v-if="showDetails">
                     <div
                       v-if="vm.templateid && ['KVM', 'VMware', 'XenServer'].includes(hypervisor) && !template.deployasis">
                       <a-form-item :label="$t('label.boottype')" name="boottype" ref="boottype">
