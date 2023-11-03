2f97e3bd83506aff52c220654777568e1c04b3ee
gzhao9 <74684732+gzhao9@users.noreply.github.com>
2023-11-03 07:49:32 -0400

    refactor MockNetworkVO (#8137)
    * refactor MockNetworkVO
    
    * Apply suggestions from code review
    
    Co-authored-by: dahn <daan.hoogland@gmail.com>
    
    * adding static
    
    adding a static method to the MockNetworkVO class that generates a MockNetworkVO rather than using new everytime.
    
    ---------
    
    Co-authored-by: dahn <daan.hoogland@gmail.com>

--- plugins/network-elements/juniper-contrail/src/test/java/org/apache/cloudstack/network/contrail/model/InstanceIpModelTest.java
+++ plugins/network-elements/juniper-contrail/src/test/java/org/apache/cloudstack/network/contrail/model/InstanceIpModelTest.java
@@ -16,6 +16,7 @@
 // under the License.
 
 package org.apache.cloudstack.network.contrail.model;
+
 import static org.mockito.Matchers.anyInt;
 import static org.mockito.Matchers.anyLong;
 import static org.mockito.Matchers.anyString;
@@ -44,26 +45,19 @@ import net.juniper.contrail.api.ApiConnectorMock;
 
 public class InstanceIpModelTest extends TestCase {
     private static final Logger s_logger =
-        Logger.getLogger(InstanceIpModelTest.class);
+            Logger.getLogger(InstanceIpModelTest.class);
 
     @Test
     public void testCreateInstanceIp() throws IOException {
 
         ContrailManagerImpl contrailMgr = mock(ContrailManagerImpl.class);
-        ModelController controller      = mock(ModelController.class);
+        ModelController controller = mock(ModelController.class);
         ApiConnector api = new ApiConnectorMock(null, 0);
         when(controller.getApiAccessor()).thenReturn(api);
         when(controller.getManager()).thenReturn(contrailMgr);
 
         // Create Virtual-Network (VN)
-        NetworkVO network = mock(NetworkVO.class);
-        when(network.getName()).thenReturn("testnetwork");
-        when(network.getState()).thenReturn(Network.State.Implemented);
-        when(network.getGateway()).thenReturn("10.1.1.1");
-        when(network.getCidr()).thenReturn("10.1.1.0/24");
-        when(network.getPhysicalNetworkId()).thenReturn(42L);
-        when(network.getDomainId()).thenReturn(10L);
-        when(network.getAccountId()).thenReturn(42L);
+        NetworkVO network = MockNetworkVO.getNetwork(Network.State.Implemented);
         NetworkDao networkDao = mock(NetworkDao.class);
         when(networkDao.findById(anyLong())).thenReturn(network);
         when(controller.getNetworkDao()).thenReturn(networkDao);
@@ -86,7 +80,7 @@ public class InstanceIpModelTest extends TestCase {
         when(vm.getState()).thenReturn(VirtualMachine.State.Running);
         when(vm.getDomainId()).thenReturn(10L);
         when(vm.getAccountId()).thenReturn(42L);
-        UserVmDao VmDao      = mock(UserVmDao.class);
+        UserVmDao VmDao = mock(UserVmDao.class);
         when(VmDao.findById(anyLong())).thenReturn(null);
         when(controller.getVmDao()).thenReturn(VmDao);
 
