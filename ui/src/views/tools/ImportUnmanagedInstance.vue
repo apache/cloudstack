081f39fe42b2e15cf6dd584b65f27fef198c9ed9
nvazquez <nicovazquez90@gmail.com>
2023-11-30 12:02:57 -0300

    Fix allow duplicate mac addresses UI position

--- ui/src/views/tools/ImportUnmanagedInstance.vue
+++ ui/src/views/tools/ImportUnmanagedInstance.vue
@@ -266,6 +266,18 @@
                   </template>
                   <span>{{ $t('message.ip.address.changes.effect.after.vm.restart') }}</span>
                 </a-form-item>
+                <a-row :gutter="12" justify="end">
+                  <a-col style="text-align: right">
+                    <a-form-item name="forced" ref="forced">
+                      <template #label>
+                        <tooltip-label
+                          :title="selectedVmwareVcenter ? $t('label.allow.duplicate.macaddresses') : $t('label.forced')"
+                          :tooltip="apiParams.forced.description"/>
+                      </template>
+                      <a-switch v-model:checked="form.forced" @change="val => { switches.forced = val }" />
+                    </a-form-item>
+                  </a-col>
+                </a-row>
                 <multi-network-selection
                   :items="nics"
                   :zoneId="cluster.zoneid"
@@ -289,14 +301,6 @@
                       <tooltip-label :title="$t('label.migrate.allowed')" :tooltip="apiParams.migrateallowed.description"/>
                     </template>
                     <a-switch v-model:checked="form.migrateallowed" @change="val => { switches.migrateAllowed = val }" />
-                  </a-form-item>
-                </a-col>
-                <a-col :md="24" :lg="12">
-                  <a-form-item name="forced" ref="forced">
-                    <template #label>
-                      <tooltip-label :title="$t('label.forced')" :tooltip="apiParams.forced.description"/>
-                    </template>
-                    <a-switch v-model:checked="form.forced" @change="val => { switches.forced = val }" />
                   </a-form-item>
                 </a-col>
               </a-row>
