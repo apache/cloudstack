f6ad2985d3de0b4f5f2fe3166fc00ec0828df0b5
nvazquez <nicovazquez90@gmail.com>
2023-12-04 18:55:21 -0300

    Display unmanage VM button for KVM and do not let use an offering that doesnt match on KVM manage

--- ui/src/views/tools/ImportUnmanagedInstance.vue
+++ ui/src/views/tools/ImportUnmanagedInstance.vue
@@ -204,6 +204,7 @@
                 :minimumCpunumber="isVmRunning ? resource.cpunumber : null"
                 :minimumCpuspeed="isVmRunning ? resource.cpuspeed : null"
                 :minimumMemory="isVmRunning ? resource.memory : null"
+                :exactMatch="isKVMUnmanage ? true : false"
                 size="small"
                 @select-compute-item="($event) => updateComputeOffering($event)"
                 @handle-search-filter="($event) => fetchComputeOfferings($event)" />
