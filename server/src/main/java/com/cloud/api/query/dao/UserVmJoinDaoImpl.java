a11fc43788e873f09031d0b03cdbf6477d8305da
Abhishek Kumar <abhishek.mrt22@gmail.com>
2023-12-09 11:43:52 +0530

    server: fix diskoffering details in vm response (#8135)
    Fixes #8120

--- server/src/main/java/com/cloud/api/query/dao/UserVmJoinDaoImpl.java
+++ server/src/main/java/com/cloud/api/query/dao/UserVmJoinDaoImpl.java
@@ -27,6 +27,7 @@ import java.util.stream.Collectors;
 
 import javax.inject.Inject;
 
+import com.cloud.storage.Volume;
 import org.apache.cloudstack.affinity.AffinityGroupResponse;
 import org.apache.cloudstack.annotation.AnnotationService;
 import org.apache.cloudstack.annotation.dao.AnnotationDao;
@@ -42,6 +43,7 @@ import org.apache.cloudstack.context.CallContext;
 import org.apache.cloudstack.framework.config.dao.ConfigurationDao;
 import org.apache.cloudstack.query.QueryService;
 import org.apache.commons.lang3.BooleanUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Component;
 
@@ -548,6 +550,11 @@ public class UserVmJoinDaoImpl extends GenericDaoBaseWithTagInformation<UserVmJo
             resp.setObjectName("affinitygroup");
             resp.setAccountName(uvo.getAccountName());
             userVmData.addAffinityGroup(resp);
+        }
+
+        if (StringUtils.isEmpty(userVmData.getDiskOfferingId()) && !Volume.Type.ROOT.equals(uvo.getVolumeType())) {
+            userVmData.setDiskOfferingId(uvo.getDiskOfferingUuid());
+            userVmData.setDiskOfferingName(uvo.getDiskOfferingName());
         }
 
         return userVmData;
