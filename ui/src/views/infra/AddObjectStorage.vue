dc12e08e0f67bbcb7179228f44b2b3c72787c712
Ronny <ronald.feicht@scsynergy.de>
2023-12-14 10:24:45 +0100

    added plugin for Huawei OBS object storage 	added "Huawei OBS" to the UI provider dropdown

--- ui/src/views/infra/AddObjectStorage.vue
+++ ui/src/views/infra/AddObjectStorage.vue
@@ -82,7 +82,7 @@ export default {
   inject: ['parentFetchData'],
   data () {
     return {
-      providers: ['MinIO', 'Simulator'],
+      providers: ['MinIO', 'Huawei OBS', 'Simulator'],
       zones: [],
       loading: false
     }
