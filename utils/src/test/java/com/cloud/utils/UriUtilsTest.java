c8eb90ce9631a16973582f71c7eeb09bb0799942
Wido den Hollander <wido@widodh.nl>
2023-12-18 09:51:29 +0100

    utils: Add tests for UriUtils.validateUrl();

--- utils/src/test/java/com/cloud/utils/UriUtilsTest.java
+++ utils/src/test/java/com/cloud/utils/UriUtilsTest.java
@@ -273,4 +273,16 @@ public class UriUtilsTest {
         Assert.assertTrue(UriUtils.isUrlForCompressedFile("https://abc.com/xyz.gz"));
         Assert.assertFalse(UriUtils.isUrlForCompressedFile("http://abc.com/xyz.qcow2"));
     }
+
+    @Test
+    public void validateUrl() {
+        Pair<String, Integer> url1 = UriUtils.validateUrl("https://www.cloudstack.org");
+        Assert.assertEquals(url1.first(), "www.cloudstack.org");
+
+        Pair<String, Integer> url2 = UriUtils.validateUrl("https://www.apache.org");
+        Assert.assertEquals(url2.first(), "www.apache.org");
+
+        Pair<String, Integer> url3 = UriUtils.validateUrl("https://ipv6.google.com");
+        Assert.assertEquals(url3.first(), "ipv6.google.com");
+    }
 }
