fdf2af3e1aa67b7615aedf615339a6ece60e944a
Abhishek Kumar <abhishek.mrt22@gmail.com>
2023-12-20 19:06:09 +0530

    Update test/integration/smoke/test_vm_life_cycle.py

--- test/integration/smoke/test_vm_life_cycle.py
+++ test/integration/smoke/test_vm_life_cycle.py
@@ -1037,7 +1037,7 @@ class TestSecuredVmMigration(cloudstackTestCase):
                         hostid=host.id,
                         type='Routing'
                     )[0]
-                    if restarted_host.state == 'Up':
+                    if restarted_host.state == "Up":
                         break
                     retries = retries - 1
 
