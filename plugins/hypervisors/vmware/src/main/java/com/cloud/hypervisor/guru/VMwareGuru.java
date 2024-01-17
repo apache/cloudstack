a3732164903b573d90a0ad650d07f2fb5849cdf0
Gabriel P Santos <gabriel.santos@scclouds.com.br>
2024-01-17 15:53:39 -0300

    add a call to vmware vm.destroy when finalizing expunge process

--- plugins/hypervisors/vmware/src/main/java/com/cloud/hypervisor/guru/VMwareGuru.java
+++ plugins/hypervisors/vmware/src/main/java/com/cloud/hypervisor/guru/VMwareGuru.java
@@ -27,6 +27,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
+import com.cloud.agent.api.CleanupVMCommand;
+import com.cloud.storage.VolumeApiService;
+import com.cloud.utils.LogUtils;
 import javax.inject.Inject;
 
 import com.cloud.agent.api.to.NfsTO;
@@ -369,6 +372,13 @@ public class VMwareGuru extends HypervisorGuruBase implements HypervisorGuru, Co
             return guid;
 
         return tokens[0] + "@" + vCenterIp;
+    }
+
+    @Override public List<Command> finalizeExpunge(VirtualMachine vm) {
+        List<Command> commands = new ArrayList<Command>();
+        final CleanupVMCommand cleanupVMCommand = new CleanupVMCommand(vm.getInstanceName(), true);
+        commands.add(cleanupVMCommand);
+        return commands;
     }
 
     @Override public List<Command> finalizeExpungeNics(VirtualMachine vm, List<NicProfile> nics) {
