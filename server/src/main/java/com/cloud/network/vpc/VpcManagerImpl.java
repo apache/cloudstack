f96f464fc16b79d46f4ae5e6e44834b3d32072fc
Pearl Dsilva <pearl1594@gmail.com>
2024-01-26 10:35:40 -0500

    add source nat to the vpc offering

--- server/src/main/java/com/cloud/network/vpc/VpcManagerImpl.java
+++ server/src/main/java/com/cloud/network/vpc/VpcManagerImpl.java
@@ -403,7 +403,7 @@ public class VpcManagerImpl extends ManagerBase implements VpcManager, VpcProvis
                         if (List.of(Service.UserData, Service.Dhcp, Service.Dns).contains(svc)) {
                             final Set<Provider> userDataProvider = Set.of(Provider.VPCVirtualRouter);
                             svcProviderMap.put(svc, userDataProvider);
-                        } else if (Service.NetworkACL.equals(svc)){
+                        } else if (List.of(Service.SourceNat, Service.NetworkACL).contains(svc)) {
                             svcProviderMap.put(svc, defaultProviders);
                         }
                     }
