c465e2f5fdafebb2cea6ad77c6620ce8a3fd08c7
Daan Hoogland <daan@onecht.net>
2024-02-15 12:51:26 +0100

    extract pvland rules method

--- server/src/main/java/com/cloud/network/router/VirtualNetworkApplianceManagerImpl.java
+++ server/src/main/java/com/cloud/network/router/VirtualNetworkApplianceManagerImpl.java
@@ -2788,15 +2788,19 @@ Configurable, StateListener<VirtualMachine.State, VirtualMachine.Event, VirtualM
         for (final Nic nic : routerNics) {
             final Network network = _networkModel.getNetwork(nic.getNetworkId());
 
-            if (network.getTrafficType() == TrafficType.Guest && nic.getBroadcastUri() != null && nic.getBroadcastUri().getScheme().equals("pvlan")) {
-                final NicProfile nicProfile = new NicProfile(nic, network, nic.getBroadcastUri(), nic.getIsolationUri(), 0, false, "pvlan-nic");
+            removeDhcpRulesForPvLan(domR, nic, network, dcVO);
+        }
+    }
 
-                final NetworkTopology networkTopology = _networkTopologyContext.retrieveNetworkTopology(dcVO);
-                try {
-                    networkTopology.setupDhcpForPvlan(false, domR, domR.getHostId(), nicProfile);
-                } catch (final ResourceUnavailableException e) {
-                    s_logger.debug("ERROR in finalizeStop: ", e);
-                }
+    private void removeDhcpRulesForPvLan(DomainRouterVO domR, Nic nic, Network network, DataCenterVO dcVO) {
+        if (network.getTrafficType() == TrafficType.Guest && nic.getBroadcastUri() != null && nic.getBroadcastUri().getScheme().equals("pvlan")) {
+            final NicProfile nicProfile = new NicProfile(nic, network, nic.getBroadcastUri(), nic.getIsolationUri(), 0, false, "pvlan-nic");
+
+            final NetworkTopology networkTopology = _networkTopologyContext.retrieveNetworkTopology(dcVO);
+            try {
+                networkTopology.setupDhcpForPvlan(false, domR, domR.getHostId(), nicProfile);
+            } catch (final ResourceUnavailableException e) {
+                s_logger.debug("ERROR in finalizeStop: ", e);
             }
         }
     }
