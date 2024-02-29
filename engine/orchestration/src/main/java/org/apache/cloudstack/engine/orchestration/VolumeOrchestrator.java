c462be1412d06bb81a8d9d2a1bd6718e20f5a477
Harikrishna <harikrishna.patnala@gmail.com>
2024-02-29 14:41:49 +0530

    New API "checkVolume" to check and repair any leaks or issues reported by qemu-img check (#8577)
    * Introduced a new API checkVolumeAndRepair that allows users or admins to check and repair if any leaks observed.
    Currently this is supported only for KVM
    
    * some fixes
    
    * Added unit tests
    
    * addressed review comments
    
    * add repair volume while granting access
    
    * Changed repair parameter to accept both leaks/all
    
    * Introduced new global setting volume.check.and.repair.before.use to do volume check and repair before VM start or volume attach operations
    
    * Added volume check and repair changes only during VM start and volume attach operations
    
    * Refactored the names to look similar across the code
    
    * Some code fixes
    
    * remove unused code
    
    * Renamed repair values
    
    * Fixed unit tests
    
    * changed version
    
    * Address review comments
    
    * Code refactored
    
    * used volume name in logs
    
    * Changed the API to Async and the setting scope to storage pool
    
    * Fixed exit value handling with check volume command
    
    * Fixed storage scope to the setting
    
    * Fix volume format issues
    
    * Refactored the log messages
    
    * Fix formatting

--- engine/orchestration/src/main/java/org/apache/cloudstack/engine/orchestration/VolumeOrchestrator.java
+++ engine/orchestration/src/main/java/org/apache/cloudstack/engine/orchestration/VolumeOrchestrator.java
@@ -1915,6 +1915,8 @@ public class VolumeOrchestrator extends ManagerBase implements VolumeOrchestrati
                             }
                         }
                     }
+                } else {
+                    handleCheckAndRepairVolume(vol, vm.getVirtualMachine().getHostId());
                 }
             } else if (task.type == VolumeTaskType.MIGRATE) {
                 pool = (StoragePool)dataStoreMgr.getDataStore(task.pool.getId(), DataStoreRole.Primary);
@@ -1954,6 +1956,16 @@ public class VolumeOrchestrator extends ManagerBase implements VolumeOrchestrati
                 }
             }
 
+        }
+    }
+
+    private void handleCheckAndRepairVolume(Volume vol, Long hostId) {
+        Host host = _hostDao.findById(hostId);
+        try {
+            volService.checkAndRepairVolumeBasedOnConfig(volFactory.getVolume(vol.getId()), host);
+        } catch (Exception e) {
+            String volumeToString = getReflectOnlySelectedFields(vol);
+            s_logger.debug(String.format("Unable to check and repair volume [%s] on host [%s], due to %s.", volumeToString, host, e.getMessage()));
         }
     }
 
