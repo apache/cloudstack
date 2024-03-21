b76092bcc3f9ec4fdd8c244408179d265e3acf19
Suresh Kumar Anaparti <sureshkumar.anaparti@gmail.com>
2024-03-21 17:39:50 +0530

    cleanup unused code

--- plugins/hypervisors/vmware/src/main/java/com/cloud/hypervisor/guru/VMwareGuru.java
+++ plugins/hypervisors/vmware/src/main/java/com/cloud/hypervisor/guru/VMwareGuru.java
@@ -1358,9 +1358,6 @@ public class VMwareGuru extends HypervisorGuruBase implements HypervisorGuru, Co
             throw new CloudRuntimeException(err);
         }
 
-        String datastorePath = datastoreMO.getDatastorePathOnHost(sourceHost.getMor().getValue());
-        s_logger.info("Host datastore path: " + datastorePath);
-
         relocateClonedVMToSourceHost(clonedVM, sourceHost);
         return clonedVM;
     }
