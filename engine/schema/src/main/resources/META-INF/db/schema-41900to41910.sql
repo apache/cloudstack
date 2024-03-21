38575a3bcd23f4431096125dcaf40fcf1af5d8c1
Bryan Lima <bryan.lima@hotmail.com>
2024-03-21 10:32:08 -0300

    Fix update paths

--- engine/schema/src/main/resources/META-INF/db/schema-41900to41910.sql
+++ engine/schema/src/main/resources/META-INF/db/schema-41900to41910.sql
@@ -30,3 +30,21 @@ UPDATE cloud_usage.quota_tariff
 SET usage_unit = 'IOPS', updated_on = NOW()
 WHERE effective_on = '2010-05-04 00:00:00'
 AND name IN ('VM_DISK_IO_READ', 'VM_DISK_IO_WRITE');
+
+CALL `cloud`.`IDEMPOTENT_ADD_COLUMN`('cloud.guest_os', 'display', 'tinyint(1) DEFAULT ''1'' COMMENT ''should this guest_os be shown to the end user'' ');
+
+-- PR #7236 - [Usage] Create network billing
+CREATE TABLE IF NOT EXISTS `cloud_usage`.`usage_networks` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `network_offering_id` bigint(20) unsigned NOT NULL,
+  `zone_id` bigint(20) unsigned NOT NULL,
+  `network_id` bigint(20) unsigned NOT NULL,
+  `account_id` bigint(20) unsigned NOT NULL,
+  `domain_id` bigint(20) unsigned NOT NULL,
+  `state` varchar(100) DEFAULT NULL,
+  `removed` datetime DEFAULT NULL,
+  `created` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB CHARSET=utf8;
+
+CALL `cloud_usage`.`IDEMPOTENT_ADD_COLUMN`('cloud_usage.cloud_usage', 'state', 'VARCHAR(100) DEFAULT NULL');
