7affbb1dacf709424b33f30a5305c44ea201acd4
dahn <daan@onecht.net>
2024-04-19 12:23:31 +0200

    protect against null-path (#8915)
    Co-authored-by: Vladimir Dombrovski <vladimir.dombrovski@bso.co>
    Co-authored-by: Vishesh <vishesh92@gmail.com>
    Co-authored-by: Suresh Kumar Anaparti <sureshkumar.anaparti@gmail.com>

--- plugins/hypervisors/vmware/src/main/java/com/cloud/hypervisor/vmware/resource/VmwareResource.java
+++ plugins/hypervisors/vmware/src/main/java/com/cloud/hypervisor/vmware/resource/VmwareResource.java
@@ -3066,7 +3066,10 @@ public class VmwareResource extends ServerResourceBase implements StoragePoolRes
     }
 
     private String appendFileType(String path, String fileType) {
-        if (path.toLowerCase().endsWith(fileType.toLowerCase())) {
+        if (StringUtils.isBlank(path)) {
+            throw new CloudRuntimeException("No path given, cannot append filetype " + fileType);
+        }
+        if (fileType == null || path.toLowerCase().endsWith(fileType.toLowerCase())) {
             return path;
         }
 
