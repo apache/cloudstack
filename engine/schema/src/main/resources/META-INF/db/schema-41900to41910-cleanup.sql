d3677aa2f69a1ab0826b14102a6387bda85a4d93
Rohit Yadav <rohit.yadav@shapeblue.com>
2024-04-27 10:25:23 +0530

    address default global setting to allow faster list VMs response time
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>

--- engine/schema/src/main/resources/META-INF/db/schema-41900to41910-cleanup.sql
+++ engine/schema/src/main/resources/META-INF/db/schema-41900to41910-cleanup.sql
@@ -19,4 +19,6 @@
 -- Schema upgrade cleanup from 4.19.0.0 to 4.19.1.0
 --;
 
+-- List VMs response optimisation https://github.com/apache/cloudstack/pull/8985
+UPDATE cloud.configuration set value='false' where name='vm.stats.increment.metrics';
 DELETE from cloud.configuration where name='vm.stats.increment.metrics.in.memory';
