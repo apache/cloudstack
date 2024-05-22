a9479af39361058a78bc383d0f232ede5a45197d
dahn <daan.hoogland@gmail.com>
2024-05-22 10:21:07 +0200

    Apply suggestions from code review
    Co-authored-by: João Jandre <48719461+JoaoJandre@users.noreply.github.com>

--- plugins/integrations/kubernetes-service/src/main/java/com/cloud/kubernetes/cluster/utils/KubernetesClusterUtil.java
+++ plugins/integrations/kubernetes-service/src/main/java/com/cloud/kubernetes/cluster/utils/KubernetesClusterUtil.java
@@ -266,11 +266,11 @@ public class KubernetesClusterUtil {
                 socket.connect(new InetSocketAddress(ipAddress, port), 10000);
                 controlVmRunning = true;
             } catch (IOException e) {
-                LOGGER.info("Waiting for Kubernetes cluster : %s control node VMs to be accessible", kubernetesCluster.getName());
+                LOGGER.info("Waiting for Kubernetes cluster : {} control node VMs to be accessible", kubernetesCluster.getName());
                 try {
                     Thread.sleep(10000);
                 } catch (InterruptedException ex) {
-                    LOGGER.warn("Error while waiting for Kubernetes cluster : %s control node VMs to be accessible", kubernetesCluster.getName(), ex);
+                    LOGGER.warn("Error while waiting for Kubernetes cluster : {} control node VMs to be accessible", kubernetesCluster.getName(), ex);
                 }
             }
         }
