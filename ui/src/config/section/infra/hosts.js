422bb13152450553b72d5cabb7b4bd8c6abe3444
Abhishek Kumar <abhishek.mrt22@gmail.com>
2024-05-24 18:30:55 +0530

    utils,ui: obfuscate sensitive log info, use POST for configureOutOfBandManagement
    Fixes #8526
    
    Signed-off-by: Abhishek Kumar <abhishek.mrt22@gmail.com>

--- ui/src/config/section/infra/hosts.js
+++ ui/src/config/section/infra/hosts.js
@@ -149,6 +149,7 @@ export default {
       message: 'label.outofbandmanagement.configure',
       docHelp: 'adminguide/hosts.html#out-of-band-management',
       dataView: true,
+      post: true,
       args: ['hostid', 'address', 'port', 'username', 'password', 'driver'],
       mapping: {
         hostid: {
