b24e990b1f28905bdb6f10f303639a5f0149c1de
abh1sar <abhisar.sinha@gmail.com>
2024-06-14 11:46:49 +0530

    Null check and leading and trailing whitespace trimming only

--- plugins/user-authenticators/oauth2/src/main/java/org/apache/cloudstack/oauth2/OAuth2AuthManagerImpl.java
+++ plugins/user-authenticators/oauth2/src/main/java/org/apache/cloudstack/oauth2/OAuth2AuthManagerImpl.java
@@ -138,9 +138,9 @@ public class OAuth2AuthManagerImpl extends ManagerBase implements OAuth2AuthMana
     public OauthProviderVO registerOauthProvider(RegisterOAuthProviderCmd cmd) {
         String description = cmd.getDescription();
         String provider = cmd.getProvider();
-        String clientId = cmd.getClientId().replaceAll("\\s", "");
-        String redirectUri = cmd.getRedirectUri().replaceAll("\\s", "");
-        String secretKey = cmd.getSecretKey().replaceAll("\\s", "");
+        String clientId = StringUtils.trim(cmd.getClientId());
+        String redirectUri = StringUtils.trim(cmd.getRedirectUri());
+        String secretKey = StringUtils.trim(cmd.getSecretKey());
 
         if (!isOAuthPluginEnabled()) {
             throw new CloudRuntimeException("OAuth is not enabled, please enable to register");
@@ -170,9 +170,9 @@ public class OAuth2AuthManagerImpl extends ManagerBase implements OAuth2AuthMana
     public OauthProviderVO updateOauthProvider(UpdateOAuthProviderCmd cmd) {
         Long id = cmd.getId();
         String description = cmd.getDescription();
-        String clientId = cmd.getClientId().replaceAll("\\s", "");
-        String redirectUri = cmd.getRedirectUri().replaceAll("\\s", "");
-        String secretKey = cmd.getSecretKey().replaceAll("\\s", "");
+        String clientId = StringUtils.trim(cmd.getClientId());
+        String redirectUri = StringUtils.trim(cmd.getRedirectUri());
+        String secretKey = StringUtils.trim(cmd.getSecretKey());
         Boolean enabled = cmd.getEnabled();
 
         OauthProviderVO providerVO = _oauthProviderDao.findById(id);
