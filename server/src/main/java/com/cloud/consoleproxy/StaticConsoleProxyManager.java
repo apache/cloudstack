313a165e62bea8ed4b31e79ab05b8c5c07e6c091
Wei Zhou <weizhou@apache.org>
2024-06-21 12:26:20 +0200

    server: add global setting consoleproxy.sslEnabled (#8809)

--- server/src/main/java/com/cloud/consoleproxy/StaticConsoleProxyManager.java
+++ server/src/main/java/com/cloud/consoleproxy/StaticConsoleProxyManager.java
@@ -72,7 +72,7 @@ public class StaticConsoleProxyManager extends AgentBasedConsoleProxyManager imp
             _ip = "127.0.0.1";
         }
 
-        String value = (String)params.get("consoleproxy.sslEnabled");
+        String value = (String)params.get(ConsoleProxySslEnabled.key());
         if (value != null && value.equalsIgnoreCase("true")) {
             _sslEnabled = true;
         }
