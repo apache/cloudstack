66e0fbfce77c550dfdb772754ab1c5e23a83ff92
abh1sar <abhisar.sinha@gmail.com>
2024-06-25 14:41:46 +0530

    Restart agent when host comes out of maintenance

--- server/src/main/java/com/cloud/resource/ResourceManagerImpl.java
+++ server/src/main/java/com/cloud/resource/ResourceManagerImpl.java
@@ -2757,7 +2757,7 @@ public class ResourceManagerImpl extends ManagerBase implements ResourceManager,
             }
         }
 
-        handleAgentIfNotConnected(host, vms_migrating);
+        handleAgentConnectAndRestart(host, vms_migrating);
 
         try {
             resourceStateTransitTo(host, ResourceState.Event.AdminCancelMaintenance, _nodeId);
@@ -2772,16 +2772,16 @@ public class ResourceManagerImpl extends ManagerBase implements ResourceManager,
     }
 
     /**
-     * Handle agent (if available) if its not connected before cancelling maintenance.
      * Agent must be connected before cancelling maintenance.
+     * Connect and restart agent (if available) before cancelling maintenance.
      * If the host status is not Up:
      * - If kvm.ssh.to.agent is true, then SSH into the host and restart the agent.
      * - If kvm.shh.to.agent is false, then fail cancelling maintenance
      */
-    protected void handleAgentIfNotConnected(HostVO host, boolean vmsMigrating) {
+    protected void handleAgentConnectAndRestart(HostVO host, boolean vmsMigrating) {
         final boolean isAgentOnHost = host.getHypervisorType() == HypervisorType.KVM ||
                 host.getHypervisorType() == HypervisorType.LXC;
-        if (!isAgentOnHost || vmsMigrating || host.getStatus() == Status.Up) {
+        if (!isAgentOnHost || vmsMigrating) {
             return;
         }
         final boolean sshToAgent = Boolean.parseBoolean(_configDao.getValue(KvmSshToAgentEnabled.key()));
