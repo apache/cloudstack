3c306699362f4aa39d4a9f22e187c36cc15ea982
Bryan Lima <bryan.lima@hotmail.com>
2024-08-05 17:27:53 -0300

    Normalize package name

--- api/src/main/java/org/apache/cloudstack/gui/theme/GuiTheme.java
+++ api/src/main/java/org/apache/cloudstack/gui/theme/GuiTheme.java
@@ -14,7 +14,7 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
-package org.apache.cloudstack.gui.themes;
+package org.apache.cloudstack.gui.theme;
 
 import org.apache.cloudstack.api.Identity;
 import org.apache.cloudstack.api.InternalIdentity;
