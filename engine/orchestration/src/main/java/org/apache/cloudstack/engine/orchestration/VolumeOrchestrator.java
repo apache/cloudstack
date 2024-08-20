bipinge/cloudstack-build

Apply IOPS limitation when resizing/migrating volume

engine/orchestration/src/main/java/org/apache/cloudstack/engine/orchestration/VolumeOrchestrator.java
@@ -300,7 +300,7 @@ public class VolumeOrchestrator extends ManagerBase implements VolumeOrchestrati
             throw new CloudRuntimeException(String.format("Failed to find a storage pool with enough capacity to move the volume [%s] to.", volumeToString));
         }
 
-        Volume newVol = migrateVolume(volumeInfo, destPool);
+        Volume newVol = migrateVolume(volumeInfo, destPool, diskOffering);
         return volFactory.getVolume(newVol.getId());
     }
 
@@ -1346,13 +1346,14 @@ public class VolumeOrchestrator extends ManagerBase implements VolumeOrchestrati
 
     @Override
     @DB
-    public Volume migrateVolume(Volume volume, StoragePool destPool) throws StorageUnavailableException {
+    public Volume migrateVolume(Volume volume, StoragePool destPool, DiskOffering newDiskOffering) throws StorageUnavailableException {
         String volumeToString = getVolumeIdentificationInfos(volume);
 
         VolumeInfo vol = volFactory.getVolume(volume.getId());
         if (vol == null){
             throw new CloudRuntimeException(String.format("Volume migration failed because volume [%s] is null.", volumeToString));
         }
+        vol.addPayload(newDiskOffering);
         if (destPool == null) {
             throw new CloudRuntimeException("Volume migration failed because the destination storage pool is not available.");
         }
@@ -1483,7 +1484,7 @@ public class VolumeOrchestrator extends ManagerBase implements VolumeOrchestrati
             s_logger.debug("Offline VM migration was not done up the stack in VirtualMachineManager. Trying to migrate the VM here.");
         }
         for (Map.Entry<Volume, StoragePool> entry : volumeStoragePoolMap.entrySet()) {
-            Volume result = migrateVolume(entry.getKey(), entry.getValue());
+            Volume result = migrateVolume(entry.getKey(), entry.getValue(), null);
             if (result == null) {
                 return false;
             }
@@ -1920,7 +1921,7 @@ public class VolumeOrchestrator extends ManagerBase implements VolumeOrchestrati
                 }
             } else if (task.type == VolumeTaskType.MIGRATE) {
                 pool = (StoragePool)dataStoreMgr.getDataStore(task.pool.getId(), DataStoreRole.Primary);
-                vol = migrateVolume(task.volume, pool);
+                vol = migrateVolume(task.volume, pool, null);
             } else if (task.type == VolumeTaskType.RECREATE) {
                 Pair<VolumeVO, DataStore> result = recreateVolume(task.volume, vm, dest);
                 pool = (StoragePool)dataStoreMgr.getDataStore(result.second().getId(), DataStoreRole.Primary);

apache/cloudstack

engine/orchestration/src/main/java/org/apache/cloudstack/engine/orchestration/VolumeOrchestrator.java
@@ -310,7 +310,7 @@ public class VolumeOrchestrator extends ManagerBase implements VolumeOrchestrati
             throw new CloudRuntimeException(String.format("Failed to find a storage pool with enough capacity to move the volume [%s] to.", volumeToString));
         }
 
-        Volume newVol = migrateVolume(volumeInfo, destPool);
+        Volume newVol = migrateVolume(volumeInfo, destPool, diskOffering);
         return volFactory.getVolume(newVol.getId());
     }
 
@@ -1343,13 +1343,14 @@ public class VolumeOrchestrator extends ManagerBase implements VolumeOrchestrati
 
     @Override
     @DB
-    public Volume migrateVolume(Volume volume, StoragePool destPool) throws StorageUnavailableException {
+    public Volume migrateVolume(Volume volume, StoragePool destPool, DiskOffering newDiskOffering) throws StorageUnavailableException {
         String volumeToString = getVolumeIdentificationInfos(volume);
 
         VolumeInfo vol = volFactory.getVolume(volume.getId());
         if (vol == null){
             throw new CloudRuntimeException(String.format("Volume migration failed because volume [%s] is null.", volumeToString));
         }
+        vol.addPayload(newDiskOffering);
         if (destPool == null) {
             throw new CloudRuntimeException("Volume migration failed because the destination storage pool is not available.");
         }
@@ -1478,7 +1479,7 @@ public class VolumeOrchestrator extends ManagerBase implements VolumeOrchestrati
         }
         logger.debug("Offline VM migration was not done up the stack in VirtualMachineManager. Trying to migrate the VM here.");
         for (Map.Entry<Volume, StoragePool> entry : volumeStoragePoolMap.entrySet()) {
-            Volume result = migrateVolume(entry.getKey(), entry.getValue());
+            Volume result = migrateVolume(entry.getKey(), entry.getValue(), null);
             if (result == null) {
                 return false;
             }
@@ -1939,7 +1940,7 @@ public class VolumeOrchestrator extends ManagerBase implements VolumeOrchestrati
             } else if (task.type == VolumeTaskType.MIGRATE) {
                 store = (PrimaryDataStore) dataStoreMgr.getDataStore(task.pool.getId(), DataStoreRole.Primary);
                 updateVolumeSize(store, task.volume);
-                vol = migrateVolume(task.volume, store);
+                vol = migrateVolume(task.volume, store, null);
             } else if (task.type == VolumeTaskType.RECREATE) {
                 Pair<VolumeVO, DataStore> result = recreateVolume(task.volume, vm, dest);
                 store = (PrimaryDataStore) dataStoreMgr.getDataStore(result.second().getId(), DataStoreRole.Primary);