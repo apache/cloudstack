c5bf82aa7b9668c4403614839848508713601e22
Nicolas Vazquez <nicovazquez90@gmail.com>
2024-10-10 11:03:42 -0300

    Add dependency and Netris API client (#4)
    * Add dependency and first approach to Netris API client
    
    * Fix authentication and create Netris API client, in progress sites listing
    
    * Fix get sites

---
+++ plugins/network-elements/netris/src/test/java/org/apache/cloudstack/service/NetrisApiClientImplTest.java
@@ -0,0 +1,43 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+package org.apache.cloudstack.service;
+
+import io.netris.model.GetSiteBody;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+public class NetrisApiClientImplTest {
+
+    private static final String endpointUrl = "https://shapeblue-ctl.netris.dev";
+    private static final String username = "netris";
+    private static final String password = "qHHa$CZ2oJv*@!7mwoSR";
+
+    private NetrisApiClientImpl client = new NetrisApiClientImpl(endpointUrl, username, password);
+
+    @Test
+    public void testNetrisAuthStatus() {
+        Assert.assertTrue(client.isSessionAlive());
+    }
+
+    @Test
+    public void testListSites() {
+        List<GetSiteBody> sites = client.listSites();
+        Assert.assertTrue(sites.size() > 0);
+    }
+}
