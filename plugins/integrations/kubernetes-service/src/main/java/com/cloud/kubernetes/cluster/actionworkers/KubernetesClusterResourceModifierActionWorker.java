bipinge/cloudstack-build

enable to create VPC porfowarding rules with source cidr

plugins/integrations/kubernetes-service/src/main/java/com/cloud/kubernetes/cluster/actionworkers/KubernetesClusterResourceModifierActionWorker.java
@@ -482,7 +482,7 @@ public class KubernetesClusterResourceModifierActionWorker extends KubernetesClu
                                         srcPortFinal, srcPortFinal,
                                         vmIp,
                                         22, 22,
-                                        "tcp", networkId, accountId, domainId, vmIdFinal);
+                                        "tcp", networkId, accountId, domainId, vmIdFinal, null);
                         newRule.setDisplay(true);
                         newRule.setState(FirewallRule.State.Add);
                         newRule = portForwardingRulesDao.persist(newRule);

apache/cloudstack

plugins/integrations/kubernetes-service/src/main/java/com/cloud/kubernetes/cluster/actionworkers/KubernetesClusterResourceModifierActionWorker.java
@@ -472,7 +472,7 @@ public class KubernetesClusterResourceModifierActionWorker extends KubernetesClu
                             sourcePort, sourcePort,
                             vmIp,
                             destPort, destPort,
-                            "tcp", networkId, accountId, domainId, vmId);
+                            "tcp", networkId, accountId, domainId, vmId, null);
             newRule.setDisplay(true);
             newRule.setState(FirewallRule.State.Add);
             newRule = portForwardingRulesDao.persist(newRule);