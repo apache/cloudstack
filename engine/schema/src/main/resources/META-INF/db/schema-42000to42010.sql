2f72e5b02efed5d135b2fae372bc1582c939ac6f
abh1sar <abhisar.sinha@gmail.com>
2024-12-03 14:22:08 +0530

    moved db changes from 41900to42000 to 42000to42010

--- engine/schema/src/main/resources/META-INF/db/schema-42000to42010.sql
+++ engine/schema/src/main/resources/META-INF/db/schema-42000to42010.sql
@@ -22,3 +22,11 @@
 -- Add column api_key_access to user and account tables
 CALL `cloud`.`IDEMPOTENT_ADD_COLUMN`('cloud.user', 'api_key_access', 'boolean DEFAULT NULL COMMENT "is api key access allowed for the user" AFTER `secret_key`');
 CALL `cloud`.`IDEMPOTENT_ADD_COLUMN`('cloud.account', 'api_key_access', 'boolean DEFAULT NULL COMMENT "is api key access allowed for the account" ');
+
+-- Create a new group for Usage Server related configurations
+INSERT INTO `cloud`.`configuration_group` (`name`, `description`, `precedence`) VALUES ('Usage Server', 'Usage Server related configuration', 9);
+DELETE FROM `cloud`.`configuration_subgroup` WHERE `name`='Usage';
+INSERT INTO `cloud`.`configuration_subgroup` (`name`, `keywords`, `precedence`, `group_id`) VALUES ('Usage', NULL, 1, (SELECT id FROM `cloud`.`configuration_group` WHERE `name` = 'Usage Server'));
+
+-- Update the description to indicate this setting applies only to volume snapshots on running instances
+UPDATE `cloud`.`configuration` SET `description`='whether volume snapshot is enabled on running instances on KVM hosts' WHERE `name`='kvm.snapshot.enabled';
