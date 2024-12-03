d17de834a57237b79f24850f97190bfc378f31a9
Abhisar Sinha <63767682+abh1sar@users.noreply.github.com>
2024-12-03 12:10:54 +0530

    Disable API Key Access for users, accounts and domains (#9741)
    * cli changes to update user/account, list by apikeyaccess, domain level setting
    
    * UI changes for updating user/account and searchfilter in listview
    
    * make the api parameters and setting accessible only to root admin
    
    * revert changes to ui/package-lock.json
    
    * minor changes to description strings
    
    * UT for ApiServer and AccountManagerImpl classes
    
    * fix pre-commit failure
    
    * Added a constant for the string System
    
    * UT for searchForUsers and searchForAccounts
    
    * Fix marvin test error
    
    * Update schema to use idempotent add column
    
    * Fix `updateTemplatePermission` when the UI is set to a language other than English (#9766)
    
    * Fix updateTemplatePermission UI in non-english language
    
    * Improve fix
    
    ---------
    
    Co-authored-by: Lucas Martins <lucas.martins@scclouds.com.br>
    
    * Added user name uuid to logging
    
    * Add events when api key access is changed via api or config setting
    
    * fix the userid for api key access update event
    
    * Fix ut failure after event logging
    
    * Convert drop down to radio-button in edit user and account
    
    * Add ApiKeyAccess status in User InfoCard for Users if Api key is generated
    
    * Return apiKeyAccess in user and account response only for Root Admin
    
    * fixed noredist build failure
    
    * Show apikeyaccess on the left panel in the user view for root admins as well
    
    * don't show divider if apiKeyAccess is not shown to user
    
    * Fix events generated to set Username, Account and Domain of the caller correctly
    
    * cli changes to update user/account, list by apikeyaccess, domain level setting
    
    * UI changes for updating user/account and searchfilter in listview
    
    * make the api parameters and setting accessible only to root admin
    
    * revert changes to ui/package-lock.json
    
    * minor changes to description strings
    
    * UT for ApiServer and AccountManagerImpl classes
    
    * fix pre-commit failure
    
    * Added a constant for the string System
    
    * UT for searchForUsers and searchForAccounts
    
    * Fix marvin test error
    
    * Update schema to use idempotent add column
    
    * Added user name uuid to logging
    
    * Add events when api key access is changed via api or config setting
    
    * fix the userid for api key access update event
    
    * Fix ut failure after event logging
    
    * Convert drop down to radio-button in edit user and account
    
    * Add ApiKeyAccess status in User InfoCard for Users if Api key is generated
    
    * Return apiKeyAccess in user and account response only for Root Admin
    
    * fixed noredist build failure
    
    * Show apikeyaccess on the left panel in the user view for root admins as well
    
    * don't show divider if apiKeyAccess is not shown to user
    
    * Fix events generated to set Username, Account and Domain of the caller correctly
    
    * Added DB upgrade path from 42000 to 42010
    
    ---------
    
    Co-authored-by: Daan Hoogland <daan@onecht.net>
    Co-authored-by: Lucas Martins <56271185+lucas-a-martins@users.noreply.github.com>
    Co-authored-by: Lucas Martins <lucas.martins@scclouds.com.br>

--- server/src/main/java/com/cloud/api/query/QueryManagerImpl.java
+++ server/src/main/java/com/cloud/api/query/QueryManagerImpl.java
@@ -661,10 +661,13 @@ public class QueryManagerImpl extends MutualExclusiveIdsManagerBase implements Q
      * .api.command.admin.user.ListUsersCmd)
      */
     @Override
-    public ListResponse<UserResponse> searchForUsers(ListUsersCmd cmd) throws PermissionDeniedException {
+    public ListResponse<UserResponse> searchForUsers(ResponseView responseView, ListUsersCmd cmd) throws PermissionDeniedException {
         Pair<List<UserAccountJoinVO>, Integer> result = searchForUsersInternal(cmd);
         ListResponse<UserResponse> response = new ListResponse<UserResponse>();
-        List<UserResponse> userResponses = ViewResponseHelper.createUserResponse(CallContext.current().getCallingAccount().getDomainId(),
+        if (CallContext.current().getCallingAccount().getType() == Account.Type.ADMIN) {
+            responseView = ResponseView.Full;
+        }
+        List<UserResponse> userResponses = ViewResponseHelper.createUserResponse(responseView, CallContext.current().getCallingAccount().getDomainId(),
                 result.first().toArray(new UserAccountJoinVO[result.first().size()]));
         response.setResponses(userResponses, result.second());
         return response;
@@ -691,10 +694,10 @@ public class QueryManagerImpl extends MutualExclusiveIdsManagerBase implements Q
         Object state = null;
         String keyword = null;
 
-        Pair<List<UserAccountJoinVO>, Integer> result =  getUserListInternal(caller, permittedAccounts, listAll, id, username, type, accountName, state, keyword, domainId, recursive,
-                null);
+        Pair<List<UserAccountJoinVO>, Integer> result =  getUserListInternal(caller, permittedAccounts, listAll, id,
+                username, type, accountName, state, keyword, null, domainId, recursive, null);
         ListResponse<UserResponse> response = new ListResponse<UserResponse>();
-        List<UserResponse> userResponses = ViewResponseHelper.createUserResponse(CallContext.current().getCallingAccount().getDomainId(),
+        List<UserResponse> userResponses = ViewResponseHelper.createUserResponse(ResponseView.Restricted, CallContext.current().getCallingAccount().getDomainId(),
                 result.first().toArray(new UserAccountJoinVO[result.first().size()]));
         response.setResponses(userResponses, result.second());
         return response;
@@ -719,6 +722,7 @@ public class QueryManagerImpl extends MutualExclusiveIdsManagerBase implements Q
         String accountName = cmd.getAccountName();
         Object state = cmd.getState();
         String keyword = cmd.getKeyword();
+        String apiKeyAccess = cmd.getApiKeyAccess();
 
         Long domainId = cmd.getDomainId();
         boolean recursive = cmd.isRecursive();
@@ -727,11 +731,11 @@ public class QueryManagerImpl extends MutualExclusiveIdsManagerBase implements Q
 
         Filter searchFilter = new Filter(UserAccountJoinVO.class, "id", true, startIndex, pageSizeVal);
 
-        return getUserListInternal(caller, permittedAccounts, listAll, id, username, type, accountName, state, keyword, domainId, recursive, searchFilter);
+        return getUserListInternal(caller, permittedAccounts, listAll, id, username, type, accountName, state, keyword, apiKeyAccess, domainId, recursive, searchFilter);
     }
 
     private Pair<List<UserAccountJoinVO>, Integer> getUserListInternal(Account caller, List<Long> permittedAccounts, boolean listAll, Long id, Object username, Object type,
-            String accountName, Object state, String keyword, Long domainId, boolean recursive, Filter searchFilter) {
+            String accountName, Object state, String keyword, String apiKeyAccess, Long domainId, boolean recursive, Filter searchFilter) {
         Ternary<Long, Boolean, ListProjectResourcesCriteria> domainIdRecursiveListProject = new Ternary<Long, Boolean, ListProjectResourcesCriteria>(domainId, recursive, null);
         accountMgr.buildACLSearchParameters(caller, id, accountName, null, permittedAccounts, domainIdRecursiveListProject, listAll, false);
         domainId = domainIdRecursiveListProject.first();
@@ -757,6 +761,9 @@ public class QueryManagerImpl extends MutualExclusiveIdsManagerBase implements Q
         sb.and("domainId", sb.entity().getDomainId(), Op.EQ);
         sb.and("accountName", sb.entity().getAccountName(), Op.EQ);
         sb.and("state", sb.entity().getState(), Op.EQ);
+        if (apiKeyAccess != null) {
+            sb.and("apiKeyAccess", sb.entity().getApiKeyAccess(), Op.EQ);
+        }
 
         if ((accountName == null) && (domainId != null)) {
             sb.and("domainPath", sb.entity().getDomainPath(), Op.LIKE);
@@ -809,6 +816,15 @@ public class QueryManagerImpl extends MutualExclusiveIdsManagerBase implements Q
 
         if (state != null) {
             sc.setParameters("state", state);
+        }
+
+        if (apiKeyAccess != null) {
+            try {
+                ApiConstants.ApiKeyAccess access = ApiConstants.ApiKeyAccess.valueOf(apiKeyAccess.toUpperCase());
+                sc.setParameters("apiKeyAccess", access.toBoolean());
+            } catch (IllegalArgumentException ex) {
+                throw new InvalidParameterValueException("ApiKeyAccess value can only be Enabled/Disabled/Inherit");
+            }
         }
 
         return _userAccountJoinDao.searchAndCount(sc, searchFilter);
@@ -2897,6 +2913,7 @@ public class QueryManagerImpl extends MutualExclusiveIdsManagerBase implements Q
         Object state = cmd.getState();
         Object isCleanupRequired = cmd.isCleanupRequired();
         Object keyword = cmd.getKeyword();
+        String apiKeyAccess = cmd.getApiKeyAccess();
 
         SearchBuilder<AccountVO> accountSearchBuilder = _accountDao.createSearchBuilder();
         accountSearchBuilder.select(null, Func.DISTINCT, accountSearchBuilder.entity().getId()); // select distinct
@@ -2909,6 +2926,9 @@ public class QueryManagerImpl extends MutualExclusiveIdsManagerBase implements Q
         accountSearchBuilder.and("typeNEQ", accountSearchBuilder.entity().getType(), SearchCriteria.Op.NEQ);
         accountSearchBuilder.and("idNEQ", accountSearchBuilder.entity().getId(), SearchCriteria.Op.NEQ);
         accountSearchBuilder.and("type2NEQ", accountSearchBuilder.entity().getType(), SearchCriteria.Op.NEQ);
+        if (apiKeyAccess != null) {
+            accountSearchBuilder.and("apiKeyAccess", accountSearchBuilder.entity().getApiKeyAccess(), Op.EQ);
+        }
 
         if (domainId != null && isRecursive) {
             SearchBuilder<DomainVO> domainSearch = _domainDao.createSearchBuilder();
@@ -2969,6 +2989,15 @@ public class QueryManagerImpl extends MutualExclusiveIdsManagerBase implements Q
                 sc.setJoinParameters("domainSearch", "path", domain.getPath() + "%");
             } else {
                 sc.setParameters("domainId", domainId);
+            }
+        }
+
+        if (apiKeyAccess != null) {
+            try {
+                ApiConstants.ApiKeyAccess access = ApiConstants.ApiKeyAccess.valueOf(apiKeyAccess.toUpperCase());
+                sc.setParameters("apiKeyAccess", access.toBoolean());
+            } catch (IllegalArgumentException ex) {
+                throw new InvalidParameterValueException("ApiKeyAccess value can only be Enabled/Disabled/Inherit");
             }
         }
 
