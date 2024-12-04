39115147ed772085f28007f0110213fa9330ed0e
leo79901 <szz799@163.com>
2024-12-04 23:03:51 +0800

    Update KVMHAProvider.java

--- plugins/hypervisors/kvm/src/main/java/org/apache/cloudstack/kvm/ha/KVMHAProvider.java
+++ plugins/hypervisors/kvm/src/main/java/org/apache/cloudstack/kvm/ha/KVMHAProvider.java
@@ -89,18 +89,18 @@ public final class KVMHAProvider extends HAAbstractHostProvider implements HAPro
             if (r != null && outOfBandManagementService.isOutOfBandManagementEnabled(r)) {
                 // check host status
                 if (Host.Status.DOWN.equals(r.getStatus())) {
-                    LOG.info("Host " + r.getName() + " is already down. Returning success.");
+                    logger.info("Host " + r.getName() + " is already down. Returning success.");
                     return true;
                 } else {
                     final OutOfBandManagementResponse resp = outOfBandManagementService.executePowerOperation(r, PowerOperation.OFF, null);
                     return resp.getSuccess();
                 }
             } else {
-                LOG.warn("OOBM fence operation failed for this host " + r.getName());
+                logger.warn("OOBM fence operation failed for this host " + r.getName());
                 return false;
             }
         } catch (Exception e) {
-            LOG.warn("OOBM service is not configured or enabled for this host " + r.getName() + " error is " + e.getMessage());
+            logger.warn("OOBM service is not configured or enabled for this host " + r.getName() + " error is " + e.getMessage());
             throw new HAFenceException("OBM service is not configured or enabled for this host " + r.getName(), e);
         }
     }
