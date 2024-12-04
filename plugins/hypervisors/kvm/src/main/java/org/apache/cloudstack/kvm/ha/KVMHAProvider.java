3e8c57d8b160041b5a2ce0721455b74475993aeb
leo79901 <szz799@163.com>
2024-12-04 23:13:52 +0800

    Update KVMHAProvider.java

--- plugins/hypervisors/kvm/src/main/java/org/apache/cloudstack/kvm/ha/KVMHAProvider.java
+++ plugins/hypervisors/kvm/src/main/java/org/apache/cloudstack/kvm/ha/KVMHAProvider.java
@@ -101,7 +101,7 @@ public final class KVMHAProvider extends HAAbstractHostProvider implements HAPro
             }
         } catch (Exception e){
             logger.warn("OOBM service is not configured or enabled for this host " + r.getName() + " error is " + e.getMessage());
-            throw new HAFenceException("OBM service is not configured or enabled for this host " + r.getName(), e);
+            throw new HAFenceException("OBM service is not configured or enabled for this host " + r.getName() , e);
         }
     }
 
