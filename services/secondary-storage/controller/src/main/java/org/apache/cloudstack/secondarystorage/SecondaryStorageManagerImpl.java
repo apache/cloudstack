6a18b66e9c15ac844455ab5c45c1740b5b2df07a
Daan Hoogland <daan@onecht.net>
2025-01-09 18:19:14 +0100

    refactor cidr evaluation for internal nets

--- services/secondary-storage/controller/src/main/java/org/apache/cloudstack/secondarystorage/SecondaryStorageManagerImpl.java
+++ services/secondary-storage/controller/src/main/java/org/apache/cloudstack/secondarystorage/SecondaryStorageManagerImpl.java
@@ -402,11 +402,17 @@ public class SecondaryStorageManagerImpl extends ManagerBase implements Secondar
         }
         String[] cidrs = _allowedInternalSites.split(",");
         for (String cidr : cidrs) {
-            if (NetUtils.isValidIp4Cidr(cidr) || NetUtils.isValidIp4(cidr) || !cidr.startsWith("0.0.0.0")) {
-                if (NetUtils.getCleanIp4Cidr(cidr).equals(cidr)) {
+            if (NetUtils.isValidIp4Cidr(cidr)) {
+                if (! NetUtils.getCleanIp4Cidr(cidr).equals(cidr)) {
                     s_logger.warn(String.format("Invalid CIDR %s in %s", cidr, SecStorageAllowedInternalDownloadSites.key()));
                 }
-                allowedCidrs.add(cidr);
+                allowedCidrs.add(NetUtils.getCleanIp4Cidr(cidr));
+            } else if (NetUtils.isValidIp4(cidr)) {
+                String newCidr = cidr + "/32";
+                s_logger.warn(String.format("Ip address is not a valid CIDR; %s using %s/32", cidr, newCidr));
+                allowedCidrs.add(newCidr);
+            } else if (!cidr.startsWith("0.0.0.0")) {
+                allowedCidrs.add(NetUtils.getCleanIp4Cidr(cidr));
             }
         }
         return allowedCidrs;
