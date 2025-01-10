f36283e99f3d1c26fafbf110258ea7580a14a279
Pierre Charton <p.charton@dimsi.fr>
2025-01-10 10:49:58 +0100

    remove of build_backroll_plugin.sh

--- build_backroll_plugin.sh
+++
@@ -1,1 +0,0 @@
-mvn install -f "/opt/cloudstack/plugins/backup/backroll/pom.xml" -DskipTests && mvn install -f "/opt/cloudstack/plugins/pom.xml" -DskipTests && mvn install -f "/opt/cloudstack/client/pom.xml" -DskipTests && mvn -pl :cloud-client-ui jetty:run 
