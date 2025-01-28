8d61f77838572283e4545dbe762c3e1f46ca65ae
Abhishek Kumar <abhishek.mrt22@gmail.com>
2025-01-28 17:04:08 +0530

    ui: multi-arch improvements
    Signed-off-by: Abhishek Kumar <abhishek.mrt22@gmail.com>

--- ui/src/config/section/image.js
+++ ui/src/config/section/image.js
@@ -42,7 +42,7 @@ export default {
               }
               return 'Not Ready'
             }
-          }, 'ostypename', 'hypervisor']
+          }, 'ostypename', 'arch', 'hypervisor']
         if (['Admin', 'DomainAdmin'].includes(store.getters.userInfo.roletype)) {
           fields.push('size')
           fields.push('account')
@@ -218,7 +218,7 @@ export default {
               }
               return 'Not Ready'
             }
-          }, 'ostypename']
+          }, 'ostypename', 'arch']
         if (['Admin', 'DomainAdmin'].includes(store.getters.userInfo.roletype)) {
           fields.push('size')
           fields.push('account')
