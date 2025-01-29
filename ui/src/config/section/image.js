13f3f148a295f06d25e42260430353cc1bf15b20
Abhishek Kumar <abhishek.mrt22@gmail.com>
2025-01-29 13:07:34 +0530

    ui: add arch display and filter for kubernetes iso
    Signed-off-by: Abhishek Kumar <abhishek.mrt22@gmail.com>

--- ui/src/config/section/image.js
+++ ui/src/config/section/image.js
@@ -370,8 +370,8 @@ export default {
       icon: ['fa-solid', 'fa-dharmachakra'],
       docHelp: 'plugins/cloudstack-kubernetes-service.html#kubernetes-supported-versions',
       permission: ['listKubernetesSupportedVersions'],
-      searchFilters: ['zoneid', 'minimumsemanticversion'],
-      columns: ['name', 'state', 'semanticversion', 'isostate', 'mincpunumber', 'minmemory', 'zonename'],
+      searchFilters: ['zoneid', 'minimumsemanticversion', 'arch'],
+      columns: ['name', 'state', 'semanticversion', 'isostate', 'mincpunumber', 'minmemory', 'arch', 'zonename'],
       details: ['name', 'semanticversion', 'supportsautoscaling', 'zoneid', 'zonename', 'isoid', 'isoname', 'isostate', 'mincpunumber', 'minmemory', 'supportsha', 'state', 'created'],
       tabs: [
         {
