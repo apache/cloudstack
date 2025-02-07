03c3c18ae3289edf8c77d5212fe34e8ba58270b2
Lucas Martins <lucas.martins@scclouds.com.br>
2025-02-07 17:28:11 -0300

    Change size column order

--- ui/src/config/section/storage.js
+++ ui/src/config/section/storage.js
@@ -38,7 +38,7 @@ export default {
         }
       },
       columns: () => {
-        const fields = ['name', 'state', 'type', 'vmname', 'vmstate', 'sizegb']
+        const fields = ['name', 'state', 'sizegb', 'type', 'vmname', 'vmstate']
         const metricsFields = ['diskkbsread', 'diskkbswrite', 'diskiopstotal']
 
         if (store.getters.userInfo.roletype === 'Admin') {
