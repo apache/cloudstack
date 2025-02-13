6fa3fe3ad35572222114024eac7bd1e2a1bc2e25
Pearl Dsilva <pearl1594@gmail.com>
2025-02-13 09:56:02 -0500

    get cpu archs only if there are suitable hosts

--- server/src/main/java/com/cloud/server/ManagementServerImpl.java
+++ server/src/main/java/com/cloud/server/ManagementServerImpl.java
@@ -1614,9 +1614,11 @@ public class ManagementServerImpl extends ManagerBase implements ManagementServe
         }
 
         // Only list hosts of the same architecture as the source Host in a multi-arch zone
-        List<CPU.CPUArch> clusterArchs = ApiDBUtils.listZoneClustersArchs(vm.getDataCenterId());
-        if (CollectionUtils.isNotEmpty(clusterArchs) && clusterArchs.size() > 1 && !suitableHosts.isEmpty()) {
-            suitableHosts = suitableHosts.stream().filter(h -> h.getArch() == srcHost.getArch()).collect(Collectors.toList());
+        if (!suitableHosts.isEmpty()) {
+            List<CPU.CPUArch> clusterArchs = ApiDBUtils.listZoneClustersArchs(vm.getDataCenterId());
+            if (CollectionUtils.isNotEmpty(clusterArchs) && clusterArchs.size() > 1) {
+                suitableHosts = suitableHosts.stream().filter(h -> h.getArch() == srcHost.getArch()).collect(Collectors.toList());
+            }
         }
 
         return new Ternary<>(otherHosts, suitableHosts, requiresStorageMotion);
