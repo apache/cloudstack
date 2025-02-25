5526ef0168cff65535e5a7eef1c580f52f1f4254
Daan Hoogland <daan@onecht.net>
2025-02-25 10:51:46 +0100

    spurious versions

--- tools/marvin/setup.py
+++ tools/marvin/setup.py
@@ -27,7 +27,7 @@ except ImportError:
         raise RuntimeError("python setuptools is required to build Marvin")
 
 
-VERSION = "4.19.2.0"
+VERSION = "4.19.3.0-SNAPSHOT"
 
 setup(name="Marvin",
       version=VERSION,
