055c76f6d54a2fa037608600b718c5f06bcf7f0e
Vishesh <vishesh92@gmail.com>
2025-03-04 12:17:28 +0530

    Support ConfigDrive with VPC

--- server/src/main/java/com/cloud/network/element/ConfigDriveNetworkElement.java
+++ server/src/main/java/com/cloud/network/element/ConfigDriveNetworkElement.java
@@ -203,8 +203,8 @@ public class ConfigDriveNetworkElement extends AdapterBase implements NetworkEle
     private static Map<Service, Map<Capability, String>> setCapabilities() {
         Map<Service, Map<Capability, String>> capabilities = new HashMap<>();
         capabilities.put(Service.UserData, null);
-        capabilities.put(Service.Dhcp, new HashMap<>());
-        capabilities.put(Service.Dns, new HashMap<>());
+        capabilities.put(Service.Dhcp, Map.of(Network.Capability.DhcpAccrossMultipleSubnets, "true"));
+        capabilities.put(Service.Dns, Map.of(Capability.AllowDnsSuffixModification, "true"));
         return capabilities;
     }
 
