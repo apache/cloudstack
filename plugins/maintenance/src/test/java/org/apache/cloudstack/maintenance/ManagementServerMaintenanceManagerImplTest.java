1e5fe0271a35e355a284a61553c64bb350507659
Suresh Kumar Anaparti <suresh.anaparti@shapeblue.com>
2025-03-12 11:14:00 +0530

    testcase fix

--- plugins/maintenance/src/test/java/org/apache/cloudstack/maintenance/ManagementServerMaintenanceManagerImplTest.java
+++ plugins/maintenance/src/test/java/org/apache/cloudstack/maintenance/ManagementServerMaintenanceManagerImplTest.java
@@ -114,7 +114,7 @@ public class ManagementServerMaintenanceManagerImplTest {
     @Test
     public void triggerShutdown() {
         Mockito.doNothing().when(jobManagerMock).disableAsyncJobs();
-        Mockito.when(spy.isShutdownTriggered()).thenReturn(false);
+        Mockito.lenient().when(spy.isShutdownTriggered()).thenReturn(false);
         spy.triggerShutdown();
         Mockito.verify(jobManagerMock).disableAsyncJobs();
 
