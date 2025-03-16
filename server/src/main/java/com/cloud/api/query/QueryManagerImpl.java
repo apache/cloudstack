c9c0da0889990a3e7089d2f1f7e04e551032e77c
Harikrishna Patnala <harikrishna.patnala@gmail.com>
2025-03-16 08:05:10 +0530

    working env

--- server/src/main/java/com/cloud/api/query/QueryManagerImpl.java
+++ server/src/main/java/com/cloud/api/query/QueryManagerImpl.java
@@ -4614,9 +4614,7 @@ public class QueryManagerImpl extends MutualExclusiveIdsManagerBase implements Q
         List<HypervisorType> hypers = null;
         if (!isIso) {
             hypers = _resourceMgr.listAvailHypervisorInZone(null);
-            if (hypers == null || hypers.isEmpty()) {
-                return new Pair<>(new ArrayList<>(), 0);
-            }
+            hypers.add(HypervisorType.External);
         }
 
         VMTemplateVO template;
