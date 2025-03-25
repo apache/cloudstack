6aa691c3da3d2ee18d90f116f1621f0eadf8ea37
Imvedansh <veds17007@gmail.com>
2025-03-25 22:14:08 +0530

    vpc fill gateway and netmask

--- ui/src/views/network/VpcTiersTab.vue
+++ ui/src/views/network/VpcTiersTab.vue
@@ -617,6 +617,43 @@ export default {
       this.initForm()
       this.fetchNetworkAclList()
       this.fetchNetworkOfferings()
+      const cidr = this.resource.cidr
+      if (cidr && cidr.includes('/')) {
+        const [address, maskBits] = cidr.split('/')
+        this.form.gateway = address
+
+        const prefix = Number(maskBits)
+
+        const subnetMasks = {
+          8: '255.0.0.0',
+          9: '255.128.0.0',
+          10: '255.192.0.0',
+          11: '255.224.0.0',
+          12: '255.240.0.0',
+          13: '255.248.0.0',
+          14: '255.252.0.0',
+          15: '255.254.0.0',
+          16: '255.255.0.0',
+          17: '255.255.128.0',
+          18: '255.255.192.0',
+          19: '255.255.224.0',
+          20: '255.255.240.0',
+          21: '255.255.248.0',
+          22: '255.255.252.0',
+          23: '255.255.254.0',
+          24: '255.255.255.0',
+          25: '255.255.255.128',
+          26: '255.255.255.192',
+          27: '255.255.255.224',
+          28: '255.255.255.240',
+          29: '255.255.255.248',
+          30: '255.255.255.252',
+          31: '255.255.255.254',
+          32: '255.255.255.255'
+        }
+
+        this.form.netmask = subnetMasks[prefix] || '255.255.255.0'
+      }
       this.showCreateNetworkModal = true
       this.rules = {
         name: [{ required: true, message: this.$t('label.required') }],
