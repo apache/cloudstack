32fc7f8228d6197e17697fa08e9f0e403c909c10
Harikrishna Patnala <harikrishna.patnala@gmail.com>
2025-03-27 17:37:11 +0530

    Fix Map and String

--- api/src/main/java/org/apache/cloudstack/api/command/admin/host/AddHostCmd.java
+++ api/src/main/java/org/apache/cloudstack/api/command/admin/host/AddHostCmd.java
@@ -155,27 +155,27 @@ public class AddHostCmd extends BaseCmd {
     }
 
     public Map<String, String> getExternalDetails() {
-        boolean isstringmap = true;
-        if (externaldetails instanceof map<?, ?>) {
-            for (map.entry<?, ?> entry : ((map<?, ?>) externaldetails).entryset()) {
-                if (!(entry.getkey() instanceof string) || !(entry.getvalue() instanceof string)) {
-                    isstringmap = false;
+        boolean isStringMap = true;
+        if (externalDetails instanceof Map<?, ?>) {
+            for (Map.Entry<?, ?> entry : ((Map<?, ?>) externalDetails).entrySet()) {
+                if (!(entry.getKey() instanceof String) || !(entry.getValue() instanceof String)) {
+                    isStringMap = false;
                     break;
                 }
             }
         }
 
-        if (!isstringmap) {
-            map<string, string> customparametermap = convertdetailstomap(externaldetails);
-            map<string, string> details = new hashmap<>();
-            for (string key : customparametermap.keyset()) {
-                string value = customparametermap.get(key);
-                details.put(vmdetailconstants.external_detail_prefix + key, value);
+        if (!isStringMap) {
+            Map<String, String> customParameterMap = convertDetailsToMap(externalDetails);
+            Map<String, String> details = new HashMap<>();
+            for (String key : customParameterMap.keySet()) {
+                String value = customParameterMap.get(key);
+                details.put(VmDetailConstants.EXTERNAL_DETAIL_PREFIX + key, value);
             }
             return details;
         }
 
-        return externaldetails;
+        return (Map<String, String>) externalDetails;
     }
 
     /////////////////////////////////////////////////////
