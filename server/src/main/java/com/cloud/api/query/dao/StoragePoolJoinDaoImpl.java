45aa0dfd3f7f817382a03ee114f37d229f7e8f7f
Suresh Kumar Anaparti <suresh.anaparti@shapeblue.com>
2025-04-16 16:23:17 +0530

    Return details of the storage pool in the response including url, and update capacityBytes and capacityIops if applicable while creating storage pool

--- server/src/main/java/com/cloud/api/query/dao/StoragePoolJoinDaoImpl.java
+++ server/src/main/java/com/cloud/api/query/dao/StoragePoolJoinDaoImpl.java
@@ -40,6 +40,7 @@ import org.springframework.stereotype.Component;
 import com.cloud.api.ApiDBUtils;
 import com.cloud.api.query.vo.StoragePoolJoinVO;
 import com.cloud.capacity.CapacityManager;
+import com.cloud.server.ResourceTag;
 import com.cloud.storage.DataStoreRole;
 import com.cloud.storage.ScopeType;
 import com.cloud.storage.Storage;
@@ -180,6 +181,8 @@ public class StoragePoolJoinDaoImpl extends GenericDaoBase<StoragePoolJoinVO, Lo
         poolResponse.setIsTagARule(pool.getIsTagARule());
         poolResponse.setOverProvisionFactor(Double.toString(CapacityManager.StorageOverprovisioningFactor.valueIn(pool.getId())));
         poolResponse.setManaged(storagePool.isManaged());
+        Map<String, String> details = ApiDBUtils.getResourceDetails(pool.getId(), ResourceTag.ResourceObjectType.Storage);
+        poolResponse.setDetails(details);
 
         // set async job
         if (pool.getJobId() != null) {
