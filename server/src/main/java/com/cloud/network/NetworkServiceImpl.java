db06bb7b29feadd71fa15d0759be4f0f988e593d
Wei Zhou <weizhou@apache.org>
2025-04-22 08:31:32 +0200

    Update server/src/main/java/com/cloud/network/NetworkServiceImpl.java

--- server/src/main/java/com/cloud/network/NetworkServiceImpl.java
+++ server/src/main/java/com/cloud/network/NetworkServiceImpl.java
@@ -1645,7 +1645,7 @@ public class NetworkServiceImpl extends ManagerBase implements NetworkService, C
                 throw new CloudRuntimeException("IPv4 address range needs to be provided");
             }
             if (ipv6) {
-                logger.info(String.format("ip range for network '%s' is specified as %s - %s", name, startIPv6, endIPv6));
+                s_logger.info(String.format("ip range for network '%s' is specified as %s - %s", name, startIPv6, endIPv6));
             }
         }
         Pair<Integer, Integer> interfaceMTUs = validateMtuConfig(publicMtu, privateMtu, zone.getId());
