a05caf7452914b8d40e3ef3ac5acebf39f325a24
Abhishek Kumar <abhishek.mrt22@gmail.com>
2025-05-09 14:13:16 +0530

    ui: add an infinite scroll select component
    A select component which can retrieve more items from the API when scoll
    reaches at the end. It will allow selecting options in case of large
    number of items.
    
    Signed-off-by: Abhishek Kumar <abhishek.mrt22@gmail.com>

---
+++ ui/src/components/widgets/InfiniteScrollSelect.vue
@@ -0,0 +1,137 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+<template>
+  <a-select
+    :filter-option="false"
+    :loading="loading"
+    show-search
+    placeholder="Select"
+    @search="onSearch"
+    @popupScroll="onScroll"
+    @change="onChange"
+  >
+    <a-select-option v-for="option in options" :key="option.id" :value="option.id">
+      <span>
+        <resource-icon v-if="option.icon && option.icon.base64image" :option="option.icon.base64image" size="1x" style="margin-right: 5px"/>
+        <render-icon :icon="defaultIcon" style="margin-right: 5px" />
+        <span>{{ option.name }}</span>
+      </span>
+    </a-select-option>
+  </a-select>
+</template>
+
+<script>
+import { api } from '@/api'
+import ResourceIcon from '@/components/view/ResourceIcon'
+
+export default {
+  name: 'InfiniteScrollSelect',
+  components: {
+    ResourceIcon
+  },
+  props: {
+    api: {
+      type: String,
+      required: true
+    },
+    resourceType: {
+      type: String,
+      required: true
+    },
+    defaultOption: {
+      type: Object,
+      default: null
+    },
+    defaultIcon: {
+      type: String,
+      default: 'cloud-outlined'
+    }
+  },
+  data () {
+    return {
+      options: [],
+      page: 1,
+      pageSize: 50,
+      totalCount: null,
+      loading: false,
+      searchQuery: '',
+      scrollHandlerAttached: false
+    }
+  },
+  created () {
+    this.addDefaultOptionIfNeeded(true)
+  },
+  emits: ['change-option'],
+  methods: {
+    async fetchItems () {
+      if (this.loading) return
+      this.loading = true
+      const params = {
+        page: this.page,
+        pagesize: this.pageSize,
+        keyword: this.searchQuery,
+        listall: true
+      }
+      api(this.api, params).then(json => {
+        const response = json[this.api.toLowerCase() + 'response'] || {}
+        if (this.totalCount === null) {
+          this.totalCount = response.count || 0
+        }
+        const newOpts = response[this.resourceType] || []
+        this.options = this.options.concat(newOpts)
+        this.page++
+      }).catch(error => {
+        this.$notifyError(error)
+      }).finally(() => {
+        this.loading = false
+      })
+    },
+    addDefaultOptionIfNeeded () {
+      if (this.defaultOption) {
+        this.options.push(this.defaultOption)
+      }
+    },
+    onSearch (value) {
+      this.searchQuery = value
+      this.page = 1
+      this.totalCount = null
+      this.options = []
+      if (!this.searchQuery) {
+        this.addDefaultOptionIfNeeded()
+      }
+      this.fetchItems()
+    },
+    onScroll (e) {
+      const nearBottom = e.target.scrollTop + e.target.clientHeight >= e.target.scrollHeight - 10
+      const hasMore = this.options.length < this.totalCount
+      if (nearBottom && hasMore && !this.loading) {
+        this.fetchItems()
+      }
+    },
+    onChange (id) {
+      const match = this.options.find(entry => entry.id === id)
+      if (match) {
+        this.$emit('change-option', match)
+      }
+    }
+  },
+  mounted () {
+    this.fetchItems()
+  }
+}
+</script>
