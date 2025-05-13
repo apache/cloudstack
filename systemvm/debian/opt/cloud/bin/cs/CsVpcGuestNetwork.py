ae99f249ac8828f99f87fa6ba3bcf37c23292d51
Nicolas Vazquez <nicovazquez90@gmail.com>
2025-05-13 20:41:40 -0300

    [VR] Fix IPv6 NIC IP on the VR

--- systemvm/debian/opt/cloud/bin/cs/CsVpcGuestNetwork.py
+++ systemvm/debian/opt/cloud/bin/cs/CsVpcGuestNetwork.py
@@ -58,6 +58,8 @@ class CsVpcGuestNetwork(CsDataBag):
             self.enable_ipv6(entry['device'])
             cidr_size = entry['router_guest_ip6_cidr'].split("/")[-1]
             full_addr = entry['router_guest_ip6_gateway'] + "/" + cidr_size
+            if entry['router_guest_ip6'] is not entry['router_guest_ip6_gateway']:
+                full_addr = entry['router_guest_ip6'] + "/" + cidr_size
             if not CsHelper.execute("ip -6 addr show dev %s | grep -w %s" % (entry['device'], full_addr)):
                 CsHelper.execute("ip -6 addr add %s dev %s" % (full_addr, entry['device']))
             if 'router_ip6' in list(entry.keys()) and entry['router_ip6']:
@@ -74,6 +76,8 @@ class CsVpcGuestNetwork(CsDataBag):
         if 'router_guest_ip6' in list(entry.keys()) and entry['router_guest_ip6']:
             cidr_size = entry['router_guest_ip6_cidr'].split("/")[-1]
             full_addr = entry['router_guest_ip6_gateway'] + "/" + cidr_size
+            if entry['router_guest_ip6'] is not entry['router_guest_ip6_gateway']:
+                full_addr = entry['router_guest_ip6'] + "/" + cidr_size
             CsHelper.execute("ip -6 addr del %s dev %s" % (full_addr, entry['device']))
             if 'router_ip6' in list(entry.keys()) and entry['router_ip6']:
                 full_public_addr = entry['router_ip6'] + "/" + cidr_size
@@ -98,6 +102,8 @@ class CsVpcGuestNetwork(CsDataBag):
         if 'router_guest_ip6' in list(entry.keys()) and entry['router_guest_ip6']:
             cidr_size = entry['router_guest_ip6_cidr'].split("/")[-1]
             full_addr = entry['router_guest_ip6_gateway'] + "/" + cidr_size
+            if entry['router_guest_ip6'] is not entry['router_guest_ip6_gateway']:
+                full_addr = entry['router_guest_ip6_cidr']
             self.conf.append("interface %s" % entry['device'])
             self.conf.append("{")
             self.conf.append("    AdvSendAdvert on;")
