9629f7d942fe1f0baa98704d9a0a5ba6f54473dc
Abhisar Sinha <63767682+abh1sar@users.noreply.github.com>
2025-05-20 09:58:25 +0530

    Update engine/schema/src/main/java/com/cloud/upgrade/dao/DbUpgrade.java
    Co-authored-by: João Jandre <48719461+JoaoJandre@users.noreply.github.com>

--- engine/schema/src/main/java/com/cloud/upgrade/dao/DbUpgrade.java
+++ engine/schema/src/main/java/com/cloud/upgrade/dao/DbUpgrade.java
@@ -39,5 +39,7 @@ public interface DbUpgrade {
 
     InputStream[] getCleanupScripts();
 
-    default boolean refreshPoolConnectionsAfterUpgrade() { return false; }
+    default boolean refreshPoolConnectionsAfterUpgrade() { 
+        return false; 
+    }
 }
