785dbf4f342ccefe391c66c604a10c366db278a4
Rohit Yadav <rohityadav89@gmail.com>
2025-05-27 12:00:39 +0530

    Update server/src/main/java/org/apache/cloudstack/vm/lease/VMLeaseManager.java
    Co-authored-by: Vishesh <vishesh92@gmail.com>

--- server/src/main/java/org/apache/cloudstack/vm/lease/VMLeaseManager.java
+++ server/src/main/java/org/apache/cloudstack/vm/lease/VMLeaseManager.java
@@ -44,7 +44,7 @@ public interface VMLeaseManager extends Manager {
 
     ConfigKey<Boolean> InstanceLeaseEnabled = new ConfigKey<>(ConfigKey.CATEGORY_ADVANCED, Boolean.class,
             "instance.lease.enabled", "false", "Indicates whether to enable the Instance lease," +
-            " will be applicable only on instances created after lease is enabled. Disabling the feature cancels lease on existing instances with lease." +
+            " will be applicable only on instances created after lease is enabled. Disabling the feature cancels lease on existing instances with lease. " +
             "Re-enabling feature will not cause lease expiry actions on grandfathered instances",
             true, List.of(ConfigKey.Scope.Global));
 
