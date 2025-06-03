204bc28b7f1877c59b2cc54fe8d6a55f91381e99
Abhishek Kumar <abhishek.mrt22@gmail.com>
2025-06-03 15:22:50 +0530

    fix
    Signed-off-by: Abhishek Kumar <abhishek.mrt22@gmail.com>

--- framework/extensions/src/main/java/org/apache/cloudstack/framework/extensions/manager/ExtensionsManagerImpl.java
+++ framework/extensions/src/main/java/org/apache/cloudstack/framework/extensions/manager/ExtensionsManagerImpl.java
@@ -30,7 +30,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import javax.inject.Inject;
