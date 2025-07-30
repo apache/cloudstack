apache/cloudstack

commit bce17b627d1cc186c3be5257e529cce384dfaa9f

ui,api,server: template categorization based on os (#10773)
Adds new interface for image selection (template/iso) for an instance in UI.
Old interface can still be used and it can be configured using UI configuration (config.json)

OS categories/Guest OS categories have been improved with ability to create new categories, delete an existing category, and marking a category as featured to allow it to show up in the UI in the image selection interface.

New APIs added:
- addOsCategory
- deleteOsCategory
- updateOsCategory

APIs updated:
- updateOsType
- listTemplates
- listOsCategories

Several improvements in UI especially related to forms - DeloyVM, ReinstallVM, CreateVnfAppliance, AddAutoscaleGroup.

DeployVM form can now be opened from template/ISO details view with query params.

Reorganized (removed and added some) OS categories to the following (in the same order):
```
1. Ubuntu
2. Debian
3. Fedora
4. CentOS
5. Rocky Linux
6. Alma Linux
7. Oracle
8. RedHat
9. SUSE
10. Windows
11. Other
```

Documentation PR: https://github.com/apache/cloudstack-documentation/pull/500

Signed-off-by: Abhishek Kumar <abhishek.mrt22@gmail.com>

ui/src/views/compute/ReinstallVm.vue
@@ -19,28 +19,35 @@
   <a-form
     v-ctrl-enter="handleSubmit"
     @finish="handleSubmit"
-    layout="vertical"
-  >
-    <a-alert
-      type="warning"
-      show-icon
-    >
-      <template #message><span
-          style="margin-bottom: 5px"
-          v-html="$t('message.reinstall.vm')"
-        /></template>
+    layout="vertical">
+    <a-alert type="warning" show-icon>
+      <template #message>
+        <span style="margin-bottom: 5px" v-html="$t('message.reinstall.vm')" />
+      </template>
     </a-alert>
-    <a-form-item>
+    <os-based-image-selection
+      v-if="isModernImageSelection"
+      :imageTypeSelectionAllowed="false"
+      :guestOsCategories="options.guestOsCategories"
+      :guestOsCategoriesLoading="loading.guestOsCategories"
+      :selectedGuestOsCategoryId="selectedGuestOsCategoryId"
+      :imageItems="options.templates"
+      :imagesLoading="loading.templates"
+      :filterOption="filterOption"
+      :preFillContent="dataPreFill"
+      @change-guest-os-category="onSelectGuestOsCategory"
+      @handle-image-search-filter="($event) => fetchAllTemplates($event)"
+      @update-image="updateFieldValue" />
+    <a-form-item v-else>
       <template-iso-selection
         input-decorator="templateid"
-        :items="templates"
+        :items="options.templates"
         :selected="tabKey"
         :loading="loading.templates"
-        :preFillContent="dataPrefill"
+        :preFillContent="dataPreFill"
         :key="templateKey"
         @handle-search-filter="($event) => fetchAllTemplates($event)"
-        @update-template-iso="updateFieldValue"
-      />
+        @update-template-iso="updateFieldValue" />
     </a-form-item>
     <a-form-item>
       <template #label>
@@ -56,12 +63,12 @@
     </a-form-item>
     <a-form-item v-if="overrideDiskOffering">
       <disk-offering-selection
-        :items="diskOfferings"
-        :row-count="diskOfferingCount"
+        :items="options.diskOfferings"
+        :row-count="count.diskOfferings"
         :zoneId="resource.zoneId"
         :value="diskOffering ? diskOffering.id : ''"
         :loading="loading.diskOfferings"
-        :preFillContent="dataPrefill"
+        :preFillContent="dataPreFill"
         :isIsoSelected="false"
         :isRootDiskOffering="true"
         @on-selected-disk-size="onSelectDiskSize"
@@ -127,6 +134,7 @@
 import { api } from '@/api'
 import DiskOfferingSelection from '@views/compute/wizard/DiskOfferingSelection'
 import DiskSizeSelection from '@views/compute/wizard/DiskSizeSelection'
+import OsBasedImageSelection from '@views/compute/wizard/OsBasedImageSelection'
 import TemplateIsoSelection from '@views/compute/wizard/TemplateIsoSelection'
 import TooltipLabel from '@/components/widgets/TooltipLabel'
 import _ from 'lodash'
@@ -137,6 +145,7 @@ export default {
     DiskOfferingSelection,
     DiskSizeSelection,
     TemplateIsoSelection,
+    OsBasedImageSelection,
     TooltipLabel
   },
   props: {
@@ -151,10 +160,19 @@ export default {
       overrideDiskOffering: false,
       overrideDiskSize: false,
       expungeDisk: false,
-      selectedDiskOffering: {},
+      selectedGuestOsCategoryId: null,
+      options: {
+        templates: {},
+        diskOfferings: [],
+        guestOsCategories: []
+      },
       loading: {
         templates: false,
-        diskOfferings: false
+        diskOfferings: false,
+        guestOsCategories: false
+      },
+      count: {
+        diskOfferings: 0
       },
       rootDiskSizeKey: 'details[0].rootdisksize',
       minIopsKey: 'details[0].minIops',
@@ -162,16 +180,11 @@ export default {
       rootdisksize: 0,
       minIops: 0,
       maxIops: 0,
-      templateFilter: [
-        'featured',
-        'community',
-        'selfexecutable',
-        'sharedexecutable'
-      ],
       diskOffering: {},
-      diskOfferingCount: 0,
+      imageSearchFilters: null,
+      templateid: null,
       templateKey: 0,
-      dataPrefill: {
+      dataPreFill: {
         templateid: this.resource.templateid,
         diskofferingid: this.resource.diskofferingid
       }
@@ -183,10 +196,52 @@ export default {
   created () {
     this.fetchData()
   },
+  computed: {
+    isNormalAndDomainUser () {
+      return ['DomainAdmin', 'User'].includes(this.$store.getters.userInfo.roletype)
+    },
+    isModernImageSelection () {
+      return this.$config.imageSelectionInterface === undefined || this.$config.imageSelectionInterface === 'modern'
+    },
+    imageSelection () {
+      return this.isModernImageSelection ? 'modern' : 'legacy'
+    },
+    showUserCategoryForModernImageSelection () {
+      return this.$config.showUserCategoryForModernImageSelection === undefined || this.$config.showUserCategoryForModernImageSelection
+    },
+    showAllCategoryForModernImageSelection () {
+      return this.$config.showAllCategoryForModernImageSelection
+    }
+  },
   methods: {
     fetchData () {
       this.fetchDiskOfferings({})
-      this.fetchAllTemplates()
+      if (this.isModernImageSelection) {
+        this.fetchGuestOsCategories()
+      } else {
+        this.fetchAllTemplates()
+      }
+    },
+    getImageFilters (params, forReset) {
+      if (this.isModernImageSelection) {
+        if (this.selectedGuestOsCategoryId === '0') {
+          return ['self']
+        }
+        if (this.isModernImageSelection && params && !forReset) {
+          if (params.featured) {
+            return ['featured']
+          } else if (params.public) {
+            return ['community']
+          }
+        }
+        return this.isNormalAndDomainUser ? ['executable'] : ['all']
+      }
+      return [
+        'featured',
+        'community',
+        'selfexecutable',
+        'sharedexecutable'
+      ]
     },
     closeAction () {
       this.$emit('close-action')
@@ -244,19 +299,56 @@ export default {
         this.closeAction()
       })
     },
+    fetchGuestOsCategories () {
+      this.loading.guestOsCategories = true
+      const params = {
+        zoneid: this.resource.zoneid,
+        arch: this.resource.arch,
+        isiso: false,
+        featured: true,
+        showicon: true
+      }
+      api('listOsCategories', params).then((response) => {
+        this.options.guestOsCategories = response?.listoscategoriesresponse?.oscategory || []
+        if (this.showUserCategoryForModernImageSelection) {
+          const userCategory = {
+            id: '0',
+            name: this.$t('label.user')
+          }
+          if (this.$store.getters.avatar) {
+            userCategory.icon = {
+              base64image: this.$store.getters.avatar
+            }
+          }
+          this.options.guestOsCategories.push(userCategory)
+        }
+        if (this.showAllCategoryForModernImageSelection) {
+          this.options.guestOsCategories.push({
+            id: '-1',
+            name: this.$t('label.all')
+          })
+        }
+        this.selectedGuestOsCategoryId = this.options.guestOsCategories[0].id
+      }).finally(() => {
+        this.loading.guestOsCategories = false
+        this.fetchAllTemplates()
+      })
+    },
     fetchAllTemplates (params) {
       const promises = []
       const templates = {}
       this.loading.templates = true
-      this.templateFilter.forEach((filter) => {
+      this.imageSearchFilters = params
+      const templateFilters = this.getImageFilters(params)
+      templateFilters.forEach((filter) => {
         templates[filter] = { count: 0, template: [] }
         promises.push(this.fetchTemplates(filter, params))
       })
-      this.templates = templates
+      this.options.templates = templates
       Promise.all(promises).then((response) => {
         response.forEach((resItem, idx) => {
-          templates[this.templateFilter[idx]] = _.isEmpty(resItem.listtemplatesresponse) ? { count: 0, template: [] } : resItem.listtemplatesresponse
-          this.templates = { ...templates }
+          templates[templateFilters[idx]] = _.isEmpty(resItem.listtemplatesresponse) ? { count: 0, template: [] } : resItem.listtemplatesresponse
+          this.options.templates = { ...templates }
         })
       }).catch((reason) => {
         console.log(reason)
@@ -266,12 +358,18 @@ export default {
     },
     fetchTemplates (templateFilter, params) {
       const args = Object.assign({}, params)
-      if (args.keyword || args.category !== templateFilter) {
+      if (this.isModernImageSelection && this.selectedGuestOsCategoryId && !['-1', '0'].includes(this.selectedGuestOsCategoryId)) {
+        args.oscategoryid = this.selectedGuestOsCategoryId
+      }
+      if (args.keyword || (args.category && args.category !== templateFilter)) {
         args.page = 1
         args.pageSize = args.pageSize || 10
       }
-      args.zoneid = _.get(this.zone, 'id')
+      args.zoneid = this.resource.zoneid
       args.templatefilter = templateFilter
+      if (this.resource.arch) {
+        args.arch = this.resource.arch
+      }
       args.details = 'all'
       args.showicon = 'true'
 
@@ -285,17 +383,24 @@ export default {
     },
     fetchDiskOfferings (params) {
       api('listDiskOfferings', { zoneid: this.resource.zoneid, listall: true, ...params }).then((response) => {
-        this.diskOfferings = response?.listdiskofferingsresponse?.diskoffering || []
-        this.diskOfferingCount = response?.listdiskofferingsresponse?.count || 0
+        this.options.diskOfferings = response?.listdiskofferingsresponse?.diskoffering || []
+        this.count.diskOfferings = response?.listdiskofferingsresponse?.count || 0
       })
+    },
+    onSelectGuestOsCategory (value) {
+      this.selectedGuestOsCategoryId = value
+      this.fetchAllTemplates(this.imageSearchFilters)
     },
     onSelectDiskSize (rowSelected) {
       this.diskOffering = rowSelected
-      this.dataPrefill.diskofferingid = rowSelected.id
+      this.dataPreFill.diskofferingid = rowSelected.id
     },
     updateFieldValue (input, value) {
       this[input] = value
-      this.dataPrefill[input] = value
+      this.dataPreFill[input] = value
+    },
+    filterOption (input, option) {
+      return option.label.toUpperCase().indexOf(input.toUpperCase()) >= 0
     }
   }
 }

commit fa79d0ec01395866ec0458ff5c474b2106889091

ui: fix initial pagination for images in deploy forms
Regression from #10773

Signed-off-by: Abhishek Kumar <abhishek.mrt22@gmail.com>

ui/src/views/compute/CreateAutoScaleVmGroup.vue
@@ -2980,10 +2980,10 @@ export default {
       if (this.isModernImageSelection && this.form.guestoscategoryid && !['-1', '0'].includes(this.form.guestoscategoryid)) {
         args.oscategoryid = this.form.guestoscategoryid
       }
-      if (args.keyword || (args.category && args.category !== templateFilter)) {
+      if (!args.page || args.keyword || (args.category && args.category !== templateFilter)) {
         args.page = 1
-        args.pageSize = args.pageSize || 10
       }
+      args.pageSize = args.pageSize || 10
       args.zoneid = _.get(this.zone, 'id')
       if (this.isZoneSelectedMultiArch) {
         args.arch = this.selectedArchitecture

ui/src/views/compute/DeployVM.vue
@@ -2602,10 +2602,10 @@ export default {
       if (this.isModernImageSelection && this.form.guestoscategoryid && !['-1', '0'].includes(this.form.guestoscategoryid)) {
         args.oscategoryid = this.form.guestoscategoryid
       }
-      if (args.keyword || (args.category && args.category !== templateFilter)) {
+      if (!args.page || args.keyword || (args.category && args.category !== templateFilter)) {
         args.page = 1
-        args.pageSize = args.pageSize || 10
       }
+      args.pageSize = args.pageSize || 10
       args.zoneid = _.get(this.zone, 'id')
       if (this.isZoneSelectedMultiArch) {
         args.arch = this.selectedArchitecture
@@ -2640,10 +2640,10 @@ export default {
       if (this.isModernImageSelection && this.form.guestoscategoryid) {
         args.oscategoryid = this.form.guestoscategoryid
       }
-      if (args.keyword || args.category !== isoFilter) {
+      if (!args.page || args.keyword || (args.category && args.category !== isoFilter)) {
         args.page = 1
-        args.pageSize = args.pageSize || 10
       }
+      args.pageSize = args.pageSize || 10
       args.zoneid = _.get(this.zone, 'id')
       if (this.isZoneSelectedMultiArch) {
         args.arch = this.selectedArchitecture

ui/src/views/compute/DeployVnfAppliance.vue
@@ -2546,10 +2546,10 @@ export default {
       if (this.isModernImageSelection && this.form.guestoscategoryid && !['-1', '0'].includes(this.form.guestoscategoryid)) {
         args.oscategoryid = this.form.guestoscategoryid
       }
-      if (args.keyword || (args.category && args.category !== templateFilter)) {
+      if (!args.page || args.keyword || (args.category && args.category !== templateFilter)) {
         args.page = 1
-        args.pageSize = args.pageSize || 10
       }
+      args.pageSize = args.pageSize || 10
       args.zoneid = _.get(this.zone, 'id')
       if (this.isZoneSelectedMultiArch) {
         args.arch = this.selectedArchitecture

ui/src/views/compute/ReinstallVm.vue
@@ -361,10 +361,10 @@ export default {
       if (this.isModernImageSelection && this.selectedGuestOsCategoryId && !['-1', '0'].includes(this.selectedGuestOsCategoryId)) {
         args.oscategoryid = this.selectedGuestOsCategoryId
       }
-      if (args.keyword || (args.category && args.category !== templateFilter)) {
+      if (!args.page || args.keyword || (args.category && args.category !== templateFilter)) {
         args.page = 1
-        args.pageSize = args.pageSize || 10
       }
+      args.pageSize = args.pageSize || 10
       args.zoneid = this.resource.zoneid
       args.templatefilter = templateFilter
       if (this.resource.arch) {