ba42d2516247fa634d843b634d6a3f96adc319c4
Wei Zhou <weizhou@apache.org>
2025-09-01 08:08:43 +0200

    packaging: add pre-check.sh (#11135)

--- scripts/installer/pre-check.sh
+++ scripts/installer/pre-check.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
@@ -15,25 +16,23 @@
 # specific language governing permissions and limitations
 # under the License.
 
-# Do not modify this file as your changes will be lost in the next CSM update.
-# If you need to add specific dependencies to this service unit do it in the
-# /etc/systemd/system/cloudstack-management.service.d/ directory
+if [ ! -f "/usr/bin/java" ];then
+    echo "ERROR: /usr/bin/java does not exist"
+    exit 1
+fi
 
-[Unit]
-Description=CloudStack Agent
-Documentation=http://www.cloudstack.org/
-Requires=libvirtd.service
-After=libvirtd.service
+JAVA_MAJOR_VERSION=$(/usr/bin/java --version | grep -oE '[0-9]+\.[0-9]+(\.[0-9]+)?' | head -n 1 |cut -d "." -f1)
 
-[Service]
-Type=simple
-EnvironmentFile=/etc/default/cloudstack-agent
-ExecStartPre=/usr/share/cloudstack-common/scripts/installer/pre-check.sh
-ExecStart=/usr/bin/java $JAVA_OPTS $JAVA_DEBUG -cp $CLASSPATH $JAVA_CLASS
-Restart=always
-RestartSec=10s
-StandardOutput=append:/var/log/cloudstack/agent/agent.out
-StandardError=append:/var/log/cloudstack/agent/agent.err
+if [ -z $JAVA_MAJOR_VERSION ];then
+    echo "WARNING: Cannot determine the JAVA version"
+    exit 0
+fi
 
-[Install]
-WantedBy=multi-user.target
+if [ "$JAVA_MAJOR_VERSION" != "17" ] && [ "$JAVA_MAJOR_VERSION" != "11" ];then
+    echo "ERROR: JAVA $JAVA_MAJOR_VERSION is not supported. Currently only JAVA 17 and JAVA 11 are supported."
+    exit 1
+fi
+
+if [ "$JAVA_MAJOR_VERSION" != "17" ];then
+    echo "WARNING: JAVA version is $JAVA_MAJOR_VERSION. JAVA 17 is recommended."
+fi
