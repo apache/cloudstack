c626a1fb82c441da274ab42be34ae5dd3f6a6648
Abhishek Kumar <abhishek.mrt22@gmail.com>
2025-09-04 10:38:24 +0530

    refactor: add null check for BroadcastDomainType retrievals
    Add null check for URI in BroadcastDomainType to prevent NullPointerException.
    When passing a null URI while enum should be reported as UnDecided, value as null.
    
    Signed-off-by: Abhishek Kumar <abhishek.mrt22@gmail.com>

--- api/src/main/java/com/cloud/network/Networks.java
+++ api/src/main/java/com/cloud/network/Networks.java
@@ -176,7 +176,7 @@ public class Networks {
          * @return the scheme as BroadcastDomainType
          */
         public static BroadcastDomainType getSchemeValue(URI uri) {
-            return toEnumValue(uri.getScheme());
+            return toEnumValue(uri == null ? null : uri.getScheme());
         }
 
         /**
@@ -190,7 +190,7 @@ public class Networks {
             if (com.cloud.dc.Vlan.UNTAGGED.equalsIgnoreCase(str)) {
                 return Native;
             }
-            return getSchemeValue(new URI(str));
+            return getSchemeValue(str == null ? null : new URI(str));
         }
 
         /**
@@ -219,7 +219,7 @@ public class Networks {
          * @return the host part as String
          */
         public String getValueFrom(URI uri) {
-            return uri.getHost();
+            return uri == null ? null : uri.getHost();
         }
 
         /**
@@ -242,7 +242,7 @@ public class Networks {
          * @throws URISyntaxException the string is not even an uri
          */
         public static String getValue(String uriString) throws URISyntaxException {
-            return getValue(new URI(uriString));
+            return getValue(uriString == null ? null : new URI(uriString));
         }
 
         /**
