7ecfb412f12433210ac6808821e2d69ae0e42e13
Wei Zhou <weizhou@apache.org>
2025-09-04 08:48:03 +0200

    server: run the vm scheduler poll task on only one management server

--- server/src/main/java/org/apache/cloudstack/vm/schedule/VMSchedulerImpl.java
+++ server/src/main/java/org/apache/cloudstack/vm/schedule/VMSchedulerImpl.java
@@ -19,12 +19,15 @@
 package org.apache.cloudstack.vm.schedule;
 
 import com.cloud.api.ApiGsonHelper;
+import com.cloud.cluster.ManagementServerHostVO;
+import com.cloud.cluster.dao.ManagementServerHostDao;
 import com.cloud.event.ActionEventUtils;
 import com.cloud.event.EventTypes;
 import com.cloud.user.User;
 import com.cloud.utils.DateUtil;
 import com.cloud.utils.component.ComponentContext;
 import com.cloud.utils.component.ManagerBase;
+import com.cloud.utils.db.Filter;
 import com.cloud.utils.db.GlobalLock;
 import com.cloud.vm.UserVmManager;
 import com.cloud.vm.VirtualMachine;
@@ -40,6 +43,7 @@ import org.apache.cloudstack.framework.jobs.AsyncJobDispatcher;
 import org.apache.cloudstack.framework.jobs.AsyncJobManager;
 import org.apache.cloudstack.framework.jobs.impl.AsyncJobVO;
 import org.apache.cloudstack.managed.context.ManagedContextTimerTask;
+import org.apache.cloudstack.utils.identity.ManagementServerNode;
 import org.apache.cloudstack.vm.schedule.dao.VMScheduleDao;
 import org.apache.cloudstack.vm.schedule.dao.VMScheduledJobDao;
 import org.apache.commons.lang.time.DateUtils;
@@ -68,6 +72,9 @@ public class VMSchedulerImpl extends ManagerBase implements VMScheduler, Configu
     private UserVmManager userVmManager;
     @Inject
     private AsyncJobManager asyncJobManager;
+    @Inject
+    private ManagementServerHostDao managementServerHostDao;
+
     private AsyncJobDispatcher asyncJobDispatcher;
     private Timer vmSchedulerTimer;
     private Date currentTimestamp;
@@ -190,6 +197,11 @@ public class VMSchedulerImpl extends ManagerBase implements VMScheduler, Configu
         final TimerTask schedulerPollTask = new ManagedContextTimerTask() {
             @Override
             protected void runInContext() {
+                ManagementServerHostVO msHost = managementServerHostDao.findOneInUpState(new Filter(ManagementServerHostVO.class, "id", false, 0L, 1L));
+                if (msHost == null || (msHost.getMsid() != ManagementServerNode.getManagementServerId())) {
+                    logger.debug("Skipping the vm scheduler poll task on this management server");
+                    return;
+                }
                 try {
                     poll(new Date());
                 } catch (final Throwable t) {
