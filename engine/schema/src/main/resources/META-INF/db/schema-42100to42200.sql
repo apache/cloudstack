aca8732102f6033f9ee60f71731665c0a0227a02
dahn <daan@onecht.net>
2025-09-22 08:09:05 +0200

    [router] make a distinction between fatal errors, warnings and unknown as healthcheck result (#10710)
    * [routers] distiction between fatal failure and warning or unknown on healthchecks
    
    * UI status for router health checks
    
    * status from scripts varied
    
    * automation signalled errors
    
    * revert removal of update sql
    
    * upgradeversion
    
    * move config item and further cleanup
    
    * handling services better
    
    * backwards compatible response
    
    ---------
    
    Co-authored-by: Daan Hoogland <dahn@apache.org>

--- engine/schema/src/main/resources/META-INF/db/schema-42100to42200.sql
+++ engine/schema/src/main/resources/META-INF/db/schema-42100to42200.sql
@@ -19,5 +19,9 @@
 -- Schema upgrade from 4.21.0.0 to 4.22.0.0
 --;
 
+
+-- health check status as enum
+CALL `cloud`.`IDEMPOTENT_CHANGE_COLUMN`('router_health_check', 'check_result', 'check_result', 'varchar(16) NOT NULL COMMENT "check executions result: SUCCESS, FAILURE, WARNING, UNKNOWN"');
+
 -- Increase length of scripts_version column to 128 due to md5sum to sha512sum change
 CALL `cloud`.`IDEMPOTENT_CHANGE_COLUMN`('cloud.domain_router', 'scripts_version', 'scripts_version', 'VARCHAR(128)');
