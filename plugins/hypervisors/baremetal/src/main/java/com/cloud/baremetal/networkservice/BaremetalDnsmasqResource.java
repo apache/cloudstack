c97662d2289dc7d5d041646adbfa2637fdad08e6
YoulongChen <30854794+YLChen-007@users.noreply.github.com>
2025-11-07 21:51:40 +0800

    Update plugins/hypervisors/baremetal/src/main/java/com/cloud/baremetal/networkservice/BaremetalDnsmasqResource.java
    Co-authored-by: dahn <daan.hoogland@gmail.com>

--- plugins/hypervisors/baremetal/src/main/java/com/cloud/baremetal/networkservice/BaremetalDnsmasqResource.java
+++ plugins/hypervisors/baremetal/src/main/java/com/cloud/baremetal/networkservice/BaremetalDnsmasqResource.java
@@ -46,7 +46,7 @@ public class BaremetalDnsmasqResource extends BaremetalDhcpResourceBase {
         com.trilead.ssh2.Connection sshConnection = null;
         try {
             super.configure(name, params);
-            logger.debug(String.format("Trying to connect to DHCP server(IP=%1$s, username=%2$s, password=%3$s)", _ip, _username, "******"));
+            logger.debug(String.format("Trying to connect to DHCP server(IP=%1$s, username=%2$s, password=******", _ip, _username));
             sshConnection = SSHCmdHelper.acquireAuthorizedConnection(_ip, _username, _password);
             if (sshConnection == null) {
                 throw new ConfigurationException(String.format("Cannot connect to DHCP server(IP=%1$s, username=%2$s, password=******", _ip, _username));
