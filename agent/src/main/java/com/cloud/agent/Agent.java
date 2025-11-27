d21aaa4271f4b4f1626f5fc5f44a7fe7ac403ac1
Suresh Kumar Anaparti <sureshkumar.anaparti@gmail.com>
2025-11-27 15:05:07 +0530

    logs update

--- agent/src/main/java/com/cloud/agent/Agent.java
+++ agent/src/main/java/com/cloud/agent/Agent.java
@@ -1231,10 +1231,11 @@ public class Agent implements HandlerFactory, IAgentControl, AgentStatusUpdater 
                 try {
                     // an issue has been found if reconnect immediately after disconnecting. please refer to https://github.com/apache/cloudstack/issues/8517
                     // wait 5 seconds before reconnecting
+                    logger.debug("Wait for 5 secs before reconnecting, disconnect task - {}", () -> getLinkLog(task.getLink()));
                     Thread.sleep(5000);
                 } catch (InterruptedException e) {
                 }
-                logger.debug("Executing disconnect task - {}", () -> getLinkLog(task.getLink()));
+                logger.debug("Executing disconnect task - {} and reconnecting", () -> getLinkLog(task.getLink()));
                 reconnect(task.getLink());
             } else if (task.getType() == Task.Type.OTHER) {
                 processOtherTask(task);
