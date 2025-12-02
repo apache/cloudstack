e79dacb78eeaf2c1223d9f93071141a336efaee7
Daan Hoogland <dahn@apache.org>
2025-12-02 16:29:55 +0100

    imports

--- engine/schema/src/test/java/com/cloud/upgrade/DatabaseUpgradeCheckerTest.java
+++ engine/schema/src/test/java/com/cloud/upgrade/DatabaseUpgradeCheckerTest.java
@@ -55,7 +55,12 @@ import com.cloud.upgrade.dao.Upgrade490to4910;
 
 import com.cloud.utils.db.TransactionLegacy;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
+
 
 @RunWith(MockitoJUnitRunner.class)
 public class DatabaseUpgradeCheckerTest {
