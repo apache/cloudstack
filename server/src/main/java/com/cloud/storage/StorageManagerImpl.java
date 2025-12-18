e8200a0b749ed2a53ec32e66da2ef02ff97e7a6c
Fabricio Duarte <fabricio.duarte.jr@gmail.com>
2025-12-18 06:53:27 -0300

    Prioritize copying templates from other secondary storages instead of downloading them (#10363)
    * Prioritize copying templates from other secondary storages instead of downloading them
    
    * Treat some corner cases

--- server/src/main/java/com/cloud/storage/StorageManagerImpl.java
+++ server/src/main/java/com/cloud/storage/StorageManagerImpl.java
@@ -4194,7 +4194,8 @@ public class StorageManagerImpl extends ManagerBase implements StorageManager, C
                 VmwareAllowParallelExecution,
                 DataStoreDownloadFollowRedirects,
                 AllowVolumeReSizeBeyondAllocation,
-                StoragePoolHostConnectWorkers
+                StoragePoolHostConnectWorkers,
+                COPY_PUBLIC_TEMPLATES_FROM_OTHER_STORAGES
         };
     }
 
