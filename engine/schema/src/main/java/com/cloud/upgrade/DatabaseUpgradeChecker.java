8db065a14eb41bab0fb3420e66ee96722f1ed6ad
Manoj Kumar <manojkr.itbhu@gmail.com>
2026-01-23 21:04:52 +0530

    limit iso filename to have 251 chars at max (#12430)

--- engine/schema/src/main/java/com/cloud/upgrade/DatabaseUpgradeChecker.java
+++ engine/schema/src/main/java/com/cloud/upgrade/DatabaseUpgradeChecker.java
@@ -33,6 +33,7 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import com.cloud.upgrade.dao.Upgrade42020to42030;
 import com.cloud.utils.FileUtil;
 import org.apache.cloudstack.utils.CloudStackVersion;
 import org.apache.commons.lang3.StringUtils;
@@ -236,6 +237,7 @@ public class DatabaseUpgradeChecker implements SystemIntegrityChecker {
                 .next("4.19.0.0", new Upgrade41900to41910())
                 .next("4.19.1.0", new Upgrade41910to42000())
                 .next("4.20.0.0", new Upgrade42000to42010())
+                .next("4.20.2.0", new Upgrade42020to42030())
                 .build();
     }
 
