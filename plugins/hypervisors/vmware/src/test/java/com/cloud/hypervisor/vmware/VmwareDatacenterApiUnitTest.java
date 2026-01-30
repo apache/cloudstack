86dfab51a19ec7461a0733db6011c039c99373e3
Pearl Dsilva <pearl1594@gmail.com>
2026-01-29 22:38:41 -0500

    fix test

--- plugins/hypervisors/vmware/src/test/java/com/cloud/hypervisor/vmware/VmwareDatacenterApiUnitTest.java
+++ plugins/hypervisors/vmware/src/test/java/com/cloud/hypervisor/vmware/VmwareDatacenterApiUnitTest.java
@@ -240,17 +240,13 @@ public class VmwareDatacenterApiUnitTest {
         closeable.close();
     }
 
-    //@Test(expected = InvalidParameterValueException.class)
-    @Test
-    @Ignore
+    @Test(expected = InvalidParameterValueException.class)
     public void testAddVmwareDcToInvalidZone() throws ResourceInUseException, IllegalArgumentException, DiscoveryException, Exception {
         Mockito.when(addCmd.getZoneId()).thenReturn(2L);
         _vmwareDatacenterService.addVmwareDatacenter(addCmd);
     }
 
-    //@Test(expected = ResourceInUseException.class)
-    @Test
-    @Ignore
+    @Test(expected = ResourceInUseException.class)
     public void testAddVmwareDcToZoneWithClusters() throws ResourceInUseException, IllegalArgumentException, DiscoveryException, Exception {
         Mockito.when(_clusterDao.listByZoneId(1L)).thenReturn(clusterList);
         _vmwareDatacenterService.addVmwareDatacenter(addCmd);
@@ -269,47 +265,37 @@ public class VmwareDatacenterApiUnitTest {
     }
 
     //@Test(expected = ResourceInUseException.class)
-    @Test
     @Ignore
     public void testAddVmwareDcToZoneWithVmwareDc() throws ResourceInUseException, IllegalArgumentException, DiscoveryException, Exception {
         Mockito.when(_vmwareDcDao.getVmwareDatacenterByNameAndVcenter(vmwareDcName, vCenterHost)).thenReturn(vmwareDcs);
         _vmwareDatacenterService.addVmwareDatacenter(addCmd);
     }
 
-    //@Test(expected = InvalidParameterValueException.class)
-    @Test
-    @Ignore
+    @Test(expected = InvalidParameterValueException.class)
     public void testAddVmwareDcWithNullUser() throws ResourceInUseException, IllegalArgumentException, DiscoveryException, Exception {
         Mockito.when(addCmd.getUsername()).thenReturn(null);
         _vmwareDatacenterService.addVmwareDatacenter(addCmd);
     }
 
-    //@Test(expected = InvalidParameterValueException.class)
-    @Test
-    @Ignore
+    @Test(expected = InvalidParameterValueException.class)
     public void testAddVmwareDcWithNullPassword() throws ResourceInUseException, IllegalArgumentException, DiscoveryException, Exception {
         Mockito.when(addCmd.getPassword()).thenReturn(null);
         _vmwareDatacenterService.addVmwareDatacenter(addCmd);
     }
 
-    //@Test(expected = InvalidParameterValueException.class)
-    @Test
-    @Ignore
+    @Test(expected = InvalidParameterValueException.class)
     public void testAddVmwareDcWithNullUrl() throws ResourceInUseException, IllegalArgumentException, DiscoveryException, Exception {
         Mockito.when(addCmd.getVcenter()).thenReturn(null);
         _vmwareDatacenterService.addVmwareDatacenter(addCmd);
     }
 
-    //@Test(expected = InvalidParameterValueException.class)
-    @Test
-    @Ignore
+    @Test(expected = InvalidParameterValueException.class)
     public void testAddVmwareDcWithNullDcName() throws ResourceInUseException, IllegalArgumentException, DiscoveryException, Exception {
         Mockito.when(addCmd.getName()).thenReturn(null);
         _vmwareDatacenterService.addVmwareDatacenter(addCmd);
     }
 
     //@Test(expected = CloudRuntimeException.class)
-    @Test
     @Ignore
     public void testReAddVmwareDc() throws ResourceInUseException, IllegalArgumentException, DiscoveryException, Exception {
         Mockito.when(_vmwareDcZoneMapDao.findByZoneId(1L)).thenReturn(dcZoneMap);
