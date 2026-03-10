098ba06e62b8defdd9b1820a5c7bdd0ad48e49d5
Aaron Chung <aaron_chung@apple.com>
2026-03-10 16:46:14 -0700

    Add debug logging exception

--- api/src/main/java/org/apache/cloudstack/acl/APIChecker.java
+++ api/src/main/java/org/apache/cloudstack/acl/APIChecker.java
@@ -24,10 +24,13 @@ import com.cloud.utils.component.Adapter;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.log4j.Logger;
+
 /**
  * APICheckers is designed to verify the ownership of resources and to control the access to APIs.
  */
 public interface APIChecker extends Adapter {
+    Logger LOGGER = Logger.getLogger(APIChecker.class);
     // Interface for checking access for a role using apiname
     // If true, apiChecker has checked the operation
     // If false, apiChecker is unable to handle the operation or not implemented
@@ -51,7 +54,7 @@ public interface APIChecker extends Adapter {
                 checkAccess(account, apiName);
                 allowedApis.add(apiName);
             } catch (PermissionDeniedException e) {
-                // not allowed, skip
+                LOGGER.debug("Account [" + account + "] is not allowed to access API [" + apiName + "]", e);
             }
         }
         return allowedApis;
