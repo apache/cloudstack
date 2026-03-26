4c6157bae3ab0c2564d36d2496697d6b5890c59c
Erik Böck <89930804+erikbocks@users.noreply.github.com>
2026-03-26 09:44:20 -0300

    Apply suggestion from @GaOrtiga
    Co-authored-by: GaOrtiga <49285692+GaOrtiga@users.noreply.github.com>

--- engine/schema/src/main/resources/META-INF/db/schema-42200to42210.sql
+++ engine/schema/src/main/resources/META-INF/db/schema-42200to42210.sql
@@ -37,9 +37,9 @@ UPDATE `cloud`.`configuration` SET description = 'True if the management server 
 UPDATE `cloud`.`vm_template` SET guest_os_id = 99 WHERE name = 'kvm-default-vm-import-dummy-template';
 
 -- Update `user.password.reset.mail.template` configuration value to match new logic
-update configuration
-set value = 'Hello {{username}}!\nYou have requested to reset your password. Please click the following link to reset your password:\n{{{resetLink}}}\nIf you did not request a password reset,please ignore this email.\n\nRegards,\nThe CloudStack Team'
-where value in (
+UPDATE configuration
+SET value = 'Hello {{username}}!\nYou have requested to reset your password. Please click the following link to reset your password:\n{{{resetLink}}}\nIf you did not request a password reset,please ignore this email.\n\nRegards,\nThe CloudStack Team'
+WHERE value IN (
     'Hello {{username}}!\nYou have requested to reset your password. Please click the following link to reset your password:\nhttp://{{{resetLink}}}\nIf you did not request a password reset,please ignore this email.\n\nRegards,\nThe CloudStack Team',
     'Hello {{username}}!\nYou have requested to reset your password. Please click the following link to reset your password:\n{{{domainUrl}}}{{{resetLink}}}\nIf you did not request a password reset,please ignore this email.\n\nRegards,\nThe CloudStack Team'
 )
