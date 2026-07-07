ef7a8cc9e85ff92aa019b53a78d224d03e97897d
Wei Zhou <weizhou@apache.org>
2026-07-07 10:49:18 +0200

    fix since

--- api/src/main/java/org/apache/cloudstack/api/response/VpcResponse.java
+++ api/src/main/java/org/apache/cloudstack/api/response/VpcResponse.java
@@ -166,7 +166,7 @@ public class VpcResponse extends BaseResponseWithAnnotations implements Controll
     private String ipv4Routing;
 
     @SerializedName(ApiConstants.IPV6_ROUTING)
-    @Param(description = "The IPv6 routing mode of VPC", since = "4.22.1")
+    @Param(description = "The IPv6 routing mode of VPC", since = "4.22.2")
     private String ipv6Routing;
 
     @SerializedName(ApiConstants.IPV4_ROUTES)
