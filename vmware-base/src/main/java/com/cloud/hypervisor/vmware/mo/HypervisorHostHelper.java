e8eeed21d5c404684baaeba714f3eb912fa1921a
Dogface2k <100990646+Dogface2k@users.noreply.github.com>
2026-07-30 16:22:57 +0100

    VMware: wait for NSX DV port group readiness

--- vmware-base/src/main/java/com/cloud/hypervisor/vmware/mo/HypervisorHostHelper.java
+++ vmware-base/src/main/java/com/cloud/hypervisor/vmware/mo/HypervisorHostHelper.java
@@ -649,7 +649,8 @@ public class HypervisorHostHelper {
                 bWaitPortGroupReady = false;
             } else if (BroadcastDomainType.NSX == broadcastDomainType && Objects.nonNull(netName)){
                 networkName = netName;
-                bWaitPortGroupReady = false;
+                // NSX creates the segment outside vCenter, where its backing DV port group may not be immediately visible.
+                bWaitPortGroupReady = true;
             } else {
                 boolean dvSwitchSupportNewPolicies = (isFeatureSupportedInVcenterApiVersion(vcApiVersion, MINIMUM_VCENTER_API_VERSION_WITH_DVS_NEW_POLICIES_SUPPORT)
                         && isVersionEqualOrHigher(dvSwitchVersion, MINIMUM_DVS_VERSION_WITH_NEW_POLICIES_SUPPORT));
