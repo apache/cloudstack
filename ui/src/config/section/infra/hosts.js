9e9dddb3f81ae18324c0360dba25ce9924d9ea18
Wei Zhou <weizhou@apache.org>
2026-08-05 13:40:28 +0200

    UI: add last connected of host

--- ui/src/config/section/infra/hosts.js
+++ ui/src/config/section/infra/hosts.js
@@ -44,7 +44,7 @@ export default {
     fields.push('zonename')
     return fields
   },
-  details: ['name', 'id', 'resourcestate', 'ipaddress', 'hypervisor', 'arch', 'type', 'clustername', 'podname', 'zonename', 'disconnected', 'created'],
+  details: ['name', 'id', 'resourcestate', 'ipaddress', 'hypervisor', 'arch', 'type', 'clustername', 'podname', 'zonename', 'lastpinged', 'disconnected', 'created'],
   tabs: [{
     name: 'details',
     component: shallowRef(defineAsyncComponent(() => import('@/components/view/DetailsTab.vue')))
