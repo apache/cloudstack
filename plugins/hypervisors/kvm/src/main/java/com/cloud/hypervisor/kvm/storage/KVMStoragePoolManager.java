028b52dade4ec3e0442ff1428268ba04e2b7739a
sandeeplocharla <85344604+sandeeplocharla@users.noreply.github.com>
2026-08-06 15:32:00 +0530

    Fix for NFS3 primary storage pool is failing to come out of maintenan… (#71)
    Fix for NFS3 primary storage pool is failing to come out of maintenance
    mode
    
    ### Description
    
    This PR has the following:
    
    1. For NetworkFileSystem type, libvirtd handles mounting and unmounting
    of nfs mount [Ref: https://libvirt.org/storage.html]
    2. KVM adaptor hasn't overridden `deleteStoragePool` method leading to
    only just the change in the DB. This was leading to error in case of
    `Cancel Maintenance` as the pool already exists with the host.
    3. Also, when `Enable Maintenance` call comes, it was first removing the
    nfs mount, which was causing the libvirtd to error out during `Destroy
    Pool` call as the mount wasn't available.
    
    <!---
    *******************************************************************************
    -->
    <!--- NOTE: AUTOMATION USES THE DESCRIPTIONS TO SET LABELS AND PRODUCE
    DOCUMENTATION. -->
    <!--- PLEASE PUT AN 'X' in only **ONE** box -->
    <!---
    *******************************************************************************
    -->
    
    ### Types of changes
    
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to change)
    - [ ] New feature (non-breaking change which adds functionality)
    - [X] Bug fix (non-breaking change which fixes an issue)
    - [ ] Enhancement (improves an existing feature and functionality)
    - [ ] Cleanup (Code refactoring and cleanup, that may add test cases)
    - [ ] Build/CI
    - [ ] Test (unit or integration test code)
    
    ### Feature/Enhancement Scale or Bug Severity
    
    #### Feature/Enhancement Scale
    
    - [] Major
    - [ ] Minor
    
    #### Bug Severity
    
    - [ ] BLOCKER
    - [ ] Critical
    - [X] Major
    - [ ] Minor
    - [ ] Trivial
    
    ### Screenshots (if appropriate):
    
    ### How Has This Been Tested?
    
    `Previously:`
    
    <img width="1035" height="362" alt="Screenshot 2026-07-08 at 8 00 05 AM"
    src="https://github.com/user-attachments/assets/91a3bcd7-cb6f-4b53-81f5-0851abd337ee"
    />
    <img width="1301" height="365" alt="Screenshot 2026-07-08 at 7 58 31 AM"
    src="https://github.com/user-attachments/assets/5444b1ae-5cfa-4709-a48f-107b8a257e29"
    />
    <img width="1035" height="362" alt="Screenshot 2026-07-08 at 8 00 29 AM"
    src="https://github.com/user-attachments/assets/bbeaece1-8795-46ef-b1c7-9a9b24910b21"
    />
    <img width="1301" height="327" alt="Screenshot 2026-07-08 at 8 01 37 AM"
    src="https://github.com/user-attachments/assets/c9086ef2-066c-4661-a684-c4788e471fdc"
    />
    
    So, clearly though the nfs mount was removed, the libvirtd still has the
    pool details with it.
    
    `Now:`
    
    <img width="1042" height="351" alt="Screenshot 2026-07-08 at 8 30 46 AM"
    src="https://github.com/user-attachments/assets/59d2c629-41bd-4991-9988-3989dd3497aa"
    />
    <img width="1300" height="382" alt="Screenshot 2026-07-08 at 8 31 18 AM"
    src="https://github.com/user-attachments/assets/9b774243-8329-4fb8-babb-a0891d20fc6a"
    />
    <img width="1043" height="355" alt="Screenshot 2026-07-08 at 8 32 41 AM"
    src="https://github.com/user-attachments/assets/5207517a-c2d0-4935-adf0-db70877b79fa"
    />
    <img width="1299" height="312" alt="Screenshot 2026-07-08 at 8 33 02 AM"
    src="https://github.com/user-attachments/assets/fd5014bb-8d46-423b-8039-b2caa391b885"
    />
    <img width="1049" height="340" alt="Screenshot 2026-07-08 at 8 34 32 AM"
    src="https://github.com/user-attachments/assets/ec951783-a90e-4118-89d2-8d62b28f138e"
    />
    <img width="1299" height="365" alt="Screenshot 2026-07-08 at 8 33 46 AM"
    src="https://github.com/user-attachments/assets/a64a2c86-9089-4f67-a345-90a0dbfa88fc"
    />

--- plugins/hypervisors/kvm/src/main/java/com/cloud/hypervisor/kvm/storage/KVMStoragePoolManager.java
+++ plugins/hypervisors/kvm/src/main/java/com/cloud/hypervisor/kvm/storage/KVMStoragePoolManager.java
@@ -448,7 +448,7 @@ public class KVMStoragePoolManager {
         if (type == StoragePoolType.NetworkFilesystem) {
             _haMonitor.removeStoragePool(uuid);
         }
-        boolean deleteStatus = adaptor.deleteStoragePool(uuid);;
+        boolean deleteStatus = adaptor.deleteStoragePool(uuid);
         synchronized (_storagePools) {
             _storagePools.remove(uuid);
         }
@@ -457,10 +457,12 @@ public class KVMStoragePoolManager {
 
     public boolean deleteStoragePool(StoragePoolType type, String uuid, Map<String, String> details) {
         StorageAdaptor adaptor = getStorageAdaptor(type);
+        // For NetworkFilesystem, libvirt will take care of unmounting the nfs mount. If nfs mount has been removed before libvirt's pool
+        // delete, libvirt will throw an error.
+        boolean deleteStatus = adaptor.deleteStoragePool(uuid, details);
         if (type == StoragePoolType.NetworkFilesystem) {
             _haMonitor.removeStoragePool(uuid);
         }
-        boolean deleteStatus = adaptor.deleteStoragePool(uuid, details);
         synchronized (_storagePools) {
             _storagePools.remove(uuid);
         }
